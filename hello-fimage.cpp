b3a4e26b52d52ddbe23f71d77d5898c5e0f85c7c
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-03-18 17:47:25 -0400

    Trivial hello-fimage

--- hello-fimage.cpp
+++ hello-fimage.cpp
@@ -2,10 +2,28 @@
 
 int main(int argc, char **argv) {
     
-    FImage im(2);
+    int integers[] = { 1, 2, 3, 4 };
+    
+    FImage im(1,1);
     Var x(0, im.size[0]);
-    im(x) = x; ///im.size[0];
+    Var y(0, im.size[1]);
+    //x.unroll(4);
+    //x.vectorize(4);
+    for (int i = 0; i < im.size[0]; i++) {
+        for (int j = 0; j < im.size[1]; j++) {
+            if (j % 2) im(i,j) = -integers[i];
+            else im(i,j) = integers[i % sizeof(integers)/sizeof(integers[0])];
+        }
+    }
+    im(x,y) = im(x,y) * 2;
     im.evaluate();
+    
+    for (int j = 0; j < im.size[1]; j++) {
+        for (int i = 0; i < im.size[0]; i++) {
+            printf("%f ", im(i,j));
+        }
+        printf("\n");
+    }
     
     // Slightly more complex example
     #if 0
