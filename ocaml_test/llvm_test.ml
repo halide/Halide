800831dc1cc5d60ac6ee9fe091cdfb8d75fdcdbd
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-05-14 19:58:55 -0400

    Extended llvm_test.ml with SSE intrinsics. Works.
    Former-commit-id: 0ef318164bbdc278a7f7b0ff19a19c461b1ea13e

--- ocaml_test/llvm_test.ml
+++ ocaml_test/llvm_test.ml
@@ -10,6 +10,8 @@ let main filename =
 
   let i8_t  = i8_type c in
   let i32_t = i32_type c in
+  let f32_t = float_type c in
+  let f32x4_t = vector_type f32_t 4 in
 
   let m = create_module c filename in
 
@@ -22,9 +24,28 @@ let main filename =
    declare_function "puts"
      (function_type i32_t [|pointer_type i8_t|]) m in
 
+  (* declare void @printv(<4 x float>)
+   * nonce extern which forces preservation of vector results *)
+  let printv =
+   declare_function "printv"
+     (function_type (void_type c) [|f32x4_t|]) m in
+
+  (* declare <4 x float> @llvm.x86.sse.sqrt.ps(<4 x float>) nounwind readnone *)
+  let sqrtps =
+   declare_function "llvm.x86.sse.sqrt.ps"
+     (function_type f32x4_t [|f32x4_t|]) m in
+
   (* define i32 @main() { entry: *)
   let main = define_function "main" (function_type i32_t [| |]) m in
   let at_entry = builder_at_end c (entry_block main) in
+
+  (*%sqrtps = call <4 x float> @llvm.x86.sse.sqrt.ps(<4 x float> <float 1.000000e+00, float 2.000000e+00, float 3.000000e+00, float 4.000000e+00>)*)
+  let cv1234 = const_vector [| const_float f32_t 1.0; const_float f32_t 2.0;
+  const_float f32_t 3.0; const_float f32_t 4.0 |] in
+  let sqrt = build_call sqrtps [| cv1234 |] "sqrtps" at_entry in
+  
+  (* call void printv(sqrtps) *)
+  ignore (build_call printv [| sqrt |] "" at_entry);
 
   (* %tmp = getelementptr [14 x i8]* @greeting, i32 0, i32 0 *)
   let zero = const_int i32_t 0 in
