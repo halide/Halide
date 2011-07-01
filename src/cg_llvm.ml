7afa7f237308d8d73498e651957b484ac0bc8a7e
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-07-01 18:57:11 -0400

    Refactored codegen functions under enclosed scope.
    Encapsulated the recursive codegen functions in a local scope which is implicitly closed over the context/module/buffer pointers, removing the need to pass around and unpack context objects all over the place.

--- src/cg_llvm.ml
+++ src/cg_llvm.ml
@@ -1,124 +1,121 @@
 open Ir
 open Llvm
 
-(* TODO idea: could we encapsulate the recursive codegen functions in a local
- * scope within the main codegen function which was implicitly closed over the
- * context/module pointers? *)
-
 let dbgprint = true
 
 let entrypoint_name = "_im_main"
 let caml_entrypoint_name = entrypoint_name ^ "_caml_runner"
 
-type context = llcontext * llmodule * llbuilder
+exception UnsupportedType of val_type
+exception MissingEntrypoint
+exception UnimplementedInstruction
 
-let int_imm_t c = i32_type c
-let float_imm_t c = float_type c
 let buffer_t c = pointer_type (i8_type c)
 
-exception UnsupportedType of val_type
-let type_of_val_type ctx t = match ctx with (c,_,_) ->
-  match t with
-    | UInt(1) | Int(1) -> i1_type c
-    | UInt(8) | Int(8) -> i8_type c
-    | UInt(16) | Int(16) -> i16_type c
-    | UInt(32) | Int(32) -> i32_type c
-    | UInt(64) | Int(64) -> i64_type c
-    | Float(32) -> float_type c
-    | Float(64) -> double_type c
-    | _ -> raise (UnsupportedType(t))
+(* Function to encapsulate shared state for primary codegen *)
+let codegen_root (c:llcontext) (m:llmodule) (b:llbuilder) (s:stmt) =
 
-let type_of_expr ctx e = type_of_val_type ctx (val_type_of_expr e)
+  let int_imm_t = i32_type c in
+  let float_imm_t = float_type c in
 
-let buffer_name b = "buf" ^ string_of_int b
+  let type_of_val_type t =
+    match t with
+      | UInt(1) | Int(1) -> i1_type c
+      | UInt(8) | Int(8) -> i8_type c
+      | UInt(16) | Int(16) -> i16_type c
+      | UInt(32) | Int(32) -> i32_type c
+      | UInt(64) | Int(64) -> i64_type c
+      | Float(32) -> float_type c
+      | Float(64) -> double_type c
+      | _ -> raise (UnsupportedType(t))
+  in
 
-exception MissingEntrypoint
-let ptr_to_buffer ctx b = match ctx with (c,m,_) ->
-  (* TODO: put buffers in their own memory spaces *)
-  match lookup_function entrypoint_name m with
-    | Some(f) -> param f (b-1)
-    | None -> raise (MissingEntrypoint)
+  let ptr_to_buffer buf =
+    (* TODO: put buffers in their own memory spaces *)
+    match lookup_function entrypoint_name m with
+      | Some(f) -> param f (buf-1)
+      | None -> raise (MissingEntrypoint)
+  in
 
-(* this should throw errors if used *)
-(* TODO: remove when codegen is ~complete *)
-let nop ctx = match ctx with (_,_,b) ->
-  build_ret_void b
+  (* Convention: codegen functions unpack context into c[ontext], m[odule],
+   * b[uffer], if they need them, with pattern-matching.
+   * TODO: cleaner way to carry and match this context state? It may grow... *)
 
-(* Convention: codegen functions unpack context into c[ontext], m[odule],
- * b[uffer], if they need them, with pattern-matching.
- * TODO: cleaner way to carry and match this context state? It may grow... *)
+  let rec codegen_expr e =
+    match e with
+      (* constants *)
+      | IntImm(i) | UIntImm(i) -> const_int (int_imm_t) i
+      | FloatImm(f) -> const_float (float_imm_t) f
 
-let rec codegen_expr ctx e = match ctx with (c,_,b) ->
-  match e with
-    (* constants *)
-    | IntImm(i) | UIntImm(i) -> const_int (int_imm_t c) i
-    | FloatImm(f) -> const_float (float_imm_t c) f
+      (* TODO: codegen Cast *)
+      | Cast(t,e) -> codegen_cast t e
 
-    (* TODO: codegen Cast *)
-    | Cast(t,e) -> codegen_cast ctx t e
+      (* arithmetic *)
+      | Add(_, (l, r)) -> build_add (codegen_expr l) (codegen_expr r) "" b
 
-    (* arithmetic *)
-    | Add(_, (l, r)) -> build_add (codegen_expr ctx l) (codegen_expr ctx r) "" b
+      (* memory *)
+      | Load(t, mr) -> build_load (codegen_memref mr t) "" b
 
-    (* memory *)
-    | Load(t, mr) -> build_load (codegen_memref ctx mr t) "" b
+      (* TODO: fill out other ops *)
+      | _ -> raise UnimplementedInstruction
 
-    (* TODO: fill out other ops *)
-    | _ -> nop ctx
+  and codegen_cast t e =
+    match (val_type_of_expr e, t) with
 
-and codegen_cast ctx t e = match ctx with (c,_,b) ->
-  match (val_type_of_expr e, t) with
+      | (UInt(fbits),Int(tbits)) when fbits > tbits ->
+          (* truncate to t-1 bits, then zero-extend to t bits to avoid sign bit *)
+          build_zext
+            (build_trunc (codegen_expr e) (integer_type c (tbits-1)) "" b)
+            (integer_type c tbits) "" b
 
-    | (UInt(fbits),Int(tbits)) when fbits > tbits ->
-        (* truncate to t-1 bits, then zero-extend to t bits to avoid sign bit *)
-        build_zext
-          (build_trunc (codegen_expr ctx e) (integer_type c (tbits-1)) "" b)
-          (integer_type c tbits) "" b
+      | (UInt(fbits),Int(tbits)) when fbits < tbits ->
+          build_zext (codegen_expr e) (type_of_val_type t) "" b
 
-    | (UInt(fbits),Int(tbits)) when fbits < tbits ->
-        build_zext (codegen_expr ctx e) (type_of_val_type ctx t) "" b
+      | (Int(fbits),UInt(tbits)) when fbits > tbits ->
+          build_trunc (codegen_expr e) (type_of_val_type t) "" b
 
-    | (Int(fbits),UInt(tbits)) when fbits > tbits ->
-        build_trunc (codegen_expr ctx e) (type_of_val_type ctx t) "" b
+      | (Int(fbits),UInt(tbits)) when fbits < tbits ->
+          (* truncate to f-1 bits, then zero-extend to t bits to avoid sign bit *)
+          build_zext
+            (build_trunc (codegen_expr e) (integer_type c (fbits-1)) "" b)
+            (integer_type c tbits) "" b
 
-    | (Int(fbits),UInt(tbits)) when fbits < tbits ->
-        (* truncate to f-1 bits, then zero-extend to t bits to avoid sign bit *)
-        build_zext
-          (build_trunc (codegen_expr ctx e) (integer_type c (fbits-1)) "" b)
-          (integer_type c tbits) "" b
+      | (UInt(fbits),Int(tbits)) | (Int(fbits),UInt(tbits)) when fbits < tbits ->
+          (* do nothing *)
+          codegen_expr e
+      (*
+      LLVM Reference:
+       Instruction::CastOps opcode =
+          (SrcBits == DstBits ? Instruction::BitCast :
+           (SrcBits > DstBits ? Instruction::Trunc :
+            (isSigned ? Instruction::SExt : Instruction::ZExt)));
+       *)
+   
+      (* build_intcast in the C/OCaml interface assumes signed, so only
+       * works for Int *)
+      | (Int(_),Int(_)) -> build_intcast (codegen_expr e) (type_of_val_type t) "" b
 
-    | (UInt(fbits),Int(tbits)) | (Int(fbits),UInt(tbits)) when fbits < tbits ->
-        (* do nothing *)
-        codegen_expr ctx e
-(*
-LLVM Reference:
- Instruction::CastOps opcode =
-    (SrcBits == DstBits ? Instruction::BitCast :
-     (SrcBits > DstBits ? Instruction::Trunc :
-      (isSigned ? Instruction::SExt : Instruction::ZExt)));
- *)
- 
-    (* build_intcast in the C/OCaml interface assumes signed, so only
-     * works for Int *)
-    | (Int(_),Int(_)) -> build_intcast (codegen_expr ctx e) (type_of_val_type ctx t) "" b
+      (* TODO: remaining casts *)
+      | _ -> raise UnimplementedInstruction
 
-    (* TODO: remaining casts *)
-    | _ -> nop ctx
+  and codegen_stmt s =
+    match s with
+      | Store(e, mr) ->
+          let ptr = codegen_memref mr (val_type_of_expr e) in
+            build_store (codegen_expr e) ptr b
+      | _ -> build_ret_void b (* TODO: this is our simplest NOP *)
 
-and codegen_stmt ctx s = match ctx with (_,_,b) ->
-  match s with
-    | Store(e, mr) ->
-        let ptr = codegen_memref ctx mr (val_type_of_expr e) in
-          build_store (codegen_expr ctx e) ptr b
-    | _ -> build_ret_void b (* TODO: this is our simplest NOP *)
+  and codegen_memref mr vt =
+    (* load the global buffer** *)
+    let base = ptr_to_buffer mr.buf in
+    (* build getelementpointer into buffer *)
+    let ptr = build_gep base [| codegen_expr mr.idx |] "" b in
+      (* cast pointer to pointer-to-target-type *)
+      build_pointercast ptr (pointer_type (type_of_val_type vt)) "" b
+  in
 
-and codegen_memref ctx mr vt = match ctx with (_,_,b) ->
-  (* load the global buffer** *)
-  let base = ptr_to_buffer ctx mr.buf in
-  (* build getelementpointer into buffer *)
-  let ptr = build_gep base [| codegen_expr ctx mr.idx |] "" b in
-    (* cast pointer to pointer-to-target-type *)
-    build_pointercast ptr (pointer_type (type_of_val_type ctx vt)) "" b
+    codegen_stmt s
+
 
 module BufferSet = Set.Make (
 struct
@@ -182,7 +179,7 @@ let codegen c s =
   let b = builder_at_end c (entry_block main) in
 
     (* codegen body *)
-    ignore (codegen_stmt (c,m,b) s);
+    ignore (codegen_root c m b s);
 
     (* return void from main *)
     ignore (build_ret_void b);
@@ -214,16 +211,6 @@ let codegen_to_file filename s =
 (*
  * Wrappers
  *)
-(* ba is an llvalue of the pointer generated by:
- *   GenericValue.of_pointer some_bigarray_object *)
-let codegen_bigarray_to_buffer ctx (ba:llvalue) = match ctx with (c,_,b) ->
-  (* fetch object pointer = ((void* )val)+1 *)
-  let field_ptr = build_gep ba [| const_int (i32_type c) 1 |] "" b in
-  (* deref object pointer *)
-  let ptr = build_load field_ptr "" b in
-    (* cast to buffer_t for passing into im function *)
-    build_pointercast ptr (buffer_t c) "" b
-
 let codegen_caml_wrapper c m f =
 
   let is_buffer p = type_of p = buffer_t c in
@@ -241,10 +228,21 @@ let codegen_caml_wrapper c m f =
 
   let b = builder_at_end c (entry_block wrapper) in
 
+  (* ba is an llvalue of the pointer generated by:
+   *   GenericValue.of_pointer some_bigarray_object *)
+  let codegen_bigarray_to_buffer (ba:llvalue) =
+    (* fetch object pointer = ((void* )val)+1 *)
+    let field_ptr = build_gep ba [| const_int (i32_type c) 1 |] "" b in
+    (* deref object pointer *)
+    let ptr = build_load field_ptr "" b in
+      (* cast to buffer_t for passing into im function *)
+      build_pointercast ptr (buffer_t c) "" b
+  in
+
   let args = Array.mapi 
                (fun i p ->
                   if is_buffer p then
-                    codegen_bigarray_to_buffer (c,m,b) (param wrapper i)
+                    codegen_bigarray_to_buffer (param wrapper i)
                   else
                     param wrapper i)
                (params f) in
