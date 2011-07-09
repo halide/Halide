c4474cbf8317958dbcea8fd5570f8479d39206c5
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-07-09 01:22:02 -0400

    Factored out common run-program-on-image pattern
    Former-commit-id: 3f43bfa54ca71c5fe6fe377e53378ff82edd57f0

--- src/cg_test.ml
+++ src/cg_test.ml
@@ -21,21 +21,8 @@ let outref = { buf=outbuf; idx=UIntImm(0) }
 let v = Vector(UInt(8), vecwidth)
 let load = Load (v, {buf = inbuf; idx = x})
 let store vec = Store(vec, {buf = outbuf; idx = x})
-let prgm len = Map((xdom len), store (Add(v, (load, load))))
- 
+let prgm w h c = Map((xdom (w*h*c)), store (Add(v, (load, load))))
+
+
 let () =
-  (* load test.png into input and output arrays *)
-  let (w,h,inarr) = Imageio.load "test.png" in
-  let outarr = Util.copy_bigarray inarr in
-
-  (* instantiate the program with x range = image size *)
-  let p = prgm (w*h*3) in
-
-    (* run the program *)
-    Runner.run p [| inarr; outarr |];
-
-    (* save outarr to test.out.png *)
-    Imageio.save outarr w h "test.out.png";
-
-    (* save bitcode for debugging *)
-    Cg_llvm.codegen_to_file "cg_test.bc" p;
+  Runner.run_on_image prgm "test.png" "cg_test.png" ~dbgfile:"cg_test.bc"
--- src/runner.ml
+++ src/runner.ml
@@ -2,7 +2,7 @@ open Llvm_executionengine
 
 (* TODO: support typed args array by defining `argument` algebraic type and
 * defining generic_value_of_argument *)
-let run prgm args =
+let run (prgm:Ir.stmt) args =
   (* JITing requires initializing targetdata to the native target, or else 
    * subtle endianness and similar bugs will crop up in generated code *)
   ignore (initialize_native_target ());
@@ -15,3 +15,25 @@ let run prgm args =
   ignore (
     ExecutionEngine.run_function f (Array.map GenericValue.of_pointer args) ee
   )
+
+(* the program template type  *)
+type program_template = int -> int -> int -> Ir.stmt
+
+(* Run the program, parameterized on image w,h,channels, on the image loaded
+ * from infile, saving the results to outfile, and writing  *)
+let run_on_image (prgm:program_template) ?(dbgfile = "") infile outfile = 
+  (* load test.png into input and output arrays *)
+  let (w,h,inarr) = Imageio.load infile in
+  let outarr = Util.copy_bigarray inarr in
+
+  (* instantiate the program with image w,h,channels *)
+  let p = prgm w h 3 in
+
+    (* save bitcode for debugging *)
+    if not (dbgfile = "") then Cg_llvm.codegen_to_file dbgfile p;
+
+    (* run the program *)
+    run p [| inarr; outarr |];
+
+    (* save outarr to test.out.png *)
+    Imageio.save outarr w h outfile
