9d213fb83480072fdd4500e98e9c45ae5df78b13
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-07-09 00:50:58 -0400

    Refactored cg_test to run over whole image.
    General cleanup of test program structure.

--- src/cg_test.ml
+++ src/cg_test.ml
@@ -3,7 +3,8 @@ open Ir_printer
 
 let dom nm rn = { name = nm; range = rn }
 
-let xdom = dom "x" (0,16)
+let vecwidth = 16
+let xdom len = dom "x" (0,len/vecwidth)
 
 let x = Var("x")
 
@@ -17,18 +18,24 @@ let outref = { buf=outbuf; idx=UIntImm(0) }
 (*let prgm = Store( Add( i64, ( Cast( i64, IntImm( 1 ) ), Load( i64, inref ) ) ), outref ) *)
 (*let prgm = Store( Load( i64, inref ), outref )*)
 (*let prgm = Store( Div( f32, ( Cast( f32, FloatImm( 17.4 ) ), Load( f32, inref ) ) ), outref ) *)
-let v = Vector(UInt(8), 16) 
+let v = Vector(UInt(8), vecwidth)
 let load = Load (v, {buf = inbuf; idx = x})
 let store vec = Store(vec, {buf = outbuf; idx = x})
-let prgm = Map(xdom, store (Add(v, (load, load))))
+let prgm len = Map((xdom len), store (Add(v, (load, load))))
+ 
 let () =
-
-  
-  Cg_llvm.codegen_to_file "cg_test.bc" prgm;
-  
   (* load test.png into input and output arrays *)
   let (w,h,inarr) = Imageio.load "test.png" in
   let outarr = Util.copy_bigarray inarr in
-    Runner.run prgm [| inarr; outarr |];
+
+  (* instantiate the program with x range = image size *)
+  let p = prgm (w*h*3) in
+
+    (* run the program *)
+    Runner.run p [| inarr; outarr |];
+
+    (* save outarr to test.out.png *)
     Imageio.save outarr w h "test.out.png";
 
+    (* save bitcode for debugging *)
+    Cg_llvm.codegen_to_file "cg_test.bc" p;
