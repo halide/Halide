7761592a5edb6a3c06596f9802cc0b54e542349e
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-07-09 00:48:07 -0400

    Factor JIT runner and bigarray utils out of cg_test
    Former-commit-id: 70c35916967a6a327f5f7c5bb7d86348bef5e3dc

--- src/cg_test.ml
+++ src/cg_test.ml
@@ -1,5 +1,4 @@
 open Ir
-open Llvm_executionengine
 open Ir_printer
 
 let dom nm rn = { name = nm; range = rn }
@@ -22,44 +21,14 @@ let v = Vector(UInt(8), 16)
 let load = Load (v, {buf = inbuf; idx = x})
 let store vec = Store(vec, {buf = outbuf; idx = x})
 let prgm = Map(xdom, store (Add(v, (load, load))))
-
-let mkarr sz =
-  let arr =
-    Bigarray.Array1.create Bigarray.int8_unsigned Bigarray.c_layout sz in
-    Bigarray.Array1.fill arr 0;
-    arr
-
 let () =
 
-  Printf.printf "%s\n" (string_of_stmt prgm);
   
   Cg_llvm.codegen_to_file "cg_test.bc" prgm;
   
-  (* JITing requires initializing targetdata to the native target, or else 
-   * subtle endianness and similar bugs will crop up in generated code *)
-  ignore (initialize_native_target ());
-  
-  let (m,f) = Cg_llvm.codegen_to_ocaml_callable prgm in
-  
-  let ee = ExecutionEngine.create m in
- 
+  (* load test.png into input and output arrays *)
   let (w,h,inarr) = Imageio.load "test.png" in
+  let outarr = Util.copy_bigarray inarr in
+    Runner.run prgm [| inarr; outarr |];
+    Imageio.save outarr w h "test.out.png";
 
-  let copy_bigarray arr =
-    let cp = Bigarray.Array1.create
-               (Bigarray.Array1.kind arr)
-               (Bigarray.Array1.layout arr)
-               (Bigarray.Array1.dim arr) in
-      Bigarray.Array1.blit arr cp;
-      cp
-  in
-  let outarr = copy_bigarray inarr in
-
-    ignore (
-      ExecutionEngine.run_function
-        f
-        [| GenericValue.of_pointer inarr; GenericValue.of_pointer outarr |]
-        ee
-    );
-  
-    Imageio.save outarr w h "test.out.png"
---
+++ src/runner.ml
@@ -0,0 +1,18 @@
+open Llvm_executionengine
+
+(* TODO: move into runtime module *)
+(* TODO: support typed args array by defining `argument` algebraic type and
+* defining generic_value_of_argument *)
+let run prgm args =
+  (* JITing requires initializing targetdata to the native target, or else 
+   * subtle endianness and similar bugs will crop up in generated code *)
+  ignore (initialize_native_target ());
+  
+  (* codegen the program, including an OCaml argument-passing wrapper *)
+  let (m,f) = Cg_llvm.codegen_to_ocaml_callable prgm in
+  
+  let ee = ExecutionEngine.create m in
+
+  ignore (
+    ExecutionEngine.run_function f (Array.map GenericValue.of_pointer args) ee
+  )
