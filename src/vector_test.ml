41fd12b459341f3bfd6db2b225ba92556af741c2
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-07-29 23:41:57 -0400

    Modified vector_test to stride by 16
    Former-commit-id: 4440ee8fc31bd55311807ade02cd9de77b98ba3c

--- src/vector_test.ml
+++ src/vector_test.ml
@@ -7,6 +7,8 @@ let c = Var("c")
 let outbuf = 2
 let inbuf = 1
 
+let vecWidth = 16
+
 let prgm w h ch =
   let imAddr x y c =
     Add(x,
@@ -17,14 +19,17 @@ let prgm w h ch =
 
   let imRef im x y c = { buf = im; idx = imAddr x y c } in
 
-  let outRef = imRef outbuf (Mul(x, IntImm(4))) y c in
+  let outRef = imRef outbuf (Mul(x, IntImm(vecWidth))) y c in
 
   (* Reverse every block of 4 pixels *)
-  let lx k = Load(UInt(8), imRef inbuf (Add(Mul(x, IntImm(4)), IntImm(k))) y c) in
+  let lx k = Load(UInt(8), imRef inbuf (Add(Mul(x, IntImm(vecWidth)), IntImm(k))) y c) in
   
   let stmt = Store(
     MakeVector([
-               lx 3; lx 2; lx 1; lx 0
+               lx 3; lx 2; lx 1; lx 0;
+               lx 7; lx 6; lx 5; lx 4;
+               lx 11; lx 10; lx 9; lx 8;
+               lx 15; lx 14; lx 13; lx 12
               ]),
     outRef) in
    
@@ -34,7 +39,7 @@ let prgm w h ch =
   Map(
   {name = "y"; range = (0, h)},
   Map(
-  {name = "x"; range = (0, w/4)},
+  {name = "x"; range = (0, w/vecWidth)},
   stmt
  )
  )
