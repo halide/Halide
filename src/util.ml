62a9365f5e2e10ead42ae3a9fc56b4de163160d8
Andrew Adams <abadams@csail.mit.edu>
2011-08-25 15:11:03 -0400

    Added until library
    Former-commit-id: 7a48f77652fe08c1077ab97c8ea9bb1060d4bc79

--- src/util.ml
+++ src/util.ml
@@ -14,3 +14,13 @@ let copy_bigarray arr =
   let cp = clone_bigarray arr in
     Array3.blit arr cp;
     cp
+
+
+(* A range operator *)
+let (--) i j = 
+  let rec aux n acc =
+    if n < i then acc else aux (n-1) (n :: acc)
+  in aux (j-1) []
+
+
+exception Wtf of string
