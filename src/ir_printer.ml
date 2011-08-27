c0e0d540538055d192264c8fafabbb05e44a03cb
Andrew Adams <abadams@csail.mit.edu>
2011-08-27 18:13:03 -0400

    Did a pass over statements, flattening map, and commenting out ones that we don't currently use. Wrote code to auto-vectorize statements. Added some tests for this to vectorize_test.

--- src/ir_printer.ml
+++ src/ir_printer.ml
@@ -31,32 +31,31 @@ and string_of_expr = function
   | Bop(op, l, r) -> "(" ^ string_of_expr l ^ string_of_op op ^ string_of_expr r ^ ")"
   | Cmp(op, l, r) -> "(" ^ string_of_expr l ^ string_of_cmp op ^ string_of_expr r ^ ")"
   | Select(c, t, f) ->
-      "(" ^ string_of_expr c ^ "?" ^ string_of_expr t ^ ":" ^ string_of_expr f ^ ")"
+    "(" ^ string_of_expr c ^ "?" ^ string_of_expr t ^ ":" ^ string_of_expr f ^ ")"
   | Var(s) -> s
   | Load(t, mr) -> "load" ^ "(" ^ string_of_val_type t ^ "," ^ string_of_memref mr ^ ")"
   | MakeVector [] -> "vec[]"
   | MakeVector l -> 
-      let els = List.map string_of_expr l in
-      "vec[" ^ (List.fold_left
-                  (fun x y -> x ^ ", " ^ y) 
-                  (List.hd els)
-                  (List.tl els)
-               ) ^ "]"
+    let els = List.map string_of_expr l in
+    "vec[" ^ (List.fold_left
+                (fun x y -> x ^ ", " ^ y) 
+                (List.hd els)
+                (List.tl els)
+    ) ^ "]"
   | Broadcast(e, n) -> "[" ^ string_of_expr e ^ "x" ^ string_of_int n ^ "]"
   | ExtractElement(a, b) -> "(" ^ string_of_expr a ^ "@" ^ string_of_expr b ^ ")"
   | _ -> "<<UNHANDLED>>"
 
 and string_of_stmt = function
-  | If(e, s) -> "if (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
-  | IfElse(e, ts, fs) -> "if (" ^ string_of_expr e ^ ") " ^ string_of_stmt ts ^
-                         " else " ^ string_of_stmt fs
-  | Map(d, s) -> "map (" ^ string_of_domain d ^ ") " ^ string_of_stmt s
-  | For(d, s) -> "for (" ^ string_of_domain d ^ ") " ^ string_of_stmt s
+  (*  | If(e, s) -> "if (" ^ string_of_expr e ^ ") " ^ string_of_stmt s *)
+  (* | IfElse(e, ts, fs) -> "if (" ^ string_of_expr e ^ ") " ^ string_of_stmt ts ^ 
+                         " else " ^ string_of_stmt fs *)
+  | Map(n, min, max, s) -> sprintf "map (%s from %d to %d) %s" n min max (string_of_stmt s)
   | Block(stmts) -> "{" ^ "\n" ^
                     String.concat ";\n" (List.map string_of_stmt stmts) ^
                     "}" ^ "\n"
-  | Reduce(op, e, mr) -> string_of_memref mr ^ string_of_reduce_op op ^ string_of_expr e
-  | Store(e, mr) -> string_of_memref mr ^ "=" ^ string_of_expr e
+  (* | Reduce(op, e, mr) -> string_of_memref mr ^ string_of_reduce_op op ^ string_of_expr e *)
+  | Store(e, mr) -> string_of_memref mr ^ " = " ^ string_of_expr e
 
 and string_of_reduce_op = function
   | AddEq -> "+="
--- src/vectorize_test.ml
+++ src/vectorize_test.ml
@@ -8,22 +8,32 @@ open Ir_printer
 let () =
   let x = Var "x" and y = Var "y" in
   let vec e = vectorize_expr e "x" 4 in
+  let vecs e = vectorize_stmt e "x" 4 in
   let test e = printf "%s \n-> %s\n\n" (string_of_expr e) (string_of_expr (vec e)) in
+  let tests e = printf "%s \n-> %s\n\n" (string_of_stmt e) (string_of_stmt (vecs e)) in
   let load e = Load(u8, {buf=0; idx=e}) in
+  let store a b = Store(a, {buf=1; idx=b}) in
 
-  (* basic vector arithmetic *)
+  printf "Basic vectorized arithmetic:\n";
   test ((IntImm 2) +~ x);
   test ((IntImm 2) -~ x);
   test (x *~ (IntImm 2));
 
-  (* vector load *)
+  printf "Vector load:\n";
   test (load x);
     
-  (* vector load plus scalar load *)
+  printf "Vector load plus scalar load:\n";
   test ((load x) +~ (load y));
 
-  (* Gather *)
+  printf "Gather:\n";
   test (load (load x));
 
-  (* Vector load with base address that depends on y *)
+  printf "Conditional load with matching strides:\n";
   test (load (Select(IntImm(3) =~ y, x +~ IntImm(1), x +~ IntImm(10))));
+
+  printf "Memcpy:\n";
+  tests (Map ("x", 0, 100, store (load x) x));
+
+  printf "Histogram:\n";
+  let v = load x in
+  tests (Map ("x", 0, 100, store ((Load(u8, {buf=1; idx = v})) +~ IntImm(1)) v));
