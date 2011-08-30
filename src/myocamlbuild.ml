b2635625f7b37b0ed656417bac462a4352919a5c
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-08-30 01:13:32 -0400

    Modified ocamlbuild to allow standalone build
    Doesn't yet build the actual cimg.o object from c,
    but no longer requires many path arguments when 
    running ocamlbuild. Just `ocamlbuild target.type` 
    should work.
    
    Former-commit-id: 28310399dfc661c8b62b9352ddcce046285c4149

--- src/myocamlbuild.ml
+++ src/myocamlbuild.ml
@@ -2,12 +2,12 @@ open Ocamlbuild_plugin;;
 
 (* require individual LLVM component libraries here *)
 ocaml_lib ~extern:true "unix";;
-ocaml_lib ~extern:true "llvm";;
-ocaml_lib ~extern:true "llvm_analysis";;
-ocaml_lib ~extern:true "llvm_bitwriter";;
-ocaml_lib ~extern:true "llvm_bitreader";;
-ocaml_lib ~extern:true "llvm_target";;
-ocaml_lib ~extern:true "llvm_executionengine";;
+ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm";;
+ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_analysis";;
+ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_bitwriter";;
+ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_bitreader";;
+ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_target";;
+ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_executionengine";;
 
 (* define ocamlc link flag: -cc g++ *)
 (* presumably necessary to ensure linkage of libstdc++ for LLVM *)
@@ -16,3 +16,4 @@ ocaml_lib ~extern:true "llvm_executionengine";;
 tons of deprecation warning noise with g++ 4.4, just linking stdc++ below works
 better *)
 flag ["link"; "ocaml"; "g++"] (S[A"-cclib"; A"-lstdc++"]);;
+flag ["link"; "ocaml"; "use_cimg"] (S[A"cimg.o"; A"-cclib"; A"-lpng"]);;
