53fafa9d51b6d5e2c4d2fa114b6c2e0553a6fd9c
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-08-30 01:42:23 -0400

    Moved entire build into ocamlbuild, including cimg
    Commented and cleaned up myocamlbuild.ml/_tags
    
    Former-commit-id: 131f3a9e62cdab2f34009c045f88d7ab73573ccc

--- src/myocamlbuild.ml
+++ src/myocamlbuild.ml
@@ -1,7 +1,16 @@
 open Ocamlbuild_plugin;;
 
+(*
+ * Essential ocamlbuild setup for full project, including LLVM libs and cimg.c.
+ * Cf. http://brion.inria.fr/gallium/index.php/Using_an_external_library
+ *     http://brion.inria.fr/gallium/index.php/Ocamlbuild_example_with_C_stubs
+ * for reference examples. 
+ *)
+
 (* require individual LLVM component libraries here *)
-ocaml_lib ~extern:true "unix";;
+(* ~dir:... specifies search path for each lib. Need on *.ml as well as *.byte
+ * to search for includes, not just libs to link. *)
+ocaml_lib ~extern:true "unix";; (* Unix is (oddly) needed by llvm when building a toplevel. Must be first. *)
 ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm";;
 ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_analysis";;
 ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_bitwriter";;
@@ -9,11 +18,21 @@ ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_bitreade
 ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_target";;
 ocaml_lib ~extern:true ~dir:"../../llvm/Debug+Asserts/lib/ocaml/" "llvm_executionengine";;
 
-(* define ocamlc link flag: -cc g++ *)
-(* presumably necessary to ensure linkage of libstdc++ for LLVM *)
-(* this actually gets set for a target with the `g++` tag in _tags *)
+(* Define ocamlc link flag: -cc g++ *)
+(* This is necessary to ensure linkage of libstdc++ for LLVM. *)
+(* This actually gets set for a target with the `g++` tag in `_tags`. *)
 (*flag ["link"; "ocaml"; "g++"] (S[A"-cc"; A"g++"]);;*) (* this version spews
 tons of deprecation warning noise with g++ 4.4, just linking stdc++ below works
 better *)
 flag ["link"; "ocaml"; "g++"] (S[A"-cclib"; A"-lstdc++"]);;
-flag ["link"; "ocaml"; "use_cimg"] (S[A"cimg.o"; A"-cclib"; A"-lpng"]);;
+
+(* Link the cimg module *)
+flag ["link"; "ocaml"; "use_cimg"] (S[A"-cclib"; A"-lpng"]);;
+(* When something links cimg, cimg.o needs to be up to date *)
+dep ["link"; "ocaml"; "use_cimg"] ["cimg.o"];;
+
+(* C compiler arguments for building cimg.c -> cimg.o *)
+let include_ocaml = "-I/usr/local/lib/ocaml" in
+let use_c99 = "-std=c99" in
+flag ["c"; "compile"; "cimg_cflags"]
+  (S[A"-ccopt"; A include_ocaml; A"-ccopt"; A use_c99]);;
