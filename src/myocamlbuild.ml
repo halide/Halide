228aa2de7bb944bb94b775e5be60e23a17e639ee
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-08-30 00:47:51 -0400

    Added ability to build basic toplevel REPL fimage.top

--- src/myocamlbuild.ml
+++ src/myocamlbuild.ml
@@ -1,6 +1,7 @@
 open Ocamlbuild_plugin;;
 
 (* require individual LLVM component libraries here *)
+ocaml_lib ~extern:true "unix";;
 ocaml_lib ~extern:true "llvm";;
 ocaml_lib ~extern:true "llvm_analysis";;
 ocaml_lib ~extern:true "llvm_bitwriter";;
