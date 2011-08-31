e5a8a4fc8899aab60dd0563951ea21b76ef45169
Andrew Adams <abadams@csail.mit.edu>
2011-08-31 19:04:34 -0400

    Very basic jitting from c++ working. Need to automatically number buffers.
    Former-commit-id: 97660fc1b35b353cc5adcc4d3f1abe5357d5f6cf

--- cpp_bindings/FImage.h
+++ cpp_bindings/FImage.h
@@ -4,11 +4,29 @@
 #include "MLVal.h"
 
 namespace FImage {
-    typedef MLVal Expr;
+
+    class Expr {
+      public:
+        Expr(int);
+        Expr(MLVal);
+        MLVal val;
+        Expr operator+(const Expr &b);
+    };
+
+    class Var : public Expr {
+      public:
+        Var();
+        Var(const char *str);
+    };
+
     Expr Add(const Expr &, const Expr &);
     Expr IntImm(int);
-    Expr Var(const char *c);
+    //Expr Var(const char *c);
     void print(const Expr &);
+
+    void run(const Expr &stmt, void *args);
+    Expr Load(int buf, const Expr &idx);
+    Expr Store(const Expr &val, int buf, const Expr &idx);
 }
 
 
