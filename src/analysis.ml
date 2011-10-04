360518936dc2c6891f1c82cf7e1224624ff9c2ca
Andrew Adams <abadams@csail.mit.edu>
2011-10-04 17:48:14 -0400

    Refactor analyses in terms of fold_expr/stmt
    Subs, name replacement.
    Add prefix name helpers, too.

--- src/analysis.ml
+++ src/analysis.ml
@@ -70,39 +70,84 @@ let expr_contains_expr query e =
     | x -> false
   in recurse e
 
+let fold_children_in_expr mutator combiner base_case = function
+  | Not e
+  | Load (_, _, e)
+  | Broadcast (e, _)
+  | Cast (_, e)           -> mutator e
     
+  | Ramp (a, b, _)
+  | ExtractElement (a, b)
+  | Bop (_, a, b)
+  | Cmp (_, a, b)
+  | Let (_, a, b)
+  | And (a, b)
+  | Or (a, b)             -> combiner (mutator a) (mutator b)
+    
+  | Select (c, a, b)      -> combiner (combiner (mutator c) (mutator a)) (mutator b)
+    
+  | MakeVector l
+  | Call (_, _, l)        -> List.fold_left combiner base_case (List.map mutator l)
+
+  | x                     -> base_case
+
+let fold_children_in_stmt expr_mutator stmt_mutator combiner = function
+  | Map (name, min, max, body) ->
+    combiner (combiner (expr_mutator min) (expr_mutator max)) (stmt_mutator body)
+  | Block l -> List.fold_left combiner (stmt_mutator (List.hd l)) (List.map stmt_mutator (List.tl l))
+  | Store (expr, buf, idx) -> combiner (expr_mutator expr) (expr_mutator idx)
+  | Pipeline (name, ty, size, produce, consume) -> 
+    combiner (combiner (expr_mutator size) (stmt_mutator produce)) (stmt_mutator consume)
+
+(* E.g:
+let rec stmt_contains_zero stmt =
+  let rec expr_mutator = function
+    | IntImm 0 -> true
+    | x -> fold_children_in_expr expr_mutator (or) false x
+  fold_children_in_stmt expr_mutator stmt_contains_zero (or)
+*)
+
+let expr_contains_expr query e =
+  if (query = e) then 
+    true 
+  else
+    fold_children_in_expr (expr_contains_expr query) (or) false e
+
+let mutate_children_in_expr mutator = function
+  | Cast (t, e)           -> Cast (t, mutator e)
+  | Bop (op, a, b)        -> Bop (op, mutator a, mutator b)
+  | Cmp (op, a, b)        -> Cmp (op, mutator a, mutator b)
+  | And (a, b)            -> And (mutator a, mutator b)
+  | Or (a, b)             -> Or (mutator a, mutator b)
+  | Not a                 -> Not (mutator a)
+  | Select (c, a, b)      -> Select (mutator c, mutator a, mutator b)
+  | Load (t, b, i)        -> Load (t, b, mutator i)
+  | MakeVector l          -> MakeVector (List.map mutator l)
+  | Broadcast (a, n)      -> Broadcast (mutator a, n)
+  | Ramp (b, s, n)        -> Ramp (mutator b, mutator s, n)
+  | ExtractElement (a, b) -> ExtractElement (mutator a, mutator b)
+  | Call (f, t, args)     -> Call (f, t, List.map mutator args)
+  | Let (n, a, b)         -> Let (n, mutator a, mutator b)
+  | x -> x
+    
+let mutate_children_in_stmt expr_mutator stmt_mutator = function
+  | Map (name, min, max, body) ->
+    Map (name, expr_mutator min, expr_mutator max, stmt_mutator body)
+  | Block l -> Block (List.map stmt_mutator l)
+  | Store (expr, buf, idx) -> 
+    Store (expr_mutator expr, buf, expr_mutator idx)
+  | Pipeline (name, ty, size, produce, consume) -> 
+    Pipeline (name, ty, expr_mutator size, stmt_mutator produce, stmt_mutator consume)
+
 (* Expression subsitution *)
-let rec subs_stmt oldexpr newexpr = function
-  | Map (name, min, max, stmt) -> Map (name, 
-                                       subs_expr oldexpr newexpr min,
-                                       subs_expr oldexpr newexpr max,
-                                       subs_stmt oldexpr newexpr stmt)
-  | Block l -> Block (List.map (subs_stmt oldexpr newexpr) l)
-  | Store (expr, buf, idx) -> Store (subs_expr oldexpr newexpr expr, buf, subs_expr oldexpr newexpr idx)
-  | Pipeline (name, ty, size, produce, consume) -> Pipeline (name, ty,
-                                                             subs_expr oldexpr newexpr size,
-                                                             subs_stmt oldexpr newexpr produce,
-                                                             subs_stmt oldexpr newexpr consume)
+let rec subs_stmt oldexpr newexpr stmt =
+  mutate_children_in_stmt (subs_expr oldexpr newexpr) (subs_stmt oldexpr newexpr) stmt
 
 and subs_expr oldexpr newexpr expr = 
-    let subs = subs_expr oldexpr newexpr in
-    if expr = oldexpr then newexpr else
-      match expr with
-        | Cast (t, e)           -> Cast (t, subs e)
-        | Bop (op, a, b)        -> Bop (op, subs a, subs b)
-        | Cmp (op, a, b)        -> Cmp (op, subs a, subs b)
-        | And (a, b)            -> And (subs a, subs b)
-        | Or (a, b)             -> Or (subs a, subs b)
-        | Not a                 -> Not (subs a)
-        | Select (c, a, b)      -> Select (subs c, subs a, subs b)
-        | Load (t, b, i)        -> Load (t, b, subs i)
-        | MakeVector l          -> MakeVector (List.map subs l)
-        | Broadcast (a, n)      -> Broadcast (subs a, n)
-        | Ramp (b, s, n)        -> Ramp (subs b, subs s, n)
-        | ExtractElement (a, b) -> ExtractElement (subs a, subs b)
-        | Call (f, t, args)     -> Call (f, t, List.map subs args)
-        | Let (n, a, b)         -> Let (n, subs a, subs b)
-        | x -> x
+  if expr = oldexpr then 
+    newexpr 
+  else 
+    mutate_children_in_expr (subs_expr oldexpr newexpr) expr
     
 and subs_name_stmt oldname newname stmt =
   let subs = subs_name_stmt oldname newname in
@@ -123,22 +168,33 @@ and subs_name_stmt oldname newname stmt =
 and subs_name_expr oldname newname expr =
   let subs = subs_name_expr oldname newname in
   match expr with 
-    | Cast (t, e)           -> Cast (t, subs e)
-    | Bop (op, a, b)        -> Bop (op, subs a, subs b)
-    | Cmp (op, a, b)        -> Cmp (op, subs a, subs b)
-    | And (a, b)            -> And (subs a, subs b)
-    | Or (a, b)             -> Or (subs a, subs b)
-    | Not a                 -> Not (subs a)
-    | Select (c, a, b)      -> Select (subs c, subs a, subs b)
-    | Load (t, b, i)        -> Load (t, (if b = oldname then newname else b), subs i)
-    | MakeVector l          -> MakeVector (List.map subs l)
-    | Broadcast (a, n)      -> Broadcast (subs a, n)
-    | Ramp (b, s, n)        -> Ramp (subs b, subs s, n)
-    | ExtractElement (a, b) -> ExtractElement (subs a, subs b)
-    | Var (t, n)            -> Var (t, if n = oldname then newname else n)
-    | Let (n, a, b)         -> Let ((if n = oldname then newname else n), subs a, subs b)
-    | Call (f, t, args)     -> Call (f, t, List.map subs args)
-    | x -> x
+    | Load (t, b, i)    -> Load (t, (if b = oldname then newname else b), subs i)
+    | Var (t, n)        -> Var (t, if n = oldname then newname else n)
+    | Let (n, a, b)     -> Let ((if n = oldname then newname else n), subs a, subs b)
+    | Call (f, t, args) -> Call ((if f = oldname then newname else f), t, List.map subs args)
+    | x                 -> mutate_children_in_expr subs x
+
+and prefix_name_expr prefix expr =
+  let recurse = prefix_name_expr prefix in
+  match expr with 
+    | Load (t, b, i)    -> Load (t, prefix ^ b, recurse i)
+    | Var (t, n)        -> Var (t, prefix ^ n)
+    | Let (n, a, b)     -> Let (prefix ^ n, recurse a, recurse b)
+    | Call (f, t, args) -> Call (prefix ^ f, t, List.map recurse args)
+    | x                 -> mutate_children_in_expr recurse x  
+
+and prefix_name_stmt prefix stmt =
+  let recurse_stmt = prefix_name_stmt prefix in
+  let recurse_expr = prefix_name_expr prefix in
+  match stmt with
+    | Map (name, min, max, body) ->
+      Map (prefix ^ name, recurse_expr min, recurse_expr max, recurse_stmt body)
+    | Block l -> Block (List.map recurse_stmt l)
+    | Store (expr, buf, idx) -> 
+      Store (recurse_expr expr, prefix ^ buf, recurse_expr idx)
+    | Pipeline (name, ty, size, produce, consume) -> 
+      Pipeline (prefix ^ name, ty, recurse_expr size, recurse_stmt produce, recurse_stmt consume)      
+
 
 (* Assuming a program has 1024 expressions, the probability of a collision using a k-bit hash is roughly:
 
