074a5de5e144eaa2ee707d4de284b08947419cf6
Andrew Adams <abadams@csail.mit.edu>
2011-10-04 17:49:32 -0400

    Progress on schedule and lowering

--- src/test_schedule_tree.ml
+++ src/test_schedule_tree.ml
@@ -6,6 +6,9 @@ let _ =
   let s = empty_schedule in
   let s = set_schedule s ["f"] Root [Serial ("fx", IntImm 0, IntImm 16); Serial ("fy", IntImm 0, IntImm 10)] in
   let s = set_schedule s ["f"; "g"] (Chunk ("fx", true)) [Serial ("gx", IntImm 0, IntImm 16)] in 
+  let s = set_schedule s ["a"; "b"; "c"] (Inline) [Serial ("cx", IntImm 0, IntImm 1)] in
+  let s = set_schedule s ["a"] (Inline) [Serial ("ax", IntImm 0, IntImm 1)] in
+  let s = set_schedule s ["a"; "b"] (Inline) [Serial ("bx", IntImm 0, IntImm 1)] in
   
   print_schedule s
     
