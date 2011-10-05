6a3a7cc2c71098b7d4019c6a227188c49540d19a
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-10-05 00:28:14 -0400

    Add initial stride_list helper for schedule
    `stride_list schedule_list argname_list` returns a list of exprs in the
    order of the argname list for the individual stride components of each
    arg. (These values should be successively multiplied together to compute
    the stride up to that point.)
    
    (Only tested so far for simple case in test_schedule_tree.)

--- src/test_schedule_tree.ml
+++ src/test_schedule_tree.ml
@@ -4,11 +4,15 @@ open Ir
 
 let _ = 
   let s = empty_schedule in
-  let s = set_schedule s ["f"] Root [Serial ("fx", IntImm 0, IntImm 16); Serial ("fy", IntImm 0, IntImm 10)] in
+  let s = set_schedule s ["f"] Root [Serial ("fx", IntImm 0, IntImm 16); Split ("fy", 2, "fyo", "fyi"); Serial ("fyo", IntImm 0, IntImm 5); Serial ("fyi", IntImm 0, IntImm 2)] in
   let s = set_schedule s ["f"; "g"] (Chunk ("fx", true)) [Serial ("gx", IntImm 0, IntImm 16)] in 
   let s = set_schedule s ["a"; "b"; "c"] (Inline) [Serial ("cx", IntImm 0, IntImm 1)] in
   let s = set_schedule s ["a"] (Inline) [Serial ("ax", IntImm 0, IntImm 1)] in
   let s = set_schedule s ["a"; "b"] (Inline) [Serial ("bx", IntImm 0, IntImm 1)] in
   
-  print_schedule s
-    
+  print_schedule s;
+
+  let (_, f_sched) = find_schedule s "f" in
+  let f_args = ["fx"; "fy"] in
+    printf "\n%s\n" (String.concat "\n"
+      (List.map Ir_printer.string_of_expr (stride_list f_sched f_args)))
