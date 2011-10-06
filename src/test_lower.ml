3a15d4c5f543f673bd06f58f6cca65e7870b455e
Andrew Adams <abadams@csail.mit.edu>
2011-10-06 19:14:33 -0400

    Introduced false-dependence-breaking pass.

--- src/test_lower.ml
+++ src/test_lower.ml
@@ -12,13 +12,18 @@ let _ =
 
   let f_call_sched = (Chunk "g.xi") in
   (*let f_sched = [Serial ("x", ((Var (i32, "g.xo")) *~ (IntImm 4)) -~ one, IntImm 6)] in *)
-  let f_sched = [Split ("x", "xo", "xi", (Var (i32, "g.xo") *~ (IntImm 4)) -~ (IntImm 1));
-                 Vectorized ("xi", IntImm 0, 4); Unrolled ("xo", IntImm 0, 2)] in 
+  (* let f_sched = [Split ("x", "xo", "xi", (Var (i32, "g.xo") *~ (IntImm 4)) -~ (IntImm 1));
+                 Vectorized ("xi", IntImm 0, 4); Unrolled ("xo", IntImm 0, 2)] in  *)
+
+  let f_sched = [Split ("x", "xo", "xi", ((Var (i32, "g.xo")) *~ (IntImm 4)) -~ one);
+                 Vectorized ("xi", IntImm 0, 4);
+                 Unrolled ("xo", IntImm 0, 2)
+                ] in 
 
   let g = ("g", [(i32, "x")], f32, Pure ((Call (f32, "f", [x +~ one])) +~ (Call (f32, "f", [x -~ one])))) in
   
   let g_call_sched = Root in
-  let g_sched = [Split ("x", "xo", "xi", IntImm 0); Unrolled ("xi", IntImm 0, 4); Parallel ("xo", IntImm 0, IntImm 25)] in
+  let g_sched = [Split ("x", "xo", "xi", IntImm 0); Vectorized ("xi", IntImm 0, 4); Parallel ("xo", IntImm 0, IntImm 25)] in
 
   let env = Environment.empty in
   let env = Environment.add "f" f env in
@@ -34,8 +39,11 @@ let _ =
   let callf = Call(f32, "g", [x]) in
   let stmt = For ("x", IntImm 0, IntImm 100, false, Store (callf, ".result", x)) in
 
-  let lowered = Constant_fold.constant_fold_stmt (lower stmt env sched) in
-  
+  let lowered = lower stmt env sched in
+  let lowered = Break_false_dependence.break_false_dependence_stmt lowered in
+  let lowered = Constant_fold.constant_fold_stmt lowered in
+
+
   Printf.printf "\n\nLowered to:\n%s\n" (Ir_printer.string_of_stmt lowered);
   
   Cg_llvm.codegen_to_file "test_lower.bc" ([Buffer ".input"; Buffer "result"], lowered)
