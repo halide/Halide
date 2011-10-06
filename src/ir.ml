0eca6337a316b537900e928eb3ca960b91704241
Andrew Adams <abadams@csail.mit.edu>
2011-10-06 16:13:25 -0400

    Style pass over ir.
    Former-commit-id: 834556256105a264e7e0eaad564f7477a07a1e17

--- src/ir.ml
+++ src/ir.ml
@@ -1,57 +1,57 @@
 open Util
 
-(* casting or separate op types for float/(u)int? *)
-
-(* make different levels of IR subtype off increasingly restrictive, partially
- * compatible interface types? *)
-
-(* bits per element *)
 type val_type = 
-    | Int of int 
-    | UInt of int 
-    | Float of int 
-    (* TODO: this technically allows Vector(Vector(...)) which the pattern
-     * matching checks don't like, among other things. *)
-    (* TODO: switch to explicit IntVector, UIntVector, FloatVector? *)
-    (* TODO: separate BoolVector or mask type? *)
-    | IntVector of int * int
-    | UIntVector of int * int
-    | FloatVector of int * int
+  (* bits per element *)    
+  | Int of int 
+  | UInt of int 
+  | Float of int 
+  (* bits per element, vector width *)
+  | IntVector of int * int
+  | UIntVector of int * int
+  | FloatVector of int * int
 
-let bool1 = UInt(1)
-let u8 = UInt(8)
-let u16 = UInt(16)
-let u32 = UInt(32)
-let u64 = UInt(64)
-let i8 = Int(8)
-let i16 = Int(16)
-let i32 = Int(32)
-let i64 = Int(64)
-let f16 = Float(16)
-let f32 = Float(32)
-let f64 = Float(64)
+let bool1 = UInt 1
+let u8    = UInt 8
+let u16   = UInt 16
+let u32   = UInt 32
+let u64   = UInt 64
+let i8    = Int 8
+let i16   = Int 16
+let i32   = Int 32
+let i64   = Int 64
+let f16   = Float 16
+let f32   = Float 32
+let f64   = Float 64
 
 let element_val_type = function
-  | IntVector(x, _) -> Int(x)
-  | UIntVector(x, _) -> UInt(x)
-  | FloatVector(x, _) -> Float(x)
+  | IntVector   (x, _) -> Int x
+  | UIntVector  (x, _) -> UInt x
+  | FloatVector (x, _) -> Float x
   | x -> x
 
 let vector_of_val_type t n = match t with
-  | Int(x) -> IntVector(x, n)
-  | UInt(x) -> UIntVector(x, n)
-  | Float(x) -> FloatVector(x, n)
+  | Int x   -> IntVector (x, n)
+  | UInt x  -> UIntVector (x, n)
+  | Float x -> FloatVector (x, n)
   | _ -> raise (Wtf("vector_of_val_type called on vector type"))
 
 let bit_width = function
-  | Int(x) | UInt(x) | Float(x) -> x
-  | IntVector(x, n) | UIntVector(x, n) | FloatVector(x, n) -> x*n
+  | Int x 
+  | UInt x 
+  | Float x -> x
+  | IntVector (x, n) 
+  | UIntVector (x, n) 
+  | FloatVector (x, n) -> x*n
 
 let element_width t = bit_width (element_val_type t)
 
 let vector_elements = function
-  | Int(x) | UInt(x) | Float(x) -> 1
-  | IntVector(x, n) | UIntVector(x, n) | FloatVector(x, n) -> n
+  | Int x 
+  | UInt x 
+  | Float x -> 1
+  | IntVector (x, n) 
+  | UIntVector (x, n)
+  | FloatVector (x, n) -> n
 
 type binop = Add | Sub | Mul | Div
 type cmpop = EQ | NE | LT | LE | GT | GE
@@ -76,106 +76,117 @@ let caml_op_of_cmp = function
   | GT -> ( >  )
   | GE -> ( >= )
 
-(* how to enforce valid arithmetic subtypes for different subexpressions? 
- * e.g. only logical values expressions for Logical? Declare interfaces for 
- * each subtype? *)
 type expr =
-    (* constants *)
-    (* TODO: add val_type to immediates? *)
-    | IntImm of int
-    | UIntImm of int
-    | FloatImm of float
+  (* constants *)
+  (* TODO: add val_type to immediates? *)
+  | IntImm of int
+  | UIntImm of int
+  | FloatImm of float
+      
+  (* TODO: separate "cast" from "convert"?  Cast changes operational
+   * interpretation of variables, but doesn't touch stored bits.
+   * Convert changes data to bits with equivalent interpretation in
+   * new type. *)
 
-    (* TODO: separate "cast" from "convert"?
-     * Cast changes operational interpretation of variables, but doesn't touch
-     * stored bits.
-     * Convert changes data to bits with equivalent interpretation in new type. *)
-    (* TODO: validate operand type matches with Caml type parameters *)
-    | Cast of val_type * expr
+  | Cast of val_type * expr
 
-    | Var of val_type * string
+  (* Variables. May be loop variables, function argument, or a
+     variable introduced with Let *)
+  | Var of val_type * string
 
-    (* basic binary ops *)
-    | Bop of binop * expr * expr
+  (* basic binary ops *)
+  | Bop of binop * expr * expr
 
-    (* comparison *)
-    | Cmp of cmpop * expr * expr
+  (* comparison *)
+  | Cmp of cmpop * expr * expr
 
-    (* logical *)
-    | And of expr * expr
-    | Or  of expr * expr
-    | Not of expr
+  (* logical *)
+  | And of expr * expr
+  | Or  of expr * expr
+  | Not of expr
 
-    (* Select of [condition], [true val], [false val] *)
-    (* Type is type of [true val] & [false val], which must match *)
-    | Select of expr * expr * expr
+  (* Select: condition, then case, else case *)
+  | Select of expr * expr * expr
 
-    (* memory *)
-    | Load of val_type * buffer * expr
-          
-    (* Three different ways to make vectors *)
-    | MakeVector of (expr list)
-    | Broadcast of expr * int
-    | Ramp of expr * expr * int (* Base, stride, length *)
+  (* Load: type, buffer name, index. If type is a vector, index refers to the element type. *)
+  | Load of val_type * buffer * expr
+      
+  (* Three different ways to make vectors *)
+  | MakeVector of (expr list)
+  | Broadcast of expr * int
+  | Ramp of expr * expr * int (* Base, stride, length *)
 
-    (* TODO: Unpack/swizzle vectors *)
-    | ExtractElement of expr * expr
+  (* Extract an element: vector, index *)
+  | ExtractElement of expr * expr
 
-    (* Function calls *) 
-    | Call of val_type * string * (expr list)
+  (* Function call: return type, function name, args *) 
+  | Call of val_type * string * (expr list)
 
-    (* Let expressions *)
-    | Let of string * expr * expr
+  (* Let expressions *)
+  | Let of string * expr * expr
 
-and buffer = string (* TODO: just an ID for now *)
+and buffer = string (* just a name for now *)
 
 exception ArithmeticTypeMismatch of val_type * val_type
 
-(* TODO: assert that l,r subexpressions match. Do as separate checking pass? *)
 let rec val_type_of_expr = function
-  | IntImm _ -> i32
-  | UIntImm _ -> u32
+  (* Immediates are currently all 32-bit *)
+  | IntImm _   -> i32
+  | UIntImm _  -> u32
   | FloatImm _ -> f32
-  | Cast(t,_) -> t
-  | Bop(_, l, r) | Select(_,l,r) ->
+      
+  (* Binary operators and selects expect matching types on their
+     sub-expressions *)
+  | Bop (_, l, r) 
+  | Select (_, l, r) ->
       let lt = val_type_of_expr l and rt = val_type_of_expr r in
-      if (lt <> rt) then raise (ArithmeticTypeMismatch(lt,rt));
+      if (lt <> rt) then raise (ArithmeticTypeMismatch (lt, rt));
       lt
-  | Var (vt,_) -> vt
-  (* boolean expressions on vector types return bool vectors of equal length*)
-  (* boolean expressions on scalars return scalar bools *)
-  | Cmp(_, l, r) -> 
+
+  (* And, Or, and Not all have the same type as their first arg (which
+     should be a bool or bool vector) *)
+  | And (e, _) 
+  | Or (e, _) 
+  | Not e -> val_type_of_expr e
+
+  (* Comparisons on vector types return bool vectors of equal length*)
+  (* Comparisons on scalars return scalar bools *)
+  | Cmp (_, l, r) -> 
       let lt = val_type_of_expr l and rt = val_type_of_expr r in
-      if (lt <> rt) then raise (ArithmeticTypeMismatch(lt,rt));
-      begin
-        match lt with
-          | IntVector(_, len) | UIntVector(_, len) | FloatVector(_, len) ->
-              IntVector(1, len)
-          | _ -> bool1
+      if (lt <> rt) then raise (ArithmeticTypeMismatch (lt, rt));
+      begin match lt with
+        | IntVector (_, n) 
+        | UIntVector (_, n) 
+        | FloatVector (_, n) -> IntVector (1, n)
+        | _ -> bool1
       end
-  (* And/Or/Not only allow boolean operands, and return the same type *)
-  | And(e,_) | Or(e,_) | Not e ->
-      (* TODO: add checks *)
-      val_type_of_expr e
-  | Load(t,_,_) -> t
-  | MakeVector(l) -> vector_of_val_type (val_type_of_expr (List.hd l)) (List.length l)
-  | Broadcast(e,len) -> vector_of_val_type (val_type_of_expr e) len
-  | Ramp(b, s, len) -> vector_of_val_type (val_type_of_expr b) len
-  | ExtractElement(e, idx) -> element_val_type (val_type_of_expr e)
-  | Call(ty, _, _) -> ty
-  | Let(_, _, b) -> val_type_of_expr b
+
+  (* Ops that construct vectors have vector types *)
+  | MakeVector l          -> vector_of_val_type (val_type_of_expr (List.hd l)) (List.length l)
+  | Broadcast (e, n)      -> vector_of_val_type (val_type_of_expr e) n
+  | Ramp (b, s, n)        -> vector_of_val_type (val_type_of_expr b) n
+
+  (* Extracting an element from a vector gives the element type *)
+  | ExtractElement (e, _) -> element_val_type (val_type_of_expr e)
+
+  (* A Let expression has the type of its body (rather than the type
+     of the thing its defining) *)
+  | Let (_, _, b)         -> val_type_of_expr b
+
+  (* Some ops explicitly declare their result type *)
+  | Cast (t, _)
+  | Var  (t, _) 
+  | Load (t, _, _)
+  | Call (t, _, _) -> t
+
 
 type stmt =
-  (* TODO: | Blit of -- how to express sub-ranges? -- split and merge
-   * sub-ranges *)
-  (* | If of expr * stmt  *)
-  (* | IfElse of expr * stmt * stmt  *)
   | For of string * expr * expr * bool * stmt
-    (* TODO: For might need landing pad: always executes before, only if *any* iteration
-     * fired. Same for Map - useful for loop invariant code motion. Easier for
-     * Map if multiple dimensions are fused into 1. *)
+
+  (* An in-order sequence of statements *)
   | Block of stmt list
-  (* | Reduce of reduce_op * expr * memref *) (* TODO: initializer expression? *)
+
+  (* Store an expression to a buffer at a given index *)
   | Store of expr * buffer * expr
 
   (* Allocate temporary storage of a given size, produce into it with
@@ -185,16 +196,7 @@ type stmt =
   *)
   | Pipeline of buffer * val_type * expr * stmt * stmt
 
-(*
-(* TODO:  *)
-and reduce_op =
-    | AddEq
-    | SubEq\
-    | MulEq
-    | DivEq
- *)
-
-(* (name, arg names, return type, body). The body is a stmt that fills a buffer called "result" *)
+(* A function definition: (name, args, return type, body) *)
 and definition = (string * ((val_type * string) list) * val_type * function_body)
 
 and function_body = 
@@ -225,7 +227,6 @@ let ( ||~ ) a b = Or (a, b)
 let ( &&~ ) a b = And (a, b)
 let ( !~ ) a    = Not a
 
-(* more helpers *)
+(* More helpers for examining ir nodes *)
 let is_scalar x = (vector_elements (val_type_of_expr x)) = 1
-
 let is_vector x = not (is_scalar x)
