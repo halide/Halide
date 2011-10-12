402ef4a4de6033fd0faca4ec4f914d3e5d4790ac
Andrew Adams <abadams@csail.mit.edu>
2011-10-12 17:35:02 -0400

    Refactoring and some cleanup of lower.
    Now you lower statements or functions. Removes redundant description of the schedule of the root function that previously existed in test_lower.
    
    Former-commit-id: 2de3e6f3b8f8cce2b4fcccce466e25bf6c35eb42

--- src/test_lower.ml
+++ src/test_lower.ml
@@ -11,7 +11,7 @@ let _ =
   let f = ("f", [(i32, "x")], f32, Pure ((Load (f32, ".input", x)) *~ (FloatImm 2.7))) in
 
   let f_call_sched = (Chunk "g.xi") in
-  (*let f_sched = [Serial ("x", ((Var (i32, "g.xo")) *~ (IntImm 4)) -~ one, IntImm 6)] in *)
+  (* let f_sched = [Vectorized ("x", ((Var (i32, "g.xo")) *~ (IntImm 4)) -~ two, 8)] in *)
   (* let f_sched = [Split ("x", "xo", "xi", (Var (i32, "g.xo") *~ (IntImm 4)) -~ (IntImm 1));
                  Vectorized ("xi", IntImm 0, 4); Unrolled ("xo", IntImm 0, 2)] in  *)
 
@@ -24,28 +24,23 @@ let _ =
   
   let g_call_sched = Root in
   let g_sched = [Split ("x", "xo", "xi", IntImm 0); Vectorized ("xi", IntImm 0, 4); Parallel ("xo", IntImm 0, IntImm 25)] in
-
+    
   let env = Environment.empty in
   let env = Environment.add "f" f env in
   let env = Environment.add "g" g env in
   
   let sched = empty_schedule in
   let sched = set_schedule sched "g.f" f_call_sched f_sched in
-  let sched = set_schedule sched "g" g_call_sched g_sched in
+  let sched = set_schedule sched "g" g_call_sched g_sched in  
 
   print_schedule sched;
-  ignore (find_schedule sched "g.f");
-  
-  let callf = Call(f32, "g", [x]) in
-  let stmt = For ("x", IntImm 0, IntImm 100, false, Store (callf, ".result", x)) in
 
-  let lowered = lower stmt env sched in
+  let lowered = lower_function "g" env sched in
   let lowered = Break_false_dependence.break_false_dependence_stmt lowered in
   let lowered = Constant_fold.constant_fold_stmt lowered in
 
-
   Printf.printf "\n\nLowered to:\n%s\n" (Ir_printer.string_of_stmt lowered);
   
-  Cg_llvm.codegen_to_file "test_lower.bc" ([Buffer ".input"; Buffer "result"], lowered)
+  Cg_llvm.codegen_to_file "test_lower.bc" ([Buffer ".input"; Buffer ".result"], lowered)
     
     
