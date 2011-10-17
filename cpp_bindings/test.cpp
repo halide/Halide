5650cc65b82d1b561de4be6913cd4b030c4c11b3
Andrew Adams <abadams@csail.mit.edu>
2011-10-17 14:56:34 -0400

    Updated test.cpp to a currently broken test case.
    Former-commit-id: c953bdfe4f1341da9912ae6b4d3c6d4bf8ffdd1e

--- cpp_bindings/test.cpp
+++ cpp_bindings/test.cpp
@@ -3,8 +3,8 @@
 
 using namespace FImage;
 
-#define W 3008
-#define H 3008
+#define W 3072
+#define H 3072
 
 template<typename T, int N>
 struct _sum {
@@ -41,7 +41,7 @@ float operator-(const timeval &after, const timeval &before) {
 int main(int argc, char **argv) {
     Var x, y;
     Func f, g;
-    Image im(W, H);       
+    Image im(W+16, H+16);       
 
     for (int y = 0; y < H; y++) {
         for (int x = 0; x < W; x++) {
@@ -50,20 +50,26 @@ int main(int argc, char **argv) {
     }
 
     printf("Defining function...\n");
-
     
-    f(x, y) = im(x, y) * im(x+1, y);
-    g(x, y) = f(x, y) * f(x, y+1);
+
+    f(x, y) = im(x, y) * im(x+1, y) * im(x+2, y) * im(x+3, y) * im(x+4, y);
+    g(x, y) = f(x, y) * f(x, y+1) * f(x, y+2) * f(x, y+3) * f(x, y+4);
 
     Var xo, xi, yo, yi;
 
-    g.split(y, yo, yi, 4);
-    g.split(x, xo, xi, 4);
-    g.transpose(xo, yi);
-    //f.chunk(xi);
-    f.chunk(yi, Range(xo*4, 4) * Range(yo*4, 5));
-    //f.vectorize(x);
-    //g.vectorize(xi);
+    if (argc > 1) {
+        int chunk = atoi(argv[1]);
+        g.split(y, yo, yi, chunk);
+        f.chunk(yi, Range(0, W) * Range(yo*chunk, chunk+4));
+
+        g.split(x, xo, xi, 4);
+        g.vectorize(xi);
+
+        f.split(y, yo, yi, 4);
+        f.transpose(x, yi);
+        f.vectorize(yi);
+    }
+
 
     printf("Realizing function...\n");
 
@@ -82,25 +88,25 @@ int main(int argc, char **argv) {
 
     // Clear it before the next run
     for (int y = 0; y < H; y++) {
-        for (int x = 0; x < W-1; x++) {
+        for (int x = 0; x < W; x++) {
             im2(x, y) = -1; 
         }
     }    
     
-    Image tmp(W-1, H);
+    Image tmp(W+16, H+16);
     
 
     gettimeofday(&before, NULL);
 
     for (int y = 0; y < H; y++) {
-        for (int x = 0; x < W-1; x++) {
-            tmp(x, y) = im(x, y) * im(x+1, y);
+        for (int x = 0; x < W; x++) {
+            tmp(x, y) = im(x, y) * im(x+1, y) * im(x+2, y) * im(x+3, y) * im(x+4, y);
         }
     }
 
-    for (int y = 0; y < H-1; y++) {
-        for (int x = 0; x < W-1; x++) {
-            im2(x, y) = tmp(x, y) * tmp(x, y+1);
+    for (int y = 0; y < H; y++) {
+        for (int x = 0; x < W; x++) {
+            im2(x, y) = tmp(x, y) * tmp(x, y+1) * tmp(x, y+2) * tmp(x, y+3) * tmp(x, y+4);
         }
     }
 
