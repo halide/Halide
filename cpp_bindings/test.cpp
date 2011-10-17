cda887b505b7e21e10523a7cbd8e851206828f48
Andrew Adams <abadams@csail.mit.edu>
2011-10-17 15:42:04 -0400

    Fixed a bug in lower.ml
    We were forgetting to prefix min exprs in Splits using the calling context.
    
    Former-commit-id: af40433cfae61e64bec8203b29096e3ffbb00f99

--- cpp_bindings/test.cpp
+++ cpp_bindings/test.cpp
@@ -52,8 +52,11 @@ int main(int argc, char **argv) {
     printf("Defining function...\n");
     
 
-    f(x, y) = im(x, y) * im(x+1, y) * im(x+2, y) * im(x+3, y) * im(x+4, y);
-    g(x, y) = f(x, y) * f(x, y+1) * f(x, y+2) * f(x, y+3) * f(x, y+4);
+    f(x, y) = im(x, y) + im(x+1, y) + im(x+2, y) + im(x+3, y) + im(x+4, y);
+    g(x, y) = f(x, y) + f(x, y+1) + f(x, y+2) + f(x, y+3) + f(x, y+4);
+
+    //f(x, y) = im(x, y) + im(x+1, y);
+    //g(x, y) = f(x, y) + f(x, y+1);
 
     Var xo, xi, yo, yi;
 
@@ -65,9 +68,8 @@ int main(int argc, char **argv) {
         g.split(x, xo, xi, 4);
         g.vectorize(xi);
 
-        f.split(y, yo, yi, 4);
-        f.transpose(x, yi);
-        f.vectorize(yi);
+        f.split(x, xo, xi, 4);
+        f.vectorize(xi);
     }
 
 
@@ -100,13 +102,13 @@ int main(int argc, char **argv) {
 
     for (int y = 0; y < H; y++) {
         for (int x = 0; x < W; x++) {
-            tmp(x, y) = im(x, y) * im(x+1, y) * im(x+2, y) * im(x+3, y) * im(x+4, y);
+            tmp(x, y) = im(x, y) + im(x+1, y) + im(x+2, y) + im(x+3, y) + im(x+4, y);
         }
     }
 
     for (int y = 0; y < H; y++) {
         for (int x = 0; x < W; x++) {
-            im2(x, y) = tmp(x, y) * tmp(x, y+1) * tmp(x, y+2) * tmp(x, y+3) * tmp(x, y+4);
+            im2(x, y) = tmp(x, y) + tmp(x, y+1) + tmp(x, y+2) + tmp(x, y+3) + tmp(x, y+4);
         }
     }
 
