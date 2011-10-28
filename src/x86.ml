2618d4f37b9d1d258f7708b5ce443b76abe836c1
Andrew Adams <abadams@csail.mit.edu>
2011-10-28 18:22:13 -0400

    Started adding stuff to x86 codegen. Currently unused.
    Former-commit-id: 4b0d5fd0e4e5e0f0b1a2f58a2f7c7717a85626b1

--- src/x86.ml
+++ src/x86.ml
@@ -2,9 +2,18 @@ open Ir
 open Llvm
 
 let cg_expr_x86 (c:llcontext) (m:llmodule) (b:llbuilder) (cg_expr : expr -> llvalue) (expr : expr) =
-  cg_expr expr
+
+  let i8x16_t = vector_type (i8_type c) 16 in
+
+  let paddb_16 = declare_function "paddb_16"
+    (function_type (i8x16_t) [|i8x16_t; i8x16_t|]) m in
+
+  match expr with 
+    (* llvm sometimes codegens vector adds to silly things (e.g. vector shifts) *)
+    | _ -> cg_expr expr 
 
 let cg_stmt_x86 (c:llcontext) (m:llmodule) (b:llbuilder) (cg_stmt : stmt -> llvalue) (stmt : stmt) =
   cg_stmt stmt
 
 
+    
