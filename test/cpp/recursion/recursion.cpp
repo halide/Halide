6b3774fa68e8b159339ed0aa4d68bc722da05f41
Andrew Adams <abadams@csail.mit.edu>
2011-11-02 17:02:43 -0400

    Added recursive test case

---
+++ test/cpp/recursion/recursion.cpp
@@ -0,0 +1,26 @@
+#include <FImage.h>
+#include <sys/time.h>
+
+using namespace FImage;
+
+int main(int argc, char **argv) {
+
+    Func fib("fib", Int(32));
+    Var x;
+    fib(x) = Select(x > 1, fib(x-1) + fib(x-2), 1);
+
+    Image<int32_t> out = fib.realize(1024);
+
+    for (int i = 2; i < 1024; i++) {
+        if (out(i) != out(i-1) + out(i-2)) {
+            printf("Failed!\n");
+            for (int i = 0; i < 1024; i++) {
+                printf("%d\n", out(i));
+            }
+            return -1;
+        }
+    }
+
+    printf("Success!\n");
+    return 0;
+}
