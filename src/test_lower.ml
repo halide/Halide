5abc0cf9db9ee58fe14b3f7363798c66f94870c3
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-11-09 01:49:20 -0500

    Fix test_lower
    It has been broken for ages.
    
    Former-commit-id: 0e5899ac82f11b623cb2f99182acfc7d347ccf87

--- src/test_lower.ml
+++ src/test_lower.ml
@@ -10,20 +10,21 @@ let _ =
 
   let f = ("f", [(i32, "x")], f32, Pure ((Load (f32, ".input", x)) *~ (FloatImm 2.7))) in
 
-  let f_call_sched = (Chunk "g.xi") in
+  let f_call_sched = (Chunk "gxi") in
+  (*let f_call_sched = Inline in*)
   (* let f_sched = [Vectorized ("x", ((Var (i32, "g.xo")) *~ (IntImm 4)) -~ two, 8)] in *)
   (* let f_sched = [Split ("x", "xo", "xi", (Var (i32, "g.xo") *~ (IntImm 4)) -~ (IntImm 1));
                  Vectorized ("xi", IntImm 0, 4); Unrolled ("xo", IntImm 0, 2)] in  *)
 
-  let f_sched = [Split ("x", "xo", "xi", ((Var (i32, "g.xo")) *~ (IntImm 4)) -~ one);
-                 Vectorized ("xi", IntImm 0, 4);
-                 Unrolled ("xo", IntImm 0, 2)
+  let f_sched = [Split ("x", "fxo", "fxi", ((Var (i32, "gxo")) *~ (IntImm 4)) -~ one);
+                 Vectorized ("fxi", IntImm 0, 4);
+                 Unrolled ("fxo", IntImm 0, 2)
                 ] in 
 
   let g = ("g", [(i32, "x")], f32, Pure ((Call (f32, "f", [x +~ one])) +~ (Call (f32, "f", [x -~ one])))) in
   
   let g_call_sched = Root in
-  let g_sched = [Split ("x", "xo", "xi", IntImm 0); Vectorized ("xi", IntImm 0, 4); Parallel ("xo", IntImm 0, IntImm 25)] in
+  let g_sched = [Split ("x", "gxo", "gxi", IntImm 0); Vectorized ("gxi", IntImm 0, 4); Parallel ("gxo", IntImm 0, IntImm 25)] in
     
   let env = Environment.empty in
   let env = Environment.add "f" f env in
