78c6d13cf9f0584a7599fe2c36e026bd8a5669d5
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-12-08 23:18:20 -0500

    Reorganize architecture stdlib ll modules
    * Rename from *.target.c to *.initmod.c
    * Add ptx_dev architecture (ptx now refers to host)
    
    Former-commit-id: 3b67f72e104d99be8bbdb2b647410251bbdd68c2

--- src/stdlib.ml
+++ src/stdlib.ml
@@ -1,4 +1,5 @@
 open Llvm
-external init_module_ptx : llmodule -> unit = "init_module_ptx"
-external init_module_x86 : llmodule -> unit = "init_module_x86"
-external init_module_arm : llmodule -> unit = "init_module_arm"
+external init_module_ptx     : llmodule -> unit = "init_module_ptx"
+external init_module_ptx_dev : llmodule -> unit = "init_module_ptx_dev"
+external init_module_x86     : llmodule -> unit = "init_module_x86"
+external init_module_arm     : llmodule -> unit = "init_module_arm"
