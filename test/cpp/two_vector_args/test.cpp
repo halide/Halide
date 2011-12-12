e76bca70f6188b7b40bdc804c7eaef2e66034ad7
Andrew Adams <abadams@csail.mit.edu>
2011-12-12 13:21:28 -0500

    Better vectorized inlining

---
+++ test/cpp/two_vector_args/test.cpp
@@ -0,0 +1,22 @@
+#include <FImage.h>
+#include <sys/time.h>
+
+using namespace FImage;
+
+int main(int argc, char **argv) {
+
+    Func f, g;
+    Var x, y;
+    
+    g(x, y) = x+y;
+
+    f(x, y) = g(x, x);
+    
+    f.vectorize(x, 4);
+
+    Image<int> out = f.realize(4, 4);
+    
+    return 0;
+}
+
+
