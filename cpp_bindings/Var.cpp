de30b4864bf503efa0fda930824b30d448e0a23b
Andrew Adams <abadams@csail.mit.edu>
2011-12-16 10:21:01 -0500

    Reindented Var.cpp
    Former-commit-id: c2b095441d3496ed491fc60ac7bd59a2a8c4804a

--- cpp_bindings/Var.cpp
+++ cpp_bindings/Var.cpp
@@ -1,31 +1,34 @@
 #include "Var.h"
 #include "Expr.h"
 
+namespace FImage {
 
-struct RVar::Contents {
-    Contents(Expr min, Expr size, const std::string &name) : min(min), size(size), name(name) {}
-    Expr min, size;
-    std::string name;
-};
+    struct RVar::Contents {
+        Contents(Expr min, Expr size, const std::string &name) : min(min), size(size), name(name) {}
+        Expr min, size;
+        std::string name;
+    };
+    
+    RVar::RVar(const Expr &min, const Expr &size) : 
+        contents(new Contents(min, size, uniqueName('r'))) {}
+    
+    RVar::RVar(const Expr &min, const Expr &size, const std::string &name) :
+        contents(new Contents(min, size, name)) {}
+    
+    const Expr &RVar::min() const {
+        return contents->min;
+    }
+    
+    const Expr &RVar::size() const {
+        return contents->size;
+    }
+    
+    const std::string &RVar::name() const {
+        return contents->name;
+    }
+    
+    bool RVar::operator==(const RVar &other) const {
+        return name() == other.name();
+    }
 
-RVar::RVar(const Expr &min, const Expr &size) : 
-    contents(new Contents(min, size, uniqueName('r'))) {}
-
-RVar::RVar(const Expr &min, const Expr &size, const std::string &name) :
-    contents(new Contents(min, size, name)) {}
-
-const Expr &RVar::min() const {
-    return contents->min;
-}
-
-const Expr &RVar::size() const {
-    return contents->size;
-}
-
-const std::string &RVar::name() const {
-    return contents->name;
-}
-
-bool RVar::operator==(const RVar &other) const {
-    return name() == other.name();
 }
