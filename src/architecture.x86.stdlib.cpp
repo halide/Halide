68daaa44cb3fda8e1a663c83b07840cfb09c02f3
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2011-12-26 15:48:52 -0500

    Switch architecture.*.stdlib.c to cpp
    Former-commit-id: 8bb49d4a8fbb13fd00ae721fa2ae3f50922f990f

--- src/architecture.x86.stdlib.cpp
+++ src/architecture.x86.stdlib.cpp
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 
+extern "C" {
+
+#define PTR_OFFSET(ptr,offset)	((void*)((char*)(ptr) + (offset)))
+
 void *safe_malloc(size_t x) {
     void *mem;
     x = ((x + 4095)/4096) * 4096;
@@ -9,18 +13,20 @@ void *safe_malloc(size_t x) {
     //printf("Allocated %lu bytes at %p with an electric fence\n", x, mem);
 
     // write the end address to unprotect in the initial fence
-    ((void **)mem)[0] = mem + x + 4096;
+    ((void **)mem)[0] = PTR_OFFSET(mem, x + 4096);
     
     mprotect(mem, 4096, PROT_NONE);
-    mprotect(mem + x + 4096, 4096, PROT_NONE);
+    mprotect(PTR_OFFSET(mem, x + 4096), 4096, PROT_NONE);
     
-    return mem + 4096;
+    return PTR_OFFSET(mem, 4096);
 }
 
 void safe_free(void *ptr) {
-    void *start = ptr - 4096;
+    void *start = PTR_OFFSET(ptr, -4096);
     mprotect(start, 4096, PROT_READ | PROT_WRITE | PROT_EXEC);
     void *end = ((void **)start)[0];
     mprotect(end, 4096, PROT_READ | PROT_WRITE | PROT_EXEC);
     free(start);
+}
+
 }
