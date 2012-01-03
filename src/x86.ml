30145f11f8f4aa36c4c4a3d62858cdb9c858e86e
Andrew Adams <abadams@csail.mit.edu>
2012-01-02 20:02:31 -0500

    better unaligned vector stores and loads
    They are now the responsibility of the architecture module. This fixes the vector_cast and vectorize test failures on x86. Arm probably needs some more cases to prevent scatters/gathers.
    
    Former-commit-id: c7d54567393917ef7f5a5726be0487ca6cbc366b

--- src/x86.ml
+++ src/x86.ml
@@ -16,28 +16,57 @@ let rec cg_expr (con:cg_context) (expr:expr) =
   let c = con.c and m = con.m and b = con.b in
   let cg_expr = cg_expr con in
 
-  (* let i8x16_t = vector_type (i8_type c) 16 in *)
-
-  (* let paddb_16 = declare_function "paddb_16"
-    (function_type (i8x16_t) [|i8x16_t; i8x16_t|]) m in *)
-
+  let ptr_t = pointer_type (i8_type c) in
   let i16x8_t = vector_type (i16_type c) 8 in
-  let pmulhw = declare_function "llvm.x86.sse2.pmulh.w"
-    (function_type (i16x8_t) [|i16x8_t; i16x8_t|]) m in
+  let i8x16_t = vector_type (i8_type c) 16 in
 
   match expr with 
     (* x86 doesn't do 16 bit vector division, but for constants you can do multiplication instead. *)
     | Bop (Div, x, Broadcast (Cast (UInt 16, IntImm y), 8)) ->
+        let pmulhw = declare_function "llvm.x86.sse2.pmulh.w"
+          (function_type (i16x8_t) [|i16x8_t; i16x8_t|]) m in
         let z = (65536/y + 1) in
         let lhs = cg_expr x in
         let rhs = cg_expr (Broadcast (Cast (UInt 16, IntImm z), 8)) in        
         build_call pmulhw [|lhs; rhs|] "" b
+          
+    (* unaligned dense 128-bit loads use movups *)
+    | Load (t, buf, Ramp(base, IntImm 1, n)) when (bit_width t = 128) ->
+        begin match (Analysis.reduce_expr_modulo base n) with 
+          | Some 0 -> con.cg_expr expr
+          | None   -> con.cg_expr expr
+          | _ ->              
+              let unaligned_load_128 = declare_function "unaligned_load_128"
+                (function_type (i8x16_t) [|ptr_t|]) m in
+              let addr = build_pointercast (con.cg_memref t buf base) ptr_t "" b in
+              let value = build_call unaligned_load_128 [|addr|] "" b in
+              build_bitcast value (type_of_val_type c t) "" b        
+        end
 
     (* We don't have any special tricks up our sleeve for this case *)
     | _ -> con.cg_expr expr 
+        
+let rec cg_stmt (con:cg_context) (stmt:stmt) =
+  let c = con.c and m = con.m and b = con.b in
+  let cg_expr = cg_expr con in
+  let cg_stmt = cg_stmt con in
+  let ptr_t = pointer_type (i8_type c) in
+  let i8x16_t = vector_type (i8_type c) 16 in
 
-let cg_stmt (con:cg_context) (stmt:stmt) =
-  con.cg_stmt stmt
+  match stmt with
+    (* unaligned 128-bit dense stores use movups *)
+    | Store (e, buf, Ramp(base, IntImm 1, n)) when (bit_width (val_type_of_expr e)) = 128 ->
+        begin match (Analysis.reduce_expr_modulo base n) with
+          | Some 0 -> con.cg_stmt stmt
+          | _ ->
+              let t = val_type_of_expr e in
+              let unaligned_store_128 = declare_function "unaligned_store_128"
+                (function_type (void_type c) [|i8x16_t; ptr_t|]) m in
+              let addr = build_pointercast (con.cg_memref t buf base) ptr_t "" b in
+              let value = build_bitcast (cg_expr e) i8x16_t "" b in
+              build_call unaligned_store_128 [|value; addr|] "" b        
+        end
+    | _ -> con.cg_stmt stmt
 
 let malloc (con:cg_context) (size:expr) = 
   let c = con.c and m = con.m and b = con.b in  
@@ -50,3 +79,4 @@ let free (con:cg_context) (address:llvalue) =
   build_call free [|address|] "" b   
 
 let env = Environment.empty
+  
