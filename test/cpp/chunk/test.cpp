2193c26cc251e78f5cf01d8a6982868c0a631d33
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-01-06 02:52:31 -0500

    Chunk test works on PTX

--- test/cpp/chunk/test.cpp
+++ test/cpp/chunk/test.cpp
@@ -3,34 +3,38 @@
 using namespace FImage;
 
 int main(int argc, char **argv) {
-  Var x, y;
-    Func f, g;
+    Var x("x"), y("y");
+    Var xo("blockidx"), xi("threadidx"), yo("blockidy"), yi("threadidy");
+    
+    Func f("f"), g("g");
 
     printf("Defining function...\n");
 
-    f(x, y) = 2.0;
+    // This is kind of ugly -- trying to force g.f to be over threadid{x,y} when chunked
+    // Also: CUDA on older machines is unhappy with doubles, so now use floats
+    f(xi, yi) = 2.0f;
     g(x, y) = f(x+1, y) + f(x-1, y);
 
-    Var xo, xi, yo, yi;
-
-    g.split(x, xo, xi, 4);
-    g.split(y, yo, yi, 4);
+    g.split(x, xo, xi, 8);
+    g.split(y, yo, yi, 8);
     g.transpose(xo, yi);
     f.chunk(xo);
+    
+    if (use_gpu()) {
+        g.parallel(xo).parallel(xi).parallel(yo).parallel(yi);
+        f.parallel(xi).parallel(yi);
+    }
 
     printf("Realizing function...\n");
 
-    assert(f.returnType() == Float(64));
-    assert(g.returnType() == Float(64));
-
-    Image<double> im = g.realize(32, 32);
+    Image<float> im = g.realize(32, 32);
 
     for (size_t i = 0; i < 32; i++) {
-        if (im(i) != 4.0) {
-            for (size_t j = 0; j < 32; j++) {
-                printf("im[%d] = %f\n", j, im(j));
+        for (size_t j = 0; j < 32; j++) {
+            if (im(i,j) != 4.0) {
+                printf("im[%d, %d] = %f\n", (int)i, (int)j, im(i,j));
+                return -1;
             }
-            return -1;
         }
     }
 
