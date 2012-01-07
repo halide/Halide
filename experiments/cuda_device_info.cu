67ff040ac378c6c5b449b48210de15c045a73e57
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-01-07 17:39:00 -0500

    Fix printf for size_t values in cuda_device_info

--- experiments/cuda_device_info.cu
+++ experiments/cuda_device_info.cu
@@ -6,19 +6,19 @@ void printDevProp(cudaDeviceProp devProp)
     printf("Major revision number:         %d\n",  devProp.major);
     printf("Minor revision number:         %d\n",  devProp.minor);
     printf("Name:                          %s\n",  devProp.name);
-    printf("Total global memory:           %u\n",  devProp.totalGlobalMem);
-    printf("Total shared memory per block: %u\n",  devProp.sharedMemPerBlock);
+    printf("Total global memory:           %zu\n",  devProp.totalGlobalMem);
+    printf("Total shared memory per block: %zu\n",  devProp.sharedMemPerBlock);
     printf("Total registers per block:     %d\n",  devProp.regsPerBlock);
     printf("Warp size:                     %d\n",  devProp.warpSize);
-    printf("Maximum memory pitch:          %u\n",  devProp.memPitch);
+    printf("Maximum memory pitch:          %zu\n",  devProp.memPitch);
     printf("Maximum threads per block:     %d\n",  devProp.maxThreadsPerBlock);
     for (int i = 0; i < 3; ++i)
     printf("Maximum dimension %d of block:  %d\n", i, devProp.maxThreadsDim[i]);
     for (int i = 0; i < 3; ++i)
     printf("Maximum dimension %d of grid:   %d\n", i, devProp.maxGridSize[i]);
     printf("Clock rate:                    %d\n",  devProp.clockRate);
-    printf("Total constant memory:         %u\n",  devProp.totalConstMem);
-    printf("Texture alignment:             %u\n",  devProp.textureAlignment);
+    printf("Total constant memory:         %zu\n",  devProp.totalConstMem);
+    printf("Texture alignment:             %zu\n",  devProp.textureAlignment);
     printf("Concurrent copy and execution: %s\n",  (devProp.deviceOverlap ? "Yes" : "No"));
     printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
     printf("Kernel execution timeout:      %s\n",  (devProp.kernelExecTimeoutEnabled ? "Yes" : "No"));
