6e220c5fe9957aa8430191709bc8b07b4737a2e9
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-01-11 23:42:28 -0500

    Working chunked camera pipe on GPU
    Produces correct output on kaotom.
    
    - named variables, functions
    - switched output to 16 bit, cast down for process dump to png
    - set schedule for GPU

--- apps/camera_pipe/process.cpp
+++ apps/camera_pipe/process.cpp
@@ -12,9 +12,14 @@ extern "C" {
 #include "../png.h"
 using namespace FImage;
 
+// TODO: fold into module
+extern "C" { typedef struct CUctx_st *CUcontext; }
+namespace FImage { CUcontext cuda_ctx = 0; }
+
 int main(int argc, char **argv) {
     Image<uint16_t> input = load<uint16_t>(argv[1]);
-    Image<uint8_t> output(input.width(), input.height(), 3);
+    Image<uint16_t> output(input.width(), input.height(), 3);
+    Image<uint8_t> fout(output.width(), output.height(), 3);
 
     timeval t1, t2;
     
@@ -28,15 +33,28 @@ int main(int argc, char **argv) {
                                 {-0.3826f,  1.5906f, -0.2080f, -25.4311f},
                                 {-0.0888f, -0.7344f,  2.2832f, -20.0826f}};    
 
+    // Warm up run
+    //curved(atof(argv[2]), atof(argv[3]), atof(argv[4]),
+    //       matrix_3200, matrix_7000, input, output);
+
     gettimeofday(&t1, NULL);
-    curved(atof(argv[2]), atof(argv[3]), atof(argv[4]),
-           matrix_3200, matrix_7000, input, output);
+    curved(
+            atof(argv[2]),
+            atof(argv[3]), atof(argv[4]),
+            matrix_3200, matrix_7000,
+            input, output);
 
     //curved(atof(argv[2]), atof(argv[3]), input, output);
     gettimeofday(&t2, NULL);
 
     printf("%3.3f ms\n", (t2.tv_sec - t1.tv_sec)*1000.0f + (t2.tv_usec - t1.tv_usec)/1000.0f);
 
-    save(output, argv[5]);
+    // convert output to 8 bits for easy viewing
+    for (int x = 0; x < output.width(); x++)
+        for (int y = 0; y < output.height(); y++)
+            for (int c = 0; c < output.channels(); c++)
+                fout(x,y,c) = output(x,y,c);
+    save(fout, argv[5]);
+    save(output, "out.16.png");
     return 0;
 }
