35182ceb04d37399419eec9319ece82c198685c2
Andrew Adams <abadams@csail.mit.edu>
2012-01-13 00:47:31 -0500

    scalar bilateral grid code faster than sylvain.
    good.
    
    Also checked in Sylvain's algorithm

--- apps/bilateral_grid/bilateral_grid.cpp
+++ apps/bilateral_grid/bilateral_grid.cpp
@@ -2,83 +2,87 @@
 
 using namespace FImage;
 
+Expr lerp(Expr a, Expr b, Expr alpha) {
+    return (1.0f - alpha)*a + alpha*b;
+}
+
 int main(int argc, char **argv) {
-    UniformImage input(UInt(16), 2);
+    UniformImage input(Float(32), 2);
     Uniform<float> r_sigma;
-    Uniform<int> s_sigma;
+    int s_sigma = atoi(argv[1]);
     Var x("x"), y("y"), z("z"), c("c"), xi("xi"), yi("yi");
 
-    // Convert the 16-bit input to floats
-    Func floating;
-    floating(x, y, c) = Cast<float>(input(x, y, c)) / 65535.0f;
-
-    // Take the luminance
-    Func luminance;
-    luminance(x, y) = floating(x, y, 0) * 0.299f + floating(x, y, 1) * 0.587f + floating(x, y, 2) * 0.114f;
-
     // Add a boundary condition 
-    Func clamped;
-    clamped(x, y) = luminance(Clamp(x, 0, input.width()),
-                              Clamp(y, 0, input.height()));                                
+    Func clamped("clamped");
+    clamped(x, y) = input(Clamp(x, 0, input.width()),
+                          Clamp(y, 0, input.height()));                                
 
-    // Do linear splats to the grid
+    // Construct the bilateral grid 
     RVar i(0, s_sigma, "i"), j(0, s_sigma, "j");
     Expr val = clamped(x * s_sigma + i - s_sigma/2, y * s_sigma + j - s_sigma/2);
-    Expr zv = val / r_sigma;
-    Expr zi = Cast<int>(floor(zv));
-    Expr zf = zv - floor(zv);
+    val = Clamp(val, 0.0f, 1.0f);
+    Expr zi = Cast<int>(val * (1.0f/r_sigma) + 0.5f);
     Func grid("grid");
-    RVar k(0, 2, "k");
-    grid(x, y, zi+k) += Select(k == 0, 1.0f-zf, zf) * (val, 1.0f);
+    printf("A\n");
+    grid(x, y, z, c) = 0.0f;
+    printf("B\n");
+    grid(x, y, zi, c) += Select(c == 0, val, 1.0f);
+    printf("C\n");
 
-    // Blur the grid
-    Func blurx, blury, blurz;
-    blurx(x, y, z) = grid(x-1, y, z) + 2*grid(x, y, z) + grid(x+1, y, z);
-    blury(x, y, z) = blurx(x, y-1, z) + 2*blurx(x, y, z) + blurx(x, y+1, z);
-    blurz(x, y, z) = blury(x, y, z-1) + 2*blury(x, y, z) + blury(x, y, z+1);
-    blurz.root();
+    // Blur the grid using a five-tap filter
+    Func blurx("blurx"), blury("blury"), blurz("blurz");
+    blurx(x, y, z) = grid(x-2, y, z) + grid(x-1, y, z)*4 + grid(x, y, z)*6 + grid(x+1, y, z)*4 + grid(x+1, y, z);
+    blury(x, y, z) = blurx(x, y-2, z) + blurx(x, y-1, z)*4 + blurx(x, y, z)*6 + blurx(x, y+1, z)*4 + blurx(x, y+2, z);
+    blurz(x, y, z) = blury(x, y, z-2) + blury(x, y, z-1)*4 + blury(x, y, z)*6 + blury(x, y, z+1)*4 + blury(x, y, z+2);
 
     // Take trilinear samples to compute the output in tiles
-    val = Clamp(clamped(x*s_sigma + xi, y*s_sigma + yi), 0.0f, 1.0f);
-    zv = val / r_sigma;
-    zi = Cast<int>(floor(zv));
-    zf = zv - floor(zv);
+    val = clamped(x*s_sigma + xi, y*s_sigma + yi);
+    val = Clamp(val, 0.0f, 1.0f);
+    Expr zv = val * (1.0f/r_sigma);
+    zi = Cast<int>(zv);
+    Expr zf = zv - zi;
     Expr xf = Cast<float>(xi) / s_sigma;
     Expr yf = Cast<float>(yi) / s_sigma;
+
+    Func interpolated("interpolated");    
+
+    interpolated(x, y, xi, yi, c) = 
+        lerp(lerp(lerp(blurz(x, y, zi, c), blurz(x+1, y, zi, c), xf),
+                  lerp(blurz(x, y+1, zi, c), blurz(x+1, y+1, zi, c), xf),
+                  yf),
+             lerp(lerp(blurz(x, y, zi+1, c), blurz(x+1, y, zi+1, c), xf),
+                  lerp(blurz(x, y+1, zi+1, c), blurz(x+1, y+1, zi+1, c), xf),
+                  yf),
+             zf);
+
+    /*
+      // Precompute the lerp weights
+    Func bilerpWeight;
+    bilerpWeight(xi, yi) = xf*yf;
+    bilerpWeight.root();
+    
+    
+    interpolated(x, y, xi, yi, c) = 
+        (bilerpWeight(s_sigma-xi-1, s_sigma-yi-1) * lerp(blurz(x,   y,   zi, c), blurz(x,   y, zi+1, c), zf) + 
+         bilerpWeight(xi, s_sigma-yi-1) * lerp(blurz(x+1, y,   zi, c), blurz(x+1, y, zi+1, c), zf) + 
+         bilerpWeight(s_sigma-xi-1, yi) * lerp(blurz(x,   y+1, zi, c), blurz(x,   y+1, zi+1, c), zf) + 
+         bilerpWeight(xi, yi) * lerp(blurz(x+1, y+1, zi, c), blurz(x+1, y+1, zi+1, c), zf));
+    */
+
     Func outTiles;
-    outTiles(x, y, xi, yi, c) = 
-        (blurz(x, y, zi, c) * (1.0f - xf) * (1.0f - yf) * (1.0f - zf) + 
-         blurz(x+1, y, zi, c) * xf * (1.0f - yf) * (1.0f - zf) + 
-         blurz(x, y+1, zi, c) * (1.0f - xf) * yf * (1.0f - zf) + 
-         blurz(x+1, y+1, zi, c) * xf * yf * (1.0f - zf) + 
-         blurz(x, y, zi+1, c) * (1.0f - xf) * (1.0f - yf) * zf + 
-         blurz(x+1, y, zi+1, c) * xf * (1.0f - yf) * zf + 
-         blurz(x, y+1, zi+1, c) * (1.0f - xf) * yf * zf + 
-         blurz(x+1, y+1, zi+1, c) * xf * yf * zf);
+    outTiles(x, y, xi, yi) = interpolated(x, y, xi, yi, 0) / interpolated(x, y, xi, yi, 1);
 
-    outTiles.root();
+    // Remove tiles to get the result
+    Func smoothed("smoothed");
+    smoothed(x, y) = outTiles(x/s_sigma, y/s_sigma, x%s_sigma, y%s_sigma);
 
-    // Remove tiles to get the result in homogeneous form
-    Func homogeneous;
-    homogeneous(x, y) = outTiles(x/s_sigma, y/s_sigma, x%s_sigma, y%s_sigma);
+    grid.root();
+    blurx.root();
+    blury.root();
+    blurz.root();
+    smoothed.root();
 
-    // Normalize
-    Func smoothed;
-    smoothed(x, y) = homogeneous(x, y, 0)/homogeneous(x, y, 1);
-
-    // Add clarity to the luminance channel by extrapolating away from the smoothed version
-    Func clarified;
-    clarified = 2.0f*luminance - smoothed;
-
-    // reintroduce color
-    Func color;
-    color(x, y, c) = clarified(x, y) * floating(x, y, c) / luminance(x, y);
-
-    // convert back to 16-bit
-    Func output("clarity");
-    output = Cast<uint16_t>(Clamp(color, 0.0f, 1.0f) * 65535.0f);
-
-    output.compile();
+    smoothed.compileToFile("bilateral_grid");
 
     return 0;
 }
--- apps/bilateral_grid/filter.cpp
+++ apps/bilateral_grid/filter.cpp
@@ -4,17 +4,28 @@
 #include <sys/time.h>
 
 extern "C" {
-  #include "clarity.h"
+  #include "bilateral_grid.h"
 }
 
 #include "../Util.h"
+#include "../png.h"
+using namespace FImage;
 
 int main(int argc, char **argv) {
 
-    Image<uint16_t> input = load<uint16_t>(argv[1]);
-    Image<uint16_t> output(input.width(), input.height(), input.channels());
+    Image<float> input = load<float>(argv[1]);
+    Image<float> output(input.width(), input.height(), 1);
 
-    clarity(10, 0.1f, input, output);
+    timeval t1, t2;
+
+    gettimeofday(&t1, NULL);
+    for (int i = 0; i < 1; i++) 
+        bilateral_grid(atof(argv[3]), input, output);
+    gettimeofday(&t2, NULL);
+
+    double t = (t2.tv_sec - t1.tv_sec)*1000.0 + (t2.tv_usec - t1.tv_usec)/1000.0;
+    printf("Time: %fms\n", t);
+
     save(output, argv[2]);
 
     return 0;
