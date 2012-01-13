d2e0677ddf7c6231101b9ebd42b4f23485e76f1d
Andrew Adams <abadams@csail.mit.edu>
2012-01-12 22:07:42 -0500

    Fast camera pipe on arm.

--- apps/camera_pipe/camera_pipe.cpp
+++ apps/camera_pipe/camera_pipe.cpp
@@ -14,6 +14,7 @@ Func hot_pixel_suppression(Func input) {
     Func denoised("denoised");
     denoised(x, y) = Clamp(input(x, y), min, max);
     
+    // Compute this in chunks over tiles, vectorized by 8
     denoised.chunk(tx).vectorize(x, 8);
 
     return denoised;
@@ -40,11 +41,24 @@ Func demosaic(Func raw) {
     // x_y = the value of channel x at a site in the input of channel y
     // gb refers to green sites in the blue rows
     // gr refers to green sites in the red rows
+
+    // Deinterleave the color channels
+    Func deinterleaved("deinterleaved");
+    Var t, ti;
+    deinterleaved(x, y, t) = Select(t == 0, raw(2*x, 2*y), 
+                                    Select(t == 1, raw(2*x+1, 2*y), 
+                                           Select(t == 2, raw(2*x, 2*y+1), raw(2*x+1, 2*y+1))));
+
+    // Unroll the tuple part and compute it in the inside loop
+    deinterleaved.split(t, t, ti, 4).unroll(ti).transpose(y, ti).transpose(x, ti);
+    deinterleaved.chunk(tx).vectorize(x, 8);
+
+    // Give more convenient names to the four channels we know
     Func r_r("r_r"), g_gr("g_gr"), g_gb("g_gb"), b_b("b_b");    
-    g_gr(x, y) = raw(2*x, 2*y);
-    r_r(x, y)  = raw(2*x+1, 2*y);
-    b_b(x, y)  = raw(2*x, 2*y+1);
-    g_gb(x, y) = raw(2*x+1, 2*y+1);
+    g_gr(x, y) = deinterleaved(x, y, 0);
+    r_r(x, y)  = deinterleaved(x, y, 1);
+    b_b(x, y)  = deinterleaved(x, y, 2);
+    g_gb(x, y) = deinterleaved(x, y, 3);
 
     // These are the ones we need to interpolate
     Func b_r("b_r"), g_r("g_r");
@@ -64,16 +78,12 @@ Func demosaic(Func raw) {
 
     g_r(x, y)  = Select(ghd_r < gvd_r, gh_r, gv_r);
 
-    g_r.chunk(tx).vectorize(x, 8);
-
     Expr gv_b  =    (g_gr(x, y+1) + g_gr(x, y))/2;
     Expr gvd_b = abs(g_gr(x, y+1) - g_gr(x, y));
     Expr gh_b  =    (g_gb(x-1, y) + g_gb(x, y))/2;
     Expr ghd_b = abs(g_gb(x-1, y) - g_gb(x, y));
 
     g_b(x, y)  = Select(ghd_b < gvd_b, gh_b, gv_b);
-
-    g_b.chunk(tx).vectorize(x, 8);
 
     // Next interpolate red at gr by first interpolating, then
     // correcting using the error green would have had if we had
@@ -83,24 +93,16 @@ Func demosaic(Func raw) {
     correction = g_gr(x, y) - (g_r(x, y) + g_r(x-1, y))/2;
     r_gr(x, y) = correction + (r_r(x-1, y) + r_r(x, y))/2;
 
-    r_gr.chunk(tx).vectorize(x, 8);
-    
     // Do the same for other reds and blues at green sites
     correction = g_gr(x, y) - (g_b(x, y) + g_b(x, y-1))/2;
     b_gr(x, y) = correction + (b_b(x, y) + b_b(x, y-1))/2;
 
-    b_gr.chunk(tx).vectorize(x, 8);
-
     correction = g_gb(x, y) - (g_r(x, y) + g_r(x, y+1))/2;
     r_gb(x, y) = correction + (r_r(x, y) + r_r(x, y+1))/2;
-
-    r_gb.chunk(tx).vectorize(x, 8);
 
     correction = g_gb(x, y) - (g_b(x, y) + g_b(x+1, y))/2;
     b_gb(x, y) = correction + (b_b(x, y) + b_b(x+1, y))/2;
 
-    b_gb.chunk(tx).vectorize(x, 8);
-            
     // Now interpolate diagonally to get red at blue and blue at
     // red. Hold onto your hats; this gets really fancy. We do the
     // same thing as for interpolating green where we try both
@@ -120,7 +122,6 @@ Func demosaic(Func raw) {
 
     r_b(x, y)  = Select(rpd_b < rnd_b, rp_b, rn_b);
 
-    r_b.chunk(tx).vectorize(x, 8);
 
     // Same thing for blue at red
     correction = g_r(x, y)  - (g_b(x, y) + g_b(x+1, y-1))/2;
@@ -133,8 +134,6 @@ Func demosaic(Func raw) {
 
     b_r(x, y)  =  Select(bpd_r < bnd_r, bp_r, bn_r);    
 
-    b_r.chunk(tx).vectorize(x, 8);
-
     // Interleave the resulting channels
     Func r = interleave_y(interleave_x(r_gr, r_r),
                           interleave_x(r_b, r_gb));
@@ -143,10 +142,19 @@ Func demosaic(Func raw) {
     Func b = interleave_y(interleave_x(b_gr, b_r),                          
                           interleave_x(b_b, b_gb));
 
-    // Compute these guys in tiles before interleaving
-    r.chunk(tx);
-    g.chunk(tx);
-    b.chunk(tx);
+    // Compute these in chunks over tiles, vectorized by 8
+    g_r.chunk(tx).vectorize(x, 8);
+    g_b.chunk(tx).vectorize(x, 8);
+    r_gr.chunk(tx).vectorize(x, 8);
+    b_gr.chunk(tx).vectorize(x, 8);
+    r_gb.chunk(tx).vectorize(x, 8);
+    b_gb.chunk(tx).vectorize(x, 8);
+    r_b.chunk(tx).vectorize(x, 8);
+    b_r.chunk(tx).vectorize(x, 8);
+    // These interleave in y, so unrolling them in y might help
+    r.chunk(tx).vectorize(x, 8).unroll(y, 2);
+    g.chunk(tx).vectorize(x, 8).unroll(y, 2);
+    b.chunk(tx).vectorize(x, 8).unroll(y, 2);
 
     Func output("dem");
     output(x, y) = (r(x, y), g(x, y), b(x, y));
@@ -166,15 +174,20 @@ Func color_correct(Func input, UniformImage matrix_3200, UniformImage matrix_700
     matrix.root();
 
     Func corrected("corrected");
-    Expr v[] = {matrix(3, 0), matrix(3, 1), matrix(3, 2)};
-    for (int j = 0; j < 3; j++) {
-        for (int i = 0; i < 3; i++) {
-            v[j] = v[j] + matrix(i, j) * Cast<int32_t>(input(x, y, i));
-        }
-        v[j] = Cast<int16_t>(v[j]/256);
-    }
+    Expr ir = Cast<int32_t>(input(x, y, 0));
+    Expr ig = Cast<int32_t>(input(x, y, 1));
+    Expr ib = Cast<int32_t>(input(x, y, 2));
+   
+    Expr r, g, b;
+    r = matrix(3, 0) + matrix(0, 0) * ir + matrix(1, 0) * ig + matrix(2, 0) * ib;
+    g = matrix(3, 1) + matrix(0, 1) * ir + matrix(1, 1) * ig + matrix(2, 1) * ib;
+    b = matrix(3, 2) + matrix(0, 2) * ir + matrix(1, 2) * ig + matrix(2, 2) * ib;
+
+    corrected(x, y) = Cast<int16_t>((r, g, b)/256);
+
+    Var c = corrected.arg(2);
+    corrected.chunk(tx).transpose(y, c).transpose(x, c).vectorize(x, 4).unroll(c, 3);
     
-    corrected(x, y) = (v[0], v[1], v[2]);
     return corrected;
 }
 
@@ -183,7 +196,7 @@ Func apply_curve(Func input, Uniform<float> gamma, Uniform<float> contrast) {
     // copied from FCam
     Func curve("curve");
 
-    Expr xf = Cast<float>(x)/1024.0f;
+    Expr xf = Clamp(Cast<float>(x)/1024.0f, 0.0f, 1.0f);
     Expr g = pow(xf, 1.0f/gamma);
     Expr b = 2.0f - pow(2.0f, contrast/100.0f);
     Expr a = 2.0f - 2.0f*b; 
@@ -191,14 +204,12 @@ Func apply_curve(Func input, Uniform<float> gamma, Uniform<float> contrast) {
                     1.0f - (a*(1.0f-g)*(1.0f-g) + b*(1.0f-g)),
                     a*g*g + b*g);
     Expr val = Cast<uint8_t>(Clamp(z*256.0f, 0.0f, 255.0f));
-    curve(x) = val; //Debug(val, "curve ", x, xf, g, z, val);
-
+    curve(x) = val;
     curve.root(); // It's a LUT, compute it once ahead of time.
 
     Func curved("curved");
-    // This is after the color transform, so the input could be
-    // negative or very large. Clamp it back to 10 bits before applying the curve.
-    curved(x, y, c) = curve(Clamp(Cast<int32_t>(input(x, y, c)), 0, 1023));
+    curved(x, y, c) = curve(input(x, y, c));
+
     return curved;
 }
 
@@ -215,9 +226,11 @@ Func process(Func raw,
     im = color_correct(im, matrix_3200, matrix_7000, color_temp);
     im = apply_curve(im, gamma, contrast);
 
-    processed(tx, ty, c) = im(tx, ty, c);
-    Var co;
-    processed.tile(tx, ty, xi, yi, 32, 32).split(c, co, c, 3).transpose(ty, c).transpose(tx, c).unroll(c);//.vectorize(xi, 8).parallel(ty);
+    Var co, ci;    
+    processed(tx, ty, c) = im(tx, ty, ci);
+
+    processed.split(c, co, ci, 3);
+    processed.tile(tx, ty, xi, yi, 32, 32).transpose(ty, ci).transpose(tx, ci);//.vectorize(xi, 8).parallel(ty);
 
     return processed;
 }
