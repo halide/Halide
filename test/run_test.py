8657cfc936b6679d4685009aeed100d66ce5e7ae
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-01-13 22:24:11 -0500

    run_test.py works on python2.5

--- test/run_test.py
+++ test/run_test.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python
 
-from subprocess import check_call,check_output,CalledProcessError
+from __future__ import with_statement
 import os
 import os.path
 import json
+
+try:
+    from subprocess import check_call,check_output,CalledProcessError
+except:
+    import subprocess
+    def check_output(*popenargs, **kwargs):
+        r"""Run command with arguments and return its output as a byte string.
+    
+        Backported from Python 2.7 as it's implemented as pure python on stdlib.
+    
+        >>> check_output(['/usr/bin/python', '--version'])
+        Python 2.6.2
+        """
+        process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+        output, unused_err = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            error = subprocess.CalledProcessError(retcode, cmd)
+            error.output = output
+            raise error
+        return output
+    check_call=subprocess.check_call
+    CalledProcessError=subprocess.CalledProcessError
 
 # TODO: make pch: `g++ -x c++-header -I ../ImageStack/src test_plugin.h`
 EPSILON = 0.001
