acfeb00b2c2813df7e2c5c735b2b6e65bebe9d35
Andrew Adams <abadams@csail.mit.edu>
2012-01-31 14:43:55 -0500

    Added wavelet app
    Former-commit-id: 6333930c47ac645d5a865a51a70d302deabf0c1b

---
+++ apps/wavelet/wavelet.cpp
@@ -0,0 +1,86 @@
+#include "Halide.h"
+
+using namespace Halide;
+
+Var x, y, c;
+
+/*
+class Lambda {
+public:
+    Lambda(Var x, Var y) : x(x), y(y) {
+    }
+
+    Func operator=(Expr rhs) {
+        f(x, y) = rhs;
+        return f;
+    }
+
+private:
+    Var x, y;
+    Func f;
+};
+*/
+
+Func haar_x(Func in) {
+    Func out;
+    out(x, y) = (in(2*x, y) + in(2*x+1, y),
+                 in(2*x, y) - in(2*x+1, y))/2;
+    return out;
+}
+
+Func inverse_haar_x(Func in) {
+    Func out;
+    out(x, y) = select(x%2 == 0, 
+                       in(x/2, y, 0) + in(x/2, y, 1),
+                       in(x/2, y, 0) - in(x/2, y, 1));
+    out.unroll(x, 2);
+    return out;
+}
+
+const float D0 = 0.4829629131445341f;
+const float D1 = 0.83651630373780772f;
+const float D2 = 0.22414386804201339f;
+const float D3 = -0.12940952255126034f;
+
+Func daubechies_x(Func in) {
+    Func out;
+    out(x, y) = (D0*in(2*x-1, y) + D1*in(2*x, y) + D2*in(2*x+1, y) + D3*in(2*x+2, y),
+                 D3*in(2*x-1, y) - D2*in(2*x, y) + D1*in(2*x+1, y) - D0*in(2*x+2, y));
+    return out;
+}
+
+Func inverse_daubechies_x(Func in) {
+    Func out;
+    out(x, y) = select(x%2 == 0,
+                       D2*in(x/2, y, 0) + D1*in(x/2, y, 1) + D0*in(x/2+1, y, 0) + D3*in(x/2+1, y, 1),
+                       D3*in(x/2, y, 0) - D0*in(x/2, y, 1) + D1*in(x/2+1, y, 0) - D2*in(x/2+1, y, 1));
+    out.unroll(x, 2);
+    return out;
+}
+
+int main(int argc, char **argv) {
+
+    UniformImage image(Float(32), 2);    
+    UniformImage wavelet(Float(32), 3);
+
+    // Add a boundary condition for daubechies
+    Func clamped;
+    clamped(x, y) = image(clamp(x, 0, image.width()),
+                          clamp(y, 0, image.height()));
+    Func wavelet_clamped;
+    wavelet_clamped(x, y, c) = wavelet(clamp(x, 0, wavelet.width()),
+                                       clamp(y, 0, wavelet.height()), c);
+    inverse_daubechies_x(wavelet_clamped).compileToFile("inverse_daubechies_x");
+
+    Func dx = daubechies_x(clamped);
+    dx.compileToFile("daubechies_x");
+
+    inverse_haar_x(wavelet_clamped).compileToFile("inverse_haar_x");
+    haar_x(clamped).compileToFile("haar_x");
+
+
+    return 0;
+}
+
+
+
