43041e40a89049c240d6ee75ced7bcc9967e8031
Andrew Adams <andrew.b.adams@gmail.com>
2012-03-16 14:36:50 -0400

    Better error message for adding multiple reduction update rules
    Former-commit-id: 530d9079a07fb6c775e4c3e60e0343bf4a604d24

--- apps/error/error.cpp
+++ apps/error/error.cpp
@@ -4,14 +4,21 @@ using namespace Halide;
 #include "../png.h"
 
 int main(int argc, char **argv) {
+    Var x("x");
+    RVar rx(0, 100, "rx");
 
     /* Multiple definitions */
+    /*
     Func f1("f1");
-    Var x;
     f1(x) = x;
     f1(x) = x*2;
+    */
 
-    f1.compileToFile("f1");    
+    /* Add more than one update step to a reduction */
+    Func f2("f2");
+    f2(x) = x;
+    f2(rx) += 5*rx;
+    f2(rx) *= rx;
 
     return 0;
 }
