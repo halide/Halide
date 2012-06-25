6c16b9bd05b1823feec3cce283412eff42b372ca
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-06-25 13:10:19 -0400

    Stub C module
    (Stashing old work before replacing with more structured codegen)
    
    Former-commit-id: 57c9dc05759b29889e202967baa3671270152b0e

---
+++ src/test_cg_c.ml
@@ -0,0 +1,15 @@
+open Ir
+open Sexplib
+open Batteries_uni
+
+let _ =
+  let entry =
+    File.with_file_in Sys.argv.(1)
+      (fun ins -> entrypoint_of_sexp (Sexp.of_string (String.strip (IO.read_all ins))))
+  in
+
+  let name,args,body = entry in
+
+  Printf.printf "loaded %s:\n%s\n  ==>\n" name (Ir_printer.string_of_stmt body);
+
+  Printf.printf "%s\n" (Cg_c.cg_entry entry)
