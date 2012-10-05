cf37f6c85855827e8e0a6994f0d6f669c8112d03
Connelly Barnes <cbarnes@mgarbi-MacBookPro.(none)>
2012-10-05 16:03:51 -0400

    py bindings -- example child autotune script

---
+++ py_bindings/child_autotune.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+HL_NUMTHREADS=4 python autotune.py autotune_compile_child examples.blur.filter_func "blur_x_blurUInt16.root().vectorize(x_blurUInt16,8)" apollo2.png 5 12680 ./tune/0_72
+HL_NUMTHREADS=4 python autotune.py autotune_run_child examples.blur.filter_func "blur_x_blurUInt16.root().vectorize(x_blurUInt16,8)" apollo2.png 5 12680 ./tune/0_72
