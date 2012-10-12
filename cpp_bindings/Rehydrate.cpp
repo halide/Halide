4bfa2c0ba50d59069fe60f01936ca3dd81fa1147
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-10-12 02:00:33 -0400

    Add initial rehydration of Func from sexp
    There is still missing functionality:
    
    - uniforms
    - uniform images
    - reduction functions
    
    and not much has been tested, but it works for test/cpp/rehydrate.cpp.
    
    
    Former-commit-id: 6c681c72d9488108b712f86505de2d287b3979b9

---
+++ cpp_bindings/Rehydrate.cpp
@@ -0,0 +1,155 @@
+#include "Rehydrate.h"
+#include "FuncContents.h"
+#include "ExprContents.h"
+#include "Util.h"
+
+#include <map>
+#include <vector>
+using std::map;
+using std::vector;
+using std::pair;
+
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+namespace Halide {
+
+    ML_FUNC1(deserializeEnv)
+    ML_FUNC1(getEnvDefinitions)
+    ML_FUNC1(typeOfExpr)
+    ML_FUNC1(varsInExpr)
+    ML_FUNC1(callsInExpr)
+
+    ML_FUNC1(callIsFunc)
+    ML_FUNC1(callIsExtern)
+    ML_FUNC1(callIsImage)
+
+    // Mirror helpers for unpacking `definitions` from the deserialized environment.
+    // Yes, this is ugly at this point. Better ideas (short of adding dozens of
+    // accessor callback helpers to halide_cpp.ml) are welcome.
+    struct Definition {
+        Definition() {}
+
+        Definition(MLVal d) : name(string(d[0])), ret_t(d[2]), body(d[3]) {
+            MLVal _args = arrayOfList(d[1]);
+            for (int i = 0; i < _args.array_length(); i++) {
+                args.push_back(make_pair(string(_args[i][1]), _args[i][0]));
+            }
+        }
+
+        struct Body {
+            Body() {}
+
+            Body(MLVal b) {
+                if (functionIsPure(b)) {
+                    ty = Pure;
+                    rhs = getPureBody(b);
+                } else {
+                    assert(functionIsReduce(b));
+                    ty = Reduce;
+                    MLVal red = getReduceBody(b);
+                    rhs = red[0];
+                    updateLoc = arrayOfList(red[1]);
+                    updateFunc = string(red[2]);
+                    dom = arrayOfList(red[3]);
+                }
+            }
+
+            enum {
+                Pure,
+                Reduce
+            } ty;
+
+            MLVal rhs; // pure or initializer expr
+
+            // if reduce:
+            MLVal updateLoc;
+            string updateFunc;
+            MLVal dom;
+        };
+
+        string name;
+        vector<pair<string, Type> > args;
+        Type ret_t;
+        Body body;
+
+        bool isReduce() { return body.ty == Body::Reduce; }
+    };
+
+    void testArray(Func f) {
+        string sexp = f.serialize();
+        cerr << sexp << endl;
+        rehydrate(sexp, f.name());
+    }
+
+    Expr rehydrateExpr(map<string, Definition>& defs, map<string, Func>& env, MLVal expr);
+    Func rehydrateFunc(map<string, Definition>& defs, map<string, Func>& env, const string func);
+
+    Expr rehydrateExpr(map<string, Definition>& defs, map<string, Func>& env, MLVal expr) {
+        Expr e(expr, typeOfExpr(expr));
+
+        cerr << "rehydrateExpr: " << string(stringOfExpr(expr)) << endl;
+
+        // Track dependences
+        MLVal vars = arrayOfList(varsInExpr(expr));
+        for (int i = 0; i < vars.array_length(); i++) {
+            e.child(Var(vars[i]));
+            // TODO: handle uniforms!
+            cerr << "  var: " << string(vars[i]) << endl;
+        }
+
+        MLVal calls = arrayOfList(callsInExpr(expr));
+        for (int i = 0; i < calls.array_length(); i++) {
+            string name = calls[i][0];
+            Type ret = calls[i][1][1];
+            if (callIsFunc(calls[i][1][0])) {
+                Func f = rehydrateFunc(defs, env, name);
+                e.child(FuncRef(f));
+            } else if (callIsImage(calls[i][1][0])) {
+                // TODO: figure out dimensionality of call
+            }
+        }
+
+        return e;
+    }
+
+    Func rehydrateFunc(map<string, Definition>& defs, map<string, Func>& env, const string func) {
+        // If we've already rehydrated this, return it
+        if (env.count(func)) return env[func];
+
+        cerr << "Rehydrating " << func << endl;
+
+        // Build a new FuncContents from the definition
+        Definition& def = defs[func];
+        FuncContents *c = new FuncContents(def.name, def.ret_t);
+        assert(!def.isReduce()); // TODO: this isn't handled yet
+        
+        for (int i = 0; i < def.args.size(); i++) {
+            c->args.push_back(Var(def.args[i].first));
+        }
+
+        // Rehydrate the rhs Expr
+        c->rhs = rehydrateExpr(defs, env, def.body.rhs);
+
+        // Add it to the environment
+        env[func] = c->toFunc();
+
+        return env[func];
+    }
+
+    Func rehydrate(const string sexp, const string rootFunc) {
+        map<string, Func> env;
+
+        MLVal d = getEnvDefinitions(deserializeEnv(sexp));
+        map<string, Definition> defs;
+        for (int i = 0; i < d.array_length(); i++) {
+            Definition def(d[i]);
+            defs[def.name] = def;
+            cerr << def.name << " is " << (def.isReduce() ? "" : "not ") << "reduce" << endl;
+        }
+
+        return rehydrateFunc(defs, env, rootFunc);
+    }
+
+}
---
+++ test/cpp/rehydrate/test.cpp
@@ -0,0 +1,44 @@
+#include <Halide.h>
+using namespace Halide;
+
+namespace Halide
+{
+	extern void testArray(Func f);
+}
+
+int main(int argc, char **argv) {
+
+    Func f, g, h; Var x, y;
+    
+    h(x) = x;
+    g(x) = h(x-1) + h(x+1);
+    f(x, y) = (g(x-1) + g(x+1)) + y;
+
+    Func ff = rehydrate(f.serialize(), f.name());
+#if 0
+	testArray(f);
+
+    h.root();
+    g.root();
+
+    if (use_gpu()) {
+        f.cudaTile(x, y, 16, 16);
+        g.cudaTile(x, 128);
+        h.cudaTile(x, 128);
+    }
+#endif
+    
+    Image<int> out = ff.realize(32, 32);
+
+    for (int y = 0; y < 32; y++) {
+        for (int x = 0; x < 32; x++) {
+            if (out(x, y) != x*4 + y) {
+                printf("out(%d, %d) = %d instead of %d\n", x, y, out(x, y), x*4+y);
+                return -1;
+            }
+        }
+    }
+
+    printf("Success!\n");
+    return 0;
+}
