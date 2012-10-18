b11549f579f562b69c9f12faf5d9a599273c96e1
Connelly Barnes <csbarnes@bigboy.csail.mit.edu>
2012-10-18 19:05:25 -0400

    py bindings -- only include environ_fix on Mac
    Former-commit-id: 5e8dd2df3d1251c3824dcda1b82e8fdd5e6959ea

--- py_bindings/setup.py
+++ py_bindings/setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
 #from Cython.Distutils import build_ext
-import os, os.path
+import os, os.path, sys
 
 import subprocess
 
 png_cflags  = subprocess.check_output('libpng-config --cflags',  shell=True).strip()
 png_ldflags = subprocess.check_output('libpng-config --ldflags', shell=True).strip()
 
-ext_modules = [Extension("_cHalide", ["cHalide_wrap.cxx", 'py_util.cpp', 'environ_fix.cpp'],
+ext_modules = [Extension("_cHalide", ["cHalide_wrap.cxx", 'py_util.cpp'] + (['environ_fix.cpp'] if sys.platform == 'darwin' else []),
                          include_dirs=['../cpp_bindings', '/opt/local/include/libpng14'],
                          extra_compile_args=('-ffast-math -O3 -msse -Wl,-dead_strip -fno-common' + ' ' + png_cflags).split(),
                          #libraries=['Halide.a'],
