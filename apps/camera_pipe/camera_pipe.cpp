d6374980edc8be1cf69c922c77ff4bb39baf71a9
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2012-10-18 16:46:42 -0400

    Testing serialization of camera_pipe
    This required fixing the hack used to make processed iterate over a bounded
    color loop, since deserialization can't cope with RHS vars which aren't in the
    LHS args.

--- apps/camera_pipe/camera_pipe.cpp
+++ apps/camera_pipe/camera_pipe.cpp
@@ -220,7 +220,7 @@ Func color_correct(Func input, UniformImage matrix_3200, UniformImage matrix_700
 
 Func apply_curve(Func input, Type result_type, Uniform<float> gamma, Uniform<float> contrast) {
     // copied from FCam
-    Func curve;
+    Func curve("curved");
 
     Expr xf = clamp(cast<float>(x)/1024.0f, 0.0f, 1.0f);
     Expr g = pow(xf, 1.0f/gamma);
@@ -244,7 +244,7 @@ Func process(Func raw, Type result_type,
              UniformImage matrix_3200, UniformImage matrix_7000, Uniform<float> color_temp, 
              Uniform<float> gamma, Uniform<float> contrast) {
 
-    Func processed;
+    Func processed("processed");
     Var xi, yi;
 
     Func denoised = hot_pixel_suppression(raw);
@@ -255,8 +255,8 @@ Func process(Func raw, Type result_type,
 
     // Schedule
     Var co, ci; 
-    processed(tx, ty, c) = curved(tx, ty, ci);
-    processed.split(c, co, ci, 3); // bound color loop to 0-3
+    processed(tx, ty, c) = curved(tx, ty, c);
+    //processed.split(c, co, ci, 3); // bound color loop to 0-3
     if (schedule == 0) {
         // Compute in chunks over tiles, vectorized by 8
         denoised.chunk(tx).vectorize(x, 8);
@@ -307,6 +307,9 @@ int main(int argc, char **argv) {
     
     // Build the pipeline
     Func processed = process(shifted, result_type, matrix_3200, matrix_7000, color_temp, gamma, contrast);
+
+    string s = processed.serialize();
+    printf("%s\n", s.c_str());
 
     // In C++-11, this can be done as a simple initializer_list {color_temp,gamma,etc.} in place.
     Func::Arg args[] = {color_temp, gamma, contrast, input, matrix_3200, matrix_7000};
