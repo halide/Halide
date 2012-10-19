cc91eed3a9df9c40716c7c87d46b154f723bf348
Connelly Barnes <cbarnes@mgarbi-MacBookPro.(none)>
2012-10-19 17:16:13 -0400

    py bindings -- commit broken camera_pipe for debugging
    Former-commit-id: 3fc1adbf335f3435d87e10db102424fc936d0909

---
+++ py_bindings/tune_camera_pipe/f00_000_compile2.sh
@@ -0,0 +1,1 @@
+python autotune.py autotune_compile_run_child examples.camera_pipe2.filter_func "corrected.root()\ncurve.root()\ncurved.root()\nmatrix.root()\nmatrix_3200.root()\nmatrix_7200.root()\nprocessed.root()\nraw2.root()\nshifted.root()" /Users/cbarnes/Halide/apps/camera_pipe/raw.png 5 /Users/cbarnes/Halide/py_bindings/tune_camera_pipe/f00_000 1 "" 2560 1920 3
