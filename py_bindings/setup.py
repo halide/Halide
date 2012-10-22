26c09b54cf1eb06c58ee733b355414c91b5be4cd
Connelly Barnes <cbarnes@mgarbi-MacBookPro.(none)>
2012-10-22 19:56:16 -0400

    py bindings -- environ fix for linux
    Former-commit-id: 904bc25ee16cb07ecff4b8301d79d1753871f2d4

--- py_bindings/setup.py
+++ py_bindings/setup.py
@@ -8,7 +8,7 @@ import subprocess
 png_cflags  = subprocess.check_output('libpng-config --cflags',  shell=True).strip()
 png_ldflags = subprocess.check_output('libpng-config --ldflags', shell=True).strip()
 
-ext_modules = [Extension("_cHalide", ["cHalide_wrap.cxx", 'py_util.cpp'] + (['environ_fix.cpp'] if sys.platform == 'darwin' else []),
+ext_modules = [Extension("_cHalide", ["cHalide_wrap.cxx", 'py_util.cpp', 'environ_fix.cpp'],
                          include_dirs=['../cpp_bindings', '/opt/local/include/libpng14'],
                          extra_compile_args=('-ffast-math -O3 -msse -Wl,-dead_strip -fno-common' + ' ' + png_cflags).split(),
                          #libraries=['Halide.a'],
