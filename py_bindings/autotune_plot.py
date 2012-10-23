b109f5fe6508770af132b28ae594c14b509fbc72
Connelly Barnes <csbarnes@bigboy.csail.mit.edu>
2012-10-22 22:36:20 -0400

    py bindings -- headless plot for linux

--- py_bindings/autotune_plot.py
+++ py_bindings/autotune_plot.py
@@ -3,7 +3,7 @@ Plot running time vs generation.
 """
 
 import sys
-import pylab
+import matplotlib
 import os
 
 def main():
@@ -24,7 +24,8 @@ def main():
         val = L[i].split()[0]
         try:
             val = float(val)
-            time_d[gen] = val
+            if not gen in time_d:
+                time_d[gen] = val
         except ValueError:
             if L[i].startswith('Generation'):
                 gen = int(L[i].split()[1])
@@ -34,13 +35,19 @@ def main():
         x.append(xv)
         y.append(yv)
 
+    if out_filename is not None:
+        matplotlib.use('Agg')     # headless
+
+    import pylab
+
     pylab.plot(x, y)
     pylab.title('Best time vs Generation\n(%s)'%tunedir)
     pylab.ylabel('Best time [sec]')
     pylab.xlabel('Generation')
     if out_filename is not None:
         pylab.savefig(out_filename, dpi=300)
-    pylab.show()
+    else:
+        pylab.show()
     
 if __name__ == '__main__':
     main()
