ccb3e7da397e96557bdd3fa353332bc5479121ae
Connelly Barnes <cbarnes@mgarbi-MacBookPro.(none)>
2012-10-26 15:36:57 -0400

    py bindings -- fix minor typo
    Former-commit-id: 5e9fe27de50274eabbcfcbebd2230838822f808f

--- py_bindings/autotune.py
+++ py_bindings/autotune.py
@@ -425,7 +425,7 @@ def apply_grouping(schedule, grouping):
             for indiv in group:
                 ans[indiv] = FragmentList(d_func[indiv], list(schedule.d[group[0]]))
         #return Schedule(schedule.root_func, ans)
-        return Schedule(schedule.root_func, d, schedule.genomelog, schedule.generation, schedule.index, schedule.identity_str)
+        return Schedule(schedule.root_func, ans, schedule.genomelog, schedule.generation, schedule.index, schedule.identity_str)
     return schedule
     
 def next_generation(prevL, p, root_func, constraints, generation_idx, timeL):
