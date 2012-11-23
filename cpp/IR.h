bd4080450741929e95fcc93f9461910b79b031aa
Andrew Adams <abadams@mojo.(none)>
2012-11-23 14:34:11 -0500

    Updates to C++ IR

--- cpp/IR.h
+++ cpp/IR.h
@@ -2,7 +2,10 @@
 #define IR_H
 #include <string>
 #include <vector>
-#include <pair>
+#include <utility>
+#include <assert.h>
+
+#include "IRVisitor.h"
 
 namespace HalideInternal {
 
@@ -17,8 +20,8 @@ namespace HalideInternal {
     };
 
     struct IR {
-        virtual void visit(IRVisitor *v) = 0;
-        int gc_mark, gc_ref_count;
+        virtual void visit(IRVisitor *v) const = 0;
+        int gc_mark;
     };
 
     struct Expr : public IR {
@@ -31,290 +34,402 @@ namespace HalideInternal {
         int value;
         IntImm(float v) : value(v) {}
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct FloatImm : public Expr {
         float value;
         FloatImm(float v) : value(v) {}
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Cast : public Expr {
         Type type;
-        Expr *value;
-        Cast(Type t, Expr *v) : type(t), value(v) {}
+        const Expr *value;
+        Cast(Type t, const Expr *v) : type(t), value(v) {
+            assert(v && "Cast of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Var : public Expr {
         Type type;
-        string name;
-        Var(Type t, string n) : type(t), name(n) {}
+        const string name;
+        Var(Type t, const string n) : type(t), name(n) {}
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Add : public Expr {
-        Expr *a, *b;
-        Add(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Add(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Add of NULL");
+            assert(b && "Add of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Sub : public Expr {
-        Expr *a, *b;
-        Sub(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Sub(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Sub of NULL");
+            assert(b && "Sub of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Mul : public Expr {
-        Expr *a, *b;
-        Mul(Expr *_a, Expr *_b) : a(_a), b(_b) {}        
+        const Expr *a, *b;
+        Mul(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Mul of NULL");
+            assert(b && "Mul of NULL");
+        }        
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Div : public Expr {
-        Expr *a, *b;
-        Div(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Div(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Div of NULL");
+            assert(b && "Div of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Mod : public Expr {
-        Expr *a, *b;
-        Mod(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Mod(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Mod of NULL");
+            assert(b && "Mod of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Min : public Expr {
-        Expr *a, *b;
-        Min(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Min(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Min of NULL");
+            assert(b && "Min of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Max : public Expr {
-        Expr *a, *b;
-        Max(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Max(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Max of NULL");
+            assert(b && "Max of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct EQ : public Expr {
-        Expr *a, *b;
-        EQ(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        EQ(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "EQ of NULL");
+            assert(b && "EQ of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct NE : public Expr {
-        Expr *a, *b;
-        NE(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        NE(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "NE of NULL");
+            assert(b && "NE of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct LT : public Expr {
-        Expr *a, *b;
-        LT(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        LT(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "LT of NULL");
+            assert(b && "LT of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct LE : public Expr {
-        Expr *a, *b;
-        LE(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        LE(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "LE of NULL");
+            assert(b && "LE of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct GT : public Expr {
-        Expr *a, *b;
-        GT(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        GT(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "GT of NULL");
+            assert(b && "GT of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct GE : public Expr {
-        Expr *a, *b;
-        GE(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        GE(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "GE of NULL");
+            assert(b && "GE of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct And : public Expr {
-        Expr *a, *b;
-        And(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        And(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "And of NULL");
+            assert(b && "And of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Or : public Expr {
-        Expr *a, *b;
-        Or(Expr *_a, Expr *_b) : a(_a), b(_b) {}
+        const Expr *a, *b;
+        Or(const Expr *_a, const Expr *_b) : a(_a), b(_b) {
+            assert(a && "Or of NULL");
+            assert(b && "Or of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Not : public Expr {
-        Expr *a;
-        Not(Expr *_a) : a(_a) {}
+        const Expr *a;
+        Not(const Expr *_a) : a(_a) {
+            assert(a && "Not of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Select : public Expr {
-        Expr *condition, *true_value, *false_value;
-        Select(Expr *c, Expr *t, Expr *f) : 
-            condition(c), true_value(t), false_value(f) {}
+        const Expr *condition, *true_value, *false_value;
+        Select(const Expr *c, const Expr *t, const Expr *f) : 
+            condition(c), true_value(t), false_value(f) {
+            assert(condition && "Select of NULL");
+            assert(true_value && "Select of NULL");
+            assert(false_value && "Select of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}        
+        void visit(IRVisitor *v) const {v->visit(this);}        
     };
 
     struct Load : public Expr {
         Type type;
-        string buffer;
-        Expr *index;
-        Load(Type t, string b, Expr *i) : 
-            type(t), buffer(b), index(i) {}
+        const string buffer;
+        const Expr *index;
+        Load(Type t, const string b, const Expr *i) : 
+            type(t), buffer(b), index(i) {
+            assert(index && "Load of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Ramp : public Expr {
-        Expr *base, *stride;
+        const Expr *base, *stride;
         int width;
-        Ramp(Expr *b, Expr *s, int w) : 
-            base(b), stride(s), width(w) {}
+        Ramp(const Expr *b, const Expr *s, int w) : 
+            base(b), stride(s), width(w) {
+            assert(base && "Ramp of NULL");
+            assert(stride && "Ramp of NULL");
+            assert(w > 0 && "Ramp of width <= 0");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Call : public Expr {
         Type type;
-        string buffer;
-        vector<Expr *> args;
+        const string buffer;
+        vector<const Expr *> args;
         typedef enum {Image, Extern, Halide} CallType;
         CallType call_type;
 
-        Call(Type t, string b, const vector<Expr *> &a, CallType ct) : 
-            type(t), buffer(b), args(a), call_type(ct) {}
+        Call(Type t, const string b, const vector<const Expr *> &a, CallType ct) : 
+            type(t), buffer(b), args(a), call_type(ct) {
+            for (size_t i = 0; i < args.size(); i++) {
+                assert(args[i] && "Call of NULL");
+            }
+        }
 
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Let : public Expr {
-        string name;
-        Expr *a, *b;
-        Let(string n, Expr *_a, Expr *_b) : 
-            name(n), a(_a), b(_b) {}
+        const string name;
+        const Expr *value, *body;
+        Let(const string n, const Expr *v, const Expr *b) : 
+            name(n), value(v), body(b) {
+            assert(value && "Let of NULL");
+            assert(body && "Let of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct LetStmt : public Stmt {
-        string name;
-        Expr *value;
-        Stmt *body;
-        LetStmt(string n, Expr *v, Stmt *b) : 
-            name(n), value(v), body(b) {}
+        const string name;
+        const Expr *value;
+        const Stmt *body;
+        LetStmt(const string n, const Expr *v, const Stmt *b) : 
+            name(n), value(v), body(b) {
+            assert(value && "LetStmt of NULL");
+            assert(body && "LetStmt of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct PrintStmt : public Stmt {
-        string prefix;
-        vector<Expr *> args;
-        Stmt *next;
-        PrintStmt(string p, const vector<Expr *> &a, Stmt *n) :
-            prefix(p), args(a), next(n) {}
+        const string prefix;
+        vector<const Expr *> args;
+        PrintStmt(const string p, const vector<const Expr *> &a) :
+            prefix(p), args(a) {
+            for (size_t i = 0; i < args.size(); i++) {
+                assert(args[i] && "PrintStmt of NULL");
+            }
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct AssertStmt : public Stmt {
         // if condition then val else error out with message
-        Expr *condition;
-        string message;
-        Stmt *next;
+        const Expr *condition;
+        const string message;
 
-        AssertStmt(string p, const vector<Expr *> &a, Stmt *n) :
-            prefix(p), args(a), next(n) {}
+        AssertStmt(const Expr *c, const string m) :
+            condition(c), message(m) {
+            assert(condition && "AssertStmt of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Pipeline : public Stmt {
-        Stmt *produce, *update, *consume;
+        const Stmt *produce, *update, *consume;
 
-        Pipeline(Stmt *p, Stmt *u, Stmt *c) : 
-            produce(p), update(u), consume(c);
+        Pipeline(const Stmt *p, const Stmt *u, const Stmt *c) : 
+            produce(p), update(u), consume(c) {
+            assert(produce && "Pipeline of NULL");
+            // update is allowed to be null
+            assert(consume && "Pipeline of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
     
     struct For : public Stmt {
-        string name;
-        Expr *min, *extent;
+        const string name;
+        const Expr *min, *extent;
         typedef enum {Serial, Parallel, Vectorized, Unrolled} ForType;
         ForType for_type;
-        Stmt *body;        
+        const Stmt *body;
 
-        For(string n, Expr *m, Expr *e, ForType f, Stmt *b) :
-            name(n), min(m), extent(e), for_type(f), body(b) {}
+        For(const string n, const Expr *m, const Expr *e, ForType f, const Stmt *b) :
+            name(n), min(m), extent(e), for_type(f), body(b) {
+            assert(min && "For of NULL");
+            assert(extent && "For of NULL");
+            assert(body && "For of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Store : public Stmt {
-        string buffer;
-        Expr *value, *index;
+        const string buffer;
+        const Expr *value, *index;
 
-        Store(string b, Expr *v, Expr *i) :
-            buffer(b), value(v), index(i) {}
+        Store(const string b, const Expr *v, const Expr *i) :
+            buffer(b), value(v), index(i) {
+            assert(value && "Store of NULL");
+            assert(index && "Store of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Provide : public Stmt {
-        string buffer;
-        Expr *value;
-        vector<Expr *> args;
+        const string buffer;
+        const Expr *value;
+        vector<const Expr *> args;
 
-        Provide(string b, Expr *v, const vector<Expr *> &a) : 
-            buffer(b), value(v), args(a) {}
+        Provide(const string b, const Expr *v, const vector<const Expr *> &a) : 
+            buffer(b), value(v), args(a) {
+            assert(value && "Provide of NULL");
+            for (size_t i = 0; i < args.size(); i++) {
+                assert(args[i] && "Provide of NULL");
+            }
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Allocate : public Stmt {
-        string buffer;
+        const string buffer;
         Type type;
-        Expr *size;
-        Stmt *body;
+        const Expr *size;
+        const Stmt *body;
 
-        Allocate(string buf, Type t, Expr *s, Stmt *bod) : 
-            buffer(buf), type(t), size(s), body(bod) {}
+        Allocate(const string buf, Type t, const Expr *s, const Stmt *bod) : 
+            buffer(buf), type(t), size(s), body(bod) {
+            assert(size && "Allocate of NULL");
+            assert(body && "Allocate of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 
     struct Realize : public Stmt {
-        string buffer;
+        const string buffer;
         Type type;
-        vector<pair<Expr *, Expr *> > bounds;
-        Stmt *body;
+        vector<pair<const Expr *, const Expr *> > bounds;
+        const Stmt *body;
 
-        Provide(string buf, Type t, const vector<pair<Expr *, Expr *> > &bou, Stmt *bod) : 
-            buffer(buf), type(t), bounds(bou), body(bod) {}
+        Realize(const string buf, Type t, const vector<pair<const Expr *, const Expr *> > &bou, const Stmt *bod) : 
+            buffer(buf), type(t), bounds(bou), body(bod) {
+            for (size_t i = 0; i < bounds.size(); i++) {
+                assert(bounds[i].first && "Realize of NULL");
+                assert(bounds[i].second && "Realize of NULL");
+            }
+            assert(body && "Realize of NULL");
+        }
 
-        void visit(IRVisitor *v) {v->visit(this);}
+        void visit(IRVisitor *v) const {v->visit(this);}
+    };
+
+    struct Block : public Stmt {
+        const Stmt *first, *rest;
+        
+        Block(const Stmt *f, const Stmt *r) : 
+            first(f), rest(r) {
+            assert(first && "Block of NULL");
+            // rest is allowed to be null
+        }
+        
+        void visit(IRVisitor *v) const {v->visit(this);}
     };
 }
+
+#endif
