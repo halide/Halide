8e7056a648a58597e8a27e208d02f0d0abc178d1
Andrew Adams <abadams@calculon.(none)>
2012-11-26 15:56:29 -0500

    Added code generation for x86 to the tests
    Former-commit-id: 2a688554c51080cb684bf636c55ba3886855989b

--- cpp/test_all.cpp
+++ cpp/test_all.cpp
@@ -1,10 +1,11 @@
 #include "IR.h"
 #include "IRPrinter.h"
+#include "CodeGen_X86.h"
 
 using namespace HalideInternal;
 
 int main(int argc, const char **argv) {
     IRPrinter::test();
-
+    CodeGen_X86::test();
     return 0;
 }
