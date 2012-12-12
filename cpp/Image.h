c2b29115e12300cdc28906c049642d341892e3b9
Andrew Adams <abadams@calculon.(none)>
2012-12-12 18:14:31 -0500

    Starting work on buffer and image front-end constructs
    Former-commit-id: e15b7c92cf856952ee2708382d468937a25a1ed6

---
+++ cpp/Image.h
@@ -0,0 +1,43 @@
+#ifndef HALIDE_IMAGE_H
+#define HALIDE_IMAGE_H
+
+#include "Buffer.h"
+struct buffer_t;
+
+namespace Halide {
+
+template<typename T>
+class Image {
+private:
+    Buffer buf;    
+
+    // These fields are also stored in the buffer, but they're cached here in the handle to make operator() fast
+    T *base;
+    int stride_1, stride_2, stride_3;
+
+public:
+    Image();
+    Image(int);
+    Image(int, int);
+    Image(int, int, int);
+    Image(int, int, int, int);
+    Image(const Buffer &);
+    Image(const buffer_t *);
+
+    T operator()(int) const;
+    T operator()(int, int) const;
+    T operator()(int, int, int) const;
+    T operator()(int, int, int, int) const;
+
+    T &operator()(int);
+    T &operator()(int, int);
+    T &operator()(int, int, int);
+    T &operator()(int, int, int, int);
+    
+    operator const buffer_t *() {return buf.raw_buffer();}
+    operator Buffer() {return buf;}
+};
+
+}
+
+#endif
