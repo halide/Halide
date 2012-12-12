e4fb7fe10f6fcee44177489ec7fc1dce80535d5e
Andrew Adams <abadams@calculon.(none)>
2012-12-12 10:59:42 -0500

    Started on region required analysis

--- cpp/Bounds.h
+++ cpp/Bounds.h
@@ -4,12 +4,14 @@
 #include "IRVisitor.h"
 #include "Scope.h"
 #include <utility>
+#include <vector>
 
 namespace Halide {
 namespace Internal {
     
 using std::pair;
-        
+using std::vector;    
+    
 /* Given an expression in some variables, and a map from those
  * variables to their bounds (in the form of (minimum possible value,
  * maximum possible value)), compute two expressions that give the
@@ -21,6 +23,17 @@ using std::pair;
  * loaded by a chunk of code.
  */
 pair<Expr, Expr> bounds_of_expr_in_scope(Expr expr, const Scope<pair<Expr, Expr> > &scope);    
+
+/* Compute a rectangular domain large enough to cover all the 'Call's
+ * to a function that occur within a given statement. This is useful
+ * for figuring out what regions of things to evaluate. */
+vector<pair<Expr, Expr> > region_required_of_function_in_stmt(string func, Stmt s, const Scope<pair<Expr, Expr> > &scope);
+
+/* Compute a rectangular domain large enough to cover all the
+ * 'Provide's to a function the occur within a given statement. This
+ * is useful for figuring out what region of a function a scattering
+ * reduction (e.g. a histogram) will touch. */
+vector<pair<Expr, Expr> > region_provided_of_function_in_stmt(string func, Stmt s, const Scope<pair<Expr, Expr> > &scope);
 
 void bounds_test();
         
