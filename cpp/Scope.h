ff7c9bc9e9881951f5ebbdb5c598e803dcb04b80
Andrew Adams <abadams@calculon.(none)>
2012-12-12 18:32:30 -0500

    Simplification for variables
    Former-commit-id: c8498325de663f86e1cca3b438fe90c9848c09d3

--- cpp/Scope.h
+++ cpp/Scope.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <map>
 #include <stack>
+#include <utility>
 
 namespace Halide { 
 namespace Internal {
@@ -12,6 +13,8 @@ namespace Internal {
 using std::string;
 using std::map;
 using std::stack;
+using std::pair;
+using std::make_pair;
 
 /* A common pattern when traversing Halide IR is that you need to
  * keep track of stuff when you find a Let or a LetStmt. This
@@ -19,25 +22,36 @@ using std::stack;
 template<typename T>
 class Scope {
 private:
-    map<string, stack<T> > table;
+    int count;
+    map<string, stack<pair<T, int> > > table;
 public:
+    Scope() : count(0) {}
+
     /* Retrive the value referred to by a name */
     T get(const string &name) const {
-        typename map<string, stack<T> >::const_iterator iter = table.find(name);
+        typename map<string, stack<pair<T, int> > >::const_iterator iter = table.find(name);
         assert(iter != table.end() && "Symbol not found");
-        return iter->second.top();
+        return iter->second.top().first;
     }
         
     /* Return a reference to an entry */
     T &ref(const string &name) {
-        typename map<string, stack<T> >::iterator iter = table.find(name);
+        typename map<string, stack<pair<T, int> > >::iterator iter = table.find(name);
         assert(iter != table.end() && "Symbol not found");
-        return iter->second.top();            
+        return iter->second.top().first;
+    }
+
+    /* Get the depth of an entry. The depth of A is less than the
+     * depth of B if A was pushed before B. */
+    int depth(const string &name) {
+        typename map<string, stack<pair<T, int> > >::const_iterator iter = table.find(name);
+        assert(iter != table.end() && "Symbol not found");
+        return iter->second.top().second;
     }
 
     /* Tests if a name is in scope */
     bool contains(const string &name) const {
-        typename map<string, stack<T> >::const_iterator iter = table.find(name);
+        typename map<string, stack<pair<T, int> > >::const_iterator iter = table.find(name);
         return iter != table.end() && !iter->second.empty();
     }
 
@@ -45,7 +59,8 @@ public:
      * values that have this name until we pop this name.
      */
     void push(const string &name, T value) {
-        table[name].push(value);
+        table[name].push(make_pair(value, count));
+        count++;
     }
 
     /* A name goes out of scope. Restore whatever its old value
@@ -54,6 +69,7 @@ public:
     void pop(const string &name) {
         assert(!table[name].empty() && "Name not in symbol table");
         table[name].pop();
+        count--;
     }
 };
     
