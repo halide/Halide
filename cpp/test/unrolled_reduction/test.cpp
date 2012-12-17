4a8f66a44269710a94a942405fa9872cb2c95a22
Andrew Adams <abadams@calculon.(none)>
2012-12-17 12:56:02 -0500

    Added initial versions of tests

---
+++ cpp/test/unrolled_reduction/test.cpp
@@ -0,0 +1,29 @@
+#include "Halide.h"
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Var x("x"), y("y"), z("z");
+
+    Image<float> noise(32);
+    for (int i = 0; i < 32; i++) {
+        noise(i) = (float)rand() / RAND_MAX;
+    }
+
+    Func f("f");
+    Func g("g");
+    RDom r(0, 32);
+
+    g(x, y) = 0.0f;
+    g(r.x, y) += noise(r.x);
+
+    f(x, y, z) = g(x, y) + g(x+1, y);
+
+    g.chunk(y);
+    f.unroll(z, 2);
+
+    Image<float> im = f.realize(64, 64, 4);
+
+    printf("Success!\n");
+    return 0;
+}
