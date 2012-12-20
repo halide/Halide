25dd36e9db0cb356e723cfc3a8765e794800bf1f
Andrew Adams <abadams@calculon.(none)>
2012-12-20 11:47:30 -0500

    Added casting rules to IROperator
    Former-commit-id: 00efbdd438eece41c63b2283decf313320fe55c8

--- cpp/src/IROperator.h
+++ cpp/src/IROperator.h
@@ -5,67 +5,101 @@
 
 namespace Halide {
 
+namespace Internal {
+bool is_const(Expr e);
+bool is_positive_const(Expr e);
+bool is_negative_const(Expr e);
+bool is_zero(Expr e);
+bool is_one(Expr e);
+Expr make_zero(Type t);
+Expr make_one(Type t);
+Expr const_true(int width = 1);
+Expr const_false(int width = 1);
+void match_types(Expr &a, Expr &b);
+}
+
+template<typename T>
+inline Expr cast(Expr a) {
+    return cast(type_of<T>(), a);
+}
+
+inline Expr cast(Type t, Expr a) {
+    if (a.type() == t) return a;
+    return new Cast(t, a);
+}
+
 inline Expr operator+(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Add(a, b);
 }
     
 inline Expr &operator+=(Expr &a, Expr b) {
-    a = new Add(a, b);
+    a = new Add(a, cast(a.type(), b));
     return a;
 }
 
 inline Expr operator-(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Sub(a, b);
 }
     
 inline Expr &operator-=(Expr &a, Expr b) {
-    a = new Sub(a, b);
+    a = new Sub(a, cast(a.type(), b));
     return a;
 }
 
 inline Expr operator*(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Mul(a, b);
 }
     
 inline Expr &operator*=(Expr &a, Expr b) {
-    a = new Mul(a, b);
+    a = new Mul(a, cast(a.type(), b));
     return a;
 }
 
 inline Expr operator/(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Div(a, b);
 }
 
 inline Expr &operator/=(Expr &a, Expr b) {
-    a = new Div(a, b);
+    a = new Div(a, cast(a.type(), b));
     return a;
 }
 
 inline Expr operator%(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Mod(a, b);
 }
 
 inline Expr operator>(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new GT(a, b);
 }
 
 inline Expr operator<(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new LT(a, b);
 }
 
 inline Expr operator<=(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new LE(a, b);
 }
 
 inline Expr operator>=(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new GE(a, b);
 }
 
 inline Expr operator==(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new EQ(a, b);
 }
 
 inline Expr operator!=(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new NE(a, b);
 }
 
@@ -82,25 +116,19 @@ inline Expr operator!(Expr a) {
 }
 
 inline Expr max(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Max(a, b);
 }
 
 inline Expr min(Expr a, Expr b) {
+    Internal::match_types(a, b);
     return new Min(a, b);
 }
 
 inline Expr clamp(Expr a, Expr min_val, Expr max_val) {
+    min_val = cast(a.type(), min_val);
+    max_val = cast(a.type(), max_val);
     return new Max(new Min(a, max_val), min_val);
-}
-
-template<typename T>
-inline Expr cast(Expr a) {
-    return new Cast(type_of<T>(), a);
-}
-
-inline Expr cast(Type t, Expr a) {
-    if (a.type() == t) return a;
-    return new Cast(t, a);
 }
 
 inline Expr select(Expr a, Expr b, Expr c) {
@@ -180,18 +208,6 @@ inline Expr pow(Expr x, Expr y) {
         y = cast<float>(y);
         return new Call(Float(32), "pow_f32", vec(x, y));
     }
-}
-
-namespace Internal {
-bool is_const(Expr e);
-bool is_positive_const(Expr e);
-bool is_negative_const(Expr e);
-bool is_zero(Expr e);
-bool is_one(Expr e);
-Expr make_zero(Type t);
-Expr make_one(Type t);
-Expr const_true(int width = 1);
-Expr const_false(int width = 1);
 }
 
 }
