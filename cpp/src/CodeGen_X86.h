ec1683c746fe19946d8b23f322860e71556f04e3
Andrew Adams <abadams@calculon.(none)>
2013-01-01 17:39:58 -0500

    Implemented more sse intrinsics

--- cpp/src/CodeGen_X86.h
+++ cpp/src/CodeGen_X86.h
@@ -11,7 +11,7 @@ namespace Internal {
 class CodeGen_X86 : public CodeGen {
 public:
 
-    CodeGen_X86();
+    CodeGen_X86(bool use_sse_41 = true);
         
     /* Compile to an llvm module. Takes a halide statement, the
      * name of the function produced, and the arguments to the
@@ -24,15 +24,24 @@ public:
     static void test();
 
 protected:
+    bool use_sse_41;
+
     // Some useful types
     llvm::Type *i32x4, *i32x8;
+    
+    // Some variables used for matching
+    Expr i8_1, i8_2, i16_1, i16_2, i32_1, i32_2;
+    Expr u8_1, u8_2, u16_1, u16_2, u32_1, u32_2;
+    Expr min_i8, max_i8, min_i16, max_i16;
+    Expr min_u8, max_u8u, max_u8i, min_u16, max_u16u, max_u16i;
 
     llvm::Value *call_intrin(Type t, const string &name, Expr arg1, Expr arg2 = Expr());    
 
     // Nodes that we handle specially
     void visit(const Cast *);
     void visit(const Allocate *);        
-
+    void visit(const Min *);
+    void visit(const Max *);
 
 };
 
