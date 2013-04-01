447797d389ed0cd2b41ce80cc8365553e9d2923e
Zalman Stern <zalman@zalman-macbookpro.local>
2013-03-31 21:45:46 -0700

    Add error checking for passing too many arguments to a Halide function. Caused a segmentation violation previously.
    Add support to the test infrastructure for error message tests. Allows
    writing tests that should result in a compile time failure and
    verifying one does occur as opposed to the code being accepted or
    crashing.

---
+++ cpp/test/error/reduction_type_mismatch.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <Halide.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Var x;
+    Func f;
+    RDom dom(0, 50);
+
+    f(x) = cast<uint8_t>(0); // The type here...
+    f(dom) += 1.0f;          // does not match the type here.
+
+    // Should result in an error
+    Image<float> result = f.realize(50);
+
+    printf("Success!\n");
+    return 0;
+}
