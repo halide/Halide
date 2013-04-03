1c44e0f3fed4ac58289f32c457a2aba991e28767
Andrew Adams <andrew.b.adams@gmail.com>
2013-04-02 18:37:11 -0700

    Added an exhaustive test for constant integer division tricks
    Former-commit-id: 46a89fd5b91be25c4a51f2236d50cb471fa8711b

---
+++ cpp/test/const_division.cpp
@@ -0,0 +1,98 @@
+#include <Halide.h>
+#include <stdio.h>
+#include <stdint.h>
+
+using namespace Halide;
+
+template<typename T>
+bool test(int w) {
+    Func f, g;
+    Var x, y;
+
+    size_t bits = sizeof(T)*8;
+    bool is_signed = (T)(-1) < (T)(0);
+
+    printf("Testing %sint%lu_t x %d\n", 
+           is_signed ? "" : "u", 
+           bits, w);
+
+    int min_val = 2, num_vals = 254;    
+
+    if (bits <= 8 && is_signed) {        
+        // There are two types of integer division that cause runtime crashes:
+        // 1) Division by zero
+        // 2) Division of the smallest negative number by -1 (because
+        // the result overflows)
+        // In either case, let's avoid overflows to dodge such errors.
+        num_vals = 126;
+    }
+
+    Image<T> input(w, num_vals);
+
+    for (int y = 0; y < num_vals; y++) {
+        for (int x = 0; x < input.width(); x++) {
+            uint32_t bits = rand() ^ (rand() << 16);
+            input(x, y) = (T)bits;
+        }
+    }
+
+    f(x, y) = input(x, y)/cast<T>(y + min_val);
+
+    // Reference good version
+    g(x, y) = input(x, y)/cast<T>(y + min_val);
+    
+    // Try dividing by all the known constants using vectors
+    f.bound(y, 0, num_vals).bound(x, 0, input.width()).unroll(y);
+    if (w > 1) f.vectorize(x);
+
+    f.compile_jit();
+    g.compile_jit();
+
+    Image<T> correct = g.realize(input.width(), num_vals);
+    Image<T> fast = f.realize(input.width(), num_vals);
+
+    for (int y = 0; y < num_vals; y++) {
+        for (int x = 0; x < input.width(); x++) {
+            if (fast(x, y) != correct(x, y)) {
+                printf("fast(%d, %d) = %ld instead of %ld (%ld/%d)\n", 
+                       x, y,
+                       (int64_t)fast(x, y), 
+                       (int64_t)correct(x, y), 
+                       (int64_t)input(x, y),
+                       (T)(y + min_val));
+                return false;
+            }
+        }
+    }
+
+    return true;
+    
+}
+
+int main(int argc, char **argv) {
+
+    srand(time(NULL));
+
+    bool success = true;
+    // Scalar
+    success = success && test<int32_t>(1);
+    success = success && test<int16_t>(1);
+    success = success && test<int8_t>(1);
+    success = success && test<uint32_t>(1);
+    success = success && test<uint16_t>(1);
+    success = success && test<uint8_t>(1);
+    // Vector
+    success = success && test<int32_t>(4);
+    success = success && test<int16_t>(8);
+    success = success && test<int8_t>(16);
+    success = success && test<uint32_t>(4);
+    success = success && test<uint16_t>(8);
+    success = success && test<uint8_t>(16);
+
+    if (success) {
+        printf("Success!\n");
+        return 0;
+    } else {
+        return -1;
+    }
+}
