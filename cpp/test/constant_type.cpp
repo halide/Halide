e9277a371948afd73cc02b2a88872dcbb3ec9704
Andrew Adams <andrew.b.adams@gmail.com>
2013-05-03 08:45:29 -0700

    Added a test to ensure operators on constants don't upgrade types unexpectedly
    Former-commit-id: ceff4d0e19ae850054adbfb2b10e261e384acd62

---
+++ cpp/test/constant_type.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <Halide.h>
+
+using namespace Halide;
+
+template<typename T>
+bool test_type() {
+    Type t = type_of<T>();
+    Func f;
+    Var x;
+    f(x) = cast<T>(1);
+    Image<T> im = f.realize(10);
+
+    if (f.value().type() != t) {
+        std::cout << "Function was defined with type " << t << " but has type " << f.value().type() << "\n";
+        return false;
+    }
+
+    Expr add_one = im() + 1;
+    if (add_one.type() != t) {
+        std::cout << "Add 1 changed type from " << t << " to " << add_one.type() << "\n";
+        return false;
+    }
+
+    Expr one_add = 1 + im();
+    if (one_add.type() != t) {
+        std::cout << "Pre-add 1 changed type from " << t << " to " << one_add.type() << "\n";
+        return false;
+    }
+
+    /*
+      The following will indeed change the type, because we don't do early constant folding
+    Expr add_exp = im() + (Expr(1) + 1);
+    if (add_exp.type() != t) {
+        std::cout << "Add constant expression changed type from " << t << " to " << add_exp.type() << "\n";
+    }
+    */
+
+    return true;
+}
+
+int main(int argc, char **argv) {
+    bool ok = true;
+
+    ok = ok && test_type<uint8_t>();
+    ok = ok && test_type<uint16_t>();
+    ok = ok && test_type<uint32_t>();
+    ok = ok && test_type<int8_t>();
+    ok = ok && test_type<int16_t>();
+    ok = ok && test_type<int32_t>();
+    ok = ok && test_type<float>();
+    ok = ok && test_type<double>();
+    
+    if (ok) {
+        printf("Success!\n");
+        return 0;
+    } else {
+        return -1; 
+    }
+}
