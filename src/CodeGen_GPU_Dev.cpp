902b4ed6647053f02079cb255f95990eb3716c6d
Z Stern <zalman@google.com>
2013-06-03 20:59:49 -0700

    Add virtual destructor to class to prevent warning/error for class with virtual methods and no virtual destructor.

--- src/CodeGen_GPU_Dev.cpp
+++ src/CodeGen_GPU_Dev.cpp
@@ -3,6 +3,9 @@
 namespace Halide {
 namespace Internal {
 
+CodeGen_GPU_Dev::~CodeGen_GPU_Dev() {
+}
+
 bool CodeGen_GPU_Dev::is_gpu_var(const std::string &name) {
     std::string n = base_name(name);
 
