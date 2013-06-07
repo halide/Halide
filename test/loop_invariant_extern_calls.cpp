8fb54ff00074051a71ed18b320adb7f3541d66f2
Andrew Adams <andrew.b.adams@gmail.com>
2013-06-07 16:27:37 -0700

    Mark extern calls as side-effect free.
    This lets llvm hoist them out.
    
    Also had to change the sliding window test, because this let llvm be
    clever and call the call_counter fewer times.
    
    
    Former-commit-id: e51294f1ca3aef578c47461b15a2915ceb7b517f

---
+++ test/loop_invariant_extern_calls.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <Halide.h>
+
+using namespace Halide;
+
+// NB: You must compile with -rdynamic for llvm to be able to find the appropriate symbols
+
+int call_counter[4] = {0, 0, 0, 0};
+extern "C" int my_func(int counter, int x) {
+    call_counter[counter]++;
+    return x;
+}
+HalideExtern_2(int, my_func, int, int);
+
+int main(int argc, char **argv) {
+    Var x, y;
+    Func f;
+
+    f(x, y) = my_func(0, Expr(0)) + my_func(1, y) + my_func(2, x);
+
+    // llvm rightly refuses to lift loop invariants out of loops that
+    // might have an extent of zero. It's possible wasted work.
+    f.bound(x, 0, 32).bound(y, 0, 32);
+    
+    Image<int> im = f.realize(32, 32);
+
+    // Check the result was what we expected
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 32; j++) {
+            int correct = i + j; 
+            if (im(i, j) != correct) {
+                printf("im[%d, %d] = %d instead of %d\n", i, j, im(i, j), correct);
+                return -1;
+            }
+        }
+    }
+
+    // Check the call counters
+    if (call_counter[0] != 1 || call_counter[1] != 32 || call_counter[2] != 32*32) {
+        printf("Call counters were %d %d %d instead of %d %d %d\n", 
+               call_counter[0], call_counter[1], call_counter[2], 
+               1, 32, 32*32);
+        return -1;
+    }
+
+    // Note that things don't get lifted out of parallel loops - Each
+    // thread will independently call your extern function.
+    Func g;
+    g(x, y) = my_func(3, Expr(0));
+    g.parallel(y);
+    g.realize(32, 32);
+
+    if (call_counter[3] != 32) {
+        printf("Call counter for parallel call was %d instead of %d\n", 
+               call_counter[3], 32);
+        return -1;
+    }
+
+    printf("Success!\n");
+    return 0;
+}
