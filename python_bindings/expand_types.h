27bbe48977ea46f5669dbf3d7bed7bd414f11d5c
Connelly Barnes <csbarnes@cbarnes-mbp.local>
2013-06-17 18:00:15 -0400

    add python bindings (still have swig errors)

---
+++ python_bindings/expand_types.h
@@ -0,0 +1,11 @@
+/* Include file that calls DEFINE_TYPE(T) on all numeric types T. */
+
+DEFINE_TYPE(uint8_t)
+DEFINE_TYPE(uint16_t)
+DEFINE_TYPE(uint32_t)
+DEFINE_TYPE(int8_t)
+DEFINE_TYPE(int16_t)
+DEFINE_TYPE(int32_t)
+DEFINE_TYPE(float)
+DEFINE_TYPE(double)
+
