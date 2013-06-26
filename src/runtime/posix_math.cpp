4096f5ba9876b00736fc882985a3ceb6a355078c
Yongyi Chen <yongyi781@hotmail.com>
2013-06-26 11:22:36 -0400

    Portability
    used std::numeric_limits instead of constants (which aren't the same on
    all operating systems).

--- src/runtime/posix_math.cpp
+++ src/runtime/posix_math.cpp
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <math.h>
 #include <float.h>
+#include <limits>
 
 #define INLINE inline __attribute__((used)) __attribute__((always_inline)) __attribute__((nothrow))
 
@@ -92,8 +93,8 @@ INLINE int64_t abs_i64(int64_t a) {return a >= 0 ? a : -a;}
 INLINE float abs_f32(float a) {return fabsf(a);}
 INLINE double abs_f64(double a) {return fabs(a);}
 
-INLINE float nan_f32() {return NAN;}
-INLINE float neg_inf_f32() {return -INFINITY;}
-INLINE float inf_f32() {return INFINITY;}
+INLINE float nan_f32() {return std::numeric_limits<float>::signaling_NaN();}
+INLINE float neg_inf_f32() {return -std::numeric_limits<float>::infinity();}
+INLINE float inf_f32() {return std::numeric_limits<float>::infinity();}
 
 }
