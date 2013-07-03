02b78e2cbf130af497adab22138491dcd0c8ca52
Andrew Adams <andrew.b.adams@gmail.com>
2013-07-03 16:02:58 -0700

    Updates to HelloAndroid to make it work again.

--- apps/HelloAndroid/jni/Android.mk
+++ apps/HelloAndroid/jni/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native
+LOCAL_ARM_MODE  := arm
 LOCAL_SRC_FILES := native.cpp halide.s
 LOCAL_LDLIBS    := -lm -llog -landroid
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
