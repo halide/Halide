2d868d5270dd362baf282c6cc1ac68a2fcd7a56e
Andrew Adams <andrew.b.adams@gmail.com>
2013-07-15 10:43:43 -0700

    Added better error messages for bad vectorization widths
    Former-commit-id: b957c29db9462c37e3ccb974f22636a2dacb22cf

---
+++ test/error/vectorize_dynamic.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <Halide.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Var x, y;
+
+    Image<int> input(5, 5);
+    Func f;
+    f(x, y) = input(x, y) * 2; 
+    Var xo, xi;
+
+    Param<int> vector_size;
+
+    // You can only vectorize across compile-time-constant sizes.
+    f.split(x, xo, xi, vector_size).vectorize(xi);
+
+    // Should result in an error
+    vector_size.set(4);
+    Image<int> out = f.realize(5, 5);
+
+    printf("Success!\n");
+    return 0;
+}
