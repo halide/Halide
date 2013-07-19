48af6993a821b91e0ed36b2b6a3aaf9d02bd260f
Andrew Adams <andrew.b.adams@gmail.com>
2013-07-19 09:14:18 -0700

    Fixed a bug in bounds inference.
    We were lifting out Call exprs that were loads from Funcs. You can't move
    these around, as their value might change (especially if they're a reduction).
    
    Added an error that previously did not flag as an error, and a test that uses
    this to do an argmax.
    
    
    Former-commit-id: 5141161397bc2d2623dca1ea8070677df1d36abf

---
+++ test/error/reduction_bounds.cpp
@@ -0,0 +1,23 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Func f("f"), g("g");
+    Var x("x");
+    RDom r(0, 100, "r");
+
+    f(x) = x;
+
+    g(x) = 0;
+    g(x) = f(g(x-1)) + r;
+
+    f.compute_at(g, r.x);
+
+    // Use of f is unbounded in g.
+
+    g.realize(100);
+
+    return 0;
+}
