321a40a12df3a98c099cd755fe933144875c5867
Z Stern <zalman@google.com>
2013-08-01 17:36:19 -0700

    Add built-in lerp operator to Halide. Documentation in IROperator.h doc comment.
    Includes correctness tests and tests for compilation error messages.
    
    Also made two changes to allow writing a test cases that takes and
    returns bool values. (Which are packed in an 8-bit byte.)
    
    
    Former-commit-id: 6d28c7ff3953d9d4c18e20744bae663697b00f49

---
+++ test/error/lerp_float_weight_out_of_range.cpp
@@ -0,0 +1,14 @@
+#include <Halide.h>
+
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    // This should trigger an error.
+    Func f;
+    f() = lerp(0, 42, 1.5f);
+
+    printf("Success!\n");
+    return 0;
+}
