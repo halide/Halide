424d307ce4c7b8b9124511e761a369f597973c40
Andrew Adams <andrew.b.adams@gmail.com>
2013-08-12 11:20:35 -0700

    Added test for realizations into named buffers over shifted domains.
    Former-commit-id: f3d7a986762aff4493678eedd20c6cd77f849714

---
+++ test/realize_over_shifted_domain.cpp
@@ -0,0 +1,36 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Image<int> input(100, 50, "input");
+
+    // This image represents the range [100, 199]*[50, 99]
+    input.set_min(100, 50);
+
+    input(100, 50) = 123;
+    input(198, 99) = 234;
+
+    Func f;
+    Var x, y;
+    f(x, y) = input(2*x, y/2);
+
+    f.compile_jit();
+
+    // The output will represent the range from [50, 99]*[100, 199]
+    Image<int> result(50, 100);
+    result.set_min(50, 100);
+
+    f.realize(result);
+
+    if (result(50, 100) != 123 || result(99, 199) != 234) {
+        printf("Error: f(50, 100) = %d (supposed to be 123)\n"
+               "f(99, 199) = %d (supposed to be 234)\n",
+               result(50, 100), result(99, 199));
+    }
+
+    printf("Success!\n");
+
+    return 0;
+}
