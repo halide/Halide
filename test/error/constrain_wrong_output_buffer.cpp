2a2ba2067a1b90f56ee2c285facbc6d6faf94803
Andrew Adams <andrew.b.adams@gmail.com>
2013-08-13 14:41:19 -0700

    Added some unit tests for error conditions when realizing tuple funcs
    Former-commit-id: 6423628011b432847c5ea230d744aba13eb11c11

---
+++ test/error/constrain_wrong_output_buffer.cpp
@@ -0,0 +1,17 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Func f;
+    Var x;
+    f(x) = Tuple(x, sin(x));
+
+    // Don't do this. Instead constrain the size of output buffer 0.
+    f.output_buffers()[1].set_min(0, 4);
+
+    f.compile_jit();
+
+    return 0;
+}
---
+++ test/multi_output_pipeline_with_bad_sizes.cpp
@@ -0,0 +1,32 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+bool error_occurred;
+void halide_error(char *msg) {
+    printf("%s\n", msg);
+    error_occurred = true;
+}
+
+int main(int argc, char **argv) {
+    Func f;
+    Var x;
+    f(x) = Tuple(x, sin(x));
+
+    // These should be the same size
+    Image<int> x_out(100);
+    Image<float> sin_x_out(101);
+
+    f.set_error_handler(&halide_error);
+    error_occurred = false;
+
+    f.realize(Realization(x_out, sin_x_out));
+
+    if (!error_occurred) {
+        printf("There should have been an error\n");
+        return -1;
+    }
+
+    return 0;
+}
