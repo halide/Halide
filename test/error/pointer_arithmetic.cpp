51e2c322cc93cda922e7ea9ec3d85d61178990ff
Andrew Adams <andrew.b.adams@gmail.com>
2013-08-15 11:29:22 -0700

    Added test to make sure we can't do pointer arithmetic.

---
+++ test/error/pointer_arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Param<const char *> p;
+    p.set("Hello, world!\n");
+
+    Func f;
+    Var x;
+    // Should error out during match_types
+    f(x) = p + 2;
+
+    return 0;
+}
