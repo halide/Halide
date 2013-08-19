5eeb2ebf051e508068f03fff84ac167c91c3ad45
Andrew Adams <andrew.b.adams@gmail.com>
2013-08-19 10:55:31 -0700

    Made local laplacian use get_target instead of sniffing HL_TARGET
    Former-commit-id: 832f60f982ddbee7f30a539221e269e9797527c7

--- apps/local_laplacian/local_laplacian.cpp
+++ apps/local_laplacian/local_laplacian.cpp
@@ -6,8 +6,8 @@ Var x, y;
 // Downsample with a 1 3 3 1 filter
 Func downsample(Func f) {
     Func downx, downy;
-    
-    downx(x, y) = (f(2*x-1, y) + 3.0f * (f(2*x, y) + f(2*x+1, y)) + f(2*x+2, y)) / 8.0f;    
+
+    downx(x, y) = (f(2*x-1, y) + 3.0f * (f(2*x, y) + f(2*x+1, y)) + f(2*x+2, y)) / 8.0f;
     downy(x, y) = (downx(x, 2*y-1) + 3.0f * (downx(x, 2*y) + downx(x, 2*y+1)) + downx(x, 2*y+2)) / 8.0f;
 
     return downy;
@@ -16,19 +16,19 @@ Func downsample(Func f) {
 // Upsample using bilinear interpolation
 Func upsample(Func f) {
     Func upx, upy;
-    
+
     upx(x, y) = 0.25f * f((x/2) - 1 + 2*(x % 2), y) + 0.75f * f(x/2, y);
     upy(x, y) = 0.25f * upx(x, (y/2) - 1 + 2*(y % 2)) + 0.75f * upx(x, y/2);
 
     return upy;
-    
+
 }
 
 int main(int argc, char **argv) {
 
     /* THE ALGORITHM */
 
-    // Number of pyramid levels 
+    // Number of pyramid levels
     const int J = 8;
 
     // number of intensity levels
@@ -45,20 +45,20 @@ int main(int argc, char **argv) {
     Func remap;
     Expr fx = cast<float>(x) / 256.0f;
     remap(x) = alpha*fx*exp(-fx*fx/2.0f);
-    
+
     // Convert to floating point
     Func floating;
     floating(x, y, c) = cast<float>(input(x, y, c)) / 65535.0f;
-    
+
     // Set a boundary condition
     Func clamped;
     clamped(x, y, c) = floating(clamp(x, 0, input.width()-1), clamp(y, 0, input.height()-1), c);
-    
+
     // Get the luminance channel
     Func gray;
     gray(x, y) = 0.299f * clamped(x, y, 0) + 0.587f * clamped(x, y, 1) + 0.114f * clamped(x, y, 2);
 
-    // Make the processed Gaussian pyramid. 
+    // Make the processed Gaussian pyramid.
     Func gPyramid[J];
     // Do a lookup into a lut with 256 entires per intensity level
     Expr idx = gray(x, y)*cast<float>(levels-1)*256.0f;
@@ -66,7 +66,7 @@ int main(int argc, char **argv) {
     gPyramid[0](x, y, k) = beta*gray(x, y) + remap(idx - 256*k);
     for (int j = 1; j < J; j++) {
         gPyramid[j](x, y, k) = downsample(gPyramid[j-1])(x, y, k);
-    }    
+    }
 
     // Get its laplacian pyramid
     Func lPyramid[J];
@@ -80,7 +80,7 @@ int main(int argc, char **argv) {
     inGPyramid[0](x, y) = gray(x, y);
     for (int j = 1; j < J; j++) {
         inGPyramid[j](x, y) = downsample(inGPyramid[j-1])(x, y);
-    }        
+    }
 
     // Make the laplacian pyramid of the output
     Func outLPyramid[J];
@@ -92,19 +92,19 @@ int main(int argc, char **argv) {
         // Linearly interpolate between the nearest processed pyramid levels
         outLPyramid[j](x, y) = (1.0f - lf) * lPyramid[j](x, y, li) + lf * lPyramid[j](x, y, li+1);
     }
-    
+
     // Make the Gaussian pyramid of the output
     Func outGPyramid[J];
     outGPyramid[J-1](x, y) = outLPyramid[J-1](x, y);
     for (int j = J-2; j >= 0; j--) {
         outGPyramid[j](x, y) = upsample(outGPyramid[j+1])(x, y) + outLPyramid[j](x, y);
-    }    
+    }
 
     // Reintroduce color (Connelly: use eps to avoid scaling up noise w/ apollo3.png input)
     Func color;
     float eps = 0.01f;
     color(x, y, c) = outGPyramid[0](x, y) * (clamped(x, y, c)+eps) / (gray(x, y)+eps);
-        
+
     Func output("local_laplacian");
     // Convert back to 16-bit
     output(x, y, c) = cast<uint16_t>(clamp(color(x, y, c), 0.0f, 1.0f) * 65535.0f);
@@ -114,8 +114,7 @@ int main(int argc, char **argv) {
     /* THE SCHEDULE */
     remap.compute_root();
 
-    char *target = getenv("HL_TARGET");
-    if (target && std::string(target) == "ptx") {
+    if (get_target() == "ptx") {
         // gpu schedule
         output.compute_root().cuda_tile(x, y, 32, 32);
         for (int j = 0; j < J; j++) {
