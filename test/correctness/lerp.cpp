c10edf474ded810f690c8b8f43cf341269b255e8
Zalman Stern <zalman@zalman-macbookpro.local>
2013-08-19 00:07:07 -0700

    Add support for lerp on float values with an integer weight. Add test case.

--- test/correctness/lerp.cpp
+++ test/correctness/lerp.cpp
@@ -126,86 +126,99 @@ void check_range(int32_t zero_min, int32_t zero_extent, value_t zero_offset, val
 }
 
 int main(int argc, char **argv) {
-  // Test bool
-  check_range<bool, uint8_t>(0, 2, 0, 1,
-                             0, 2, 0, 1,
-                             0, 256, 0, 1,
-                             "<bool, uint8_t> exhaustive");
-
-  // Exhaustively test 8-bit cases
-  check_range<uint8_t, uint8_t>(0, 256, 0, 1,
-                                0, 256, 0, 1,
-                                0, 256, 0, 1,
-                                "<uint8_t, uint8_t> exhaustive");
-  check_range<int8_t, uint8_t>(0, 256, -128, 1,
-                               0, 256, -128, 1,
+    // Test bool
+    check_range<bool, uint8_t>(0, 2, 0, 1,
+                               0, 2, 0, 1,
                                0, 256, 0, 1,
-                               "<int8_t, uint8_t> exhaustive");
-  check_range<uint8_t, float>(0, 256, 0, 1,
-                              0, 256, 0, 1,
-                              0, 256, 0, 1/255.0f,
-                              "<uint8_t, float> exhaustive");
-  check_range<int8_t, float>(0, 256, -128, 1,
-                             0, 256, -128, 1,
-                             0, 256, 0, 1/255.0f,
-                             "<int8_t, float> exhaustive");
+                               "<bool, uint8_t> exhaustive");
 
-  // Check all delta values for 16-bit, verify swapping arguments doesn't break
-  check_range<uint16_t, uint16_t>(0, 65536, 0, 1,
-                                  65535, 1, 0, 1,
-                                  0, 257, 255, 1,
-                                  "<uint16_t, uint16_t> all zero starts");
-  check_range<uint16_t, uint16_t>(65535, 1, 0, 1,
-                                  0, 65536, 0, 1,
-                                  0, 257, 255, 1,
-                                  "<uint16_t, uint16_t> all one starts");
+    // Exhaustively test 8-bit cases
+    check_range<uint8_t, uint8_t>(0, 256, 0, 1,
+                                  0, 256, 0, 1,
+                                  0, 256, 0, 1,
+                                  "<uint8_t, uint8_t> exhaustive");
+    check_range<int8_t, uint8_t>(0, 256, -128, 1,
+                                 0, 256, -128, 1,
+                                 0, 256, 0, 1,
+                                 "<int8_t, uint8_t> exhaustive");
+    check_range<uint8_t, float>(0, 256, 0, 1,
+                                0, 256, 0, 1,
+                                0, 256, 0, 1/255.0f,
+                                "<uint8_t, float> exhaustive");
+    check_range<int8_t, float>(0, 256, -128, 1,
+                               0, 256, -128, 1,
+                               0, 256, 0, 1/255.0f,
+                               "<int8_t, float> exhaustive");
+
+    // Check all delta values for 16-bit, verify swapping arguments doesn't break
+    check_range<uint16_t, uint16_t>(0, 65536, 0, 1,
+                                    65535, 1, 0, 1,
+                                    0, 257, 255, 1,
+                                    "<uint16_t, uint16_t> all zero starts");
+    check_range<uint16_t, uint16_t>(65535, 1, 0, 1,
+                                    0, 65536, 0, 1,
+                                    0, 257, 255, 1,
+                                    "<uint16_t, uint16_t> all one starts");
 
 
-  // Verify different bit sizes for value and weight types
-  check_range<uint16_t, uint8_t>(0, 1, 0, 1,
+    // Verify different bit sizes for value and weight types
+    check_range<uint16_t, uint8_t>(0, 1, 0, 1,
+                                   65535, 1, 0, 1,
+                                   0, 255, 1, 1,
+                                   "<uint16_t, uint8_t> zero, one uint8_t weight test");
+    check_range<uint16_t, uint32_t>(0, 1, 0, 1,
+                                   65535, 1, 0, 1,
+                                   std::numeric_limits<int32_t>::min(), 257, 255 * 65535, 1,
+                                   "<uint16_t, uint8_t> zero, one uint32_t weight test");
+    check_range<uint32_t, uint8_t>(0, 1, 0, 1,
+                                   1 << 31, 1, 0, 1,
+                                   0, 255, 0, 1,
+                                   "<uint32_t, uint8_t> weight test");
+    check_range<uint32_t, uint16_t>(0, 1, 0, 1,
+                                    1 << 31, 1, 0, 1,
+                                    0, 65535, 0, 1,
+                                   "<uint32_t, uint16_t> weight test");
+
+    // Verify float weights with integer values
+    check_range<uint16_t, float>(0, 1, 0, 1,
                                  65535, 1, 0, 1,
-                                 0, 255, 1, 1,
-                                 "<uint16_t, uint8_t> zero, one uint8_t weight test");
-  check_range<uint16_t, uint32_t>(0, 1, 0, 1,
-                                 65535, 1, 0, 1,
+                                 0, 257, 255, 1/65535.0f,
+                                 "<uint16_t, float> zero, one float weight test");
+
+    check_range<int16_t, uint16_t>(0, 65536, -32768, 1,
+                                   0, 1, 0, 1,
+                                   0, 257, 255, 1,
+                                   "<int16_t, uint16_t> all zero starts");
+
+  #if 0 // takes too long, difficult to test with uint32_t
+    // Check all delta values for 32-bit, do it in signed arithmetic
+    check_range<int32_t, uint32_t>(std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max(), 0, 1,
+                                   1 << 31, 1, 0, 1,
+                                   0, 1, 1 << 31, 1,
+                                    "<uint32_t, uint32_t> all zero starts");
+  #endif
+
+    check_range<float, float>(0, 100, 0, .01,
+                              0, 100, 0, .01,
+                              0, 100, 0, .01,
+                              "<float, float> float values 0 to 1 by 1/100ths");
+
+    check_range<float, float>(0, 100, -5, .1,
+                              0, 100, 0, .1,
+                              0, 100, 0, .1,
+                              "<float, float> float values -5 to 5 by 1/100ths");
+
+    // Verify float values with integer weights
+    check_range<float, uint8_t>(0, 100, -5, .1,
+                              0, 100, 0, .1,
+                              0, 255, 0, 1,
+                              "<float, uint8_t> float values -5 to 5 by 1/100ths");
+    check_range<float, uint16_t>(0, 100, -5, .1,
+                                 0, 100, 0, .1,
+                                 0, 255, 0, 257,
+                                 "<float, uint16_t> float values -5 to 5 by 1/100ths");
+    check_range<float, uint32_t>(0, 100, -5, .1,
+                                 0, 100, 0, .1,
                                  std::numeric_limits<int32_t>::min(), 257, 255 * 65535, 1,
-                                 "<uint16_t, uint8_t> zero, one uint32_t weight test");
-  check_range<uint32_t, uint8_t>(0, 1, 0, 1,
-                                 1 << 31, 1, 0, 1,
-                                 0, 255, 0, 1,
-                                 "<uint32_t, uint8_t> weight test");
-  check_range<uint32_t, uint16_t>(0, 1, 0, 1,
-                                  1 << 31, 1, 0, 1,
-                                  0, 65535, 0, 1,
-                                 "<uint32_t, uint16_t> weight test");
-
-  // Verify float weights with integer values
-  check_range<uint16_t, float>(0, 1, 0, 1,
-                               65535, 1, 0, 1,
-                               0, 257, 255, 1/65535.0f,
-                               "<uint16_t, float> zero, one float weight test");
-
-  check_range<int16_t, uint16_t>(0, 65536, -32768, 1,
-                                 0, 1, 0, 1,
-                                 0, 257, 255, 1,
-                                 "<int16_t, uint16_t> all zero starts");
-
-#if 0 // takes too long, difficult to test with uint32_t
-  // Check all delta values for 32-bit, do it in signed arithmetic
-  check_range<int32_t, uint32_t>(std::numeric_limits<int32_t>::min(), std::numeric_limits<int32_t>::max(), 0, 1,
-                                 1 << 31, 1, 0, 1,
-                                 0, 1, 1 << 31, 1,
-                                  "<uint32_t, uint32_t> all zero starts");
-#endif
-
-  check_range<float, float>(0, 100, 0, .01,
-                            0, 100, 0, .01,
-                            0, 100, 0, .01,
-                            "<float, float> float values 0 to 1 by 1/100ths");
-
-  check_range<float, float>(0, 100, -5, .1,
-                            0, 100, 0, .1,
-                            0, 100, 0, .1,
-                            "<float, float> float values -5 to 5 by 1/100ths");
-
+                                 "<float, uint32_t> float values -5 to 5 by 1/100ths");
 }
