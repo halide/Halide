1dde3f893341d8177130a33f40289916155ac809
Andrew Adams <andrew.b.adams@gmail.com>
2013-08-19 09:57:48 -0700

    Added type checks when casting to Image<T> from Buffer
    Also caught a bug in one of the tests
    
    
    Former-commit-id: 522d3ac79d9fb3d36fbb5aba3f4279bc4045c680

---
+++ test/error/wrong_type.cpp
@@ -0,0 +1,13 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Func f;
+    Var x;
+    f(x) = x;
+    Image<float> im = f.realize(100);
+
+    return 0;
+}
