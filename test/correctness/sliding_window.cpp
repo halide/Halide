9346c9b65623f98d1e25ec84c381840cd2d7b17a
Andrew Adams <andrew.b.adams@gmail.com>
2013-08-28 17:08:26 -0700

    Fix storage folding to fold across multiple dimensions.
    Also make it avoid buffers that are treated specially.
    
    
    Former-commit-id: f840eddeec16f4ac3312d44ba1f54ddf1fce3dc2

--- test/correctness/sliding_window.cpp
+++ test/correctness/sliding_window.cpp
@@ -10,9 +10,17 @@ extern "C" int call_counter(int x, int y) {
 }
 HalideExtern_2(int, call_counter, int, int);
 
+extern "C" void *my_malloc(size_t x) {
+    printf("Malloc wasn't supposed to be called!\n");
+    exit(-1);
+}
+
+extern "C" void my_free(void *) {
+}
+
 int main(int argc, char **argv) {
-    Func f1, g1;
-    Var x;
+    Func f1("f1"), g1("g1");
+    Var x("x");
 
     f1(x) = call_counter(x, 0);
     g1(x) = f1(x) + f1(x-1);
@@ -30,15 +38,15 @@ int main(int argc, char **argv) {
     // Now try with a reduction
     count = 0;
     RDom r(0, 100);
-    Var y;
-    Func f2, g2;
+    Var y("y");
+    Func f2("f2"), g2("g2");
 
     f2(x, y) = 0;
     f2(r, y) = call_counter(r, y);
     f2.store_root().compute_at(g2, y);
 
     g2(x, y) = f2(x, y) + f2(x, y-1);
-    
+
     Image<int> im2 = g2.realize(10, 10);
 
     // For each value of y, f should be evaluated over (0 .. 100) in
@@ -50,27 +58,27 @@ int main(int argc, char **argv) {
     }
 
     // Now try sliding over multiple dimensions at once
-    Func f3, g3;
-    
+    Func f3("f3"), g3("g3");
+
     count = 0;
     f3(x, y) = call_counter(x, y);
     g3(x, y) = f3(x-1, y) + f3(x, y) + f3(x, y-1);
     f3.store_root().compute_at(g3, x);
 
     Image<int> im3 = g3.realize(10, 10);
-    
+
     if (count != 11*11) {
         printf("f was called %d times instead of %d times\n", count, 11*11);
         return -1;
     }
 
-    Func f4, g4;
+    Func f4("f4"), g4("g4");
 
     // Now a trickier example. In order for this to work, Halide would have to slide diagonally. We don't handle this.
     count = 0;
-    f4(x, y) = call_counter(x, y);    
+    f4(x, y) = call_counter(x, y);
     // When x was two smaller the second term was computed. When y was two smaller the third term was computed.
-    g4(x, y) = f4(x+y, x-y) + f4((x-2)+y, (x-2)-y) + f4(x+(y-2), x-(y-2)); 
+    g4(x, y) = f4(x+y, x-y) + f4((x-2)+y, (x-2)-y) + f4(x+(y-2), x-(y-2));
     f4.store_root().compute_at(g4, x);
 
     Image<int> im4 = g4.realize(10, 10);
@@ -80,6 +88,14 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    // Now make sure Halide folds the example in Func.h down to a stack allocation
+    Func f5("f5"), g5("g5");
+    f5(x, y) = x*y;
+    g5(x, y) = f5(x, y) + f5(x+1, y) + f5(x, y+1) + f5(x+1, y+1);
+    f5.store_at(g5, y).compute_at(g5, x);
+    g5.set_custom_allocator(&my_malloc, &my_free);
+    Image<int> im5 = g5.realize(10, 10);
+
     printf("Success!\n");
-    return 0;    
+    return 0;
 }
