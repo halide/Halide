0e566652427cd51d638db990219061e61cf4f7b5
Zalman Stern <zalman@google.com>
2013-09-05 17:18:29 -0700

    Catch division and modulo by constant zero in IROperator.h rather than crashing in Simplify's Mod visitor later.
    Former-commit-id: b71c4ec72b4979dcefd4a8fa6d539e3e4d252740

---
+++ test/error/modulo_constant_zero.cpp
@@ -0,0 +1,17 @@
+#include <Halide.h>
+
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Func f;
+    Var x;
+    f(x) = x % 0;
+
+    Buffer result = f.realize(10);
+
+    printf("Success!\n");
+    return 0;
+}
+
