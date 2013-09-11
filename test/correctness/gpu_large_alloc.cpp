abcf4c67c6b1fd96ccd8ba8657dc3e408a783c8b
Andrew Adams <andrew.b.adams@gmail.com>
2013-09-11 15:50:20 -0700

    Adjusted gpu alloc test to use less memory.
    It was sometimes failing during the night.

--- test/correctness/gpu_large_alloc.cpp
+++ test/correctness/gpu_large_alloc.cpp
@@ -8,8 +8,10 @@ int main(int argc, char **argv) {
     Func f, g;
 
     // Up to about 40MB/image * 2 buffers seems to work on luxosr, when freshly booted
-    // 130MB works on 2GB Quadro 4000
-    int W = 1024*130/4, H = 1024;
+    // 130MB works on 2GB Quadro 4000 when freshly booted
+
+    // Here we'll allocated 10MB/image * 2 buffers, so that the test passes reliably.
+    int W = 1024*10/4, H = 1024;
 
     printf("Defining function...\n");
 
