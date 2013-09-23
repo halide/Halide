56c709d6f1c52808a586662493441ea5f2943d24
Andrew Adams <andrew.b.adams@gmail.com>
2013-09-23 11:16:46 -0700

    Adding test to previous commit.
    Former-commit-id: 0987b8abb20ebe7fab63c2c3d63eebba1347153f

---
+++ test/correctness/bounds_of_cast.cpp
@@ -0,0 +1,38 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+void check(Func f, ImageParam in, int min, int extent) {
+    Image<int> output(12345);
+    output.set_min(-1234);
+
+    Buffer buf;
+    in.set(buf);
+    f.infer_input_bounds(output);
+    Image<int> im = in.get();
+
+    if (im.extent(0) != extent || im.min(0) != min) {
+        printf("Inferred size was [%d, %d] instead of [%d, %d]\n",
+               im.min(0), im.extent(0), min, extent);
+        exit(-1);
+    }
+}
+
+int main(int argc, char **argv) {
+    ImageParam input(Int(32), 1);
+    Var x;
+    Func f1 = lambda(x, input(cast<uint8_t>(x)));
+    Func f2 = lambda(x, input(cast<int8_t>(x)));
+    Func f3 = lambda(x, input(cast<uint16_t>(x)));
+    Func f4 = lambda(x, input(cast<int16_t>(x)));
+
+    // input should only be required from 0 to 256
+    check(f1, input, 0, 256);
+    check(f2, input, -128, 256);
+    check(f3, input, 0, 65536);
+    check(f4, input, -32768, 65536);
+
+    printf("Success!\n");
+    return 0;
+}
