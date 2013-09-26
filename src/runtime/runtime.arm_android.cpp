e7cff337fcb5b7b9f6822f2dc3a35c1629297c53
Andrew Adams <andrew.b.adams@gmail.com>
2013-09-26 08:38:37 -0700

    Added tracing.cpp to arm android runtime.
    Former-commit-id: 36dc83a346aa5b4f6087380d694781045d865366

--- src/runtime/runtime.arm_android.cpp
+++ src/runtime/runtime.arm_android.cpp
@@ -4,6 +4,7 @@
 #include "posix_error_handler.cpp"
 #include "write_debug_image.cpp"
 #include "android_io.cpp"
+#include "tracing.cpp"
 #include "posix_math.cpp"
 #include "posix_thread_pool.cpp"
 #include "copy_to_host_noop.cpp"
