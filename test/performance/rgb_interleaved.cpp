d9ed64595543b158e6be48eb3dc8de2513c76ade
Zalman Stern <zalman@google.com>
2013-09-30 13:31:48 -0700

    Add semi-planar case to rgb_interleaved performance test.
    Former-commit-id: 48a70ff6dab08ca55e7a8da558eb62d35e22dc7d

--- test/performance/rgb_interleaved.cpp
+++ test/performance/rgb_interleaved.cpp
@@ -26,11 +26,14 @@ int main(int argc, char **argv) {
     // Allocate two 16 megapixel, 3 channel, 8-bit images -- input and output
     const int32_t buffer_size = 1 << 24;
     const int32_t buffer_side_length = 1 << 12;
+
     uint8_t *src_storage(new uint8_t[buffer_size * 3]);
     uint8_t *dst_storage(new uint8_t[buffer_size * 3]);
 
-    // Setup src to be RGB interleaved, with no extra padding between channels or rows.
     buffer_t src_buffer;
+    buffer_t dst_buffer;
+
+    // Setup src to be RGB interleaved, with no extra padding between channels or rows.
     memset(&src_buffer, 0, sizeof(src_buffer));
     src_buffer.host = src_storage;
     src_buffer.extent[0] = buffer_side_length;
@@ -42,7 +45,6 @@ int main(int argc, char **argv) {
     src_buffer.elem_size = 1;
 
     // Setup dst to be planar, with no extra padding between channels or rows.
-    buffer_t dst_buffer;
     memset(&dst_buffer, 0, sizeof(dst_buffer));
     dst_buffer.host = dst_storage;
     dst_buffer.extent[0] = buffer_side_length;
@@ -72,6 +74,8 @@ int main(int argc, char **argv) {
     dst.realize(dst_image);
     double t2 = currentTime();
 
+    printf("Interelaved to planar bandwidth %.3e byte/s.\n", (buffer_size / (t2 - t1)) * 1000);
+
     for (int32_t x = 0; x < buffer_side_length; x++) {
         for (int32_t y = 0; y < buffer_side_length; y++) {
             assert(dst_image(x, y, 0) == 0);
@@ -80,12 +84,34 @@ int main(int argc, char **argv) {
         }
     }
 
+    // Setup a semi-planar output case.
+    memset(&dst_buffer, 0, sizeof(dst_buffer));
+    dst_buffer.host = dst_storage;
+    dst_buffer.extent[0] = buffer_side_length;
+    dst_buffer.stride[0] = 1;
+    dst_buffer.extent[1] = buffer_side_length;
+    dst_buffer.stride[1] = dst_buffer.stride[0] * dst_buffer.extent[0] * 3;
+    dst_buffer.extent[2] = 3;
+    dst_buffer.stride[2] = dst_buffer.extent[0];
+    dst_buffer.elem_size = 1;
+
+    double t3 = currentTime();
+    dst.realize(dst_image);
+    double t4 = currentTime();
+
+    for (int32_t x = 0; x < buffer_side_length; x++) {
+        for (int32_t y = 0; y < buffer_side_length; y++) {
+            assert(dst_image(x, y, 0) == 0);
+            assert(dst_image(x, y, 1) == 128);
+            assert(dst_image(x, y, 2) == 255);
+        }
+    }
+
+    printf("Interelaved to semi-planar bandwidth %.3e byte/s.\n", (buffer_size / (t4 - t3)) * 1000);
+
     delete[] src_storage;
     delete[] dst_storage;
-
-    
-    printf("Interelaved to planar bandwidth %.3e byte/s.\n", (buffer_size / (t2 - t1)) * 1000);
-
+   
     printf("Success!\n");
     return 0;
 }
