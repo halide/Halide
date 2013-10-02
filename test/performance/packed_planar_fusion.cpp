978ec241ef02563816257231bb760415108dd427
Andrew Adams <andrew.b.adams@gmail.com>
2013-10-02 15:36:34 -0700

    Added performance test for new fusion operator.
    Former-commit-id: 46543b4c633e124adb51cdef03640b374aabcb3f

---
+++ test/performance/packed_planar_fusion.cpp
@@ -0,0 +1,111 @@
+#include <Halide.h>
+#include <stdio.h>
+#include "clock.h"
+#include <memory>
+
+using namespace Halide;
+
+double test_copy(Image<uint8_t> src, Image<uint8_t> dst) {
+    Var x, y, c;
+    Func f;
+    f(x, y, c) = src(x, y, c);
+
+    for (int i = 0; i < 3; i++) {
+        f.output_buffer()
+            .set_stride(i, dst.stride(i))
+            .set_extent(i, dst.extent(i))
+            .set_min(i, dst.min(i));
+    }
+
+    if (dst.stride(0) == 1) {
+        f.vectorize(x, 16);
+    } else if (dst.stride(0) == 3 && src.stride(0) == 3) {
+        // For a memcpy of packed rgb data, we can fuse x and c and
+        // vectorize over the combination.
+        Var fused("fused");
+        f.reorder(c, x, y).fuse(c, x, fused).vectorize(fused, 16);
+    }
+
+    f.realize(dst);
+
+    f.compile_to_assembly(std::string("copy_") + f.name() + ".s", Internal::vec<Argument>(src), "copy");
+
+    double t1 = currentTime();
+    for (int i = 0; i < 10; i++) {
+        f.realize(dst);
+    }
+    double t2 = currentTime();
+    return t2 - t1;
+}
+
+Image<uint8_t> make_packed(uint8_t *host, int W, int H) {
+    buffer_t buf = {0};
+    buf.host = host;
+    buf.extent[0] = W;
+    buf.stride[0] = 3;
+    buf.extent[1] = H;
+    buf.stride[1] = buf.stride[0] * buf.extent[0];
+    buf.extent[2] = 3;
+    buf.stride[2] = 1;
+    buf.elem_size = 1;
+    return Image<uint8_t>(&buf);
+}
+
+Image<uint8_t> make_planar(uint8_t *host, int W, int H) {
+    buffer_t buf = {0};
+    buf.host = host;
+    buf.extent[0] = W;
+    buf.stride[0] = 1;
+    buf.extent[1] = H;
+    buf.stride[1] = buf.stride[0] * buf.extent[0];
+    buf.extent[2] = 3;
+    buf.stride[2] = buf.stride[1] * buf.extent[1];
+    buf.elem_size = 1;
+    return Image<uint8_t>(&buf);
+}
+
+int main(int argc, char **argv) {
+
+    const int W = 1<<11, H = 1<<11;
+
+    // Allocate two 4 megapixel, 3 channel, 8-bit images -- input and output
+    uint8_t *storage_1(new uint8_t[W * H * 3]);
+    uint8_t *storage_2(new uint8_t[W * H * 3]);
+
+    double t_packed_packed = test_copy(make_packed(storage_1, W, H),
+                                       make_packed(storage_2, W, H));
+    double t_packed_planar = test_copy(make_packed(storage_1, W, H),
+                                       make_planar(storage_2, W, H));
+    double t_planar_packed = test_copy(make_planar(storage_1, W, H),
+                                       make_packed(storage_2, W, H));
+    double t_planar_planar = test_copy(make_planar(storage_1, W, H),
+                                       make_planar(storage_2, W, H));
+
+
+
+
+    delete[] storage_1;
+    delete[] storage_2;
+
+    if (t_planar_planar > t_packed_packed * 1.2 ||
+        t_packed_packed > t_packed_planar * 1.2 ||
+        t_packed_planar > t_planar_packed * 1.2) {
+        printf("Times were not in expected order:\n"
+               "planar -> planar: %f \n"
+               "packed -> packed: %f \n"
+               "packed -> planar: %f \n"
+               "planar -> packed: %f \n",
+               t_planar_planar,
+               t_packed_packed,
+               t_packed_planar,
+               t_planar_packed);
+
+        return -1;
+    }
+
+
+
+
+    printf("Success!\n");
+    return 0;
+}
