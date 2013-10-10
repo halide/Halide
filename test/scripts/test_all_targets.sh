a591e14d6268d9535fb782b52f7ff29866028676
Andrew Adams <andrew.b.adams@gmail.com>
2013-10-10 09:17:07 -0700

    Made test scripts more robust, and also generate a distribution

--- test/scripts/test_all_targets.sh
+++ test/scripts/test_all_targets.sh
@@ -18,9 +18,9 @@ mkdir -p testing/deps
 if [[ ! -f testing/deps/libpng32.a ]]; then
     cd testing/deps
     echo Acquiring and building libpng
-    curl -L http://sourceforge.net/projects/libpng/files/libpng16/1.6.3/lpng163.zip/download -o lpng163.zip || exit 1
-    unzip -n lpng163.zip
-    cd lpng163
+    curl -L http://sourceforge.net/projects/libpng/files/libpng16/1.6.6/lpng166.zip/download -o lpng166.zip || exit 1
+    unzip -n lpng166.zip
+    cd lpng166
     if [[ `uname` == Darwin ]]; then
         make -f scripts/makefile.darwin clean
         make -f scripts/makefile.darwin ARCH="-arch i386 -arch x86_64" || exit 1
@@ -94,7 +94,7 @@ for LLVM in trunk release-3.2 release-3.3 pnacl; do
         CLANG_REPO=http://llvm.org/svn/llvm-project/cfe/branches/release_33
     fi
 
-    # Check out and build this llvm if necessary
+    # Check out llvm if necessary
     if [ ! -d llvm/${LLVM} ]; then
         if [[ "$LLVM_REPO" == *.git ]]; then
             git clone $LLVM_REPO llvm/${LLVM}
@@ -103,20 +103,27 @@ for LLVM in trunk release-3.2 release-3.3 pnacl; do
             svn co $LLVM_REPO llvm/${LLVM}
             svn co $CLANG_REPO llvm/${LLVM}/tools/clang
         fi
+    fi
 
-        # Build it
-        cd llvm/${LLVM}
+    # Configure and build it
+    cd llvm/${LLVM}
+    if [ ! -f build-32/bin/llvm-config ]; then
         mkdir build-32
         cd build-32
-        cmake -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD="X86;ARM;NVPTX" -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_32_BITS=ON ..
+        CC=gcc CXX=g++ cmake -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD="X86;ARM;NVPTX" -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_32_BITS=ON ..
         make -j8
         cd ..
+    fi
+    if [ ! -f build-64/bin/llvm-config ]; then
         mkdir build-64
         cd build-64
-        cmake -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD="X86;ARM;NVPTX" -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_BUILD_TYPE=Release ..
+        CC=gcc CXX=g++ cmake -DLLVM_ENABLE_TERMINFO=OFF -DLLVM_TARGETS_TO_BUILD="X86;ARM;NVPTX" -DLLVM_ENABLE_ASSERTIONS=ON -DCMAKE_BUILD_TYPE=Release ..
         make -j8
-        cd ../../../
-    elif [[ "$LLVM" == trunk ]]; then
+        cd ..
+    fi
+    cd ../../
+
+    if [[ "$LLVM" == trunk ]]; then
         # Update this llvm and rebuild if it's trunk
         cd llvm/${LLVM}
         svn up &&
@@ -137,7 +144,9 @@ for LLVM in trunk release-3.2 release-3.3 pnacl; do
         make -j8 -C build-64 &&
         cd ../../
     fi
+done
 
+for LLVM in trunk release-3.2 release-3.3 pnacl; do
     if [[ "$LLVM" == pnacl ]]; then
         TARGETS="x86-32-nacl x86-32-sse41-nacl x86-64-nacl x86-64-sse41-nacl"
     else
--- test/scripts/test_target.sh
+++ test/scripts/test_target.sh
@@ -3,6 +3,7 @@ LLVM=$1
 export HL_TARGET=$2
 
 if [[ "$HL_TARGET" == x86-3* ]]; then
+    BITS=32
     UNAME=`uname`
     if [[ `uname` == Linux ]]; then
         export LD="ld -melf_i386"
@@ -10,18 +11,19 @@ if [[ "$HL_TARGET" == x86-3* ]]; then
         # OS X auto-detects the output format correctly
         export LD="ld"
     fi
-    export CC="cc -m32"
-    export CXX="c++ -m32"
+    export CC="gcc -m32"
+    export CXX="g++ -m32"
     export GXX="g++ -m32"
     export CLANG=llvm/${LLVM}/build-32/bin/clang
     export LLVM_CONFIG=llvm/${LLVM}/build-32/bin/llvm-config
     export LIBPNG_LIBS="-Ltesting/deps -L../../testing/deps -lpng32 -lz32"
     export LIBPNG_CXX_FLAGS="-Itesting/deps -I../../testing/deps"
 else
+    BITS=64
     # ptx falls into this category
     export LD="ld"
-    export CC="cc -m64"
-    export CXX="c++ -m64"
+    export CC="gcc -m64"
+    export CXX="g++ -m64"
     export GXX="g++ -m64"
     export CLANG=llvm/${LLVM}/build-64/bin/clang
     export LLVM_CONFIG=llvm/${LLVM}/build-64/bin/llvm-config
@@ -35,11 +37,15 @@ echo Using CC = $CC
 echo Using CXX = $CXX
 make clean &&
 make -j8 build_tests || exit 1
+make distrib || exit 1
+DATE=`date +%Y_%m_%d`
+HOST=`uname`
+mv distrib/halide.tgz distrib/halide_${HOST}_${BITS}_${LLVM}_${DATE}.tgz
 if [[ "$HL_TARGET" == *nacl ]]; then
     # The tests don't work for nacl yet. It's still worth testing that everything builds.
     echo "Halide builds but tests not run."
 else
-    make run_tests &&
+    make run_tests -j8 &&
     make test_apps &&
     echo "All tests pass"
 fi
