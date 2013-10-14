88c2cfa5896ce904ce05279cda5d79b2b32e09cc
Andrew Adams <andrew.b.adams@gmail.com>
2013-10-14 16:10:25 -0700

    Make the android build script assume tools in path
    Former-commit-id: 529c9a32cf630046ab0bb5302a04ea73b5bba91f

--- apps/HelloAndroid/build.sh
+++ apps/HelloAndroid/build.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+set -e
 android update project -p .
-cd jni && \
-c++ halide.cpp -L ../../../bin -lHalide -I ../../../include -ldl -lpthread &&  \
-LD_LIBRARY_PATH=../../../bin HL_TARGET=arm-32-android ./a.out &&  \
-cd .. &&  \
-~/android-ndk-r8d/ndk-build && \
-ant debug &&  \
-adb install -r bin/HelloAndroid-debug.apk && \
+cd jni 
+c++ halide.cpp -L ../../../bin -lHalide -I ../../../include -ldl -lpthread 
+DYLD_LIBRARY_PATH=../../../bin LD_LIBRARY_PATH=../../../bin HL_TARGET=arm-32-android ./a.out
+cd .. 
+pwd 
+ndk-build 
+ant debug 
+adb install -r bin/HelloAndroid-debug.apk 
 adb logcat
