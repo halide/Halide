2c6a91ec23dcda08d6e5b4429875a34c7634e18d
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2013-10-30 13:30:07 -0400

    Fix up python_bindings build
    - support run_apps, run_apps_headless without first installing globally.
    - fix dependencies in basic build rules.
    - make setup.py support `$BUILD_PREFIX` as used by the main Halide build.

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -6,17 +6,25 @@ import glob
 import subprocess
 import shutil
 
+build_prefix = os.getenv('BUILD_PREFIX')
+if not build_prefix:
+    build_prefix = ''
+halide_root = '..'
+include_path = os.path.join(halide_root, 'include')
+bin_path = os.path.join(halide_root, 'bin', build_prefix)
+image_path = os.path.join(halide_root, 'apps', 'images')
+
 png_cflags  = subprocess.check_output('libpng-config --cflags',  shell=True).strip()
 png_ldflags = subprocess.check_output('libpng-config --ldflags', shell=True).strip()
 
 ext_modules = [Extension("halide/_cHalide", ["halide/cHalide_wrap.cxx", 'halide/py_util.cpp'],
-                         include_dirs=['../include'],
+                         include_dirs=[include_path],
                          extra_compile_args=('-ffast-math -O3 -msse -Wl,-dead_strip -fno-common' + ' ' + png_cflags).split(),
-                         extra_link_args=['../bin/libHalide.a', '-lpthread', '-ldl', '-lstdc++', '-lc']+png_ldflags.split(),
+                         extra_link_args=[os.path.join(bin_path, 'libHalide.a'), '-lpthread', '-ldl', '-lstdc++', '-lc']+png_ldflags.split(),
                          language='c++')]
 
 if glob.glob('halide/data/*.png') == []:
-    shutil.copytree('../apps/images/', 'halide/data')
+    shutil.copytree(image_path, 'halide/data')
     
 setup(
     name = 'halide',
