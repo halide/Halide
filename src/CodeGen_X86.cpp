b4d374a1736dad7cd55a2d68c97826fcb6b62a24
Andrew Adams <abadams@google.com>
2013-11-04 09:55:20 -0800

    X86 patterns now pick up broadcasts

--- src/CodeGen_X86.cpp
+++ src/CodeGen_X86.cpp
@@ -73,7 +73,7 @@ void CodeGen_X86::compile(Stmt stmt, string name, const vector<Argument> &args) 
         } else {
             // Use COFF files instead of elf.
             if (target.bits == 32) {
-                module->setTargetTriple("i386-pc-win32-unknown"); 
+                module->setTargetTriple("i386-pc-win32-unknown");
             } else {
                 module->setTargetTriple("x86_64-pc-win32-unknown");
             }
@@ -167,6 +167,48 @@ Value *CodeGen_X86::call_intrin(llvm::Type *result_type, const string &name, vec
     return call;
 }
 
+namespace {
+
+// Attempt to cast an expression to a smaller type while provably not
+// losing information. If it can't be done, return an undefined Expr.
+Expr lossless_cast(Type t, Expr e) {
+    if (t.can_represent(e.type())) {
+        return e;
+    }
+
+    if (const Cast *c = e.as<Cast>()) {
+        if (t == c->value.type()) {
+            return c->value;
+        } else if (t.can_represent(c->value.type())) {
+            return cast(t, c->value);
+        } else {
+            return Expr();
+        }
+    }
+
+    if (const Broadcast *b = e.as<Broadcast>()) {
+        Expr v = lossless_cast(t.element_of(), b->value);
+        if (v.defined()) {
+            return Broadcast::make(v, b->width);
+        } else {
+            return Expr();
+        }
+    }
+
+    if (const IntImm *i = e.as<IntImm>()) {
+        int x = int_cast_constant(t, i->value);
+        if (x == i->value) {
+            return e;
+        } else {
+            return Expr();
+        }
+    }
+
+    return Expr();
+}
+
+}
+
 void CodeGen_X86::visit(const Cast *op) {
 
     vector<Expr> matches;
@@ -174,43 +216,44 @@ void CodeGen_X86::visit(const Cast *op) {
     struct Pattern {
         bool needs_sse_41;
         bool extern_call;
+        bool wide_op;
         Type type;
         string intrin;
         Expr pattern;
     };
 
     Pattern patterns[] = {
-        {false, false, Int(8, 16), "sse2.padds.b",
-         _i8(clamp(_i16(wild_i8x16) + _i16(wild_i8x16), -128, 127))},
-        {false, false, Int(8, 16), "sse2.psubs.b",
-         _i8(clamp(_i16(wild_i8x16) - _i16(wild_i8x16), -128, 127))},
-        {false, false, UInt(8, 16), "sse2.paddus.b",
-         _u8(min(_u16(wild_u8x16) + _u16(wild_u8x16), 255))},
-        {false, false, UInt(8, 16), "sse2.psubus.b",
-         _u8(max(_i16(wild_u8x16) - _i16(wild_u8x16), 0))},
-        {false, false, Int(16, 8), "sse2.padds.w",
-         _i16(clamp(_i32(wild_i16x8) + _i32(wild_i16x8), -32768, 32767))},
-        {false, false, Int(16, 8), "sse2.psubs.w",
-         _i16(clamp(_i32(wild_i16x8) - _i32(wild_i16x8), -32768, 32767))},
-        {false, false, UInt(16, 8), "sse2.paddus.w",
-         _u16(min(_u32(wild_u16x8) + _u32(wild_u16x8), 65535))},
-        {false, false, UInt(16, 8), "sse2.psubus.w",
-         _u16(max(_i32(wild_u16x8) - _i32(wild_u16x8), 0))},
-        {false, false, Int(16, 8), "sse2.pmulh.w",
-         _i16((_i32(wild_i16x8) * _i32(wild_i16x8)) / 65536)},
-        {false, false, UInt(16, 8), "sse2.pmulhu.w",
-         _u16((_u32(wild_u16x8) * _u32(wild_u16x8)) / 65536)},
-        {false, false, UInt(8, 16), "sse2.pavg.b",
-         _u8(((_u16(wild_u8x16) + _u16(wild_u8x16)) + 1) / 2)},
-        {false, false, UInt(16, 8), "sse2.pavg.w",
-         _u16(((_u32(wild_u16x8) + _u32(wild_u16x8)) + 1) / 2)},
-        {false, true, Int(16, 8), "packssdw",
+        {false, false, true, Int(8, 16), "sse2.padds.b",
+         _i8(clamp(wild_i16x16 + wild_i16x16, -128, 127))},
+        {false, false, true, Int(8, 16), "sse2.psubs.b",
+         _i8(clamp(wild_i16x16 - wild_i16x16, -128, 127))},
+        {false, false, true, UInt(8, 16), "sse2.paddus.b",
+         _u8(min(wild_u16x16 + wild_u16x16, 255))},
+        {false, false, true, UInt(8, 16), "sse2.psubus.b",
+         _u8(max(wild_i16x16 - wild_i16x16, 0))},
+        {false, false, true, Int(16, 8), "sse2.padds.w",
+         _i16(clamp(wild_i32x8 + wild_i32x8, -32768, 32767))},
+        {false, false, true, Int(16, 8), "sse2.psubs.w",
+         _i16(clamp(wild_i32x8 - wild_i32x8, -32768, 32767))},
+        {false, false, true, UInt(16, 8), "sse2.paddus.w",
+         _u16(min(wild_u32x8 + wild_u32x8, 65535))},
+        {false, false, true, UInt(16, 8), "sse2.psubus.w",
+         _u16(max(wild_i32x8 - wild_i32x8, 0))},
+        {false, false, true, Int(16, 8), "sse2.pmulh.w",
+         _i16((wild_i32x8 * wild_i32x8) / 65536)},
+        {false, false, true, UInt(16, 8), "sse2.pmulhu.w",
+         _u16((wild_u32x8 * wild_u32x8) / 65536)},
+        {false, false, true, UInt(8, 16), "sse2.pavg.b",
+         _u8(((wild_u16x16 + wild_u16x16) + 1) / 2)},
+        {false, false, true, UInt(16, 8), "sse2.pavg.w",
+         _u16(((wild_u32x8 + wild_u32x8) + 1) / 2)},
+        {false, true, false, Int(16, 8), "packssdw",
          _i16(clamp(wild_i32x8, -32768, 32767))},
-        {false, true, Int(8, 16), "packsswb",
+        {false, true, false, Int(8, 16), "packsswb",
          _i8(clamp(wild_i16x16, -128, 127))},
-        {false, true, UInt(8, 16), "packuswb",
+        {false, true, false, UInt(8, 16), "packuswb",
          _u8(clamp(wild_i16x16, 0, 255))},
-        {true, true, UInt(16, 8), "packusdw",
+        {true, true, false, UInt(16, 8), "packusdw",
          _u16(clamp(wild_i32x8, 0, 65535))}
     };
 
@@ -218,6 +261,16 @@ void CodeGen_X86::visit(const Cast *op) {
         const Pattern &pattern = patterns[i];
         if (!(target.features | Target::SSE41) && pattern.needs_sse_41) continue;
         if (expr_match(pattern.pattern, op, matches)) {
+            bool ok = true;
+            if (pattern.wide_op) {
+                // Try to narrow the matches to the target type.
+                for (size_t i = 0; i < matches.size(); i++) {
+                    matches[i] = lossless_cast(pattern.type, matches[i]);
+                    if (!matches[i].defined()) ok = false;
+                }
+            }
+            if (!ok) continue;
+
             if (pattern.extern_call) {
                 value = codegen(Call::make(pattern.type, pattern.intrin, matches, Call::Extern));
             } else {
