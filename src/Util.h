35844e82049a518ddb719e092da1bd0d0beec0a0
dsharlet-intel <dillon.sharlet@intel.com>
2013-11-18 12:34:13 -0800

    Ensured __declspec(dll*) is only defined on windows
    Former-commit-id: 3359d6ae1f1759f38a76e1d06da98c2c84f6b67b

--- src/Util.h
+++ src/Util.h
@@ -18,7 +18,7 @@
 #include <cstring>
 
 // by default, the symbol EXPORT does nothing. In windows dll builds we can define it to __declspec(dllexport)
-#ifdef Halide_SHARED
+#if defined(_WIN32) && defined(Halide_SHARED)
 #ifdef Halide_EXPORTS
 #define EXPORT __declspec(dllexport)
 #else
