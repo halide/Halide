9fa46466955cba57d251a0a1e341a38dc9ad1e88
dsharlet-intel <dillon.sharlet@intel.com>
2013-12-02 10:37:32 -0800

    Used existing unique_name from Util

--- src/CodeGen_GPU_Host.h
+++ src/CodeGen_GPU_Host.h
@@ -63,12 +63,7 @@ protected:
     /** Reaches inside the module at sets it to use a single shared
      * cuda context */
     void jit_finalize(llvm::ExecutionEngine *ee, llvm::Module *mod, std::vector<void (*)()> *cleanup_routines);
-
-    /** Creates a unique name for the kernels generated in this program */
-    std::string unique_name(const std::string &name);
-
-    std::map<std::string, int> unique_names;
-
+    
     static bool lib_cuda_linked;
 
     static CodeGen_GPU_Dev* make_dev(Target);
