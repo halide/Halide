231e39a8710461a3bc66ef36fa1cd9f62f70f0b1
Andrew Adams <andrew.b.adams@gmail.com>
2013-12-16 10:35:14 -0800

    Added min accessor to ImageParams, and top/left/bottom/right helpers.
    Former-commit-id: d1cbd6c68f8a44d6457fe9aed80f9e06064cd803

---
+++ test/correctness/min_extent.cpp
@@ -0,0 +1,53 @@
+// Test whether min[] and extent[] of an ImageParam are correctly passed into
+// the filter.
+#include <stdio.h>
+#include <Halide.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Var x("x");
+    Func f("f");
+    ImageParam in(Int(32), 1, "in");
+    Expr factor("factor");
+
+    // Multiply by -10 or +10 for coordinates that fall outside the input
+    // image.
+    factor = select(x < in.left(), -10,
+                    select(x > in.right(), 10, 1));
+    f(x) = factor * x;
+
+    // Create input and output buffers. The input pixels are never accessed,
+    // but we initialize them anyway.
+    Image<int> input(5);
+    Image<int> out(10);
+    for (int i = 0; i < input.width(); i++) {
+        input(i) = 0;
+    }
+    for (int i = 0; i < out.width(); i++) {
+        out(i) = 0;
+    }
+
+    // Change coordinate origin of input and output buffer so that they are
+    // aligned as follows:
+    // input         |------|
+    // out     |-----------------|
+    const int INOFF = 4;
+    const int OUTOFF = 1;
+    in.set(input);
+    input.set_min(INOFF);
+    out.set_min(OUTOFF);
+    f.realize(out);
+
+    // Check correctness of result
+    int expected[] = { -10, -20, -30, 4, 5, 6, 7, 8, 90, 100 };
+    for (int i=0; i<out.width(); i++) {
+        if (out(i + OUTOFF) != expected[i]) {
+            printf("Unexpected output: %d != %d\n", out(i + OUTOFF), expected[i]);
+            return -1;
+        }
+    }
+
+    printf("Success!\n");
+    return 0;
+}
