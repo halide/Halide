9f0e4a0e695c68215bad3b086ba43852e06571b4
Andrew Adams <andrew.b.adams@gmail.com>
2014-01-30 09:21:07 -0800

    Better errors for unbounded access to input/output images.
    Former-commit-id: 5dfba20f3a078db260f1c9654e3654041c49e697

---
+++ test/error/unbounded_input.cpp
@@ -0,0 +1,21 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Func f;
+    Var x, y;
+
+    ImageParam in(Float(32), 2);
+    ImageParam x_coord(Int(32), 2);
+    ImageParam y_coord(Int(32), 2);
+
+    f(x, y) = in(x_coord(x, y), y_coord(x, y));
+
+    f.compile_jit();
+
+    printf("I should not have reached here\n");
+
+    return 0;
+}
