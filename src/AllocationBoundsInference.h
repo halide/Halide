bbf1a0a4fd70a045568d8ba55aeefce4d212c7d5
Andrew Adams <andrew.b.adams@gmail.com>
2014-02-09 10:38:25 -0800

    Added missing header guard
    Former-commit-id: a6c1eff2c057aec5090d727a1831aacd9b2cf6da

--- src/AllocationBoundsInference.h
+++ src/AllocationBoundsInference.h
@@ -1,5 +1,9 @@
-#include "IR.h"
+#ifndef ALLOCATION_BOUNDS_INFERENCE_H
+#define ALLOCATION_BOUNDS_INFERENCE_H
+
 #include <map>
+#include <string>
+#include "IR.h"
 
 namespace Halide {
 namespace Internal {
@@ -8,3 +12,5 @@ Stmt allocation_bounds_inference(Stmt s, const std::map<std::string, Function> &
 
 }
 }
+
+#endif
