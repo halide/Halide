35ca95a05d76b62731e86b441af4bda015e74497
Andrew Adams <andrew.b.adams@gmail.com>
2014-02-11 13:45:00 -0800

    Added some missing docs.

--- src/AllocationBoundsInference.h
+++ src/AllocationBoundsInference.h
@@ -1,6 +1,10 @@
 #ifndef HALIDE_ALLOCATION_BOUNDS_INFERENCE_H
 #define HALIDE_ALLOCATION_BOUNDS_INFERENCE_H
 
+/** \file
+ * Defines the lowering pass that determines how large internal allocations should be.
+ */
+
 #include <map>
 #include <string>
 #include "IR.h"
@@ -8,6 +12,8 @@
 namespace Halide {
 namespace Internal {
 
+/** Take a partially statement with Realize nodes in terms of
+ * variables, and define values for those variables. */
 Stmt allocation_bounds_inference(Stmt s, const std::map<std::string, Function> &env);
 
 }
