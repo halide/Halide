16bf8028749e91ec3d3c61cdaf1898f1f64cca64
James Price <j.price@bristol.ac.uk>
2014-02-20 12:45:42 +0000

    CodeGen_GPU_Host now a template to generate both X86 and ARM variants.
    Former-commit-id: 89b228439e6d968c43ecd9d2423c025d5c75866a

--- src/CodeGen_GPU_Host.h
+++ src/CodeGen_GPU_Host.h
@@ -5,6 +5,7 @@
  * Defines the code-generator for producing GPU host code
  */
 
+#include "CodeGen_ARM.h"
 #include "CodeGen_X86.h"
 
 namespace Halide {
@@ -12,71 +13,14 @@ namespace Internal {
 
 struct CodeGen_GPU_Dev;
 
-/** A code generator that emits GPU code from a given Halide stmt. */
-class CodeGen_GPU_Host : public CodeGen_X86 {
-public:
+// Generate class declarations for each host target
+#define GPU_HOST_TARGET X86
+#include "CodeGen_GPU_Host_Template.h"
+#undef GPU_HOST_TARGET
 
-    /** Create a GPU code generator. GPU target is selected via
-     * CodeGen_GPU_Options. Processor features can be enabled using the
-     * appropriate flags from CodeGen_X86_Options */
-    CodeGen_GPU_Host(Target);
-
-    virtual ~CodeGen_GPU_Host();
-
-    /** Compile to an internally-held llvm module. Takes a halide
-     * statement, the name of the function produced, and the arguments
-     * to the function produced. After calling this, call
-     * CodeGen::compile_to_file or
-     * CodeGen::compile_to_function_pointer to get at the x86 machine
-     * code. */
-    void compile(Stmt stmt, std::string name,
-                 const std::vector<Argument> &args,
-                 const std::vector<Buffer> &images_to_embed);
-
-protected:
-    using CodeGen_X86::visit;
-
-    class Closure;
-
-    /** Nodes for which we need to override default behavior for the GPU runtime */
-    // @{
-    void visit(const For *);
-    void visit(const Allocate *);
-    void visit(const Free *);
-    void visit(const Pipeline *);
-    void visit(const Call *);
-    // @}
-
-    // We track buffer_t's for each allocation in order to manage dirty bits
-    bool track_buffers() {return true;}
-
-    //** Runtime function handles */
-    // @{
-    llvm::Function *dev_malloc_fn;
-    llvm::Function *dev_free_fn;
-    llvm::Function *copy_to_dev_fn;
-    llvm::Function *copy_to_host_fn;
-    llvm::Function *dev_run_fn;
-    llvm::Function *dev_sync_fn;
-    // @}
-
-    /** Finds and links in the CUDA runtime symbols prior to jitting */
-    void jit_init(llvm::ExecutionEngine *ee, llvm::Module *mod);
-
-    /** Reaches inside the module at sets it to use a single shared
-     * cuda context */
-    void jit_finalize(llvm::ExecutionEngine *ee, llvm::Module *mod, std::vector<void (*)()> *cleanup_routines);
-
-    static bool lib_cuda_linked;
-
-    static CodeGen_GPU_Dev* make_dev(Target);
-
-    llvm::Value *get_module_state();
-
-private:
-    /** Child code generator for device kernels. */
-    CodeGen_GPU_Dev *cgdev;
-};
+#define GPU_HOST_TARGET ARM
+#include "CodeGen_GPU_Host_Template.h"
+#undef GPU_HOST_TARGET
 
 }}
 
