06c95ac72bcb3b7090efbab7f8859da3190c701f
Andrew Adams <andrew.b.adams@gmail.com>
2014-02-20 09:15:29 -0800

    Clean up static_image

--- apps/camera_pipe/process.cpp
+++ apps/camera_pipe/process.cpp
@@ -24,19 +24,24 @@ int main(int argc, char **argv) {
 
     Image<uint16_t> input = load<uint16_t>(argv[1]);
     Image<uint8_t> output(2560-32, 1920, 3); // image size is hard-coded for the N900 raw pipeline
-    
+
     // These color matrices are for the sensor in the Nokia N900 and are
     // taken from the FCam source.
     float _matrix_3200[][4] = {{ 1.6697f, -0.2693f, -0.4004f, -42.4346f},
                                 {-0.3576f,  1.0615f,  1.5949f, -37.1158f},
                                 {-0.2175f, -1.8751f,  6.9640f, -26.6970f}};
-    
+
     float _matrix_7000[][4] = {{ 2.2997f, -0.4478f,  0.1706f, -39.0923f},
                                 {-0.3826f,  1.5906f, -0.2080f, -25.4311f},
                                 {-0.0888f, -0.7344f,  2.2832f, -20.0826f}};
-    Image<float> matrix_3200(4, 3, 1); matrix_3200.copy(_matrix_3200[0], 4, 3);
-    Image<float> matrix_7000(4, 3, 1); matrix_7000.copy(_matrix_7000[0], 4, 3);
-    
+    Image<float> matrix_3200(4, 3), matrix_7000(4, 3);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            matrix_3200(j, i) = _matrix_3200[i][j];
+            matrix_7000(j, i) = _matrix_7000[i][j];
+        }
+    }
+
     float color_temp = atof(argv[2]);
     float gamma = atof(argv[3]);
     float contrast = atof(argv[4]);
@@ -81,6 +86,6 @@ int main(int argc, char **argv) {
 
     // Timings on N900 as of SIGGRAPH 2012 camera ready are (best of 10)
     // Halide: 722ms, FCam: 741ms
-    
+
     return 0;
 }
