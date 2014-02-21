022a167edbfc68d89629b53b29ff9cdccec770df
Andrew Adams <andrew.b.adams@gmail.com>
2014-02-21 11:08:34 -0800

    Added a test for issue #190
    Former-commit-id: 914cab775b028e01368b190d7df1c70d4b672bfc

---
+++ test/correctness/gpu_free_sync.cpp
@@ -0,0 +1,31 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    // Make sure that freeing GPU buffers doesn't occur before the
+    // computation that is filling them completes.
+    Func f;
+    Var x, y;
+    RDom r(0, 100);
+    f(x, y) = sum(sqrt(sqrt(sqrt(sqrt(x+y+r)))));
+
+    Target t = get_jit_target_from_environment();
+
+    if (t.features & (Target::OpenCL | Target::CUDA)) {
+        f.cuda_tile(x, y, 16, 16);
+
+        // This allocates a buffer, does gpu compute into it, and then
+        // frees it (calling dev_free) possibly before the compute is
+        // done.
+        for (int i = 0; i < 10; i++) {
+            f.realize(1024, 1024, t);
+        }
+    } else {
+        // Skip this test if gpu target not enabled (it's pretty slow on a cpu).
+    }
+
+    printf("Success!\n");
+    return 0;
+}
