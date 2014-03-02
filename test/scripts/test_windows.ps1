6ee4fe87f10e681115200b2c0910a2035400b0f9
Andrew Adams <andrew.b.adams@gmail.com>
2014-03-01 22:42:13 -0800

    Added a distribution building script for windows
    Updates llvm and Halide, compiles, tests, and builds a distro. I'll set
    it up to run every night.
    
    Also updated the testing script to hit cuda

--- test/scripts/test_windows.ps1
+++ test/scripts/test_windows.ps1
@@ -1,3 +1,5 @@
+$env:HL_JIT_TARGET = "host"
+
 Get-ChildItem ../../build/bin/Release -filter correctness*.exe | ForEach { 
   echo ""
   echo $_.Fullname
@@ -9,6 +11,18 @@ Get-ChildItem ../../build/bin/Release -filter correctness*.exe | ForEach {
 }
 
 Get-ChildItem ../../build/bin/Release -filter performance*.exe | ForEach { 
+  echo ""
+  echo $_.Fullname
+  &$_.Fullname 
+  if ($LastExitCode) {
+    echo "Test failed!"
+    exit $LastExitCode
+  }
+}
+
+$env:HL_JIT_TARGET = "cuda"
+
+Get-ChildItem ../../build/bin/Release -filter correctness*.exe | ForEach { 
   echo ""
   echo $_.Fullname
   &$_.Fullname 
