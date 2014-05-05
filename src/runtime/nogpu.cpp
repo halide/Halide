49b73df9171f4b2c9188bd66f2f32c707313baca
Daniel Heck <dheck@google.com>
2014-05-05 11:47:44 +0200

    Fix signatures of GPU runtime functions in a few more places
    Former-commit-id: 3e56fb8c840700fb6ffef06071e4d27cbbbda848

--- src/runtime/nogpu.cpp
+++ src/runtime/nogpu.cpp
@@ -1,8 +1,9 @@
 // Architectures that do not distinguish between device and host
 // (i.e. not gpus), don't need a definition of copy_to_host
-
+#include "HalideRuntime.h"
 #include "mini_stdint.h"
 #include "../buffer_t.h"
 
-extern "C" WEAK void halide_copy_to_host(void *user_context, buffer_t *) {}
-
+extern "C" WEAK int halide_copy_to_host(void *user_context, buffer_t *) {
+    return 0;
+}
