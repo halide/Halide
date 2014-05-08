549e15b80d51b0f9056ae59ac502adf0a2c8bade
Fred Rotbart <rotbart@softbart.com>
2014-05-08 16:38:30 +0300

    Added missing libpng-config call in the example build command lines for OS X in tutorial07 and tutorial09

--- tutorial/lesson_07_multi_stage_pipelines.cpp
+++ tutorial/lesson_07_multi_stage_pipelines.cpp
@@ -7,7 +7,7 @@
 // LD_LIBRARY_PATH=../bin ./lesson_07
 
 // On os x:
-// g++ lesson_07*.cpp -g -I ../include -L ../bin -lHalide -o lesson_07
+// g++ lesson_07*.cpp -g -I ../include -L ../bin -lHalide `libpng-config --cflags --ldflags` -o lesson_07
 // DYLD_LIBRARY_PATH=../bin ./lesson_07
 
 #include <Halide.h>
