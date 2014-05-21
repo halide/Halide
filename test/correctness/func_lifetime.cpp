8d0b325ee348fe8cf4ef9c48f71e8d8a6fa9ef85
Dillon Sharlet <dsharlet@google.com>
2014-05-21 14:27:56 -0700

    Moved redundant validation code to a separate function

--- test/correctness/func_lifetime.cpp
+++ test/correctness/func_lifetime.cpp
@@ -4,6 +4,21 @@
 
 using namespace Halide;
 
+bool validate(const Image<float> &im, float add)
+{
+    // Check the result was what we expected
+    for (int i = 0; i < im.width(); i++) {
+        for (int j = 0; j < im.height(); j++) {
+            float correct = i*j + add;
+            if (fabs(im(i, j) - correct) > 0.001f) {
+                printf("im[%d, %d] = %f instead of %f\n", i, j, im(i, j), correct);
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 int main(int argc, char **argv) {
 
     Var x("x"), y("y");
@@ -11,7 +26,7 @@ int main(int argc, char **argv) {
 
     printf("Defining function f...\n");
 
-    f(x, y) = x*y + 2.4f;
+    f(x, y) = x*y + 1.0f;
 
     Target target = get_jit_target_from_environment();
     if (target.has_gpu_feature()) {
@@ -22,16 +37,8 @@ int main(int argc, char **argv) {
         printf("Realizing function f...\n");
 
         Image<float> imf = f.realize(32, 32, target);
-
-        // Check the result was what we expected
-        for (int i = 0; i < 32; i++) {
-            for (int j = 0; j < 32; j++) {
-                float correct = i*j + 2.4f;
-                if (fabs(imf(i, j) - correct) > 0.001f) {
-                    printf("imf[%d, %d] = %f instead of %f\n", i, j, imf(i, j), correct);
-                    return -1;
-                }
-            }
+        if (!validate(imf, 1.0f)) {
+            return -1;
         }
     }
 
@@ -41,7 +48,7 @@ int main(int argc, char **argv) {
 
         printf("Defining function g...\n");
 
-        g(x, y) = 2.0f*(x*y + 2.4f);
+        g(x, y) = x*y + 2.0f;
 
         if (target.has_gpu_feature()) {
             g.gpu_tile(x, y, 8, 8, GPU_Default);
@@ -50,32 +57,17 @@ int main(int argc, char **argv) {
         printf("Realizing function g...\n");
 
         Image<float> img = g.realize(32, 32, target);
-
-        // Check the result was what we expected
-        for (int i = 0; i < 32; i++) {
-            for (int j = 0; j < 32; j++) {
-                float correct = 2.0f*(i*j + 2.4f);
-                if (fabs(img(i, j) - correct) > 0.001f) {
-                    printf("imf[%d, %d] = %f instead of %f\n", i, j, img(i, j), correct);
-                    return -1;
-                }
-            }
+        if (!validate(img, 2.0f)) {
+            return -1;
         }
     }
 
+    // Try using f again to ensure it is still valid (after g's destruction).
     printf("Realizing function f again...\n");
 
     Image<float> imf2 = f.realize(32, 32, target);
-
-    // Check the result was what we expected
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            float correct = i*j + 2.4f;
-            if (fabs(imf2(i, j) - correct) > 0.001f) {
-                printf("imf2[%d, %d] = %f instead of %f\n", i, j, imf2(i, j), correct);
-                return -1;
-            }
-        }
+    if (!validate(imf2, 1.0f)) {
+        return -1;
     }
 
     printf("Success!\n");
