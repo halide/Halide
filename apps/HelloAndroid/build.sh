50e8a12ec766ad865792628c1a514377b05aa6ca
Daniel Heck <dheck@google.com>
2014-05-22 14:08:30 +0200

    Update HelloAndroid for more recent Android SDK
    Former-commit-id: 803c10e8c2b7f6257081433a35be99d85ee62a5d

--- apps/HelloAndroid/build.sh
+++ apps/HelloAndroid/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-android update project -p .
+android update project -p . --target android-17
 cd jni
 c++ halide.cpp -L ../../../bin -lHalide -I ../../../include -ldl -lpthread -lz
 HL_TARGET=arm-32-android DYLD_LIBRARY_PATH=../../../bin LD_LIBRARY_PATH=../../../bin ./a.out
