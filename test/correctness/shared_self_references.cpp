7ae0ace184120910c0c4dedeeb14051a6c3552e0
Andrew Adams <andrew.b.adams@gmail.com>
2014-05-22 10:19:37 -0700

    Bugfix for sharing a self-reference across multiple reduction definitions.
    Solved by making a unique copy of each self-reference for each definition.

---
+++ test/correctness/shared_self_references.cpp
@@ -0,0 +1,24 @@
+#include <Halide.h>
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    // Check that recursive references get tracked properly
+    {
+        Func f;
+        Var x;
+        f(x) = x;
+        {
+            Expr e = f(2);
+            f(0) = e;
+            f(1) = e;
+        } // Destroy e
+    } // Destroy f
+
+    // f should have been cleaned up. valgrind will complain if it
+    // hasn't been.
+
+    printf("Success!\n");
+    return 0;
+}
