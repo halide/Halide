f8762c753305c1a2eeb8c43615e87b5767e82242
Andrew Adams <andrew.b.adams@gmail.com>
2014-06-02 17:50:04 -0700

    Comment fixes.
    Former-commit-id: 358584d13bdd976db7f13994999322e0f8f02259

--- src/FuseGPUThreadLoops.h
+++ src/FuseGPUThreadLoops.h
@@ -2,7 +2,8 @@
 #define HALIDE_SYNCTHREADS_H
 
 /** \file
- * TODO
+ * Defines the lowering pass that fuses and normalizes loops over gpu
+ * threads to target OpenCL and CUDA.
  */
 
 #include "IR.h"
@@ -10,7 +11,12 @@
 namespace Halide {
 namespace Internal {
 
-/** TODO */
+/** Converts Halide's GPGPU IR to the OpenCL/CUDA model. Within every
+ * loop over gpu block indices, fuse the inner loops over thread
+ * indices into a single loop (with predication to turn off
+ * threads). Also injects synchronization points as needed, and hoists
+ * allocations at the block level out into a single shared memory
+ * array. */
 Stmt fuse_gpu_thread_loops(Stmt s);
 
 }
