a32047420fccc67a29875a81450a8f0aefbce4b0
Andrew Adams <andrew.b.adams@gmail.com>
2014-06-09 09:14:56 -0700

    Only test gpu targets with trunk
    Because llvm 3.4 and earlier duplicate syncthreads incorrectly
    and you get race conditions.
    
    
    Former-commit-id: 32460b77849ee125a1b42530321df9c0e9661936

--- test/scripts/test_all_targets.sh
+++ test/scripts/test_all_targets.sh
@@ -176,10 +176,10 @@ pwd
 for LLVM in ${LLVMS}; do
     if [[ "$LLVM" == pnacl ]]; then
         TARGETS="x86-32-sse41 x86-64-avx x86-32-nacl x86-32-sse41-nacl x86-64-nacl x86-64-sse41-nacl"
-    elif [[ "$LLVM" == release-3.2 ]]; then
-        TARGETS="x86-32 x86-32-sse41 x86-64 x86-64-sse41 x86-64-avx"
-    else
+    elif [[ "$LLVM" == trunk ]]; then
         TARGETS="x86-32 x86-32-sse41 x86-64 x86-64-sse41 x86-64-avx ptx opencl"
+    else
+        TARGETS="x86-32 x86-32-sse41 x86-64 x86-64-sse41 x86-64-avx"
     fi
 
     for TARGET in $TARGETS; do
