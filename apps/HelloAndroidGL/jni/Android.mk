d80dc327f76bcbc39df8a4d386d09e7d758e4356
Daniel Heck <dheck@google.com>
2014-06-11 17:04:24 +0200

    Added small sample app for testing OpenGL functionality on Android
    Former-commit-id: f60c19ff86eeb2a2d3a91f4947e8d04c257233b0

---
+++ apps/HelloAndroidGL/jni/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := native
+LOCAL_ARM_MODE  := arm
+LOCAL_SRC_FILES := native.cpp
+LOCAL_LDFLAGS   := -Ljni
+LOCAL_LDLIBS    := -lm -llog -ljnigraphics -landroid -lEGL -lGLESv2 jni/halide.o # -lOpenCL -lllvm-a3xx
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
---
+++ apps/HelloAndroidGL/jni/halide.h
@@ -0,0 +1,24 @@
+#ifndef HALIDE_halide
+#define HALIDE_halide
+#ifndef BUFFER_T_DEFINED
+#define BUFFER_T_DEFINED
+#include <stdint.h>
+typedef struct buffer_t {
+    uint64_t dev;
+    uint8_t* host;
+    int32_t extent[4];
+    int32_t stride[4];
+    int32_t min[4];
+    int32_t elem_size;
+    bool host_dirty;
+    bool dev_dirty;
+} buffer_t;
+#endif
+#ifndef HALIDE_FUNCTION_ATTRS
+#define HALIDE_FUNCTION_ATTRS
+#endif
+#ifdef __cplusplus
+extern "C"
+#endif
+int halide(buffer_t *_input, buffer_t *_f0) HALIDE_FUNCTION_ATTRS;
+#endif
