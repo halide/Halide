becf8762273aaf61be5afd40bdfd0edef3d224dd
Dillon Sharlet <dsharlet@google.com>
2014-06-11 17:32:46 -0700

    Added test that uses a constant generated Func (should map to constant memory in GPU targets).
    Former-commit-id: f7decc67b92cc5888a7c980c69e03b38ed699ac0

---
+++ test/correctness/convolution_generated_kernel.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <Halide.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+
+    //int W = 64*3, H = 64*3;
+    const int W = 16, H = 16;
+
+    Image<uint16_t> in(W, H);
+    for (int y = 0; y < H; y++) {
+        for (int x = 0; x < W; x++) {
+            in(x, y) = rand() & 0xff;
+        }
+    }
+
+
+    Var x("x"), y("y");
+
+    // The kernel in this test is just a simple box blur.
+    Func box;
+    box(x, y) = cast<uint16_t>(1);
+
+    Func input("input");
+    input(x, y) = in(clamp(x, 0, W-1), clamp(y, 0, H-1));
+    input.compute_root();
+
+    RDom r(0, 3, 0, 3);
+
+    /* This iterates over r outermost. I.e. the for loop looks like:
+     * for y:
+     *   for x:
+     *     blur1(x, y) = 0
+     * for r.y:
+     *   for r.x:
+     *     for y:
+     *       for x:
+     *         blur1(x, y) += tent(r.x, r.y) * input(x + r.x - 1, y + r.y - 1)
+     *
+     * In general, reductions iterate over the reduction domain outermost.
+     */
+    Func blur1("blur1");
+    blur1(x, y) += box(r.x, r.y) * input(x + r.x - 1, y + r.y - 1);
+
+
+    /* This uses an inline reduction, and is the more traditional way
+     * of scheduling a convolution. "sum" creates an anonymous
+     * reduction function that is computed within the for loop over x
+     * in blur2. blur2 isn't actually a reduction. The loop nest looks like:
+     * for y:
+     *   for x:
+     *     tmp = 0
+     *     for r.y:
+     *       for r.x:
+     *         tmp += tent(r.x, r.y) * input(x + r.x - 1, y + r.y - 1)
+     *     blur(x, y) = tmp
+     */
+    Func blur2("blur2");
+    blur2(x, y) = sum(box(r.x, r.y) * input(x + r.x - 1, y + r.y - 1));
+
+    box.compute_root();
+
+    Target target = get_jit_target_from_environment();
+    if (target.has_gpu_feature()) {
+        // Initialization (basically memset) done in a GPU kernel
+        blur1.gpu_tile(x, y, 16, 16, GPU_Default);
+
+        // Summation is done as an outermost loop is done on the cpu
+        blur1.update().gpu_tile(x, y, 16, 16, GPU_Default);
+
+        // Summation is done as a sequential loop within each gpu thread
+        blur2.gpu_tile(x, y, 16, 16, GPU_Default);
+    } else {
+        // Take this opportunity to test scheduling the pure dimensions in a reduction
+        Var xi("xi"), yi("yi");
+        blur1.tile(x, y, xi, yi, 6, 6);
+        blur1.update().tile(x, y, xi, yi, 4, 4).vectorize(xi).parallel(y);
+
+        blur2.vectorize(x, 4).parallel(y);
+    }
+
+    Image<uint16_t> out1 = blur1.realize(W, H, target);
+    Image<uint16_t> out2 = blur2.realize(W, H, target);
+
+    for (int y = 1; y < H-1; y++) {
+        for (int x = 1; x < W-1; x++) {
+            uint16_t correct = (1*in(x-1, y-1) + 1*in(x, y-1) + 1*in(x+1, y-1) +
+                                1*in(x-1, y)   + 1*in(x, y) +   1*in(x+1, y) +
+                                1*in(x-1, y+1) + 1*in(x, y+1) + 1*in(x+1, y+1));
+            if (out1(x, y) != correct) {
+                printf("out1(%d, %d) = %d instead of %d\n", x, y, out1(x, y), correct);
+                return -1;
+            }
+            if (out2(x, y) != correct) {
+                printf("out2(%d, %d) = %d instead of %d\n", x, y, out2(x, y), correct);
+                return -1;
+            }
+        }
+    }
+
+    printf("Success!\n");
+
+    return 0;
+
+}
