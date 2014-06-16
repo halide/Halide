9e1d16094d6a8b513b2dbf0856e29828bda12157
Dillon Sharlet <dsharlet@google.com>
2014-06-15 21:15:56 -0700

    Fixed test behavior when not running on GPU target.

--- test/correctness/gpu_object_lifetime.cpp
+++ test/correctness/gpu_object_lifetime.cpp
@@ -12,23 +12,23 @@ void halide_print(void *user_context, const char *str) {
 }
 
 int main(int argc, char *argv[]) {
+    Target target = get_jit_target_from_environment();
+    if (!target.has_gpu_feature()) {
+        printf("Not running test because no gpu target enabled\n");
+        return 0;
+    }
+    // We need gpu_debug to record object creation.
+    target.features |= Target::GPUDebug;
+
     for (int i = 0; i < 2; i++) {
         Var x;
-
         Func f;
         f(x) = x;
 
-        Target target = get_jit_target_from_environment();
-        if (target.has_gpu_feature()) {
-            f.gpu_tile(x, 32);
-        }
-
+        f.gpu_tile(x, 32);
         f.set_custom_print(halide_print);
-        // We need gpu_debug to record object creation.
-        target.features |= Target::GPUDebug;
 
         Image<int32_t> result = f.realize(256, target);
-
         for (int i = 0; i < 256; i++) {
             if (result(i) != i) {
                 std::cout << "Error! " << result(i) << " != " << i << std::endl;
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    int ret = validate_gpu_object_lifetime(true /* allow_globals */);
+    int ret = validate_gpu_object_lifetime(true /* allow_globals */, false /* allow_none */);
     if (ret != 0) {
         return ret;
     }
