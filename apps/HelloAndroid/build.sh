07a936e5891d168996cedded17ea00ad4835dc3b
Daniel Heck <dheck@google.com>
2014-06-17 14:22:07 +0200

    Revert unintended changes to HelloAndroid example

--- apps/HelloAndroid/build.sh
+++ apps/HelloAndroid/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-android update project -p . --target android-17
+android update project -p .
 cd jni
 c++ halide.cpp -L ../../../bin -lHalide -I ../../../include -ldl -lpthread -lz
 HL_TARGET=arm-32-android DYLD_LIBRARY_PATH=../../../bin LD_LIBRARY_PATH=../../../bin ./a.out
