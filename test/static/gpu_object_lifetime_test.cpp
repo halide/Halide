2b7fa6819e102af3e43780bd97078f692f5ca8bb
Dillon Sharlet <dsharlet@google.com>
2014-06-16 22:11:18 -0700

    Used dev_free helper instead of relying on destructor.
    Former-commit-id: 34aa7b5d9528a6bb0e6cc4072069f4074aa37256

--- test/static/gpu_object_lifetime_test.cpp
+++ test/static/gpu_object_lifetime_test.cpp
@@ -16,17 +16,17 @@ int main(int argc, char **argv) {
 
     // Run the whole program several times.
     for (int i = 0; i < 2; i++) {
-        {
-            Image<int> output(80);
+        Image<int> output(80);
 
-            func_gpu_object_lifetime(output);
+        func_gpu_object_lifetime(output);
 
-            output.copy_to_host();
-            for (int x = 0; x < output.width(); x++) {
-                if (output(x) != x) {
-                    printf("Error! %d != %d\n", output(x), x);
-                    return -1;
-                }
+        output.copy_to_host();
+        output.dev_free();
+
+        for (int x = 0; x < output.width(); x++) {
+            if (output(x) != x) {
+                printf("Error! %d != %d\n", output(x), x);
+                return -1;
             }
         }
 
