83fc84f5e24d9659d9fd0d63f93c31666fe2acf1
Daniel Heck <dheck@google.com>
2014-06-17 14:57:07 +0200

    Make halide_printf output to LOG_INFO, not LOG_FATAL
    Former-commit-id: 645fa2bb2a4a579a7d0fc799814c27da4b9757e4

--- src/runtime/android_io.cpp
+++ src/runtime/android_io.cpp
@@ -6,10 +6,12 @@ extern "C" {
 
 extern int __android_log_vprint(int, const char *, const char *, __builtin_va_list);
 
+#define ANDROID_LOG_INFO 4
+
 WEAK int halide_printf(void *user_context, const char * fmt, ...) {
     __builtin_va_list args;
     __builtin_va_start(args,fmt);
-    int result = __android_log_vprint(7, "halide", fmt, args);
+    int result = __android_log_vprint(ANDROID_LOG_INFO, "halide", fmt, args);
     __builtin_va_end(args);
     return result;
 }
