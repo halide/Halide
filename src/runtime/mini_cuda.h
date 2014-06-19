509703f2d3fc105cd9cf6418b0c34da6a6bf9b11
Dillon Sharlet <dsharlet@google.com>
2014-06-19 14:49:16 -0700

    Added declarations for cuMemcpy3D.

--- src/runtime/mini_cuda.h
+++ src/runtime/mini_cuda.h
@@ -11,6 +11,7 @@
 #define cuMemFree                           cuMemFree_v2
 #define cuMemcpyHtoD                        cuMemcpyHtoD_v2
 #define cuMemcpyDtoH                        cuMemcpyDtoH_v2
+#define cuMemcpy3D                          cuMemcpy3D_v2
 // API version >= 4000
 #define cuCtxDestroy                        cuCtxDestroy_v2
 #define cuCtxPopCurrent                     cuCtxPopCurrent_v2
@@ -30,6 +31,7 @@ typedef struct CUmod_st *CUmodule;                        /**< CUDA module */
 typedef struct CUfunc_st *CUfunction;                     /**< CUDA function */
 typedef struct CUstream_st *CUstream;                     /**< CUDA stream */
 typedef struct CUevent_st *CUevent;                       /**< CUDA event */
+typedef struct CUarray_st* CUarray;
 typedef enum {
     CUDA_SUCCESS                              = 0,
     CUDA_ERROR_INVALID_VALUE                  = 1,
@@ -76,6 +78,43 @@ typedef enum {
     CUDA_ERROR_UNKNOWN                        = 999
 } CUresult;
 
+typedef enum CUmemorytype_enum {
+    CU_MEMORYTYPE_HOST = 0x01,
+    CU_MEMORYTYPE_DEVICE = 0x02,
+    CU_MEMORYTYPE_ARRAY = 0x03,
+    CU_MEMORYTYPE_UNIFIED = 0x04
+} CUmemorytype;
+
+typedef struct CUDA_MEMCPY3D_st {
+    size_t srcXInBytes;         /**< Source X in bytes */
+    size_t srcY;                /**< Source Y */
+    size_t srcZ;                /**< Source Z */
+    size_t srcLOD;              /**< Source LOD */
+    CUmemorytype srcMemoryType; /**< Source memory type (host, device, array) */
+    const void *srcHost;        /**< Source host pointer */
+    CUdeviceptr srcDevice;      /**< Source device pointer */
+    CUarray srcArray;           /**< Source array reference */
+    void *reserved0;            /**< Must be NULL */
+    size_t srcPitch;            /**< Source pitch (ignored when src is array) */
+    size_t srcHeight;           /**< Source height (ignored when src is array; may be 0 if Depth==1) */
+
+    size_t dstXInBytes;         /**< Destination X in bytes */
+    size_t dstY;                /**< Destination Y */
+    size_t dstZ;                /**< Destination Z */
+    size_t dstLOD;              /**< Destination LOD */
+    CUmemorytype dstMemoryType; /**< Destination memory type (host, device, array) */
+    void *dstHost;              /**< Destination host pointer */
+    CUdeviceptr dstDevice;      /**< Destination device pointer */
+    CUarray dstArray;           /**< Destination array reference */
+    void *reserved1;            /**< Must be NULL */
+    size_t dstPitch;            /**< Destination pitch (ignored when dst is array) */
+    size_t dstHeight;           /**< Destination height (ignored when dst is array; may be 0 if Depth==1) */
+
+    size_t WidthInBytes;        /**< Width of 3D memory copy in bytes */
+    size_t Height;              /**< Height of 3D memory copy */
+    size_t Depth;               /**< Depth of 3D memory copy */
+} CUDA_MEMCPY3D;
+
 #define CU_POINTER_ATTRIBUTE_CONTEXT 1
 
 extern "C" {
@@ -85,6 +124,7 @@ CUresult CUDAAPI cuDeviceGetCount(int *count);
 CUresult CUDAAPI cuDeviceGet(CUdevice *device, int ordinal);
 CUresult CUDAAPI cuCtxCreate(CUcontext *pctx, unsigned int flags, CUdevice dev);
 CUresult CUDAAPI cuCtxDestroy(CUcontext pctx);
+CUresult CUDAAPI cuCtxGetApiVersion(CUcontext ctx, unsigned int *version);
 CUresult CUDAAPI cuModuleLoadData(CUmodule *module, const void *image);
 CUresult CUDAAPI cuModuleUnload(CUmodule module);
 CUresult CUDAAPI cuModuleGetFunction(CUfunction *hfunc, CUmodule hmod, const char *name);
@@ -92,6 +132,7 @@ CUresult CUDAAPI cuMemAlloc(CUdeviceptr *dptr, size_t bytesize);
 CUresult CUDAAPI cuMemFree(CUdeviceptr dptr);
 CUresult CUDAAPI cuMemcpyHtoD(CUdeviceptr dstDevice, const void *srcHost, size_t ByteCount);
 CUresult CUDAAPI cuMemcpyDtoH(void *dstHost, CUdeviceptr srcDevice, size_t ByteCount);
+CUresult CUDAAPI cuMemcpy3D(const CUDA_MEMCPY3D *pCopy);
 CUresult CUDAAPI cuLaunchKernel(CUfunction f,
                                 unsigned int gridDimX,
                                 unsigned int gridDimY,
