37dc284fabaac52c2127d5488c38394b05a99eaa
Andrew Adams <andrew.b.adams@gmail.com>
2014-06-23 10:29:54 -0700

    Fix non-contiguous copy test
    Was assuming that the gpu buffer didn't alias with the cpu buffer.
    
    
    Former-commit-id: 81617cd713b0de2debbf21f75bea330013c4ef61

--- test/correctness/gpu_non_contiguous_copy.cpp
+++ test/correctness/gpu_non_contiguous_copy.cpp
@@ -7,31 +7,50 @@ int main(int argc, char **argv) {
     Var x, y, z, w;
     Image<int> full(80, 60, 10, 10);
 
+    const int x_off = 4, y_off = 8, z_off = 2, w_off = 4;
+    const int x_size = 16, y_size = 16, z_size = 3, w_size = 3;
+
     buffer_t cropped = *full.raw_buffer();
-    cropped.host = (uint8_t *)&(full(4, 8, 2, 4));
+    cropped.host = (uint8_t *)&(full(x_off, y_off, z_off, w_off));
     cropped.min[0] = 0;
     cropped.min[1] = 0;
     cropped.min[2] = 0;
     cropped.min[3] = 0;
-    cropped.extent[0] = 16;
-    cropped.extent[1] = 16;
-    cropped.extent[2] = 3;
-    cropped.extent[3] = 3;
+    cropped.extent[0] = x_size;
+    cropped.extent[1] = y_size;
+    cropped.extent[2] = z_size;
+    cropped.extent[3] = w_size;
     cropped.stride[0] *= 2;
     cropped.stride[1] *= 2;
     cropped.stride[2] *= 2;
     cropped.stride[3] *= 2;
     Buffer out(Int(32), &cropped);
 
-    Func f;
+    // Make a bitmask representing the region inside the crop.
+    Image<bool> in_subregion(80, 60, 10, 10);
+    Expr test = ((x >= x_off) && (x < x_off + x_size*2) &&
+                 (y >= y_off) && (y < y_off + y_size*2) &&
+                 (z >= z_off) && (z < z_off + z_size*2) &&
+                 (w >= w_off) && (w < w_off + w_size*2) &&
+                 (x % 2 == 0) &&
+                 (y % 2 == 0) &&
+                 (z % 2 == 0) &&
+                 (w % 2 == 0));
+    Func test_func;
+    test_func(x, y, z, w) = test;
+    test_func.realize(in_subregion);
 
+    Func f;
     f(x, y, z, w) = 3*x + 2*y + z + 4*w;
     f.gpu_tile(x, y, 16, 16);
     f.output_buffer().set_stride(0, Expr());
     f.realize(out);
 
-    // Put some data in the full host buffer.
-    lambda(x, y, z, w, 4*x + 3*y + 2*z + w).realize(full);
+
+    // Put some data in the full host buffer, avoiding the region
+    // being evaluated above.
+    Expr change_out_of_subregion = select(test, undef<int>(), 4*x + 3*y + 2*z + w);
+    lambda(x, y, z, w, change_out_of_subregion).realize(full);
 
     // Copy back the output subset from the GPU.
     out.copy_to_host();
@@ -40,19 +59,15 @@ int main(int argc, char **argv) {
         for (int z = 0; z < full.extent(2); ++z) {
             for (int y = 0; y < full.extent(1); ++y) {
                 for (int x = 0; x < full.extent(0); ++x) {
-                    int correct = 4*x + 3*y + 2*z + w;
-
-                    int w_ = (w - 4)/2;
-                    int z_ = (z - 2)/2;
-                    int y_ = (y - 8)/2;
-                    int x_ = (x - 4)/2;
-
-                    if (cropped.min[3] <= w_ && w_ < cropped.min[3] + cropped.extent[3] &&
-                        cropped.min[2] <= z_ && z_ < cropped.min[2] + cropped.extent[2] &&
-                        cropped.min[1] <= y_ && y_ < cropped.min[1] + cropped.extent[1] &&
-                        cropped.min[0] <= x_ && x_ < cropped.min[0] + cropped.extent[0] &&
-                        x % 2 == 0 && y % 2 == 0 && z % 2 == 0 && w % 2 == 0) {
+                    int correct;
+                    if (in_subregion(x, y, z, w)) {
+                        int x_ = (x - x_off)/2;
+                        int y_ = (y - y_off)/2;
+                        int z_ = (z - z_off)/2;
+                        int w_ = (w - w_off)/2;
                         correct = 3*x_ + 2*y_ + z_ + 4*w_;
+                    } else {
+                        correct = 4*x + 3*y + 2*z + w;
                     }
                     if (full(x, y, z, w) != correct) {
                         printf("Error! Incorrect value %i != %i at %i, %i, %i, %i\n", full(x, y, z, w), correct, x, y, z, w);
