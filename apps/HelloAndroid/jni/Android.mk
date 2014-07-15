4008ffc4438d0b32c97771dc5bc5f7da3e41e59a
Andrew Adams <andrew.b.adams@gmail.com>
2014-07-15 10:48:53 -0700

    cherry-pick some cleanups from pull request #370
    Former-commit-id: 93d4b332f35acb8fdfa88e8297450cf70956486b

--- apps/HelloAndroid/jni/Android.mk
+++ apps/HelloAndroid/jni/Android.mk
@@ -6,8 +6,9 @@ LOCAL_MODULE    := native
 LOCAL_ARM_MODE  := arm
 LOCAL_SRC_FILES := native.cpp
 LOCAL_LDFLAGS   := -Ljni
-LOCAL_LDLIBS    := -lm -llog -landroid jni/halide.o # -lOpenCL -lllvm-a3xx
+LOCAL_LDLIBS    := -lm -llog -landroid jni/halide_generated.o # -lOpenCL -lllvm-a3xx
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../include
 
 include $(BUILD_SHARED_LIBRARY)
 
