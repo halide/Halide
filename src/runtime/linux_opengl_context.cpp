2051a43c8bb96eae5f973c7df16d3bd473da2d49
Daniel Heck <dheck@google.com>
2014-09-10 18:19:31 +0200

    Add support for running with OpenGL 3.2 core profile
    Right now, the runtime doesn't work with older OpenGL and ES anymore, so
    there's some backporting work still to be done...

--- src/runtime/linux_opengl_context.cpp
+++ src/runtime/linux_opengl_context.cpp
@@ -3,20 +3,71 @@
 
 extern "C" {
 
-extern void *glXGetProcAddressARB(const char *);
+typedef void *GLXContext;
+typedef void *GLXFBConfig;
+typedef int Bool;
+typedef void Display;
+
+typedef void (*__GLXextFuncPtr)(void);
+extern __GLXextFuncPtr glXGetProcAddressARB (const char *);
 extern void *XOpenDisplay(void *);
 extern int XDefaultScreen(void *);
 extern int glXQueryExtension(void *, void *, void *);
-extern void *glXCreateNewContext(void *, void *, int, void *, int);
+extern const char *glXQueryExtensionsString(Display *dpy, int screen);
+extern GLXContext glXCreateNewContext(void *, void *, int, void *, int);
 extern void **glXChooseFBConfig(void *, int, const int *, int *);
 extern unsigned long glXCreatePbuffer(void *, void *, const int *);
 extern int XFree(void *);
 extern int XSync(void *, int);
-extern void *glXGetCurrentContext();
+extern GLXContext glXGetCurrentContext();
 extern int glXMakeContextCurrent(void *, unsigned long, unsigned long, void *);
 
+
+#define GLX_RENDER_TYPE 0x8011
+#define GLX_RGBA_TYPE 0x8014
+#define GLX_RGBA_BIT 1
+#define GLX_RED_SIZE 8
+#define GLX_GREEN_SIZE 8
+#define GLX_BLUE_SIZE 8
+#define GLX_ALPHA_SIZE 8
+
+#define GLX_CONTEXT_MAJOR_VERSION_ARB 0x2091
+#define GLX_CONTEXT_MINOR_VERSION_ARB 0x2092
+typedef GLXContext (*glXCreateContextAttribsARBProc)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
+
+const char *strstr(const char *, const char *);
+
+}  // extern "C"
+
+namespace Halide { namespace Runtime { namespace Internal {
+
+// Helper to check for extension string presence. Adapted from:
+//   http://www.opengl.org/resources/features/OGLextensions/
+bool extension_supported(const char *extlist, const char *extension) {
+    // Extension names should not have spaces.
+    if (strchr(extension, ' ') != NULL || *extension == '\0')
+        return false;
+
+    const char *start = extlist;
+    while (const char *pos = strstr(start, extension)) {
+        const char *end = pos + strlen(extension);
+        // Ensure the found match is a full word, not a substring.
+        if ((pos == start || pos[-1] == ' ') &&
+            (*end == ' ' || *end == '\0')) {
+            return true;
+        }
+        start = end;
+    }
+    return false;
+}
+
+}}}  // namespace Halide::Runtime::Internal
+
+
+extern "C" {
+
 WEAK void *halide_opengl_get_proc_address(void *user_context, const char *name) {
-    return glXGetProcAddressARB(name);
+    return (void*)glXGetProcAddressARB(name);
 }
 
 // Initialize OpenGL
@@ -41,24 +92,64 @@ WEAK int halide_opengl_create_context(void *user_context) {
     int screen = XDefaultScreen(dpy);
 
     int attribs[] = {
-        0x8011 /* GLX_RENDER_TYPE */, 1 /* GLX_RGBA_BIT */,
-        8 /* GLX_RED_SIZE */, 8,
-        9 /* GLX_GREEN_SIZE */, 8,
-        10 /* GLX_BLUE_SIZE */, 8,
-        11 /* GLX_ALPHA_SIZE */, 8,
+        GLX_RENDER_TYPE, GLX_RGBA_BIT,
+        GLX_RED_SIZE, 8,
+        GLX_GREEN_SIZE, 8,
+        GLX_BLUE_SIZE, 8,
+        GLX_ALPHA_SIZE, 8,
         0
     };
     int num_configs = 0;
-    void** fb_config = glXChooseFBConfig(dpy, screen, attribs, &num_configs);
+    void** fbconfigs = glXChooseFBConfig(dpy, screen, attribs, &num_configs);
     if (!num_configs) {
         halide_error(user_context, "Could not get framebuffer config.\n");
         return -1;
     }
 
-    void *ctx = glXCreateNewContext(dpy, fb_config[0],
-                                    0x8014 /* GLX_RGBA_TYPE */,
-                                    NULL /* share list */, 1 /* direct */);
-    if (!ctx) {
+    void *fbconfig = fbconfigs[0];
+
+    const char *glxexts = glXQueryExtensionsString(dpy, screen);
+
+    // NOTE: It is not necessary to create or make current to a context before
+    // calling glXGetProcAddressARB
+    glXCreateContextAttribsARBProc glXCreateContextAttribsARB = 0;
+    glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)
+        glXGetProcAddressARB("glXCreateContextAttribsARB");
+
+    void *context = 0;
+
+    if (!extension_supported(glxexts, "GLX_ARB_create_context") ||
+        !glXCreateContextAttribsARB) {
+        context = glXCreateNewContext(dpy, fbconfig,
+                                      GLX_RGBA_TYPE,
+                                      NULL /* share list */, 1 /* direct */);
+    } else {
+        int desired_major_version = 3;
+        int desired_minor_version = 2;
+        // bool compatibility_profile = false;
+
+        int context_attribs[] = {
+            GLX_CONTEXT_MAJOR_VERSION_ARB, desired_major_version,
+            GLX_CONTEXT_MINOR_VERSION_ARB, desired_minor_version,
+            // TODO: compatibility?
+            0
+        };
+
+        context = glXCreateContextAttribsARB(dpy, fbconfig, NULL /* */, 1 /* direct */,
+                                             context_attribs);
+//        XSync(dpy, 0 /* ??? */);
+        if (context == 0) {
+            halide_printf(user_context, "Opening 3.0 context failed, trying compatibility context\n");
+            context_attribs[1] = 2;
+            context_attribs[3] = 0;
+            context = glXCreateContextAttribsARB(dpy, fbconfig, NULL /* */, 1 /* direct */,
+                                                 context_attribs);
+
+        }
+        halide_printf(user_context, "Opened %d.%d context\n", context_attribs[1], context_attribs[3]);
+    }
+
+    if (!context) {
         halide_error(user_context, "Could not create OpenGL context.\n");
         return -1;
     }
@@ -68,13 +159,13 @@ WEAK int halide_opengl_create_context(void *user_context) {
         0x8040 /* GLX_PBUFFER_HEIGHT */, 32,
         0
     };
-    unsigned long pbuffer = glXCreatePbuffer(dpy, fb_config[0], pbuffer_attribs);
+    unsigned long pbuffer = glXCreatePbuffer(dpy, fbconfig, pbuffer_attribs);
 
     // clean up:
-    XFree(fb_config);
+    XFree(fbconfigs);
     XSync(dpy, 0);
 
-    if (!glXMakeContextCurrent(dpy, pbuffer, pbuffer, ctx)) {
+    if (!glXMakeContextCurrent(dpy, pbuffer, pbuffer, context)) {
         halide_error(user_context, "Could not make context current.\n");
         return -1;
     }
