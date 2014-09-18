d4f4949f9275f30a57f1368af2fe31692228f162
Andrew Adams <andrew.b.adams@gmail.com>
2014-09-18 09:36:58 -0700

    Revert change to test scripts that was deleting all but one distrib

--- test/scripts/test_target.sh
+++ test/scripts/test_target.sh
@@ -37,7 +37,6 @@ echo Testing target $HL_TARGET with llvm $LLVM
 echo Using LD = $LD
 echo Using CC = $CC
 echo Using CXX = $CXX
-rm -rf distrib/*
 make clean &&
 make -j8 build_tests || exit 1
 make distrib || exit 1
