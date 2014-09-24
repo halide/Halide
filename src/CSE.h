e2ad56690ee0f8a6023116a63e4b8481f7021c22
Andrew Adams <andrew.b.adams@gmail.com>
2014-09-23 19:03:50 -0700

    Rewrite of common-subexpression elimination.
    Produces much saner final IR, in approximately the same amount of time.
    It was necessary to add an optional cache to IR comparisons to keep track of
    known-equal Exprs.
    
    Things tried that didn't help:
    - Adding hash codes to all IR nodes to speed up deep compares.
    - Caching results of IR comparisons that return not-equal.
    
    Except in pathological cases, comparing unequal IR nodes terminates
    very quickly, so the caching in those cases adds pointless overhead.
    However, equal IR nodes do a tree traversal of what might be a graph,
    so you get exponential runtime if you don't do some caching.
    
    
    Former-commit-id: 102bf47ea0d8d92a3795d79edc28f85054adf328

--- src/CSE.h
+++ src/CSE.h
@@ -18,22 +18,13 @@ namespace Internal {
  * considered as a graph, but combinatorially large when considered as
  * a tree. For an example of a such a case, see
  * test/code_explosion.cpp */
-
-Expr common_subexpression_elimination(Expr);
+EXPORT Expr common_subexpression_elimination(Expr);
 
 /** Do common-subexpression-elimination on each expression in a
  * statement. Does not introduce let statements. */
-Stmt common_subexpression_elimination(Stmt);
+EXPORT Stmt common_subexpression_elimination(Stmt);
 
-/** Remove all lets from a statement or expression by substituting
- * them in. All sub-expressions will exist once in memory, but may
- * have many pointers to them, so this doesn't cause a combinatorial
- * explosion. If you walk over this as if it were a tree, however,
- * you're going to have a bad time. */
-// @{
-Expr remove_lets(Expr);
-Stmt remove_lets(Stmt);
-// @}
+EXPORT void cse_test();
 
 }
 }
