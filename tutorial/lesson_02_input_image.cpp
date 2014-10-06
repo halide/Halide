1f5085d154d100b7ee8480b8734896c69b8d8173
Andrew Adams <andrew.b.adams@gmail.com>
2014-10-06 09:27:42 -0700

    typo fix
    Former-commit-id: 9b687044b0314617b9ded0344ed6e13107f6e876

--- tutorial/lesson_02_input_image.cpp
+++ tutorial/lesson_02_input_image.cpp
@@ -5,7 +5,7 @@
 // This lesson can be built by invoking the command:
 //    make tutorial_lesson_02_input_image
 // in a shell with the current directory at the top of the halide source tree.
-// Otherwise, see the platform specific compiler involcations below.
+// Otherwise, see the platform-specific compiler invocations below.
 
 // On linux, you can compile and run it like so:
 // g++ lesson_02*.cpp -g -I ../include -L ../bin -lHalide `libpng-config --cflags --ldflags` -lpthread -ldl -o lesson_02
--- tutorial/lesson_04_debugging_2.cpp
+++ tutorial/lesson_04_debugging_2.cpp
@@ -5,7 +5,7 @@
 // This lesson can be built by invoking the command:
 //    make tutorial_lesson_04_debugging_2
 // in a shell with the current directory at the top of the halide source tree.
-// Otherwise, see the platform specific compiler involcations below.
+// Otherwise, see the platform-specific compiler invocations below.
 
 // On linux, you can compile and run it like so:
 // g++ lesson_04*.cpp -g -I ../include -L ../bin -lHalide -lpthread -ldl -o lesson_04
