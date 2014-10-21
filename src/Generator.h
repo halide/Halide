63ce19972548cbca3501108682782db97a38bc79
Steven Johnson <steven@crinklink.com>
2014-10-21 10:07:15 -0700

    Generator::natural_vector_size() wraps Target::natural_vector_size()
    Former-commit-id: 3c768d94ace86c466a96e6dcbd82a03190c16ca2

--- src/Generator.h
+++ src/Generator.h
@@ -370,17 +370,7 @@ public:
      * for that data type when compiling for the current target. */
     template <typename data_t>
     int natural_vector_size() const {
-        const bool is_avx2 = get_target().has_feature(Halide::Target::AVX2);
-        const bool is_avx = get_target().has_feature(Halide::Target::AVX) && !is_avx2;
-        const bool is_integer = std::numeric_limits<data_t>::is_integer;
-
-        // AVX has 256-bit SIMD registers, other existing targets have 128-bit ones.
-        // However, AVX has a very limited complement of integer instructions;
-        // restricting us to SSE4.1 size for integer operations produces much
-        // better performance. (AVX2 does have good integer operations for 256-bit
-        // registers.)
-        int vector_byte_size = (is_avx2 || (is_avx && !is_integer)) ? 32 : 16;
-        return vector_byte_size / static_cast<int>(sizeof(data_t));
+        return get_target().natural_vector_size<data_t>();
     }
 
     // Call build() and produce compiled output of the given func.
