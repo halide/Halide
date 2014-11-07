9c1119ce7b148e5e4603ac26bdfe12d23547b1aa
Kree Cole-McLaughlin <kree@google.com>
2014-11-07 13:46:12 -0800

    Added an optional scope argument to expr_uses_var(s). This allows the user to pass in a scope of exprs bound to variables, e.g. from let expressions. The visitor will dive into the bound expressions, whenever a variable in scope is encountered.
    Former-commit-id: b129b84347f8459c085c99d491e46a031a8cfe91

--- src/ExprUsesVar.cpp
+++ src/ExprUsesVar.cpp
@@ -6,25 +6,35 @@ namespace Internal {
 using std::string;
 
 namespace {
-class ExprUsesVar : public IRVisitor {
-    using IRVisitor::visit;
+class ExprUsesVar : public IRGraphVisitor {
+    using IRGraphVisitor::visit;
 
     const string &var;
+    Scope<Expr> scope;
 
     void visit(const Variable *v) {
         if (v->name == var) {
             result = true;
+        } else if (scope.contains(v->name)) {
+            include(scope.get(v->name));
         }
     }
 public:
-    ExprUsesVar(const string &v) : var(v), result(false) {}
+  ExprUsesVar(const string &v, const Scope<Expr> *s = NULL) : var(v), result(false) {
+        scope.set_containing_scope(s);
+    }
     bool result;
 };
-
 }
 
 bool expr_uses_var(Expr e, const string &v) {
     ExprUsesVar uses(v);
+    e.accept(&uses);
+    return uses.result;
+}
+
+bool expr_uses_var(Expr e, const string &v, const Scope<Expr> &s) {
+    ExprUsesVar uses(v, &s);
     e.accept(&uses);
     return uses.result;
 }
