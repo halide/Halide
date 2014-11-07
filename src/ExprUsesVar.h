b129b84347f8459c085c99d491e46a031a8cfe91
Kree Cole-McLaughlin <kree@google.com>
2014-11-07 13:46:12 -0800

    Added an optional scope argument to expr_uses_var(s). This allows the user to pass in a scope of exprs bound to variables, e.g. from let expressions. The visitor will dive into the bound expressions, whenever a variable in scope is encountered.

--- src/ExprUsesVar.h
+++ src/ExprUsesVar.h
@@ -13,27 +13,40 @@ namespace Internal {
 
 /** Test if an expression references the given variable. */
 bool expr_uses_var(Expr e, const std::string &v);
+bool expr_uses_var(Expr e, const std::string &v, const Scope<Expr> &s);
 
 template<typename T>
-class ExprUsesVars : public IRVisitor {
-    using IRVisitor::visit;
+class ExprUsesVars : public IRGraphVisitor {
+    using IRGraphVisitor::visit;
 
-    const Scope<T> &scope;
+    const Scope<T> &vars;
+    Scope<Expr> scope;
 
     void visit(const Variable *v) {
-        if (scope.contains(v->name)) {
+        if (vars.contains(v->name)) {
             result = true;
+        } else if (scope.contains(v->name)) {
+            include(scope.get(v->name));
         }
     }
 public:
-    ExprUsesVars(const Scope<T> &s) : scope(s), result(false) {}
+    ExprUsesVars(const Scope<T> &v, const Scope<Expr> *s = NULL) : vars(v), result(false) {
+        scope.set_containing_scope(s);
+    }
     bool result;
 };
 
 /** Test if an expression references any of the variables in a scope. */
 template<typename T>
-inline bool expr_uses_vars(Expr e, const Scope<T> &s) {
-    ExprUsesVars<T> uses(s);
+inline bool expr_uses_vars(Expr e, const Scope<T> &v) {
+    ExprUsesVars<T> uses(v);
+    e.accept(&uses);
+    return uses.result;
+}
+
+template<typename T>
+inline bool expr_uses_vars(Expr e, const Scope<T> &v, const Scope<Expr> &s) {
+    ExprUsesVars<T> uses(v, &s);
     e.accept(&uses);
     return uses.result;
 }
