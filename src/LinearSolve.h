7fe79e8a32951cd8e3aa34efdfbd554855f653ee
Kree Cole-McLaughlin <kree@google.com>
2014-11-11 15:29:57 -0800

    Added a function expr_is_linear_in_vars to LinearSolve. This is useful for detecting expressions that we could have a reasonable chance of solving.

--- src/LinearSolve.h
+++ src/LinearSolve.h
@@ -1,8 +1,8 @@
-#ifndef HALIDE_SOLVE_H
-#define HALIDE_SOLVE_H
+#ifndef HALIDE_LINEAR_SOLVE_H
+#define HALIDE_LINEAR_SOLVE_H
 
 /** \file
- * Defines a lowering pass that simplifies code using clamped ramps.
+ * Defines a method for solving linear equations for a given variable.
  */
 
 #include "IR.h"
@@ -12,7 +12,22 @@
 namespace Halide {
 namespace Internal {
 
-/** A struct that represents a simple term in an expression. The term
+/**
+ * Returns true if the input Expr is linear in the named variable, or in any of the free
+ * variables contained in the first scope argument. We say that an expression is linear
+ * if at least one of the variables appear in the expression and at most one free variable
+ * appears in each linear term. So expressions constant in the variables are not considered
+ * linear. The second scope argument contains variables that are bound to linear expressions
+ * in the free variables.
+ */
+// @{
+bool expr_is_linear_in_var(Expr expr, const std::string &var);
+bool expr_is_linear_in_vars(Expr expr, const Scope<int> &free_vars);
+bool expr_is_linear_in_vars(Expr expr, const Scope<int> &free_vars, const Scope<bool> &bound_vars);
+// @}
+
+/**
+ * A struct that represents a simple term in an expression. The term
  * is defined as the coefficient times the variable. If the variable
  * is a null pointer, then the term is a constant value equal to the
  * coefficient.
@@ -22,7 +37,8 @@ struct Term {
   const Variable* var;
 };
 
-/** This function collects the terms of a linear expression into an
+/**
+ * This function collects the terms of a linear expression into an
  * array. The function will return true if the expression is indeed
  * linear, and so this can be used to detect linear expressions as
  * well.
@@ -35,7 +51,9 @@ EXPORT bool collect_linear_terms(Expr e, std::vector<Term> &terms,
                                  const Scope<int> &free_vars,
                                  const Scope<Expr> &scope);
 // @}
-/* This function solves a conditional expression made up of linear
+
+/**
+ * This function solves a conditional expression made up of linear
  * expressions for a particular variable. If the expression contains
  * logical conjunctives, then each proposition is solved
  * independently. It returns the solved expression if it succeeds,
