4c1d5f2704ae3da88fdbc7a175e93d13373d6811
Zalman Stern <zalman@google.com>
2014-11-23 03:13:37 -0800

    Remove dependency on CodeGen_LLVM via function_takes_user_context, which doesn't actulally need LLVM at all.
    Former-commit-id: 99df2a701dcbc497556ebdbb7529fd6f54822645

--- src/CodeGen_LLVM.cpp
+++ src/CodeGen_LLVM.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 
 #include "IRPrinter.h"
+#include "CodeGen.h"
 #include "CodeGen_LLVM.h"
 #include "IROperator.h"
 #include "Debug.h"
@@ -1481,42 +1482,6 @@ Expr unbroadcast(Expr e) {
     } else {
         return e;
     }
-}
-
-// Returns true if the given function name is one of the Halide runtime
-// functions that takes a user_context pointer as its first parameter.
-bool CodeGen_LLVM::function_takes_user_context(const string &name) {
-    static const char *user_context_runtime_funcs[] = {
-        "halide_copy_to_host",
-        "halide_copy_to_dev",
-        "halide_current_time_ns",
-        "halide_debug_to_file",
-        "halide_dev_free",
-        "halide_dev_malloc",
-        "halide_dev_run",
-        "halide_dev_sync",
-        "halide_do_par_for",
-        "halide_do_task",
-        "halide_error",
-        "halide_free",
-        "halide_init_kernels",
-        "halide_malloc",
-        "halide_print",
-        "halide_profiling_timer",
-        "halide_release",
-        "halide_start_clock",
-        "halide_trace",
-        "halide_memoization_cache_lookup",
-        "halide_memoization_cache_store"
-    };
-    const int num_funcs = sizeof(user_context_runtime_funcs) /
-        sizeof(user_context_runtime_funcs[0]);
-    for (int i = 0; i < num_funcs; ++i) {
-        if (name == user_context_runtime_funcs[i]) {
-            return true;
-        }
-    }
-    return false;
 }
 
 Value *CodeGen_LLVM::interleave_vectors(Type type, const std::vector<Expr>& vecs) {
