244b3226d2c6a78d30b2c804c369cf436298d050
Andrew Adams <andrew.b.adams@gmail.com>
2014-12-15 14:13:23 -0800

    Dead code elimination.

--- src/RemoveDeadAllocations.h
+++ src/RemoveDeadAllocations.h
@@ -2,7 +2,8 @@
 #define HALIDE_REMOVE_DEAD_ALLOCATIONS_H
 
 /** \file
- * Defines the lowering pass that removes allocate and free nodes that are not used.
+ * Defines the lowering pass that removes allocate and free nodes that
+ * are not used.
  */
 
 #include "IR.h"
@@ -10,8 +11,9 @@
 namespace Halide {
 namespace Internal {
 
-/** Find Allocate/Free pairs that are never loaded from or stored to, and remove them from the Stmt. This
- *  doesn't touch Realize/Call nodes and so must be called after storage_flattening.
+/** Find Allocate/Free pairs that are never loaded from or stored to,
+ *  and remove them from the Stmt. This doesn't touch Realize/Call
+ *  nodes and so must be called after storage_flattening.
  */
 Stmt remove_dead_allocations(Stmt s);
 
