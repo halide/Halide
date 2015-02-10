c0bd52ded366730a3a617d90084a3250a34f90fa
Kree Cole-McLaughlin <kree@google.com>
2015-02-10 14:09:49 -0800

    Improved the performance of the L2 algorithms, speed is now on par with optimized BLAS libs for these subroutines.

--- apps/linear_algebra/src/blas_l2_generators.cpp
+++ apps/linear_algebra/src/blas_l2_generators.cpp
@@ -16,10 +16,9 @@ class GEMVGenerator :
     using Base::natural_vector_size;
 
     GeneratorParam<bool> assertions_enabled_ = {"assertions_enabled", false};
-    GeneratorParam<bool> use_fma_ = {"use_fma", false};
     GeneratorParam<bool> vectorize_ = {"vectorize", true};
     GeneratorParam<bool> parallel_ = {"parallel", true};
-    GeneratorParam<int>  block_size_ = {"block_size", 1 << 5};
+    GeneratorParam<int>  block_size_ = {"block_size", 1 << 8};
     GeneratorParam<bool> transpose_ = {"transpose", false};
 
     // Standard ordering of parameters in GEMV functions.
@@ -35,55 +34,78 @@ class GEMVGenerator :
                        .with_feature(Target::NoAsserts)
                        .with_feature(Target::NoBoundsQuery));
         }
-
-        if (use_fma_) {
-            target.set(get_target().with_feature(Target::FMA));
-        }
     }
 
     Func build() {
         SetupTarget();
 
         const int vec_size = vectorize_? natural_vector_size(type_of<T>()): 1;
-        const Expr num_rows = A_.width();
-        const Expr num_cols = A_.height();
+        const int unroll_size = 4;
 
         Var i("i"), j("j");
         Func result("result");
 
-        Func A;
-        A(i, j) = select(j < num_cols, A_(i, clamp(j, 0, num_cols)), cast<T>(0));
-
         if (transpose_) {
-            const Expr proxy_size = (num_cols + vec_size - 1) / vec_size;
+            const Expr size = A_.height();
+            const Expr sum_size = A_.width();
+            const Expr sum_size_vecs = sum_size / vec_size;
 
-            RDom k(0, proxy_size, "k");
-            Func Ax("Ax");
-            Ax(j, i) += A_(k*vec_size + j, i) * x_(k*vec_size + j);
-
-            Func At("At");
-            At(i, j) = Ax(j, i);
-
-            RDom sum_lanes(0, vec_size);
             Func prod("prod");
-            prod(i)  += At(i, sum_lanes);
-            result(i) = b_ * y_(i) + a_ * prod(i);
+            prod(j, i) = A_(j, i) * x_(j);
+
+            RDom k(0, sum_size_vecs, "k");
+            Func accum_vecs("accum_vecs");
+            accum_vecs(j, i) += prod(k * vec_size + j, i);
+
+            Func accum_vecs_transpose("accum_vecs_transpose");
+            accum_vecs_transpose(i, j) = accum_vecs(j, i);
+
+            RDom lanes(0, vec_size);
+            Func sum_lanes("sum_lanes");
+            sum_lanes(i) += accum_vecs_transpose(i, lanes);
+
+            RDom tail(sum_size_vecs * vec_size, sum_size - sum_size_vecs * vec_size);
+            Func sum_tail("sum_tail");
+            sum_tail(i) = sum_lanes(i);
+            sum_tail(i) += prod(tail, i);
+
+            Func Ax("Ax");
+            Ax(i) = sum_tail(i);
+            result(i) = b_ * y_(i) + a_ * Ax(i);
+
+            Var ii("ii"), t("t");
+            result.specialize((sum_size / vec_size) * vec_size == sum_size)
+                    .specialize(size >= unroll_size).vectorize(i, unroll_size)
+                    .specialize(size >= block_size_)
+                    .split(i, t, i, block_size_ / unroll_size).parallel(t);
+
+            result
+                    .specialize(size >= unroll_size).vectorize(i, unroll_size)
+                    .specialize(size >= block_size_)
+                    .split(i, t, i, block_size_ / unroll_size).parallel(t);
+
+            accum_vecs
+                    .compute_at(result, i).unroll(i).unroll(j)
+                    .update().reorder(i, j, k).unroll(i).unroll(j);
+            accum_vecs_transpose
+                    .compute_at(result, i).unroll(i).unroll(j);
+            sum_lanes
+                    .compute_at(result, i).update().unroll(lanes);
+            sum_tail
+                    .compute_at(result, i)
+                    .update().reorder(i, tail);//.unroll(i);
+
 
             if (vectorize_) {
-                result.specialize(num_rows >= vec_size).vectorize(i, vec_size);
+                accum_vecs.vectorize(j)
+                        .update().vectorize(j);
+                accum_vecs_transpose.vectorize(j);
 
-                prod.compute_at(result, i);
-                prod.vectorize(i, vec_size).unroll(i);
-                prod.update(0).specialize(num_rows >= vec_size)
-                    .reorder(i, sum_lanes).unroll(sum_lanes)
-                    .vectorize(i, vec_size).unroll(i);
+                sum_lanes.specialize(size >= vec_size).vectorize(i, vec_size);//.unroll(i);
+                sum_lanes.update().specialize(size >= vec_size).vectorize(i, vec_size);//.unroll(i);
 
-                Ax.compute_at(result, i).vectorize(j).unroll(i);
-                Ax.update(0).specialize(num_rows >= vec_size)
-                    .reorder(i, j, k)
-                    .vectorize(j).unroll(i);;
-
-                At.compute_at(result, i).vectorize(i, vec_size).unroll(j);
+                sum_tail.specialize(size >= vec_size).vectorize(i, vec_size);//.unroll(i);
+                sum_tail.update().specialize(size >= vec_size).vectorize(i, vec_size);//.unroll(i);
             }
 
             A_.set_min(0, 0).set_min(1, 0);
@@ -91,27 +113,43 @@ class GEMVGenerator :
             y_.set_bounds(0, 0, A_.height());
             result.output_buffer().set_bounds(0, 0, A_.height());
         } else {
-            const int block_size = 4;
-            const Expr proxy_size = ((num_cols + block_size - 1) / block_size) * block_size;
+            const Expr size = A_.width();
+            const Expr sum_size = A_.height();
+            const Expr sum_size_cols = (sum_size / unroll_size) * unroll_size;
+            const Expr tail_size = sum_size - sum_size_cols;
 
-            RDom k(0, proxy_size, "k");
-            result(i)  = b_ * y_(i);
-            result(i) += a_ * A_(i, k) * x_(k);
+            RDom k(0, sum_size_cols, "k");
+            RDom tail(sum_size_cols, tail_size, "tail");
+            Func block("block");
+            block(i)  = b_ * y_(i);
+            block(i) += a_ * A_(i, k) * x_(k);
+            block(i) += a_ * A_(i, tail) * x_(tail);
+            result(i) = block(i);
 
-            if (vectorize_) {
+            RVar ki("ki");
+            Var ii("ii");
+            result.specialize(tail_size == 0)
+                    .specialize(size >= vec_size).vectorize(i, vec_size)
+                    .specialize(size >= unroll_size * vec_size).unroll(i, unroll_size)
+                    .specialize(size >= block_size_)
+                    .split(i, i, ii, block_size_ / (unroll_size * vec_size)).parallel(i);
 
-                RVar ki("ki");
-                Var ii("ii");
-                result.update(0).specialize(num_rows >= vec_size && num_cols >= block_size)
+            result.specialize(size >= vec_size).vectorize(i, vec_size)
+                    .specialize(size >= unroll_size * vec_size).unroll(i, unroll_size)
+                    .specialize(size >= block_size_)
+                    .split(i, i, ii, block_size_ / (unroll_size * vec_size)).parallel(i);
+
+            block.compute_at(result, i);
+            block.specialize(size >= vec_size).vectorize(i, vec_size);
+            block.update().specialize(size >= vec_size && sum_size >= unroll_size)
                     .split(i, i, ii, vec_size)
-                    .split(k, k, ki, block_size)
+                    .split(k, k, ki, unroll_size)
                     .reorder(ii, ki, i, k)
                     .vectorize(ii).unroll(ki);
-
-                result.specialize(num_rows >= vec_size).vectorize(i, vec_size);
-                result.update(0).specialize(num_rows >= vec_size).vectorize(i, vec_size);
-            }
-
+            block.update().specialize(size >= vec_size).vectorize(i, vec_size);
+            block.update(1).reorder(i, tail)
+                    .specialize(size >= vec_size).vectorize(i, vec_size)
+                    .specialize(sum_size >= unroll_size).unroll(i, unroll_size);
 
             A_.set_min(0, 0).set_min(1, 0);
             x_.set_bounds(0, 0, A_.height());
