d15a535cfe119daa16c25636570580d54bd383e2
Steven Johnson <steven@crinklink.com>
2015-02-17 18:31:44 -0800

    _jit_wrapper -> _argv
    Former-commit-id: bbfbc105012fb9e629dab496dfda73fff09dfef3

---
+++ test/generator/argvcall_aottest.cpp
@@ -0,0 +1,53 @@
+#include "HalideRuntime.h"
+
+#include <math.h>
+#include <stdio.h>
+
+#include "argvcall.h"
+#include "static_image.h"
+
+const int kSize = 32;
+
+void verify(const Image<int32_t> &img, float f1, float f2) {
+    for (int i = 0; i < kSize; i++) {
+        for (int j = 0; j < kSize; j++) {
+            for (int c = 0; c < 3; c++) {
+                int expected = (int32_t)(c * (i > j ? i : j) * f1 / f2);
+                if (img(i, j, c) != expected) {
+                    printf("img[%d, %d, %d] = %d (expected %d)\n", i, j, c, img(i, j, c), expected);
+                    exit(-1);
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+
+  int result;
+  Image<int32_t> output(kSize, kSize, 3);
+
+  // We can, of course, pass whatever values for Param/ImageParam that we like.
+  result = argvcall(1.2f, 3.4f, output);
+  if (result != 0) {
+    fprintf(stderr, "Result: %d\n", result);
+    exit(-1);
+  }
+  verify(output, 1.2f, 3.4f);
+
+  // verify that calling via the _argv entry point
+  // also produces the correct result
+  float arg0 = 1.234f;
+  float arg1 = 3.456f;
+  buffer_t arg2 = *output;
+  void* args[3] = { &arg0, &arg1, &arg2 };
+  result = argvcall_argv(args);
+  if (result != 0) {
+    fprintf(stderr, "Result: %d\n", result);
+    exit(-1);
+  }
+  verify(output, arg0, arg1);
+
+  printf("Success!\n");
+  return 0;
+}
