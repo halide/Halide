643c1b0c22731d124c07669d92236a9d31608912
Pranav Bhandarkar <pranavb@codeaurora.org>
2015-02-24 15:46:29 -0600

    [18496] - Implement a testing system for running unit tests.
    This patch implements a system that uses a combination of a shell
    script and lit/FileCheck to allow us to run small unit tests.
    The shell script reads a 'testlist' file and compiles the tests
    mentioned in that file using g++. Then lit and FileCheck are used
    to run the executables so created - which are cross compilers
    themselves, in one sense. The idea is that with every visit function
    added, we'll create a corresponding test that will test the
    generation of the command intended to be generated using the visitor
    function.
    
    Change-Id: I6596d26badc70dce6301339880c56584aab57dee
    Reviewed-on: https://review-hexagon.quicinc.com/27238
    Tested-by: Pranav Bhandarkar <pranavb@codeaurora.org>
    Reviewed-by: Pranav Bhandarkar <pranavb@codeaurora.org>

--- test/hexagon/varith.cpp
+++ test/hexagon/varith.cpp
@@ -3,7 +3,11 @@
 #include <stdio.h>
 using namespace Halide;
 
+// RUN: ./varith.out | FileCheck %s
+#define COMPILE(X)  ((X).compile_to_assembly("/dev/stdout", args, target))
 
+//CHECK: Subt,
+//CHECK: vsub
 void testSub(Target& target) {
   Halide::Var x("x"), y("y");
   ImageParam inputOne (type_of<int8_t>(), 2);
@@ -16,10 +20,12 @@ void testSub(Target& target) {
   std::vector<Argument> args(2);
   args[0]  = inputOne;
   args[1] = inputTwo;
-  Subt.compile_to_bitcode("vsubb.bc", args, target);
-  Subt.compile_to_assembly("vsubb.s", args, target);
+  //  Subt.compile_to_bitcode("vsubb.bc", args, target);
+  //  Subt.compile_to_assembly("/dev/stdout", args, target);
+  COMPILE(Subt);
 }
-
+//CHECK: Addb,
+//CHECK: vadd
 void testAdd(Target& target) {
   Halide::Var x("x"), y("y");
   ImageParam inputOne (type_of<int8_t>(), 2);
@@ -32,23 +38,26 @@ void testAdd(Target& target) {
   std::vector<Argument> args(2);
   args[0]  = inputOne;
   args[1] = inputTwo;
-  Addb.compile_to_bitcode("vaddb.bc", args, target);
-  Addb.compile_to_assembly("vaddb.s", args, target);
+  //  Addb.compile_to_bitcode("vaddb.bc", args, target);
+  COMPILE(Addb);
 }
+
+//CHECK: Avgb,
+//CHECK: vavg
 void testAvg(Target& target) {
   Halide::Var x("x"), y("y");
   ImageParam inputOne (type_of<uint8_t>(), 2);
   ImageParam inputTwo (type_of<uint8_t>(), 2);
-  Halide::Func Addb;
-  Addb (x, y) = (inputOne(x, y) + inputTwo(x, y))/2;
+  Halide::Func Avgb;
+  Avgb (x, y) = (inputOne(x, y) + inputTwo(x, y))/2;
   Var x_outer, x_inner;
-  Addb.split(x, x_outer, x_inner, 64);
-  Addb.vectorize(x_inner);
+  Avgb.split(x, x_outer, x_inner, 64);
+  Avgb.vectorize(x_inner);
   std::vector<Argument> args(2);
   args[0]  = inputOne;
   args[1] = inputTwo;
-  Addb.compile_to_bitcode("vavgub.bc", args, target);
-  Addb.compile_to_assembly("vavgub.s", args, target);
+  //  Avgb.compile_to_bitcode("vavgub.bc", args, target);
+  COMPILE(Avgb);
 }
 int main(int argc, char **argv) {
   Target target;
