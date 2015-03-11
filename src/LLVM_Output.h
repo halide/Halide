8d9d3f9bb0008e8016c05dde3e0f15e213a2dfbb
Dillon Sharlet <dsharlet@google.com>
2015-03-10 20:33:50 -0700

    Renamed output_ to compile_...
    Former-commit-id: 407b81af2277edb894c89b8cf7c065b1a10432d8

--- src/LLVM_Output.h
+++ src/LLVM_Output.h
@@ -20,24 +20,24 @@ EXPORT void get_target_options(const llvm::Module *module, llvm::TargetOptions &
 EXPORT void clone_target_options(const llvm::Module *from, llvm::Module *to);
 
 /** Generate an LLVM module. */
-EXPORT llvm::Module *output_llvm_module(const Module &module, llvm::LLVMContext &context);
+EXPORT llvm::Module *compile_halide_module_to_llvm_module(const Module &module, llvm::LLVMContext &context);
 
 /** Compile an LLVM module to native targets (objects, native assembly). */
 // @{
-EXPORT void output_object(llvm::Module *module, const std::string &filename);
-EXPORT void output_assembly(llvm::Module *module, const std::string &filename);
-EXPORT void output_native(llvm::Module *module,
-                          const std::string &object_filename,
-                          const std::string &assembly_filename);
+EXPORT void compile_llvm_module_to_object(llvm::Module *module, const std::string &filename);
+EXPORT void compile_llvm_module_to_assembly(llvm::Module *module, const std::string &filename);
+EXPORT void compile_llvm_module_to_native(llvm::Module *module,
+                                          const std::string &object_filename,
+                                          const std::string &assembly_filename);
 // @}
 
 /** Compile an LLVM module to LLVM targets (bitcode, LLVM assembly). */
 // @{
-EXPORT void output_bitcode(llvm::Module *module, const std::string &filename);
-EXPORT void output_llvm_assembly(llvm::Module *module, const std::string &filename);
-EXPORT void output_llvm(llvm::Module *module,
-                        const std::string &bitcode_filename,
-                        const std::string &llvm_assembly_filename);
+EXPORT void compile_llvm_module_to_bitcode(llvm::Module *module, const std::string &filename);
+EXPORT void compile_llvm_module_to_llvm_assembly(llvm::Module *module, const std::string &filename);
+EXPORT void compile_llvm_module_to_llvm(llvm::Module *module,
+                                        const std::string &bitcode_filename,
+                                        const std::string &llvm_assembly_filename);
 // @}
 
 }
--- src/Output.cpp
+++ src/Output.cpp
@@ -8,82 +8,82 @@
 
 namespace Halide {
 
-void output_object(const Module &module, std::string filename) {
+void compile_halide_module_to_object(const Module &module, std::string filename) {
     if (filename.empty()) filename = module.name() + ".o";
 
     llvm::LLVMContext context;
-    llvm::Module *llvm = output_llvm_module(module, context);
-    output_object(llvm, filename);
+    llvm::Module *llvm = compile_halide_module_to_llvm_module(module, context);
+    compile_llvm_module_to_object(llvm, filename);
     delete llvm;
 }
 
-void output_assembly(const Module &module, std::string filename)  {
+void compile_halide_module_to_assembly(const Module &module, std::string filename)  {
     if (filename.empty()) filename = module.name() + ".s";
 
     llvm::LLVMContext context;
-    llvm::Module *llvm = output_llvm_module(module, context);
-    output_assembly(llvm, filename);
+    llvm::Module *llvm = compile_halide_module_to_llvm_module(module, context);
+    compile_llvm_module_to_assembly(llvm, filename);
     delete llvm;
 }
 
-void output_native(const Module &module,
+void compile_halide_module_to_native(const Module &module,
                    std::string object_filename,
                    std::string assembly_filename) {
     if (object_filename.empty()) object_filename = module.name() + ".o";
     if (assembly_filename.empty()) assembly_filename = module.name() + ".s";
 
     llvm::LLVMContext context;
-    llvm::Module *llvm = output_llvm_module(module, context);
-    output_object(llvm, object_filename);
-    output_assembly(llvm, assembly_filename);
+    llvm::Module *llvm = compile_halide_module_to_llvm_module(module, context);
+    compile_llvm_module_to_object(llvm, object_filename);
+    compile_llvm_module_to_assembly(llvm, assembly_filename);
     delete llvm;
 }
 
-void output_bitcode(const Module &module, std::string filename)  {
+void compile_halide_module_to_bitcode(const Module &module, std::string filename)  {
     if (filename.empty()) filename = module.name() + ".bc";
 
     llvm::LLVMContext context;
-    llvm::Module *llvm = output_llvm_module(module, context);
-    output_bitcode(llvm, filename);
+    llvm::Module *llvm = compile_halide_module_to_llvm_module(module, context);
+    compile_llvm_module_to_bitcode(llvm, filename);
     delete llvm;
 }
 
-void output_llvm_assembly(const Module &module, std::string filename)  {
+void compile_halide_module_to_llvm_assembly(const Module &module, std::string filename)  {
     if (filename.empty()) filename = module.name() + ".ll";
 
     llvm::LLVMContext context;
-    llvm::Module *llvm = output_llvm_module(module, context);
-    output_llvm_assembly(llvm, filename);
+    llvm::Module *llvm = compile_halide_module_to_llvm_module(module, context);
+    compile_llvm_module_to_llvm_assembly(llvm, filename);
     delete llvm;
 }
 
-void output_llvm(const Module &module,
+void compile_halide_module_to_llvm(const Module &module,
                  std::string bitcode_filename,
                  std::string llvm_assembly_filename)  {
     if (bitcode_filename.empty()) bitcode_filename = module.name() + ".bc";
     if (llvm_assembly_filename.empty()) llvm_assembly_filename = module.name() + ".ll";
 
     llvm::LLVMContext context;
-    llvm::Module *llvm = output_llvm_module(module, context);
-    output_bitcode(llvm, bitcode_filename);
-    output_llvm_assembly(llvm, llvm_assembly_filename);
+    llvm::Module *llvm = compile_halide_module_to_llvm_module(module, context);
+    compile_llvm_module_to_bitcode(llvm, bitcode_filename);
+    compile_llvm_module_to_llvm_assembly(llvm, llvm_assembly_filename);
     delete llvm;
 }
 
-void output_html(const Module &module, std::string filename) {
+void compile_halide_module_to_html(const Module &module, std::string filename) {
     if (filename.empty()) filename = module.name() + ".html";
 
     Internal::print_to_html(filename, module);
 }
 
-void output_text(const Module &module, std::string filename) {
+void compile_halide_module_to_text(const Module &module, std::string filename) {
     if (filename.empty()) filename = module.name() + ".stmt";
 
     std::ofstream file(filename.c_str());
     file << module;
 }
 
-void output_c_header(const Module &module, std::string filename) {
+void compile_halide_module_to_c_header(const Module &module, std::string filename) {
     if (filename.empty()) filename = module.name() + ".h";
 
     std::ofstream file(filename.c_str());
@@ -91,7 +91,7 @@ void output_c_header(const Module &module, std::string filename) {
     cg.compile(module);
 }
 
-void output_c_source(const Module &module, std::string filename) {
+void compile_halide_module_to_c_source(const Module &module, std::string filename) {
     if (filename.empty()) filename = module.name() + ".c";
 
     std::ofstream file(filename.c_str());
@@ -99,11 +99,11 @@ void output_c_source(const Module &module, std::string filename) {
     cg.compile(module);
 }
 
-void output_c(const Module &module,
+void compile_halide_module_to_c(const Module &module,
               std::string h_filename,
               std::string c_filename) {
-    output_c_header(module, h_filename);
-    output_c_source(module, c_filename);
+    compile_halide_module_to_c_header(module, h_filename);
+    compile_halide_module_to_c_source(module, c_filename);
 }
 
 }  // namespace Halide
