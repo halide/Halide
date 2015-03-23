295d5a8ea29f7808a1ad0518f125ca47761cc2bd
Tyler Denniston <tyler@csail.mit.edu>
2015-03-23 13:15:38 -0400

    Add schedule optimization level 1.

--- src/ScheduleOptimizationLevels.cpp
+++ src/ScheduleOptimizationLevels.cpp
@@ -1,9 +1,24 @@
+#include "Bounds.h"
+#include "FindCalls.h"
 #include "ScheduleOptimizationLevels.h"
+#include "Simplify.h"
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 namespace Halide {
 namespace Internal {
 
+using std::set;
+using std::string;
+using std::vector;
+using std::map;
+
 namespace {
+
+const unsigned UNDEFINED_FOOTPRINT_SIZE = ~0U;
+
 /** Return the optimization level controlled by HL_SCHED_OPT
  * environment variable. */
 ScheduleOptimization::Level get_optimization_level() {
@@ -12,6 +27,8 @@ ScheduleOptimization::Level get_optimization_level() {
     switch (i) {
     case 0:
         return ScheduleOptimization::LEVEL_0;
+    case 1:
+        return ScheduleOptimization::LEVEL_1;
     default:
         return ScheduleOptimization::LEVEL_0;
     }
@@ -22,10 +39,168 @@ ScheduleOptimization *get_optimization(ScheduleOptimization::Level level) {
     switch (level) {
     case ScheduleOptimization::LEVEL_0:
         return new OptimizationLevel0();
+    case ScheduleOptimization::LEVEL_1:
+        return new OptimizationLevel1();
     }
     return NULL;
 }
+
+/** Convenience class representing the callgraph for a pipeline. */
+class CallGraph {
+public:
+    CallGraph(Function root) {
+        set<string> visited;
+        construct(root, visited);
+    }
+
+    /** Return list of functions directly calling function f. */
+    const vector<Function> &callers(Function f) {
+        map<string, vector<Function> >::const_iterator I = call_to_caller.find(f.name());
+        if (I == call_to_caller.end()) {
+            call_to_caller[f.name()] = vector<Function>();
+            I = call_to_caller.find(f.name());
+        }
+        return I->second;
+    }
+
+    /** Return list of functions directly called by f. */
+    const vector<Function> &calls(Function f) {
+        map<string, vector<Function> >::const_iterator I = call_to_callee.find(f.name());
+        if (I == call_to_callee.end()) {
+            call_to_callee[f.name()] = vector<Function>();
+            I = call_to_callee.find(f.name());
+        }
+        return I->second;
+    }
+
+    /** Return list of functions transitively called by f. This does
+     * not include f. */
+    vector<Function> transitive_calls(Function f) {
+        const vector<Function> &dir_calls = calls(f);
+        set<string> visited;
+        vector<Function> result(dir_calls.begin(), dir_calls.end());
+        visited.insert(f.name());
+        for (vector<Function>::const_iterator I = dir_calls.begin(), E = dir_calls.end(); I != E; ++I) {
+            transitive_calls_helper(*I, result, visited);
+        }
+        return result;
+    }
+private:
+    map<string, Function> name_to_func;
+    map<string, vector<Function> > call_to_callee;
+    map<string, vector<Function> > call_to_caller;
+
+    void construct(Function f, set<string> &visited) {
+        visited.insert(f.name());
+        map<string, Function> calls = find_direct_calls(f);
+        for (map<string, Function>::iterator I = calls.begin(), E = calls.end(); I != E; ++I) {
+            string name = I->first;
+            Function func = I->second;
+            name_to_func[name] = func;
+            call_to_callee[f.name()].push_back(func);
+            call_to_caller[func.name()].push_back(f);
+
+            if (visited.find(I->first) == visited.end()) {
+                construct(I->second, visited);
+            }
+        }
+    }
+
+    void transitive_calls_helper(Function f, vector<Function> &result, set<string> &visited) {
+        if (visited.find(f.name()) != visited.end()) return;
+        visited.insert(f.name());
+        const vector<Function> &dir_calls = calls(f);
+        result.insert(result.end(), dir_calls.begin(), dir_calls.end());
+        for (vector<Function>::const_iterator I = dir_calls.begin(), E = dir_calls.end(); I != E; ++I) {
+            transitive_calls_helper(*I, result, visited);
+        }
+    }
+};
+
+/** Return min/max bounds for each dimension of the given function
+ * across all callsites. If the bounds cannot be computed, returns an
+ * empty vector. */
+vector<Interval> get_function_bounds(Function f, CallGraph &cg) {
+    vector<Interval> result;
+    result.resize(f.dimensions());
+    vector<bool> initialized;
+    initialized.resize(f.dimensions(), false);
+    vector<Function> callers = cg.callers(f);
+    if (callers.empty()) {
+        // Return undefined bounds for uncalled functions.
+        result.clear();
+        return result;
+    }
+    for (vector<Function>::const_iterator I = callers.begin(), E = callers.end(); I != E; ++I) {
+        const vector<Expr> outputs = (*I).values();
+        internal_assert(outputs.size() == 1) << "Unhandled number of outputs";
+        Box b = boxes_required(outputs[0])[f.name()];
+        const unsigned dim = b.bounds.size();
+        if (dim == 0) {
+            // Unable to compute the bounds for the function. Return undefined bounds.
+            result.clear();
+            return result;
+        }
+        internal_assert((int)dim == f.dimensions());
+        for (unsigned i = 0; i < dim; ++i) {
+            Interval interval = b.bounds[i];
+            if (!initialized[i]) {
+                initialized[i] = true;
+                result[i] = interval;
+            } else {
+                result[i].min = min(result[i].min, interval.min);
+                result[i].max = max(result[i].max, interval.max);
+            }
+        }
+    }
+    return result;
+}
+
+
+/** Return the footprint (required region) of the given function over
+ * all callsites. This is a minimum of 1 when a function is
+ * pointwise. If the footprint cannot be calculated, returns
+ * UNDEFINED_FOOTPRINT_SIZE. */
+unsigned calculate_footprint_size(Function f, CallGraph &cg) {
+    vector<Interval> bounds = get_function_bounds(f, cg);
+    if (bounds.size() == 0) {
+        // Unable to calculate function bounds.
+        return UNDEFINED_FOOTPRINT_SIZE;
+    }
+    Expr footprint = 1;
+    for (vector<Interval>::iterator I = bounds.begin(), E = bounds.end(); I != E; ++I) {
+        const Interval &i = *I;
+        internal_assert(i.min.defined() && i.max.defined());
+        Expr diff = i.max - i.min + 1;
+        footprint *= diff;
+    }
+    const int *result = as_const_int(simplify(footprint));
+    if (result) {
+        return (unsigned)*result;
+    } else {
+        return UNDEFINED_FOOTPRINT_SIZE;
+    }
+}
+
 } // end anonymous namespace
+
+
+void OptimizationLevel1::apply(Func func) {
+    Function root = func.function();
+    // Construct a callgraph for the pipeline.
+    CallGraph cg(root);
+    vector<Function> all_functions = cg.transitive_calls(root);
+    for (vector<Function>::iterator I = all_functions.begin(), E = all_functions.end(); I != E; ++I) {
+        Function f = *I;
+        Func wrapper(f);
+        unsigned footprint = calculate_footprint_size(f, cg);
+        if (footprint == 1) {
+            wrapper.compute_inline();
+        } else {
+            wrapper.store_root().compute_root();
+        }
+    }
+}
 
 void apply_schedule_optimization(Func func) {
     ScheduleOptimization::Level level = get_optimization_level();
--- src/ScheduleOptimizationLevels.h
+++ src/ScheduleOptimizationLevels.h
@@ -13,13 +13,26 @@ namespace Internal {
 
 class ScheduleOptimization {
 public:
-    typedef enum { LEVEL_0 } Level;
+    /** Level of optimization. */
+    typedef enum { LEVEL_0, LEVEL_1 } Level;
+    /** Apply the schedule optimization to the pipeline. 'func' should
+     * be the output of the pipeline. */
     virtual void apply(Func func) = 0;
 };
 
+/** Optimization level 0 does nothing. */
 class OptimizationLevel0 : public ScheduleOptimization {
 public:
     virtual void apply(Func func) { }
+};
+
+/** Optimization level 1 performs the following simple optimization:
+ * - Functions with a single callsite are inlined.
+ * - Functions called as a stencil are compute_root.
+ */
+class OptimizationLevel1 : public ScheduleOptimization {
+public:
+    virtual void apply(Func func);
 };
 
 /** Apply schedule optimizations, controled by the HL_SCHED_OPT
