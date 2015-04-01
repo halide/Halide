b16209827b13f8c3756dc764306b3ca88955f664
Steven Johnson <steven@crinklink.com>
2015-04-01 11:35:03 -0700

    Formatting and padding fixes
    Former-commit-id: a7aac6cff6dfe21b9ad263893131453740be4abc

--- test/generator/metadata_tester_generator.cpp
+++ test/generator/metadata_tester_generator.cpp
@@ -2,7 +2,8 @@
 
 namespace {
 
-enum class SomeEnum { Foo, Bar };
+enum class SomeEnum { Foo,
+                      Bar };
 
 class MetadataTester : public Halide::Generator<MetadataTester> {
 public:
@@ -10,18 +11,18 @@ public:
     GeneratorParam<Type> output_type{ "output_type", Float(32) };
 
     ImageParam input{ UInt(8), 3, "input" };
-    Param<bool> b{"b", true};
-    Param<int8_t> i8{"i8", 8, -8, 127};
-    Param<int16_t> i16{"i16", 16, -16, 127};
-    Param<int32_t> i32{"i32", 32, -32, 127};
-    Param<int64_t> i64{"i64", 64, -64, 127};
-    Param<uint8_t> u8{"u8", 80, 8, 255};
-    Param<uint16_t> u16{"u16", 160, 16, 2550};
-    Param<uint32_t> u32{"u32", 320, 32, 2550};
-    Param<uint64_t> u64{"u64", 640, 64, 2550};
-    Param<float> f32{"f32", 32.1234f, -3200.1234f, 3200.1234f};
-    Param<double> f64{"f64", 64.25f, -6400.25f, 6400.25f};
-    Param<void*> h{"h", NULL};
+    Param<bool> b{ "b", true };
+    Param<int8_t> i8{ "i8", 8, -8, 127 };
+    Param<int16_t> i16{ "i16", 16, -16, 127 };
+    Param<int32_t> i32{ "i32", 32, -32, 127 };
+    Param<int64_t> i64{ "i64", 64, -64, 127 };
+    Param<uint8_t> u8{ "u8", 80, 8, 255 };
+    Param<uint16_t> u16{ "u16", 160, 16, 2550 };
+    Param<uint32_t> u32{ "u32", 320, 32, 2550 };
+    Param<uint64_t> u64{ "u64", 640, 64, 2550 };
+    Param<float> f32{ "f32", 32.1234f, -3200.1234f, 3200.1234f };
+    Param<double> f64{ "f64", 64.25f, -6400.25f, 6400.25f };
+    Param<void *> h{ "h", NULL };
 
     Func build() override {
         input = ImageParam(input_type, input.dimensions(), input.name());
@@ -38,6 +39,6 @@ public:
     }
 };
 
-Halide::RegisterGenerator<MetadataTester> register_MetadataTester{"metadata_tester"};
+Halide::RegisterGenerator<MetadataTester> register_MetadataTester{ "metadata_tester" };
 
 }  // namespace
