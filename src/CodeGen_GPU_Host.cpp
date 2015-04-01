d82d188b2b1735126c81ab02a20502deaeba885c
Andrew Adams <andrew.b.adams@gmail.com>
2015-04-01 09:24:28 -0700

    Entry basic block was messed up for gpu host
    Assertions came before their tracking allocas
    
    
    Former-commit-id: 4a02ceb612ef982a855135cc602125c17916d7df

--- src/CodeGen_GPU_Host.cpp
+++ src/CodeGen_GPU_Host.cpp
@@ -237,8 +237,28 @@ void CodeGen_GPU_Host<CodeGen_CPU>::compile_func(const LoweredFunc &f) {
     // Call the base implementation to create the function.
     CodeGen_CPU::compile_func(f);
 
-    // Remember the entry block so we can branch to it upon init success.
+    // We need to insert code after the existing entry block, so that
+    // the destructor stack slots exist before we do the assertions
+    // involved in initializing gpu kernels.
+
+    // Split the entry block just before its end.
     BasicBlock *entry = &function->getEntryBlock();
+    llvm::Instruction *terminator = entry->getTerminator();
+    internal_assert(terminator);
+    BasicBlock *post_entry = entry->splitBasicBlock(terminator);
+
+    // Create some code that does the GPU initialization.
+    BasicBlock *init_kernels_bb = BasicBlock::Create(*context, "init_kernels",
+                                                     function, post_entry);
+
+    // The entry block should go to the init kernels block instead of
+    // the post entry block.
+    entry->getTerminator()->eraseFromParent();
+    builder->SetInsertPoint(entry);
+    builder->CreateBr(init_kernels_bb);
+
+    // Fill out the init kernels block
+    builder->SetInsertPoint(init_kernels_bb);
 
     for (iter = cgdev.begin(); iter != cgdev.end(); iter++) {
         if (iter->first == DeviceAPI::Default_GPU) {
@@ -263,10 +283,7 @@ void CodeGen_GPU_Host<CodeGen_CPU>::compile_func(const LoweredFunc &f) {
             CodeGen_CPU::create_constant_binary_blob(kernel_src,
                                                      "halide_" + function_name + "_" + api_unique_name + "_kernel_src");
 
-        // Insert a new block to run initialization at the beginning of the function.
-        BasicBlock *init_kernels_bb = BasicBlock::Create(*context, "init_kernels_" + api_unique_name,
-                                                         function, entry);
-        builder->SetInsertPoint(init_kernels_bb);
+
         Value *user_context = get_user_context();
         Value *kernel_size = ConstantInt::get(i32, kernel_src.size());
         std::string init_kernels_name = "halide_" + api_unique_name + "_initialize_kernels";
@@ -277,14 +294,13 @@ void CodeGen_GPU_Host<CodeGen_CPU>::compile_func(const LoweredFunc &f) {
                                              kernel_src_ptr, kernel_size);
         Value *did_succeed = builder->CreateICmpEQ(result, ConstantInt::get(i32, 0));
         CodeGen_CPU::create_assertion(did_succeed, "Failure inside " + init_kernels_name);
-
-        // Upon success, jump to the previous entry.
-        builder->CreateBr(entry);
-        // Set the entry for the next init block to the init block just generated
-        entry = init_kernels_bb;
     }
 
+    // the init kernels block should branch to the post-entry block
+    builder->CreateBr(post_entry);
+
     function_name = "";
+
 }
 
 template<typename CodeGen_CPU>
