ab5d1f5c8ae26238e28b75b9ed2821c6387c639f
Andrew Adams <andrew.b.adams@gmail.com>
2015-04-09 12:30:58 -0700

    Make metadata linked list a global to fix windows issue

--- src/runtime/metadata.cpp
+++ src/runtime/metadata.cpp
@@ -16,14 +16,9 @@ namespace Halide { namespace Runtime { namespace Internal {
 struct list_head_t {
     halide_mutex mutex;
     _halide_runtime_internal_registered_filter_t *next;
-
-    list_head_t() : mutex(), next(NULL) {}
 };
 
-static list_head_t* get_list_head() {
-    static list_head_t head;
-    return &head;
-}
+WEAK list_head_t list_head;
 
 } } }
 
@@ -36,16 +31,14 @@ WEAK void halide_runtime_internal_register_metadata(_halide_runtime_internal_reg
     // to by it may not be initialized yet (since order of execution is not guaranteed in this case);
     // it is essential that this code not do anything with that pointer other than store
     // it for future use. (The name argument will always be valid here, however.)
-    list_head_t* head = get_list_head();
-    ScopedMutexLock lock(&head->mutex);
-    info->next = head->next;
-    head->next = info;
+    ScopedMutexLock lock(&list_head.mutex);
+    info->next = list_head.next;
+    list_head.next = info;
 }
 
 WEAK int halide_enumerate_registered_filters(void *user_context, void* enumerate_context, enumerate_func_t func) {
-    list_head_t* head = get_list_head();
-    ScopedMutexLock lock(&head->mutex);
-    for (_halide_runtime_internal_registered_filter_t* f = head->next; f != NULL; f = f->next) {
+    ScopedMutexLock lock(&list_head.mutex);
+    for (_halide_runtime_internal_registered_filter_t* f = list_head.next; f != NULL; f = f->next) {
         int r = (*func)(enumerate_context, f->metadata, f->argv_func);
         if (r != 0) return r;
     }
