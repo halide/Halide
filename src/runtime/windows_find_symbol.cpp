bb22895ade212f44ed5405c47f6299861405aa37
Dillon Sharlet <dsharlet@google.com>
2015-04-14 19:31:07 -0700

    Fixed 32 bit calling convention.
    Former-commit-id: a1f46eadfc32aa0cd733b4fbb498d50688b29a21

--- src/runtime/windows_find_symbol.cpp
+++ src/runtime/windows_find_symbol.cpp
@@ -1,6 +1,12 @@
 #include "runtime_internal.h"
 
-extern "C" void *GetProcAddress(void *, const char *);
+#ifdef BITS_64
+#define WIN32API
+#else
+#define WIN32API __stdcall
+#endif
+
+extern "C" WIN32API void *GetProcAddress(void *, const char *);
 
 namespace Halide { namespace Runtime { namespace Internal {
 
