96a32ecf6c6a181d5dbdd85b868f544af297f27f
Steven Johnson <steven@crinklink.com>
2015-04-16 15:35:50 -0700

    MSVC2013 doesn't understand alignof

--- test/correctness/buffer_t.cpp
+++ test/correctness/buffer_t.cpp
@@ -4,6 +4,12 @@
 #define CHECK(f, s32, s64) \
     static_assert(offsetof(buffer_t, f) == (sizeof(void*) == 8 ? (s64) : (s32)), #f " is wrong")
 
+#ifdef _MSC_VER
+    // VC2013 doesn't support alignof, apparently
+    #define ALIGN_OF(x) __alignof(x)
+#else
+    #define ALIGN_OF(x) alignof(x)
+#endif
 
 int main(int argc, char **argv) {
 
@@ -25,7 +31,7 @@ int main(int argc, char **argv) {
                     (sizeof(buffer_t) == 68 || sizeof(buffer_t) == 72), "size is wrong");
 
     // Ensure alignment is at least that of a pointer.
-    static_assert(alignof(buffer_t) >= alignof(uint8_t*), "align is wrong");
+    static_assert(ALIGN_OF(buffer_t) >= ALIGN_OF(uint8_t*), "align is wrong");
 
     printf("Success!\n");
     return 0;
