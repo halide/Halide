96b1cb353b74debdbc7aba2935ce2e52f13745bd
Andrew Adams <andrew.b.adams@gmail.com>
2015-04-20 13:09:43 -0700

    Don't run the octave test when we're testing 32-bit on a 64-bit system
    The installed octave is almost certainly 64-bit, and can't run 32-bit
    plugins.
    
    
    Former-commit-id: aec1ac9cffeb78f08417bab8f99fef742e8eb49f

--- apps/HelloMatlab/run.sh
+++ apps/HelloMatlab/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# This script is run by the nightly tests to check that mex_halide works.
+
 command -v octave >/dev/null 2>&1 || { echo >&2 "Octave not found.  Aborting."; exit 0; }
 
-rm -f blurred.png
+if [[ $CXX == *"-m32"* ]]; then
+    echo "Not proceeding because Halide is compiled in 32-bit mode but octave is (likely) 64-bit"
+    exit 0
+fi
+
+rm -f blurred.png iir_blur.mex
 octave run.m
 
 if [ -f blurred.png ]
@@ -13,6 +20,3 @@ fi
 
 echo "Failed to produce blurred.png!"
 exit 1
-
-
-    
