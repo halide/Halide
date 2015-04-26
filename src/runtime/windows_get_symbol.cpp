2ad8190b2c1cc1e030915f852790b07691ca0e7a
Dillon Sharlet <dsharlet@google.com>
2015-04-26 12:03:06 -0700

    Fixed load_library/get_symbol to not get stripped.
    Former-commit-id: ed604c9efae87bac9e1be5dd043b979092338032

--- src/runtime/windows_get_symbol.cpp
+++ src/runtime/windows_get_symbol.cpp
@@ -6,26 +6,26 @@
 #define WIN32API __stdcall
 #endif
 
-extern "C" WIN32API void *LoadLibrary(const char *);
-extern "C" WIN32API void *GetProcAddress(void *, const char *);
-extern "C" WIN32API int FreeLibrary(void *);
+extern "C" {
 
-namespace Halide { namespace Runtime { namespace Internal {
+WIN32API void *LoadLibrary(const char *);
+WIN32API void *GetProcAddress(void *, const char *);
+WIN32API int FreeLibrary(void *);
 
-WEAK void *get_symbol(const char *name) {
+WEAK void *halide_get_symbol(const char *name) {
     return GetProcAddress(NULL, name);
 }
 
-WEAK void *load_library(const char *name) {
+WEAK void *halide_load_library(const char *name) {
     return LoadLibrary(name);
 }
 
-WEAK void *get_library_symbol(void *lib, const char *name) {
+WEAK void *halide_get_library_symbol(void *lib, const char *name) {
     return GetProcAddress(lib, name);
 }
 
-WEAK void free_library(void *lib) {
+WEAK void halide_free_library(void *lib) {
     FreeLibrary(lib);
 }
 
-}}}
+}
