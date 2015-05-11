3be295b1a80a5e8defc888122a321f904150592c
Steven Johnson <steven@crinklink.com>
2015-05-11 14:49:19 -0700

    Add a buffer-image-randomizer to SimpleApps; add static_image.h to include paths
    Former-commit-id: 6588375ea52273f558d739550e76aea59d835584

--- apps/templates/SimpleAppAPI.h
+++ apps/templates/SimpleAppAPI.h
@@ -1,6 +1,9 @@
 #ifndef _H_SimpleAppAPI
 #define _H_SimpleAppAPI
 
+#include <random>
+#include <vector>
+
 #include "HalideRuntime.h"
 
 extern "C" {
@@ -18,5 +21,39 @@ int halide_buffer_print(const buffer_t* buffer);
 int halide_buffer_display(const buffer_t* buffer);
 
 }  // extern "C"
+
+// Fill the buffer's host storage field with random data of the given type
+// in the given range. Not very rigorous, but convenient for simple testing
+// or profiling.
+// If the buffer's elem_size doesn't match the size, or if host is null,
+// return false.
+template<typename T>
+bool halide_randomize_buffer_host(int seed, T min, T max, buffer_t* buf) {
+    if (sizeof(T) != buf->elem_size) {
+        return false;
+    }
+    if (!buf->host) {
+      return false;
+    }
+    std::mt19937 rnd(seed);
+    T *p0 = reinterpret_cast<T*>(buf->host);
+    for (int i0 = 0; i0 < std::max(1, buf->extent[0]); ++i0) {
+        T *p1 = p0;
+        for (int i1 = 0; i1 < std::max(1, buf->extent[1]); ++i1) {
+            T *p2 = p1;
+            for (int i2 = 0; i2 < std::max(1, buf->extent[2]); ++i2) {
+                T *p3 = p2;
+                for (int i3 = 0; i3 < std::max(1, buf->extent[3]); ++i3) {
+                    *p3 = min + (T) (((double) rnd() / (double) 0xffffffff) * (max - min));
+                    p3 += buf->stride[3];
+                }
+                p2 += buf->stride[2];
+            }
+            p1 += buf->stride[1];
+        }
+        p0 += buf->stride[0];
+    }
+    return true;
+}
 
 #endif  // _H_SimpleAppAPI
