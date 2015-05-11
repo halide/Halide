75a0f41d3bf1353f9dc711c005702f4e26f7978c
Steven Johnson <steven@crinklink.com>
2015-05-11 16:42:21 -0700

    Replaced profiling macros with simpler classes for now
    Former-commit-id: 0b8f8abae3fe1421574533c1e12160da4301807b

--- apps/templates/SimpleAppAPI.h
+++ apps/templates/SimpleAppAPI.h
@@ -1,6 +1,8 @@
 #ifndef _H_SimpleAppAPI
 #define _H_SimpleAppAPI
 
+#include <stdarg.h>
+
 #include <random>
 #include <vector>
 
@@ -20,63 +22,84 @@ int halide_buffer_print(const buffer_t* buffer);
 // displayed as a png image.
 int halide_buffer_display(const buffer_t* buffer);
 
+// These aren't "official" public runtime API, so please don't use them outside of this code.
+int halide_start_clock(void *user_context);
+int64_t halide_current_time_ns(void *user_context);
+
 }  // extern "C"
 
-// This set of macros may be used to placing timing code around a halide
-// function to be benchmarked.
-#ifdef __APPLE__
-#define PROFILE_SHADER 1
-
-// In a program that calls the halide function repeatedly, e.g. once per frame
-// set the number of iterations to compute statistics over.
-#define PROFILE_ITERATIONS 1
-#ifdef PROFILE_SHADER
-#import <mach/mach_time.h>
-
-#define PROFILE_BEGIN(msg) \
-{\
-  uint64_t begin_time = mach_absolute_time();
-
-// This macro is used to stop timing a section of code. If PROFILE_ITERATIONS
-// have occurred, the average, min, and max time will be printed to the console
-#define PROFILE_END(msg,w,h) \
-  uint64_t end_time = mach_absolute_time();\
-\
-  static mach_timebase_info_data_t info = { 0, 0 };\
-  if (!info.denom) {\
-    mach_timebase_info(&info);\
-  }\
-  static float totalDuration = 0;\
-  static float minDuration = 1000.0f;\
-  static float maxDuration = 0.0f;\
-  static int iterations = 0;\
-\
-  float duration = ((end_time-begin_time)*info.numer)/info.denom * 1e-9;\
-  totalDuration += duration;\
-  minDuration = (duration < minDuration) ? duration : minDuration;\
-  maxDuration = (duration > maxDuration) ? duration : maxDuration;\
-\
-  if (!(++iterations % PROFILE_ITERATIONS)) {\
-\
-    const size_t pixelsPerFrame = (w) * (h);\
-    float Mpixelpers = (((float)pixelsPerFrame*(float)PROFILE_ITERATIONS)/1e6) / totalDuration;\
-    float avgDuration = totalDuration / (float)PROFILE_ITERATIONS;\
-\
-    NSLog(@"H>> %s %3.3f Mpixels/sec %3.3f avg sec %3.3f min %3.3f max (%d iterations)\n", \
-      #msg, Mpixelpers, avgDuration, minDuration, maxDuration, \
-      PROFILE_ITERATIONS);\
-\
-    totalDuration = 0;\
-    minDuration = 1000.0f;\
-    maxDuration = 0.0f;\
-    iterations = 0;\
-  }\
+// VarArg wrapper around halide_print()
+void halide_printf(void* user_context, const char* fmt, ...) {
+  char buffer[4096];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(buffer, sizeof(buffer), fmt, args);
+  halide_print(user_context, buffer);
+  va_end(args);
 }
-#else
-#define PROFILE_BEGIN(msg)
-#define PROFILE_END(msg)
-#endif
-#endif
+
+// VarArg wrapper around halide_error()
+void halide_errorf(void* user_context, const char* fmt, ...) {
+  char buffer[4096];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(buffer, sizeof(buffer), fmt, args);
+  halide_error(user_context, buffer);
+  va_end(args);
+}
+
+class SimpleTimer {
+ public:
+  SimpleTimer(void *user_context)
+      : user_context_(user_context), time_net_(0), time_start_(0), running_(false) {
+    halide_start_clock(user_context_);
+  }
+  void start() {
+    if (!running_) {
+      time_start_ = halide_current_time_ns(user_context_);
+      running_ = true;
+    }
+  }
+  void stop() {
+    if (running_) {
+      int64_t time_end = halide_current_time_ns(user_context_);
+      time_net_ += (time_end - time_start_);
+      time_start_ = 0;
+      running_ = false;
+    }
+  }
+  void reset() {
+    time_net_ = 0;
+    time_start_ = 0;
+    running_ = false;
+  }
+  int64_t net_nsec() { return running_ ? 0 : time_net_; }
+  double net_usec() { return running_ ? 0 : (double) time_net_ / 1e3; }
+  double net_msec() { return running_ ? 0 : (double)time_net_ / 1e6; }
+  double net_sec() { return running_ ? 0 : (double)time_net_ / 1e9; }
+ private:
+  void *user_context_;
+  int64_t time_net_;
+  int64_t time_start_;
+  bool running_;
+};
+
+class ScopedTimer {
+ public:
+  ScopedTimer(void *user_context, const char *msg)
+      : user_context_(user_context), timer_(user_context), msg_(msg) {
+    timer_.start();
+  }
+  ~ScopedTimer() {
+    timer_.stop();
+    halide_printf(user_context_, "%s: %f usec", msg_, timer_.net_usec());
+  }
+ private:
+  void *user_context_;
+  SimpleTimer timer_;
+  const char *msg_;
+};
+
 
 // Fill the buffer's host storage field with random data of the given type
 // in the given range. Not very rigorous, but convenient for simple testing
--- src/runtime/HalideRuntimeOpenGL.h
+++ src/runtime/HalideRuntimeOpenGL.h
@@ -11,7 +11,7 @@ extern "C" {
  *  Routines specific to the Halide OpenGL runtime.
  */
 
-extern struct halide_device_interface *halide_glsl;
+extern const struct halide_device_interface *halide_opengl_device_interface();
 
 /** These are forward declared here to allow clients to override the
  *  Halide Glsl runtime. Do not call them. */
