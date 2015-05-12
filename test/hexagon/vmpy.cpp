faa5d6e5a127357c1006352829a2d5c53d18509f
Pranav Bhandarkar <pranavb@codeaurora.org>
2015-05-12 13:53:22 -0500

    [19342] - Fix testcase vmpy.cpp.
    Change-Id: I2eca8da4bbd9cb4abc3af7f93dfdb6ded2c2f65a
    Reviewed-on: https://review-hexagon.quicinc.com/28836
    Reviewed-by: Pranav Bhandarkar <pranavb@codeaurora.org>
    Tested-by: Pranav Bhandarkar <pranavb@codeaurora.org>

--- test/hexagon/vmpy.cpp
+++ test/hexagon/vmpy.cpp
@@ -14,7 +14,7 @@ using namespace Halide;
 int main(int argc, char **argv) {
   Target target;
   setupHexagonTarget(target);
-  //CHECK: vmpy(v{{[0-9]+}}.b,v{{[0-9]+}}.b)
+  //CHECK: vmpy(v{{[0-9]+}}.ub,v{{[0-9]+}}.ub)
   testVMPY<uint8_t, uint8_t, uint16_t>(target);
   //CHECK: vmpy(v{{[0-9]+}}.h,v{{[0-9]+}}.h)
   testVMPY<int16_t, int16_t, int32_t>(target);
