2085a315105f5e11adf0ff5009f4ba98c9ec93c6
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-08 17:23:25 +0200

    Expr and Var now can be operated with ints and floats
    Former-commit-id: aa1b16c6f5fcebaf15bed75d3c217ba3a50352cf

--- boost_python_bindings/python/add_operators.h
+++ boost_python_bindings/python/add_operators.h
@@ -5,12 +5,62 @@
 #include <boost/python/self.hpp>
 //#include <boost/python/def.hpp>
 
+template<typename PythonClass, typename T>
+void add_operators_with(PythonClass &class_instance)
+{
+    using namespace boost::python;
+
+    class_instance
+            .def(self + T())
+            .def(T() + self)
+
+            .def(self - T())
+            .def(T() - self)
+
+            .def(self * T())
+            .def(T() * self)
+
+            .def(self / T())
+            .def(T() / self)
+
+            .def(self % T())
+            .def(T() % self)
+
+            .def(pow(self, T()))
+            .def(pow(T(), self))
+
+            .def(self & T())
+            .def(T() & self)
+
+            .def(self | T())
+            .def(T() | self)
+
+            .def(self < T())
+            .def(T() < self)
+
+            .def(self <= T())
+            .def(T() <= self)
+
+            .def(self == T())
+            .def(T() == self)
+
+            .def(self != T())
+            .def(T() != self)
+
+            .def(self > T())
+            .def(T() > self)
+
+            .def(self >= T())
+            .def(T() >= self);
+
+    return;
+}
+
 template<typename PythonClass>
 void add_operators(PythonClass &class_instance)
 {
     using namespace boost::python;
 
-    // FIXME Var + int, Var + float not yet working
     class_instance
             .def(self + self)
             .def(self - self)
@@ -30,6 +80,8 @@ void add_operators(PythonClass &class_instance)
             .def(self > self)
             .def(self >= self);
 
+    add_operators_with<PythonClass, int>(class_instance);
+    add_operators_with<PythonClass, float>(class_instance);
     return;
 }
 
