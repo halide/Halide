5f68528f18b63cdcf6b73769ba374e3e00479398
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-10 15:03:51 +0200

    Improved support for Type class members
    Former-commit-id: 7dec32567a9b72b18c6782d43ecc72d94e918fe9

--- boost_python_bindings/python/Type.cpp
+++ boost_python_bindings/python/Type.cpp
@@ -4,18 +4,56 @@
 #include <boost/python.hpp>
 
 #include "../../src/Type.h"
-
+#include "../../src/Expr.h"
 
 #include <string>
 
 void defineType()
 {
 
+    using Halide::Type;
     namespace h = Halide;
     namespace p = boost::python;
     using p::self;
 
-    p::class_<h::Type>("Type", p::no_init);
+    p::class_<Type>("Type", p::no_init)
+            .def_readonly("bits", &Type::bits,
+                          "The number of bits of precision of a single scalar value of this type.")
+            .def("bytes", &Type::bytes,
+                 "The number of bytes required to store a single scalar value of this type. Ignores vector width.")
+            .def_readonly("width", &Type::width,
+                          "How many elements (if a vector type). Should be 1 for scalar types.")
+            .def("is_bool", &Type::is_bool,
+                 "Is this type boolean (represented as UInt(1))?")
+            .def("is_vector", &Type::is_vector,
+                 "Is this type a vector type? (width > 1)")
+            .def("is_scalar", &Type::is_scalar,
+                 "Is this type a scalar type? (width == 1)")
+            .def("is_float", &Type::is_float,
+                 "Is this type a floating point type (float or double).")
+            .def("is_int", &Type::is_int,
+                 "Is this type a signed integer type?")
+            .def("is_uint", &Type::is_uint,
+                 "Is this type an unsigned integer type?")
+            .def("is_handle", &Type::is_handle,
+                 "Is this type an opaque handle type (void *)")
+            .def(p::self == p::self)
+            .def(p::self != p::self)
+            .def("vector_of", &Type::vector_of, p::arg("w"),
+                 "Produce a vector of this type, with 'width' elements")
+            .def("element_of", &Type::element_of,
+                 "Produce the type of a single element of this vector type")
+            .def("can_represent", &Type::can_represent, p::arg("other"),
+                 "Can this type represent all values of another type?")
+            .def("imax", &Type::imax,
+                 "Return an integer which is the maximum value of this type.")
+            .def("max", &Type::max,
+                 "Return an expression which is the maximum value of this type")
+            .def("imin", &Type::imin,
+                 "Return an integer which is the minimum value of this type")
+            .def("min", &Type::min,
+                 "Return an expression which is the minimum value of this type")
+            ;
 
     p::def("Int", h::Int,
            (p::arg("bits"), p::arg("width")=1),
