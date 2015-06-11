99c20d69bc8fcc308e3b012f97d53518880f9b27
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-11 22:44:47 +0200

    blur.py runs as desired hip-hip hurra \!
    Former-commit-id: 52756019b8b5fefdcb624f463d1fb2fa5457a080

--- boost_python_bindings/apps/blur.py
+++ boost_python_bindings/apps/blur.py
@@ -58,33 +58,70 @@ def filter_image(input, out_func, in_image, disp_time=False, compile=True, eval_
     return evaluate
 
 
-def main():
-    input = ImageParam(UInt(16), 2, 'input')
+def get_blur(input):
+
+    assert type(input) == ImageParam
+    assert input.dimensions() == 2
+
     x, y = Var('x'), Var('y')
+
+    #clamped_input = repeat_edge(cast(UInt(16), input))
+    #ci = clamped_input
+    clamped_input = repeat_edge(input)
+
+    input_uint16 = Func('input_uint16')
+    input_uint16[x,y] = cast(UInt(16), clamped_input[x,y])
+    ci = input_uint16 # cast does not work over ImageParam ?
 
     blur_x = Func('blur_x')
     blur_y = Func('blur_y')
 
-    print("ping 0.0")
-    input[x,y]
-    print("ping 0.1")
-    input[x+1,y]
-    print("ping 0.2")
-    (input[x,y]+input[x+1,y])
-    print("ping 0.3")
-    (input[x,y]+input[x+1,y]) / 2
-    print("ping 0.4")
-    blur_x[x,y] = input[x,y]
+    blur_x[x,y] = (ci[x,y]+ci[x+1,y]+ci[x+2,y])/3
+    blur_y[x,y] = cast(UInt(8), (blur_x[x,y]+blur_x[x,y+1]+blur_x[x,y+2])/3)
 
-    print("ping 0.5")
-    blur_x[x,y] = (input[x,y]+input[x+1,y]+input[x+2,y])/3
-    print("ping 1")
-    blur_y[x,y] = (blur_x[x,y]+blur_x[x,y+1]+blur_x[x,y+2])/3
-
+    # schedule
     xi, yi = Var('xi'), Var('yi')
-    print("ping 2")
     blur_y.tile(x, y, xi, yi, 8, 4).parallel(y).vectorize(xi, 8)
     blur_x.compute_at(blur_y, x).vectorize(x, 8)
+
+    return blur_y
+
+def main():
+    input = ImageParam(UInt(8), 2, "input_param")
+    blur = get_blur(input)
+    blur.compile_jit()
+
+    import numpy as np
+    from scipy.misc import imread, imsave
+    import os.path
+    image_path = os.path.join(os.path.dirname(__file__), "../../apps/images/rgb.png")
+    assert os.path.exists(image_path), \
+        "Could not find %s" % image_path
+    rgb_data = imread(image_path)
+    print("rgb_data", type(rgb_data), rgb_data.shape, rgb_data.dtype)
+
+    grey_data = np.mean(rgb_data, axis=2, dtype=float).astype(rgb_data.dtype)
+    input_data = np.copy(grey_data, order="F")
+    input_image = ndarray_to_image(input_data, "input_image")
+    input.set(input_image)
+
+    output_data = np.empty(rgb_data.shape[:2],
+                              dtype=rgb_data.dtype, order="F")
+    output_image = ndarray_to_image(output_data, "output_image")
+
+    print("input_image", input_image)
+    print("output_image", output_image)
+
+    blur.realize(output_image)
+
+    output_path = "blur_result.png"
+    print("blur realized on output image.",
+          "Result saved at", output_path)
+
+    imsave("input_data.png", grey_data)
+    imsave(output_path, output_data)
+
+    return
 
     print("ping 3")
     maxval = 255
