ef40cfdc71c63f68a653a79da08b50d731b0ded5
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-11 23:19:39 +0200

    blur.py clean-up
    Former-commit-id: 078b9d46f0015841a3922cd6a2cae7dbca1c468e

--- boost_python_bindings/apps/blur.py
+++ boost_python_bindings/apps/blur.py
@@ -1,62 +1,5 @@
-import os, sys
+
 from halide import *
-
-OUT_DIMS = (1536, 2560)
-
-
-def filter_image(input, out_func, in_image, disp_time=False, compile=True, eval_func=None, out_dims=None, times=1): #, pad_multiple=1):
-    """
-    Utility function to filter an image filename or numpy array with a Halide Func, returning output Image of the same size.
-
-    Given input and output Funcs, and filename/numpy/PIL array (in_image), returns evaluate. Calling evaluate() returns the output Image.
-    """
-#    print('filter_image, input=', input, 'dtype', input.type())
-    dtype = input.type()
-    if isinstance(input, ImageParamType):
-        if isinstance(in_image, str):
-            input_png = Image(dtype, in_image)
-        elif isinstance(in_image, numpy.ndarray):
-            input_png = Image(in_image)
-        elif hasattr(in_image, 'putpixel'):         # PIL image
-            input_png = Image(dtype, in_image)
-        else:
-            input_png = in_image
-        input.set(input_png)
-    else:
-        input_png = input
-
-    w = input_png.width() if out_dims is None else out_dims[0]
-    h = input_png.height() if out_dims is None else out_dims[1]
-    nchan = input_png.channels() if out_dims is None else (out_dims[2] if len(out_dims) >= 3 else 1)
-    #print(w, h, nchan, out_dims)
-    if compile:
-        out_func.compile_jit()
-
-    def evaluate():
-        T = []
-        for i in range(times):
-            T0 = time.time()
-            if eval_func is not None:
-                realized = eval_func(input_png)
-            else:
-                if nchan != 1:
-                    realized = out_func.realize(w, h, nchan)
-                else:
-                    realized = out_func.realize(w, h)
-            T.append(time.time()-T0)
-        out = Image(realized) #.set(realized)
-
-
-        assert out.width() == w and out.height() == h, (out.width(), out.height(), w, h)
-        if disp_time:
-            if times > 1:
-                print('Filtered %d times, min: %.6f secs, mean: %.6f secs.' % (times, numpy.min(T), numpy.mean(T)))
-            else:
-                print('Filtered in %.6f secs' % T[0])
-
-        return out
-    return evaluate
-
 
 def get_blur(input):
 
@@ -65,13 +8,11 @@ def get_blur(input):
 
     x, y = Var('x'), Var('y')
 
-    #clamped_input = repeat_edge(cast(UInt(16), input))
-    #ci = clamped_input
     clamped_input = repeat_edge(input)
 
     input_uint16 = Func('input_uint16')
     input_uint16[x,y] = cast(UInt(16), clamped_input[x,y])
-    ci = input_uint16 # cast does not work over ImageParam ?
+    ci = input_uint16
 
     blur_x = Func('blur_x')
     blur_y = Func('blur_y')
@@ -87,10 +28,13 @@ def get_blur(input):
     return blur_y
 
 def main():
+
+    # define and compile the function
     input = ImageParam(UInt(8), 2, "input_param")
     blur = get_blur(input)
     blur.compile_jit()
 
+    # preparing input and output memory buffers (numpy ndarrays)
     import numpy as np
     from scipy.misc import imread, imsave
     import os.path
@@ -112,27 +56,22 @@ def main():
     print("input_image", input_image)
     print("output_image", output_image)
 
+    # do the actual computation
     blur.realize(output_image)
 
+    # save results
+    input_path = "blur_input.png"
     output_path = "blur_result.png"
-    print("blur realized on output image.",
-          "Result saved at", output_path)
-
-    imsave("input_data.png", grey_data)
+    imsave(input_path, grey_data)
     imsave(output_path, output_data)
+    print("\nblur realized on output image.",
+          "Result saved at", output_path,
+          "( input data copy at", input_path, ")")
 
+
+    print("\nEnd of game. Have a nice day!")
     return
 
-    print("ping 3")
-    maxval = 255
-    in_image = Image(UInt(16), builtin_image('rgb.png'), scale=1.0) # Set scale to 1 so that we only use 0...255 of the UInt(16) range
-    print("ping 4")
-    eval_func = filter_image(input, blur_y, in_image, disp_time=True, out_dims = (OUT_DIMS[0]-8, OUT_DIMS[1]-8), times=5)
-    I = eval_func()
-    if len(sys.argv) >= 2:
-        I.save(sys.argv[1], maxval)
-    else:
-        I.show(maxval)
 
 if __name__ == '__main__':
     main()
