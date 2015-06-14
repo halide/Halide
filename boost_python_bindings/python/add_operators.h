3825b7164f1eca65bd51d43f34d19efdb7d9c3ce
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-14 21:43:21 +0200

    Added exception message for '//' operator

--- boost_python_bindings/python/add_operators.h
+++ boost_python_bindings/python/add_operators.h
@@ -5,10 +5,21 @@
 #include <boost/python/self.hpp>
 //#include <boost/python/def.hpp>
 
+
+template<typename A, typename B, typename ReturnType>
+ReturnType floordiv(A /*a*/, B /*b*/)
+{
+    throw std::invalid_argument("Halide floordiv not yet implemented, use '/' instead.");
+    return ReturnType();
+}
+
+
 template<typename PythonClass, typename T>
 void add_operators_with(PythonClass &class_instance)
 {
     using namespace boost::python;
+
+    typedef typename PythonClass::wrapped_type wrapped_t;
 
     class_instance
             .def(self + T())
@@ -51,7 +62,12 @@ void add_operators_with(PythonClass &class_instance)
             .def(T() > self)
 
             .def(self >= T())
-            .def(T() >= self);
+            .def(T() >= self)
+
+            .def("__floordiv__", &floordiv<wrapped_t, T, wrapped_t>)
+            .def("__floordiv__", &floordiv<T, wrapped_t, wrapped_t>)
+
+            ;
 
     return;
 }
@@ -60,6 +76,11 @@ template<typename PythonClass>
 void add_operators(PythonClass &class_instance)
 {
     using namespace boost::python;
+
+    // The order of definitions matters.
+    // Python first will try input value as int, then float, then as same class (Expr, Var, etc.)
+    add_operators_with<PythonClass, int>(class_instance);
+    add_operators_with<PythonClass, float>(class_instance);
 
     class_instance
             .def(self + self)
@@ -80,8 +101,6 @@ void add_operators(PythonClass &class_instance)
             .def(self > self)
             .def(self >= self);
 
-    add_operators_with<PythonClass, int>(class_instance);
-    add_operators_with<PythonClass, float>(class_instance);
     return;
 }
 
