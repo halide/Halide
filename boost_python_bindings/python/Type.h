da7bbc0505c49ccfa474b1cfacfb6682fc85cc2b
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-14 16:10:01 +0200

    Working on C++ and Boost.Python magic to get Param(...) working as desired. Code does not compile.
    Former-commit-id: 1dec98c598194350e6e7b45375299deccac4c006

--- boost_python_bindings/python/Type.h
+++ boost_python_bindings/python/Type.h
@@ -1,6 +1,14 @@
 #ifndef TYPE_H
 #define TYPE_H
 
+#include <string>
+
+namespace Halide {
+class Type; // forward declaration
+}
+
 void defineType();
+
+std::string type_repr(const Halide::Type &t); // helper function
 
 #endif // TYPE_H
