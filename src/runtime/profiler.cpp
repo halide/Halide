aa74c006e3cfc931dbd17936fb7c82a8ff759b01
Andrew Adams <andrew.b.adams@gmail.com>
2015-06-18 11:39:25 -0700

    Expose profiler state in HalideRuntime.h

--- src/runtime/profiler.cpp
+++ src/runtime/profiler.cpp
@@ -1,6 +1,5 @@
 #include "runtime_internal.h"
 #include "HalideRuntime.h"
-#include "profiler_state.h"
 #include "scoped_mutex_lock.h"
 
 // Note: The profiler thread may out-live any valid user_context, or
@@ -9,26 +8,29 @@
 
 extern "C" {
 // Returns the address of the global halide_profiler state
-WEAK profiler_state *halide_profiler_get_state() {
-    static profiler_state s;
+WEAK halide_profiler_state *halide_profiler_get_state() {
+    static halide_profiler_state s = {{{0}}, NULL, 1000000, 0, 0, false};
     return &s;
 }
 }
 
 namespace Halide { namespace Runtime { namespace Internal {
 
-WEAK pipeline_stats *find_or_create_pipeline(const char *pipeline_name, int num_funcs, const char **func_names) {
-    profiler_state *s = halide_profiler_get_state();
+WEAK halide_profiler_pipeline_stats *find_or_create_pipeline(const char *pipeline_name, int num_funcs, const char **func_names) {
+    halide_profiler_state *s = halide_profiler_get_state();
 
-    for (pipeline_stats *p = s->pipelines; p; p = (pipeline_stats *)(p->next)) {
-        // The same pipeline will deliver the same global constant string, so they can be compared by pointer.
+    for (halide_profiler_pipeline_stats *p = s->pipelines; p;
+         p = (halide_profiler_pipeline_stats *)(p->next)) {
+        // The same pipeline will deliver the same global constant
+        // string, so they can be compared by pointer.
         if (p->name == pipeline_name &&
             p->num_funcs == num_funcs) {
             return p;
         }
     }
     // Create a new pipeline stats entry.
-    pipeline_stats *p = (pipeline_stats *)halide_malloc(NULL, sizeof(pipeline_stats));
+    halide_profiler_pipeline_stats *p =
+        (halide_profiler_pipeline_stats *)halide_malloc(NULL, sizeof(halide_profiler_pipeline_stats));
     if (!p) return NULL;
     p->next = s->pipelines;
     p->name = pipeline_name;
@@ -37,7 +39,7 @@ WEAK pipeline_stats *find_or_create_pipeline(const char *pipeline_name, int num_
     p->runs = 0;
     p->time = 0;
     p->samples = 0;
-    p->funcs = (func_stats *)halide_malloc(NULL, num_funcs * sizeof(func_stats));
+    p->funcs = (halide_profiler_func_stats *)halide_malloc(NULL, num_funcs * sizeof(halide_profiler_func_stats));
     if (!p->funcs) {
         halide_free(NULL, p);
         return NULL;
@@ -51,13 +53,14 @@ WEAK pipeline_stats *find_or_create_pipeline(const char *pipeline_name, int num_
     return p;
 }
 
-WEAK void bill_func(profiler_state *s, int func_id, uint64_t time) {
-    pipeline_stats *p_prev = NULL;
-    for (pipeline_stats *p = s->pipelines; p; p = (pipeline_stats *)(p->next)) {
+WEAK void bill_func(halide_profiler_state *s, int func_id, uint64_t time) {
+    halide_profiler_pipeline_stats *p_prev = NULL;
+    for (halide_profiler_pipeline_stats *p = s->pipelines; p;
+         p = (halide_profiler_pipeline_stats *)(p->next)) {
         if (func_id >= p->first_func_id && func_id < p->first_func_id + p->num_funcs) {
             if (p_prev) {
                 // Bubble the pipeline to the top to speed up future queries.
-                p_prev->next = (pipeline_stats *)(p->next);
+                p_prev->next = (halide_profiler_pipeline_stats *)(p->next);
                 p->next = s->pipelines;
                 s->pipelines = p;
             }
@@ -73,7 +76,7 @@ WEAK void bill_func(profiler_state *s, int func_id, uint64_t time) {
         << "Internal error in profiler: \n"
         << "Could not find pipeline for func_id " << func_id << "\n"
         << "Pipelines:\n";
-    for (pipeline_stats *p = s->pipelines; p; p = (pipeline_stats *)(p->next)) {
+    for (halide_profiler_pipeline_stats *p = s->pipelines; p; p = (halide_profiler_pipeline_stats *)(p->next)) {
         print(NULL) << p->name << " : " << p->first_func_id << ", " << p->num_funcs << "\n";
     }
     error(NULL) << "Could not proceed.\n";
@@ -82,19 +85,19 @@ WEAK void bill_func(profiler_state *s, int func_id, uint64_t time) {
 extern "C" void usleep(int);
 
 WEAK void *sampling_profiler_thread(void *) {
-    profiler_state *s = halide_profiler_get_state();
+    halide_profiler_state *s = halide_profiler_get_state();
 
     // grab the lock
     halide_mutex_lock(&s->lock);
 
-    while (s->current_func != please_stop) {
+    while (s->current_func != halide_profiler_please_stop) {
 
         uint64_t t1 = halide_current_time_ns(NULL);
         uint64_t t = t1;
         while (1) {
             uint64_t t_now = halide_current_time_ns(NULL);
             int func = s->current_func;
-            if (func == please_stop) {
+            if (func == halide_profiler_please_stop) {
                 break;
             } else if (func >= 0) {
                // Assume all time since I was last awake is due to
@@ -104,8 +107,9 @@ WEAK void *sampling_profiler_thread(void *) {
             t = t_now;
 
             // Release the lock, sleep, reacquire.
+            uint64_t sleep_ns = s->sleep_time;
             halide_mutex_unlock(&s->lock);
-            usleep(1000);
+            usleep(sleep_ns/1000);
             halide_mutex_lock(&s->lock);
         }
     }
@@ -125,7 +129,7 @@ WEAK int halide_profiler_pipeline_start(void *user_context,
                                         const char *pipeline_name,
                                         int num_funcs,
                                         const char **func_names) {
-    profiler_state *s = halide_profiler_get_state();
+    halide_profiler_state *s = halide_profiler_get_state();
 
     ScopedMutexLock lock(&s->lock);
 
@@ -134,7 +138,8 @@ WEAK int halide_profiler_pipeline_start(void *user_context,
         s->started = true;
     }
 
-    pipeline_stats *p = find_or_create_pipeline(pipeline_name, num_funcs, func_names);
+    halide_profiler_pipeline_stats *p =
+        find_or_create_pipeline(pipeline_name, num_funcs, func_names);
     if (!p) {
         // Allocating space to track the statistics failed.
         return halide_error_out_of_memory(user_context);
@@ -147,14 +152,15 @@ WEAK int halide_profiler_pipeline_start(void *user_context,
 }
 
 WEAK void halide_profiler_report(void *user_context) {
-    profiler_state *s = halide_profiler_get_state();
+    halide_profiler_state *s = halide_profiler_get_state();
 
     char line_buf[160];
     Printer<StringStreamPrinter, sizeof(line_buf)> sstr(user_context, line_buf);
 
     ScopedMutexLock lock(&s->lock);
 
-    for (pipeline_stats *p = s->pipelines; p; p = (pipeline_stats *)(p->next)) {
+    for (halide_profiler_pipeline_stats *p = s->pipelines; p;
+         p = (halide_profiler_pipeline_stats *)(p->next)) {
         float t = p->time / 1000000.0f;
         if (!p->runs) continue;
         sstr.clear();
@@ -167,7 +173,7 @@ WEAK void halide_profiler_report(void *user_context) {
         if (p->time) {
             for (int i = 0; i < p->num_funcs; i++) {
                 sstr.clear();
-                func_stats *fs = p->funcs + i;
+                halide_profiler_func_stats *fs = p->funcs + i;
                 sstr << "  " << fs->name << ": ";
                 while (sstr.size() < 25) sstr << " ";
 
@@ -185,13 +191,13 @@ WEAK void halide_profiler_report(void *user_context) {
 }
 
 WEAK void halide_profiler_reset() {
-    profiler_state *s = halide_profiler_get_state();
+    halide_profiler_state *s = halide_profiler_get_state();
 
     ScopedMutexLock lock(&s->lock);
 
     while (s->pipelines) {
-        pipeline_stats *p = s->pipelines;
-        s->pipelines = (pipeline_stats *)(p->next);
+        halide_profiler_pipeline_stats *p = s->pipelines;
+        s->pipelines = (halide_profiler_pipeline_stats *)(p->next);
         halide_free(NULL, p->funcs);
         halide_free(NULL, p);
     }
@@ -201,14 +207,14 @@ WEAK void halide_profiler_reset() {
 namespace {
 __attribute__((destructor))
 WEAK void halide_profiler_shutdown() {
-    profiler_state *s = halide_profiler_get_state();
-    s->current_func = please_stop;
+    halide_profiler_state *s = halide_profiler_get_state();
+    s->current_func = halide_profiler_please_stop;
     do {
         // Memory barrier.
         __sync_synchronize(&s->started,
                            &s->current_func);
     } while (s->started);
-    s->current_func = outside_of_halide;
+    s->current_func = halide_profiler_outside_of_halide;
 
     // print results
     halide_profiler_report(NULL);
@@ -219,7 +225,7 @@ WEAK void halide_profiler_shutdown() {
 }
 
 WEAK void halide_profiler_pipeline_end(void *user_context, void *state) {
-    ((profiler_state *)state)->current_func = outside_of_halide;
+    ((halide_profiler_state *)state)->current_func = halide_profiler_outside_of_halide;
 }
 
 }
