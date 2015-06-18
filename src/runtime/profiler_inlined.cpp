cbf3a74f8f0f9cf7cb8b0232067f99e2fc0edb43
Andrew Adams <andrew.b.adams@gmail.com>
2015-06-18 11:39:25 -0700

    Expose profiler state in HalideRuntime.h
    Former-commit-id: aa74c006e3cfc931dbd17936fb7c82a8ff759b01

--- src/runtime/profiler_inlined.cpp
+++ src/runtime/profiler_inlined.cpp
@@ -1,10 +1,9 @@
 #include "HalideRuntime.h"
 #include "runtime_internal.h"
-#include "profiler_state.h"
 
 extern "C" {
 
-WEAK __attribute__((always_inline)) int halide_profiler_set_current_func(profiler_state *state, int tok, int t) {
+WEAK __attribute__((always_inline)) int halide_profiler_set_current_func(halide_profiler_state *state, int tok, int t) {
     state->current_func = tok + t;
     return 0;
 }
