f571f46bdf53adc679e2722c872377e6592c62eb
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-19 23:47:54 +0200

    lesson_10_aot_compilation_run.py works as desired
    Former-commit-id: 5c825e8dded30eb44d9123aad8c3b7a583202b2d

--- boost_python_bindings/tutorial/lesson_03_debugging_1.py
+++ boost_python_bindings/tutorial/lesson_03_debugging_1.py
@@ -61,7 +61,9 @@ def main():
     # the generated function should take (none in this case), and the
     # name of the generated function. Have a look inside gradient.cpp
     # after compiling and running this lesson.
-    gradient.compile_to_c("gradient.cpp", [], "gradient")
+    args = ArgumentsVector()
+    # args stays empty
+    gradient.compile_to_c("gradient.cpp", args, "gradient")
 
     # Using these two tricks -- setting HL_DEBUG_CODEGEN and calling
     # compile_to_c -- you can usually figure out what code Halide is
