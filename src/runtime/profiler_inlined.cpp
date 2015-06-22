0def6b14725930d5a89948bbc3651fced588543d
Andrew Adams <andrew.b.adams@gmail.com>
2015-06-22 16:16:01 -0700

    Add test for profiler
    Turns out we needed some memory fences

--- src/runtime/profiler_inlined.cpp
+++ src/runtime/profiler_inlined.cpp
@@ -4,7 +4,9 @@
 extern "C" {
 
 WEAK __attribute__((always_inline)) int halide_profiler_set_current_func(halide_profiler_state *state, int tok, int t) {
+    __sync_synchronize();
     state->current_func = tok + t;
+    __sync_synchronize();
     return 0;
 }
 
