239377313711992476b7ac17af9c7273299cdcc0
Alexander Aprelev <aam@google.com>
2015-06-23 21:53:50 -0700

    Fix slots_taken() declaration.
    Former-commit-id: b1eca2bf8dafab4786139bb9fc6a059b77daadf2

--- src/CodeGen_Renderscript_Dev.h
+++ src/CodeGen_Renderscript_Dev.h
@@ -40,7 +40,8 @@ public:
 
     std::string api_unique_name() { return "renderscript"; }
 
-size_t slots_taken() const;
+    virtual size_t slots_taken() const;
+
 protected:
     using CodeGen_LLVM::visit;
 
