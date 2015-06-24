dc8975cd3eb3537749e020084fe9af680e457743
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2015-06-25 00:13:00 +0200

    interpolate.py runs as desired.

--- python_bindings/apps/blur.py
+++ python_bindings/apps/blur.py
@@ -54,11 +54,11 @@ def main():
 
     # preparing input and output memory buffers (numpy ndarrays)
     input_data = get_input_data()
-    input_image = ndarray_to_image(input_data, "input_image")
+    input_image = Image(input_data, "input_image")
     input.set(input_image)
 
     output_data = np.empty(input_data.shape, dtype=input_data.dtype, order="F")
-    output_image = ndarray_to_image(output_data, "output_image")
+    output_image = Image(output_data, "output_image")
 
     print("input_image", input_image)
     print("output_image", output_image)
