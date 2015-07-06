f4da7fb6760ee141205448127528c378fb0fb60e
Andrew Adams <andrew.b.adams@gmail.com>
2015-07-06 14:48:35 -0700

    Add a NoRuntime target feature
    Prevents inclusion of the runtime in AOT-compiled code.
    
    Uses the same split as JIT does, so inlined math stuff is still included
    in every module.
    
    Declarations for the runtime API are provided to codegen by including a
    runtime module that contains them as extern declarations (using an
    attribute((used)) trick). This same runtime module also simplifies the
    similar logic for JITting.
    
    Also added a method to just compile a runtime, and a generator command-
    line flag that does that (so any Generator can give you a copy of the
    runtime for any target).

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -118,6 +118,7 @@ DECLARE_CPP_INITMOD(posix_get_symbol)
 DECLARE_CPP_INITMOD(osx_get_symbol)
 DECLARE_CPP_INITMOD(windows_get_symbol)
 DECLARE_CPP_INITMOD(renderscript)
+DECLARE_CPP_INITMOD(runtime_api)
 
 #ifdef WITH_ARM
 DECLARE_LL_INITMOD(arm)
@@ -515,6 +516,7 @@ void add_underscores_to_posix_calls_on_windows(llvm::Module *m) {
 llvm::Module *get_initial_module_for_target(Target t, llvm::LLVMContext *c, bool for_shared_jit_runtime, bool just_gpu) {
     enum InitialModuleType {
         ModuleAOT,
+        ModuleAOTNoRuntime,
         ModuleJITShared,
         ModuleJITInlined,
         ModuleGPU
@@ -528,6 +530,8 @@ llvm::Module *get_initial_module_for_target(Target t, llvm::LLVMContext *c, bool
         } else {
             module_type = ModuleJITInlined;
         }
+    } else if (t.has_feature(Target::NoRuntime)) {
+        module_type = ModuleAOTNoRuntime;
     } else {
         module_type = ModuleAOT;
     }
@@ -544,7 +548,7 @@ llvm::Module *get_initial_module_for_target(Target t, llvm::LLVMContext *c, bool
     vector<llvm::Module *> modules;
 
     if (module_type != ModuleGPU) {
-        if (module_type != ModuleJITInlined) {
+        if (module_type != ModuleJITInlined && module_type != ModuleAOTNoRuntime) {
             // OS-dependent modules
             if (t.os == Target::Linux) {
                 modules.push_back(get_initmod_linux_clock(c, bits_64, debug));
@@ -597,7 +601,7 @@ llvm::Module *get_initial_module_for_target(Target t, llvm::LLVMContext *c, bool
             modules.push_back(get_initmod_destructors(c, bits_64, debug));
         }
 
-        if (module_type != ModuleJITInlined) {
+        if (module_type != ModuleJITInlined && module_type != ModuleAOTNoRuntime) {
             // These modules are always used and shared
             modules.push_back(get_initmod_gpu_device_selection(c, bits_64, debug));
             modules.push_back(get_initmod_tracing(c, bits_64, debug));
@@ -676,6 +680,11 @@ llvm::Module *get_initial_module_for_target(Target t, llvm::LLVMContext *c, bool
 
     if (module_type == ModuleAOT && t.has_feature(Target::Matlab)) {
         modules.push_back(get_initmod_matlab(c, bits_64, debug));
+    }
+
+    if (module_type == ModuleAOTNoRuntime ||
+        module_type == ModuleJITInlined) {
+        modules.push_back(get_initmod_runtime_api(c, bits_64, debug));
     }
 
     link_modules(modules, t);
