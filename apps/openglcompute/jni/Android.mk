6629969174e149110b0b7277c6ccd184b3c8179a
Alexander Aprelev <aam@google.com>
2015-07-26 21:36:17 -0400

    Support multiple kernels.

--- apps/openglcompute/jni/Android.mk
+++ apps/openglcompute/jni/Android.mk
@@ -33,4 +33,37 @@ LOCAL_C_INCLUDES += ./
 
 include $(BUILD_SHARED_LIBRARY)
 
+# === oglc_two_kernels_run ===
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE           := oglc_two_kernels_run
+LOCAL_SRC_FILES        := oglc_two_kernels_run.cpp
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+LOCAL_STATIC_LIBRARIES += libOpengl
+LOCAL_LDLIBS           := -lm -llog -landroid -lEGL -lGLESv2 two_kernels_filter.o
+LOCAL_ARM_MODE         := arm
+
+LOCAL_CPPFLAGS += -std=c++11 -I../support -I../../include
+
+LOCAL_C_INCLUDES += ./
+
+include $(BUILD_EXECUTABLE)
+
+# === oglc_two_kernels library ===
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE           := oglc_two_kernels
+LOCAL_SRC_FILES        := oglc_two_kernels_run.cpp
+LOCAL_STATIC_LIBRARIES += libOpengl
+LOCAL_LDLIBS           := -lm -llog -landroid -lEGL -lGLESv2 two_kernels_filter.o
+LOCAL_ARM_MODE         := arm
+
+LOCAL_CPPFLAGS += -std=c++11 -I../support -I../../include
+
+LOCAL_C_INCLUDES += ./
+
+include $(BUILD_SHARED_LIBRARY)
+
 $(call import-module,android/native_app_glue)
