f24514091aa563ee134a63b30254a02c08417869
Dillon Sharlet <dsharlet@google.com>
2015-07-28 15:55:41 -0700

    Use high_resolution_clock.
    Former-commit-id: 4be8a0a7d761e9914c83770edd2c9795a20a16d6

--- apps/support/benchmark.h
+++ apps/support/benchmark.h
@@ -40,11 +40,11 @@ template <typename F>
 double benchmark(int samples, int iterations, F op) {
     double best = std::numeric_limits<double>::infinity();
     for (int i = 0; i < samples; i++) {
-        auto t1 = std::chrono::system_clock::now();
+        auto t1 = std::chrono::high_resolution_clock::now();
         for (int j = 0; j < iterations; j++) {
             op();
         }
-        auto t2 = std::chrono::system_clock::now();
+        auto t2 = std::chrono::high_resolution_clock::now();
         double dt = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() / 1e6;
         if (dt < best) best = dt;
     }
