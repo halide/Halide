5b4f4d048608046a336ed3b28ddf53dbb98e9515
Andrew Adams <andrew.b.adams@gmail.com>
2015-07-28 10:03:56 -0700

    Simplify some code in CodeGen_C
    Former-commit-id: 233dd78c8943a14b7bd0de9ced183e91ee84f5d0

--- src/CodeGen_C.cpp
+++ src/CodeGen_C.cpp
@@ -717,9 +717,7 @@ void CodeGen_C::visit(const Not *op) {
 }
 
 void CodeGen_C::visit(const IntImm *op) {
-    ostringstream oss;
-    oss << op->value;
-    id = oss.str();
+    id = std::to_string(op->value);
 }
 
 void CodeGen_C::visit(const StringImm *op) {
@@ -1358,11 +1356,8 @@ void CodeGen_C::visit(const IfThenElse *op) {
 }
 
 void CodeGen_C::visit(const Evaluate *op) {
+    if (is_const(op->value)) return;
     string id = print_expr(op->value);
-    if (id == "0") {
-        // Skip evaluate(0) nodes. They're how we represent no-ops.
-        return;
-    }
     do_indent();
     stream << "(void)" << id << ";\n";
 }
