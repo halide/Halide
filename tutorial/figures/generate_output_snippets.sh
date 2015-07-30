71ad734c7695ebbf57c907aa3d5245059dd8fc30
Andrew Adams <andrew.b.adams@gmail.com>
2015-07-30 15:46:14 -0700

    Make output snippet script more reliable

--- tutorial/figures/generate_output_snippets.sh
+++ tutorial/figures/generate_output_snippets.sh
@@ -1,6 +1,6 @@
 for f in ../lesson_*.cpp; do
     # Figure out which source lines contain a realize call
-    LINES=$(egrep -n "[.]realize|[.]print_loop_nest" $f  | cut -d':' -f1)
+    LINES=$(egrep -n '[.]realize\(|print_loop_nest\(\)|Printing a complex Expr' $f  | grep -v ": *//" | cut -d':' -f1)
 
     echo $LINES
 
@@ -18,25 +18,36 @@ for f in ../lesson_*.cpp; do
     rm -f gdb_script.txt
     rm -f stderr.txt
     touch gdb_script.txt
-    echo "set args -s 2>stderr.txt" >> gdb_script.txt
-    echo start >> gdb_script.txt
+    echo "set args -s 2> stderr.txt > stdout.txt" >> gdb_script.txt
+    echo "start" >> gdb_script.txt
     for l in $LINES; do
         echo advance ${FILENAME}:${l} >> gdb_script.txt
+        echo call "fflush(stdout)" >> gdb_script.txt
+        echo call "fflush(stderr)" >> gdb_script.txt
         echo call "fprintf(stderr, \"BEGIN_REALIZE_${l}_\\n\")" >> gdb_script.txt
+        echo call "fprintf(stdout, \"BEGIN_REALIZE_${l}_\\n\")" >> gdb_script.txt
+        echo call "fflush(stdout)" >> gdb_script.txt
+        echo call "fflush(stderr)" >> gdb_script.txt
         echo next >> gdb_script.txt
+        echo call "usleep(10000)" >> gdb_script.txt
         echo call "fprintf(stderr, \"END_REALIZE_${l}_\\n\")" >> gdb_script.txt
+        echo call "fprintf(stdout, \"END_REALIZE_${l}_\\n\")" >> gdb_script.txt
+        echo call "fflush(stdout)" >> gdb_script.txt
+        echo call "fflush(stderr)" >> gdb_script.txt
     done
+
 
     cd ..
     LD_LIBRARY_PATH=../bin gdb ../bin/tutorial_${LESSON} < figures/gdb_script.txt
-    mv stderr.txt figures/
+    mv stdout.txt stderr.txt figures/
     cd figures
 
     # get the output for each realize call
     rm -f ${LESSON_ROOT}_output_*.txt
     for l in $LINES; do
         F=${LESSON_ROOT}_output_${l}.txt
-        sed -n "/BEGIN_REALIZE_${l}_/,/END_REALIZE_${l}_/p" < stderr.txt | grep -v _REALIZE > ${F}
+        cat stdout.txt | sed -n "/BEGIN_REALIZE_${l}_/,/END_REALIZE_${l}_/p" | grep -v _REALIZE > ${F}
+        cat stderr.txt | sed -n "/BEGIN_REALIZE_${l}_/,/END_REALIZE_${l}_/p" | grep -v _REALIZE >> ${F}
         # delete the empty ones
         if [ ! -s ${F} ]; then
             rm ${F}
