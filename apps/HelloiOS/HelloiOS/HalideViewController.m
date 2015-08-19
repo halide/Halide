a7fd014fe52e66b17c6280b4682907e66e24a70b
Z Stern <zalman@zalman-macbookpro2.roam.corp.google.com>
2015-08-18 17:16:44 -0700

    Clean up changes due to (stupid) Xcode git integration.
    Former-commit-id: 0cfdff45b815b3124bc387e324ecffdec4ac4a98

--- apps/HelloiOS/HelloiOS/HalideViewController.m
+++
@@ -1,9 +0,0 @@
-//
-//  HalideViewController.m
-//  HelloiOS
-//
-//  Created by Z Stern on 8/10/15.
-//  Copyright (c) 2015 Andrew Adams. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
