cddd570df6110ed36182a06a7e01c04f851db12d
Z Stern <zalman@zalman-macbookpro2.roam.corp.google.com>
2015-08-18 19:29:32 -0700

    Backut a couple of changes relative to master that don't seem correct.

--- src/LLVM_Output.cpp
+++ src/LLVM_Output.cpp
@@ -83,6 +83,9 @@ void get_target_options(const llvm::Module *module, llvm::TargetOptions &options
     #endif
     options.NoZerosInBSS = false;
     options.GuaranteedTailCallOpt = false;
+    #if LLVM_VERSION < 37
+    options.DisableTailCalls = false;
+    #endif
     options.StackAlignmentOverride = 0;
     #if LLVM_VERSION < 37
     options.TrapFuncName = "";
