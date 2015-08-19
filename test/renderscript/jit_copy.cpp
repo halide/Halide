bda6166291b7ad3006d851c8a7d2ab24c4bf91e2
Andrew Adams <andrew.b.adams@gmail.com>
2015-08-19 11:06:00 -0700

    Avoid variable-sized stack allocations
    msvc doesn't like them
    
    
    Former-commit-id: 65842337f5849ed9c10009a95bb794d31e27e83f

--- test/renderscript/jit_copy.cpp
+++ test/renderscript/jit_copy.cpp
@@ -196,8 +196,8 @@ void copy_interleaved(bool vectorized = false, int channels = 4) {
         .set_stride(1, Halide::Expr())
         .set_stride(2, 1)
         .set_bounds(2, 0, channels);  // expecting interleaved image
-    uint8_t in_buf[128 * 128 * channels];
-    uint8_t out_buf[128 * 128 * channels];
+    uint8_t *in_buf = new uint8_t[128 * 128 * channels];
+    uint8_t *out_buf = new uint8_t[128 * 128 * channels];
     Image<uint8_t> in = make_interleaved_image(in_buf, 128, 128, channels);
     Image<uint8_t> out = make_interleaved_image(out_buf, 128, 128, channels);
     input8.set(in);
@@ -220,6 +220,9 @@ void copy_interleaved(bool vectorized = false, int channels = 4) {
             new ValidateInterleavedVectorizedPipeline(channels):
             new ValidateInterleavedPipeline(channels));
     result.realize(out);
+
+    delete[] in_buf;
+    delete[] out_buf;
 }
 
 int main(int argc, char **argv) {
