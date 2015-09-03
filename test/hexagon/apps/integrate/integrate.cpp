795816755e82876bafc8d1bd17fe21d9bbf6ae0e
Ron Lieberman <ronl@codeaurora.org>
2015-09-03 12:12:17 -0500

    [20536] Enable vector schedule in integrate
    Change-Id: I4f7fde534efeecca254434c78c24b1a5d540ba8b
    Reviewed-on: https://review-hexagon.quicinc.com/33419
    Reviewed-by: Pranav Bhandarkar <pranavb@codeaurora.org>
    Reviewed-by: Ron Lieberman <ronl@quicinc.com>
    Tested-by: Ron Lieberman <ronl@quicinc.com>

--- test/hexagon/apps/integrate/integrate.cpp
+++ test/hexagon/apps/integrate/integrate.cpp
@@ -28,9 +28,8 @@ int main(int argc, char **argv) {
     integrate(r1.x, r1.y) += integrate(r1.x - 1, r1.y);
     RDom r0(0, 1, 1, In.height()-1);
     integrate(x, r0.y) += integrate(x, r0.y - 1);
-#ifdef DOVECTOR
     integrate.vectorize(x, 1 << LOG2VLEN);
-#endif
+
     std::vector<Argument> args(1);
     args[0] = In;
 #ifdef BITCODE
