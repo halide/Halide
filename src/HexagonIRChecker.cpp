02d2cb43362bf749e8da3e3c98e0657353974a7a
Pranav Bhandarkar <pranavb@codeaurora.org>
2015-10-14 16:49:21 -0500

    [QTOOL-1523] - Warn if we have Vector Quads generated
    Change-Id: I38619b693083018b66386ebb03b94f8dbc915a0a
    Reviewed-on: https://review-hexagon.quicinc.com/36243
    Reviewed-by: Dan Palermo <dpalermo@codeaurora.org>
    Tested-by: Anshu Das Gupta <adasgupt@quicinc.com>
    Reviewed-by: Pranav Bhandarkar <pranavb@codeaurora.org>
    Tested-by: Pranav Bhandarkar <pranavb@codeaurora.org>

--- src/HexagonIRChecker.cpp
+++ src/HexagonIRChecker.cpp
@@ -11,6 +11,12 @@ using std::vector;
 
 class HexagonIRChecker  : public IRMutator {
   using IRMutator::visit;
+public:
+  enum HVX_mode {
+    Single,
+    Double
+  };
+
   void visit(const EQ *cmp_op) {
     expr = check_type (cmp_op);
     return;
@@ -36,21 +42,43 @@ class HexagonIRChecker  : public IRMutator {
     expr = check_type (cmp_op);
     return;
   }
+  void visit(const Cast *cast_op) {
+    expr = check_type (cast_op);
+  }
+
 private:
-  bool is_invalid_type(Type t) {
+  bool is_invalid_type(Type t, HexagonIRChecker::HVX_mode m ) {
     if (!t.is_vector())
       return false;
-    if (t.bits * t.width != hexagon_vector_size_bits())
+    if (t.bits * t.width != hexagon_vector_size_bits(m))
       return true;
     return false;
   }
-  int hexagon_vector_size_bits() {
-    // Do double mode for now.
-    return 128*8;
+  bool is_vector_quad(Type t, HexagonIRChecker::HVX_mode m) {
+    if (!t.is_vector())
+      return false;
+    if (t.bits * t.width == 4 * hexagon_vector_size_bits(m))
+      return true;
+    return false;
+  }
+  int hexagon_vector_size_bits(HexagonIRChecker::HVX_mode m) {
+    switch(m) {
+    case HexagonIRChecker::Single: return 64 * 8;
+    case HexagonIRChecker::Double: return 128 * 8;
+    }
   }
   Expr check_type(const BaseExprNode *op) {
-    if(is_invalid_type(op->type)) {
-      user_warning << "Invalid type" << (Expr) op << "\n";
+    if(is_invalid_type(op->type, HexagonIRChecker::Single)) {
+      user_warning << "Invalid type:{SINGLE_MODE}: (" << op->type << "): " << (Expr) op << "\n";
+    }
+    if(is_invalid_type(op->type, HexagonIRChecker::Double)) {
+      user_warning << "Invalid type:{DOUBLE_MODE}: (" << op->type << "): " << (Expr) op << "\n";
+    }
+    if(is_vector_quad(op->type, HexagonIRChecker::Single)) {
+      user_warning << "Vector Quad:{SINGLE_MODE}: (" << op->type << "): " << (Expr) op << "\n";
+    }
+    if(is_vector_quad(op->type, HexagonIRChecker::Double)) {
+      user_warning << "Vector Quad:{DOUBLE_MODE}: (" << op->type << "): " << (Expr) op << "\n";
     }
     return op;
   }
