152a00135af46dfeb077f600cf63a05299fd283d
Tyler Denniston <tyler@csail.mit.edu>
2015-10-28 14:25:08 -0400

    Fix *_larger_than_two_gigs tests.
    Former-commit-id: 57cdc1ebe099f798d87bcdd9d14bde11eb07e558

--- test/correctness/output_larger_than_two_gigs.cpp
+++ test/correctness/output_larger_than_two_gigs.cpp
@@ -11,37 +11,35 @@ void halide_error(void *ctx, const char *msg) {
 using namespace Halide;
 
 int main(int argc, char **argv) {
-    Target t = get_jit_target_from_environment();
-
     Var x, y, z;
     Func identity_uint8;
     identity_uint8(x, y, z) = cast<uint8_t>(42);
+
+    uint8_t c[4096];
+    memset(c, 42, sizeof(c));
+
+    buffer_t buf;
+    memset(&buf, 0, sizeof(buf));
+    buf.host = c;
+    buf.extent[0] = 4096;
+    buf.extent[1] = 4096;
+    buf.extent[2] = 256;
+    buf.stride[0] = 1;
+    buf.stride[1] = 0;
+    buf.stride[2] = 0;
+    buf.elem_size = 1;
+
     identity_uint8.set_error_handler(&halide_error);
 
-    if (t.bits == 64) {
-        Image<uint8_t> output(4096, 4096, 256);
-        assert(output.data());
-        identity_uint8.realize(output);
-    } else {
-        uint8_t c[4096];
-        memset(c, 42, sizeof(c));
+    Buffer output_buf(UInt(8), &buf);
+    identity_uint8.realize(output_buf);
 
-        buffer_t buf;
-        memset(&buf, 0, sizeof(buf));
-        buf.host = c;
-        buf.extent[0] = 4096;
-        buf.extent[1] = 4096;
-        buf.extent[2] = 256;
-        buf.stride[0] = 1;
-        buf.stride[1] = 0;
-        buf.stride[2] = 0;
-        buf.elem_size = 1;
-        Buffer output_buf(UInt(8), &buf);
-        identity_uint8.realize(output_buf);
-    }
-
+    Target t = get_jit_target_from_environment();
     if (t.bits == 64) {
         assert(!error_occurred);
+        Image<uint8_t> output = output_buf;
+        assert(output(0, 0, 0) == 42);
+        assert(output(output.extent(0) - 1, output.extent(1) - 1, output.extent(2) - 1) == 42);
     } else {
         assert(error_occurred);
     }
