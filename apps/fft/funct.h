ceff3807c207613b4ce68419bc4f3cd29dca6be2
Dillon Sharlet <dsharlet@google.com>
2015-11-10 13:55:58 -0800

    Reformatting indentation.
    Former-commit-id: 7b74f85ccfec77a949913c15f767919cca25cc54

--- apps/fft/funct.h
+++ apps/fft/funct.h
@@ -8,70 +8,70 @@
 
 template <typename T>
 class FuncRefVarT : public T {
-  Halide::FuncRefVar untyped;
+    Halide::FuncRefVar untyped;
 
- public:
-  typedef Halide::Stage Stage;
-  typedef Halide::Tuple Tuple;
+public:
+    typedef Halide::Stage Stage;
+    typedef Halide::Tuple Tuple;
 
-  FuncRefVarT(const Halide::FuncRefVar& untyped)
-      : T(untyped.function().has_pure_definition() ? T(Tuple(untyped)) : T()),
-        untyped(untyped) {}
+    FuncRefVarT(const Halide::FuncRefVar& untyped)
+        : T(untyped.function().has_pure_definition() ? T(Tuple(untyped)) : T()),
+            untyped(untyped) {}
 
-  Stage operator=(T x) { return untyped = x; }
-  Stage operator+=(T x) { return untyped = T(Tuple(untyped)) + x; }
-  Stage operator-=(T x) { return untyped = T(Tuple(untyped)) - x; }
-  Stage operator*=(T x) { return untyped = T(Tuple(untyped)) * x; }
-  Stage operator/=(T x) { return untyped = T(Tuple(untyped)) / x; }
+    Stage operator=(T x) { return untyped = x; }
+    Stage operator+=(T x) { return untyped = T(Tuple(untyped)) + x; }
+    Stage operator-=(T x) { return untyped = T(Tuple(untyped)) - x; }
+    Stage operator*=(T x) { return untyped = T(Tuple(untyped)) * x; }
+    Stage operator/=(T x) { return untyped = T(Tuple(untyped)) / x; }
 };
 
 template <typename T>
 class FuncRefExprT : public T {
-  Halide::FuncRefExpr untyped;
+    Halide::FuncRefExpr untyped;
 
- public:
-  typedef Halide::Stage Stage;
-  typedef Halide::Tuple Tuple;
+public:
+    typedef Halide::Stage Stage;
+    typedef Halide::Tuple Tuple;
 
-  FuncRefExprT(const Halide::FuncRefExpr& untyped)
-      : T(Tuple(untyped)), untyped(untyped) {}
+    FuncRefExprT(const Halide::FuncRefExpr& untyped)
+        : T(Tuple(untyped)), untyped(untyped) {}
 
-  Stage operator=(T x) { return untyped = x; }
-  Stage operator+=(T x) { return untyped = T(Tuple(untyped)) + x; }
-  Stage operator-=(T x) { return untyped = T(Tuple(untyped)) - x;}
-  Stage operator*=(T x) { return untyped = T(Tuple(untyped)) * x; }
-  Stage operator/=(T x) { return untyped = T(Tuple(untyped)) / x; }
+    Stage operator=(T x) { return untyped = x; }
+    Stage operator+=(T x) { return untyped = T(Tuple(untyped)) + x; }
+    Stage operator-=(T x) { return untyped = T(Tuple(untyped)) - x;}
+    Stage operator*=(T x) { return untyped = T(Tuple(untyped)) * x; }
+    Stage operator/=(T x) { return untyped = T(Tuple(untyped)) / x; }
 };
 
 template <typename T>
 class FuncT : public Halide::Func {
- public:
-  typedef Halide::Var Var;
-  typedef Halide::Expr Expr;
-  typedef Halide::Func Func;
+public:
+    typedef Halide::Var Var;
+    typedef Halide::Expr Expr;
+    typedef Halide::Func Func;
 
-  explicit FuncT(const std::string &name) : Func(name) {}
-  FuncT() {}
-  explicit FuncT(Expr e) : Func(e) {}
-  explicit FuncT(Func f) : Func(f) {}
-  explicit FuncT(Halide::Internal::Function f) : Func(f) {}
+    explicit FuncT(const std::string &name) : Func(name) {}
+    FuncT() {}
+    explicit FuncT(Expr e) : Func(e) {}
+    explicit FuncT(Func f) : Func(f) {}
+    explicit FuncT(Halide::Internal::Function f) : Func(f) {}
 
-  FuncRefVarT<T> operator()() const { return Func::operator()(); }
-  FuncRefVarT<T> operator()(Var x) const { return Func::operator()(x); }
-  FuncRefVarT<T> operator()(Var x, Var y) const { return Func::operator()(x, y); }
-  FuncRefVarT<T> operator()(Var x, Var y, Var z) const { return Func::operator()(x, y, z); }
-  FuncRefVarT<T> operator()(Var x, Var y, Var z, Var w) const { return Func::operator()(x, y, z, w); }
-  FuncRefVarT<T> operator()(Var x, Var y, Var z, Var w, Var u) const { return Func::operator()(x, y, z, w, u); }
-  FuncRefVarT<T> operator()(Var x, Var y, Var z, Var w, Var u, Var v) const { return Func::operator()(x, y, z, w, u, v); }
-  FuncRefVarT<T> operator()(std::vector<Var> vars) const { return Func::operator()(vars); }
+    FuncRefVarT<T> operator()() const { return Func::operator()(); }
+    FuncRefVarT<T> operator()(Var x) const { return Func::operator()(x); }
+    FuncRefVarT<T> operator()(Var x, Var y) const { return Func::operator()(x, y); }
+    FuncRefVarT<T> operator()(Var x, Var y, Var z) const { return Func::operator()(x, y, z); }
+    FuncRefVarT<T> operator()(Var x, Var y, Var z, Var w) const { return Func::operator()(x, y, z, w); }
+    FuncRefVarT<T> operator()(Var x, Var y, Var z, Var w, Var u) const { return Func::operator()(x, y, z, w, u); }
+    FuncRefVarT<T> operator()(Var x, Var y, Var z, Var w, Var u, Var v) const { return Func::operator()(x, y, z, w, u, v); }
+    FuncRefVarT<T> operator()(std::vector<Var> vars) const { return Func::operator()(vars); }
 
-  FuncRefExprT<T> operator()(Expr x) const { return Func::operator()(x); }
-  FuncRefExprT<T> operator()(Expr x, Expr y) const { return Func::operator()(x, y); }
-  FuncRefExprT<T> operator()(Expr x, Expr y, Expr z) const { return Func::operator()(x, y, z); }
-  FuncRefExprT<T> operator()(Expr x, Expr y, Expr z, Expr w) const { return Func::operator()(x, y, z, w); }
-  FuncRefExprT<T> operator()(Expr x, Expr y, Expr z, Expr w, Expr u) const { return Func::operator()(x, y, z, w, u); }
-  FuncRefExprT<T> operator()(Expr x, Expr y, Expr z, Expr w, Expr u, Expr v) const { return Func::operator()(x, y, z, w, u, v); }
-  FuncRefExprT<T> operator()(std::vector<Expr> vars) const { return Func::operator()(vars); }
+    FuncRefExprT<T> operator()(Expr x) const { return Func::operator()(x); }
+    FuncRefExprT<T> operator()(Expr x, Expr y) const { return Func::operator()(x, y); }
+    FuncRefExprT<T> operator()(Expr x, Expr y, Expr z) const { return Func::operator()(x, y, z); }
+    FuncRefExprT<T> operator()(Expr x, Expr y, Expr z, Expr w) const { return Func::operator()(x, y, z, w); }
+    FuncRefExprT<T> operator()(Expr x, Expr y, Expr z, Expr w, Expr u) const { return Func::operator()(x, y, z, w, u); }
+    FuncRefExprT<T> operator()(Expr x, Expr y, Expr z, Expr w, Expr u, Expr v) const { return Func::operator()(x, y, z, w, u, v); }
+    FuncRefExprT<T> operator()(std::vector<Expr> vars) const { return Func::operator()(vars); }
 };
 
 // Forward operator overload invocations on FuncRefVarT/FuncRefExprT to
