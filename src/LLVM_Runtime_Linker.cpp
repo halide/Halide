14f005bf9de8b764172162fad0ecfabb55f973e9
Ron Lieberman <ronl@codeaurora.org>
2015-12-02 06:54:55 -0600

    Resolved API issue with linker modules, we are not caught up to llvm tip yet on old master.

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -397,7 +397,8 @@ void link_modules(std::vector<llvm::Module *> &modules, Target t) {
     // Link them all together
     for (size_t i = 1; i < modules.size(); i++) {
         string err_msg;
-        #if LLVM_VERSION >= 38
+//      #if LLVM_VERSION >= 38
+        #if LLVM_VERSION >  38
         bool failed = llvm::Linker::linkModules(*modules[0], *modules[i]);
         #elif LLVM_VERSION >= 36
         bool failed = llvm::Linker::LinkModules(modules[0], modules[i]);
