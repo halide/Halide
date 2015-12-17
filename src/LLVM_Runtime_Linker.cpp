eaee891a07ad3df8ac31bf93a75fd8b1b91b357e
Ron Lieberman <ronl@codeaurora.org>
2015-12-17 15:07:29 -0600

    fixed llvm modules issue, we have not seen yet.

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -393,7 +393,8 @@ void link_modules(std::vector<llvm::Module *> &modules, Target t) {
     for (size_t i = 1; i < modules.size(); i++) {
         string err_msg;
         #if LLVM_VERSION >= 38
-        bool failed = llvm::Linker::linkModules(*modules[0], std::unique_ptr<llvm::Module>(modules[i]));
+        //bool failed = llvm::Linker::linkModules(*modules[0], std::unique_ptr<llvm::Module>(modules[i]));
+        bool failed = llvm::Linker::linkModules(*modules[0], *modules[i]);
         #elif LLVM_VERSION >= 36
         bool failed = llvm::Linker::LinkModules(modules[0], modules[i]);
         #else
