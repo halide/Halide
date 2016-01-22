58b28600452a93f19e9a18a0cf73c16f83d86122
Andrew Adams <andrew.b.adams@gmail.com>
2016-01-22 12:55:27 -0800

    Expose set_custom_foo methods in HalideRuntime.h and use them in generator tests. More portable than weak linkage

--- test/generator/user_context_insanity_aottest.cpp
+++ test/generator/user_context_insanity_aottest.cpp
@@ -12,7 +12,7 @@ const int num_launcher_tasks = 1000;
 
 static bool got_context[num_launcher_tasks];
 
-extern "C" int32_t halide_trace(void *context, const halide_trace_event *e) {
+extern "C" int32_t my_halide_trace(void *context, const halide_trace_event *e) {
     bool *bool_ptr = (bool *)context;
     *bool_ptr = true;
     return 0;
@@ -33,6 +33,8 @@ int launcher_task(void *user_context, int index, uint8_t *closure) {
 }
 
 int main(int argc, char **argv) {
+    halide_set_custom_trace(&my_halide_trace);
+
     // Hijack halide's runtime to run a bunch of instances of this function
     // in parallel.
     halide_do_par_for(NULL, launcher_task, 0, num_launcher_tasks, NULL);
