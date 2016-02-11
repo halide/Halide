499448a9a5d1059db7236f3f25fcd41d8731b7b6
Dillon Sharlet <dsharlet@google.com>
2016-02-10 17:23:19 -0800

    Removed JIT hexagon offload test, added app.

---
+++ apps/hexagon/process.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <memory.h>
+
+#include "scale.h"
+
+int main(int argc, char **argv) {
+    const int W = 64;
+    const int H = 64;
+
+    uint8_t in_host[W * H * 3];
+    uint8_t out_host[W * H * 3];
+    memset(in_host, 0, W * H * 3);
+
+    buffer_t in = { 0 };
+    in.host = in_host;
+    in.elem_size = 1;
+    in.extent[0] = W;
+    in.extent[1] = H;
+    in.extent[2] = 3;
+    in.stride[0] = 1;
+    in.stride[1] = W;
+    in.stride[2] = W * H;
+
+    buffer_t out = in;
+    out.host = out_host;
+
+    int result = scale(2, &in, &out);
+
+    return result;
+}
---
+++ apps/hexagon/scale.h
@@ -0,0 +1,39 @@
+#ifndef HALIDE__scale_h
+#define HALIDE__scale_h
+#ifndef HALIDE_ATTRIBUTE_ALIGN
+  #ifdef _MSC_VER
+    #define HALIDE_ATTRIBUTE_ALIGN(x) __declspec(align(x))
+  #else
+    #define HALIDE_ATTRIBUTE_ALIGN(x) __attribute__((aligned(x)))
+  #endif
+#endif
+#ifndef BUFFER_T_DEFINED
+#define BUFFER_T_DEFINED
+#include <stdbool.h>
+#include <stdint.h>
+typedef struct buffer_t {
+    uint64_t dev;
+    uint8_t* host;
+    int32_t extent[4];
+    int32_t stride[4];
+    int32_t min[4];
+    int32_t elem_size;
+    HALIDE_ATTRIBUTE_ALIGN(1) bool host_dirty;
+    HALIDE_ATTRIBUTE_ALIGN(1) bool dev_dirty;
+    HALIDE_ATTRIBUTE_ALIGN(1) uint8_t _padding[10 - sizeof(void *)];
+} buffer_t;
+#endif
+struct halide_filter_metadata_t;
+#ifndef HALIDE_FUNCTION_ATTRS
+#define HALIDE_FUNCTION_ATTRS
+#endif
+#ifdef __cplusplus
+extern "C" {
+#endif
+int scale(const int32_t _scale, buffer_t *_input_buffer, buffer_t *_h_buffer) HALIDE_FUNCTION_ATTRS;
+int scale_argv(void **args) HALIDE_FUNCTION_ATTRS;
+extern const struct halide_filter_metadata_t scale_metadata;
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+#endif
