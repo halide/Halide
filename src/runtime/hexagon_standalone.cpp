cc69c3256c1dc8ed1b80255f30787c68a750caf5
Dillon Sharlet <dsharlet@google.com>
2016-02-11 22:34:40 -0800

    Remove duplicate implementation of posix_allocator.
    Former-commit-id: 771779b56fff1babcaaafe730ffe5294fa5cef84

--- src/runtime/hexagon_standalone.cpp
+++ src/runtime/hexagon_standalone.cpp
@@ -1,48 +1,8 @@
 #include "HalideRuntime.h"
 #include "printer.h"
 #include "runtime_internal.h"
-#ifdef __cplusplus
+
 extern "C" {
-  namespace Halide { namespace Runtime { namespace Internal {
-        WEAK void *default_malloc(void *user_context, size_t x) {
-          void *orig = malloc(x+40);
-          if (orig == NULL) {
-            // Will result in a failed assertion and a call to halide_error
-            return NULL;
-          }
-          // Round up to next multiple of 32. Should add at least 8 bytes so we can fit the original pointer.
-          void *ptr = (void *)((((size_t)orig + 32) >> 5) << 5);
-          ((void **)ptr)[-1] = orig;
-          return ptr;
-        }
-
-        WEAK void default_free(void *user_context, void *ptr) {
-          free(((void**)ptr)[-1]);
-        }
-
-        WEAK void *(*custom_malloc)(void *, size_t) = default_malloc;
-        WEAK void (*custom_free)(void *, void *) = default_free;
-      }}}
-
-  WEAK void *(*halide_set_custom_malloc(void *(*user_malloc)(void *, size_t)))(void *, size_t) {
-    void *(*result)(void *, size_t) = custom_malloc;
-    custom_malloc = user_malloc;
-    return result;
-  }
-
-  WEAK void (*halide_set_custom_free(void (*user_free)(void *, void *)))(void *, void *) {
-    void (*result)(void *, void *) = custom_free;
-    custom_free = user_free;
-    return result;
-  }
-
-  WEAK void *halide_malloc(void *user_context, size_t x) {
-    return custom_malloc(user_context, x);
-  }
-
-  WEAK void halide_free(void *user_context, void *ptr) {
-    custom_free(user_context, ptr);
-  }
 
 typedef int32_t (*trace_fn)(void *, const halide_trace_event *);
 
@@ -154,4 +114,3 @@ namespace Halide { namespace Runtime { namespace Internal {
         return 0;
   }
 }
-#endif
