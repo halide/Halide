2edb2c2c0cfd9749c08559c28e86f2d929ac0122
Dillon Sharlet <dsharlet@google.com>
2016-02-12 10:29:22 -0800

    Revert pass-through of abs of unsigned values.
    Former-commit-id: d1791c47939ee2523640c09276cdc2fe275fce27

--- src/Simplify.cpp
+++ src/Simplify.cpp
@@ -2821,9 +2821,7 @@ private:
                 expr = make_const(op->type, ia);
             } else if (ta.is_uint()) {
               // abs(uint) is a no-op.
-              // For Hexagon, do not treat this as a no-op. See explanation
-              // in IROperator.h:inline Expr abs(Expr a)
-              expr = abs(a);
+              expr = a;
             } else if (const_float(a, &fa)) {
                 if (fa < 0) {
                     fa = -fa;
