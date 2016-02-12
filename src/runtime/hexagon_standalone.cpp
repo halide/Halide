7196c90fc6f8a3df608bd9c67dc7b5bde030ab2f
Dillon Sharlet <dsharlet@google.com>
2016-02-11 22:30:29 -0800

    Remove bad thread pool implementation from hexagon_standalone, use fake_thread_pool instead.
    Former-commit-id: 2b86e4b7881c0321b31308c4dcfd2ae3a4f74458

--- src/runtime/hexagon_standalone.cpp
+++ src/runtime/hexagon_standalone.cpp
@@ -3,15 +3,6 @@
 #include "runtime_internal.h"
 #ifdef __cplusplus
 extern "C" {
-  WEAK void halide_mutex_lock(struct halide_mutex *mutex) {
-  }
-  WEAK void halide_mutex_unlock(struct halide_mutex *mutex) {}
-  WEAK void halide_mutex_cleanup(struct halide_mutex *mutex_arg) {}
-  WEAK int halide_do_par_for(void *user_context, int (*f)(void *, int, uint8_t *),
-                             int min, int size, uint8_t *closure) {
-    return -1;
-  }
-
   namespace Halide { namespace Runtime { namespace Internal {
         WEAK void *default_malloc(void *user_context, size_t x) {
           void *orig = malloc(x+40);
@@ -52,37 +43,6 @@ extern "C" {
   WEAK void halide_free(void *user_context, void *ptr) {
     custom_free(user_context, ptr);
   }
-
-struct spawn_thread_task {
-    void (*f)(void *);
-    void *closure;
-};
-WEAK void *halide_spawn_thread_helper(void *arg) {
-    spawn_thread_task *t = (spawn_thread_task *)arg;
-    t->f(t->closure);
-    free(t);
-    return NULL;
-}
-
-WEAK void halide_spawn_thread(void *user_context, void (*f)(void *), void *closure) {
-#if 0
-    // Note that we don't pass the user_context through to the
-    // thread. It may begin well after the user context is no longer a
-    // valid thing.
-    pthread_t thread;
-    // For the same reason we use malloc instead of
-    // halide_malloc. Custom malloc/free overrides may well not behave
-    // well if run at unexpected times (e.g. the matching free may
-    // occur at static destructor time if the thread never returns).
-    spawn_thread_task *t = (spawn_thread_task *)malloc(sizeof(spawn_thread_task));
-    t->f = f;
-    t->closure = closure;
-    pthread_create(&thread, NULL, halide_spawn_thread_helper, t);
-#else
-    halide_error(user_context, "Halide spawn thread called");
-#endif
-}
-
 
 typedef int32_t (*trace_fn)(void *, const halide_trace_event *);
 
