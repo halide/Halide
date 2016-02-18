a9d7ddff515450ac28feb38a8be3b9a01335efbc
Dillon Sharlet <dsharlet@google.com>
2016-02-18 12:24:12 -0800

    Move allocation padding from runtime allocators to CodeGen_Posix.
    Former-commit-id: 340759f8fbea99f14b9177d69062d2843a749c93

--- src/runtime/posix_allocator.cpp
+++ src/runtime/posix_allocator.cpp
@@ -10,16 +10,15 @@ extern void free(void *);
 namespace Halide { namespace Runtime { namespace Internal {
 
 WEAK void *default_malloc(void *user_context, size_t x) {
-    // We want to return an aligned address to the application.
-    // In addition, we should be able to read a double beyond the
-    // buffer. So we allocate more space than what was asked for.
+    // Allocate enough space for aligning the pointer we return.
     const size_t alignment = 128;
-    void *orig = malloc(x + alignment + sizeof(double));
+    void *orig = malloc(x + alignment);
     if (orig == NULL) {
         // Will result in a failed assertion and a call to halide_error
         return NULL;
     }
-    void *ptr = (void *)(((size_t)orig + alignment) & ~(alignment - 1));
+    // We want to store the original pointer prior to the pointer we return.
+    void *ptr = (void *)(((size_t)orig + alignment + sizeof(void*) - 1) & ~(alignment - 1));
     ((void **)ptr)[-1] = orig;
     return ptr;
 }
