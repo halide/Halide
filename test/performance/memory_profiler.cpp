c09998caf16fef9e94a5d76b5233801b90c18601
psuriana <psuriana@psuriana-macbookpro.roam.corp.google.com>
2016-03-07 15:48:01 -0800

    Fixed bugs in memory_profiler test + Added test for trivial Allocate always false condition
    Assign the function name manually since the memory_profiler test checker
    depends on having a correct naming format

--- test/performance/memory_profiler.cpp
+++ test/performance/memory_profiler.cpp
@@ -15,10 +15,10 @@ void reset_stats() {
 
 void my_print(void *, const char *msg) {
     float this_ms;
-    int this_percentage, this_memory_current, this_memory_peak, this_memory_total;
-    int val = sscanf(msg, " g: %fms (%d%%) (%d, %d, %d",
-        &this_ms, &this_percentage, &this_memory_current, &this_memory_peak, &this_memory_total);
-    if (val == 5) {
+    int idx, this_percentage, this_memory_current, this_memory_peak, this_memory_total;
+    int val = sscanf(msg, " g_%d: %fms (%d%%) (%d, %d, %d",
+        &idx, &this_ms, &this_percentage, &this_memory_current, &this_memory_peak, &this_memory_total);
+    if (val == 6) {
         memory_current = this_memory_current;
         memory_peak = this_memory_peak;
         memory_total = this_memory_total;
@@ -27,8 +27,8 @@ void my_print(void *, const char *msg) {
 
 // Return 0 if there is no error found
 int check_error(int expected_current, int expected_peak, int expected_total) {
-    printf("Memory current: %d bytes, peak: %d bytes, total: %d bytes\n",
-            memory_current, memory_peak, memory_total);
+    /*printf("Memory current: %d bytes, peak: %d bytes, total: %d bytes\n",
+            memory_current, memory_peak, memory_total);*/
     if (memory_current != expected_current) {
         printf("Current memory was %d instead of %d\n", memory_current, expected_current);
         return -1;
@@ -47,15 +47,35 @@ int check_error(int expected_current, int expected_peak, int expected_total) {
 int main(int argc, char **argv) {
     Target t = get_jit_target_from_environment().with_feature(Target::Profile);
 
-    Var x, y;
+    Var x("x"), y("y");
 
     {
+        printf("Running simple stack allocation test...\n");
+        reset_stats();
+        // On stack allocation (less than 1024*8)
+        Func f("f_1"), g("g_1");
+        g(x, y) = x;
+        f(x, y) = g(x-1, y) + g(x, y-1);
+        g.compute_root();
+
+        f.set_custom_print(&my_print);
+        Image<int> im = f.realize(10, 10, t);
+
+        //f.print_loop_nest();
+
+        if (check_error(0, 0, 0) != 0) {
+            return -1;
+        }
+    }
+
+    {
+        printf("Running simple heap allocation test...\n");
         reset_stats();
         // On heap allocation (bigger than 1024*8)
         const int size_x = 1000;
         const int size_y = 1000;
 
-        Func f, g;
+        Func f("f_2"), g("g_2");
         g(x, y) = x;
         f(x, y) = g(x-1, y) + g(x, y-1);
         g.compute_root();
@@ -63,7 +83,7 @@ int main(int argc, char **argv) {
         f.set_custom_print(&my_print);
         Image<int> im = f.realize(size_x, size_y, t);
 
-        f.print_loop_nest();
+        //f.print_loop_nest();
 
         int total = (size_x+1)*(size_y+1)*sizeof(int);
         if (check_error(0, total, total) != 0) {
@@ -72,20 +92,18 @@ int main(int argc, char **argv) {
     }
 
     {
+        printf("Running allocate condition is always false test...\n");
         reset_stats();
-        // On stack allocation (less than 1024*8)
-        const int size_x = 10;
-        const int size_y = 10;
-
-        Func f, g;
-        g(x, y) = x;
-        f(x, y) = g(x-1, y) + g(x, y-1);
+        // Allocate condiiton is always false
+        Func f("f_3"), g("g_3");
+        g(x, y) = x*y;
+        f(x, y) = select(1 == 2, g(x-1, y), 0);
         g.compute_root();
 
         f.set_custom_print(&my_print);
-        Image<int> im = f.realize(size_x, size_y, t);
+        Image<int> im = f.realize(1000, 1000, t);
 
-        f.print_loop_nest();
+        //f.print_loop_nest();
 
         int total = 0;
         if (check_error(0, total, total) != 0) {
@@ -93,9 +111,8 @@ int main(int argc, char **argv) {
         }
     }
 
-    // TODO(psuriana): Add test for Allocate with non-trivial condition and always
-    // false condition
-
+    // TODO(psuriana): Add test for Allocate with non-trivial condition
+    
     // TODO(psuriana): Add test for Allocate with non-trivial extent size (force
     // alloc on the heap)
 
