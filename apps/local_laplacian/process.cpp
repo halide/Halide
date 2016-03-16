5eb68feb6022f9fb8ee04f2a3d67e3fb7a825afc
Dan Palermo <dpalermo@codeaurora.org>
2016-03-15 21:36:19 -0500

    [2985] Fixed point Local Laplacian, compile, run.
    - Local Laplacian fixed point now compiling and running in
      Hexagon scalar mode (-DSCHEDULE=1) with rgb_small.ppm
    - Changes under ifdef HEX_TOOLS/HEXAGON/__hexagon__ so app
      should still run for other architectures to preserve
      behavior on master
    - Currently defined schedules:
        SCHEDULE=0  Original cpu schedule
        SCHEDULE=1  Scalar schedule
        SCHEDULE=2  Non-vector parallel schedule
        SCHEDULE=3  HVX vector schedule
    - scalar takes about 2 minutes when run on simulator:
        % make SCHEDULE=1 ... all out_small.ppm
        local_laplacian:        6933.4412 cycles/pixel
        output: out_small.ppm
                192 320
    
    Change-Id: Idf60ce4a2ea62f9ff13348f7a175bff9e2bb54a4
    Reviewed-on: https://review-hexagon.quicinc.com/52485
    Tested-by: Anshu Das Gupta <adasgupt@quicinc.com>
    Reviewed-by: Pranav Bhandarkar <pranavb@codeaurora.org>
    Reviewed-by: Ron Lieberman <ronl@quicinc.com>
    Reviewed-by: Dan Palermo <dpalermo@codeaurora.org>
    
    Former-commit-id: f8bcd18548622f68bb517b84c2b6f70dbf108acb

--- apps/local_laplacian/process.cpp
+++ apps/local_laplacian/process.cpp
@@ -1,11 +1,19 @@
 #include <cstdio>
+#ifndef NOCHRONO
 #include <chrono>
+#endif
 
 #include "local_laplacian.h"
 
 #include "benchmark.h"
+#include "stdint.h"
 #include "halide_image.h"
 #include "halide_image_io.h"
+
+#if defined(__hexagon__)
+#include "hexagon_standalone.h"
+#include "io.h"
+#endif
 
 using namespace Halide::Tools;
 
@@ -16,22 +24,47 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    Image<uint16_t> input = load_image(argv[1]);
+    Image<uint8_t> input = load_image(argv[1]);
+#ifdef DEBUG
+    info(input, "input");
+    stats(input, "input");
+#endif
     int levels = atoi(argv[2]);
     float alpha = atof(argv[3]), beta = atof(argv[4]);
-    Image<uint16_t> output(input.width(), input.height(), 3);
+    Image<uint8_t> output(input.width(), input.height(), 3);
     int timing = atoi(argv[5]);
+#if defined(__hexagon__)
+    SIM_ACQUIRE_HVX;
+#if LOG2VLEN == 7
+    SIM_SET_HVX_DOUBLE_MODE;
+#endif
+#endif
 
     // Timing code
     double best = benchmark(timing, 1, [&]() {
         local_laplacian(levels, alpha/(levels-1), beta, input, output);
     });
+#ifdef PCYCLES
+    best = best/output.height()/output.width()/timing;
+    fprintf(stderr, "local_laplacian:\t%0.4f cycles/pixel\n", best);
+#else
     printf("%gus\n", best * 1e6);
+#endif
 
+    // local_laplacian(levels, alpha/(levels-1), beta, input, output);
 
-    local_laplacian(levels, alpha/(levels-1), beta, input, output);
+#if defined(__hexagon__)
+    SIM_RELEASE_HVX;
+#if DEBUG
+    printf ("Done calling the halide func. and released the vector context\n");
+#endif
+#endif
 
+    fprintf(stderr, "output: %s\n", argv[6]);
+#ifndef NOSAVE
     save_image(output, argv[6]);
+#endif
+    fprintf(stderr, "        %d %d\n", output.width(), output.height());
 
     return 0;
 }
