cfa88331844c5d10e06f01faaf66a42024941dbe
Patricia Suriana <psuriana@psuriana0.mtv.corp.google.com>
2016-03-23 11:29:54 -0700

    Revert mandelbrot_generator and EXPORT static

--- test/generator/memory_profiler_mandelbrot_generator.cpp
+++ test/generator/memory_profiler_mandelbrot_generator.cpp
@@ -57,6 +57,7 @@ public:
                         lerp(y_min, y_max, cast<float>(y) / h));
         Complex c(c_real, c_imag);
 
+        Func mandelbrot;
         mandelbrot(x, y, z) = initial;
         RDom t(1, iters);
         Complex current = mandelbrot(x, y, t - 1);
@@ -76,10 +77,6 @@ public:
 
         return count;
     }
-private:
-    // Declared as a member variable to verify that Funcs-as-members won't cause
-    // spurious "Invalid Param name: __user_context" errors (Issue #561)
-    Func mandelbrot{"mandelbrot"};
 };
 
 RegisterGenerator<Mandelbrot> register_my_gen{"memory_profiler_mandelbrot"};
