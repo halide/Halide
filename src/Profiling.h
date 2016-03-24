c6ce68255bf73539a949f1e1e1963cf11b3ade2d
Patricia Suriana <psuriana@psuriana0.mtv.corp.google.com>
2016-03-24 10:49:58 -0700

    Remove dead stat from sample output in the doc

--- src/Profiling.h
+++ src/Profiling.h
@@ -18,7 +18,7 @@
  * Sample output:
  * memory_profiler_mandelbrot
  *  total time: 59.832336 ms   samples: 43   runs: 1000   time/run: 0.059832 ms
- *  heap allocations: 104000   peak heap usage: 505344 bytes   peak stack usage: 20 bytes
+ *  heap allocations: 104000   peak heap usage: 505344 bytes
  *   f0:          0.025673ms (42%)
  *   mandelbrot:  0.006444ms (10%)   peak: 505344   num: 104000   avg: 5376
  *   argmin:      0.027715ms (46%)   stack: 20
