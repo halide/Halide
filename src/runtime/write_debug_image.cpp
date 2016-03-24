478bee7aa56787a239ee44bda89ffc37ce0f014f
Andrew Adams <andrew.b.adams@gmail.com>
2016-03-24 16:49:24 -0700

    copy to host before debugging to file
    Otherwise GPU allocations might have stale data
    
    
    Former-commit-id: c30f3130b13a56abd50368283c70ecf730baa962

--- src/runtime/write_debug_image.cpp
+++ src/runtime/write_debug_image.cpp
@@ -115,6 +115,8 @@ WEAK uint8_t *get_pointer_to_data(int32_t dim0, int32_t dim1, int32_t dim2, int3
 WEAK extern "C" int32_t halide_debug_to_file(void *user_context, const char *filename,
                                              int32_t type_code, struct buffer_t *buf) {
 
+    halide_copy_to_host(user_context, buf);
+
     void *f = fopen(filename, "wb");
     if (!f) return -1;
 
