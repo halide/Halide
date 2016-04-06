7ec88775e7757b45bacf6426b5d6a1e7f1d7a01a
Dillon Sharlet <dsharlet@google.com>
2016-04-06 15:02:10 -0700

    Simplified interleave_x test.
    Former-commit-id: c0666dc266c484a2feb3796f50cbf2c09c2ebab5

--- test/correctness/interleave_x.cpp
+++ test/correctness/interleave_x.cpp
@@ -5,22 +5,10 @@ using namespace Halide;
 
 int main(int argc, char **argv) {
 
-    //int W = 64*3, H = 64*3;
-    const int W = 128, H = 48;
-
-    Image<uint16_t> in(W, H, 2);
-    for (int c = 0; c < 2; c++) {
-        for (int y = 0; y < H; y++) {
-            for (int x = 0; x < W; x++) {
-                in(x, y, c) = rand() & 0xff;
-            }
-        }
-    }
-
     Var x("x"), y("y");
 
     Func interleaved("interleaved");
-    interleaved(x, y) = select(x%2 == 0, in(x, y, 0), in(x, y, 1));
+    interleaved(x, y) = select(x%2 == 0, cast<uint16_t>(3), cast<uint16_t>(7));
 
     Target target = get_jit_target_from_environment();
     if (target.has_gpu_feature()) {
@@ -32,10 +20,10 @@ int main(int argc, char **argv) {
         interleaved.tile(x, y, xo, yo, x, y, 8, 8).vectorize(x);
     }
 
-    Image<uint16_t> out = interleaved.realize(W, H, target);
-    for (int y = 0; y < H; y++) {
-        for (int x = 0; x < W; x++) {
-            uint16_t correct = x%2 == 0 ? in(x, y, 0) : in(x, y, 1);
+    Image<uint16_t> out = interleaved.realize(128, 128, target);
+    for (int y = 0; y < out.height(); y++) {
+        for (int x = 0; x < out.width(); x++) {
+            uint16_t correct = x%2 == 0 ? 3 : 7;
             if (out(x, y) != correct) {
                 printf("out(%d, %d) = %d instead of %d\n", x, y, out(x, y), correct);
                 return -1;
