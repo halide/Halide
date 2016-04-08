3d49bda7f3e02a595c14dd22a8808719acf4adbf
Dillon Sharlet <dsharlet@google.com>
2016-04-08 16:10:13 -0700

    Remove target dependence of interleave/deinterleave in Hexagon.
    Former-commit-id: 6802310f28fe9035fe67a5225e65bbb86d8a01ea

--- src/HexagonOptimize.h
+++ src/HexagonOptimize.h
@@ -6,7 +6,6 @@
  */
 
 #include "IR.h"
-#include "Target.h"
 
 namespace Halide {
 namespace Internal {
@@ -16,15 +15,15 @@ namespace Internal {
  * rewrites widenings/narrowings to be explicit in the IR, and
  * attempts to simplify away most of the
  * interleaving/deinterleaving. */
-EXPORT Stmt optimize_hexagon(Stmt s, const Target &target);
+EXPORT Stmt optimize_hexagon(Stmt s);
 
 /** Generate deinterleave or interleave operations, operating on
  * groups of vectors at a time. */
 //@{
-EXPORT Expr deinterleave(Expr x, const Target &target);
-EXPORT Expr interleave(Expr x, const Target &target);
-EXPORT bool is_deinterleave(Expr x, const Target &target);
-EXPORT bool is_interleave(Expr x, const Target &target);
+EXPORT Expr native_deinterleave(Expr x);
+EXPORT Expr native_interleave(Expr x);
+EXPORT bool is_native_deinterleave(Expr x);
+EXPORT bool is_native_interleave(Expr x);
 //@}
 
 }  // namespace Internal
