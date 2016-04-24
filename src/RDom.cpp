185785ddecde4de508416557e5d2a2390619e360
Patricia Suriana <psuriana@google.com>
2016-04-23 17:28:21 -0700

    compilation error forgot to add ;
    Former-commit-id: 14dcbea30cb153b4321329f82f624c150d4b0984

--- src/RDom.cpp
+++ src/RDom.cpp
@@ -209,7 +209,7 @@ void RDom::where(Expr predicate) {
     user_assert(!dom.frozen())
         << (*this) << " cannot be given a new predicate, because it has already"
         << " been used in the update definition of some function.\n";
-    user_assert(dom.defined()) << "Error: Can't add predicate to undefined RDom.\n"
+    user_assert(dom.defined()) << "Error: Can't add predicate to undefined RDom.\n";
     dom.where(predicate);
 }
 
