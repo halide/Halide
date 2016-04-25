34adac73b9a1458390137ab2d0003c8f73222980
Patricia Suriana <psuriana@google.com>
2016-04-25 11:10:12 -0700

    add more comments, removed dead codes, rename and_predicates into fold_predicates
    Former-commit-id: 527e9ea4c769a0b9cf51b9f39b3784e5a43b78bb

--- src/BoundsInference.cpp
+++ src/BoundsInference.cpp
@@ -561,11 +561,15 @@ public:
 
             } else {
                 const vector<Expr> &exprs = consumer.exprs;
+                // We wrap the consumer exprs inside a "dummy" Call stmt so that we can take advantage
+                // of the IfThenElse stmt handler inside boxes_required() to determine the appropriate
+                // box bound size given some predicates on the RDom. Otherwise, we will need to update
+                // the scope to take into account of the predicates.
                 Stmt wrapped = Evaluate::make(Call::make(Int(32), "dummy", exprs, Call::PureIntrinsic));
                 if (consumer.stage > 0) {
                     const UpdateDefinition &r = consumer.func.updates()[consumer.stage - 1];
                     if (r.domain.defined() && r.domain.predicates().size() > 0) {
-                        wrapped = IfThenElse::make(r.domain.and_predicates(), wrapped);
+                        wrapped = IfThenElse::make(r.domain.fold_predicates(), wrapped);
                     }
                 }
 
