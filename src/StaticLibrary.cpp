ae260916a490cbbff7592288953f889db4eb937c
Steven Johnson <srj@google.com>
2016-05-13 10:10:30 -0700

    Add deterministic mode; expand test; insert padding only when needed

--- src/StaticLibrary.cpp
+++ src/StaticLibrary.cpp
@@ -117,35 +117,39 @@ std::string octal_string(int value, size_t pad) {
 
 }  // namespace
 
-void create_ar_file(const std::vector<std::string> &src_files, const std::string &dst_file) {
+void create_ar_file(const std::vector<std::string> &src_files, 
+                    const std::string &dst_file, bool deterministic) {
     File ar(dst_file, "wb");
     ar.write("!<arch>\x0A", 8);
-    for (const std::string &src_file : src_files) {
-        File::Stat stat = File::stat(src_file);
+    for (const std::string &src_path : src_files) {
+        File::Stat stat = File::stat(src_path);
 
-        size_t filesize = stat.file_size;
-        if (src_file.size() > 16) {
-            ar.write("#1/" + decimal_string(src_file.size(), 13));
-            filesize += src_file.size();
-        } else {
-            ar.write(pad_right(src_file, 16));
-        }
-        ar.write(decimal_string(stat.mod_time, 12));  // mod time
-        ar.write(decimal_string(stat.uid, 6));  // user id
-        ar.write(decimal_string(stat.gid, 6));  // group id
-        ar.write(octal_string(stat.mode, 8));  // mode
-        ar.write(decimal_string(filesize, 10));  // filesize
-        ar.write("\x60\x0A");  // magic
-        if (src_file.size() > 16) {
-            ar.write(src_file);
-        }
-        {
-            File src(src_file, "rb");
-            // Just slurp everything into memory and write it back out.
-            ar.write(src.read(stat.file_size));  // slurp it all over
-        }
+        // Each member must begin on an even byte boundary; insert LF as needed
         if (ar.tell() & 1) {
             ar.write("\x0A");
+        }
+        // Need to embed just the leaf name
+        std::string src_name = base_name(src_path, '/');
+        size_t filesize = stat.file_size;
+        if (src_name.size() > 16) {
+            ar.write("#1/" + decimal_string(src_name.size(), 13));
+            filesize += src_name.size();
+        } else {
+            ar.write(pad_right(src_name, 16));
+        }
+        ar.write(decimal_string(deterministic ? 0 : stat.mod_time, 12));  // mod time
+        ar.write(decimal_string(deterministic ? 0 : stat.uid, 6));  // user id
+        ar.write(decimal_string(deterministic ? 0 : stat.gid, 6));  // group id
+        ar.write(octal_string(deterministic ? 0644 : stat.mode, 8));  // mode
+        ar.write(decimal_string(filesize, 10));  // filesize
+        ar.write("\x60\x0A");  // magic
+        if (src_name.size() > 16) {
+            ar.write(src_name);
+        }
+        {
+            File src(src_path, "rb");
+            // Just slurp everything into memory and write it back out.
+            ar.write(src.read(stat.file_size));  // slurp it all over
         }
     }
 }
@@ -157,20 +161,29 @@ void static_library_test() {
 
         File b("b_long_name_is_long.tmp", "w");
         b.write("c456d");
+
+        File c("./c_path.tmp", "w");
+        c.write("e789f");
     }
 
-    create_ar_file({"a.tmp", "b_long_name_is_long.tmp"}, "arfile.a");
+    create_ar_file({"a.tmp", "b_long_name_is_long.tmp", "./c_path.tmp"}, "arfile.a");
 
     File::Stat stat = File::stat("arfile.a");
-    internal_assert(stat.file_size == 162) << "Filesize was " << stat.file_size;
-
     {
         File ar("arfile.a", "r");
-        auto slurp = ar.read(162);
-        // TODO(srj): verify contents are correct, but ignore mod/uid/gid/mode
+        auto slurp = ar.read(stat.file_size);
+        const std::string actual = std::string((const char*)slurp.data(), slurp.size());
+        const std::string expected = R"literal(!<arch>
+a.tmp           0           0     0     644     5         `
+a123b
+#1/23           0           0     0     644     28        `
+b_long_name_is_long.tmpc456dc_path.tmp      0           0     0     644     5         `
+e789f)literal";
+        internal_assert(actual == expected) << "File contents wrong, expected:(" << expected << ")\nactual:(" << actual << ")\n";
     }
     File::unlink("a.tmp");
     File::unlink("b_long_name_is_long.tmp");
+    File::unlink("./c_path.tmp");
     File::unlink("arfile.a");
 
     debug(0) << "static_library_test passed\n";
