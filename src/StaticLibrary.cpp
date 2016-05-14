6d989ea7501f7a5f5a19e5f2fcacdf43b5123ea1
Steven Johnson <srj@google.com>
2016-05-13 18:05:56 -0700

    Upgrade create_ar_file() to allow for in-memory sources (rather than filesystem sources)
    Former-commit-id: 9260627fc9256d5e7b2dfff167e4624e9ad9cd34

--- src/StaticLibrary.cpp
+++ src/StaticLibrary.cpp
@@ -32,6 +32,47 @@ std::string octal_string(int value, size_t pad) {
     return pad_right(buf, pad);
 }
 
+void append_ar_file(std::ofstream &ar, const std::string &src_path, const FileStat &src_stat, std::istream &src_data) {
+    // Each member must begin on an even byte boundary; insert LF as needed
+    if (ar.tellp() & 1) {
+        ar << "\x0A";
+    }
+    // Need to embed just the leaf name
+    std::string src_name = base_name(src_path, '/');
+    uint64_t filesize = src_stat.file_size;
+    if (src_name.size() > 16) {
+        ar << "#1/" << decimal_string(src_name.size(), 13);
+        filesize += src_name.size();
+    } else {
+        ar << pad_right(src_name, 16);
+    }
+    ar << decimal_string(src_stat.mod_time, 12);  // mod time
+    ar << decimal_string(src_stat.uid, 6);  // user id
+    ar << decimal_string(src_stat.gid, 6);  // group id
+    ar << octal_string(src_stat.mode, 8);  // mode
+    ar << decimal_string(filesize, 10);  // filesize
+    ar << "\x60\x0A";  // magic
+    if (src_name.size() > 16) {
+        ar << src_name;
+    }
+    ar << src_data.rdbuf();
+}
+
+void write_to(const char *path, const char *data) {
+    std::ofstream a(path, std::ofstream::out);
+    a << data;
+    internal_assert(a.good());
+}
+
+std::string read_from(const char *path) {
+    std::ifstream a(path, std::ifstream::in);
+    std::ostringstream actual;
+    actual << a.rdbuf();
+    internal_assert(a.good());
+    internal_assert(actual.good());
+    return actual.str();
+}
+
 }  // namespace
 
 void create_ar_file(const std::vector<std::string> &src_files, 
@@ -39,74 +80,68 @@ void create_ar_file(const std::vector<std::string> &src_files,
     std::ofstream ar(dst_file, std::ofstream::out | std::ofstream::binary);
     ar << "!<arch>\x0A";
     for (const std::string &src_path : src_files) {
-        FileStat stat = file_stat(src_path);
+        FileStat src_stat = file_stat(src_path);
+        if (deterministic) {
+            src_stat.mod_time = 0;
+            src_stat.uid = 0;
+            src_stat.gid = 0;
+            src_stat.mode = 0644;
+        }
+        std::ifstream src_data(src_path, std::ifstream::in | std::ifstream::binary);
+        append_ar_file(ar, src_path, src_stat, src_data);
+    }
+    user_assert(ar.good());
+}
 
-        // Each member must begin on an even byte boundary; insert LF as needed
-        if (ar.tellp() & 1) {
-            ar << "\x0A";
-        }
-        // Need to embed just the leaf name
-        std::string src_name = base_name(src_path, '/');
-        uint64_t filesize = stat.file_size;
-        if (src_name.size() > 16) {
-            ar << "#1/" << decimal_string(src_name.size(), 13);
-            filesize += src_name.size();
-        } else {
-            ar << pad_right(src_name, 16);
-        }
-        ar << decimal_string(deterministic ? 0 : stat.mod_time, 12);  // mod time
-        ar << decimal_string(deterministic ? 0 : stat.uid, 6);  // user id
-        ar << decimal_string(deterministic ? 0 : stat.gid, 6);  // group id
-        ar << octal_string(deterministic ? 0644 : stat.mode, 8);  // mode
-        ar << decimal_string(filesize, 10);  // filesize
-        ar << "\x60\x0A";  // magic
-        if (src_name.size() > 16) {
-            ar << src_name;
-        }
-        {
-            std::ifstream src(src_path, std::ifstream::in | std::ifstream::binary);
-            ar << src.rdbuf();
-        }
+void create_ar_file(const std::vector<ArInput> &src_files, 
+                    const std::string &dst_file) {
+    std::ofstream ar(dst_file, std::ofstream::out | std::ofstream::binary);
+    ar << "!<arch>\x0A";
+    for (const ArInput &input : src_files) {
+        FileStat src_stat = { input.data.size(), 0, 0, 0, 0644 };
+        std::istringstream src_data(input.data, std::ifstream::in);
+        append_ar_file(ar, input.name, src_stat, src_data);
     }
     user_assert(ar.good());
 }
 
 void static_library_test() {
-    {
-        std::ofstream a("a.tmp", std::ofstream::out);
-        a << "a123b";
-        user_assert(a.good());
-    }
-    {
-        std::ofstream b("b_long_name_is_long.tmp", std::ofstream::out);
-        b << "c456d";
-        user_assert(b.good());
-    }
-    {
-        std::ofstream c("./c_path.tmp", std::ofstream::out);
-        c << "e789f";
-        user_assert(c.good());
-    }
 
-    create_ar_file({"a.tmp", "b_long_name_is_long.tmp", "./c_path.tmp"}, "arfile.a");
-
-    {
-        std::ifstream ar("arfile.a", std::ifstream::in);
-        std::ostringstream actual;
-        actual << ar.rdbuf();
         const std::string expected = R"literal(!<arch>
 a.tmp           0           0     0     644     5         `
 a123b
 #1/23           0           0     0     644     28        `
 b_long_name_is_long.tmpc456dc_path.tmp      0           0     0     644     5         `
 e789f)literal";
-        internal_assert(actual.str() == expected) 
-            << "File contents wrong, expected:(" << expected << ")\nactual:(" << actual.str() << ")\n";
-    }
+
+    // Test the file version
+    write_to("a.tmp", "a123b");
+    write_to("b_long_name_is_long.tmp", "c456d");
+    write_to("./c_path.tmp", "e789f");
+
+    create_ar_file({"a.tmp", "b_long_name_is_long.tmp", "./c_path.tmp"}, "arfile.a");
+
+    std::string actual = read_from("arfile.a");
+    internal_assert(actual == expected) 
+        << "File contents wrong, expected:(" << expected << ")\nactual:(" << actual << ")\n";
+
     file_unlink("a.tmp");
     file_unlink("b_long_name_is_long.tmp");
     file_unlink("./c_path.tmp");
     file_unlink("arfile.a");
+
+    // Test the memory version
+    create_ar_file({
+        ArInput{ "a.tmp", "a123b" },
+        ArInput{ "b_long_name_is_long.tmp", "c456d" },
+        ArInput{ "./c_path.tmp", "e789f" }
+    }, "arfile2.a");
+
+    std::string actual2 = read_from("arfile2.a");
+    internal_assert(actual2 == expected) 
+        << "File contents wrong, expected:(" << expected << ")\nactual:(" << actual2 << ")\n";
+
+    file_unlink("arfile2.a");
 
     debug(0) << "static_library_test passed\n";
 }
--- src/StaticLibrary.h
+++ src/StaticLibrary.h
@@ -22,6 +22,19 @@ namespace Internal {
 EXPORT void create_ar_file(const std::vector<std::string> &src_files, 
                            const std::string &dst_file, bool deterministic = true);
 
+/**
+ * Given a list of "files" (really, names and data), create an ar file.
+ * Note that 'data' is a string, but may hold binary data; do *not*
+ * assume that c_str() will be useful.
+ * This always emits 0 for all GID/UID/timestamps, and 0644 for
+ * all modes (equivalent to the ar -D option). 
+ */
+struct ArInput {
+  std::string name, data;
+};
+EXPORT void create_ar_file(const std::vector<ArInput> &src_files, 
+                           const std::string &dst_file);
+
 EXPORT void static_library_test();
 
 }  // namespace Halide
