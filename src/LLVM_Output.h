e031ee28aadc29104709257624760dd79b798ec6
Steven Johnson <srj@google.com>
2016-05-16 14:56:13 -0700

    1213

--- src/LLVM_Output.h
+++ src/LLVM_Output.h
@@ -13,9 +13,18 @@ class TargetOptions;
 class LLVMContext;
 class raw_fd_ostream;
 class raw_pwrite_stream;
+class raw_ostream;
 }
 
 namespace Halide {
+
+namespace Internal {
+#if LLVM_VERSION < 37
+typedef llvm::raw_ostream LLVMOStream;
+#else
+typedef llvm::raw_pwrite_stream LLVMOStream;
+#endif
+}
 
 /** Generate an LLVM module. */
 EXPORT std::unique_ptr<llvm::Module> compile_module_to_llvm_module(const Module &module, llvm::LLVMContext &context);
@@ -25,14 +34,14 @@ std::unique_ptr<llvm::raw_fd_ostream> make_raw_fd_ostream(const std::string &fil
 
 /** Compile an LLVM module to native targets (objects, native assembly). */
 // @{
-EXPORT void compile_llvm_module_to_object(llvm::Module &module, llvm::raw_pwrite_stream& out);
-EXPORT void compile_llvm_module_to_assembly(llvm::Module &module, llvm::raw_pwrite_stream& out);
+EXPORT void compile_llvm_module_to_object(llvm::Module &module, Internal::LLVMOStream& out);
+EXPORT void compile_llvm_module_to_assembly(llvm::Module &module, Internal::LLVMOStream& out);
 // @}
 
 /** Compile an LLVM module to LLVM targets (bitcode, LLVM assembly). */
 // @{
-EXPORT void compile_llvm_module_to_llvm_bitcode(llvm::Module &module, llvm::raw_pwrite_stream& out);
-EXPORT void compile_llvm_module_to_llvm_assembly(llvm::Module &module, llvm::raw_pwrite_stream& out);
+EXPORT void compile_llvm_module_to_llvm_bitcode(llvm::Module &module, Internal::LLVMOStream& out);
+EXPORT void compile_llvm_module_to_llvm_assembly(llvm::Module &module, Internal::LLVMOStream& out);
 // @}
 
 }
