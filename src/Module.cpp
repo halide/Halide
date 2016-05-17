f1f1e1d3104745d6c3a2a607a89add02e89d4940
Steven Johnson <srj@google.com>
2016-05-17 16:44:15 -0700

    GenGen -r param should not take .o suffix
    This is a bit of hygiene: the -r parameter (for “generate the runtime
    only”) should omit the .o, and have it added as appropriate downstream.
    Justifications for this:
    (1) That’s the pattern for other outputs of this rule
    (2) on Windows, it should be .obj, not .o
    (3) once static library support is finished, this will allow us to
    specify the output as library via the -e format instead

--- src/Module.cpp
+++ src/Module.cpp
@@ -145,9 +145,11 @@ void Module::compile(const Outputs &output_files) const {
     }
 }
 
-void compile_standalone_runtime(std::string object_filename, Target t) {
+void compile_standalone_runtime(const Outputs &output_files, Target t) {
     Module empty("standalone_runtime", t.without_feature(Target::NoRuntime).without_feature(Target::JIT));
-    empty.compile(Outputs().object(object_filename));
+    // For runtime, it only makes sense to output object files (for now), so ignore
+    // everything else. (Soon, this will also support static libraries.)
+    empty.compile(Outputs().object(output_files.object_name));
 }
 
 }
