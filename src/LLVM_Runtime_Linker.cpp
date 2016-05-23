0730473374677c426d1f01cda7bb62cc26358dc8
Andrew Adams <andrew.b.adams@gmail.com>
2016-05-23 14:55:42 -0700

    Update pnacl to pepper_49
    Relevant pnacl-llvm commits to use:
    llvm: baa63524b6b493ec2a6aa2c5193d9f25c0c33191
    clang: cf0dc7f6e6123dfa9b8834b56743315300b34e6c

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -197,13 +197,15 @@ llvm::DataLayout get_data_layout_for_target(Target target) {
                 #endif
             } else if (target.os == Target::Windows) {
                 return llvm::DataLayout("e-m:e-p:32:32-i64:64-f80:32-n8:16:32-a:0:32-S32");
+            } else if (target.os == Target::NaCl) {
+                return llvm::DataLayout("e-m:e-p:32:32-i64:64-n8:16:32-S128");
             } else {
-                // Linux/Android/NaCl
+                // Linux/Android
                 return llvm::DataLayout("e-m:e-p:32:32-f64:32:64-f80:32-n8:16:32-S128");
             }
         } else { // 64-bit
             if (target.os == Target::NaCl) {
-                return llvm::DataLayout("e-m:e-p:32:32-i64:64-f80:128-n8:16:32:64-S128");
+                return llvm::DataLayout("e-m:e-p:32:32-i64:64-n8:16:32:64-S128");
             } else if (target.os == Target::OSX) {
                 return llvm::DataLayout("e-m:o-i64:64-f80:128-n8:16:32:64-S128");
             } else if (target.os == Target::Windows && !target.has_feature(Target::JIT)) {
@@ -218,6 +220,8 @@ llvm::DataLayout get_data_layout_for_target(Target target) {
         if (target.bits == 32) {
             if (target.os == Target::IOS) {
                 return llvm::DataLayout("e-m:o-p:32:32-f64:32:64-v64:32:64-v128:32:128-a:0:32-n32-S32");
+            } else if (target.os == Target::NaCl) {
+                return llvm::DataLayout("e-m:e-p:32:32-i64:64-v128:64:128-a:0:32-n32-S128");
             } else {
                 return llvm::DataLayout("e-m:e-p:32:32-i64:64-v128:64:128-a:0:32-n32-S64");
             }
@@ -241,7 +245,7 @@ llvm::DataLayout get_data_layout_for_target(Target target) {
             return llvm::DataLayout("e-m:e-i64:64-n32:64");
         }
     } else if (target.arch == Target::PNaCl) {
-        return llvm::DataLayout("e-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-p:32:32:32-v128:32:32");
+        return llvm::DataLayout("e-p:32:32-i64:64-n32");
     } else {
         internal_error << "Bad target arch: " << target.arch << "\n";
         return llvm::DataLayout("unreachable");
