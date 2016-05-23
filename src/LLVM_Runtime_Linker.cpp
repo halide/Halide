3ed55112a17daeb82d2a42339368b13d2005e8ec
Andrew Adams <andrew.b.adams@gmail.com>
2016-05-23 15:23:28 -0700

    Minor fix for cross compilation test
    Though I'm not sure why you'd be using pnacl llvm to cross compile
    windows binaries...

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -190,7 +190,9 @@ llvm::DataLayout get_data_layout_for_target(Target target) {
             if (target.os == Target::OSX) {
                 return llvm::DataLayout("e-m:o-p:32:32-f64:32:64-f80:128-n8:16:32-S128");
             } else if (target.os == Target::Windows && !target.has_feature(Target::JIT)) {
-                #if LLVM_VERSION >= 37
+                #if WITH_NATIVE_CLIENT
+                return llvm::DataLayout("e-m:x-p:32:32-i64:64-f80:32-n8:16:32-S32");
+                #elif LLVM_VERSION >= 37
                 return llvm::DataLayout("e-m:x-p:32:32-i64:64-f80:32-n8:16:32-a:0:32-S32");
                 #else
                 return llvm::DataLayout("e-m:w-p:32:32-i64:64-f80:32-n8:16:32-a:0:32-S32");
