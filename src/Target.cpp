aabcca03d3e9fdc235549560f7f3e017d30e081a
Steven Johnson <srj@google.com>
2016-05-24 12:55:42 -0700

    AVX2 detection in calculate_host_target appears to be wrong

--- src/Target.cpp
+++ src/Target.cpp
@@ -123,7 +123,7 @@ Target calculate_host_target() {
         // Call cpuid with eax=7, ecx=0
         int info2[4];
         cpuid(info2, 7, 0);
-        bool have_avx2 = info[1] & (1 << 5);
+        bool have_avx2 = info2[1] & (1 << 5);
         if (have_avx2) {
             initial_features.push_back(Target::AVX2);
         }
