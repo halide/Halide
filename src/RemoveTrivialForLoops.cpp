8a194feb8b3428f42970000f171d51eb5e9e98e5
Dillon Sharlet <dsharlet@google.com>
2016-06-02 17:30:35 -0700

    A few small changes from the Hexagon branch.

--- src/RemoveTrivialForLoops.cpp
+++ src/RemoveTrivialForLoops.cpp
@@ -10,12 +10,18 @@ namespace Internal {
 class RemoveTrivialForLoops : public IRMutator {
     using IRMutator::visit;
 
+    bool device_loops;
+
     void visit(const For *for_loop) {
+        if (!device_loops && for_loop->device_api != DeviceAPI::None) {
+            // Don't assume any device API loops are trivial.
+            IRMutator::visit(for_loop);
+            return;
+        }
+
         Stmt body = mutate(for_loop->body);
 
-        bool gpu_loop = CodeGen_GPU_Dev::is_gpu_var(for_loop->name);
-
-        if (!gpu_loop && is_one(for_loop->extent)) {
+        if (is_one(for_loop->extent)) {
             if (for_loop->for_type == ForType::Parallel) {
                 std::cerr << "Warning: Parallel for loop over "
                           << for_loop->name << " has extent one. "
@@ -28,7 +34,7 @@ class RemoveTrivialForLoops : public IRMutator {
             stmt = LetStmt::make(for_loop->name, for_loop->min, body);
         } else if (is_zero(for_loop->extent)) {
             stmt = Evaluate::make(0);
-        } else if (!gpu_loop && can_prove(for_loop->extent <= 1)) {
+        } else if (can_prove(for_loop->extent <= 1)) {
             // Loop has at most one iteration
             stmt = LetStmt::make(for_loop->name, for_loop->min, body);
             stmt = IfThenElse::make(for_loop->extent > 0, stmt, Stmt());
@@ -38,13 +44,15 @@ class RemoveTrivialForLoops : public IRMutator {
             stmt = For::make(for_loop->name, for_loop->min, for_loop->extent, for_loop->for_type, for_loop->device_api, body);
         }
     }
+
+public:
+    RemoveTrivialForLoops(bool device_loops) : device_loops(device_loops) {}
 };
 
 // Turn for loops of size one into let statements
-Stmt remove_trivial_for_loops(Stmt s) {
-    return RemoveTrivialForLoops().mutate(s);
+Stmt remove_trivial_for_loops(Stmt s, bool device_loops) {
+    return RemoveTrivialForLoops(device_loops).mutate(s);
 }
-
 
 }
 }
