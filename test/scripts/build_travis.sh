7fe2ef4d66fdf66c1125ffcbda9608b857ec2930
Andrew Adams <andrew.b.adams@gmail.com>
2016-06-03 09:35:36 -0700

    Missing backslash
    Former-commit-id: 9182b50ad5f4cd3cc71a0717bd96cb89a2ad3390

--- test/scripts/build_travis.sh
+++ test/scripts/build_travis.sh
@@ -17,7 +17,7 @@ if [ ${BUILD_SYSTEM} = 'CMAKE' ]; then
   : ${HALIDE_SHARED_LIBRARY:?"HALIDE_SHARED_LIBRARY must be set"}
   LLVM_VERSION_NO_DOT="$( echo ${LLVM_VERSION} | sed 's/\.//' | cut -b1,2 )"
   mkdir -p build/ && cd build/
-  cmake -DLLVM_INCLUDE="/usr/include"
+  cmake -DLLVM_INCLUDE="/usr/include" \
         -DLLVM_LIB="/usr/lib" \
         -DLLVM_BIN="/usr/bin" \
         -DLLVM_VERSION="${LLVM_VERSION_NO_DOT}" \
