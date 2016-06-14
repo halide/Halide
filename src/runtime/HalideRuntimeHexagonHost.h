1232edf4665c3fc54c5d8007275cf62323bea43b
Dillon Sharlet <dsharlet@google.com>
2016-06-14 09:24:59 -0700

    Update comments on device handle wrappers.

--- src/runtime/HalideRuntimeHexagonHost.h
+++ src/runtime/HalideRuntimeHexagonHost.h
@@ -15,31 +15,27 @@ typedef int halide_hexagon_handle_t;
 
 extern const struct halide_device_interface *halide_hexagon_device_interface();
 
-/** Set the underlying ion device handle for a buffer. The device
- * handle should be allocated using ion_alloc or similar and must
- * have an extent large enough to cover that specified by the buffer_t
- * extent fields. The dev field of the buffer_t must be NULL when this
+/** The device handle for Hexagon is simply a pointer and size, stored
+ * in the dev field of the buffer_t. If the buffer is allocated in a
+ * particular way (ion_alloc), the buffer will be shared with Hexagon
+ * (not copied). The dev field of the buffer_t must be NULL when this
  * routine is called. This call can fail due to running out of memory
  * or being passed an invalid device handle. The device and host
  * dirty bits are left unmodified. */
 extern int halide_hexagon_wrap_device_handle(void *user_context, struct buffer_t *buf,
-                                             void *ion_buf, size_t size);
+                                             void *ptr, size_t size);
 
 /** Disconnect this buffer_t from the device handle it was previously
  * wrapped around. Should only be called for a buffer_t that
  * halide_hexagon_wrap_device_handle was previously called on. Frees any
  * storage associated with the binding of the buffer_t and the device
  * handle, but does not free the device handle. The previously
- * wrapped device handle is returned. . The dev field of the buffer_t
- * will be NULL on return.
- */
+ * wrapped device handle is returned. The dev field of the buffer_t
+ * will be NULL on return. */
 extern void *halide_hexagon_detach_device_handle(void *user_context, struct buffer_t *buf);
 
-/** Return the underlying device handle for a buffer_t. This buffer
- *  must be valid on a Ion device, or not have any associated device
- *  memory. If there is no device memory (dev field is NULL), this
- *  returns 0.
- */
+/** Return the underlying device handle for a buffer_t. If there is
+ * no device memory (dev field is NULL), this returns 0. */
 extern void *halide_hexagon_get_device_handle(void *user_context, struct buffer_t *buf);
 extern size_t halide_hexagon_get_device_size(void *user_context, struct buffer_t *buf);
 
