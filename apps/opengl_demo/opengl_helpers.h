5d1fce3597249d1432011a42245894191e7ab08c
ronen barzel <ronen@barzel.org>
2016-06-21 12:50:05 -0400

    Demo of a desktop app using OpenGL with AOT filter compilation, showing a few use cases side-by-side
    * Sample filter AOT compiled to opengl target, run with data being sent to & from the host.
    
    * Same filter, run with data already in GPU textures.
    
    * Same filter, compiled to run entirely on the host.
    
    Built and tested on OS X.
    
    Former-commit-id: 15c9da1f3b45d15f08fe310771b696a87da11932

---
+++ apps/opengl_demo/opengl_helpers.h
@@ -0,0 +1,15 @@
+#ifndef _OPENGL_HELPERS_H_
+#define _OPENGL_HELPERS_H_
+
+#include <string>
+#include <OpenGL/gl.h>
+
+namespace OpenGLHelpers {
+    void setup(float dpi_scale);
+    GLuint create_texture(int width, int height, const uint8_t *data);
+    void delete_texture(GLuint texture_id);
+    void display_texture(GLuint texture_id, float x0, float x1, float y0, float y1);
+    void draw_text(const std::string &text, float x, float y);
+}
+
+#endif
