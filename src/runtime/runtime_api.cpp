0346a27ea498241f3e7c01297ee0f4123c1edbab
Steven Johnson <srj@google.com>
2016-06-23 08:12:27 -0700

    Add a bottleneck for getting Halide host symbols
    This approach allows us to provide the host interface via
    statically-linked functions (instead of requiring them to be in a
    shared library).

--- src/runtime/runtime_api.cpp
+++ src/runtime/runtime_api.cpp
@@ -73,6 +73,7 @@ extern "C" __attribute__((used)) void *halide_runtime_api_functions[] = {
     (void *)&halide_hexagon_initialize_kernels,
     (void *)&halide_hexagon_run,
     (void *)&halide_hexagon_device_release,
+    (void *)&halide_hexagon_get_host_lib_symbol,
     (void *)&halide_qurt_hvx_lock,
     (void *)&halide_qurt_hvx_unlock,
     (void *)&halide_qurt_hvx_unlock_as_destructor,
