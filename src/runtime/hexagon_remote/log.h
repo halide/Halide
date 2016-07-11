e9d9819021d53436bc939c746a9eefefca69a9c5
Dillon Sharlet <dsharlet@google.com>
2016-07-11 11:10:58 -0700

    Add mutex to log read/write.

--- src/runtime/hexagon_remote/log.h
+++ src/runtime/hexagon_remote/log.h
@@ -8,6 +8,8 @@
 
 #else
 
+#include <qurt.h>
+
 // On device, we make a circular buffer that log_printf writes to, and
 // we can read from in a remote RPC call.
 class Log {
@@ -16,31 +18,39 @@ class Log {
     int read_cursor;
     int write_cursor;
 
+    qurt_mutex_t lock;
+
 public:
     // The size must be a power of 2.
     Log(int size) : buffer(NULL), size(size), read_cursor(0), write_cursor(0) {
+        qurt_mutex_init(&lock);
         buffer = (char *)malloc(size);
     }
 
     ~Log() {
         free(buffer);
+        qurt_mutex_destroy(&lock);
     }
 
     void write(const char *in, int in_size) {
         if (!buffer) return;
 
+        qurt_mutex_lock(&lock);
         for (int i = 0; i < in_size; i++, write_cursor++) {
             buffer[write_cursor & (size - 1)] = in[i];
         }
+        qurt_mutex_unlock(&lock);
     }
 
     int read(char *out, int out_size) {
+        qurt_mutex_lock(&lock);
         if (out_size > write_cursor - read_cursor) {
             out_size = write_cursor - read_cursor;
         }
         for (int i = 0; i < out_size; i++, read_cursor++) {
             out[i] = buffer[read_cursor & (size - 1)];
         }
+        qurt_mutex_unlock(&lock);
         return out_size;
     }
 };
