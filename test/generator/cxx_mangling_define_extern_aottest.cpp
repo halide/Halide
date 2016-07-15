15f099ac21f3d356dcb94aeb462f7fb38810330d
Steven Johnson <srj@google.com>
2016-07-15 14:15:20 -0700

    Move user-context back to const-void*
    This change causes lots of downstream churn; better to special-case
    this to the status quo for now. Add user-context to one of the
    name-mangled tests to verify that mangling and constness are preserved
    properly.

--- test/generator/cxx_mangling_define_extern_aottest.cpp
+++ test/generator/cxx_mangling_define_extern_aottest.cpp
@@ -30,6 +30,7 @@ int main(int argc, char **argv) {
 
     Image<double> result(100);
 
+    const void *user_context = nullptr;
     int ptr_arg = 42;
     int *int_ptr = &ptr_arg;
     const int *const_int_ptr = &ptr_arg;
@@ -37,7 +38,7 @@ int main(int argc, char **argv) {
     const void *const_void_ptr = nullptr;
     std::string *string_ptr = nullptr;
     const std::string *const_string_ptr = nullptr;
-    assert(HalideTest::cxx_mangling_define_extern(input, int_ptr, const_int_ptr, 
+    assert(HalideTest::cxx_mangling_define_extern(user_context, input, int_ptr, const_int_ptr, 
         void_ptr, const_void_ptr, string_ptr, const_string_ptr, result) == 0);
 
     printf("Success!\n");
