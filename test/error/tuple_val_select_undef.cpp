3e6b80d6fbf6bb16689c28ebf6d8c1e2d7c83807
Patricia Suriana <psuriana@google.com>
2016-08-04 11:27:31 -0700

    added assertion in RemoveUndef to ensure all args/values in a Tuple have the same predicates

---
+++ test/error/tuple_val_select_undef.cpp
@@ -0,0 +1,17 @@
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+using namespace Halide::Internal;
+
+int main(int argc, char **argv) {
+    Var x("x");
+    Func f("f");
+
+    // Should result in an error
+    f(x) = {x, select(x < 20, 20*x, undef<int>())};
+    f.realize(10);
+
+    printf("Success!\n");
+    return 0;
+}
