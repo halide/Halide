781251fe1e482433207a56c1c80df026d207b3b2
Shoaib Kamil <kamil@adobe.com>
2016-08-10 14:20:59 -0400

    Multiple stages can now be scheduled glsl().
    Working test with two `glsl()` scheduled stages and a CPU
    stage in between.

--- test/opengl/multiple_stages.cpp
+++ test/opengl/multiple_stages.cpp
@@ -11,14 +11,24 @@ int main() {
         return 1;
     }
 
-    Func f,g;
+    Func f,g,h;
     Var x, y, c;
     g(x, y, c) = cast<uint8_t>(x);
-    f(x, y, c) = g(x, y, c) + cast<uint8_t>(y);
+    h(x, y, c) = 1 + g(x, y, c);
+    f(x, y, c) = h(x, y, c) + cast<uint8_t>(y);
     f.bound(c, 0, 3).glsl(x, y, c);
+    h.bound(c, 0, 3).compute_root();
     g.bound(c, 0, 3).compute_root().glsl(x, y, c);
 
     Image<uint8_t> result = f.realize(100, 100, 3);
+    result.copy_to_host();
+
+    for (int i=0; i<100; i++)
+      for (int j=0; j<100; j++)
+        if (result(i,j,1) != i+j+1) {
+          printf("FAILED\n");
+          return 1;
+        }
 
     printf("Success!\n");
 
