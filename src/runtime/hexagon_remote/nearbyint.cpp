c278fb483160bd36108872a090d797de1ddbdd0b
Dillon Sharlet <dsharlet@google.com>
2016-08-16 21:17:36 -0700

    Add comment on nearbyint implementation.
    Former-commit-id: 062c1bbf80adb97674a04b5ea48e3338cd3dff95

--- src/runtime/hexagon_remote/nearbyint.cpp
+++ src/runtime/hexagon_remote/nearbyint.cpp
@@ -2,6 +2,10 @@
 
 extern "C" {
 
+// Hexagon doesn't have an implementation of nearbyint/nearbyintf, so
+// we provide one here. This implementation is not great, nearbyint is
+// supposed to round to nearest even in the case of a tie.
+
 float nearbyintf(float x) {
     return floorf(x + 0.5f);
 }
