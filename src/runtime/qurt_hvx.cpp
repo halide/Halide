256f918b3ad1d86704b5b373b2d73c99b3aaa2dd
Dan Palermo <dpalermo@codeaurora.org>
2016-08-23 12:40:28 -0500

    Update prefetch descriptor computation
    Former-commit-id: ae90ba1c8a8914dda6004521251ea410e52cf221

--- src/runtime/qurt_hvx.cpp
+++ src/runtime/qurt_hvx.cpp
@@ -57,40 +57,53 @@ WEAK void halide_qurt_hvx_unlock_as_destructor(void *user_context, void * /*obj*
 
 WEAK int halide_hexagon_prefetch_buffer_t(unsigned int dim, buffer_t *buf)
 {
-    // get starting position of box
-    unsigned char *addr = buf->host;
-    addr += buf->elem_size * ((buf->min[0]) * buf->stride[0] +
-                              (buf->min[1]) * buf->stride[1] +
-                              (buf->min[2]) * buf->stride[2] +
-                              (buf->min[3]) * buf->stride[3]);
+    // Extract needed fields from buffer_t
+    int32_t elem_size_bytes = buf->elem_size;
+    int32_t extent0 = buf->extent[0];
+    int32_t extent1 = buf->extent[1];
+    int32_t stride0 = buf->stride[0];
+    int32_t stride1 = buf->stride[1];
+    int32_t stride2 = buf->stride[2];
+    int32_t stride3 = buf->stride[3];
+    int32_t min0    = buf->min[0];
+    int32_t min1    = buf->min[1];
+    int32_t min2    = buf->min[2];
+    int32_t min3    = buf->min[3];
 
-    // compute descriptor
+    // Compute starting position of box
+    unsigned char *addr = buf->host;
+    addr += elem_size_bytes * (min0 * stride0 +
+                               min1 * stride1 +
+                               min2 * stride2 +
+                               min3 * stride3);
+
+    // Compute prefetch descriptor
     // l2fetch(Rs,Rtt): 48 bit descriptor
-    unsigned int stride = (buf->stride[0]) * buf->elem_size;
-    unsigned int width  = (buf->extent[0] - buf->min[0]) * buf->elem_size;
-    unsigned int height = (buf->extent[1] - buf->min[1]);
-    unsigned int dir    = 0;  // 0 row major, 1 = colum major
+    uint32_t dir    = 1;   // 0 row major, 1 = column major
+    uint32_t stride = stride0 * elem_size_bytes;
+    uint32_t width  = extent0 * elem_size_bytes;
+    uint32_t height = extent1;
 
     dir    = dir & 0x1;           // bit  48
     stride = MIN(stride, 0xFFFF); // bits 47:32
     width  = MIN(width,  0xFFFF); // bits 31:16
     height = MIN(height, 0xFFFF); // bits 15:0
 
-    unsigned long long dirull    = dir;
-    unsigned long long strideull = stride;
-    unsigned long long desc = (dirull<<48) | (strideull<<32) | (width<<16) | height;
+    uint64_t dir64    = dir;
+    uint64_t stride64 = stride;
+    uint64_t desc = (dir64<<48) | (stride64<<32) | (width<<16) | height;
 
-    // FIXME: hard coded descriptors for testing
-    // unsigned long long desc = 0x1020002000002; // 512 width, 2 rows
-    // unsigned long long desc = 0x1020002000001; // 512 width, 1 row
-    // unsigned long long desc = 0x1144014400001; // 5184 width, 1 row
+    // Hard coded descriptors for testing
+    // uint64_t desc = 0x1020002000002; // col, 512 width, 2 rows
+    // uint64_t desc = 0x1020002000001; // col, 512 width, 1 row
+    // uint64_t desc = 0x1144014400001; // col, 5184 width, 1 row
 
     // FIXME: iterate over buffer_t 3rd & 4th dimensions
 
-    // perform prefetch
+    // Perform prefetch
     // notes:
     //  - Prefetches can be queued up to 3 deep
-    //  - If 3 are already pending, the olds requrest is dropped
+    //  - If 3 are already pending, the oldest request is dropped
     //  - USR:PFA status bit is set to indicate that prefetches are in progress
     //  - A l2fetch with any subfield set to zero cancels all pending prefetches
     __asm__ __volatile__ ("l2fetch(%0,%1)" : : "r"(addr), "r"(desc));
