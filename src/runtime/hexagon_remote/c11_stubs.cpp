66a43aecb5bbee2f55afa01ea03c17b7aed5ad45
Ron Lieberman <ronl@codeaurora.org>
2016-10-25 11:58:04 -0500

    Hexagon-tools 8.0.06 and later are dependent on 2 additional symbols:    __cxa_finalize and __cxa_atexit We are providing weak symbol definitions of the these functions.

--- src/runtime/hexagon_remote/c11_stubs.cpp
+++ src/runtime/hexagon_remote/c11_stubs.cpp
@@ -2,13 +2,11 @@ extern "C" {
 
 // Hexagon-tools 8.0.06 and later are dependent on 2 additional symbols:
 //    __cxa_finalize and __cxa_atexit
-// for backward compatability with hexagon_remote runtimes, we are
-// providing weak symbol definitions of the these functions.
+// We are providing weak symbol definitions of the these functions.
 
 #include "HAP_farf.h"
-#include "HAP_power.h"
 
-#define FARF_LOW 1    // Enable debug output
+//#define FARF_LOW 1    // Enable debug output
 
 void __attribute__ ((weak)) __cxa_finalize() {
   FARF(LOW, "Finalizing\n");
