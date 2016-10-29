88402e8fd897085978a53af3951283a8d93338b3
Ron Lieberman <ronl@codeaurora.org>
2016-10-29 16:58:52 -0500

    Add fixed point changes to local_laplacian

--- apps/local_laplacian_fixed/local_laplacian_fixed_generator.cpp
+++ apps/local_laplacian_fixed/local_laplacian_fixed_generator.cpp
@@ -4,7 +4,7 @@ namespace {
 
 constexpr int maxJ = 20;
 
-class LocalLaplacian : public Halide::Generator<LocalLaplacian> {
+class LocalLaplacianFixed : public Halide::Generator<LocalLaplacianFixed> {
 public:
 
     GeneratorParam<int>   pyramid_levels{"pyramid_levels", 8, 1, maxJ};
@@ -21,26 +21,28 @@ public:
         // Make the remapping function as a lookup table.
         Func remap;
         Expr fx = cast<float>(x) / 256.0f;
-        remap(x) = alpha*fx*exp(-fx*fx/2.0f);
+        remap(x) = cast<int16_t>(1024.0f * alpha * fx * exp(-fx*fx/2.0f));
 
         // Set a boundary condition
         Func clamped = Halide::BoundaryConditions::repeat_edge(input);
 
         // Convert to floating point
-        Func floating;
-        floating(x, y, c) = clamped(x, y, c) / 65535.0f;
+        Func fixed_8;
+        fixed_8(x, y, c) = cast<int16_t>(clamped(x, y, c));
 
         // Get the luminance channel
         Func gray;
-        gray(x, y) = 0.299f * floating(x, y, 0) + 0.587f * floating(x, y, 1) + 0.114f * floating(x, y, 2);
+        gray(x, y) = fixed_8(x, y, 0) + 2 * fixed_8(x, y, 1) + fixed_8(x, y, 2);
 
         // Make the processed Gaussian pyramid.
         Func gPyramid[maxJ];
         // Do a lookup into a lut with 256 entires per intensity level
-        Expr level = k * (1.0f / (levels - 1));
-        Expr idx = gray(x, y)*cast<float>(levels-1)*256.0f;
-        idx = clamp(cast<int>(idx), 0, (levels-1)*256);
-        gPyramid[0](x, y, k) = beta*(gray(x, y) - level) + level + remap(idx - 256*k);
+        Expr level = cast<int16_t>(( 1024 * k) / (levels - 1));
+        Expr idx = cast<int>(gray(x, y))*(levels-1)/4;
+        Expr beta_fixed = cast<uint8_t>(beta * 255);
+        //idx = clamp(cast<int>(idx), 0, (levels-1)*256);
+        gPyramid[0](x, y, k) = lerp(level, gray(x, y), beta_fixed) + remap(idx - 256*k);
+
         for (int j = 1; j < J; j++) {
             gPyramid[j](x, y, k) = downsample(gPyramid[j-1])(x, y, k);
         }
@@ -62,12 +64,18 @@ public:
         // Make the laplacian pyramid of the output
         Func outLPyramid[maxJ];
         for (int j = 0; j < J; j++) {
-            // Split input pyramid value into integer and floating parts
-            Expr level = inGPyramid[j](x, y) * cast<float>(levels-1);
-            Expr li = clamp(cast<int>(level), 0, levels-2);
-            Expr lf = level - cast<float>(li);
+            // inGPyramid is a 10-bit value stored in an int16_t, so this shouldn't overflow if levels <= 32
+            Expr level = inGPyramid[j](x, y) * (levels-1);
+
+            // Split it into integer and fractional parts
+            Expr li = clamp(level / 1024, 0, levels-2);
+            Expr lf = level % 1024;
+
+            // Turn lf into an 8-bit interpolant
+            lf = cast<uint8_t>(lf / 4);
+
             // Linearly interpolate between the nearest processed pyramid levels
-            outLPyramid[j](x, y) = (1.0f - lf) * lPyramid[j](x, y, li) + lf * lPyramid[j](x, y, li+1);
+            outLPyramid[j](x, y) = lerp(lPyramid[j](x, y, li), lPyramid[j](x, y, li+1), lf);
         }
 
         // Make the Gaussian pyramid of the output
@@ -77,16 +85,13 @@ public:
             outGPyramid[j](x, y) = upsample(outGPyramid[j+1])(x, y) + outLPyramid[j](x, y);
         }
 
-        // Reintroduce color (Connelly: use eps to avoid scaling up noise w/ apollo3.png input)
         Func color;
-        float eps = 0.01f;
-        color(x, y, c) = outGPyramid[0](x, y) * (floating(x, y, c)+eps) / (gray(x, y)+eps);
-
-        Func output("local_laplacian");
-        // Convert back to 16-bit
-        output(x, y, c) = cast<uint16_t>(clamp(color(x, y, c), 0.0f, 1.0f) * 65535.0f);
+        color(x, y, c) = fixed_8(x, y, c) + (outGPyramid[0](x, y) - gray(x, y)) / 4;
 
 
+        // Clamp and cast back to 16-bit
+        Func output;
+        output(x, y, c) = cast<uint16_t>(clamp(color(x, y, c), 0, 255));
 
         /* THE SCHEDULE */
         remap.compute_root();
@@ -106,6 +111,29 @@ public:
                 }
                 outGPyramid[j].compute_root().gpu_tile(x, y, blockw, blockh);
             }
+        } else if (get_target().features_any_of({Target::HVX_64, Target::HVX_128})) {
+            // cpu schedule
+            Var yi;
+            int vec_lanes_16 = get_target().natural_vector_size(UInt(16));
+            int vec_lanes_8  = get_target().natural_vector_size(UInt(8));
+            output.parallel(y, 32).vectorize(x, vec_lanes_8);
+            gray.compute_root().parallel(y, 32).vectorize(x, vec_lanes_16);
+            for (int j = 0; j < 4; j++) {
+                if (j > 0) {
+                    inGPyramid[j]
+                        .compute_root().parallel(y, 32).vectorize(x, vec_lanes_16);
+                    gPyramid[j]
+                        .compute_root().reorder_storage(x, k, y)
+                        .reorder(k, y).parallel(y, 8).vectorize(x, vec_lanes_16);
+                }
+                outGPyramid[j].compute_root().parallel(y, 32).vectorize(x, vec_lanes_16);
+            }
+            for (int j = 4; j < J; j++) {
+                inGPyramid[j].compute_root().hexagon();
+                gPyramid[j].compute_root().hexagon().parallel(k);
+                outGPyramid[j].compute_root().hexagon();
+            }
+
         } else {
             // cpu schedule
             Var yo;
@@ -139,8 +167,8 @@ private:
     Func downsample(Func f) {
         using Halide::_;
         Func downx, downy;
-        downx(x, y, _) = (f(2*x-1, y, _) + 3.0f * (f(2*x, y, _) + f(2*x+1, y, _)) + f(2*x+2, y, _)) / 8.0f;
-        downy(x, y, _) = (downx(x, 2*y-1, _) + 3.0f * (downx(x, 2*y, _) + downx(x, 2*y+1, _)) + downx(x, 2*y+2, _)) / 8.0f;
+        downx(x, y, _) = (f(2*x-1, y, _) + 3 * (f(2*x, y, _) + f(2*x+1, y, _)) + f(2*x+2, y, _)) / 8;
+        downy(x, y, _) = (downx(x, 2*y-1, _) + 3 * (downx(x, 2*y, _) + downx(x, 2*y+1, _)) + downx(x, 2*y+2, _)) / 8;
         return downy;
     }
 
@@ -148,14 +176,14 @@ private:
     Func upsample(Func f) {
         using Halide::_;
         Func upx, upy;
-        upx(x, y, _) = 0.25f * f((x/2) - 1 + 2*(x % 2), y, _) + 0.75f * f(x/2, y, _);
-        upy(x, y, _) = 0.25f * upx(x, (y/2) - 1 + 2*(y % 2), _) + 0.75f * upx(x, y/2, _);
+        upx(x, y, _) = (f((x/2) - 1 + 2*(x % 2), y, _) + 3 * f(x/2, y, _)) / 4;
+        upy(x, y, _) = (upx(x, (y/2) - 1 + 2*(y % 2), _) + 3 * upx(x, y/2, _)) / 4;
         return upy;
     }
 
 
 };
 
-Halide::RegisterGenerator<LocalLaplacian> register_me{"local_laplacian"};
+Halide::RegisterGenerator<LocalLaplacianFixed> register_me{"local_laplacian_fixed"};
 
 }  // namespace
