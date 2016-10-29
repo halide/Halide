245ddad589d0eea598c71881d594149e457a22c1
Ron Lieberman <ronl@codeaurora.org>
2016-10-29 16:29:35 -0500

    Add base version of local_laplacian_fixed by adding and renaming local_laplacian

---
+++ apps/local_laplacian_fixed/process.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <chrono>
+
+#include "local_laplacian_fixed.h"
+
+#include "benchmark.h"
+#include "HalideBuffer.h"
+#include "halide_image_io.h"
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    if (argc < 7) {
+        printf("Usage: ./process input.png levels alpha beta timing_iterations output.png\n"
+               "e.g.: ./process input.png 8 1 1 10 output.png\n");
+        return 0;
+    }
+
+    Image<uint16_t> input = Tools::load_image(argv[1]);
+    int levels = atoi(argv[2]);
+    float alpha = atof(argv[3]), beta = atof(argv[4]);
+    Image<uint16_t> output(input.width(), input.height(), 3);
+    int timing = atoi(argv[5]);
+
+    // Timing code
+    double best = benchmark(timing, 1, [&]() {
+        local_laplacian_fixed(input, levels, alpha/(levels-1), beta, output);
+    });
+    printf("%gus\n", best * 1e6);
+
+
+    local_laplacian_fixed(input, levels, alpha/(levels-1), beta, output);
+
+    Tools::save_image(output, argv[6]);
+
+    return 0;
+}
