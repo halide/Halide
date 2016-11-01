6a538449eeaa591bc77768a8a02342aa60161c17
Patricia Suriana <patricia.suriana@gmail.com>
2016-11-01 15:55:44 -0700

    Modify rfactor to use apply splits helper function

--- src/ApplySplits.h
+++ src/ApplySplits.h
@@ -23,14 +23,22 @@ struct ApplySplitResult {
     std::vector<Expr> predicates;
 };
 
-/** Return a list of let stmts, substitutions, and predicates given a split schedule. */
+/** Given a Split schedule on a definition (init or update), return a list of
+ * of predicates on the definition, a list of substitutions that needs to be
+ * applied to the definition (in ascending order of application), and a list
+ * of let stmts which defined the values of variables referred by the predicates
+ * and substitutions (ordered from innermost to outermost let). */
 ApplySplitResult apply_split(const Split &split, bool is_update, std::string prefix,
                              std::map<std::string, Expr> &dim_extent_alignment);
 
+/** Apply a list of Split schedules (in ascending order) on a
+ * definition (init or update). See \ref apply_split. */
 ApplySplitResult apply_splits(
     const std::vector<Split> &splits, bool is_update, std::string prefix,
     std::map<std::string, Expr> &dim_extent_alignment);
 
+/** Compute the bounds of the new dimensions resulting from applying the
+ * split schedules using the bounds of the old dimensions. */
 std::vector<std::pair<std::string, Expr>> compute_bounds_after_split(
     const Split &split, std::string prefix);
 
