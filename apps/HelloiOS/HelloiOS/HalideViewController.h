f22fa518bb62049cfdbfd2f342f412327735b066
Steven Johnson <srj@google.com>
2016-11-18 12:45:34 -0800

    Get HelloiOS working again
    Was broken for recent versions of iOS/XCode
    
    
    Former-commit-id: 1c2fc4913a9fda2b8b4b10e8fb1e051147d51aa7

---
+++ apps/HelloiOS/HelloiOS/HalideViewController.h
@@ -0,0 +1,20 @@
+//
+//  HalideViewController.h
+//
+//  Created by Z Stern on 8/10/15.
+//  Copyright (c) 2015 Andrew Adams. All rights reserved.
+//
+
+#ifndef HelloiOS_HalideViewController_h
+#define HelloiOS_HalideViewController_h
+
+#import "HalideView.h"
+#import <UIKit/UIKit.h>
+
+@interface HalideViewController : UIViewController
+
+@property HalideView *halide_view;
+
+@end
+
+#endif
