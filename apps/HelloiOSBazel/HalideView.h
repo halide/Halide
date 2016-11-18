91c89b7ad07acb29a46482a512ceb30d6cb4be2a
Steven Johnson <srj@google.com>
2016-11-17 17:21:01 -0800

    Add Bazel-adapted version of apps/HelloiOS
    Added separately so that unnecessary cruft could be pruned out.
    
    Some minor changes to the source were necessary due to different iOS
    version targets but basically the ObjC code is the same.
    
    Explicit Halide code was converted to Generator in a simpleminded way.
    
    See https://bazel.build/versions/master/docs/tutorial/ios-app.html for
    details on how to run in simulator, etc. using Xcode.
    
    
    Former-commit-id: 86cb556d9035ce868b9ebc8ec18fd731528e24f1

---
+++ apps/HelloiOSBazel/HalideView.h
@@ -0,0 +1,16 @@
+//
+//  HalideView.h
+//  Halide test
+//
+//  Created by Andrew Adams on 7/23/14.
+//  Copyright (c) 2014 Andrew Adams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HalideView : UIImageView
+
+@property CGPoint touch_position;
+@property bool touch_active;
+
+@end
