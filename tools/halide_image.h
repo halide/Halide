f651d51d97b75f12ba68f1cbfca914724136d121
Z Stern <zalman@zalman-macbookpro2.roam.corp.google.com>
2016-12-06 01:08:07 -0800

    Reformat comment into Doxygen comment for file.

--- tools/halide_image.h
+++ tools/halide_image.h
@@ -1,19 +1,19 @@
 #ifndef HALIDE_TOOLS_IMAGE_H
 #define HALIDE_TOOLS_IMAGE_H
 
-/*
-
-This allows code that relied on halide_image.h and Halide::Tools::Image to
-continue to work with newer versions of Halide where HalideBuffer.h and
-Halide::Buffer are the way to work with data.
-
-Besides mapping Halide::Tools::Image to Halide::Buffer, it defines
-USING_HALIDE_BUFFER to allow code to conditionally compile for one or the
-other.
-
-It is intended as a stop-gap measure until the code can be updated.
-
-*/
+/** \file
+ *
+ * This allows code that relied on halide_image.h and
+ * Halide::Tools::Image to continue to work with newer versions of
+ * Halide where HalideBuffer.h and Halide::Buffer are the way to work
+ * with data.
+ *
+ * Besides mapping Halide::Tools::Image to Halide::Buffer, it defines
+ * USING_HALIDE_BUFFER to allow code to conditionally compile for one
+ * or the other.
+ *
+ * It is intended as a stop-gap measure until the code can be updated.
+ */
 
 #include "HalideBuffer.h"
 
