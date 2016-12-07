db46cc5ca3f1c8e2ea2a6a309a710f896b85221b
Andrew Adams <andrew.b.adams@gmail.com>
2016-12-06 17:24:57 -0800

    Fix realizing into multiple existing buffers
    The natural way to realize into multiple existing buffers currently is
    to put them in a temporary Realization:
    
    f.realize({im1, im2});
    
    This doesn't do precisely you want (it used to, before the Buffer
    change). The Realization constructor makes a copy of those buffer
    objects that it can own, so you'll lose any mutation of the metadata
    (e.g. device allocations). It's also different to this:
    
    f.realize(im1);
    
    which takes im1 by reference and updates the metadata
    
    This PR adds a new realization-like class that collects an array of
    mutable references to existing buffers. It's a temporary type solely
    used for passing groups of buffers to realize and infer_input_bounds. It
    changes the meaning of
    
    f.realize({im1, im2})
    
    to take im1 and im2 by reference, the way it used to be.
    
    
    Former-commit-id: 73ee0d68d2f0d9b0b36631cf18c70fc6979111f6

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -920,7 +920,7 @@ struct JITFuncCallContext {
 // Make a vector of void *'s to pass to the jit call using the
 // currently bound value for all of the params and image
 // params.
-vector<const void *> Pipeline::prepare_jit_call_arguments(Realization dst, const Target &target) {
+vector<const void *> Pipeline::prepare_jit_call_arguments(BufferRefCollection &dst, const Target &target) {
     user_assert(defined()) << "Can't realize an undefined Pipeline\n";
 
     compile_jit(target);
@@ -994,8 +994,8 @@ vector<const void *> Pipeline::prepare_jit_call_arguments(Realization dst, const
     }
 
     // Then the outputs
-    for (const Buffer<> &buf : dst) {
-        arg_values.push_back(buf.raw_buffer());
+    for (size_t i = 0; i < dst.size(); i++) {
+        arg_values.push_back(dst[i].raw_buffer());
         const void *ptr = arg_values.back();
         debug(1) << "JIT output buffer @ " << ptr << "\n";
     }
@@ -1052,7 +1052,7 @@ Pipeline::make_externs_jit_module(const Target &target,
     return result;
 }
 
-void Pipeline::realize(Realization dst, const Target &t) {
+void Pipeline::realize(BufferRefCollection dst, const Target &t) {
     Target target = t;
     user_assert(defined()) << "Can't realize an undefined Pipeline\n";
 
@@ -1165,7 +1165,7 @@ void Pipeline::realize(Realization dst, const Target &t) {
     jit_context.finalize(exit_status);
 }
 
-void Pipeline::infer_input_bounds(Realization dst) {
+void Pipeline::infer_input_bounds(BufferRefCollection dst) {
 
     Target target = get_jit_target_from_environment();
 
