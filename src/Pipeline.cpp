80348692038ac335642c215e176b8eb6b0337c94
Andrew Adams <andrew.b.adams@gmail.com>
2016-12-09 14:39:20 -0800

    Rename BufferRefCollection to BufferRefs

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -920,7 +920,7 @@ struct JITFuncCallContext {
 // Make a vector of void *'s to pass to the jit call using the
 // currently bound value for all of the params and image
 // params.
-vector<const void *> Pipeline::prepare_jit_call_arguments(BufferRefCollection &dst, const Target &target) {
+vector<const void *> Pipeline::prepare_jit_call_arguments(BufferRefs &dst, const Target &target) {
     user_assert(defined()) << "Can't realize an undefined Pipeline\n";
 
     compile_jit(target);
@@ -1052,7 +1052,7 @@ Pipeline::make_externs_jit_module(const Target &target,
     return result;
 }
 
-void Pipeline::realize(BufferRefCollection dst, const Target &t) {
+void Pipeline::realize(BufferRefs dst, const Target &t) {
     Target target = t;
     user_assert(defined()) << "Can't realize an undefined Pipeline\n";
 
@@ -1165,7 +1165,7 @@ void Pipeline::realize(BufferRefCollection dst, const Target &t) {
     jit_context.finalize(exit_status);
 }
 
-void Pipeline::infer_input_bounds(BufferRefCollection dst) {
+void Pipeline::infer_input_bounds(BufferRefs dst) {
 
     Target target = get_jit_target_from_environment();
 
