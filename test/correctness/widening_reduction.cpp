84695271b24954d50517869f726f6ead6db4114f
Dillon Sharlet <dsharlet@google.com>
2016-12-16 10:48:09 -0800

    Improve widening reduction test.
    Former-commit-id: a0390759158d82cea0c77e187a11cb78ca8dec61

--- test/correctness/widening_reduction.cpp
+++ test/correctness/widening_reduction.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 using namespace Halide;
+using namespace Halide::ConciseCasts;
 
 int main(int arch, char **argv) {
     const int W = 256, H = 256;
@@ -15,13 +16,11 @@ int main(int arch, char **argv) {
     }
 
     // Define a convolution kernel, and its sum.
-    Buffer<uint8_t> kernel(3, 3);
+    Buffer<int8_t> kernel(3, 3);
     kernel.set_min(-1, -1);
-    int kernel_sum = 0;
     for (int y = -1; y <= 1; y++) {
         for (int x = -1; x <= 1; x++) {
-            kernel(x, y) = rand() % 0x7;
-            kernel_sum += kernel(x, y);
+            kernel(x, y) = rand() % 8 - 4;
         }
     }
 
@@ -35,33 +34,30 @@ int main(int arch, char **argv) {
     // Test a widening reduction, followed by a narrowing.
     {
         Func f;
-        f(x, y) = sum(cast<uint16_t>(input(x + r.x, y + r.y)) * kernel(r.x, r.y));
-
-        Func g;
-        g(x, y) = cast<uint8_t>(f(x, y) / kernel_sum);
+        f(x, y) = u8_sat(sum(i16(input(x + r.x, y + r.y)) * kernel(r.x, r.y)) / 16);
 
         // Schedule.
         Target target = get_jit_target_from_environment();
         if (target.has_gpu_feature()) {
-            g.gpu_tile(x, y, 16, 16);
+            f.gpu_tile(x, y, 16, 16);
         } else if (target.features_any_of({Target::HVX_64, Target::HVX_128})) {
-            g.hexagon().vectorize(x, 128);
+            f.hexagon().vectorize(x, 128);
         } else {
-            g.vectorize(x, target.natural_vector_size<uint8_t>());
+            f.vectorize(x, target.natural_vector_size<uint8_t>());
         }
 
         // Run the pipeline and verify the results are correct.
-        Buffer<uint8_t> out = g.realize(W, H, target);
+        Buffer<uint8_t> out = f.realize(W, H, target);
 
         for (int y = 1; y < H-1; y++) {
             for (int x = 1; x < W-1; x++) {
-                uint16_t correct = 0;
+                int16_t correct = 0;
                 for (int ry = -1; ry <= 1; ry++) {
                     for (int rx = -1; rx <= 1; rx++) {
-                        correct += static_cast<uint16_t>(in(x + rx, y + ry)) * kernel(rx, ry);
+                        correct += static_cast<int16_t>(in(x + rx, y + ry)) * kernel(rx, ry);
                     }
                 }
-                correct /= kernel_sum;
+                correct = std::min(std::max(correct / 16, 0), 255);
                 if (correct != out(x, y)) {
                     std::cout << "out(" << x << ", " << y << ") = " << (int)out(x, y) << " instead of " << correct << "\n";
                     return -1;
@@ -73,14 +69,14 @@ int main(int arch, char **argv) {
     // Test a tuple reduction with widening, followed by narrowing the result.
     {
         Func f;
-        f(x, y) = { cast<uint16_t>(0), cast<uint8_t>(0) };
+        f(x, y) = { i16(0), i8(0) };
         f(x, y) = {
-            f(x, y)[0] + cast<uint16_t>(input(x + r.x, y + r.y)) * kernel(r.x, r.y),
+            f(x, y)[0] + i16(input(x + r.x, y + r.y)) * kernel(r.x, r.y),
             f(x, y)[1] + kernel(r.x, r.y),
         };
 
         Func g;
-        g(x, y) = cast<uint8_t>((f(x, y)[0] + f(x, y)[1]) / kernel_sum);
+        g(x, y) = u8_sat((f(x, y)[0] + f(x, y)[1]) / 16);
 
         // Schedule.
         Target target = get_jit_target_from_environment();
@@ -97,13 +93,14 @@ int main(int arch, char **argv) {
 
         for (int y = 1; y < H-1; y++) {
             for (int x = 1; x < W-1; x++) {
-                uint16_t correct = kernel_sum;
+                int16_t correct = 0;
                 for (int ry = -1; ry <= 1; ry++) {
                     for (int rx = -1; rx <= 1; rx++) {
-                        correct += static_cast<uint16_t>(in(x + rx, y + ry)) * kernel(rx, ry);
+                        correct += static_cast<int16_t>(in(x + rx, y + ry)) * kernel(rx, ry);
+                        correct += kernel(rx, ry);
                     }
                 }
-                correct /= kernel_sum;
+                correct = std::min(std::max(correct / 16, 0), 255);
                 if (correct != out(x, y)) {
                     std::cout << "out(" << x << ", " << y << ") = " << (int)out(x, y) << " instead of " << correct << "\n";
                     return -1;
