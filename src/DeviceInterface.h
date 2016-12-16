3fcb6961db834f4f15c70328b187ce207012d218
Andrew Adams <andrew.b.adams@gmail.com>
2016-12-16 11:32:01 -0800

    Reconsider how device apis are selected in Buffer::copy_to_device

--- src/DeviceInterface.h
+++ src/DeviceInterface.h
@@ -9,10 +9,22 @@
 
 namespace Halide {
 
-/** Get the appropriate halide_device_interface_t * for a target. The
- * target should have a single gpu api enabled. Creates a GPU runtime
- * module for the target if necessary. */
-halide_device_interface_t *get_device_interface_for_target(const Target &t);
+/** Get the appropriate halide_device_interface_t * for a
+ * target. Corresponds to the device interface that would be used for
+ * DeviceAPI::Default_GPU. Creates a GPU runtime module for the target
+ * if necessary. Returns nullptr if no device APIs are enabled in the
+ * target. */
+halide_device_interface_t *get_default_device_interface_for_target(const Target &t);
+
+/** Gets the appropriate halide_device_interface_t * for a
+ * DeviceAPI. Asserts that the argument is not Default_GPU, None, or
+ * Host. */
+halide_device_interface_t *get_device_interface_for_device_api(const DeviceAPI &d);
+
+/** Get the specific DeviceAPI that Halide would select when presented
+ * with DeviceAPI::Default_GPU for a given target. If no suitable api
+ * is enabled in the target, returns DeviceAPI::Host. */
+DeviceAPI get_default_device_api_for_target(const Target &t);
 
 }
 
--- test/correctness/gpu_jit_explicit_copy_to_device.cpp
+++ test/correctness/gpu_jit_explicit_copy_to_device.cpp
@@ -13,30 +13,61 @@ int main(int argc, char **argv) {
     // We'll have two input buffers. For one we'll copy to the device
     // explicitly. For the other we'll do a device malloc and set
     // host_dirty.
-    Buffer<float> a(100, 100), b(100, 100);
+    {
+        Buffer<float> a(100, 100), b(100, 100);
 
-    assert(!a.host_dirty());
-    a.fill(2.0f);
-    assert(!a.has_device_allocation());
-    assert(a.host_dirty());
-    a.copy_to_device();
-    assert(a.has_device_allocation());
-    assert(!a.host_dirty());
+        assert(!a.host_dirty());
+        a.fill(2.0f);
+        assert(!a.has_device_allocation());
+        assert(a.host_dirty());
+        a.copy_to_device();
+        assert(a.has_device_allocation());
+        assert(!a.host_dirty());
 
-    assert(!b.host_dirty());
-    b.fill(3.0f);
-    assert(!b.has_device_allocation());
-    assert(b.host_dirty());
-    b.device_malloc();
-    assert(b.has_device_allocation());
-    assert(b.host_dirty());
+        assert(!b.host_dirty());
+        b.fill(3.0f);
+        assert(!b.has_device_allocation());
+        assert(b.host_dirty());
+        b.device_malloc();
+        assert(b.has_device_allocation());
+        assert(b.host_dirty());
 
-    Func f;
-    Var x, y;
-    f(x, y) = a(x, y) + b(x, y) + 2;
-    f.gpu_tile(x, y, 8, 8);
+        Func f;
+        Var x, y;
+        f(x, y) = a(x, y) + b(x, y) + 2;
+        f.gpu_tile(x, y, 8, 8);
 
-    Buffer<float> out = f.realize(100, 100);
+        Buffer<float> out = f.realize(100, 100);
+    }
+
+    if (target.has_feature(Target::CUDA)) {
+        // Now let's try again using an explicit device API
+
+        Buffer<float> a(100, 100), b(100, 100);
+
+        assert(!a.host_dirty());
+        a.fill(2.0f);
+        assert(!a.has_device_allocation());
+        assert(a.host_dirty());
+        a.copy_to_device(DeviceAPI::Cuda);
+        assert(a.has_device_allocation());
+        assert(!a.host_dirty());
+
+        assert(!b.host_dirty());
+        b.fill(3.0f);
+        assert(!b.has_device_allocation());
+        assert(b.host_dirty());
+        b.device_malloc(DeviceAPI::Cuda);
+        assert(b.has_device_allocation());
+        assert(b.host_dirty());
+
+        Func f;
+        Var x, y;
+        f(x, y) = a(x, y) + b(x, y) + 2;
+        f.gpu_tile(x, y, 8, 8, DeviceAPI::Cuda);
+
+        Buffer<float> out = f.realize(100, 100);
+    }
 
     // Here's a wart: b was copied to the device, but it still has
     // host_dirty set, because it is a *copy* of b's buffer_t that is
