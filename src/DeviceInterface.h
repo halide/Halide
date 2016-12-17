6515527ddc068e154062473c8e5e11a3ee59debc
Andrew Adams <andrew.b.adams@gmail.com>
2016-12-17 15:34:37 -0800

    Fix return of incomplete type

--- src/DeviceInterface.h
+++ src/DeviceInterface.h
@@ -14,13 +14,13 @@ namespace Halide {
  * DeviceAPI::Default_GPU. Creates a GPU runtime module for the target
  * if necessary. Returns nullptr if no device APIs are enabled in the
  * target. */
-halide_device_interface_t *get_default_device_interface_for_target(const Target &t);
+const halide_device_interface_t *get_default_device_interface_for_target(const Target &t);
 
 /** Gets the appropriate halide_device_interface_t * for a
  * DeviceAPI. Returns null if that device API is not enabled in the
  * target, or if the argument is None or Host. */
-halide_device_interface_t *get_device_interface_for_device_api(const DeviceAPI &d,
-                                                               const Target &t = get_jit_target_from_environment());
+const halide_device_interface_t *get_device_interface_for_device_api(const DeviceAPI &d,
+                                                                     const Target &t = get_jit_target_from_environment());
 
 /** Get the specific DeviceAPI that Halide would select when presented
  * with DeviceAPI::Default_GPU for a given target. If no suitable api
