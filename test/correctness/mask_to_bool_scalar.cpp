8bcfaab974b50d028e3ec5ac8d0a663631a36ba1
Ron Lieberman <ronl@codeaurora.org>
2016-12-18 14:04:55 -0600

    Modify test to execute hexagon schedule only if hvx64 or hvx128

--- test/correctness/mask_to_bool_scalar.cpp
+++ test/correctness/mask_to_bool_scalar.cpp
@@ -8,13 +8,17 @@ int main() {
     ImageParam i(Int(32), 2, "i");
 
     f(x, y) = select(x < 10 || x > 20 || y < 10 || y > 20, 0, i(x, y));
-
-    //f.vectorize(x, 128);
-    f.hexagon().vectorize(x, 128);
-
     Target t(Target::Android, Target::ARM, 64);
     t.set_feature(Target::HVX_128);
+
+    if (t.features_any_of({Target::HVX_64, Target::HVX_128})) {
+        f.hexagon().vectorize(x, 128);
+    } else {
+        f.vectorize(x, 128);
+    }
+
     f.compile_to_file("bool_to_mask_issue", f.infer_arguments(), "bool_to_mask_issue", t);
 
+    std::cout << "Success!\n";
     return 0;
 }
