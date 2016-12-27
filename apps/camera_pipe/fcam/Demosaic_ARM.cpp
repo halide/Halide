a6256306d201937eda548964d6c9e4c7f05e2c80
Andrew Adams <andrew.b.adams@gmail.com>
2016-12-27 14:02:46 -0800

    Fix camera app

--- apps/camera_pipe/fcam/Demosaic_ARM.cpp
+++ apps/camera_pipe/fcam/Demosaic_ARM.cpp
@@ -15,7 +15,7 @@ inline short max(short a, short b) {return a>b ? a : b;}
 inline short max(short a, short b, short c, short d) {return max(max(a, b), max(c, d));}
 inline short min(short a, short b) {return a<b ? a : b;}
 
-void demosaic_ARM(Halide::Buffer<uint16_t> input, Halide::Buffer<uint8_t> out, float colorTemp, float contrast, bool denoise, int blackLevel, int whiteLevel, float gamma) {
+void demosaic_ARM(Halide::Runtime::Buffer<uint16_t> input, Halide::Runtime::Buffer<uint8_t> out, float colorTemp, float contrast, bool denoise, int blackLevel, int whiteLevel, float gamma) {
 
 #ifdef __arm__ // only build on arm
     const int BLOCK_WIDTH  = 40;
