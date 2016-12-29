ea612fb420fa2f449d7255cac711b6bc44c9b797
Steven Johnson <srj@google.com>
2016-12-28 17:26:31 -0800

    Add JITSharedRuntime::set_jit_externs()
    At present there is no good, reliable way to override arbitrary runtime
    functions when using the JIT. JITSharedRuntime ::set_default_handlers()
    allows overriding of selected runtime methods, but extending for
    multiple entrypoints (e.g. all of a GPU backend, to allow running on a
    simulator) is tedious. This PR adds the ability to selectively override
    arbitrary functions in JIT compilation mode, including runtime
    functions.
    
    (It is likely that this change could be used to reimplement or simply
    replace set_default_handlers(); I invite comments on the wisdom of
    that.)
    
    (Note that this is built atop
    https://github.com/halide/Halide/pull/1723, and cannot land before it
    does)

---
+++ test/correctness/shared_runtime_set_jit_externs.cpp
@@ -0,0 +1,110 @@
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+const int kSize = 10;
+
+namespace {
+
+enum {
+    expect_none,
+    expect_set_custom_print,
+    expect_set_default_handlers_print,
+    expect_set_jit_externs_print,
+} expected_print_func = expect_none;
+
+std::vector<std::string> messages;
+
+void my_print(void *user_context, const char *message) {
+    printf("%d: %s", expected_print_func, message);
+    messages.push_back(message);
+}
+
+}  // namespace
+
+extern "C" DLLEXPORT void set_custom_print(void *user_context, const char *message) {
+    assert(expected_print_func == expect_set_custom_print);
+    my_print(user_context, message);
+}
+
+extern "C" DLLEXPORT void set_default_handlers_print(void *user_context, const char *message) {
+    assert(expected_print_func == expect_set_default_handlers_print);
+    my_print(user_context, message);
+}
+
+extern "C" DLLEXPORT void set_jit_externs_print(void *user_context, const char *message) {
+    assert(expected_print_func == expect_set_jit_externs_print);
+    my_print(user_context, message);
+}
+
+void check_results(const Buffer<int32_t> &result) {
+    for (int32_t i = 0; i < kSize; i++) {
+        if (result(i) != i * i) {
+            fprintf(stderr, "Wrong answer\n");
+            exit(-1);
+        }
+    }
+    assert(messages.size() == kSize);
+}
+
+int main(int argc, char **argv) {
+    Var x, y;
+    {
+        messages.clear();
+        expected_print_func = expect_none;
+
+        Func f;
+        f(x) = print(x * x, "the answer is", 42.0f, "unsigned", cast<uint32_t>(145));
+        Buffer<int32_t> result = f.realize(kSize);
+        assert(messages.empty());
+    }
+    {
+        messages.clear();
+        expected_print_func = expect_set_default_handlers_print;
+
+        Internal::JITHandlers handlers;
+        handlers.custom_print = set_default_handlers_print;
+        Internal::JITSharedRuntime::set_default_handlers(handlers);
+
+        Func f;
+        f(x) = print(x * x, "the answer is", 42.0f, "unsigned", cast<uint32_t>(145));
+        Buffer<int32_t> result = f.realize(kSize);
+        check_results(result);
+    }
+    {
+        messages.clear();
+        expected_print_func = expect_set_jit_externs_print;
+
+        Internal::JITSharedRuntime::set_jit_externs({
+            { "halide_print", set_jit_externs_print },
+        });
+        Internal::JITSharedRuntime::release_all();
+
+        Func f;
+        f(x) = print(x * x, "the answer is", 42.0f, "unsigned", cast<uint32_t>(145));
+        Buffer<int32_t> result = f.realize(kSize);
+        check_results(result);
+    }
+    {
+        Internal::JITSharedRuntime::set_jit_externs({});
+        Internal::JITSharedRuntime::release_all();
+        messages.clear();
+        expected_print_func = expect_set_custom_print;
+
+        Func f;
+        f(x) = print(x * x, "the answer is", 42.0f, "unsigned", cast<uint32_t>(145));
+        f.set_custom_print(set_custom_print);
+        Buffer<int32_t> result = f.realize(kSize);
+        check_results(result);
+    }
+
+    printf("Success!\n");
+    return 0;
+}
