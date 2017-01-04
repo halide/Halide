5b823a7ea816afff533c8a752ea83731fe8cc8d4
Steven Johnson <srj@google.com>
2017-01-04 13:43:07 -0800

    rework load_library test to use OpenCL, not Hexagon

--- test/correctness/load_library.cpp
+++ test/correctness/load_library.cpp
@@ -3,48 +3,45 @@
 
 using namespace Halide;
 
-// This test exercises the ability to override halide_load_library (etc)
+// This test exercises the ability to override halide_get_library_symbol (etc)
 // when using JIT code; to do so, it compiles & calls a simple pipeline
-// using a Hexagon schedule, since that is known to load a helper library
-// in a well-defined way and is unlikely to change a great deal in the
-// near future; if this test breaks because of changes to the Hexagon
-// runtime (and the way it uses these calls), we may need to revise
-// this test to use a custom library of some sort.
+// using an OpenCL schedule, since that is known to use these calls
+// in a (reasonably) well-defined way and is unlikely to change a great deal 
+// in the near future; additionally, it doesn't require a particular
+// feature in LLVM (unlike, say, Hexagon).
 
 namespace {
 
-uint32_t libhalide_hexagon_host_magic_cookie = 0xdeadbeef;
-
-int load_library_calls = 0;
-int get_library_symbol_calls = 0;
-int error_calls = 0;
-
 void my_error_handler(void* u, const char *msg) {
-    error_calls++;
-    printf("Saw error: %s\n", msg);
+    if (!strstr(msg, "OpenCL API not found")) {
+        fprintf(stderr, "Saw unexpected error: %s\n", msg);
+        exit(-1);
+    }
+    printf("Saw expected error: %s\n", msg);
+    printf("Success!\n");
+    exit(0);
 }
 
 void *my_get_symbol_impl(const char *name) {
-    fprintf(stderr, "Saw expected get_symbol: %s\n", name);
+    fprintf(stderr, "Saw unexpected call: get_symbol(%s)\n", name);
     exit(-1);
 }
 
 void *my_load_library_impl(const char *name) {
-    load_library_calls++;
-    if (strcmp(name, "libhalide_hexagon_host.so") != 0) {
+    if (!strstr(name, "OpenCL") && !strstr(name, "opencl")) {
         fprintf(stderr, "Saw unexpected call: load_library(%s)\n", name);
         exit(-1);
     }
-    // return a well-known non-null pointer.
-    return &libhalide_hexagon_host_magic_cookie;
+    printf("Saw load_library: %s\n", name);
+    return nullptr;
 }
 
 void *my_get_library_symbol_impl(void *lib, const char *name) {
-    get_library_symbol_calls++;
-    if (lib !=  &libhalide_hexagon_host_magic_cookie) {
+    if (lib != nullptr || strcmp(name, "clGetPlatformIDs") != 0) {
         fprintf(stderr, "Saw unexpected call: get_library_symbol(%p, %s)\n", lib, name);
         exit(-1);
     }
+    printf("Saw get_library_symbol: %s\n", name);
     return nullptr;
 }
 
@@ -65,22 +62,15 @@ int main(int argc, char **argv) {
     handlers.custom_get_library_symbol = my_get_library_symbol_impl;
     Internal::JITSharedRuntime::set_default_handlers(handlers);
 
-    Var x("x");
-    Func f("f");
-
-    f(x) = cast<int32_t>(x);
-
-    Target target = get_jit_target_from_environment().with_feature(Target::HVX_64);
-    f.hexagon().vectorize(x, 32);
-
+    Var x, y;
+    Func f;
+    f(x, y) = cast<int32_t>(x + y);
+    Target target = get_jit_target_from_environment().with_feature(Target::OpenCL);
+    f.gpu_tile(x, y, 8, 8, TailStrategy::Auto, DeviceAPI::OpenCL);
     f.set_error_handler(my_error_handler);
 
-    Buffer<int32_t> out = f.realize(64, target);
+    Buffer<int32_t> out = f.realize(64, 64, target);
 
-    assert(load_library_calls == 1);
-    assert(get_library_symbol_calls == 1);
-    assert(error_calls == 1);
-
-    printf("Success!\n");
-    return 0;
+    fprintf(stderr, "Should not get here.\n");
+    return -1;
 }
