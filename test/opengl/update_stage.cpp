c22ead343493a4f2f8195a6cd515c7dbd7462082
Shoaib Kamil <kamil@adobe.com>
2017-01-18 16:25:53 -0500

    Add testcase where both pure and update steps are executed in GLSL.

--- test/opengl/update_stage.cpp
+++ test/opengl/update_stage.cpp
@@ -25,20 +25,27 @@ int main() {
     // Define the algorithm.
     Var x, y, c;
     RDom r(3, 5, "r");
+    RDom r2(2, 4, "r");
     Func f, g;
 
     Expr coordx = clamp(x + r, 0, input.width() - 1);
     f(x, y, c) = cast<uint8_t>(0);
     f(r.x, y, c) = cast<uint8_t>(11);
 
+    g(x, y, c) = cast<uint8_t>(1);
+    g(r2.x, y, c) = cast<uint8_t>(9);
 
-    // Schedule f and g to compute in separate passes on the GPU.
+
+    // Schedule f to compute pure stage on CPU and update on GPU
     f.bound(c, 0, 2);
     f.update(0).glsl(r.x, y, c);
 
+    // Schedule g to compute pure and update stages on GPU
+    g.bound(c, 0, 2);
+    g.glsl(x, y, c).update(0).glsl(r2.x, y, c);
+
     // Generate the result.
     Buffer<uint8_t> result = f.realize(width, height, res_channels, target);
-
 
     result.copy_to_host();
 
@@ -55,6 +62,26 @@ int main() {
             }
         }
     }
+
+    Buffer<uint8_t> result2 = g.realize(width, height, res_channels, target);
+
+    result2.copy_to_host();
+
+    //Check the result.
+    for (int c = 0; c < result2.channels(); c++) {
+        for (int y = 0; y < result2.height(); y++) {
+            for (int x = 0; x < result2.width(); x++) {
+                uint8_t correct = (x>=2 && x<6) ? 9 : 1;
+                if (result2(x, y, c) != correct) {
+                    fprintf(stderr, "result2(%d, %d, %d) = %d, should be %d\n",
+                            x, y, c, result2(x, y, c), correct);
+                    return 1;
+                }
+            }
+        }
+    }
+
+
 
     printf("Success!\n");
     return 0;
