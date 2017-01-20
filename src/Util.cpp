1880445670bfac51caf3a412cb12d6555521652a
Steven Johnson <srj@google.com>
2017-01-19 16:44:24 -0800

    calls to getenv_s() should check the result code
    Checking the out-param “read” is probably fine, but better form to
    (also) check the result code (which is nonzero if the var doesn’t exist
    or an error occurs).

--- src/Util.cpp
+++ src/Util.cpp
@@ -46,7 +46,7 @@ std::string get_env_variable(char const *env_var_name, size_t &read) {
 
     #ifdef _MSC_VER
     char lvl[32];
-    getenv_s(&read, lvl, env_var_name);
+    if (getenv_s(&read, lvl, env_var_name) != 0) read = 0;
     #else
     char *lvl = getenv(env_var_name);
     read = (lvl)?1:0;
