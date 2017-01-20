51d8f7550b013807360d09f59408a63bccf81bdb
Steven Johnson <srj@google.com>
2017-01-20 14:35:03 -0800

    Hygiene: move Debug.h impl details to Debug.cpp
    Former-commit-id: 434ae8b6cf0ebe47c651034a1bd16ca645f6c826

--- src/CodeGen_LLVM.cpp
+++ src/CodeGen_LLVM.cpp
@@ -931,7 +931,7 @@ llvm::Type *CodeGen_LLVM::llvm_type_of(Type t) {
 void CodeGen_LLVM::optimize_module() {
     debug(3) << "Optimizing module\n";
 
-    if (debug::debug_level >= 3) {
+    if (debug::debug_level() >= 3) {
         module->dump();
     }
 
@@ -986,7 +986,7 @@ void CodeGen_LLVM::optimize_module() {
     module_pass_manager.run(*module);
 
     debug(3) << "After LLVM optimizations:\n";
-    if (debug::debug_level >= 2) {
+    if (debug::debug_level() >= 2) {
         module->dump();
     }
 }
@@ -1009,7 +1009,7 @@ llvm::Value *CodeGen_LLVM::sym_get(const string &name, bool must_succeed) const 
             std::ostringstream err;
             err << "Symbol not found: " << name << "\n";
 
-            if (debug::debug_level > 0) {
+            if (debug::debug_level() > 0) {
                 err << "The following names are in scope:\n"
                     << symbol_table << "\n";
             }
