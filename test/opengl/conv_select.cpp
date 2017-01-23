c40aed39b5836057b1f7278507b5d349574afd8d
ronen barzel <ronen@barzel.org>
2017-01-23 13:53:55 +0000

    DRY: create & use Testing::fill(), Testing::check_result() Former-commit-id: 0875d6bd835890b3b2977576da1b823bf284dd6f

--- test/opengl/conv_select.cpp
+++ test/opengl/conv_select.cpp
@@ -4,6 +4,8 @@
 #include "Halide.h"
 #include <stdio.h>
 
+#include "./testing.h"
+
 using namespace Halide;
 
 int main() {
@@ -13,13 +15,9 @@ int main() {
     // Define the input
     const int width = 10, height = 10, channels = 4, res_channels = 2;
     Buffer<float> input(width, height, channels);
-    for (int c = 0; c < input.channels(); c++) {
-        for (int y = 0; y < input.height(); y++) {
-            for (int x = 0; x < input.width(); x++) {
-                input(x, y, c) = float(x + y);
-            }
-        }
-    }
+    Testing::fill<float>(input, [](int x, int y, int c) {
+            return float(x + y);
+        });
 
     // Define the algorithm.
     Var x, y, c;
@@ -41,20 +39,11 @@ int main() {
     result.copy_to_host();
 
     //Check the result.
-    for (int c = 0; c < result.channels(); c++) {
-        for (int y = 0; y < result.height(); y++) {
-            for (int x = 0; x < result.width(); x++) {
-                float temp = ((x + y)>4)?1.0f:0.0f;
-
-                float correct = (c==0)? temp : (1.0f - temp);
-                if (result(x, y, c) != correct) {
-                    fprintf(stderr, "result(%d, %d, %d) = %f instead of %f\n",
-                            x, y, c, result(x, y, c), correct);
-                    return 1;
-                }
-            }
-        }
-    }
+    if (!(Testing::check_result<float>(result, [](int x, int y, int c) {
+                const float temp = ((x + y)>4)?1.0f:0.0f;
+                return (c==0)? temp : (1.0f - temp);
+            })))
+        return 1;
 
     printf("Success!\n");
     return 0;
--- test/opengl/sum_reduction.cpp
+++ test/opengl/sum_reduction.cpp
@@ -1,6 +1,8 @@
 #include "Halide.h"
 #include <stdio.h>
 
+#include "./testing.h"
+
 using namespace Halide;
 
 int main() {
@@ -10,13 +12,9 @@ int main() {
     // Define the input
     const int width = 10, height = 10, channels = 4;
     Buffer<float> input(width, height, channels);
-    for (int c = 0; c < input.channels(); c++) {
-        for (int y = 0; y < input.height(); y++) {
-            for (int x = 0; x < input.width(); x++) {
-                input(x, y, c) = float(x + y);
-            }
-        }
-    }
+    Testing::fill<float>(input, [](int x, int y, int c) {
+            return float(x + y);
+        });
 
     // Define the algorithm.
     Var x, y, c;
@@ -33,22 +31,14 @@ int main() {
     result.copy_to_host();
 
     // Check the result.
-    for (int c = 0; c < result.channels(); c++) {
-        for (int y = 0; y < result.height(); y++) {
-            for (int x = 0; x < result.width(); x++) {
-                float temp = 0.0f;
-                for (int r = 0; r < 5; r++){
-                    temp += input(std::min(x+r, input.width()-1), y, c);
-                }
-                float correct = temp / 10.0f * 255.0f;
-                if (fabs(result(x, y, c) - correct) > 1e-3) {
-                    fprintf(stderr, "result(%d, %d, %d) = %f instead of %f\n",
-                            x, y, c, result(x, y, c), correct);
-                    return 1;
-                }
-            }
-        }
-    }
+    if (!Testing::check_result<float>(result, [&](int x, int y, int c) {
+	    float temp = 0.0f;
+	    for (int r = 0; r < 5; r++){
+		temp += input(std::min(x+r, input.width()-1), y, c);
+	    }
+	    return temp / 10.0f * 255.0f;
+	    }, 1e-3))
+	return 1;
 
     printf("Success!\n");
     return 0;
