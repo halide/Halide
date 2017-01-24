3c7ed83d4ef18f21945e50f19d3e86dd465b6392
Steven Johnson <srj@google.com>
2017-01-24 15:16:14 -0800

    IO<Buffer> should have allowed for dim-only and type-only
    Specifying an Input<Buffer> or Output<Buffer> with one of type or dim
    (but not both) should have worked, but didn’t. Now it does.

--- src/Generator.h
+++ src/Generator.h
@@ -1273,8 +1273,7 @@ public:
     }
 
     GeneratorInput_Buffer(const std::string &name, int d)
-        : Super(name, IOKind::Buffer, std::vector<Type>{ T::static_halide_type() }, d) {
-        static_assert(T::has_static_halide_type(), "Must pass a Type argument for a Buffer with a static type of void");
+        : Super(name, IOKind::Buffer, T::has_static_halide_type() ? std::vector<Type>{ T::static_halide_type() } : std::vector<Type>{}, d) {
     }
 
 
@@ -1688,7 +1687,7 @@ protected:
         if (T::has_static_halide_type()) {
             user_assert(t.empty()) << "Cannot use pass a Type argument for a Buffer with a non-void static type\n";
         } else {
-            user_assert(t.size() == 1) << "Output<Buffer<>> requires exactly one Type\n";
+            user_assert(t.size() <= 1) << "Output<Buffer<>>(" << name << ") requires at most one Type, but has " << t.size() << "\n";
         }
     }
 
