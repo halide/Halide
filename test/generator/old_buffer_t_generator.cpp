c43bd33c88643e9332610d8523b22654c332e5c6
Andrew Adams <andrew.b.adams@gmail.com>
2017-01-25 17:13:28 -0800

    More merge fixes
    Former-commit-id: 78a003a05a9775d5f4c410f6db07188a8d48f6b6

--- test/generator/old_buffer_t_generator.cpp
+++ test/generator/old_buffer_t_generator.cpp
@@ -15,7 +15,7 @@ public:
         f(x, y) = in1(x-1, y-1) + in1(x+1, y+3) + in2(x, y) + scalar_param;
         f.compute_root();
         g.define_extern("extern_stage", {in2, f}, Int(32), 2,
-                        NameMangling::CPlusPlus,
+                        NameMangling::Default,
                         true /* uses old buffer_t */);
         return g;
     }
