1ff98a58c62a4ec83c41d8b27e0e522aba221510
Steven Johnson <srj@google.com>
2017-01-26 12:48:53 -0800

    thread_safety test should use std::thread instead of OMP
    OMP isn’t available on all interesting targets, so this test serializes
    (and effectively hangs) on those. Explicitly spawn threads instead.
    
    
    Former-commit-id: 7c9c363723b2a05ab973a2e3da1e0003467feaa9

--- test/correctness/thread_safety.cpp
+++ test/correctness/thread_safety.cpp
@@ -1,19 +1,24 @@
 #include "Halide.h"
 #include <stdio.h>
+#include <thread>
 
 using namespace Halide;
 
 int main(int argc, char **argv) {
     // Test if the compiler itself is thread-safe. This test is
-    // intended to be run in a thread-sanitizer with openmp turned on.
-    #ifdef _OPENMP
-    #pragma omp parallel for
-    #endif
+    // intended to be run in a thread-sanitizer.
+    std::vector<std::thread> threads;
     for (int i = 0; i < 1000; i++) {
-        Func f;
-        Var x;
-        f(x) = x;
-        f.realize(100);
+        threads.emplace_back([]{
+            Func f;
+            Var x;
+            f(x) = x;
+            f.realize(100);
+        });
+    }
+
+    for (auto &t : threads) {
+        t.join();
     }
 
     printf("Success!\n");
