510dd43dc2e77f0fdeef154e0b94e36d7e55b52e
Steven Johnson <srj@google.com>
2017-01-27 11:29:55 -0800

    Use a pool of 8 threads.

--- test/correctness/thread_safety.cpp
+++ test/correctness/thread_safety.cpp
@@ -1,26 +1,34 @@
 #include "Halide.h"
 #include <stdio.h>
-#include <future>
+#include <thread>
 
 using namespace Halide;
-
-static std::atomic<int> foo;
 
 int main(int argc, char **argv) {
     // Test if the compiler itself is thread-safe. This test is
     // intended to be run in a thread-sanitizer.
-    std::vector<std::future<void>> futures;
-    for (int i = 0; i < 1000; i++) {
-        futures.emplace_back(std::async(std::launch::async, []{
-            Func f;
-            Var x;
-            f(x) = x;
-            f.realize(100);
-        }));
+
+    // std::thread has implementation-dependent behavior; some implementations
+    // may refuse to create an arbitrary number. So let's create a smallish
+    // number (8) and have each one do enough work that contention is likely
+    // to be encountered.
+    constexpr int total_iters = 1024;
+    constexpr int num_threads = 8;
+
+    std::vector<std::thread> threads;
+    for (int i = 0; i < num_threads; i++) {
+        threads.emplace_back([]{
+            for (int i = 0; i < (total_iters / num_threads); i++) {
+                Func f;
+                Var x;
+                f(x) = x;
+                f.realize(100);
+            }
+        });
     }
 
-    for (auto &f : futures) {
-        f.wait();
+    for (auto &t : threads) {
+        t.join();
     }
 
     printf("Success!\n");
