db2724380de505fef89ef213488513f214f83e34
Pranav Bhandarkar <pranavb@codeaurora.org>
2017-02-24 18:03:17 -0600

    Add an app that runs small filters on hexagon.
    Former-commit-id: 25722437006beccdef876093ed5d334d75358dfa

---
+++ apps/small_filters/conv3x3a16.cpp
@@ -0,0 +1,59 @@
+#include "Halide.h"
+
+using namespace Halide;
+using namespace Halide::Internal;
+IRPrinter irp(std::cerr);
+
+class Conv3x3a16 : public Generator<Conv3x3a16> {
+public:
+    // Takes an 8 bit image; one channel.
+    Input<Buffer<uint8_t>> input{"input", 2};
+    Input<Buffer<int8_t>> mask{"mask", 2};
+
+    // Outputs an 8 bit image; one channel.
+    Output<Buffer<uint8_t>> output{"output", 2};
+    Var x{"x"}, y{"y"};
+    void generate() {
+        Func bounded_input{"input_bounded"};
+
+        bounded_input(x, y) = BoundaryConditions::repeat_edge(input)(x, y);
+
+        Expr sum = cast<int16_t>(0);
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                sum += cast<int16_t>(bounded_input(x+j, y+i)) * cast<int16_t>(mask(j+1, i+1));
+            }
+        }
+        output(x, y) = cast<uint8_t>(clamp(sum >> 4, 0, 255));
+        bounded_input.compute_root();
+    }
+
+    void schedule() {
+        Var xi{"xi"}, yi{"yi"};
+
+        auto set_min_0 = [this](int dim) {
+            input.dim(dim).set_min(0);
+            auto output_buff = Func(output).output_buffer();
+            output_buff.dim(dim).set_min(0);
+        };
+        set_min_0(0);
+        set_min_0(1);
+
+        auto set_aligned_stride = [this](int dim, int alignment) {
+            Expr input_stride = input.dim(dim).stride();
+            input.dim(dim).set_stride((input_stride/alignment) * alignment);
+
+            auto output_buff = Func(output).output_buffer();
+            Expr output_stride = output_buff.dim(dim).stride();
+            output_buff.dim(dim).set_stride((output_stride/alignment) * alignment);
+            
+        };
+        set_aligned_stride(1, 128);
+
+        int vector_size = get_target().has_feature(Target::HVX_128) ? 128 : 64;
+        Func(output).hexagon().tile(x, y, xi, yi, vector_size, 4, TailStrategy::RoundUp).vectorize(xi).unroll(yi);
+    }
+
+};
+
+HALIDE_REGISTER_GENERATOR(Conv3x3a16, "conv3x3a16");
---
+++ apps/small_filters/process.cpp
@@ -0,0 +1,175 @@
+#include <stdio.h>
+
+#include "../support/benchmark.h"
+#include "conv3x3a16_hvx128.h"
+#include "conv3x3a16_hvx64.h"
+#include "HalideRuntimeHexagonHost.h"
+#include "HalideBuffer.h"
+#include "process.h"
+
+class Conv3x3a16Descriptor : public PipelineDescriptor<pipeline3, Conv3x3a16Descriptor> {
+    typedef Halide::Runtime::Buffer<uint8_t> U8Buffer;
+    typedef Halide::Runtime::Buffer<int8_t> I8Buffer;
+    
+    Halide::Runtime::Buffer<uint8_t> u8_in, u8_out;
+    Halide::Runtime::Buffer<int8_t> i8_mask;
+
+public:
+    Conv3x3a16Descriptor(pipeline3 pipeline64, pipeline3 pipeline128, int W, int H) :
+        PipelineDescriptor<pipeline3, Conv3x3a16Descriptor>(pipeline64, pipeline128),
+        u8_in(nullptr, W, H, 2),
+        u8_out(nullptr, W, H, 2),
+        i8_mask(nullptr, 3, 3, 2) {}
+
+    void init() {
+        u8_in.device_malloc(halide_hexagon_device_interface());
+        u8_out.device_malloc(halide_hexagon_device_interface());
+        i8_mask.device_malloc(halide_hexagon_device_interface());
+
+        u8_in.for_each_value([&](uint8_t &x) {
+            x = static_cast<uint8_t>(rand());
+        });
+        u8_out.for_each_value([&](uint8_t &x) {
+            x = 0;
+        });
+
+        i8_mask(0, 0) = 1;
+        i8_mask(1, 0) = -4;
+        i8_mask(2, 0) = 7;
+
+        i8_mask(0, 0) = 2;
+        i8_mask(1, 0) = -5;
+        i8_mask(2, 0) = 8;
+
+        i8_mask(0, 0) = 3;
+        i8_mask(1, 0) = -6;
+        i8_mask(2, 0) = 7;
+    }
+
+    bool verify(const int W, const int H) {
+        u8_out.for_each_element([&](int x, int y) {
+            int16_t sum = 0;
+            for (int ry = -1; ry <= 1; ry++) {
+                for (int rx = -1; rx <= 1; rx++) {
+                    int clamped_x = (x + rx < 0) ? 0 : x + rx;
+                    clamped_x = (clamped_x >= W) ? (W-1) : clamped_x;
+
+                    int clamped_y = (y + ry < 0) ? 0 : y + ry;
+                    clamped_y = (clamped_y >= H) ? (H-1) : clamped_y;
+
+                    sum += static_cast<int16_t>(u8_in(clamped_x, clamped_y)) * static_cast<int16_t>(i8_mask(rx+1, ry+1));
+                }
+            }
+            sum = sum >> 4;
+            if (sum > 255) {
+                sum = 255;
+            } else if (sum < 0) {
+                sum = 0;
+            }
+            uint8_t out_xy = u8_out(x, y);
+            if (sum != out_xy) {
+                printf("Conv3x3a16: Mismatch at %d %d : %d != %d\n", x, y, out_xy, sum);
+                abort();
+            }
+        });
+        return true;
+    }
+    void identify_pipeline() { printf ("Running conv3x3a16...\n"); }
+    int run(hvx_mode_t mode) {
+        if (mode == hvx_mode_t::hvx64) {
+            return pipeline64(u8_in, i8_mask, u8_out);
+        } else if (mode == hvx_mode_t::hvx128) {
+            return pipeline128(u8_in, i8_mask, u8_out);
+        }
+        abort();
+    }
+};
+
+void usage(char *prg_name) {
+    const char usage_string[] = " Run a bunch of small filters\n\n"
+                                "\t -m -> hvx_mode - options are hvx64, hvx128. Default is to run both 64 and 128 byte modes>\n"
+                                "\t -n -> number of iterations\n"
+                                "\t -h -> print this help message\n";
+    printf ("%s - %s", prg_name, usage_string);
+
+}
+
+const char *to_string(hvx_mode_t mode) {
+    if (mode == hvx_mode_t::hvx64) {
+        return "(64 byte mode)";
+    } else if (mode == hvx_mode_t::hvx128) {
+        return "(128 byte mode)";
+    } else {
+        return "(cpu)";
+    }
+}
+
+int main(int argc, char **argv) {
+    // Set some defaults first.
+    const int W = 1024;
+    const int H = 1024;
+    std::vector<hvx_mode_t> modes = {hvx_mode_t::hvx64, hvx_mode_t::hvx128};
+    int iterations = 10;
+
+    // Process command line args.
+    for (int i = 1; i < argc; ++i) {
+        if (argv[i][0] == '-') {
+            switch (argv[i][1]) {
+            case 'm':
+                {
+                    std::string mode_to_run = argv[i+1];
+                    modes.clear();
+                    if (mode_to_run == "hvx64") {
+                        modes.push_back(hvx_mode_t::hvx64);
+                    } else if (mode_to_run == "hvx128") {
+                        modes.push_back(hvx_mode_t::hvx128);
+                    } else {
+                        usage(argv[0]);
+                        abort();
+                    }
+                    i++;
+                }
+                break;
+            case 'h':
+                usage(argv[0]);
+                return 0;
+                break;
+            case 'n':
+                iterations = atoi(argv[i+1]);
+                i++;
+                break;
+            }
+        }
+    }
+
+    Conv3x3a16Descriptor conv3x3a16_pipeline(conv3x3a16_hvx64, conv3x3a16_hvx128, W, H);
+    std::vector<PipelineDescriptorBase *> pipelines = {&conv3x3a16_pipeline};
+
+    for (hvx_mode_t m : modes) {
+        for (PipelineDescriptorBase *p : pipelines) {
+            p->init();
+            p->identify_pipeline();
+
+            halide_hexagon_set_performance_mode(NULL, halide_hvx_power_turbo);
+            halide_hexagon_power_hvx_on(NULL);
+
+            double time = benchmark(iterations, 10, [&]() {
+                    int result = p->run(m);
+                    if (result != 0) {
+                        printf("pipeline failed! %d\n", result);
+                    }
+                });
+            printf("Done, time: %g s %s\n", time, to_string(m));
+
+            // We're done with HVX, power it off.
+            halide_hexagon_power_hvx_off(NULL);
+
+            if (!p->verify(W, H)) {
+                abort();
+            }
+        }
+    }
+
+    printf("Success!\n");
+    return 0;
+}
