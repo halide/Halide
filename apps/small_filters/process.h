008a7f34e846181db3f113923d97166b42d4eb6f
Pranav Bhandarkar <pranavb@codeaurora.org>
2017-02-25 21:17:14 -0600

    Add a cpu schedule for conv3x3a16 and make the driver recognize a third mode of execution.
    Former-commit-id: bd113f154c0e6a2a11b2c529e9a43ff49849411c

--- apps/small_filters/process.h
+++ apps/small_filters/process.h
@@ -1,9 +1,10 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-enum hvx_mode_t {
+enum bmark_run_mode_t {
     hvx64 = 1,
     hvx128 = 2,
+    cpu = 3
 };
 
 
@@ -13,16 +14,16 @@ typedef int (*pipeline3)(buffer_t *, buffer_t *, buffer_t *);
 struct PipelineDescriptorBase {
     virtual void identify_pipeline() = 0;
     virtual void init() = 0;
-    virtual int run(hvx_mode_t mode) = 0;
+    virtual int run(bmark_run_mode_t mode) = 0;
     virtual bool verify(int W, int H) = 0;
 };
 
 template <typename T1, typename T2>
 struct PipelineDescriptor : public PipelineDescriptorBase  {
-    T1 pipeline64;
-    T1 pipeline128;
+    T1 pipeline_64, pipeline_128, pipeline_cpu;
  
-    PipelineDescriptor(T1 pipeline64, T1 pipeline128) : pipeline64(pipeline64), pipeline128(pipeline128) {}
+    PipelineDescriptor(T1 pipeline_64, T1 pipeline_128, T1 pipeline_cpu) :
+    pipeline_64(pipeline_64), pipeline_128(pipeline_128), pipeline_cpu(pipeline_cpu) {}
  
     void identify_pipeline() {
         static_cast<T2*>(this)->identify_pipeline();
@@ -30,14 +31,12 @@ struct PipelineDescriptor : public PipelineDescriptorBase  {
     void init() {
         static_cast<T2*>(this)->init();
     }
-    int run(hvx_mode_t mode) {
+    int run(bmark_run_mode_t mode) {
         return static_cast<T2*>(this)->run(mode);
     }
     bool verify(int W, int H) {
         return static_cast<T2*>(this)->verify(W, H);
     }
-
-  
 };
 
 
