48964fa41971e5fd08c26bf55b1fd4b0d3795db3
Pranav Bhandarkar <pranavb@codeaurora.org>
2017-03-05 19:33:51 -0600

    Follow convention when declaring class members of a generator
    Former-commit-id: e7dc5cb37a00792418c0cfeba162562640888aeb

--- apps/hexagon_benchmarks/conv3x3.cpp
+++ apps/hexagon_benchmarks/conv3x3.cpp
@@ -4,14 +4,12 @@ using namespace Halide;
 
 class Conv3x3 : public Generator<Conv3x3> {
 public:
+    GeneratorParam<Type> accumulator_type{"accumulator_type", Int(16)};
     // Takes an 8 bit image; one channel.
     Input<Buffer<uint8_t>> input{"input", 2};
     Input<Buffer<int8_t>> mask{"mask", 2};
     // Outputs an 8 bit image; one channel.
     Output<Buffer<uint8_t>> output{"output", 2};
-    Var x{"x"}, y{"y"};
-    Func bounded_input{"input_bounded"};
-    GeneratorParam<Type> accumulator_type{"accumulator_type", Int(16)};
 
     void generate() {
         bounded_input(x, y) = BoundaryConditions::repeat_edge(input)(x, y);
@@ -55,6 +53,9 @@ public:
                 .parallel(y, 16);
         }
     }
+private:
+    Var x{"x"}, y{"y"};
+    Func bounded_input{"input_bounded"};
 };
 
 HALIDE_REGISTER_GENERATOR(Conv3x3, "conv3x3");
