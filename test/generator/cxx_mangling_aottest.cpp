0edd53017c830c6c82947f83a19a8f78f49bbfca
Steven Johnson <srj@google.com>
2017-03-16 14:55:53 -0700

    remove WITH_PTX ifdefs
    These were added in a misguided effort
    (6605c1276cd3a7e3697318077904490304a5a3a8), but I think this code
    should be enabled in all cases

--- test/generator/cxx_mangling_aottest.cpp
+++ test/generator/cxx_mangling_aottest.cpp
@@ -6,9 +6,7 @@
 #include <string.h>
 
 #include "cxx_mangling.h"
-#if WITH_PTX
 #include "cxx_mangling_gpu.h"
-#endif
 
 using namespace Halide::Runtime;
 
@@ -58,7 +56,6 @@ int main(int argc, char **argv) {
     std::string *string_ptr = nullptr;
     const std::string *const_string_ptr = nullptr;
 
-#if WITH_PTX
     // Don't bother calling this (we haven't linked in the CUDA support it needs),
     // just force a reference to ensure it is linked in.
     int (*f)(halide_buffer_t *,
@@ -77,7 +74,6 @@ int main(int argc, char **argv) {
              halide_buffer_t *) = HalideTest::cxx_mangling_gpu;
 
     printf("HalideTest::cxx_mangling is at: %p\n", (void*) f);
-#endif
 
     my_namespace::my_class mc;
     my_namespace::my_subnamespace::my_struct ms;
