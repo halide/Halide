228cd863e687f2f1698e47928cb96f8190bac806
Steven Johnson <srj@google.com>
2017-03-21 17:51:56 -0700

    Convert LocalLaplacian to new-style Generator

--- apps/local_laplacian/local_laplacian_generator.cpp
+++ apps/local_laplacian/local_laplacian_generator.cpp
@@ -6,20 +6,18 @@ constexpr int maxJ = 20;
 
 class LocalLaplacian : public Halide::Generator<LocalLaplacian> {
 public:
+    GeneratorParam<int>         pyramid_levels{"pyramid_levels", 8, 1, maxJ};
 
-    GeneratorParam<int>   pyramid_levels{"pyramid_levels", 8, 1, maxJ};
+    Input<Buffer<uint16_t>>     input{"input", 3};
+    Input<int>                  levels{"levels"};
+    Input<float>                alpha{"alpha"};
+    Input<float>                beta{"beta"};
 
-    ImageParam            input{UInt(16), 3, "input"};
-    Param<int>            levels{"levels"};
-    Param<float>          alpha{"alpha"};
-    Param<float>          beta{"beta"};
+    Output<Buffer<uint16_t>>    output{"output", 3};
 
-    Func build() {
+    void generate() {
         /* THE ALGORITHM */
-        const int J = pyramid_levels;
-
         // Make the remapping function as a lookup table.
-        Func remap;
         Expr fx = cast<float>(x) / 256.0f;
         remap(x) = alpha*fx*exp(-fx*fx/2.0f);
 
@@ -31,37 +29,34 @@ public:
         floating(x, y, c) = clamped(x, y, c) / 65535.0f;
 
         // Get the luminance channel
-        Func gray;
         gray(x, y) = 0.299f * floating(x, y, 0) + 0.587f * floating(x, y, 1) + 0.114f * floating(x, y, 2);
 
         // Make the processed Gaussian pyramid.
-        Func gPyramid[maxJ];
         // Do a lookup into a lut with 256 entires per intensity level
         Expr level = k * (1.0f / (levels - 1));
         Expr idx = gray(x, y)*cast<float>(levels-1)*256.0f;
         idx = clamp(cast<int>(idx), 0, (levels-1)*256);
         gPyramid[0](x, y, k) = beta*(gray(x, y) - level) + level + remap(idx - 256*k);
-        for (int j = 1; j < J; j++) {
+        for (int j = 1; j < pyramid_levels; j++) {
             gPyramid[j](x, y, k) = downsample(gPyramid[j-1])(x, y, k);
         }
 
         // Get its laplacian pyramid
         Func lPyramid[maxJ];
-        lPyramid[J-1](x, y, k) = gPyramid[J-1](x, y, k);
-        for (int j = J-2; j >= 0; j--) {
+        lPyramid[pyramid_levels-1](x, y, k) = gPyramid[pyramid_levels-1](x, y, k);
+        for (int j = pyramid_levels-2; j >= 0; j--) {
             lPyramid[j](x, y, k) = gPyramid[j](x, y, k) - upsample(gPyramid[j+1])(x, y, k);
         }
 
         // Make the Gaussian pyramid of the input
-        Func inGPyramid[maxJ];
         inGPyramid[0](x, y) = gray(x, y);
-        for (int j = 1; j < J; j++) {
+        for (int j = 1; j < pyramid_levels; j++) {
             inGPyramid[j](x, y) = downsample(inGPyramid[j-1])(x, y);
         }
 
         // Make the laplacian pyramid of the output
         Func outLPyramid[maxJ];
-        for (int j = 0; j < J; j++) {
+        for (int j = 0; j < pyramid_levels; j++) {
             // Split input pyramid value into integer and floating parts
             Expr level = inGPyramid[j](x, y) * cast<float>(levels-1);
             Expr li = clamp(cast<int>(level), 0, levels-2);
@@ -71,9 +66,8 @@ public:
         }
 
         // Make the Gaussian pyramid of the output
-        Func outGPyramid[maxJ];
-        outGPyramid[J-1](x, y) = outLPyramid[J-1](x, y);
-        for (int j = J-2; j >= 0; j--) {
+        outGPyramid[pyramid_levels-1](x, y) = outLPyramid[pyramid_levels-1](x, y);
+        for (int j = pyramid_levels-2; j >= 0; j--) {
             outGPyramid[j](x, y) = upsample(outGPyramid[j+1])(x, y) + outLPyramid[j](x, y);
         }
 
@@ -82,20 +76,19 @@ public:
         float eps = 0.01f;
         color(x, y, c) = outGPyramid[0](x, y) * (floating(x, y, c)+eps) / (gray(x, y)+eps);
 
-        Func output("local_laplacian");
         // Convert back to 16-bit
         output(x, y, c) = cast<uint16_t>(clamp(color(x, y, c), 0.0f, 1.0f) * 65535.0f);
+    }
 
-
-
+    void schedule() {
         /* THE SCHEDULE */
         remap.compute_root();
 
         if (get_target().has_gpu_feature()) {
             // gpu schedule
             Var xi, yi;
-            output.compute_root().gpu_tile(x, y, xi, yi, 16, 8);
-            for (int j = 0; j < J; j++) {
+            Func(output).compute_root().gpu_tile(x, y, xi, yi, 16, 8);
+            for (int j = 0; j < pyramid_levels; j++) {
                 int blockw = 16, blockh = 8;
                 if (j > 3) {
                     blockw = 2;
@@ -110,7 +103,7 @@ public:
         } else {
             // cpu schedule
             Var yo;
-            output.reorder(c, x, y).split(y, yo, y, 64).parallel(yo).vectorize(x, 8);
+            Func(output).reorder(c, x, y).split(y, yo, y, 64).parallel(yo).vectorize(x, 8);
             gray.compute_root().parallel(y, 32).vectorize(x, 8);
             for (int j = 1; j < 5; j++) {
                 inGPyramid[j]
@@ -124,17 +117,16 @@ public:
             }
             outGPyramid[0]
                 .compute_at(output, y).vectorize(x, 8);
-            for (int j = 5; j < J; j++) {
+            for (int j = 5; j < pyramid_levels; j++) {
                 inGPyramid[j].compute_root();
                 gPyramid[j].compute_root().parallel(k);
                 outGPyramid[j].compute_root();
             }
         }
-
-        return output;
     }
 private:
     Var x, y, c, k;
+    Func remap, gray, inGPyramid[maxJ], gPyramid[maxJ], outGPyramid[maxJ];
 
     // Downsample with a 1 3 3 1 filter
     Func downsample(Func f) {
@@ -157,6 +149,6 @@ private:
 
 };
 
-Halide::RegisterGenerator<LocalLaplacian> register_me{"local_laplacian"};
+HALIDE_REGISTER_GENERATOR(LocalLaplacian, "local_laplacian")
 
 }  // namespace
