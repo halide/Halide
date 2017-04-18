734e427212d3c735f9e395a60da06e9ec8b70c3e
Steven Johnson <srj@google.com>
2017-04-18 10:05:54 -0700

    is_removed_before_lowering -> is_bound_before_lowering
    Former-commit-id: d95b653aef254033c47df24a9b6500f5eefb15d4

--- src/InferArguments.cpp
+++ src/InferArguments.cpp
@@ -85,7 +85,7 @@ private:
 
     void include_parameter(Parameter p) {
         if (!p.defined()) return;
-        if (p.is_removed_before_lowering()) return;
+        if (p.is_bound_before_lowering()) return;
         if (already_have(p.name())) return;
 
         Expr def, min, max;
