59f5e83239403591aa71ae01f2f6aecd7cc4ef5e
Z Stern <zalman@google.com>
2017-04-20 13:07:58 -0700

    Add a test case for clamp with bounds that don't make sense for the type being clamped. Code in Halide was fixed a while back, but adding test as I'm closing issue: https://github.com/halide/Halide/issues/1434 .

---
+++ test/error/clamp_out_of_range.cpp
@@ -0,0 +1,17 @@
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    Var x;
+    Func f;
+
+    f(x) = clamp(cast<int8_t>(x), 0, 255);
+    Buffer<> result = f.realize(42);
+
+    printf("Success!");
+
+    printf("I should not have reached here\n");
+    return 0;
+}
