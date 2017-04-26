8f6892fdbada04a7a838ba8ce714d5ea6a7ea2d5
Z Stern <zalman@zalman-macbookpro2.roam.corp.google.com>
2017-04-26 09:22:32 -0700

    Add cmake support, though it is not clear to me the OpenCL tests run under cmake due to #defines being missing.
    Add comment and clean up indentation in define_extern_opencl_generator.
    
    
    Former-commit-id: fe0d11058cf6ced8f426bd9e26042486dbd80848

--- test/generator/define_extern_opencl_generator.cpp
+++ test/generator/define_extern_opencl_generator.cpp
@@ -8,22 +8,24 @@ public:
     Output<Func> output{ "output", Int(32), 1 };
 
     Var x{"x"};
+    // make_a_root is necessary as there doesn't seem to be a way to
+    // get from Input<Buffer<int32_t>> to ExternFuncArgument otherwise.
     Func make_a_root{"make_a_root"};
-  Func gpu_input{"gpu_input"};
+    Func gpu_input{"gpu_input"};
 
     void generate() {
-      make_a_root(x) = input(x);
-      ExternFuncArgument arg = make_a_root;
-      gpu_input.define_extern("gpu_input", { arg }, Halide::type_of<int32_t>(), 1, NameMangling::Default, false, Halide::DeviceAPI::OpenCL);
+        make_a_root(x) = input(x);
+        ExternFuncArgument arg = make_a_root;
+        gpu_input.define_extern("gpu_input", { arg }, Halide::type_of<int32_t>(), 1, NameMangling::Default, false, Halide::DeviceAPI::OpenCL);
   
-      output(x) = gpu_input(x) - 41;
+        output(x) = gpu_input(x) - 41;
     }
 
     void schedule() {
-      make_a_root.compute_root();
-      gpu_input.compute_root();
-      Var block_x, thread_x;
-      output.gpu_tile(x, block_x, thread_x, Expr(16), TailStrategy::Auto, Halide::DeviceAPI::OpenCL);
+        make_a_root.compute_root();
+        gpu_input.compute_root();
+        Var block_x, thread_x;
+        output.gpu_tile(x, block_x, thread_x, Expr(16), TailStrategy::Auto, Halide::DeviceAPI::OpenCL);
     }
 };
 
