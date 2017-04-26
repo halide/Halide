bf36acb89159fbb90f96f0067168381e0b68670b
Andrew Adams <andrew.b.adams@gmail.com>
2017-04-26 14:06:36 -0700

    expose default implementations of replaceable runtime functions

--- src/runtime/windows_io.cpp
+++ src/runtime/windows_io.cpp
@@ -1,9 +1,9 @@
 #include "HalideRuntime.h"
 
-namespace Halide { namespace Runtime { namespace Internal {
+extern "C" {
 
-WEAK void halide_print_impl(void *user_context, const char *str) {
+WEAK void halide_default_print(void *user_context, const char *str) {
     write(STDOUT_FILENO, str, strlen(str));
 }
 
-}}} // namespace Halide::Runtime::Internal
+}
