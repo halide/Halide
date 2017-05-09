eb1801a9ca8e0f1575454b7a0e8ebe7b3d2644ea
Ankit Aggarwal <aankit@codeaurora.org>
2017-05-09 23:40:36 +0530

    Vtmpy Mutator for Hexagon

--- test/correctness/simd_op_check.cpp
+++ test/correctness/simd_op_check.cpp
@@ -1871,6 +1871,16 @@ struct Test {
         check("vmpa(v*.h,r*.b)", hvx_width/2, 2*i32(i16_1) + 3*i32(i16_2));
         check("v*.w += vmpa(v*.h,r*.b)", hvx_width/2, 2*i32(i16_1) + 3*i32(i16_2) + i32_1);
 
+        check("v*:*.h = vtmpy(v*:*.ub, r*.b)", hvx_width/1, 2*i16(in_u8(x)) + 3*i16(in_u8(x+1)) + i16(in_u8(x+2)));
+        check("v*:*.h = vtmpy(v*:*.ub, r*.b)", hvx_width/1, i16(in_u8(x)) + 3*i16(in_u8(x+1)) + i16(in_u8(x+2)));
+        check("v*:*.h = vtmpy(v*:*.ub, r*.b)", hvx_width/1, i16(in_u8(x))*2 + i16(in_u8(x+1)) + i16(in_u8(x+2)));
+        check("v*:*.h = vtmpy(v*:*.ub, r*.b)", hvx_width/1, i16(in_u8(x)) + i16(in_u8(x+1)) + i16(in_u8(x+2)));
+
+        check("v*:*.h = vtmpy(v*:*.b, r*.b)", hvx_width/1, 2*i16(in_i8(x)) + 3*i16(in_i8(x+1)) + i16(in_i8(x+2)));
+        check("v*:*.h = vtmpy(v*:*.b, r*.b)", hvx_width/1, i16(in_i8(x)) + 3*i16(in_i8(x+1)) + i16(in_i8(x+2)));
+        check("v*:*.h = vtmpy(v*:*.b, r*.b)", hvx_width/1, i16(in_i8(x))*2 + i16(in_i8(x+1)) + i16(in_i8(x+2)));
+        check("v*:*.h = vtmpy(v*:*.b, r*.b)", hvx_width/1, i16(in_i8(x)) + i16(in_i8(x+1)) + i16(in_i8(x+2)));
+
         // We only generate vdmpy if the inputs are interleaved (otherwise we would use vmpa).
         check("vdmpy(v*.ub,r*.b)", hvx_width/2, i16(in_u8(2*x))*127 + i16(in_u8(2*x + 1))*-128);
         check("vdmpy(v*.h,r*.b)", hvx_width/4, i32(in_i16(2*x))*2 + i32(in_i16(2*x + 1))*3);
