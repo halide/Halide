04592f0cdc2c370b280a237ca37acd74213687bd
Dillon Sharlet <dsharlet@google.com>
2017-05-17 09:23:19 -0700

    Remove redundant and broken test for saturating narrow, and make similar tests less confusing.
    Former-commit-id: be3bf1569a47615f54ea13f075adb5fc1af6e9f5

--- test/correctness/simd_op_check.cpp
+++ test/correctness/simd_op_check.cpp
@@ -1664,10 +1664,9 @@ struct Test {
         // tests. However, if the pipeline does widen, we want to generate
         // different instructions that have a built in interleaving that
         // we can cancel with the deinterleaving from widening.
-        check("v*.ub = vsat(v*.h,v*.h)", hvx_width/1, u8_sat(i16(i8_1) << 8));
-        check("v*.uh = vasr(v*.w,v*.w,r*):sat", hvx_width/2, u16_sat(i32(i16_1) << 16));
-        check("v*.h = vasr(v*.w,v*.w,r*):sat", hvx_width/2, u8_sat(i32(i16_1) >> 4));
-        check("v*.h = vsat(v*.w,v*.w)", hvx_width/2, i16_sat(i32(i16_1) << 16));
+        check("v*.ub = vsat(v*.h,v*.h)", hvx_width/1, u8_sat(i16(i8_1) << 1));
+        check("v*.uh = vasr(v*.w,v*.w,r*):sat", hvx_width/2, u16_sat(i32(i16_1) << 1));
+        check("v*.h = vsat(v*.w,v*.w)", hvx_width/2, i16_sat(i32(i16_1) << 1));
 
         // Also check double saturating narrows.
         check("v*.ub = vpack(v*.h,v*.h):sat", hvx_width/1, u8_sat(i32_1));
