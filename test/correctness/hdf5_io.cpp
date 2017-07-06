909ca304d3cc4cc3190370a8fb619bb1228d9fae
SanderX Vocke <sanderx.vocke@intel.com>
2017-07-06 11:23:38 +0200

    HDF5 I/O: Removed support for doubles. Added error messages and test vectors.

--- test/correctness/hdf5_io.cpp
+++ test/correctness/hdf5_io.cpp
@@ -51,6 +51,19 @@ template<> uint64_t random_number<uint64_t>() { return random_integer<uint64_t>(
 template<> float random_number<float>() { return random_real<float>(); }
 template<> double random_number<double>() { return random_real<double>(); }
 
+template<typename BufferType>
+string buffer_info(BufferType buf, string name) {
+    string retval = "Buffer '" + name + "':\n";
+    retval += "  Dimensions: " + std::to_string(buf.dimensions()) + "\n";
+    retval += "  Size: (";
+    for(size_t idx=0; idx<(size_t)buf.dimensions(); idx++) {
+        if(idx>0) retval += "x";
+        retval += std::to_string(buf.dim(idx).extent());
+    }
+    retval += ")\n";
+    return retval;
+}
+
 
 template<typename BufferType>
 void make_noise(BufferType &b) {
@@ -70,43 +83,85 @@ void make_noise(BufferType &b) {
 }
 
 template<typename T>
-void test(T &buf) {
+bool check_buffer(T &buf, T &ref, string test_name, int cur_dim) {
+    if(buf.dimensions() != ref.dimensions()) {
+        fprintf(stderr,"[ERROR @ test %s] Dimensions mismatch: orginal has %d, reference has %d\n",
+                test_name.c_str(),
+                buf.dimensions(),
+                ref.dimensions());
+        string info = "Buffer information:\n" + buffer_info(buf, "loaded buffer") + "\n" + buffer_info(ref, "reference buffer") + "\n";
+        fprintf(stderr, "%s", info.c_str());
+        return false;
+    }
+
+    if(buf.dimensions() > 0) {
+        //we do not have test vectors with nonzero mins.
+        assert(buf.dim(0).min() == 0);
+        assert(ref.dim(0).min() == 0);
+
+        if(buf.dim(0).extent() != ref.dim(0).extent()) {
+            fprintf(stderr,"[ERROR @ test %s] Size mismatch in dimension %d: orginal has %d, reference has %d\n",
+                    test_name.c_str(),
+                    cur_dim,
+                    buf.dim(0).extent(),
+                    ref.dim(0).extent());
+            string info = "Buffer information:\n" + buffer_info(buf, "loaded buffer") + "\n" + buffer_info(ref, "reference buffer") + "\n";
+            fprintf(stderr, "%s", info.c_str());
+            return false;
+        }
+
+        for(int i=0; i<buf.dim(0).extent(); i++) {
+            auto slice = buf.sliced(0, i);
+            auto refslice = ref.sliced(0, i);
+            if(!check_buffer(slice, refslice, test_name, cur_dim+1)) return false;
+        }
+    }
+
+    //If we arrive here, the buffers are 0-dimensional. Check the single element.
+    return buf() == ref();
+}
+
+template<typename T>
+bool roundtrip_test(T &buf, string test_name) {
 
     save_hdf5({"testbuffer"}, "test.h5", buf);
     T inbuf = load_from_hdf5<T>("test.h5", "testbuffer");
 
-    if( (buf.width() != inbuf.width()) || (buf.height() != inbuf.height()) ) {
-        printf("[ERROR] Size mismatch: original %dx%d, re-loaded %dx%d\n", buf.width(), buf.height(), inbuf.width(), inbuf.height());
-        exit(-1);
-    }
-
-    bool mismatch = false;
-    for (int y = 0; y < buf.height(); y++) {
-        for (int x = 0; x < buf.width(); x++) {
-            if(buf(x,y) != inbuf(x,y)) {
-                printf("[ERROR] difference at (%d,%d)\n", x, y);
-                mismatch = true;
-            }
-        }
-    }
-
-    if(mismatch) exit(-1);
+    return check_buffer(inbuf, buf, test_name, 0);
 }
 
 template<typename BufferType>
-void do_test(
+BufferType make_buf(
         vector<int> sizes
         ) {
-
     BufferType buf = BufferType(sizes);
     buf.allocate();
     make_noise(buf);
-    test(buf);
+    return buf;
+}
+
+template<typename BufferType>
+bool do_roundtrip_test(
+        vector<int> sizes,
+        string test_name
+        ) {
+    BufferType buf = make_buf<BufferType>(sizes);
+    return roundtrip_test(buf, test_name);
 }
 
 int main() {
-    do_test< Runtime::Buffer<int32_t, 3> >({5, 6, 2});
-    do_test< Runtime::Buffer<float, 4> >({10, 2, 3, 6});
+    //round-trip tests.
+    bool success = true;
+    if(!do_roundtrip_test< Runtime::Buffer<int8_t> >({5, 6, 12},     "int8")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<int16_t> >({1, 3, 2, 1}, "int16")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<int32_t> >({5, 8, 2},    "int32")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<int64_t> >({7, 6, 2},    "int64")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<uint8_t> >({5, 6, 2},     "uint8")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<uint16_t> >({1, 3, 2, 1, 1}, "uint16")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<uint32_t> >({5, 8, 2},    "uint32")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<uint64_t> >({7, 6, 2},    "uint64")) success = false;
+    if(!do_roundtrip_test< Runtime::Buffer<float> >({10, 2, 3, 6}, "float")) success = false;
+    if(!success) return -1;
     return 0;
 }
 
--- tools/halide_hdf5_io.h
+++ tools/halide_hdf5_io.h
@@ -19,12 +19,119 @@ namespace {
 
 enum Endianness {
     UseNative = 0,
-    BigEndianIfPossible,
-    LittleEndianIfPossible
+    BigEndian,
+    LittleEndian
 };
 
 void type_error(const halide_type_t &type) {
-    fprintf(stderr, "HDF5 I/O cannot handle data with type: %d bits: %d\n", type.code, type.bits);
+    fprintf(stderr, "Error: HDF5 I/O cannot handle data with type: %d bits: %d\n", type.code, type.bits);
+    exit(-1);
+}
+
+template<typename T>
+std::string type_to_string() {
+    halide_type_t type = halide_type_of<T>();
+    return ("<Type with Halide typecode " + std::to_string(type.code) + ", " + std::to_string(type.bits) + ">");
+}
+
+template<> std::string type_to_string<int8_t>()   { return "int8";   }
+template<> std::string type_to_string<int16_t>()  { return "int16";  }
+template<> std::string type_to_string<int32_t>()  { return "int32";  }
+template<> std::string type_to_string<int64_t>()  { return "int64";  }
+template<> std::string type_to_string<uint8_t>()  { return "uint8";  }
+template<> std::string type_to_string<uint16_t>() { return "uint16"; }
+template<> std::string type_to_string<uint32_t>() { return "uint32"; }
+template<> std::string type_to_string<uint64_t>() { return "uint64"; }
+template<> std::string type_to_string<float>()    { return "float";  }
+template<> std::string type_to_string<double>()   { return "double"; }
+
+std::string hdf5_type_to_string(H5::DataType &type) {
+    if(type == H5::PredType::NATIVE_INT8) return "int8 (NATIVE_INT8)";
+    else if(type == H5::PredType::STD_I8BE) return "int8 (STD_I8BE)";
+    else if(type == H5::PredType::STD_I8LE) return "int8 (STD_I8LE)";
+    else if(type == H5::PredType::NATIVE_INT16) return "int16 (NATIVE_INT16)";
+    else if(type == H5::PredType::STD_I16BE) return "int16 (STD_I16BE)";
+    else if(type == H5::PredType::STD_I16LE) return "int16 (STD_I16LE)";
+    else if(type == H5::PredType::NATIVE_INT32) return "int32 (NATIVE_INT32)";
+    else if(type == H5::PredType::STD_I32BE) return "int32 (STD_I32BE)";
+    else if(type == H5::PredType::STD_I32LE) return "int32 (STD_I32LE)";
+    else if(type == H5::PredType::NATIVE_INT64) return "int64 (NATIVE_INT64)";
+    else if(type == H5::PredType::STD_I64BE) return "int64 (STD_I64BE)";
+    else if(type == H5::PredType::STD_I64LE) return "int64 (STD_I64LE)";
+    else if(type == H5::PredType::NATIVE_UINT8) return "uint8 (NATIVE_UINT8)";
+    else if(type == H5::PredType::STD_U8BE) return "uint8 (STD_U8BE)";
+    else if(type == H5::PredType::STD_U8LE) return "uint8 (STD_U8LE)";
+    else if(type == H5::PredType::NATIVE_UINT16) return "uint16 (NATIVE_UINT16)";
+    else if(type == H5::PredType::STD_U16BE) return "uint16 (STD_U16BE)";
+    else if(type == H5::PredType::STD_U16LE) return "uint16 (STD_U16LE)";
+    else if(type == H5::PredType::NATIVE_UINT32) return "uint32 (NATIVE_UINT32)";
+    else if(type == H5::PredType::STD_U32BE) return "uint32 (STD_U32BE)";
+    else if(type == H5::PredType::STD_U32LE) return "uint32 (STD_U32LE)";
+    else if(type == H5::PredType::NATIVE_UINT64) return "uint64 (NATIVE_UINT64)";
+    else if(type == H5::PredType::STD_U64BE) return "uint64 (STD_U64BE)";
+    else if(type == H5::PredType::STD_U64LE) return "uint64 (STD_U64LE)";
+    else if(type == H5::PredType::NATIVE_FLOAT) return "float (NATIVE_FLOAT)";
+    else if(type == H5::PredType::IEEE_F32BE) return "float (IEEE_F32BE)";
+    else if(type == H5::PredType::IEEE_F32LE) return "float (IEEE_F32LE)";
+
+    return "<unsupported HDF5 data type>";
+}
+
+template<typename T> bool type_match(H5::DataType hdf5_type) { return false; }
+
+template<> bool type_match<int8_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_INT8) ||
+            (hdf5_type == H5::PredType::STD_I8BE) ||
+            (hdf5_type == H5::PredType::STD_I8LE);
+}
+template<> bool type_match<int16_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_INT16) ||
+            (hdf5_type == H5::PredType::STD_I16BE) ||
+            (hdf5_type == H5::PredType::STD_I16LE);
+}
+template<> bool type_match<int32_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_INT32) ||
+            (hdf5_type == H5::PredType::STD_I32BE) ||
+            (hdf5_type == H5::PredType::STD_I32LE);
+}
+template<> bool type_match<int64_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_INT64) ||
+            (hdf5_type == H5::PredType::STD_I64BE) ||
+            (hdf5_type == H5::PredType::STD_I64LE);
+}
+template<> bool type_match<uint8_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_UINT8) ||
+            (hdf5_type == H5::PredType::STD_U8BE) ||
+            (hdf5_type == H5::PredType::STD_U8LE);
+}
+template<> bool type_match<uint16_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_UINT16) ||
+            (hdf5_type == H5::PredType::STD_U16BE) ||
+            (hdf5_type == H5::PredType::STD_U16LE);
+}
+template<> bool type_match<uint32_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_UINT32) ||
+            (hdf5_type == H5::PredType::STD_U32BE) ||
+            (hdf5_type == H5::PredType::STD_U32LE);
+}
+template<> bool type_match<uint64_t>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_UINT64) ||
+            (hdf5_type == H5::PredType::STD_U64BE) ||
+            (hdf5_type == H5::PredType::STD_U64LE);
+}
+template<> bool type_match<float>(H5::DataType hdf5_type) {
+    return (hdf5_type == H5::PredType::NATIVE_FLOAT) ||
+            (hdf5_type == H5::PredType::IEEE_F32BE) ||
+            (hdf5_type == H5::PredType::IEEE_F32LE);
+}
+
+template<typename T>
+void type_load_match_error(H5::DataType hdf5_type, std::string buffer_name) {
+    fprintf(stderr, "Error: type of buffer found in HDF5 file does not match requested Buffer type.\n  Buffer: %s\n  Requested type: %s\n  Found type: %s\n",
+            buffer_name.c_str(),
+            type_to_string<T>().c_str(),
+            hdf5_type_to_string(hdf5_type).c_str()
+            );
     exit(-1);
 }
 
@@ -36,29 +143,29 @@ H5::DataType hdf5_type_from_halide_type(const halide_type_t &type, Endianness en
         case 8:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_INT8;
-            case BigEndianIfPossible: return H5::PredType::STD_I8BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_I8LE;
+            case BigEndian: return H5::PredType::STD_I8BE;
+            case LittleEndian: return H5::PredType::STD_I8LE;
             }
             break;
         case 16:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_INT16;
-            case BigEndianIfPossible: return H5::PredType::STD_I16BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_I16LE;
+            case BigEndian: return H5::PredType::STD_I16BE;
+            case LittleEndian: return H5::PredType::STD_I16LE;
             }
             break;
         case 32:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_INT32;
-            case BigEndianIfPossible: return H5::PredType::STD_I32BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_I32LE;
+            case BigEndian: return H5::PredType::STD_I32BE;
+            case LittleEndian: return H5::PredType::STD_I32LE;
             }
             break;
         case 64:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_INT64;
-            case BigEndianIfPossible: return H5::PredType::STD_I64BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_I64LE;
+            case BigEndian: return H5::PredType::STD_I64BE;
+            case LittleEndian: return H5::PredType::STD_I64LE;
             }
             break;
         default:
@@ -70,29 +177,29 @@ H5::DataType hdf5_type_from_halide_type(const halide_type_t &type, Endianness en
         case 8:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_UINT8;
-            case BigEndianIfPossible: return H5::PredType::STD_U8BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_U8LE;
+            case BigEndian: return H5::PredType::STD_U8BE;
+            case LittleEndian: return H5::PredType::STD_U8LE;
             }
             break;
         case 16:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_UINT16;
-            case BigEndianIfPossible: return H5::PredType::STD_U16BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_U16LE;
+            case BigEndian: return H5::PredType::STD_U16BE;
+            case LittleEndian: return H5::PredType::STD_U16LE;
             }
             break;
         case 32:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_UINT32;
-            case BigEndianIfPossible: return H5::PredType::STD_U32BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_U32LE;
+            case BigEndian: return H5::PredType::STD_U32BE;
+            case LittleEndian: return H5::PredType::STD_U32LE;
             }
             break;
         case 64:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_UINT64;
-            case BigEndianIfPossible: return H5::PredType::STD_U64BE;
-            case LittleEndianIfPossible: return H5::PredType::STD_U64LE;
+            case BigEndian: return H5::PredType::STD_U64BE;
+            case LittleEndian: return H5::PredType::STD_U64LE;
             }
             break;
         default:
@@ -104,15 +211,8 @@ H5::DataType hdf5_type_from_halide_type(const halide_type_t &type, Endianness en
         case 32:
             switch(endian) {
             case UseNative: return H5::PredType::NATIVE_FLOAT;
-            case BigEndianIfPossible: return H5::PredType::INTEL_F32;
-            case LittleEndianIfPossible: return H5::PredType::INTEL_F32;
-            }
-            break;
-        case 64:
-            switch(endian) {
-            case UseNative: return H5::PredType::NATIVE_INT64;
-            case BigEndianIfPossible: return H5::PredType::INTEL_F64;
-            case LittleEndianIfPossible: return H5::PredType::INTEL_F64;
+            case BigEndian: return H5::PredType::IEEE_F32BE;
+            case LittleEndian: return H5::PredType::IEEE_F32LE;
             }
             break;
         default:
@@ -209,7 +309,7 @@ void add_to_hdf5(H5::H5File &file, std::vector<std::string> names, int name_idx,
 
     H5::DataSpace dataspace(buffer.dimensions(), dims);
     H5::DataSet dataset = file.createDataSet(names[name_idx],
-                                             hdf5_type_from_halide_type(halide_type_of<ElemType>(), BigEndianIfPossible),
+                                             hdf5_type_from_halide_type(halide_type_of<ElemType>(), BigEndian),
                                              dataspace);
 
     ElemType *data = (ElemType *) create_dense_buffer<BufferType>(buffer);
@@ -226,48 +326,73 @@ void add_to_hdf5(H5::H5File &file, std::vector<std::string> names, int name_idx,
 
 }
 
-//Save a set of Halide buffers into an HDF5 file.
+/** Save a (set of) Halide buffer(s) into an HDF5 file and close it.
+ * Usage examples:
+ *
+ * Buffer<uint16_t> buf1;
+ * Buffer<float> buf2;
+ *
+ * save_hdf5( {"buf1_name_in_file"}, "out.h5", buf1 );
+ * save_hdf5( {"buf1_name_in_file", "buf2_name_in_file"}, "out_both.h5", buf1, buf2 );
+ */
 template<typename... BufferTypes>
-bool save_hdf5(std::vector<std::string> buffer_names, std::string filename, BufferTypes... buffers) {
-    //Open the HDF5 file.
-    H5::H5File file(filename, H5F_ACC_TRUNC);
+void save_hdf5(std::vector<std::string> buffer_names, std::string filename, BufferTypes... buffers) {
 
-    //Write the buffers into it.
-    add_to_hdf5(file, buffer_names, 0, buffers...);
+    try {
+        //Open the HDF5 file.
+        H5::H5File file(filename, H5F_ACC_TRUNC);
 
-    return true;
+        //Write the buffers into it.
+        add_to_hdf5(file, buffer_names, 0, buffers...);
+    } catch (std::exception const &error) {
+        fprintf(stderr, "%s\n", error.what());
+        exit(-1);
+    }
 }
 
-//Load a Halide buffer from an HDF5 file.
+/**
+ * Load a Halide buffer from an HDF5 file.
+ * Will abort if the buffer is not found in the file, or if the types of the requested and
+ * found buffers do not match.
+ */
 template<typename BufferType>
 BufferType load_from_hdf5(std::string filename, std::string buffer_name) {
     using ElemType = typename BufferType::ElemType;
+    BufferType retval;
 
-    //Open the HDF5 file.
-    H5::H5File file(filename, H5F_ACC_RDWR);
+    try {
+        //Open the HDF5 file.
+        H5::H5File file(filename, H5F_ACC_RDWR);
 
-    H5::DataSet dataset = file.openDataSet(buffer_name);
-    H5::DataSpace dataspace = dataset.getSpace();
-    int dimensions = dataspace.getSimpleExtentNdims();
-    assert(dimensions <= 16);
+        H5::DataSet dataset = file.openDataSet(buffer_name);
+        H5::DataSpace dataspace = dataset.getSpace();
+        H5::DataType datatype = dataset.getDataType();
+        if(!type_match<ElemType>(datatype)) {
+            type_load_match_error<ElemType>(datatype, buffer_name);
+        }
 
-    hsize_t dims[16];
-    std::vector<int> dims_vec;
-    assert( dataspace.getSimpleExtentDims(dims, NULL) == dimensions );
-    for(int d=0; d<dimensions; d++) {
-        dims_vec.insert(dims_vec.begin(), (int)dims[d]);
+        int dimensions = dataspace.getSimpleExtentNdims();
+        assert(dimensions <= 16);
+        hsize_t dims[16];
+        std::vector<int> dims_vec;
+        assert(dataspace.getSimpleExtentDims(dims, NULL) == dimensions);
+        for(int d=0; d<dimensions; d++) {
+            dims_vec.insert(dims_vec.begin(), (int)dims[d]);
+        }
+
+        retval = BufferType(BufferType::static_halide_type(), dims_vec);
+        size_t mem_size = dataset.getInMemDataSize();
+        ElemType *data = (ElemType *) malloc(mem_size);
+
+        dataset.read(data, hdf5_type_from_halide_type(halide_type_of<ElemType>(), UseNative));
+        retval.allocate();
+        fill_from_dense_buffer<BufferType>(retval, (void*)data);
+
+        free(data);
+    } catch (std::exception const &error) {
+        fprintf(stderr, "%s\n", error.what());
+        exit(-1);
     }
-
-    size_t mem_size = dataset.getInMemDataSize();
-    ElemType *data = (ElemType *) malloc(mem_size);
-
-    dataset.read(data, hdf5_type_from_halide_type(halide_type_of<ElemType>(), UseNative));
-
-    BufferType retval = BufferType(BufferType::static_halide_type(), dims_vec);
-    retval.allocate();
-    fill_from_dense_buffer<BufferType>(retval, (void*)data);
-
-    free(data);
 
     return retval;
 }
