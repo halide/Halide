f2c229bbbdcaefe87bfce4ea21bd09cc922b42d1
Steven Johnson <srj@google.com>
2017-07-11 17:23:32 -0700

    A few more tweaks, plus CMake fixes

--- tools/RunGenStubs.cpp
+++ tools/RunGenStubs.cpp
@@ -1,10 +1,6 @@
 #define HALIDE_GET_STANDARD_ARGV_FUNCTION halide_rungen_redirect_argv_getter
 #define HALIDE_GET_STANDARD_METADATA_FUNCTION halide_rungen_redirect_metadata_getter
 
-#ifndef HL_RUNGEN_FILTER_HEADER
-    #error "You must define HL_RUNGEN_FILTER_HEADER"
-#endif
-
 // This is legal C, as long as the macro expands to a single quoted (or <>-enclosed) string literal
 #include HL_RUNGEN_FILTER_HEADER
 
