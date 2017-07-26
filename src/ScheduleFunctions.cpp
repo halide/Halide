fca84d5f9818b2ee0b670075cf313009f5a5b9aa
Andrew Adams <andrew.b.adams@gmail.com>
2017-07-26 14:47:49 -0700

    Add comment
    Former-commit-id: 6d6bf3e1396514d7191cdf4095d47db830621abb

--- src/ScheduleFunctions.cpp
+++ src/ScheduleFunctions.cpp
@@ -710,6 +710,13 @@ private:
         vector<pair<string, Expr>> lets;
         while (const LetStmt *l = body.as<LetStmt>()) {
             if (!is_pure(l->value)) {
+                // The consumer of the Func we're injecting may be an
+                // extern stage, which shows up in the IR as a let
+                // stmt with a side-effecty RHS. We need to take care
+                // not to blow past it and risk injecting the producer
+                // *after* the consumer. In general it seems unwise to
+                // reorder the computation of a Func past something
+                // side-effecty, so we stop here.
                 break;
             }
             lets.push_back({ l->name, l->value });
