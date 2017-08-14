a4105ce07a67bf68fb268d133b67e7d20c35229b
Brian Leibig <brian@mperpetuo.com>
2017-08-14 16:07:32 -0700

    Error if an extern func has itself as an argument

---
+++ test/error/extern_func_self_argument.cpp
@@ -0,0 +1,18 @@
+#include "Halide.h"
+
+using namespace Halide;
+
+extern "C"
+int extern_func() {
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    Func f("f");
+
+    f.define_extern("extern_func", {f}, Int(32), 2);
+    f.infer_arguments();
+
+    printf("There should have been an error\n");
+    return 0;
+}
