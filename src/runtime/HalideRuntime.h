f7331f64b1c1bee73a7f2b821329f77fff2f43a1
Z Stern <zalman@zalman-macbookpro2.roam.corp.google.com>
2017-08-18 14:46:34 -0700

    Check for null buffer pointers and report a better error as it is fairly easy to do.
    Remove extraneous null buffer handling code from halide_device_sync as
    it did not work anyway. (Making it sync all devices requires
    registering each device_interface which has been used, which is
    perhaps possible, though seems like too much work for something noone
    uses.)

--- src/runtime/HalideRuntime.h
+++ src/runtime/HalideRuntime.h
@@ -850,6 +850,10 @@ enum halide_error_code_t {
     /** Buffer has both host and device dirty bits set, which violates
      * a Halide invariant. */
     halide_error_code_host_and_device_dirty = -37,
+
+    /** The halide_buffer_t * passed to a halide runtime routine is
+     * nullptr and this is not allowed. */
+    halide_error_code_buffer_is_null = -38,
 };
 
 /** Halide calls the functions below on various error conditions. The
@@ -925,6 +929,7 @@ extern int halide_error_specialize_fail(void *user_context, const char *message)
 extern int halide_error_no_device_interface(void *user_context);
 extern int halide_error_device_interface_no_device(void *user_context);
 extern int halide_error_host_and_device_dirty(void *user_context);
+extern int halide_error_buffer_is_null(void *user_context, const char *routine);
 
 // @}
 
