b48e2b6b2f481449a91e5ccd323ee1d248ef8c27
Steven Johnson <srj@google.com>
2017-08-30 17:15:55 -0700

    Fix for trunk LLVM
    Former-commit-id: 0ac92b19f53befeb97ac01e3f887da17b4aa8d9d

--- src/LLVM_Output.cpp
+++ src/LLVM_Output.cpp
@@ -255,8 +255,13 @@ void create_static_library(const std::vector<std::string> &src_files_in, const T
                        write_symtab, kind,
                        deterministic, thin, nullptr);
 #endif
+#if LLVM_VERSION >= 60
+    internal_assert(!result) << "Failed to write archive: " << dst_file
+        << ", reason: " << result << "\n";
+#else
     internal_assert(!result.second) << "Failed to write archive: " << dst_file
         << ", reason: " << result.second << "\n";
+#endif
 }
 
 }  // namespace Halide
