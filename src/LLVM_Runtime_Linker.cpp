a71cf238d618bfab9f688d55de21d2cd1b70d58b
Pranav Bhandarkar <pranavb@codeaurora.org>
2017-09-05 16:48:59 -0500

    Add qurt_init_fini.cpp to add runtime functions that call the initialization (not yet implemented) and termination functions for a pipeline offloaded on Hexagon.
    HelloHexagon is now working on an 8998 device.

--- src/LLVM_Runtime_Linker.cpp
+++ src/LLVM_Runtime_Linker.cpp
@@ -118,6 +118,7 @@ DECLARE_CPP_INITMOD(profiler)
 DECLARE_CPP_INITMOD(profiler_inlined)
 DECLARE_CPP_INITMOD(qurt_allocator)
 DECLARE_CPP_INITMOD(qurt_hvx)
+DECLARE_CPP_INITMOD(qurt_init_fini)
 DECLARE_CPP_INITMOD(qurt_thread_pool)
 DECLARE_CPP_INITMOD(runtime_api)
 DECLARE_CPP_INITMOD(ssp)
@@ -696,6 +697,7 @@ std::unique_ptr<llvm::Module> get_initial_module_for_target(Target t, llvm::LLVM
                 // modules.push_back(get_initmod_posix_get_symbol(c, bits_64, debug));
                 // modules.push_back(get_initmod_posix_io(c, bits_64, debug));
                 modules.push_back(get_initmod_qurt_thread_pool(c, bits_64, debug));
+                modules.push_back(get_initmod_qurt_init_fini(c, bits_64, debug));
             } else if (t.os == Target::NoOS) {
                 // The OS-specific symbols provided by the modules
                 // above are expected to be provided by the containing
