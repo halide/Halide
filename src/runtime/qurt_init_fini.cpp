4bfeec414f5c6e2bc168df52960f9ee94a126827
Pranav Bhandarkar <pranavb@codeaurora.org>
2017-09-08 15:30:17 -0500

    Add ability to handle ctors and DT_INIT in the dynamic section.
    This patch contains the following changes.
    1) src/Elf.cpp : Defines a symbol called __CTOR_END__ that points
    to the end of the list of pointers to constructors in the .ctors section.
    It also sets up the DT_INIT tag to point to the start of the .init.halide
    section.
    2) src/HexagonOffload.cpp: Prepends (uint32_t) 0 to the start of the .ctors
    section. Constructors are called by iterating backwards from (__CTOR_END__ -4)
    until we hit a NULL pointer (delimiter). This 0 serves this purpose of being a
    delimiter.
    3) src/Elf.h: Added member functions prepend_contents and fixup_relocations_offset
    to Section.
    4) src/runtime/hexagon_remote/dlib.cpp: Process the DT_INIT tag and call initialization
    function in mmap_dlopen.
    5) src/runtime/qurt_init_fini.cpp: Initialization function that calls constructors whose
    pointers are stored in .ctors.
    
    This is now working on 8996 & 8998.
    
    
    Former-commit-id: d3a09f743284461a1e29c00d30bb8442a6c9f33c

--- src/runtime/qurt_init_fini.cpp
+++ src/runtime/qurt_init_fini.cpp
@@ -7,8 +7,9 @@ typedef uint32_t addr_t;
 #endif
 
 extern addr_t __DTOR_LIST__;
+extern addr_t __CTOR_END__;
 extern "C" {
-__attribute__((section(".fini")))
+__attribute__((section(".fini.halide")))
 void run_dtors() {
     typedef void(*dtor_func)();
     addr_t *dtor_p = &__DTOR_LIST__;
@@ -20,6 +21,19 @@ void run_dtors() {
             dtor();
         }
         dtor_p++;
+    }
+}
+__attribute__((section(".init.halide")))
+void run_ctors() {
+    typedef void(*ctor_func)();
+    addr_t *ctor_p = &__CTOR_END__;
+    while (1) {
+        ctor_func ctor = (ctor_func) *(--ctor_p);
+        if (!ctor) {
+            break;
+        } else {
+            ctor();
+        }
     }
 }
 } // extern "C"
