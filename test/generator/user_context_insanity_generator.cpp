f97ff4ed0cc4cd3e4895bbf6cd24bb613ffd5dac
Steven Johnson <srj@google.com>
2017-09-20 17:39:41 -0700

    Allow Input<> to be used for Generators with build()
    Up to now, we have explicitly disallowed using the Input<> syntax for Generators that used build(), rather than generate()+schedule(); this was done in the (misguided) thought that it would encourage conversion of Generators to the newer syntax. As it turns out, it actually *discouraged* said conversion, as it didn't allow for incremental changes to be made.
    
    This PR changes that:
    - You now may freely use Input<> in any Generator, even if it uses build().
    - However, you may not mix Param/ImageParam and Input<> in the same Generator.
    - All Generators in Halide have been converted to use Input<> rather than Param/ImageParam.
    - It is still legal to use Param/ImageParam in Generators; however, they are now marked as deprecated when used in this context.
    
    
    Former-commit-id: b097d4bde279a410dcd23ae65b234defa8d70dbb

--- test/generator/user_context_insanity_generator.cpp
+++ test/generator/user_context_insanity_generator.cpp
@@ -4,7 +4,7 @@ namespace {
 
 class UserContextInsanity : public Halide::Generator<UserContextInsanity> {
 public:
-    ImageParam input{ Float(32), 2, "input" };
+    Input<Buffer<float>> input{"input", 2};
 
     Func build() {
         Var x, y;
