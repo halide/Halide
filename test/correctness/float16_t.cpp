f4c019aa3c3a15771a54385ee5346622b85b7956
Andrew Adams <andrew.b.adams@gmail.com>
2017-10-12 15:43:25 -0700

    Remove llvm dependency from float16 implementation
    and remove a bunch of unnecessary complexity from the API
    
    
    Former-commit-id: db8901afdb16f61d0654850345baf539cb14b34a

---
+++ test/correctness/float16_t.cpp
@@ -0,0 +1,32 @@
+#include "Halide.h"
+#include <stdio.h>
+#include <cmath>
+
+using namespace Halide;
+
+int main() {
+    // Check we can send a float16_t through the compiler with some simple arithmetic.
+    {
+        Param<float16_t> p;
+        p.set(float16_t(0.75));
+
+        float16_t val = evaluate<float16_t>(2*(p + 1));
+
+        if ((float)val != 3.5f) {
+            printf("Expected 3.5, got %f\n", (float)val);
+        }
+    }
+
+    {
+        // A float16_t should be implicitly upcast to a float
+        float val = evaluate<float>(0.25f + Expr(float16_t(0.75)));
+
+        if (val != 1.0f) {
+            printf("Expected 1, got %f\n", val);
+            return -1;
+        }
+    }
+
+    printf("Success!\n");
+    return 0;
+}
