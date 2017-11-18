8ef2953446212957463e5caf5b1f11024254467e
Steven Johnson <srj@google.com>
2017-11-17 17:21:45 -0800

    Update LLVM_DIR path
    Former-commit-id: 0f309e96e2dd327242970fdc29fbd78386e73ac6 [formerly b0ea1a77dadbb4e7a90b44a14f82639ff662a294]
    Former-commit-id: e35ef236d835c2c90e62ace4bd76c0809e99c326

--- test/scripts/build_travis.sh
+++ test/scripts/build_travis.sh
@@ -20,7 +20,7 @@ if [ ${BUILD_SYSTEM} = 'CMAKE' ]; then
   # Require a specific version of LLVM, just in case the Travis instance has
   # an older clang/llvm version present
   cmake -DHALIDE_REQUIRE_LLVM_VERSION="${LLVM_VERSION_NO_DOT}" \
-        -DLLVM_DIR="/usr/local/llvm/share/llvm/cmake/" \
+        -DLLVM_DIR="/usr/local/llvm/lib/cmake/llvm/" \
         -DHALIDE_SHARED_LIBRARY="${HALIDE_SHARED_LIBRARY}" \
         -DWITH_APPS=OFF \
         -DWITH_TESTS=ON \
