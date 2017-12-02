be24d31c360e4a435bf80362fd982436565150c2
Michael Liao <michael.hliao@gmail.com>
2017-12-02 01:16:05 -0800

    Enable OpenCL half support
    - Add a new 'cl_half' target feature to enable OCL half support.
    - Define math functions for f16.
    - Fix a minor issue in is_nan implementation on half.
    - Enable python support as well.

--- src/runtime/HalideRuntime.h
+++ src/runtime/HalideRuntime.h
@@ -1071,7 +1071,8 @@ typedef enum halide_target_feature_t {
     halide_target_feature_cuda_capability61 = 46,  ///< Enable CUDA compute capability 6.1 (Pascal)
     halide_target_feature_hvx_v65 = 47, ///< Enable Hexagon v65 architecture.
     halide_target_feature_hvx_v66 = 48, ///< Enable Hexagon v66 architecture.
-    halide_target_feature_end = 49, ///< A sentinel. Every target is considered to have this feature, and setting this feature does nothing.
+    halide_target_feature_cl_half = 49,  ///< Enable half support on OpenCL targets
+    halide_target_feature_end = 50, ///< A sentinel. Every target is considered to have this feature, and setting this feature does nothing.
 } halide_target_feature_t;
 
 /** This function is called internally by Halide in some situations to determine
