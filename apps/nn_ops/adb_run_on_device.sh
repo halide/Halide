8bd9614dd1deb4a8d3ec57c05107348481f2c283
Volodymyr Kysenko <vksnk@google.com>
2018-01-12 14:57:54 -0800

    Update script to run on Android

--- apps/nn_ops/adb_run_on_device.sh
+++ apps/nn_ops/adb_run_on_device.sh
@@ -17,7 +17,7 @@ BIN=bin
 APP_TARGET=arm-64-android
 
 # Build the app.
-make bin/${APP_TARGET}/MatrixMultiply bin/${APP_TARGET}/MaxPool
+make bin/${APP_TARGET}/AveragePool bin/${APP_TARGET}/Convolution bin/${APP_TARGET}/DepthwiseConvolution bin/${APP_TARGET}/Im2col bin/${APP_TARGET}/MatrixMultiply bin/${APP_TARGET}/MaxPool
 
 # Make a folder on device for the app and our dependencies.
 adb shell mkdir -p ${DEVICE_PATH}
@@ -31,11 +31,30 @@ adb push ${HEXAGON_RUNTIME_PATH}/bin/v60/signed_by_debug/libhalide_hexagon_remot
 adb shell cp /system/lib/rfsa/adsp/testsig* ${DEVICE_PATH} > /dev/null || true
 
 # Push and run the app!
+adb push ${BIN}/${APP_TARGET}/AveragePool ${DEVICE_PATH}
+adb push ${BIN}/${APP_TARGET}/Convolution ${DEVICE_PATH}
+adb push ${BIN}/${APP_TARGET}/DepthwiseConvolution ${DEVICE_PATH}
+adb push ${BIN}/${APP_TARGET}/Im2col ${DEVICE_PATH}
 adb push ${BIN}/${APP_TARGET}/MatrixMultiply ${DEVICE_PATH}
 adb push ${BIN}/${APP_TARGET}/MaxPool ${DEVICE_PATH}
+
+adb shell chmod +x ${DEVICE_PATH}/AveragePool
+adb shell chmod +x ${DEVICE_PATH}/Convolution
+adb shell chmod +x ${DEVICE_PATH}/DepthwiseConvolution
+adb shell chmod +x ${DEVICE_PATH}/Im2col
 adb shell chmod +x ${DEVICE_PATH}/MatrixMultiply
 adb shell chmod +x ${DEVICE_PATH}/MaxPool
+
+adb push AveragePool.sh ${DEVICE_PATH}
+adb push Convolution.sh ${DEVICE_PATH}
+adb push DepthwiseConvolution.sh ${DEVICE_PATH}
+adb push Im2col.sh ${DEVICE_PATH}
 adb push MatrixMultiply.sh ${DEVICE_PATH}
 adb push MaxPool.sh ${DEVICE_PATH}
+
+adb shell ${DEVICE_ENV} ${DEVICE_PATH}/AveragePool.sh ${DEVICE_PATH}/AveragePool
+adb shell ${DEVICE_ENV} ${DEVICE_PATH}/Convolution.sh ${DEVICE_PATH}/Convolution
+adb shell ${DEVICE_ENV} ${DEVICE_PATH}/DepthwiseConvolution.sh ${DEVICE_PATH}/DepthwiseConvolution
+adb shell ${DEVICE_ENV} ${DEVICE_PATH}/Im2col.sh ${DEVICE_PATH}/Im2col
 adb shell ${DEVICE_ENV} ${DEVICE_PATH}/MatrixMultiply.sh ${DEVICE_PATH}/MatrixMultiply
-adb shell ${DEVICE_ENV} ${DEVICE_PATH}/MatrixMultiply.sh ${DEVICE_PATH}/MaxPool
+adb shell ${DEVICE_ENV} ${DEVICE_PATH}/MaxPool.sh ${DEVICE_PATH}/MaxPool
