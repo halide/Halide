1e995bc208ada399b89a2a4391331d6acde50466
Marcos Slomp <slomp@adobe.com>
2018-02-01 13:44:45 -0800

    disable optimizations and function inlining around the d3d12 abi-patch entry stubs to force the compiler to emit a function call and prevent it from shuffling registers around (memory barriers did not work...)

--- src/runtime/d3d12_abi_patch_64.h
+++ src/runtime/d3d12_abi_patch_64.h
@@ -16,33 +16,43 @@ extern "C" {
 }
 #endif
 
-inline D3D12_DESCRIPTOR_HEAP_DESC Call_ID3D12DescriptorHeap_GetDesc(ID3D12DescriptorHeap* descriptorheap)
+__attribute__((optnone))
+__attribute__((noinline))
+D3D12_DESCRIPTOR_HEAP_DESC Call_ID3D12DescriptorHeap_GetDesc(ID3D12DescriptorHeap* descriptorheap)
 {
     D3D12_DESCRIPTOR_HEAP_DESC desc = { };
     Call_ID3D12DescriptorHeap_GetDesc( (int64_t*)descriptorheap, (int64_t*)&desc );
     return(desc);
 }
 
-inline D3D12_CPU_DESCRIPTOR_HANDLE Call_ID3D12DescriptorHeap_GetCPUDescriptorHandleForHeapStart(ID3D12DescriptorHeap* descriptorheap)
+__attribute__((optnone))
+__attribute__((noinline))
+D3D12_CPU_DESCRIPTOR_HANDLE Call_ID3D12DescriptorHeap_GetCPUDescriptorHandleForHeapStart(ID3D12DescriptorHeap* descriptorheap)
 {
     D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle = { };
     Call_ID3D12DescriptorHeap_GetCPUDescriptorHandleForHeapStart( (int64_t*)descriptorheap, (int64_t*)&cpuHandle );
     return(cpuHandle);
 }
 
-inline D3D12_GPU_DESCRIPTOR_HANDLE Call_ID3D12DescriptorHeap_GetGPUDescriptorHandleForHeapStart(ID3D12DescriptorHeap* descriptorheap)
+__attribute__((optnone))
+__attribute__((noinline))
+D3D12_GPU_DESCRIPTOR_HANDLE Call_ID3D12DescriptorHeap_GetGPUDescriptorHandleForHeapStart(ID3D12DescriptorHeap* descriptorheap)
 {
     D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = { };
     Call_ID3D12DescriptorHeap_GetGPUDescriptorHandleForHeapStart( (int64_t*)descriptorheap, (int64_t*)&gpuHandle );
     return(gpuHandle);
 }
 
-inline void Call_ID3D12GraphicsCommandList_SetComputeRootDescriptorTable(ID3D12GraphicsCommandList* commandList, UINT RootParameterIndex, D3D12_GPU_DESCRIPTOR_HANDLE BaseDescriptor)
+__attribute__((optnone))
+__attribute__((noinline))
+void Call_ID3D12GraphicsCommandList_SetComputeRootDescriptorTable(ID3D12GraphicsCommandList* commandList, UINT RootParameterIndex, D3D12_GPU_DESCRIPTOR_HANDLE BaseDescriptor)
 {
     Call_ID3D12GraphicsCommandList_SetComputeRootDescriptorTable(commandList, RootParameterIndex, &BaseDescriptor);
 }
 
-inline void Call_ID3D12Device_CreateConstantBufferView(ID3D12Device* device, D3D12_CONSTANT_BUFFER_VIEW_DESC* pDesc, D3D12_CPU_DESCRIPTOR_HANDLE DestDescriptor)
+__attribute__((optnone))
+__attribute__((noinline))
+void Call_ID3D12Device_CreateConstantBufferView(ID3D12Device* device, D3D12_CONSTANT_BUFFER_VIEW_DESC* pDesc, D3D12_CPU_DESCRIPTOR_HANDLE DestDescriptor)
 {
     Call_ID3D12Device_CreateConstantBufferView(device, pDesc, &DestDescriptor);
 }
