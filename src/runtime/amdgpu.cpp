de4c2936ee4cf4796096fd56931b4af4d7928755
Aditya Atluri <Aditya.Atluri@amd.com>
2018-02-21 10:56:58 -0600

    added support to load hip shared library

--- src/runtime/amdgpu.cpp
+++ src/runtime/amdgpu.cpp
@@ -13,6 +13,61 @@ namespace Halide { namespace Runtime { namespace Internal {namespace Amdgpu {
 
 #include "hip_functions.h"
 
+WEAK void *lib_hip = NULL;
 
+extern "C" WEAK void *halide_amdgpu_get_symbol(void *user_context, const char *name) {
+    // Only try to load the library if we can't already get the symbol
+    // from the library. Even if the library is NULL, the symbols may
+    // already be available in the process.
+    void *symbol = halide_get_library_symbol(lib_hip, name);
+    if (symbol) {
+        return symbol;
+    }
 
-}}}}
+    const char *lib_names[] = {
+        "libhip_hcc.so",
+    };
+    for (size_t i = 0; i < sizeof(lib_names) / sizeof(lib_names[0]); i++) {
+        lib_hip = halide_load_library(lib_names[i]);
+        if (lib_hip) {
+            debug(user_context) << "    Loaded HIP runtime library: " << lib_names[i] << "\n";
+            break;
+        }
+    }
+
+    return halide_get_library_symbol(lib_hip, name);
+}
+
+template <typename T>
+INLINE T get_amdgpu_symbol(void *user_context, const char *name, bool optional = false) {
+    T s = (T)halide_amdgpu_get_symbol(user_context, name);
+    if (!optional && !s) {
+        error(user_context) << "HIP API not found: " << name << "\n";
+    }
+    return s;
+}
+
+// Load a HIP shared object and get the HIP API function pointers from it.
+WEAK void load_libhip(void *user_context) {
+    debug(user_context) << "    load_libhip (user_context: " << user_context << ")\n";
+    halide_assert(user_context, hipInit == NULL);
+
+    #define HIP_FN(ret, fn, args) fn = get_amdgpu_symbol<ret (HIPAPI *)args>(user_context, #fn);
+
+    #include "hip_functions.h"
+}
+
+extern WEAK halide_device_interface_t amdgpu_device_interface;
+
+WEAK const char *get_error_name(hipError_t error);
+WEAK hipError_t create_amdgpu_context(void *user_context, hipCtx_t *ctx);
+
+// A cuda context defined in this module with weak linkage
+hipCtx_t WEAK context = 0;
+// This spinlock protexts the above context variable.
+volatile int WEAK context_lock = 0;
+
+}}}} // namespace Halide::Runtime::Internal::Amdgpu
+
+using namespace Halide::Runtime::Internal;
+using namespace Halide::Runtime::Internal::Amdgpu;
