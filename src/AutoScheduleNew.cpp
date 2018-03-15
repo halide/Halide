2c705134d12d1abe7eedbb693edff2b96ab48de9
Andrew Adams <andrew.b.adams@gmail.com>
2018-03-15 09:28:54 -0700

    Control random dropout with env var

--- src/AutoScheduleNew.cpp
+++ src/AutoScheduleNew.cpp
@@ -36,6 +36,10 @@ double cost_of_cold_load(double buffer_size, const MachineParams &params) {
     //return params.balance * std::log2(1 + buffer_size / params.last_level_cache_size);
 }
 
+bool use_random_dropout() {
+    static bool v = !get_env_variable("HL_RANDOM_DROPOUT").empty();
+    return v;
+}
 
 // A representation of the function DAG. The nodes and edges are both
 // in reverse realization order, so if you want to walk backwards up
@@ -786,7 +790,9 @@ struct PartialScheduleNode {
             for (auto t : tilings) {
 
                 // Random dropout. Uncomment to get a random family of plausible schedules.
-                if (rand() & 3) continue;
+                if (use_random_dropout()) {
+                    if (rand() & 3) continue;
+                }
 
                 if (parent->is_root()) {
                     // Skip root-level tilings that provide insufficient parallelism to avoid nested parallelism
