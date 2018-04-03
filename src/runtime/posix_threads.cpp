fc24ea46c8b3c0f00e8a660ba902432ea444c1fd
Z Stern <zalman@rodete-desktop-imager.corp.google.com>
2018-04-03 11:56:22 -0700

    Remove thread local parking object caching support. Did not seem to matter for performance. Left a comment referencing the commit in case anyone needs it in the future.

--- src/runtime/posix_threads.cpp
+++ src/runtime/posix_threads.cpp
@@ -30,24 +30,6 @@ extern int pthread_mutex_lock(pthread_mutex_t *mutex);
 extern int pthread_mutex_unlock(pthread_mutex_t *mutex);
 extern int pthread_mutex_destroy(pthread_mutex_t *mutex);
 
-// Indication on first machine is this is not any faster. (Probably not any slower either, but it's more complex so...)
-// Checking it in to try it on a couple other machines, but for code review purposes it will likely get deleted.
-#define TLS_CACHING_PARKER 0
-#if TLS_CACHING_PARKER
-
-struct pthread_once_t {
-    uint64_t _private[8];
-};
-
-typedef unsigned int pthread_key_t;
-
-extern int pthread_once(pthread_once_t *once_control, void (*init_routine)(void));
-extern int pthread_key_create(pthread_key_t *key, void (*destructor)(void*));
-extern int pthread_setspecific(pthread_key_t key, const void *value);
-extern void *pthread_getspecific(pthread_key_t key);
-
-#endif
-
 } // extern "C"
 
 namespace Halide { namespace Runtime { namespace Internal {
@@ -89,35 +71,15 @@ namespace Halide { namespace Runtime { namespace Internal {
 
 namespace Synchronization {
 
-#if TLS_CACHING_PARKER
-
-pthread_once_t parker_cache_key_once;
-pthread_key_t parker_cache_key;
-
-struct cached_parker {
-    pthread_mutex_t mutex;
-    pthread_cond_t condvar;
-};
-
-void destruct_cached_parker(void *arg) {
-    struct cached_parker *cached = (struct cached_parker *)arg;
-    pthread_cond_destroy(&cached->condvar);
-    pthread_mutex_destroy(&cached->mutex);
-}
-
-void make_parker_cache_key() {
-    pthread_key_create(&parker_cache_key, destruct_cached_parker);
-}
-
-#endif
+// There is code to cache the parking object in a thread local. Other
+// packages do this, but it did not seem to make a difference for
+// performance on Linux and Mac OS X as initializing a mutex and
+// condvar is cheap.  This code can be found in commit
+// 6a1ea6d2c883353f51f62fec4c2bce129649e2a7.
 
 struct thread_parker {
-#if TLS_CACHING_PARKER
-    struct cached_parker *cached;
-#else
     pthread_mutex_t mutex;
     pthread_cond_t condvar;
-#endif
     bool should_park;
 
 #if __cplusplus >= 201103L
@@ -125,29 +87,14 @@ struct thread_parker {
 #endif
 
     __attribute__((always_inline)) thread_parker() : should_park(false) {
- #if TLS_CACHING_PARKER
-        pthread_once(&parker_cache_key_once, make_parker_cache_key);
-        struct cached_parker *tls = (struct cached_parker *)pthread_getspecific(parker_cache_key);
-        if (tls == NULL) {
-            tls = (struct cached_parker *)malloc(sizeof(struct cached_parker));
-            pthread_mutex_init(&tls->mutex, NULL);
-            pthread_cond_init(&tls->condvar, NULL);
-            pthread_setspecific(parker_cache_key, tls);
-        }
-        cached = tls;
- #else
         pthread_mutex_init(&mutex, NULL);
         pthread_cond_init(&condvar, NULL);
- #endif
         should_park = false;
     }
 
     __attribute__((always_inline)) ~thread_parker() {
- #if TLS_CACHING_PARKER
- #else
         pthread_cond_destroy(&condvar);
         pthread_mutex_destroy(&mutex);
-#endif
     }
 
     __attribute__((always_inline)) void prepare_park() {
@@ -155,44 +102,24 @@ struct thread_parker {
     }
 
     __attribute__((always_inline)) void park() {
-#if TLS_CACHING_PARKER
-        pthread_mutex_lock(&cached->mutex);
-        while (should_park) {
-            pthread_cond_wait(&cached->condvar, &cached->mutex);
-        }
-        pthread_mutex_unlock(&cached->mutex);
-#else
         pthread_mutex_lock(&mutex);
         while (should_park) {
             pthread_cond_wait(&condvar, &mutex);
         }
         pthread_mutex_unlock(&mutex);
-#endif
     }
 
     __attribute__((always_inline)) void unpark_start() {
-#if TLS_CACHING_PARKER
-        pthread_mutex_lock(&cached->mutex);
-#else
         pthread_mutex_lock(&mutex);
-#endif
     }
 
     __attribute__((always_inline)) void unpark() {
         should_park = false;
-#if TLS_CACHING_PARKER
-        pthread_cond_signal(&cached->condvar);
-#else
         pthread_cond_signal(&condvar);
-#endif
     }
 
     __attribute__((always_inline)) void unpark_finish() {
-#if TLS_CACHING_PARKER
-        pthread_mutex_unlock(&cached->mutex);
-#else
         pthread_mutex_unlock(&mutex);
-#endif
     }
 };
 
