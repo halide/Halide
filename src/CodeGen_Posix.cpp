9935da89e3a43d15f0b741f37562f0e6c93bdfb2
Andrew Adams <andrew.b.adams@gmail.com>
2018-05-15 11:49:45 -0700

    Negative extents are OK - they indicate the loops that access have negative extents

--- src/CodeGen_Posix.cpp
+++ src/CodeGen_Posix.cpp
@@ -41,7 +41,7 @@ Value *CodeGen_Posix::codegen_allocation_size(const std::string &name, Type type
 
     Expr low_mask = make_const(UInt(64), (uint64_t)(0xffffffff));
     for (size_t i = 0; i < extents.size(); i++) {
-        Expr next_extent = cast(UInt(32), extents[i]);
+        Expr next_extent = cast(UInt(32), max(0, extents[i]));
 
         // Update total_size >> 32. This math can't overflow due to
         // the loop invariant:
