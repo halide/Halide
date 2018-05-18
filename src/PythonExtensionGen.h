87f6a181c5a065b88b576cf60c20cf3cbb30e822
Matthias Kramm <kramm@google.com>
2018-05-18 10:35:24 -0700

    Generate Python C extension code.

--- src/PythonExtensionGen.h
+++ src/PythonExtensionGen.h
@@ -1,7 +1,10 @@
 #ifndef HALIDE_PYTHON_EXTENSION_GEN_H_
 #define HALIDE_PYTHON_EXTENSION_GEN_H_
 
+#include <string>
 #include "Target.h"
+
+using std::string;
 
 namespace Halide {
 
@@ -15,7 +18,9 @@ public:
     PythonExtensionGen(std::ostream &dest, const std::string &header_name, Target target);
 
     void compile(const Module &module);
+    void compile(const LoweredFunc &f);
 private:
+    void convert_buffer(string name, const LoweredArgument* arg);
     std::ostream &dest;
     std::string header_name;
     Target target;
