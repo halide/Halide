33b2e124972c5ff092413adf233e31a2a293abaa
Andrew Adams <andrew.b.adams@gmail.com>
2018-05-22 14:24:55 -0700

    Add pyramid-like patterns

--- apps/random_pipeline/random_pipeline_generator.cpp
+++ apps/random_pipeline/random_pipeline_generator.cpp
@@ -48,115 +48,164 @@ public:
         }
     }
 
+    struct Stage {
+        Func func;
+        int w, h; // approx width and height;
+    };
 
     // Generate a random convolution of one dimension of f.
-    Func convolve(Func f, int dim, int kernel_min, int kernel_max) {
+    Stage convolve(Stage f, int dim, int kernel_min, int kernel_max) {
         std::cout << "Convolving dimension " << dim
                   << " with kernel [" << kernel_min << ", " << kernel_max << "]"
                   << std::endl;
 
-        vector<Var> args = f.args();
+        vector<Var> args = f.func.args();
 
-        Expr def = cast(f.value().type(), 0);
+        Expr def = cast(f.func.value().type(), 0);
         for (int i = kernel_min; i <= kernel_max; i++) {
-            vector<Expr> coords = make_arguments(f.args());
+            vector<Expr> coords = make_arguments(f.func.args());
             coords[dim] += i;
-            def = def + rand_value(f.value().type()) * f(coords);
+            def = def + rand_value(f.func.value().type()) * f.func(coords);
         }
 
         Func conv("conv");
         conv(args) = def;
 
-        return conv;
+        return {conv, f.w, f.h};
     }
 
     // Generate an upsampling or downsampling of dimension dim by factor.
-    Func upsample(Func f, int dim, int factor) {
+    Stage upsample(Stage f, int dim, int factor) {
         std::cout << "Upsampling dimension " << dim << " by " << factor << "x" << std::endl;
 
-        vector<Expr> resampled_coords = make_arguments(f.args());
+        vector<Expr> resampled_coords = make_arguments(f.func.args());
         resampled_coords[dim] = resampled_coords[dim] / factor;
 
-        Func resampled("resampled");
-        resampled(f.args()) = f(resampled_coords);
+        Func resampled("upsampled");
+        resampled(f.func.args()) = f.func(resampled_coords);
 
-        return resampled;
+        Stage s {resampled, f.w, f.h};
+        if (dim == 0) {
+            s.w *= factor;
+        } else if (dim == 1) {
+            s.h *= factor;
+        } else {
+            assert(false);
+        }
+        return s;
     }
 
-    Func downsample(Func f, int dim, int factor) {
+    Stage downsample(Stage f, int dim, int factor) {
         std::cout << "Downsampling dimension " << dim << " by " << factor << "x" << std::endl;
 
-        vector<Expr> resampled_coords = make_arguments(f.args());
+        vector<Expr> resampled_coords = make_arguments(f.func.args());
         resampled_coords[dim] = resampled_coords[dim] * factor;
 
-        Func resampled("resampled");
-        resampled(f.args()) = f(resampled_coords);
+        Func resampled("downsampled");
+        resampled(f.func.args()) = f.func(resampled_coords);
 
-        return resampled;
+        Stage s {resampled, f.w, f.h};
+        if (dim == 0) {
+            s.w = (s.w + factor - 1)/factor;
+        } else if (dim == 1) {
+            s.h = (s.h + factor - 1)/factor;
+        } else {
+            assert(false);
+        }
+        return s;
     }
 
     // Generate an all-to-all communication in dimension dim.
-    Func all_to_all(Func f, int dim) {
+    Stage all_to_all(Stage f, int dim) {
         std::cout << "All to all on dimension " << dim << std::endl;
 
         // TODO: This just assumes that the extent of the dimension is
         // 3, which is really bad.
-        vector<Expr> reduction_coords = make_arguments(f.args());
+        vector<Expr> reduction_coords = make_arguments(f.func.args());
         Expr e = 0.f;
         for (int i = 0; i < 3; i++) {
             reduction_coords[dim] = i;
-            e += f(reduction_coords) * (i + 1) * (f.args()[dim] + 1);
+            e += f.func(reduction_coords) * (i + 1) * (f.func.args()[dim] + 1);
         }
 
         Func all("all");
-        all(f.args()) = e;
+        all(f.func.args()) = e;
 
-        return all;
+        return {all, f.w, f.h};
     }
 
     // Generate a random stage using f as an input.
-    Func random_stage(Func f) {
+    Stage random_stage(Stage f) {
         int stage_type = rand_int(0, 6);
-        if (stage_type < 6) {
+        if (stage_type < 4) {
             int dim = rand_int(0, 1);
             int kernel_min = rand_int(-5, 0);
             int kernel_max = rand_int(0, 5);
             return convolve(f, dim, kernel_min, kernel_max);
-        } else if (false && stage_type == 4) {
+        } else if (stage_type == 4) {
             // For now, only upsample dimensions 0 or 1.
             int dim = rand_int(0, 1);
-            int factor = rand_int(1, 4);
-            return upsample(f, dim, factor);
-        } else if (false && stage_type == 5) {
+            int factor = 2;
+            if (f.w < 1024 && f.h < 1024) {
+                return upsample(f, dim, factor);
+            } else {
+                return all_to_all(f, 2);
+            }
+        } else if (stage_type == 5) {
             // For now, only downsample dimensions 0 or 1.
             int dim = rand_int(0, 1);
-            int factor = rand_int(1, 4);
-            return downsample(f, dim, factor);
+            int factor = 2;
+            if (f.w > 128 && f.h > 128) {
+                return downsample(f, dim, factor);
+            } else {
+                return all_to_all(f, 2);
+            }
         } else if (stage_type == 6) {
             // TODO: For now, only generate all-to-all communication in dimension 2, which is likely to be small.
             int dim = 2;
             return all_to_all(f, dim);
         } else {
             assert(false);
-            return Func();
+            return {};
         }
     }
 
     void generate() {
         rng.seed((int)seed);
 
-        Func tail = input;
+        // Assume input starts at ~1024x1024
+        Stage tail {input, 1024, 1024};
         for (int i = 0; i < stages; i++) {
-            Func next = random_stage(tail);
+            std::cout << "Approx size: " << tail.w << ", " << tail.h << "\n";
+            Stage next = random_stage(tail);
             if (!auto_schedule) {
-                next.compute_root();
+                next.func.compute_root();
             }
             tail = next;
             if (!auto_schedule) {
-                tail.compute_root().reorder(_0, _2, _1).vectorize(_0, 8).parallel(_1);
+                tail.func.compute_root().reorder(_0, _2, _1).vectorize(_0, 8).parallel(_1);
             }
         }
-        output(tail.args()) = tail(tail.args());
+
+        // Resample back to the correct resolution, or do some other
+        // thing if the resolution is correct.
+        if (tail.w >= 2048) {
+            tail = downsample(tail, 0, tail.w / 1024);
+        } else if (tail.w < 512) {
+            tail = upsample(tail, 0, 1024 / tail.w);
+        } else {
+            tail = all_to_all(tail, 2);
+        }
+
+        if (tail.h >= 2048) {
+            tail = downsample(tail, 1, tail.h / 1024);
+        } else if (tail.h < 512) {
+            tail = upsample(tail, 1, 1024 / tail.h);
+        } else {
+            tail = all_to_all(tail, 2);
+        }
+
+        output(tail.func.args()) = tail.func(tail.func.args());
 
         if (!auto_schedule) {
             output.compute_root().reorder(_0, _2, _1).vectorize(_0, 8).parallel(_1);
