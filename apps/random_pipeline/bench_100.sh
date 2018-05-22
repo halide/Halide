d1e3d96856777aa7e6068999ef5d6fbd4c710659
Andrew Adams <andrew.b.adams@gmail.com>
2018-05-22 14:27:04 -0700

    Various improvements to the random pipeline benchmarker

--- apps/random_pipeline/bench_100.sh
+++ apps/random_pipeline/bench_100.sh
@@ -13,7 +13,7 @@ PIPELINES=100
 SCHEDULES=100
 
 # Build the shared things by building one pipeline
-HL_TARGET=host-new_autoscheduler HL_SEED=root PIPELINE_SEED=0 HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=1 make build
+HL_TARGET=host-new_autoscheduler HL_SEED=root PIPELINE_SEED=0 HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=2 make build
 
 for ((b=0;b<10000000;b++)); do
     echo Batch $b
@@ -24,9 +24,9 @@ for ((b=0;b<10000000;b++)); do
     for ((p=0;p<$PIPELINES;p++)); do
 	P=$((b * $PIPELINES + p))
 	echo echo Building pipeline $P
-	echo "HL_TARGET=host-new_autoscheduler HL_SEED=root PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=1 make build 2>&1 | grep -v Nothing.to.be.done"
+	echo "HL_TARGET=host-new_autoscheduler HL_SEED=root PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=2 make build 2>&1 | grep -v Nothing.to.be.done"
 	for ((s=0;s<$SCHEDULES;s++)); do
-            echo "HL_TARGET=host-new_autoscheduler HL_SEED=$s PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=1 make build 2>&1 | grep -v Nothing.to.be.done"
+            echo "HL_TARGET=host-new_autoscheduler HL_SEED=$s PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=2 make build 2>&1 | grep -v Nothing.to.be.done"
 	done
     done | xargs -P48 -I{} bash -c "{}"
 
@@ -34,13 +34,13 @@ for ((b=0;b<10000000;b++)); do
     for ((p=0;p<$PIPELINES;p++)); do
 	P=$((b * $PIPELINES + p))
 	echo Benchmarking pipeline $P
-	F=bin/host-new_autoscheduler/pipeline_${P}/schedule_root_50_1/times.txt
-	if [ ! -f $F ]; then HL_TARGET=host-new_autoscheduler HL_SEED=root PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=1 make bench 2>&1 | grep -v "Nothing to be done"; fi
+	F=bin/host-new_autoscheduler/pipeline_${P}/schedule_root_50_2/times.txt
+	if [ ! -f $F ]; then HL_TARGET=host-new_autoscheduler HL_SEED=root PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=2 make bench 2>&1 | grep -v "Nothing to be done"; fi
 
 	grep '^Time' $F > /dev/null && echo $F >> results/files_root_${b}.txt
 	for ((s=0;s<$SCHEDULES;s++)); do
-	    F=bin/host-new_autoscheduler/pipeline_${P}/schedule_${s}_50_1/times.txt
-            if [ ! -f $F ]; then HL_TARGET=host-new_autoscheduler HL_SEED=$s PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=1 make bench 2>&1 | grep -v "Nothing to be done"; fi
+	    F=bin/host-new_autoscheduler/pipeline_${P}/schedule_${s}_50_2/times.txt
+            if [ ! -f $F ]; then HL_TARGET=host-new_autoscheduler HL_SEED=$s PIPELINE_SEED=$P HL_RANDOM_DROPOUT=50 HL_BEAM_SIZE=2 make bench 2>&1 | grep -v "Nothing to be done"; fi
 
 	    grep '^Time' $F > /dev/null && echo $F >> results/files_${b}.txt
 	done
@@ -49,6 +49,10 @@ for ((b=0;b<10000000;b++)); do
     # Extract the runtimes
     echo "Extracting runtimes..."
     cat results/files_${b}.txt | while read F; do grep '^Time' $F | cut -d: -f2 | cut -b2-; done > results/runtimes_${b}.txt
+
+    # Extract the number of malloc calls
+    echo "Extracting mallocs..."
+    cat results/files_${b}.txt | while read F; do grep '^Malloc' $F | cut -d: -f2 | cut -b2-; done > results/mallocs_${b}.txt    
 
     # Extract the compute_root runtimes
     echo "Extracting compute_root runtimes..."
@@ -60,7 +64,7 @@ for ((b=0;b<10000000;b++)); do
 
     # Extract failed proofs
     echo "Extracting any failed proofs..."
-    cat results/files_${b}.txt | while read F; do echo $(grep -A1 'Failed to prove' ${F/times/stderr} | grep '^ '); done > results/failed_proofs_${b}.txt    
+    cat results/files_${b}.txt | while read F; do grep -A1 'Failed to prove' ${F/times/stderr}; done > results/failed_proofs_${b}.txt    
 
     # Extract the cost according to the hand-designed model (just the sum of the features)
     echo "Extracting costs..."
--- apps/random_pipeline/test.cpp
+++ apps/random_pipeline/test.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
+#include <atomic>
 
 #include "halide_benchmark.h"
 #include "HalideBuffer.h"
@@ -9,6 +10,21 @@ using namespace Halide::Runtime;
 using namespace Halide::Tools;
 
 #include "random_pipeline.h"
+
+typedef void *(*halide_malloc_t)(void *, size_t);
+typedef void (*halide_free_t)(void *, void *);
+extern halide_malloc_t halide_set_custom_malloc(halide_malloc_t user_malloc);
+extern halide_free_t halide_set_custom_free(halide_free_t user_free);
+
+std::atomic<int64_t> counter;
+size_t peak;
+
+void *my_malloc(void *ucon, size_t sz) {
+    counter++;
+    if (sz > peak) peak = sz;
+
+    return halide_default_malloc(ucon, sz);
+}
 
 int main(int argc, char **argv) {
     Buffer<float> output(1024, 1024, 3);
@@ -25,11 +41,20 @@ int main(int argc, char **argv) {
     assert(input.is_bounds_query());
     random_pipeline(input, output);
     input.allocate();
+    input.fill(0.0f);
 
-    double best = benchmark(3, 3, [&]() {
+    printf("Input size: %d %d %d\n", input.width(), input.height(), input.channels());
+
+    double best = benchmark(10, 10, [&]() {
         random_pipeline(input, output);
     });
     printf("Time: %g\n", best * 1e3);
+
+    counter = 0;
+    peak = 0;
+    halide_set_custom_malloc(my_malloc);
+    random_pipeline(input, output);
+    printf("Mallocs: %lld %lld\n", (long long)counter, (long long)peak);
 
     return 0;
 }
