da7dd5c2d7f68f3512ab9c44100de981207548a2
Andrew Adams <andrew.b.adams@gmail.com>
2018-05-31 10:28:33 -0700

    dag-structured pipelines

--- apps/random_pipeline/random_pipeline_generator.cpp
+++ apps/random_pipeline/random_pipeline_generator.cpp
@@ -23,8 +23,8 @@ class RandomPipeline : public Halide::Generator<RandomPipeline> {
 public:
     // The random seed to use to generate the pipeline.
     GeneratorParam<int> seed{"seed", 1};
-    // The number of stages to generate in the random pipeline.
-    GeneratorParam<int> stages{"stages", 20};
+    // The approximate max number of stages to generate in the random pipeline.
+    GeneratorParam<int> max_stages{"max_stages", 20};
 
     Input<Buffer<float>>  input{"input", 3};
     Output<Buffer<float>> output{"output", 3};
@@ -115,6 +115,12 @@ public:
         return s;
     }
 
+    Stage add(Stage f, Stage g) {
+        Func added("added");
+        added(f.func.args()) = f.func(f.func.args()) + g.func(f.func.args());
+        return {added, f.w, f.h};
+    }
+
     // Generate an all-to-all communication in dimension dim.
     Stage all_to_all(Stage f, int dim) {
         std::cout << "All to all on dimension " << dim << std::endl;
@@ -135,74 +141,83 @@ public:
     }
 
     // Generate a random stage using f as an input.
-    Stage random_stage(Stage f) {
-        int stage_type = rand_int(0, 6);
-        if (stage_type < 4) {
+    Stage random_stage(const vector<Stage> &s) {
+        int m = (int)s.size() - 1;
+        int i2 = m > 0 ? rand_int(0, m - 1) : 0;
+        int i1 = m > 0 ? rand_int(i2 + 1, m) : 0;
+        Stage f = s[i1], g = s[i2];
+        int stage_type = rand_int(0, 7);
+        if (stage_type < 3) {
             int dim = rand_int(0, 1);
             int kernel_min = rand_int(-5, 0);
             int kernel_max = rand_int(0, 5);
             return convolve(f, dim, kernel_min, kernel_max);
-        } else if (stage_type == 4) {
+        } else if (stage_type == 3) {
             // For now, only upsample dimensions 0 or 1.
             int dim = rand_int(0, 1);
             int factor = 2;
             if (f.w < 1024 && f.h < 1024) {
                 return upsample(f, dim, factor);
             } else {
-                return all_to_all(f, 2);
+                return random_stage(s);
             }
-        } else if (stage_type == 5) {
+        } else if (stage_type == 4) {
             // For now, only downsample dimensions 0 or 1.
             int dim = rand_int(0, 1);
             int factor = 2;
             if (f.w > 128 && f.h > 128) {
                 return downsample(f, dim, factor);
             } else {
-                return all_to_all(f, 2);
+                return random_stage(s);
             }
-        } else if (stage_type == 6) {
+        } else if (stage_type == 5) {
             // TODO: For now, only generate all-to-all communication in dimension 2, which is likely to be small.
             int dim = 2;
             return all_to_all(f, dim);
+        } else if (i1 != i2) {
+            // Add two equal resolution stages
+            if (f.w == g.w && f.h == g.h) {
+                return add(f, g);
+            } else {
+                return random_stage(s);
+            }
         } else {
-            assert(false);
-            return {};
+            return random_stage(s);
         }
     }
 
     void generate() {
         rng.seed((int)seed);
 
+        vector<Stage> stages;
         // Assume input starts at ~1024x1024
-        Stage tail {input, 1024, 1024};
-        for (int i = 0; i < stages; i++) {
-            std::cout << "Approx size: " << tail.w << ", " << tail.h << "\n";
-            Stage next = random_stage(tail);
+        stages.emplace_back(Stage{input, 1024, 1024});
+
+        for (int i = 0; i < max_stages - 2; i++) {
+            std::cout << "Approx size: " << stages.back().w << ", " << stages.back().h << "\n";
+            Stage next = random_stage(stages);
             if (!auto_schedule) {
                 next.func.compute_root();
             }
-            tail = next;
+            stages.push_back(next);
             if (!auto_schedule) {
-                tail.func.compute_root().reorder(_0, _2, _1).vectorize(_0, 8).parallel(_1);
+                stages.back().func.compute_root().reorder(_0, _2, _1).vectorize(_0, 8).parallel(_1);
             }
         }
 
-        // Resample back to the correct resolution, or do some other
-        // thing if the resolution is correct.
+        Stage tail = stages.back();
+
+        // Resample back to the correct resolution
         if (tail.w >= 2048) {
             tail = downsample(tail, 0, tail.w / 1024);
         } else if (tail.w < 512) {
             tail = upsample(tail, 0, 1024 / tail.w);
-        } else {
-            tail = all_to_all(tail, 2);
         }
 
         if (tail.h >= 2048) {
             tail = downsample(tail, 1, tail.h / 1024);
         } else if (tail.h < 512) {
             tail = upsample(tail, 1, 1024 / tail.h);
-        } else {
-            tail = all_to_all(tail, 2);
         }
 
         output(tail.func.args()) = tail.func(tail.func.args());
@@ -212,10 +227,6 @@ public:
         }
 
         if (auto_schedule) {
-            // This estimate of the input bounds is unlikely to be accurate.
-            input.dim(0).set_bounds_estimate(0, 1024);
-            input.dim(1).set_bounds_estimate(0, 1024);
-            input.dim(2).set_bounds_estimate(0, 3);
             output.estimate(output.args()[0], 0, 1024);
             output.estimate(output.args()[1], 0, 1024);
             output.estimate(output.args()[2], 0, 3);
