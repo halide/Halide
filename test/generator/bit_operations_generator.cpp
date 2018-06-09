3556b5df888d6e9def1c556e7319ffe6293a2ce5
Matthias Kramm <kramm@google.com>
2018-06-09 11:08:52 -0700

    add tests for 8/16/32/64 bit

--- test/generator/bit_operations_generator.cpp
+++ test/generator/bit_operations_generator.cpp
@@ -4,12 +4,22 @@ namespace {
 
 class BitOperations : public Halide::Generator<BitOperations> {
 public:
-    Input<Buffer<uint64_t>>  input{"input", 1};
-    Output<Buffer<uint8_t>> output{"output", 1};
+    Input<Buffer<uint8_t>> input8{"input8", 1};
+    Input<Buffer<uint16_t>> input16{"input16", 1};
+    Input<Buffer<uint32_t>> input32{"input32", 1};
+    Input<Buffer<uint64_t>> input64{"input64", 1};
+
+    Output<Buffer<uint8_t>> output8{"output8", 1};
+    Output<Buffer<uint8_t>> output16{"output16", 1};
+    Output<Buffer<uint8_t>> output32{"output32", 1};
+    Output<Buffer<uint8_t>> output64{"output64", 1};
 
     void generate() {
         Var x;
-        output(x) = Halide::cast<uint8_t>(count_leading_zeros(input(x)));
+        output8(x) = Halide::cast<uint8_t>(count_leading_zeros(input8(x)));
+        output16(x) = Halide::cast<uint8_t>(count_leading_zeros(input16(x)));
+        output32(x) = Halide::cast<uint8_t>(count_leading_zeros(input32(x)));
+        output64(x) = Halide::cast<uint8_t>(count_leading_zeros(input64(x)));
     }
 };
 
