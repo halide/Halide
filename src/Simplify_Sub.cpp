0e33cb94d6f401898885941bdc2a9f7ad4e33465
Andrew Adams <andrew.b.adams@gmail.com>
2018-07-09 09:24:21 -0700

    Fix incorrect * instead of -

--- src/Simplify_Sub.cpp
+++ src/Simplify_Sub.cpp
@@ -24,10 +24,10 @@ Expr Simplify::visit(const Sub *op, ConstBounds *bounds) {
         const int lanes = op->type.lanes();
 
         if (rewrite(c0 - c1, fold(c0 - c1)) ||
-            rewrite(IRMatcher::Indeterminate() * x, a) ||
-            rewrite(x * IRMatcher::Indeterminate(), b) ||
-            rewrite(IRMatcher::Overflow() * x, a) ||
-            rewrite(x * IRMatcher::Overflow(), b) ||
+            rewrite(IRMatcher::Indeterminate() - x, a) ||
+            rewrite(x - IRMatcher::Indeterminate(), b) ||
+            rewrite(IRMatcher::Overflow() - x, a) ||
+            rewrite(x - IRMatcher::Overflow(), b) ||
             rewrite(x - 0, x)) {
             return rewrite.result;
         }
