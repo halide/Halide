0c6c2ad5795e7a047626ae94a986ba7b370c234e
Pranav Bhandarkar <pranavb@codeaurora.org>
2018-07-24 18:42:20 -0500

    Revert "Revert "Interleave bug""
    This reverts commit 38e5ca1a4f2796a52fc888ab72a281f45018b560.

--- src/HexagonOptimize.h
+++ src/HexagonOptimize.h
@@ -6,7 +6,8 @@
  */
 
 #include "IR.h"
-
+#include "ModulusRemainder.h"
+#include "Scope.h"
 namespace Halide {
 namespace Internal {
 
@@ -22,7 +23,7 @@ Stmt vtmpy_generator(Stmt s);
  * rewrites widenings/narrowings to be explicit in the IR, and
  * attempts to simplify away most of the
  * interleaving/deinterleaving. */
-Stmt optimize_hexagon_instructions(Stmt s, Target t);
+Stmt optimize_hexagon_instructions(Stmt s, Target t, Scope<ModulusRemainder>& alignment_info);
 
 /** Generate deinterleave or interleave operations, operating on
  * groups of vectors at a time. */
