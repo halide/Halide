dedc31efb24166339a2545adf73628f8191b07fe
Andrew Adams <andrew.b.adams@gmail.com>
2018-08-02 14:59:13 -0700

    Clean up padding
    Now done entirely inside the Halide pipeline, and no longer conflated
    with the allocation size

--- src/ThroughputPredictorPipeline.h
+++ src/ThroughputPredictorPipeline.h
@@ -19,7 +19,7 @@ public:
 
     Stats feature_stats;
 
-    Param<int> first_valid_param, num_stages_param;
+    Param<int> num_stages_param;
 
     Buffer<float> head1_filter;
     Buffer<float> head1_bias;
@@ -69,6 +69,8 @@ public:
         filter5(weights.conv5_filter), bias5(weights.conv5_bias),
         filter6(weights.conv6_filter), bias6(weights.conv6_bias) {
 
+        Expr padded_stages = max(num_stages_param, 22);
+        Expr first_valid = max(0, (padded_stages - num_stages_param)/2);
 
         Var c("c"), w("w"), n("n"), i("i"), j("j"), s("s");
 
@@ -98,16 +100,16 @@ public:
         RDom r6(filter6.dim(0).min(), filter6.dim(0).extent());
 
         // reduce over a region that expands to 3x1 convs from the first two stages to the last two stages with zero padding
-        RDom r_reduce(0, ( schedule_features.dim(2).extent() + 6 ) / 4);
+        RDom r_reduce(0, ( padded_stages + 6 ) / 4);
 
         normalized_pipeline_features(n, i, j, s) = 0.0f;
-        RDom r_s(first_valid_param, num_stages_param);
+        RDom r_s(first_valid, num_stages_param);
         normalized_pipeline_features(n, i, j, r_s) =
-            (pipeline_features(n, i, j, r_s - first_valid_param) - feature_stats.pipeline_mean(i, j)) / feature_stats.pipeline_std(i, j);
+            (pipeline_features(n, i, j, r_s - first_valid) - feature_stats.pipeline_mean(i, j)) / feature_stats.pipeline_std(i, j);
 
         normalized_schedule_features(n, i, s) = 0.0f;
         normalized_schedule_features(n, i, r_s) =
-            (fast_log(schedule_features(n, i, r_s - first_valid_param) + 1) - feature_stats.schedule_mean(i)) / feature_stats.schedule_std(i);
+            (fast_log(schedule_features(n, i, r_s - first_valid) + 1) - feature_stats.schedule_mean(i)) / feature_stats.schedule_std(i);
 
         f_head1_conv(n, c, w) = head1_bias(c);
         f_head1_conv(n, c, w) += head1_filter(c, r_head1.x, r_head1.y) * normalized_pipeline_features(n, r_head1.x, r_head1.y, w);
@@ -119,8 +121,8 @@ public:
 
         // we want to enforce boundary conditions on f_head1_relu and f_head2_relu because conv1 pads with 1 zero on either
         // side of the width (i.e. final dimension) of its input. We set the valid range of the width from 0 to num_stages.
-        f_head1_relu_padded = Halide::BoundaryConditions::constant_exterior(f_head1_relu, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()}});
-        f_head2_relu_padded = Halide::BoundaryConditions::constant_exterior(f_head2_relu, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()}});
+        f_head1_relu_padded = Halide::BoundaryConditions::constant_exterior(f_head1_relu, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages}});
+        f_head2_relu_padded = Halide::BoundaryConditions::constant_exterior(f_head2_relu, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages}});
 
         /***** network trunk *****/
         // first 20 input channels are from head1_relu, next 20 input channels are from head2_relu
@@ -133,7 +135,7 @@ public:
         f_ReLU1(n, c, w) = max(0, f_conv1_stage2(n, c, w));
 
         // set boundary conditions for f_ReLU1
-        f_relu1_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU1, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()}});
+        f_relu1_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU1, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages}});
 
         f_conv2(n, c, w) = bias2(c);
         f_conv2(n, c, w) += filter2(c, r2.x, r2.y) * f_relu1_padded(n, r2.x, w+r2.y-1);
@@ -141,45 +143,45 @@ public:
         f_ReLU2(n, c, w) = max(0, f_conv2(n, c, w));
 
         // set boundary conditions for f_ReLU2
-        f_relu2_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU2, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()}});
+        f_relu2_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU2, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages}});
 
         f_conv3(n, c, w) = bias3(c);
         f_conv3(n, c, w) += filter3(c, r3.x, r3.y) * f_relu2_padded(n, r3.x, w+r3.y-1);
         f_ReLU3(n, c, w) = max(0, f_conv3(n, c, w));
 
         // set boundary conditions for f_ReLU3
-        f_relu3_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU3, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()}});
+        f_relu3_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU3, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages}});
 
         f_pool3(n, c, w) = 0.5f * (f_relu3_padded(n, c, w*2-1) + f_relu3_padded(n, c, w*2));
 
         // set boundary conditions for f_pool3
-        f_pool3_padded = Halide::BoundaryConditions::constant_exterior(f_pool3, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()/2 + 1}});
+        f_pool3_padded = Halide::BoundaryConditions::constant_exterior(f_pool3, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages/2 + 1}});
 
         f_conv4(n, c, w) = bias4(c);
         f_conv4(n, c, w) += filter4(c, r4.x, r4.y) * f_pool3_padded(n, r4.x, w+r4.y-1);
         f_ReLU4(n, c, w) = max(0, f_conv4(n, c, w));
 
         // set boundary conditions for f_ReLU4
-        f_relu4_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU4, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, schedule_features.dim(2).extent()/2 + 1}});
+        f_relu4_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU4, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, padded_stages/2 + 1}});
 
         f_pool4(n, c, w) = 0.5f * (f_relu4_padded(n, c, w*2-1) + f_relu4_padded(n, c, w*2));
 
         // set boundary conditions for f_pool4
-        f_pool4_padded = Halide::BoundaryConditions::constant_exterior(f_pool4, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, (schedule_features.dim(2).extent()+6) / 4}});
+        f_pool4_padded = Halide::BoundaryConditions::constant_exterior(f_pool4, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, (padded_stages+6) / 4}});
 
         f_conv5(n, c, w) = bias5(c);
         f_conv5(n, c, w) += filter5(c, r5.x, r5.y) * f_pool4_padded(n, r5.x, w+r5.y-1);
         f_ReLU5(n, c, w) = max(0, f_conv5(n, c, w));
 
         // set boundary conditions for f_ReLU5
-        f_relu5_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU5, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, (schedule_features.dim(2).extent()+6) / 4}});
+        f_relu5_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU5, 0.0f, {{Expr(), Expr()}, {Expr(), Expr()}, {0, (padded_stages+6) / 4}});
 
         f_conv6(n, w) = bias6();
         f_conv6(n, w) += filter6(r6) * f_relu5_padded(n, r6, w);
         f_ReLU6(n, w) = max(0, f_conv6(n, w));
 
         // set boundary conditions for f_ReLU5
-        f_relu6_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU6, 0.0f, {{Expr(), Expr()}, {0, (schedule_features.dim(2).extent()+6) / 4}});
+        f_relu6_padded = Halide::BoundaryConditions::constant_exterior(f_ReLU6, 0.0f, {{Expr(), Expr()}, {0, (padded_stages+6) / 4}});
 
         f_reduce(n) = 0.0f;
         f_reduce(n) += f_relu6_padded(n, r_reduce);
@@ -190,11 +192,6 @@ public:
         // schedule
         Expr batch_size = prediction.output_buffers()[0].dim(0).extent();
         prediction.bound(n, 0, batch_size);
-        // pipeline_features.dim(0).set_bounds(0, batch_size);
-        // schedule_features.dim(0).set_bounds(0, batch_size);
-        Expr pipeline_length = schedule_features.dim(2).extent();
-        pipeline_features.dim(3).set_bounds(0, pipeline_length);
-        schedule_features.dim(2).set_bounds(0, pipeline_length);
 
         Target t = get_jit_target_from_environment();
         normalized_pipeline_features.compute_at(prediction, n)
@@ -227,34 +224,33 @@ public:
         Buffer<float> pipeline_feats(batch_size, 56, 7, 20), schedule_feats(batch_size, 18, 20);
         pipeline_feats.fill(0.0f);
         schedule_feats.fill(0.0f);
-        set_inputs(pipeline_feats, schedule_feats, 0, 20);
+        set_inputs(pipeline_feats, schedule_feats, 20);
         Buffer<float> out(batch_size);
         auto t = Halide::Tools::benchmark([&]() {prediction.realize(out);});
         debug(0) << "Throughput predictor runtime: " << ((t/batch_size) * 1000000) << " us\n";
     }
 
-    void set_inputs(Buffer<float> pipeline_feats, Buffer<float> schedule_feats, int first_valid, int num_stages) {
+    void set_inputs(Buffer<float> pipeline_feats, Buffer<float> schedule_feats, int num_stages) {
         pipeline_features.set(Buffer<float>(*pipeline_feats.raw_buffer()));
         schedule_features.set(Buffer<float>(*schedule_feats.raw_buffer()));
-        first_valid_param.set(first_valid);
         num_stages_param.set(num_stages);
     }
 
-
     Buffer<float> pipeline_feat_queue, schedule_feat_queue;
     std::vector<double *> cost_queue;
-    int cursor, first_valid, num_stages;
+    int cursor, num_stages;
     int enqueue(int ns, Buffer<float> *pipeline_feats, Buffer<float> *schedule_feats, double *cost) {
         num_stages = ns;
-        int min_stages = 22;
-        int padded_stages = std::max(num_stages, min_stages);
-        first_valid = std::max(0, (padded_stages - num_stages)/2);
 
         const int batch_size = 1024;
         if (!pipeline_feat_queue.defined() ||
-            pipeline_feat_queue.dim(3).extent() != padded_stages) {
-            pipeline_feat_queue = Buffer<float>(batch_size, 56, 7, padded_stages);
-            schedule_feat_queue = Buffer<float>(batch_size, 18, padded_stages);
+            num_stages > pipeline_feat_queue.dim(3).extent()) {
+            // Allocate 50% extra space so we only need to reallocate
+            // a logarithmic number of times as the pipeline grows
+            // during scheduling.
+            int alloc_size = num_stages + (num_stages + 1) / 2;
+            pipeline_feat_queue = Buffer<float>(batch_size, 56, 7, alloc_size);
+            schedule_feat_queue = Buffer<float>(batch_size, 18, alloc_size);
             pipeline_feat_queue.fill(0.0f);
             schedule_feat_queue.fill(0.0f);
             cost_queue.clear();
@@ -276,7 +272,7 @@ public:
     void evaluate_costs() {
         if (cursor == 0) return;
 
-        set_inputs(pipeline_feat_queue, schedule_feat_queue, first_valid, num_stages);
+        set_inputs(pipeline_feat_queue, schedule_feat_queue, num_stages);
         Buffer<float> costs = prediction.realize(cursor);
 
         for (int i = 0; i < cursor; i++) {
