5b6538d5ac8374a9dc8446d586d9418a77907d9c
Andrew Adams <andrew.b.adams@gmail.com>
2018-10-15 12:56:14 -0700

    Random pipeline generator sizing tweaks

--- apps/random_pipeline/random_pipeline_generator.cpp
+++ apps/random_pipeline/random_pipeline_generator.cpp
@@ -17,6 +17,25 @@ vector<Expr> make_arguments(vector<Var> vars) {
     return result;
 }
 
+std::mt19937 rng;
+
+// Helpers to generate random values.
+int rand_int(int min, int max) { return (rng() % (max - min + 1)) + min; }
+bool rand_bool() { return rng() % 2 == 0; }
+float rand_float() { return rand_int(0, 1 << 30) / (float)(1 << 30); }
+
+Expr rand_value(Type t) {
+    if (t.is_int()) {
+        return cast(t, rand_int(-128, 127));
+    } else if (t.is_float()) {
+        return cast(t, rand_float());
+    } else {
+        // Shouldn't get here.
+        assert(false);
+        return undef(t);
+    }
+}
+
 // Generator to produce a random pipeline. The generated pipeline will
 // be solely a function of the seed and the number of stages.
 class RandomPipeline : public Halide::Generator<RandomPipeline> {
@@ -25,33 +44,52 @@ public:
     GeneratorParam<int> seed{"seed", 1};
     // The approximate max number of stages to generate in the random pipeline.
     GeneratorParam<int> max_stages{"max_stages", 20};
-    GeneratorParam<int> max_channels{"max_channels", 512};
 
     Input<Buffer<float>>  input{"input", 3};
     Output<Buffer<float>> output{"output", 3};
 
-    std::mt19937 rng;
-
-    // Helpers to generate random values.
-    int rand_int(int min, int max) { return (rng() % (max - min + 1)) + min; }
-    bool rand_bool() { return rng() % 2 == 0; }
-    float rand_float() { return rand_int(0, 1 << 30) / (float)(1 << 30); }
-
-    Expr rand_value(Type t) {
-        if (t.is_int()) {
-            return cast(t, rand_int(-128, 127));
-        } else if (t.is_float()) {
-            return cast(t, rand_float());
-        } else {
-            // Shouldn't get here.
-            assert(false);
-            return undef(t);
-        }
-    }
 
     struct Stage {
         Func func;
         int w, h, c; // approx width and height and channels; TODO: ADD 4TH DIMENSION FOR BATCH SIZE
+
+        static constexpr int max_size = 10000000;
+        static constexpr int min_size = 100;
+
+        int size() const {
+            return w*h*c;
+        }
+
+        bool may_increase_size() const {
+            return size() < max_size;
+        }
+
+        bool may_reduce_size() const {
+            return size() > min_size;
+        }
+
+        int random_size_increase_factor() const {
+            int sz = size();
+            int max_factor = (max_size + sz - 1) / sz;
+            if (max_factor <= 1) return 1;
+            int log_max_factor = std::ceil(std::log(max_factor) / std::log(2));
+            int factor = 1 << rand_int(std::max(1, log_max_factor - 3), log_max_factor);
+            return factor;
+        }
+
+        int random_size_reduce_factor() const {
+            int sz = size();
+            int max_factor = (sz + min_size - 1) / min_size;
+            if (max_factor <= 1) return 1;
+            return std::min(8, 1 << rand_int(1, std::ceil(std::log(max_factor) / std::log(2))));
+        }
+
+        int random_out_channels() const {
+            int min = (min_size + w * h - 1) / (w * h);
+            int max = max_size / (w * h);
+            if (min >= max) return min;
+            return rand_int(min, max);
+        }
     };
 
     // Generate a random convolution of one dimension of f, statically unrolled.
@@ -114,8 +152,7 @@ public:
 
     // 50% chance of returning a pooling stage 50% chance returning 2D convolution
     Stage convolve_or_pool(Stage f, int kernel_min, int kernel_max) {
-        int pool = rand_int(0,1);
-        if (pool && f.w > 20 && f.h > 20) { // don't downsample if image too small
+        if (rand_bool() && f.may_reduce_size()) {
             int pool_type = rand_int(0,2);
             if (pool_type == 0) return pool2D(f);
             if (pool_type == 1) return pool2D_w(f);
@@ -134,16 +171,16 @@ public:
         vector<Var> args = f.func.args();
         Func pooled2D("pooled2D" + args[0].name() + args[1].name());
 
-        int kernel_min = -1;
-        int kernel_max = 1;
-        int factor = 2;
-        int scale = (kernel_max - kernel_min) * (kernel_max - kernel_min);
+        int factor = std::ceil(std::sqrt(f.random_size_reduce_factor()));
+        int min = -(factor+1)/2;
+        int extent = min + factor + 1;
+        int scale = extent * extent;
 
         Expr def = cast(f.func.value().type(), 0);
 
         // assuming input is 3d: w, h, c
-        for (int i = kernel_min; i <= kernel_max; i++) {
-            for (int j = kernel_min; j <= kernel_max; j++) {
+        for (int i = min; i < min + extent; i++) {
+            for (int j = min; j < min + extent; j++) {
                 vector<Expr> pooled_coords = make_arguments(f.func.args());
                 pooled_coords[0] = pooled_coords[0] * factor + i;
                 pooled_coords[1] = pooled_coords[1] * factor + j;
@@ -153,7 +190,7 @@ public:
 
         pooled2D(args) = def;
 
-        return {pooled2D, (f.w - 1)/factor, (f.h - 1)/factor, f.c};
+        return {pooled2D, (f.w + factor - 1) / factor, (f.h + factor - 1) / factor, f.c};
     }
 
     // Generate a 3x3 pool with stride 2 of f using a reduction.
@@ -162,20 +199,19 @@ public:
         vector<Var> args = f.func.args();
         Func pooled2D_r("pool2D_r_" + args[0].name() + args[1].name());
 
-        int kernel_min = -1;
-        int kernel_max = 1;
-        int factor = 2;
-        int scale = (kernel_max - kernel_min) * (kernel_max - kernel_min);
+        int factor = std::ceil(std::sqrt(f.random_size_reduce_factor()));
+        int min = -(factor+1)/2;
+        int extent = min + factor + 1;
+        int scale = extent * extent;
 
-        RDom r(kernel_min, kernel_max - kernel_min + 1,
-               kernel_min, kernel_max - kernel_min + 1);
+        RDom r(min, extent, min, extent);
 
         vector<Expr> coords = make_arguments(f.func.args());
         coords[0] = (coords[0] * factor + r.x) / scale;
         coords[1] = (coords[1] * factor + r.y) / scale;
         pooled2D_r(args) += f.func(coords);
 
-        return {pooled2D_r, (f.w - 1)/factor, (f.h - 1)/factor, f.c};
+        return {pooled2D_r, (f.w + factor - 1) / factor, (f.h + factor - 1) / factor, f.c};
     }
 
     // Generate a 3x3 pool with stride 2 of f using a reduction with a wrapper
@@ -184,20 +220,19 @@ public:
         vector<Var> args = f.func.args();
         Func pooled2D_w("pooled2D_w_" + args[0].name() + args[1].name());
 
-        int kernel_min = -1;
-        int kernel_max = 1;
-        int factor = 2;
-        int scale = (kernel_max - kernel_min) * (kernel_max - kernel_min);
+        int factor = std::ceil(std::sqrt(f.random_size_reduce_factor()));
+        int min = -(factor+1)/2;
+        int extent = min + factor + 1;
+        int scale = extent * extent;
 
-        RDom r(kernel_min, kernel_max - kernel_min + 1,
-               kernel_min, kernel_max - kernel_min + 1);
+        RDom r(min, extent, min, extent);
 
         vector<Expr> coords = make_arguments(f.func.args());
         coords[0] = (coords[0] * factor + r.x) / scale;
         coords[1] = (coords[1] * factor + r.y) / scale;
         pooled2D_w(args) = sum(f.func(coords));
 
-        return {pooled2D_w, (f.w - 1)/factor, (f.h - 1)/factor, f.c};
+        return {pooled2D_w, (f.w + factor - 1) / factor, (f.h + factor - 1) / factor, f.c};
     }
 
     /******* set of 2 dimensional (non separable) convs *********/
@@ -218,7 +253,7 @@ public:
                     vector<Expr> coords = make_arguments(f.func.args());
                     coords[0] += i;
                     coords[1] += j;
-                    coords[2] += c;
+                    coords[2] = c;
                     def = def + rand_value(f.func.value().type()) * f.func(coords);
                 }
             }
@@ -227,9 +262,7 @@ public:
         Func conv("conv2D_" + args[0].name() + args[1].name());
         conv(args) = def;
 
-        // choose a channel output size - 0.5 prob of doubling channel dim
-        int channels_out = min(max_channels, std::max(128, f.c * rand_int(1,2)));
-        return {conv, f.w, f.h, channels_out};
+        return {conv, f.w, f.h, f.random_out_channels()};
     }
 
     // Generate a random convolution of one dimension of f using a reduction.
@@ -248,12 +281,10 @@ public:
         vector<Expr> coords = make_arguments(f.func.args());
         coords[0] += r.x;
         coords[1] += r.y;
-        coords[2] += r.z;
+        coords[2] = r.z;
         conv(args) += rand_value(f.func.value().type()) * f.func(coords);
 
-        // choose a channel output size - 0.5 prob of doubling channel dim
-        int channels_out = min(max_channels, std::max(128, f.c * rand_int(1,2)));
-        return {conv, f.w, f.h, channels_out};
+        return {conv, f.w, f.h, f.random_out_channels()};
     }
 
     // Generate a random convolution of one dimension of f using a reduction with a wrapper
@@ -272,18 +303,19 @@ public:
         vector<Expr> coords = make_arguments(f.func.args());
         coords[0] += r.x;
         coords[1] += r.y;
-        coords[2] += r.z;
+        coords[2] = r.z;
         conv(args) = sum(rand_value(f.func.value().type()) * f.func(coords));
 
         // choose a channel output size - 0.5 prob of doubling channel dim
-        int channels_out = min(max_channels, std::max(128, f.c * rand_int(1,2)));
-        return {conv, f.w, f.h, channels_out};
+        return {conv, f.w, f.h, f.random_out_channels()};
     }
 
 
     // Generate an upsampling or downsampling of dimension dim by factor.
-    Stage upsample(Stage f, int dim, int factor) {
+    Stage upsample(Stage f, int dim, int factor = 0) {
         std::cout << "Upsampling dimension " << dim << " by " << factor << "x\n";
+
+        if (factor == 0) factor = f.random_size_increase_factor();
 
         vector<Expr> resampled_coords = make_arguments(f.func.args());
         resampled_coords[dim] = resampled_coords[dim] / factor;
@@ -302,8 +334,10 @@ public:
         return s;
     }
 
-    Stage downsample(Stage f, int dim, int factor) {
+    Stage downsample(Stage f, int dim, int factor = 0) {
         std::cout << "Downsampling dimension " << dim << " by " << factor << "x\n";
+
+        if (factor == 0) factor = f.random_size_reduce_factor();
 
         vector<Expr> resampled_coords = make_arguments(f.func.args());
         resampled_coords[dim] = resampled_coords[dim] * factor;
@@ -323,6 +357,14 @@ public:
     }
 
     Stage binary_op(Stage f, Stage g) {
+        if (f.w != g.w || f.h != g.h || f.c != g.c) {
+            if (f.size() < g.size()) {
+                f = resample_to(f, g.w, g.h, g.c);
+            } else {
+                g = resample_to(g, f.w, f.h, f.c);
+            }
+        }
+
         Func binary("binary_op");
         int op_type = rand_int(0, 4); // + , -, *, /, %
         if (op_type == 0) {
@@ -341,14 +383,14 @@ public:
             binary(f.func.args()) = f.func(f.func.args()) % g.func(f.func.args());
             std::cout << "Binary op: % \n";
         }
-        return {binary, f.w, f.h, f.c};
+        return {binary, f.w, f.h, std::min(f.c, g.c)};
     }
 
     Stage unary_op(Stage f) {
         Func unary("unary_op");
         vector<Expr> coords = make_arguments(f.func.args());
         int op_type = rand_int(0,3); // exp, log, sqrt, sin
-        
+
         if (op_type == 0) {
             unary(f.func.args()) = exp(f.func(coords));
             std::cout << "Unary op: exp\n";
@@ -379,7 +421,7 @@ public:
         Func all("all");
         all(f.func.args()) = e;
 
-        return {all, f.w, f.h, f.c};
+        return {all, f.w, f.h, f.random_out_channels()};
     }
 
     // Generate an all-to-all communication in dimension dim using an RDom
@@ -392,7 +434,7 @@ public:
         Func all("all_r");
         all(f.func.args()) += f.func(reduction_coords) * r * (f.func.args()[dim] + 1);
 
-        return {all, f.w, f.h, f.c};
+        return {all, f.w, f.h, f.random_out_channels()};
     }
 
     // Generate an all-to-all communication in dimension dim using an RDom with wrapper func
@@ -405,7 +447,7 @@ public:
         Func all("all_w");
         all(f.func.args()) = sum(f.func(reduction_coords) * r * (f.func.args()[dim] + 1));
 
-        return {all, f.w, f.h, f.c};
+        return {all, f.w, f.h, f.random_out_channels()};
      }
 
     // Generate a forwards-then-backwards scan along a dimension
@@ -440,13 +482,37 @@ public:
 
     }
 
+    Stage resample_to(Stage f, int w, int h, int c) {
+        std::cerr << "Resampling from " << f.w << ", " << f.h << ", " << f.c << " to " << w << ", " << h << ", " << c << "\n";
+        Stage out = f;
+        // First decrease any sizes that need decreasing
+        if (out.w > w) {
+            out = downsample(out, 0, out.w / w);
+        }
+        if (out.h > h) {
+            out = downsample(out, 1, out.h / h);
+        }
+        // Adapt channel count with an all-to-all
+        if (out.c != c) {
+            out = all_to_all_r(out, 2);
+        }
+        // Increase any sizes that need increasing
+        if (out.w < w) {
+            out = upsample(out, 0, (w + out.w - 1) / out.w);
+        }
+        if (out.h < h) {
+            out = upsample(out, 0, (h + out.h - 1) / out.h);
+        }
+        return out;
+    }
+
     // Generate a random stage using f as an input.
     Stage random_stage(const vector<Stage> &s) {
         int m = (int)s.size() - 1;
         int i2 = m > 0 ? rand_int(0, m - 1) : 0;
         int i1 = m > 0 ? rand_int(i2 + 1, m) : 0;
         Stage f = s[i1], g = s[i2];
-        int stage_type = rand_int(0, 18);
+        int stage_type = rand_int(0, 20);
         if (stage_type == 0) {
             int dim = rand_int(0, 1);
             int kernel_min = rand_int(-3, 0);
@@ -463,50 +529,24 @@ public:
             int kernel_max = rand_int(0, 10);
             return convolve_w(f, dim, kernel_min, kernel_max);
         } else if (stage_type >= 3 && stage_type <= 10) {
-            int dim1 = rand_int(0, 1);
-            int dim2 = 1 - dim1;
             int kernel_min = rand_int(-3, 0);
             int kernel_max = rand_int(0, 3);
             return convolve_or_pool(f, kernel_min, kernel_max);
-        } else if (stage_type == 11) {
+        } else if (stage_type == 11 && f.may_increase_size()) {
             // For now, only upsample dimensions 0 or 1.
-            int dim = rand_int(0, 1);
-            int factor = 2;
-            if (f.w < 2000 && f.h < 2000) {
-                return upsample(f, dim, factor);
-            } else {
-                return random_stage(s);
-            }
-        } else if (stage_type == 12) {
+            return upsample(f, rand_int(0, 1));
+        } else if (stage_type == 12 && f.may_reduce_size()) {
             // For now, only downsample dimensions 0 or 1.
-            int dim = rand_int(0, 1);
-            int factor = 2;
-            if (f.w > 128 && f.h > 128) {
-                return downsample(f, dim, factor);
-            } else {
-                return random_stage(s);
-            }
+            return downsample(f, rand_int(0, 1));
         } else if (stage_type == 13) {
             int dim = 2;
-            if (f.c <= 3) {
-              return all_to_all(f, dim);
-            } else {
-              return random_stage(s);
-            }
+            return all_to_all(f, dim);
         } else if (stage_type == 14) {
             int dim = 2;
-            if (f.c <= 3) {
-              return all_to_all_r(f, dim);
-            } else {
-              return random_stage(s);
-            }
+            return all_to_all_r(f, dim);
         } else if (stage_type == 15) {
             int dim = 2;
-            if (f.c <= 3) {
-              return all_to_all_w(f, dim);
-            } else {
-              return random_stage(s);
-            }
+            return all_to_all_w(f, dim);
         } else if (stage_type == 16) {
             int dim = rand_int(0, 2);
             return scan(f, dim);
@@ -516,12 +556,7 @@ public:
         } else if (stage_type == 18) {
             return unary_op(f);
         } else if (i1 != i2) {
-            // binary op two equal resolution stages
-            if (f.w == g.w && f.h == g.h) {
-                return binary_op(f, g);
-            } else {
-                return random_stage(s);
-            }
+            return binary_op(f, g);
         } else {
             return random_stage(s);
         }
@@ -540,7 +575,7 @@ public:
         stages.emplace_back(Stage{first, 2000, 2000, 3});
 
         for (int i = 0; i < max_stages - 2; i++) {
-            std::cout << "Approx size: " << stages.back().w << ", " << stages.back().h << "\n";
+            std::cout << "Approx size: " << stages.back().w << ", " << stages.back().h << ", " << stages.back().c << "\n";
             Stage next = random_stage(stages);
             stages.push_back(next);
             if (!auto_schedule) {
@@ -551,19 +586,9 @@ public:
         Stage tail = stages.back();
 
         // Resample back to the correct resolution
-        if (tail.w >= 2048) {
-            tail = downsample(tail, 0, tail.w / 2000);
-        } else if (tail.w < 512) {
-            tail = upsample(tail, 0, 2000 / tail.w);
-        }
+        tail = resample_to(tail, 2000, 2000, 3);
 
-        if (tail.h >= 2048) {
-            tail = downsample(tail, 1, tail.h / 2000);
-        } else if (tail.h < 512) { // KCMA: does this make sense for conv outputs?
-            tail = upsample(tail, 1, 2000 / tail.h);
-        }
-
-        output(tail.func.args()) = tail.func(tail.func.args());
+        output = tail.func;
 
         if (!auto_schedule) {
             output.compute_root().reorder(x, c, y).vectorize(x, 8).parallel(y);
