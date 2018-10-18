f956d42f34e85e3d133f716d932acb8a3c5b62ab
Andrew Adams <andrew.b.adams@gmail.com>
2018-10-18 09:09:09 -0700

    Don't make huge static stack allocations out of small dynamic stack allocations

--- src/BoundSmallAllocations.cpp
+++ src/BoundSmallAllocations.cpp
@@ -2,6 +2,7 @@
 #include "Bounds.h"
 #include "IRMutator.h"
 #include "Simplify.h"
+#include "CodeGen_Internal.h"
 
 namespace Halide {
 namespace Internal {
@@ -54,18 +55,24 @@ class BoundSmallAllocations : public IRMutator2 {
             << "Allocation " << op->name << " has a dynamic size. "
             << "Only fixed-size allocations are supported on the gpu. "
             << "Try storing into shared memory instead.";
+
+        if (bound.defined()) {
+            bound = simplify(cast<int32_t>(bound));
+        }
+        const int64_t *size_ptr = bound.defined() ? as_const_int(bound) : nullptr;
+        int64_t size = size_ptr ? *size_ptr : 0;
+
         // 128 bytes is a typical minimum allocation size in
         // halide_malloc. For now we are very conservative, and only
         // round sizes up to a constant if they're smaller than that.
-        Expr malloc_overhead = 128 / op->type.bytes();
-        if (bound.defined() &&
+        int malloc_overhead = 128 / op->type.bytes();
+        if (size_ptr &&
             (in_thread_loop ||
-             op->memory_type == MemoryType::Stack ||
+             (op->memory_type == MemoryType::Stack && can_allocation_fit_on_stack(size)) ||
              op->memory_type == MemoryType::Register ||
-             (op->memory_type == MemoryType::Auto && can_prove(bound <= malloc_overhead)))) {
-            user_assert(can_prove(bound <= Int(32).max()))
+             (op->memory_type == MemoryType::Auto && size <= malloc_overhead))) {
+            user_assert(size < (int64_t)1 << 31)
                 << "Allocation " << op->name << " has a size greater than 2^31: " << bound << "\n";
-            bound = simplify(cast<int32_t>(bound));
             return Allocate::make(op->name, op->type, op->memory_type, {bound}, op->condition,
                                   mutate(op->body), op->new_expr, op->free_function);
         } else {
