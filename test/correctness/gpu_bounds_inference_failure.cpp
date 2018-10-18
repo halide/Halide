d5c2b9dafd9ba69c8d3510679e215f728d7b208e
Shoaib Kamil <kamil@adobe.com>
2018-10-18 10:38:05 -0400

    Actually return & fix indentation.

--- test/correctness/gpu_bounds_inference_failure.cpp
+++ test/correctness/gpu_bounds_inference_failure.cpp
@@ -6,37 +6,38 @@ using namespace Halide;
 
 int main(int argc, char *argv[]) {
 
-  if (!t.has_feature(Target::CUDA)) {
-    printf("Not running test because cuda not enabled\n");
-  }
+    if (!t.has_feature(Target::CUDA)) {
+        printf("Not running test because cuda not enabled\n");
+        return 0;
+    }
 
-  Var x, y, p, d;
+    Var x, y, p, d;
 
-  Func f1, f2;
-  f1(x, y, p) = 0;
-  f2(x, y, p) = 0;
+    Func f1, f2;
+    f1(x, y, p) = 0;
+    f2(x, y, p) = 0;
 
-  RDom r(0, 10, 0, 10);
+    RDom r(0, 10, 0, 10);
 
-  Func b1, b2;
-  b1(p) = 0.f;
-  b1(p) += f1(r.x, r.y, p);
+    Func b1, b2;
+    b1(p) = 0.f;
+    b1(p) += f1(r.x, r.y, p);
 
-  b2(p) = 0.f;
-  b2(p) += f2(r.x, r.y, p);
+    b2(p) = 0.f;
+    b2(p) += f2(r.x, r.y, p);
 
-  Func d1, d2;
-  d1(p) = b1(p) - b2(p);
-  d2(p) = b2(p) - b1(p);
+    Func d1, d2;
+    d1(p) = b1(p) - b2(p);
+    d2(p) = b2(p) - b1(p);
 
-  Func result;
-  result(d, p) = select(d == 0, d1(p), d2(p));
+    Func result;
+    result(d, p) = select(d == 0, d1(p), d2(p));
 
-  d2.compute_root().gpu_blocks(p);
+    d2.compute_root().gpu_blocks(p);
 
-  // this used to cause an assertion error
-  result.compile_jit(Target("host-cuda"));
+    // this used to cause an assertion error
+    result.compile_jit(Target("host-cuda"));
 
-  printf("Success!\n");
-  return 0;
+    printf("Success!\n");
+    return 0;
 }
