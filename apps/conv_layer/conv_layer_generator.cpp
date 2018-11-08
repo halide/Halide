1d5c5bebbe8af8bb2f413a50352d8d1a2d64600d
Andrew Adams <andrew.b.adams@gmail.com>
2018-11-08 11:53:01 -0800

    Add conv layer MKL comparison

--- apps/conv_layer/conv_layer_generator.cpp
+++ apps/conv_layer/conv_layer_generator.cpp
@@ -15,31 +15,62 @@ public:
     void generate() {
         /* THE ALGORITHM */
 
-        Var x("x"), y("y"), z("z"), n("n");
+        Var x("x"), y("y"), c("c"), n("n");
 
         Func f_conv("conv");
-        RDom r(filter.dim(0).min(), filter.dim(0).extent(),
-               filter.dim(1).min(), filter.dim(1).extent(),
-               filter.dim(2).min(), filter.dim(2).extent());
+        RDom r(0, 64, 0, 3, 0, 3);
 
-        f_conv(x, y, z, n) = bias(z);
+        f_conv(c, x, y, n) = bias(c);
 
-        f_conv(x, y, z, n) += filter(r.x, r.y, r.z, z) * input(x + r.x, y + r.y, r.z, n);
+        f_conv(c, x, y, n) += filter(c, r.y, r.z, r.x) * input(r.x, x + r.y, y + r.z, n);
 
-        f_ReLU(x, y, z, n) = max(0, f_conv(x, y, z, n));
+        f_ReLU(c, x, y, n) = max(0, f_conv(c, x, y, n));
+
+        /*
+        Func f_conv("conv");
+        RDom r(0, 64, 0, 3, 0, 3);
+
+        f_conv(x, y, c, n) = bias(c);
+
+        f_conv(x, y, c, n) += filter(r.y, r.z, r.x, c) * input(x + r.y, y + r.z, r.x, n);
+
+        f_ReLU(x, y, c, n) = max(0, f_conv(x, y, c, n));
+        */
 
         /* THE SCHEDULE */
 
+        f_ReLU.bound(x, 0, 128)
+            .bound(y, 0, 128)
+            .bound(c, 0, 64)
+            .bound(n, 0, 4);
+
+        f_ReLU.dim(0).set_bounds(0, 64).set_stride(1)
+            .dim(1).set_bounds(0, 128).set_stride(64)
+            .dim(2).set_bounds(0, 128).set_stride(64 * 128)
+            .dim(3).set_bounds(0, 4).set_stride(64 * 128 * 128);
+
+        input.dim(0).set_bounds(0, 64).set_stride(1)
+            .dim(1).set_bounds(0, 130).set_stride(64)
+            .dim(2).set_bounds(0, 130).set_stride(64 * 130)
+            .dim(3).set_bounds(0, 4).set_stride(64 * 130 * 130);
+
+        filter.dim(0).set_bounds(0, 64).set_stride(1)
+            .dim(1).set_bounds(0, 3).set_stride(64)
+            .dim(2).set_bounds(0, 3).set_stride(64 * 3)
+            .dim(3).set_bounds(0, 64).set_stride(64 * 3 * 3);
+
+        bias.dim(0).set_bounds(0, 64).set_stride(1);
+
         if (auto_schedule) {
             // Provide estimates on the input image
-            input.dim(0).set_bounds_estimate(0, 131);
-            input.dim(1).set_bounds_estimate(0, 131);
-            input.dim(2).set_bounds_estimate(0, 64);
+            input.dim(0).set_bounds_estimate(0, 64);
+            input.dim(1).set_bounds_estimate(0, 128 + 2);
+            input.dim(2).set_bounds_estimate(0, 128 + 2);
             input.dim(3).set_bounds_estimate(0, 4);
 
-            filter.dim(0).set_bounds_estimate(0, 3);
+            filter.dim(0).set_bounds_estimate(0, 64);
             filter.dim(1).set_bounds_estimate(0, 3);
-            filter.dim(2).set_bounds_estimate(0, 64);
+            filter.dim(2).set_bounds_estimate(0, 3);
             filter.dim(3).set_bounds_estimate(0, 64);
 
             bias.dim(0).set_bounds_estimate(0, 64);
@@ -47,49 +78,67 @@ public:
             // Provide estimates on the pipeline f_ReLU
             f_ReLU.estimate(x, 0, 128)
                 .estimate(y, 0, 128)
-                .estimate(z, 0, 64)
+                .estimate(c, 0, 64)
                 .estimate(n, 0, 4);
+
+
 
         } /*else if (get_target().has_gpu_feature()) {
             // TODO: Turn off the manual GPU schedule for now.
             // For some reasons, it sometimes triggers the (err == CL_SUCCESS)
             // assertion on mingw.
-            Var ni, no, xi, xo, yi, yo, zi, zo;
+            Var ni, no, xi, xo, yi, yo, ci, co;
             f_ReLU.compute_root()
                 .split(x, xo, xi, 4)
                 .split(y, yo, yi, 4)
-                .split(z, zo, zi, 4)
-                .reorder(xi, yi, zi, n, xo, yo, zo)
-                .gpu_threads(xi, yi, zi)
-                .gpu_blocks(xo, yo, zo);
+                .split(c, co, ci, 4)
+                .reorder(xi, yi, ci, n, xo, yo, co)
+                .gpu_threads(xi, yi, ci)
+                .gpu_blocks(xo, yo, co);
 
             f_conv.compute_at(f_ReLU, n)
-                .gpu_threads(x, y, z)
+                .gpu_threads(x, y, c)
                 .update()
                 .unroll(r.x, 3)
                 .unroll(r.y, 3)
-                .gpu_threads(x, y, z);
+                .gpu_threads(x, y, c);
         }*/ else {
-            // Blocking spatially with vectorization
-            Var z_t("z_t"), y_t("y_t"), par("par");
-            int vec_len = 8;
-            int o_block_size = 32;
-            int y_block = 32;
-            f_conv.compute_root();
-            f_conv.fuse(z, n, par).parallel(par);
-            f_conv.update()
-                .reorder(x, y, r.z)
-                .split(y, y, y_t, y_block)
-                .split(z, z, z_t, o_block_size)
-                .reorder(y_t, z_t, y, r.z, z)
-                .vectorize(x, vec_len)
-                .unroll(r.x, 3)
-                .unroll(r.y, 3)
-                .fuse(z, n, par)
-                .parallel(par);
-            f_ReLU.reorder(n, z).parallel(z).vectorize(x, 8);
+
+            // Best schedule
+            Var co, ci, xo, xi, yo, yi, t;
+            f_ReLU.split(c, co, ci, 16)
+                .split(x, xo, xi, 3)
+                .split(y, yo, yi, 2)
+                .reorder(ci, xi, yi, xo, yo, n, co)
+                .vectorize(ci).unroll(yi).unroll(xi)
+                .parallel(yo).parallel(n).parallel(co);
+            f_conv.compute_at(f_ReLU, xo)
+                .vectorize(c).unroll(x).unroll(y).update()
+                .reorder(c, x, y, r.x, r.y, r.z, n)
+                .vectorize(c).unroll(x).unroll(y).unroll(r.x, 2);
+            filter.in().compute_at(f_conv, r.x).vectorize(_0);
+
+            /* MKL uses the schedule below, but LLVM won't register-allocate it properly
+            Var co, ci, cio, cii, xo, xi, yo, yi, t;
+            f_ReLU.split(c, co, ci, 32).split(ci, cio, cii, 8)
+                .split(x, xo, xi, 3)
+                .reorder(cii, xi, cio, xo, y, co, n)
+                .vectorize(cii).unroll(cio).unroll(xi)
+                .fuse(n, co, t).parallel(t);
+            RVar rxo, rxi, ryo, ryi;
+            f_conv.compute_at(f_ReLU, xo)
+                .vectorize(c, 8).unroll(c).unroll(x).unroll(y)
+                .update()
+                .split(r.x, rxo, rxi, 8)
+                .split(c, co, ci, 8)
+                .reorder(ci, x, co, rxi, r.y, rxo, r.z, y, n)
+                .vectorize(ci).unroll(x).unroll(co).unroll(rxi).unroll(r.y);
+            input.in().compute_at(f_conv, rxi).unroll(_1);
+            filter.in().compute_at(f_conv, co).vectorize(_0).unroll(_3);
+            */
+
         }
-   }
+    }
 };
 
 }  // namespace
--- apps/conv_layer/process.cpp
+++ apps/conv_layer/process.cpp
@@ -1,6 +1,5 @@
 #include <cstdio>
 #include <chrono>
-
 #include "conv_layer.h"
 #include "conv_layer_auto_schedule.h"
 
@@ -10,52 +9,150 @@
 using namespace Halide::Tools;
 using namespace Halide::Runtime;
 
+// Stuff for the MKL example
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <mkldnn.hpp>
+using namespace mkldnn;
+
 int main(int argc, char **argv) {
-    Buffer<float> input(131, 131, 64, 4);
+
+    Buffer<float> input(64, 128+2, 128+2, 4);
+    Buffer<float> filter(64, 3, 3, 64);
+    Buffer<float> bias(64);
+
+    input.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
+    filter.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
+    bias.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
+
+    Buffer<float> output(64, 128, 128, 4);
+
+    /*
+    Buffer<float> input(128+2, 128+2, 64, 4);
     Buffer<float> filter(3, 3, 64, 64);
     Buffer<float> bias(64);
 
-    for (int c = 0; c < input.dim(3).extent(); c++) {
-        for (int z = 0; z < input.channels(); z++) {
-            for (int y = 0; y < input.height(); y++) {
-                for (int x = 0; x < input.width(); x++) {
-                    input(x, y) = rand();
-                }
-            }
-        }
-    }
-
-    for (int c = 0; c < filter.dim(3).extent(); c++) {
-        for (int z = 0; z < filter.channels(); z++) {
-            for (int y = 0; y < filter.height(); y++) {
-                for (int x = 0; x < filter.width(); x++) {
-                    filter(x, y) = rand();
-                }
-            }
-        }
-    }
-
-    for (int x = 0; x < bias.width(); x++) {
-        bias(x) = rand();
-    }
+    input.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
+    filter.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
+    bias.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
 
     Buffer<float> output(128, 128, 64, 4);
+    */
 
-    conv_layer(input, filter, bias, output);
+    // conv_layer(input, filter, bias, output);
 
     // Timing code
 
     // Manually-tuned version
-    double min_t_manual = benchmark(10, 10, [&]() {
+    double min_t_manual = benchmark(20, 20, [&]() {
         conv_layer(input, filter, bias, output);
     });
     printf("Manually-tuned time: %gms\n", min_t_manual * 1e3);
 
     // Auto-scheduled version
-    double min_t_auto = benchmark(10, 10, [&]() {
+    double min_t_auto = benchmark(20, 20, [&]() {
         conv_layer_auto_schedule(input, filter, bias, output);
     });
     printf("Auto-scheduled time: %gms\n", min_t_auto * 1e3);
+
+    // MKL (adapted from https://github.com/intel/mkl-dnn/blob/master/examples/simple_net.cpp)
+    try {
+        auto cpu_engine = engine(engine::cpu, 0);
+
+        /* Create a vector primitive to hold the network. For efficienty purpose,
+         * weights are stored in a separate net to perform reordering only once. */
+        std::vector<primitive> net;
+        std::vector<primitive> net_weights;
+
+        memory::dims conv1_src_tz = { 4, 64, 128+2, 128+2 };
+        memory::dims conv1_weights_tz = { 64, 64, 3, 3 };
+        memory::dims conv1_bias_tz = { 64 };
+        memory::dims conv1_dst_tz = { 4, 64, 128, 128 };
+        memory::dims conv1_strides = { 1, 1 };
+        auto conv1_padding = { 0, 0 };
+
+        /* create memory for user data */
+        auto user_src_memory
+            = memory({ { { conv1_src_tz }, memory::data_type::f32, memory::format::nchw }, cpu_engine }, input.data());
+        auto user_weights_memory
+            = memory({ { { conv1_weights_tz }, memory::data_type::f32, memory::format::oihw }, cpu_engine }, filter.data());
+        auto user_bias_memory = memory({ { { conv1_bias_tz }, memory::data_type::f32, memory::format::x }, cpu_engine }, bias.data());
+
+        auto user_dst_memory = memory({ { { conv1_dst_tz }, memory::data_type::f32,
+                                                                memory::format::nchw },
+                    cpu_engine },
+            output.data());
+
+        /* create memory descriptors for convolution data w/ no specified format
+         */
+        auto conv1_src_md = memory::desc({ conv1_src_tz }, memory::data_type::f32, memory::format::any);
+        auto conv1_bias_md = memory::desc({ conv1_bias_tz }, memory::data_type::f32, memory::format::any);
+        auto conv1_weights_md = memory::desc({ conv1_weights_tz }, memory::data_type::f32, memory::format::any);
+        auto conv1_dst_md = memory::desc({ conv1_dst_tz }, memory::data_type::f32, memory::format::any);
+
+        /* create a convolution */
+        auto conv1_desc = convolution_forward::desc(
+                                                    prop_kind::forward_inference, convolution_direct, conv1_src_md,
+                                                    conv1_weights_md, conv1_bias_md, conv1_dst_md, conv1_strides,
+                                                    conv1_padding, conv1_padding, padding_kind::zero);
+        auto conv1_prim_desc
+            = convolution_forward::primitive_desc(conv1_desc, cpu_engine);
+
+        auto conv1_src_memory = user_src_memory;
+        if (memory::primitive_desc(conv1_prim_desc.src_primitive_desc())
+            != user_src_memory.get_primitive_desc()) {
+            conv1_src_memory = memory(conv1_prim_desc.src_primitive_desc());
+            net.push_back(reorder(user_src_memory, conv1_src_memory));
+        }
+
+        auto conv1_weights_memory = user_weights_memory;
+        if (memory::primitive_desc(conv1_prim_desc.weights_primitive_desc())
+            != user_weights_memory.get_primitive_desc()) {
+            conv1_weights_memory
+                = memory(conv1_prim_desc.weights_primitive_desc());
+            net_weights.push_back(
+                                  reorder(user_weights_memory, conv1_weights_memory));
+        }
+
+        auto conv1_dst_memory = memory(conv1_prim_desc.dst_primitive_desc());
+
+        /* create convolution primitive and add it to net */
+        net.push_back(convolution_forward(conv1_prim_desc, conv1_src_memory,
+                                          conv1_weights_memory, user_bias_memory,
+                                          conv1_dst_memory));
+
+        // A ReLU *should* be free, but in MKL it's apparently
+        // not. Switch to 0 below to check performance without the
+        // relu
+        #if 1
+        const float negative1_slope = 1.0f;
+
+        /* create relu primitive and add it to net */
+        auto relu1_desc = eltwise_forward::desc(prop_kind::forward_inference,
+                                                algorithm::eltwise_relu,
+                                                conv1_dst_memory.get_primitive_desc().desc(), negative1_slope);
+        auto relu1_prim_desc
+            = eltwise_forward::primitive_desc(relu1_desc, cpu_engine);
+
+        net.push_back(eltwise_forward(
+                                      relu1_prim_desc, conv1_dst_memory, conv1_dst_memory));
+
+        auto relu1_dst_memory = memory(relu1_prim_desc.dst_primitive_desc());
+
+        if (relu1_dst_memory != user_dst_memory) {
+            net.push_back(reorder(relu1_dst_memory, user_dst_memory));
+        }
+        #endif
+
+        stream(stream::kind::eager).submit(net_weights).wait();
+        double mkl_time = benchmark(20, 20, [&]() {
+                stream(stream::kind::eager).submit(net).wait();
+            });
+        printf("MKL time: %gms\n", mkl_time * 1e3);
+    } catch (const mkldnn::error &e) {
+        std::cerr << e.message << "\n";
+    }
 
     return 0;
 }
