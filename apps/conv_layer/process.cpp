22e2080bcea390591e1187eb02f9f5cca4ad982e
Andrew Adams <andrew.b.adams@gmail.com>
2018-11-15 10:16:03 -0800

    Conv layer generator refactoring

--- apps/conv_layer/process.cpp
+++ apps/conv_layer/process.cpp
@@ -18,27 +18,18 @@ using namespace mkldnn;
 
 int main(int argc, char **argv) {
 
-    Buffer<float> input(64, 128+2, 128+2, 4);
-    Buffer<float> filter(64, 3, 3, 64);
-    Buffer<float> bias(64);
+    // const int W = 128, H = 128, N = 4, CI = 64, CO = 64;
+    const int N = 5, CI = 120, CO = 24, W = 100, H = 80;
+
+    Buffer<float> input(CI, W+2, H+2, N);
+    Buffer<float> filter(CO, 3, 3, CI);
+    Buffer<float> bias(CO);
 
     input.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
     filter.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
     bias.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
 
-    Buffer<float> output(64, 128, 128, 4);
-
-    /*
-    Buffer<float> input(128+2, 128+2, 64, 4);
-    Buffer<float> filter(3, 3, 64, 64);
-    Buffer<float> bias(64);
-
-    input.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
-    filter.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
-    bias.for_each_value([&](float &f) {f = (double)rand()/RAND_MAX;});
-
-    Buffer<float> output(128, 128, 64, 4);
-    */
+    Buffer<float> output(CO, W, H, N);
 
     // conv_layer(input, filter, bias, output);
 
@@ -60,27 +51,26 @@ int main(int argc, char **argv) {
     try {
         auto cpu_engine = engine(engine::cpu, 0);
 
-        /* Create a vector primitive to hold the network. For efficienty purpose,
-         * weights are stored in a separate net to perform reordering only once. */
         std::vector<primitive> net;
         std::vector<primitive> net_weights;
 
-        memory::dims conv1_src_tz = { 4, 64, 128+2, 128+2 };
-        memory::dims conv1_weights_tz = { 64, 64, 3, 3 };
-        memory::dims conv1_bias_tz = { 64 };
-        memory::dims conv1_dst_tz = { 4, 64, 128, 128 };
+        memory::dims conv1_src_tz = { N, CI, H+2, W+2 };
+        memory::dims conv1_weights_tz = { CO, CI, 3, 3 };
+        memory::dims conv1_bias_tz = { CO };
+        memory::dims conv1_dst_tz = { N, CO, H, W };
         memory::dims conv1_strides = { 1, 1 };
         auto conv1_padding = { 0, 0 };
 
         /* create memory for user data */
         auto user_src_memory
-            = memory({ { { conv1_src_tz }, memory::data_type::f32, memory::format::nchw }, cpu_engine }, input.data());
+            = memory({ { { conv1_src_tz }, memory::data_type::f32, memory::format::nhwc }, cpu_engine }, input.data());
+        // mkl doesn't offer ihwo, but it swizzles the weights internally on first run, so it shouldn't matter.
         auto user_weights_memory
             = memory({ { { conv1_weights_tz }, memory::data_type::f32, memory::format::oihw }, cpu_engine }, filter.data());
         auto user_bias_memory = memory({ { { conv1_bias_tz }, memory::data_type::f32, memory::format::x }, cpu_engine }, bias.data());
 
         auto user_dst_memory = memory({ { { conv1_dst_tz }, memory::data_type::f32,
-                                                                memory::format::nchw },
+                                                                memory::format::nhwc },
                     cpu_engine },
             output.data());
 
@@ -96,8 +86,12 @@ int main(int argc, char **argv) {
                                                     prop_kind::forward_inference, convolution_direct, conv1_src_md,
                                                     conv1_weights_md, conv1_bias_md, conv1_dst_md, conv1_strides,
                                                     conv1_padding, conv1_padding, padding_kind::zero);
+
+        // Create a fused conv + relu
+        auto conv1_relu_desc = convolution_relu_forward::desc(conv1_desc, 0.0f);
+
         auto conv1_prim_desc
-            = convolution_forward::primitive_desc(conv1_desc, cpu_engine);
+            = convolution_relu_forward::primitive_desc(conv1_relu_desc, cpu_engine);
 
         auto conv1_src_memory = user_src_memory;
         if (memory::primitive_desc(conv1_prim_desc.src_primitive_desc())
@@ -118,14 +112,14 @@ int main(int argc, char **argv) {
         auto conv1_dst_memory = memory(conv1_prim_desc.dst_primitive_desc());
 
         /* create convolution primitive and add it to net */
-        net.push_back(convolution_forward(conv1_prim_desc, conv1_src_memory,
+        net.push_back(convolution_relu_forward(conv1_prim_desc, conv1_src_memory,
                                           conv1_weights_memory, user_bias_memory,
                                           conv1_dst_memory));
 
-        // A ReLU *should* be free, but in MKL it's apparently
-        // not. Switch to 0 below to check performance without the
-        // relu
-        #if 1
+        // A separate ReLU *should* be free, but in MKL it's
+        // apparently not. Switch to 0 below to check performance
+        // without the relu
+        #if 0
         const float negative1_slope = 1.0f;
 
         /* create relu primitive and add it to net */
