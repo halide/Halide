75f9b6e31c288c63b9e4f91c6b4e22d925c0bce8
Steven Johnson <srj@google.com>
2018-11-19 11:32:36 -0800

    Use static const int instead of enum

--- src/runtime/HalideRuntime.h
+++ src/runtime/HalideRuntime.h
@@ -1628,7 +1628,7 @@ struct halide_filter_argument_t {
 };
 
 struct halide_filter_metadata_t {
-    enum Version { VERSION = 1 };
+    static const int32_t VERSION = 1;
 
     /** version of this metadata; currently always 1. */
     int32_t version;
