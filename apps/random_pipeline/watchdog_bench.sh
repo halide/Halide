64b6fc310867aeb2a7101e1f643c7bf93f2f77e0
Andrew Adams <andrew.b.adams@gmail.com>
2018-12-17 16:48:37 -0800

    Reduce timeout on watchdog_bench

--- apps/random_pipeline/watchdog_bench.sh
+++ apps/random_pipeline/watchdog_bench.sh
@@ -1,7 +1,7 @@
 echo "Starting watchdog"
 while [ 1 ]; do
     sleep 10
-    ps -ax | grep '[^ 01].:.. .*bench' | grep -v grep | cut -dp -f1 | cut -d? -f1 | xargs kill 2>/dev/null
-    ps -ax | grep '[^ 01].:.. .*random_pipeline.*' | grep -v grep | cut -dp -f1 | cut -d? -f1 | xargs kill 2>/dev/null    
+    ps -ax | grep '[^ ].:.. .*bench' | grep -v grep | cut -dp -f1 | cut -d? -f1 | xargs kill 2>/dev/null
+    ps -ax | grep '[^ ].:.. .*random_pipeline.*' | grep -v grep | cut -dp -f1 | cut -d? -f1 | xargs kill 2>/dev/null    
 done
 
