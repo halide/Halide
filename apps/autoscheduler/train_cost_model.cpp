ad5bb1fcbb6f7ce1bd066686612a76fee023ddd3
Andrew Adams <andrew.b.adams@gmail.com>
2018-12-19 15:44:03 -0800

    Comment out robust fitting hack

--- apps/autoscheduler/train_cost_model.cpp
+++ apps/autoscheduler/train_cost_model.cpp
@@ -402,7 +402,7 @@ int main(int argc, char **argv) {
             if (models > 1) std::cout << "\n";
             if (samples.count(worst_miss_pipeline_id)) {
                 std::cout << " Worst: " << worst_miss << " " << samples[worst_miss_pipeline_id].schedules[worst_miss_schedule_id].filename << "\n";
-                samples[worst_miss_pipeline_id].schedules.erase(worst_miss_schedule_id);
+                // samples[worst_miss_pipeline_id].schedules.erase(worst_miss_schedule_id);
             } else {
                 std::cout << "\n";
             }
