a33e120ffba3c6d9dedd88ba00857e9f191eec43
Andrew Adams <andrew.b.adams@gmail.com>
2018-12-26 13:15:40 -0800

    Correct off-by-one bug

--- apps/autoscheduler/cost_model_generator.cpp
+++ apps/autoscheduler/cost_model_generator.cpp
@@ -300,20 +300,20 @@ public:
         store_cost += cost_of_page_faults;
 
         // Malloc aint free. Small allocations should go on the stack, but this isn't totally reliable.
-        Expr cost_of_malloc = relu1(23, w, n) * num_realizations;
+        Expr cost_of_malloc = relu1(24, w, n) * num_realizations;
 
         // Penalize working sets that start to fall out of cache
         // Expr cost_of_working_set = working_set *
 
-        Expr cost_of_parallel_launches = num_productions * select(inner_parallelism > 1, relu1(24, w, n), 0.0f);
+        Expr cost_of_parallel_launches = num_productions * select(inner_parallelism > 1, relu1(25, w, n), 0.0f);
 
-        Expr cost_of_parallel_tasks = num_productions * (inner_parallelism - 1) * relu1(25, w, n);
+        Expr cost_of_parallel_tasks = num_productions * (inner_parallelism - 1) * relu1(26, w, n);
 
         Expr cost_of_parallelism = cost_of_parallel_tasks + cost_of_parallel_launches;
 
         Expr cost = compute_cost + store_cost + load_cost + store_cost + cost_of_malloc + cost_of_parallelism;
 
-        // Keep the schedule fixed by adding a dependence to all 24 out channels
+        // Keep the schedule fixed by adding a dependence to all out channels
         for (int i = 0; i < conv1_channels; i++) {
             cost += 0.0f * relu1(i, w, n);
         }
