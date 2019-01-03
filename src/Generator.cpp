46938ac45062d8839ba3c561586b892e067852aa
Jing Pu <jimmypu07@gmail.com>
2019-01-03 13:27:03 -0800

    Move reset before creating the generator instance.

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -999,14 +999,15 @@ int generate_filter_main(int argc, char **argv, std::ostream &cerr) {
             Outputs output_files = compute_outputs(targets[0], base_path, emit_options);
             auto module_producer = [&generator_name, &generator_args]
                 (const std::string &name, const Target &target) -> Module {
+                    // Reset the counters so the function/variable names look
+                    // more consistent for different targets.
+                    reset_unique_name_counters();
+
                     auto sub_generator_args = generator_args;
                     sub_generator_args.erase("target");
                     // Must re-create each time since each instance will have a different Target.
                     auto gen = GeneratorRegistry::create(generator_name, GeneratorContext(target));
                     gen->set_generator_param_values(sub_generator_args);
-                    // Reset the counters so the function/variable names look
-                    // more consistent for different targets.
-                    reset_unique_name_counters();
                     return gen->build_module(name);
                 };
             if (targets.size() > 1 || !emit_options.substitutions.empty()) {
