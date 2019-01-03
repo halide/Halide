c2118f328a3acea6cd36c1bac2bd86f0877b5944
Steven Johnson <srj@google.com>
2019-01-03 11:50:51 -0800

    apps/conv_layer -> three_way_bench

--- apps/conv_layer/process.cpp
+++ apps/conv_layer/process.cpp
@@ -5,7 +5,7 @@
 #include "conv_layer_classic_auto_schedule.h"
 #include "conv_layer_auto_schedule.h"
 
-#include "halide_benchmark.h"
+#include "benchmark_util.h"
 #include "HalideBuffer.h"
 
 using namespace Halide::Tools;
@@ -25,25 +25,11 @@ int main(int argc, char **argv) {
 
     Buffer<float> output(CO, W, H, N);
 
-    // Timing code
-
-    // Manually-tuned version
-    double min_t_manual = benchmark(10, 10, [&]() {
-        conv_layer(input, filter, bias, output);
-    });
-    printf("Manually-tuned time: %gms\n", min_t_manual * 1e3);
-
-    // Auto-scheduled version
-    double min_t_classic_auto = benchmark(10, 10, [&]() {
-        conv_layer_classic_auto_schedule(input, filter, bias, output);
-    });
-    printf("Classic auto-scheduled time: %gms\n", min_t_classic_auto * 1e3);
-
-    // Auto-scheduled version
-    double min_t_auto = benchmark(10, 10, [&]() {
-        conv_layer_auto_schedule(input, filter, bias, output);
-    });
-    printf("Auto-scheduled time: %gms\n", min_t_auto * 1e3);
+    three_way_bench(
+        [&]() { conv_layer(input, filter, bias, output); },
+        [&]() { conv_layer_classic_auto_schedule(input, filter, bias, output); },
+        [&]() { conv_layer_auto_schedule(input, filter, bias, output); }
+    );
 
     return 0;
 }
