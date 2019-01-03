6931c1a2119e754dd4181636fa878c9131a0843e
Steven Johnson <srj@google.com>
2019-01-03 12:08:35 -0800

    Convert other apps to use three_way_bench

--- apps/hist/filter.cpp
+++ apps/hist/filter.cpp
@@ -9,49 +9,27 @@
 #include "hist_classic_auto_schedule.h"
 #include "hist_auto_schedule.h"
 
-#include "halide_benchmark.h"
+#include "benchmark_util.h"
 #include "halide_image_io.h"
 
 using namespace Halide::Tools;
 
 int main(int argc, char **argv) {
-
-    if (argc < 2) {
-        printf("Usage: %s in [out [out-classic [out-auto]]]\n", argv[0]);
+    if (argc != 3) {
+        printf("Usage: %s in out\n", argv[0]);
         return 1;
     }
+
     Halide::Runtime::Buffer<uint8_t> input = load_and_convert_image(argv[1]);
     Halide::Runtime::Buffer<uint8_t> output(input.width(), input.height(), 3);
 
-    hist(input, output);
-    double min_t = benchmark(10, 10, [&]() {
-        hist(input, output);
-        output.device_sync();
-    });
-    printf("runtime (manual schedule): %gms\n", min_t * 1e3);
-    if (argc >= 3) {
-        convert_and_save_image(output, argv[2]);
-    }
+    three_way_bench(
+        [&]() { hist(input, output); output.device_sync(); },
+        [&]() { hist_classic_auto_schedule(input, output); output.device_sync(); },
+        [&]() { hist_auto_schedule(input, output); output.device_sync(); }
+    );
 
-    hist_classic_auto_schedule(input, output);
-    min_t = benchmark(10, 10, [&]() {
-        hist_classic_auto_schedule(input, output);
-        output.device_sync();
-    });
-    printf("runtime (classic auto scheduler): %gms\n", min_t * 1e3);
-    if (argc >= 4) {
-        convert_and_save_image(output, argv[3]);
-    }
-
-    hist_auto_schedule(input, output);
-    min_t = benchmark(10, 10, [&]() {
-        hist_auto_schedule(input, output);
-        output.device_sync();
-    });
-    printf("runtime (new auto scheduler): %gms\n", min_t * 1e3);
-    if (argc >= 5) {
-        convert_and_save_image(output, argv[4]);
-    }
+    convert_and_save_image(output, argv[2]);
 
     return 0;
 }
--- apps/interpolate_generator/filter.cpp
+++ apps/interpolate_generator/filter.cpp
@@ -9,51 +9,29 @@
 #include "interpolate_classic_auto_schedule.h"
 #include "interpolate_auto_schedule.h"
 
-#include "halide_benchmark.h"
+#include "benchmark_util.h"
 #include "halide_image_io.h"
 
 using namespace Halide::Tools;
 
 int main(int argc, char **argv) {
-
-    if (argc < 2) {
-        printf("Usage: %s in [out [out-classic [out-auto]]]\n", argv[0]);
+    if (argc != 3) {
+        printf("Usage: %s in out\n", argv[0]);
         return 1;
     }
+
     Halide::Runtime::Buffer<float> input = load_and_convert_image(argv[1]);
     assert(input.channels() == 4);
 
     Halide::Runtime::Buffer<float> output(input.width(), input.height(), 3);
 
-    interpolate(input, output);
-    double min_t = benchmark(10, 10, [&]() {
-        interpolate(input, output);
-        output.device_sync();
-    });
-    printf("runtime (manual schedule): %gms\n", min_t * 1e3);
-    if (argc >= 3) {
-        convert_and_save_image(output, argv[2]);
-    }
+    three_way_bench(
+        [&]() { interpolate(input, output); output.device_sync(); },
+        [&]() { interpolate_classic_auto_schedule(input, output); output.device_sync(); },
+        [&]() { interpolate_auto_schedule(input, output); output.device_sync(); }
+    );
 
-    interpolate_classic_auto_schedule(input, output);
-    min_t = benchmark(10, 10, [&]() {
-        interpolate_classic_auto_schedule(input, output);
-        output.device_sync();
-    });
-    printf("runtime (classic auto scheduler): %gms\n", min_t * 1e3);
-    if (argc >= 4) {
-        convert_and_save_image(output, argv[3]);
-    }
-
-    interpolate_auto_schedule(input, output);
-    min_t = benchmark(10, 10, [&]() {
-        interpolate_auto_schedule(input, output);
-        output.device_sync();
-    });
-    printf("runtime (new auto scheduler): %gms\n", min_t * 1e3);
-    if (argc >= 5) {
-        convert_and_save_image(output, argv[4]);
-    }
+    convert_and_save_image(output, argv[2]);
 
     return 0;
 }
