f5a8c108b0912acb1983364019d397b0628c7d89
Benoit Steiner <benoitsteiner@fb.com>
2019-01-04 15:42:56 -0800

    Added an API to be able to call the autoscheduler code from c++

--- apps/autoscheduler/AutoSchedule.h
+++ apps/autoscheduler/AutoSchedule.h
@@ -10,10 +10,8 @@ namespace Internal {
 
 typedef PerfectHashMap<FunctionDAG::Node::Stage, ScheduleFeatures> StageMapOfScheduleFeatures;
 
-extern "C" {
 void find_and_apply_schedule(FunctionDAG& dag, const std::vector<Function> &outputs, const MachineParams &params,
 			     CostModel* cost_model, int beam_size, StageMapOfScheduleFeatures* schedule_features);
-}
 
 }
 }
