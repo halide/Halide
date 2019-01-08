6acda50d508621a690e19ed0ebf72fab990a7afb
Andrew Adams <andrew.b.adams@gmail.com>
2019-01-08 10:37:03 -0800

    Fresh weights

--- apps/autoscheduler/NetworkSize.h
+++ apps/autoscheduler/NetworkSize.h
@@ -1,8 +1,8 @@
 #ifndef NETWORK_SIZE_H
 #define NETWORK_SIZE_H
 
-const int head1_channels = 4, head1_w = 40, head1_h = 7;
-const int head2_channels = 4, head2_w = 39;
+const int head1_channels = 2, head1_w = 40, head1_h = 7;
+const int head2_channels = 2, head2_w = 39;
 const int conv1_channels = 32;
 
 #endif
