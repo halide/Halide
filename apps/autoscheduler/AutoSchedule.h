bc0df860b6cb8b40e16b73359c421cb1eda4ce93
Benoit Steiner <benoitsteiner@fb.com>
2019-01-09 10:41:15 -0800

    Code cleanup Added the definition of the new autoscheduler API

--- apps/autoscheduler/AutoSchedule.h
+++ apps/autoscheduler/AutoSchedule.h
@@ -2,14 +2,16 @@
 #include "CostModel.h"
 #include "FunctionDAG.h"
 #include "PerfectHashMap.h"
+#include "Featurization.h"
+#include <vector>
 
-namespace  Halide {
+namespace Halide {
 namespace Internal {
 
 typedef PerfectHashMap<FunctionDAG::Node::Stage, ScheduleFeatures> StageMapOfScheduleFeatures;
 
 extern "C" {
-void find_and_apply_schedule(FunctionDAG& dag, const vector<Function> &outputs, const MachineParams &params,
+void find_and_apply_schedule(FunctionDAG& dag, const std::vector<Function> &outputs, const MachineParams &params,
 			     CostModel* cost_model, int beam_size, StageMapOfScheduleFeatures* schedule_features);
 }
 
