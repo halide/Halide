a16fba0c3d962f5fde3c8d0bd5d521542f078862
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2019-01-09 21:39:21 -0500

    max filter

--- apps/max_filter/filter.cpp
+++ apps/max_filter/filter.cpp
@@ -8,6 +8,7 @@
 #include "max_filter.h"
 #include "max_filter_classic_auto_schedule.h"
 #include "max_filter_auto_schedule.h"
+#include "max_filter_simple_auto_schedule.h"
 
 #include "benchmark_util.h"
 #include "halide_image_io.h"
@@ -23,11 +24,12 @@ int main(int argc, char **argv) {
     Halide::Runtime::Buffer<float> input = load_and_convert_image(argv[1]);
     Halide::Runtime::Buffer<float> output(input.width(), input.height(), 3);
 
-    three_way_bench(
-        [&]() { max_filter(input, output); output.device_sync(); },
-        [&]() { max_filter_classic_auto_schedule(input, output); output.device_sync(); },
-        [&]() { max_filter_auto_schedule(input, output); output.device_sync(); }
-    );
+    multi_way_bench({
+        {"Manual", [&]() { max_filter(input, output); output.device_sync(); }},
+        {"Classic auto-scheduler", [&]() { max_filter_classic_auto_schedule(input, output); output.device_sync(); }},
+        {"Auto-scheduler", [&]() { max_filter_auto_schedule(input, output); output.device_sync(); }},
+        {"Simple auto-scheduler", [&]() { max_filter_simple_auto_schedule(input, output); output.device_sync();}}
+        });
 
     convert_and_save_image(output, argv[2]);
 
