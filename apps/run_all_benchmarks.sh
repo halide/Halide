ab6d29b4d807f3fa3999b0f1530ff6d53fae71ad
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-11 11:57:36 +0000

    Generalize benchmarking scripts

--- apps/run_all_benchmarks.sh
+++ apps/run_all_benchmarks.sh
@@ -1,7 +1,16 @@
 BENCHMARKS="bilateral_grid local_laplacian nl_means lens_blur camera_pipe stencil_chain blur unsharp harris interpolate max_filter hist conv_layer"
 # resnet matmul
 
+# Set EXPLORE_PARAMETERS to 1 if you want to run a script that explores
+# different architecture parameters.
+EXPLORE_PARAMETERS=0
+
+
 for b in $BENCHMARKS; do
 	echo "Running benchmarks $b"
-	./find_gpu_parameters.sh $b
+	if [ "${EXPLORE_PARAMETERS}" -ne 0 ]; then
+		./find_gpu_parameters.sh $b
+	else
+		./run_one_benchmark.sh $b
+	fi
 done
