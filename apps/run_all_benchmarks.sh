0aa2079f8e8bba0f2703ded04b2754fedc99a5ba
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-01-12 21:23:27 +0000

    Update Makefiles

--- apps/run_all_benchmarks.sh
+++ apps/run_all_benchmarks.sh
@@ -1,9 +1,9 @@
-BENCHMARKS="bilateral_grid local_laplacian nl_means lens_blur camera_pipe stencil_chain unsharp harris interpolate_generator max_filter hist conv_layer iir_blur_generator bgu0"
-# resnet matmul
+BENCHMARKS="bilateral_grid local_laplacian nl_means lens_blur camera_pipe stencil_chain harris hist max_filter unsharp interpolate_generator conv_layer iir_blur_generator bgu0"
+# resnet_50 cuda_mat_mul
 
 # Set EXPLORE_PARAMETERS to 1 if you want to run a script that explores
 # different architecture parameters.
-EXPLORE_PARAMETERS=1
+EXPLORE_PARAMETERS=0
 
 
 for b in $BENCHMARKS; do
