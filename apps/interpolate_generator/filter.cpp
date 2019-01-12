ed171f8ad7bbb48d180e679cb0af39e82fec0aa7
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2019-01-12 16:58:10 -0500

    interpolate

--- apps/interpolate_generator/filter.cpp
+++ apps/interpolate_generator/filter.cpp
@@ -8,6 +8,7 @@
 #include "interpolate.h"
 #include "interpolate_classic_auto_schedule.h"
 #include "interpolate_auto_schedule.h"
+#include "interpolate_simple_auto_schedule.h"
 
 #include "benchmark_util.h"
 #include "halide_image_io.h"
@@ -25,11 +26,12 @@ int main(int argc, char **argv) {
 
     Halide::Runtime::Buffer<float> output(input.width(), input.height(), 3);
 
-    three_way_bench(
-        [&]() { interpolate(input, output); output.device_sync(); },
-        [&]() { interpolate_classic_auto_schedule(input, output); output.device_sync(); },
-        [&]() { interpolate_auto_schedule(input, output); output.device_sync(); }
-    );
+    multi_way_bench({
+        {"Manual", [&]() { interpolate(input, output); output.device_sync(); }},
+        {"Classic auto-scheduled", [&]() { interpolate_classic_auto_schedule(input, output); output.device_sync(); }},
+        {"Auto-scheduled", [&]() { interpolate_auto_schedule(input, output); output.device_sync(); }},
+        {"Simple auto-scheduled", [&]() { interpolate_auto_schedule(input, output); output.device_sync(); }}
+        });
 
     convert_and_save_image(output, argv[2]);
 
