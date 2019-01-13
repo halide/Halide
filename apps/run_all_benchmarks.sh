a27196239914e293bbac58e161a5080cae3d74ef
aekul <aekul@users.noreply.github.com>
2019-01-12 20:47:35 -0500

    Add resnet + matmul to run_all_benchmarks

--- apps/run_all_benchmarks.sh
+++ apps/run_all_benchmarks.sh
@@ -1,5 +1,4 @@
-BENCHMARKS="bilateral_grid local_laplacian nl_means lens_blur camera_pipe stencil_chain unsharp harris interpolate_generator max_filter hist conv_layer iir_blur_generator bgu0"
-# resnet matmul
+BENCHMARKS="bilateral_grid local_laplacian nl_means lens_blur camera_pipe stencil_chain unsharp harris interpolate_generator max_filter hist conv_layer iir_blur_generator bgu0 resnet_50 mat_mul_generator"
 
 # Set EXPLORE_PARAMETERS to 1 if you want to run a script that explores
 # different architecture parameters.
