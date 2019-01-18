6e84f3e7dd4f0995eeb035663d818d0e769136b6
Andrew Adams <andrew.b.adams@gmail.com>
2019-01-18 11:06:29 -0800

    Fix for avx

--- src/CodeGen_X86.cpp
+++ src/CodeGen_X86.cpp
@@ -21,7 +21,27 @@ using std::vector;
 using namespace Halide::ConciseCasts;
 using namespace llvm;
 
-CodeGen_X86::CodeGen_X86(Target t) : CodeGen_Posix(t) {
+namespace {
+// Populate feature flags in a target according to those implied by
+// existing flags, so that instruction patterns can just check for the
+// oldest feature flag that supports an instruction.
+Target complete_x86_target(Target t) {
+    if (t.has_feature(Target::AVX512_Cannonlake) ||
+        t.has_feature(Target::AVX512_Skylake) ||
+        t.has_feature(Target::AVX512_KNL)) {
+        t.set_feature(Target::AVX2);
+    }
+    if (t.has_feature(Target::AVX2)) {
+        t.set_feature(Target::AVX);
+    }
+    if (t.has_feature(Target::AVX)) {
+        t.set_feature(Target::SSE41);
+    }
+    return t;
+}
+}
+
+CodeGen_X86::CodeGen_X86(Target t) : CodeGen_Posix(complete_x86_target(t)) {
 
     #if !(WITH_X86)
     user_error << "x86 not enabled for this build of Halide.\n";
@@ -271,19 +291,19 @@ void CodeGen_X86::visit(const Cast *op) {
          u16(max(wild_i32x_ - wild_i32x_, 0))},
 #else
         // LLVM 8.0+ require using helpers from x86.ll
-        {Target::AVX2, true, UInt(8, 32), 17, "paddusbx32",
+        {Target::AVX, true, UInt(8, 32), 17, "paddusbx32",
          u8_sat(wild_u16x_ + wild_u16x_)},
         {Target::FeatureEnd, true, UInt(8, 16), 0, "paddusbx16",
          u8_sat(wild_u16x_ + wild_u16x_)},
-        {Target::AVX2, true, UInt(8, 32), 17, "psubusbx32",
+        {Target::AVX, true, UInt(8, 32), 17, "psubusbx32",
          u8(max(wild_i16x_ - wild_i16x_, 0))},
         {Target::FeatureEnd, true, UInt(8, 16), 0, "psubusbx16",
          u8(max(wild_i16x_ - wild_i16x_, 0))},
-        {Target::AVX2, true, UInt(16, 16), 9, "padduswx16",
+        {Target::AVX, true, UInt(16, 16), 9, "padduswx16",
          u16_sat(wild_u32x_ + wild_u32x_)},
         {Target::FeatureEnd, true, UInt(16, 8), 0, "padduswx8",
          u16_sat(wild_u32x_ + wild_u32x_)},
-        {Target::AVX2, true, UInt(16, 16), 9, "psubuswx16",
+        {Target::AVX, true, UInt(16, 16), 9, "psubuswx16",
          u16(max(wild_i32x_ - wild_i32x_, 0))},
         {Target::FeatureEnd, true, UInt(16, 8), 0, "psubuswx8",
          u16(max(wild_i32x_ - wild_i32x_, 0))},
