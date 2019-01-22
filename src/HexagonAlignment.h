afd45128a19f359ae24b1827e04f6c650132dd8f
Andrew Adams <andrew.b.adams@gmail.com>
2019-01-22 12:54:48 -0800

    Fix bugs in alignment analyzer

--- src/HexagonAlignment.h
+++ src/HexagonAlignment.h
@@ -14,9 +14,11 @@ namespace Internal {
 
 // TODO: This class is barely stateful, and could probably be replaced with free functions.
 class HexagonAlignmentAnalyzer {
-    int required_alignment;
+    const int required_alignment;
 public:
-    HexagonAlignmentAnalyzer(int required_alignment) {}
+    HexagonAlignmentAnalyzer(int required_alignment) : required_alignment(required_alignment) {
+        internal_assert(required_alignment != 0);
+    }
 
     /** Analyze the index of a load/store instruction for alignment
      *  Returns true if it can determing that the address of the store or load is aligned, false otherwise.
@@ -24,8 +26,8 @@ public:
     template<typename T>
     bool is_aligned_impl(const T *op, int native_lanes, int64_t *aligned_offset) {
         debug(3) << "HexagonAlignmentAnalyzer: Check if " << op->index << " is aligned to a "
-                 << required_alignment << " byte boundary\n";
-        debug(3) << "native_lanes: " << native_lanes << "\n";
+                 << required_alignment << " byte boundary\n"
+                 << "native_lanes: " << native_lanes << "\n";
         Expr index = op->index;
         const Ramp *ramp = index.as<Ramp>();
         if (ramp) {
@@ -35,16 +37,18 @@ public:
             return false;
         }
 
+        internal_assert(native_lanes != 0) << "Type is larger than required alignment of " << required_alignment << " bytes\n";
+
         // If this is a parameter, the base_alignment should be
         // host_alignment. Otherwise, this is an internal buffer,
         // which we assume has been aligned to the required alignment.
-        if (op->param.defined() && (op->param.host_alignment() % required_alignment != 0)) {
+        if (op->param.defined() && ((op->param.host_alignment() % native_lanes) != 0)) {
             return false;
         }
 
-        bool known_alignment = (op->alignment.modulus % required_alignment) == 0;
+        bool known_alignment = (op->alignment.modulus % native_lanes) == 0;
         if (known_alignment) {
-            *aligned_offset = op->alignment.remainder % required_alignment;
+            *aligned_offset = op->alignment.remainder % native_lanes;
         }
         return known_alignment;
     }
