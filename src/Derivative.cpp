a794bae362302f5f1f845fe52b3df14fe02fcf7c
Tzu-Mao Li <tzumao@mit.edu>
2019-01-23 16:55:35 -0500

    remove redundant code

--- src/Derivative.cpp
+++ src/Derivative.cpp
@@ -739,12 +739,10 @@ void ReverseAccumulationVisitor::propagate_adjoints(
                                    update_args, i);
                 }
 
-                int count = 0;
                 // Traverse the expressions in reverse order
                 for (auto it = expr_list.rbegin(); it != expr_list.rend(); it++) {
                     // Propagate adjoints
                     it->accept(this);
-                    count++;
                 }
             }
         }
