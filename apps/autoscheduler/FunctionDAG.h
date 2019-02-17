0594f8f3e8a4612ffa159db4d70b8511f286831a
Andrew Adams <andrew.b.adams@gmail.com>
2019-02-17 14:17:59 -0800

    GPU schedule for camera pipe

--- apps/autoscheduler/FunctionDAG.h
+++ apps/autoscheduler/FunctionDAG.h
@@ -994,8 +994,10 @@ struct FunctionDAG {
                     // Get the bounds estimate
                     map<string, Span> estimates;
                     for (auto b : consumer.schedule().estimates()) {
-                        int64_t i_min = *as_const_int(b.min);
-                        int64_t i_extent = *as_const_int(b.extent);
+                        const int64_t *i_min = as_const_int(b.min);
+                        const int64_t *i_extent = as_const_int(b.extent);
+
+                        if (!i_min || !i_extent) continue;
 
                         if (false) {
                             // HACK: Some methods we compare to
@@ -1013,9 +1015,9 @@ struct FunctionDAG {
                             // the scheduling space.  TODO: Make this
                             // a documented env var
                             Func(node.func).bound(b.var, b.min, b.extent);
-                            estimates[b.var] = Span(i_min, i_min + i_extent - 1, true);
+                            estimates[b.var] = Span(*i_min, *i_min + *i_extent - 1, true);
                         } else {
-                            estimates[b.var] = Span(i_min, i_min + i_extent - 1, false);
+                            estimates[b.var] = Span(*i_min, *i_min + *i_extent - 1, false);
                         }
                     }
                     for (auto b : consumer.schedule().bounds()) {
