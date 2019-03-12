ac900b7c1b19e7e2d6c6f855e10a95d7c433d317
Steven Johnson <srj@google.com>
2019-03-12 15:25:20 -0700

    Update Introspection.cpp

--- src/Introspection.cpp
+++ src/Introspection.cpp
@@ -2201,13 +2201,8 @@ private:
 };
 
 namespace {
-
 DebugSections *debug_sections = nullptr;
-
-#if __has_feature(thread_sanitizer)
 std::mutex get_variable_name_lock;
-#endif
-
 }
 
 bool dump_stack_frame() {
@@ -2219,19 +2214,15 @@ bool dump_stack_frame() {
 }
 
 std::string get_variable_name(const void *var, const std::string &expected_type) {
+    if (!debug_sections) return "";
+    if (!debug_sections->working) return "";
 
-#if defined(__has_feature)
-#if __has_feature(thread_sanitizer)
     // At least one version of libc++ will report data races inside the regex
     // implementation if we call this from multiple threads at the same time
     // (e.g. from correctness_vector_width); this mutex serves more to silence
     // what is likely to be an irrelevant-to-us data race when using TSAN.
     std::lock_guard<std::mutex> lock(get_variable_name_lock);
-#endif
-#endif
 
-    if (!debug_sections) return "";
-    if (!debug_sections->working) return "";
     std::string name = debug_sections->get_stack_variable_name(var, expected_type);
     if (name.empty()) {
         // Maybe it's a member of a heap object.
