bfe307402c2f70390b9e882e869b6553e066f3bd
= <ataei@fb.com>
2019-04-11 12:02:31 -0700

    Update onnx_converter library

--- apps/onnx/onnx_converter.h
+++ apps/onnx/onnx_converter.h
@@ -1,39 +1,38 @@
 #ifndef CONVERT_MODEL_H_
 #define CONVERT_MODEL_H_
 
-#include "Halide.h"
-#include "onnx/onnx_pb.h"
 #include <unordered_map>
 #include <vector>
+#include "Halide.h"
+#include "onnx/onnx_pb.h"
 
 struct Tensor {
-    std::string name;
-    onnx::TensorProto::DataType type;
-    std::vector<Halide::Expr> shape;
-    Halide::Func rep;
+  std::string name;
+  onnx::TensorProto::DataType type;
+  std::vector<Halide::Expr> shape;
+  Halide::Func rep;
 };
 
 struct Node {
-    std::vector<Tensor> inputs;
-    std::vector<Tensor> outputs;
+  std::vector<Tensor> inputs;
+  std::vector<Tensor> outputs;
 
-    std::vector<Halide::Expr> requirements;
+  std::vector<Halide::Expr> requirements;
 };
 
 Node convert_node(
-    const onnx::NodeProto &node,
-    const std::vector<Tensor> &inputs,
-    const std::string &device);
+    const onnx::NodeProto& node,
+    const std::vector<Tensor>& inputs);
 
 struct Model {
-    std::unordered_map<std::string, Halide::ImageParam> inputs;
-    std::unordered_map<std::string, Tensor> outputs;
+  std::unordered_map<std::string, Halide::ImageParam> inputs;
+  std::unordered_map<std::string, Tensor> outputs;
 
-    std::unordered_map<std::string, Tensor> tensors;
+  std::unordered_map<std::string, Tensor> tensors;
 
-    std::vector<Halide::Expr> requirements;
+  std::vector<Halide::Expr> requirements;
 };
 
-Model convert_model(const onnx::ModelProto &model, const std::string &device);
+Model convert_model(const onnx::ModelProto& model);
 
 #endif
