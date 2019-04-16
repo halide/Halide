542cf9997206686c22b148020ecad8d8e88576c1
Tzu-Mao Li <tzumao@mit.edu>
2019-04-16 16:58:24 -0400

    Address review comments

--- python_bindings/correctness/autodiff.py
+++ python_bindings/correctness/autodiff.py
@@ -2,8 +2,12 @@ import halide as hl
 
 def test_autodiff():
 	x = hl.Var('x')
+	b = hl.Buffer(hl.Float(32), [3])
+	b[0] = 1.0
+	b[1] = 2.0
+	b[2] = 3.0
 	f, g, h = hl.Func('f'), hl.Func('g'), hl.Func('h')
-	f[x] = 1.0
+	f[x] = b[x]
 	g[x] = f[x] * 5.0
 	r = hl.RDom([(0, 3)])
 	h[()] = 0.0
@@ -15,6 +19,11 @@ def test_autodiff():
 	assert(d_f_buf[0] == 5.0)
 	assert(d_f_buf[1] == 5.0)
 	assert(d_f_buf[2] == 5.0)
+	d_b = d[b]
+	d_b_buf = d_b.realize(3)
+	assert(d_b_buf[0] == 5.0)
+	assert(d_b_buf[1] == 5.0)
+	assert(d_b_buf[2] == 5.0)
 
 if __name__ == "__main__":
 	test_autodiff()
--- python_bindings/src/PyDerivative.cpp
+++ python_bindings/src/PyDerivative.cpp
@@ -6,6 +6,7 @@ namespace PythonBindings {
 void define_derivative(py::module &m) {
     auto derivative_class = py::class_<Derivative>(m, "Derivative")
         .def("__getitem__", [](const Derivative &d, const Func &func) {return d(func);})
+        .def("__getitem__", [](const Derivative &d, const Func &func, int update_id) {return d(func, update_id);})
         .def("__getitem__", [](const Derivative &d, const Buffer<> &buffer) {return d(buffer);});
 
     m.def("propagate_adjoints",
