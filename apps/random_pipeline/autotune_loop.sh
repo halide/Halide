ebc3670f5e4f90a0bc8bb588f903f11703639058
aekul <aekul@users.noreply.github.com>
2019-04-30 10:11:27 -0400

    Update autotune_loop.sh parameters

--- apps/random_pipeline/autotune_loop.sh
+++ apps/random_pipeline/autotune_loop.sh
@@ -38,10 +38,10 @@ make_sample() {
     rm -f "${D}/sample.sample"
     if [[ $D == */0 ]]; then
         # Sample 0 in each batch is best effort beam search, with no randomness
-        HL_MACHINE_PARAMS=80,1,1 HL_PERMIT_FAILED_UNROLL=1 HL_SEED=${2} HL_FEATURE_FILE=${D}/sample.sample HL_WEIGHTS_DIR=${PWD}/../autoscheduler/gpu_weights HL_RANDOM_DROPOUT=100 HL_BEAM_SIZE=20 HL_SHARED_MEMORY_LIMIT=48 ${GENERATOR} -g ${PIPELINE} -o ${D} -e static_library,h,stmt,assembly,registration target=${HL_TARGET} auto_schedule=true max_stages=12 seed=${3} -p ${PWD}/bin/libauto_schedule.so 2> ${D}/compile_log_stderr.txt > ${D}/compile_log_stdout.txt
+        HL_MACHINE_PARAMS=80,1,1 HL_PERMIT_FAILED_UNROLL=1 HL_SEED=${2} HL_FEATURE_FILE=${D}/sample.sample HL_WEIGHTS_DIR=${PWD}/../autoscheduler/gpu_weights HL_RANDOM_DROPOUT=100 HL_BEAM_SIZE=20 HL_SHARED_MEMORY_LIMIT=48 ${GENERATOR} -g ${PIPELINE} -o ${D} -e static_library,h,stmt,assembly,registration target=${HL_TARGET} auto_schedule=true max_stages=40 seed=${3} -p ${PWD}/bin/libauto_schedule.so 2> ${D}/compile_log_stderr.txt > ${D}/compile_log_stdout.txt
     else
         # The other samples are random probes biased by the cost model
-        HL_MACHINE_PARAMS=80,1,1 HL_PERMIT_FAILED_UNROLL=1 HL_SEED=${2} HL_FEATURE_FILE=${D}/sample.sample HL_WEIGHTS_DIR=${PWD}/../autoscheduler/gpu_weights HL_RANDOM_DROPOUT=80 HL_BEAM_SIZE=1 HL_SHARED_MEMORY_LIMIT=48 ${GENERATOR} -g ${PIPELINE} -o ${D} -e static_library,h,stmt,assembly,registration target=${HL_TARGET} auto_schedule=true max_stages=12 seed=${3} -p ${PWD}/bin/libauto_schedule.so 2> ${D}/compile_log_stderr.txt > ${D}/compile_log_stdout.txt
+        HL_MACHINE_PARAMS=80,1,1 HL_PERMIT_FAILED_UNROLL=1 HL_SEED=${2} HL_FEATURE_FILE=${D}/sample.sample HL_WEIGHTS_DIR=${PWD}/../autoscheduler/gpu_weights HL_RANDOM_DROPOUT=5 HL_BEAM_SIZE=1 HL_SHARED_MEMORY_LIMIT=48 ${GENERATOR} -g ${PIPELINE} -o ${D} -e static_library,h,stmt,assembly,registration target=${HL_TARGET} auto_schedule=true max_stages=40 seed=${3} -p ${PWD}/bin/libauto_schedule.so 2> ${D}/compile_log_stderr.txt > ${D}/compile_log_stdout.txt
     fi
 
     c++ -std=c++11 -I ../../include ../../tools/RunGenMain.cpp ${D}/*.registration.cpp ${D}/*.a -o ${D}/bench -ljpeg -ldl -lpthread -lz -lpng
