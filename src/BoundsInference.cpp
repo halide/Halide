0ac2d51e55da82d52fb70a2fa4234a3fb1475231
Andrew Adams <andrew.b.adams@gmail.com>
2019-05-01 11:57:02 -0700

    Reduce branch to the simpler fixes to isolate problematic transformation

--- src/BoundsInference.cpp
+++ src/BoundsInference.cpp
@@ -263,29 +263,6 @@ public:
             }
 
             exprs.insert(exprs.end(), result[1].begin(), result[1].end());
-
-            // For the purposes of computation bounds inference, we
-            // don't care what sites are loaded, just what sites need
-            // to have the correct value in them. So remap all selects
-            // to if_then_elses to get tighter bounds.
-            class SelectToIfThenElse : public IRMutator {
-                using IRMutator::visit;
-                Expr visit(const Select *op) override {
-                    if (is_pure(op->condition)) {
-                        return Call::make(op->type, Call::if_then_else,
-                                          {mutate(op->condition),
-                                                  mutate(op->true_value),
-                                                  mutate(op->false_value)},
-                                          Call::PureIntrinsic);
-                    } else {
-                        return IRMutator::visit(op);
-                    }
-                }
-            } select_to_if_then_else;
-
-            for (auto &e : exprs) {
-                e.value = select_to_if_then_else.mutate(e.value);
-            }
         }
 
         // Check if the dimension at index 'dim_idx' is always pure (i.e. equal to 'dim')
