8f17cdbfa416f47a082dac83ad497115258471fb
Andrew Adams <andrew.b.adams@gmail.com>
2019-05-28 15:28:07 -0700

    File-level comments

--- src/StoreWith.h
+++ src/StoreWith.h
@@ -5,13 +5,20 @@
 
 #include "IR.h"
 
-// TODO: file docs
+/** \file
+ * Defines the lowering pass that implements the store_with scheduling
+ * directive. Internally uses some generic polyhedral optimization
+ * machinery, which should be exposed in a separate module if it
+ * becomes useful elsewhere in the compiler. */
 
 namespace Halide {
 namespace Internal {
 
-// TODO: docs
-
+/** Implements store_with scheduling directives by merging buffers
+ * according to the directives, proving that this doesn't change the
+ * meaning of the algorithm. Throws user errors in a wide variety of
+ * situations in which use of store_with would produce a different
+ * output. */
 Stmt lower_store_with(const Stmt &s,
                       const std::vector<Function> &outputs,
                       const std::map<std::string, Function> &env);
