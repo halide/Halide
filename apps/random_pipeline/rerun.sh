62ba07ebe33765e355e561cc68401edaf75d5077
aekul <aekul@users.noreply.github.com>
2019-07-19 12:09:51 -0400

    Add autoschedule with debug as a rerun command

--- apps/random_pipeline/rerun.sh
+++ apps/random_pipeline/rerun.sh
@@ -3,20 +3,28 @@
 # Re-run a failed command
 # Commands:
 # a => autoschedule
+# ad => autoschedule with debug target
 # c => compile
 # b => benchmark
 # au => augment
 # p => profile
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 batch_id sample_id command"
+if [ $# -ne 5 ]; then
+  echo "Usage: $0 dir batch_id extra_generator_args_id sample_id command"
   exit
 fi
 
-BATCH_ID=${1}
-SAMPLE_ID=${2}
-COMMAND=${3}
+DIR=${1}
+BATCH_ID=${2}
+EXTRA_GENERATOR_ARGS_ID=${3}
+SAMPLE_ID=${4}
+COMMAND=${5}
+
+DEBUG=0
 
 if [ $COMMAND == "a" ]; then
+    COMMAND="autoschedule"
+elif [ $COMMAND == "ad" ]; then
+    DEBUG=1
     COMMAND="autoschedule"
 elif [ $COMMAND == "c" ]; then
     COMMAND="compile"
@@ -31,7 +39,11 @@ else
     exit
 fi
 
-DIR=samples/batch_${BATCH_ID}/${SAMPLE_ID}
+if [[ $EXTRA_GENERATOR_ARGS_ID -ge 0 ]]; then
+    DIR=${DIR}/batch_${BATCH_ID}_${EXTRA_GENERATOR_ARGS_ID}/${SAMPLE_ID}
+else
+    DIR=${DIR}/batch_${BATCH_ID}/${SAMPLE_ID}
+fi
 
 if [[ ! -d $DIR ]]; then
     echo "Sample does not exist: ${DIR}"
@@ -44,5 +56,13 @@ if [[ ! -f $COMMAND_FILE ]]; then
     exit
 fi
 
-cat ${COMMAND_FILE}
-bash ${COMMAND_FILE}
+if [[ $DEBUG == 1 ]]; then
+    CMD=$(cat ${COMMAND_FILE} | sed "s/host-cuda/host-cuda-debug/g")
+else
+    CMD=$(cat ${COMMAND_FILE})
+fi
+
+echo "Running command in: ${COMMAND_FILE}"
+echo ${CMD}
+eval $CMD
+
