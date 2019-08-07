8d15aedae8702ce1088257ab98c9f300d4f734d8
aekul <aekul@users.noreply.github.com>
2019-08-07 16:35:09 -0400

    Add num passes as compile variable

--- apps/autoscheduler/autotune_loop.sh
+++ apps/autoscheduler/autotune_loop.sh
@@ -135,6 +135,7 @@ make_sample() {
         HL_WEIGHTS_DIR=${WEIGHTS} \
         HL_RANDOM_DROPOUT=${dropout} \
         HL_BEAM_SIZE=${beam} \
+        HL_NUM_PASSES=${HL_NUM_PASSES} \
         HL_SHARED_MEMORY_LIMIT=${shared_memory_limit} \
         HL_MACHINE_PARAMS=${HL_MACHINE_PARAMS} \
         ${TIMEOUT_CMD} -k ${COMPILATION_TIMEOUT} ${COMPILATION_TIMEOUT} \
