13f56cc363cf08030746ffeb739ab215b01c1991
Benoit Steiner <benoitsteiner@fb.com>
2019-08-20 14:53:55 -0700

    Made model.cpp compile again

--- apps/onnx/model.cpp
+++ apps/onnx/model.cpp
@@ -60,8 +60,8 @@ HalideModel convert_onnx_model(
 std::string auto_schedule(const HalideModel &pipeline) {
     // Generate a schedule for the pipeline.
     Halide::Target tgt = Halide::get_host_target();
-    std::string schedule = pipeline.rep->auto_schedule(tgt);
-    return schedule;
+    auto schedule = pipeline.rep->auto_schedule(tgt);
+    return schedule.schedule_source;
 }
 
 template<typename T>
@@ -333,7 +333,7 @@ std::vector<py::array> run(
     }
     Halide::Realization real(outputs);
     Halide::Target tgt;
-    tgt.set_feature(Halide::Target::DisableLLVMLoopOpt);
+    tgt.set_feature(Halide::Target::DisableLLVMLoopUnroll);
     if (device == "CUDA") {
         tgt.set_feature(Halide::Target::CUDA);
     }
@@ -428,7 +428,7 @@ double benchmark(const HalideModel &pipeline, int num_iters, const std::string &
 
     Halide::Realization real(outputs);
     Halide::Target tgt;
-    tgt.set_feature(Halide::Target::DisableLLVMLoopOpt);
+    tgt.set_feature(Halide::Target::DisableLLVMLoopUnroll);
     if (device == "CUDA") {
         tgt.set_feature(Halide::Target::CUDA);
     }
