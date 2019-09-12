4725c10470007fc04ef1499335ae4032ba1d2052
Steven Johnson <srj@google.com>
2019-09-12 10:33:22 -0700

    Update Generator.cpp

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -933,12 +933,6 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
         }
     }
 
-    auto target_strings = split_string(generator_args["target"].string_value, ",");
-    std::vector<Target> targets;
-    for (const auto &s : target_strings) {
-        targets.emplace_back(s);
-    }
-
     if (!runtime_name.empty()) {
         std::string base_path = compute_base_path(output_dir, runtime_name, "");
 
