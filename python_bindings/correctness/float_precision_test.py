8332b2d667060e721b57947b9581e38e5d684375
mugwort_rc <mugwort.rc@gmail.com>
2019-09-13 12:31:29 +0900

    Update test_pattern

--- python_bindings/correctness/float_precision_test.py
+++ python_bindings/correctness/float_precision_test.py
@@ -8,9 +8,9 @@ def test():
     def test_pattern(c):
         x = hl.Var("x")
         f = hl.Func("f")
-        f[x] = x * c
+        f[x] = x * c * (hl.f64(0.1) + hl.f64(0.2))
         for i, hl_value in enumerate(numpy.asarray(f.realize(10))):
-            py_value = (i * c)
+            py_value = i * c * (0.1 + 0.2)
             check = math.isclose(hl_value, py_value)
             assert check, "{}[{}]: {} != {}".format(i, c, hl_value, py_value)
 
