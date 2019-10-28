8fa7c46697b2e445e7d33d349e983943eaea7601
Steven Johnson <srj@google.com>
2019-10-28 14:00:50 -0700

    Make is_nan/is_inf/is_finite enforce strict_float() (Issue #4281)
    Quietly wrap the arg to these functions in strict_float(), to ensure the results aren't meaningless.
    
    (Also: drive-by cleanup of code in StrictifyFloat that was a holdover from when it was possible to have contexts in which strict-float was not allowed)

--- src/StrictifyFloat.cpp
+++ src/StrictifyFloat.cpp
@@ -7,7 +7,6 @@ namespace Halide {
 namespace Internal {
 
 class StrictifyFloat : public IRMutator {
-    bool strict_float_allowed;
     enum Strictness {
         FastMath,
         StrictFloat,
@@ -19,7 +18,6 @@ class StrictifyFloat : public IRMutator {
         Strictness new_strictness = strictness;
 
         if (call->is_intrinsic(Call::strict_float)) {
-            user_assert(strict_float_allowed) << "strict_float intrinsic is not allowed unless target has feature 'allow_strict_float' or 'force_strict_float'\n";
             new_strictness = StrictFloat;
             any_strict_float |= true;
         }
@@ -49,15 +47,13 @@ class StrictifyFloat : public IRMutator {
 
 public:
     enum StrictnessMode {
-        NotAllowed,
         Allowed,
         Forced
     };
     bool any_strict_float{false};
 
     StrictifyFloat(StrictnessMode mode)
-        : strict_float_allowed(mode != NotAllowed),
-          strictness((mode == Forced) ? StrictFloat : FastMath) {
+        : strictness((mode == Forced) ? StrictFloat : FastMath) {
         any_strict_float |= (mode == Forced);
     }
 };
