1f0179b299a750c7555706774ab31fa5dfb53eb3
Pranav Bhandarkar <pranavb@codeaurora.org>
2019-11-05 18:33:02 -0600

    Revert "Add default constexpr constructor to ImplicitVar because Clang complaints about it"
    This reverts commit 9ff00ffe4ed0f2d476ba974ce1bad15bd70d9390.

--- src/Var.h
+++ src/Var.h
@@ -176,7 +176,6 @@ struct ImplicitVar {
     operator Expr() const {
         return to_var();
     }
-    constexpr ImplicitVar(){};
 };
 
 /** A placeholder variable for inferred arguments. See \ref Var::implicit */
