e09043c8828d332b00d423cd9f47966831dd181c
Julie Newcomb <newcombj@cs.washington.edu>
2019-11-10 21:52:45 -0800

    refined reduction order checker for super_simplifier

--- apps/super_simplify/reduction_order.h
+++ apps/super_simplify/reduction_order.h
@@ -4,6 +4,6 @@
 #include "Halide.h"
 
 bool check_divisors(Halide::Expr &LHS, Halide::Expr &RHS);
-bool valid_reduction_order(Halide::Expr &LHS, Halide::Expr &RHS);
+bool valid_reduction_order(const Halide::Expr &LHS, const Halide::Expr &RHS);
 
 #endif
