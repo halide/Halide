7e87e474fb0f9580c6800d5c78cabe32e1068917
Shubham Pawar <shubhamp@codeaurora.org>
2019-11-13 09:31:57 -0600

    Calculate Expr bounds using function_value_bounds
    This fixes #4397 where FunctionDAG.cpp computes an unbounded
    region for the update definitions of a Func when the region for
    the consumer Func is defined by another Func (call f2), because
    the bounds for f2 are not taken into account while calculating
    the bounds of expr which defines a dimension of the consumer.
    
    This change --
    	1. hoists the computation of func_value_bounds out of loop.
    	2. uses the computed func_value_bounds while calculating
    	   bounds_of_expr_in_scope()

--- apps/autoscheduler/FunctionDAG.cpp
+++ apps/autoscheduler/FunctionDAG.cpp
@@ -587,6 +587,12 @@ FunctionDAG::FunctionDAG(const vector<Function> &outputs, const MachineParams &p
 
     int stage_count = 0;
 
+    FuncValueBounds func_value_bounds = compute_function_value_bounds(order, env);
+    for (auto &p : func_value_bounds) {
+        p.second.min = apply_param_estimates.mutate(p.second.min);
+        p.second.max = apply_param_estimates.mutate(p.second.max);
+    }
+
     for (size_t i = order.size(); i > 0; i--) {
         Function consumer = env[order[i-1]];
 
@@ -645,7 +651,7 @@ FunctionDAG::FunctionDAG(const vector<Function> &outputs, const MachineParams &p
             }
             for (int j = 0; j < consumer.dimensions(); j++) {
                 // The region computed always uses the full extent of the rvars
-                Interval in = bounds_of_expr_in_scope(def.args()[j], stage_scope_with_concrete_rvar_bounds);
+                Interval in = bounds_of_expr_in_scope(def.args()[j], stage_scope_with_concrete_rvar_bounds, func_value_bounds);
                 internal_assert(in.is_bounded())
                     << "Region computed of " << consumer.name()
                     << " is unbounded: [" << in.min << " " << in.max << "]\n";
@@ -877,12 +883,6 @@ FunctionDAG::FunctionDAG(const vector<Function> &outputs, const MachineParams &p
             stage.index = s;
 
             exprs = apply_param_estimates.mutate(exprs);
-
-            FuncValueBounds func_value_bounds = compute_function_value_bounds(order, env);
-            for (auto &p : func_value_bounds) {
-                p.second.min = apply_param_estimates.mutate(p.second.min);
-                p.second.max = apply_param_estimates.mutate(p.second.max);
-            }
 
             // For this stage scope we want symbolic bounds for the rvars
 
