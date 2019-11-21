269acc72b7d63c6d30e117b54d0c1bcad4e5cbea
Benoit Steiner <benoitsteiner@fb.com>
2019-11-21 23:33:27 +0000

    Added a test for FunctionDAG

--- apps/autoscheduler/test_function_dag.cpp
+++ apps/autoscheduler/test_function_dag.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "FunctionDAG.h"
 #include "Halide.h"
 #include <sstream>
@@ -29,6 +30,39 @@ extern "C" int generate_output_vals(
         }
     }
     return 0;
+=======
+#include <sstream>
+#include "Halide.h"
+#include "FunctionDAG.h"
+
+using namespace Halide;
+
+extern "C" int generateOutputVals(
+    halide_buffer_t* input,
+    halide_buffer_t* output) {
+  if (input->is_bounds_query()) {
+    // Bounds query: infer the input dimensions from the output dimensions. In
+    // this example, the dimensions are exactly the same
+    for (int i = 0; i < 2; ++i) {
+      input->dim[i] = output->dim[i];
+    }
+    return 0;
+  }
+
+  // Actual computation: return 2 times x as an example. The first dimension is
+  // the innermost, so iterate over it last to avoid inefficient memory access
+  // patterns.
+  for (int j = 0; j < input->dim[1].extent; ++j) {
+    for (int i = 0; i < input->dim[0].extent; ++i) {
+      float* out = (float*)output->host + i * output->dim[0].stride +
+          j * output->dim[1].stride;
+      float* in = (float*)input->host + i * input->dim[0].stride +
+          j * input->dim[1].stride;
+      (*out) = 2 * (*in);
+    }
+  }
+  return 0;
+>>>>>>> Added a test for FunctionDAG
 }
 
 int main(int argc, char **argv) {
@@ -47,12 +81,20 @@ int main(int argc, char **argv) {
         const int input_rank = 2;
         Halide::Type input_type = Halide::Float(32);
         g.define_extern(
+<<<<<<< HEAD
             "generate_output_vals",
             {arg},
             input_type,
             input_rank,
             Halide::NameMangling::C);
         g.function().extern_definition_proxy_expr() = f(x, y) * 2.0f;
+=======
+                        "generateOutputVals",
+                        {arg},
+                        input_type,
+                        input_rank,
+                        Halide::NameMangling::C /*PlusPlus*/);
+>>>>>>> Added a test for FunctionDAG
 
         h(x, y) = g(x, y) * 2 + 1;
 
@@ -61,6 +103,7 @@ int main(int argc, char **argv) {
         v.push_back(h.function());
         Halide::Internal::Autoscheduler::FunctionDAG d(v, params, target);
 
+<<<<<<< HEAD
         d.dump(with_extern);
     }
 
@@ -69,6 +112,16 @@ int main(int argc, char **argv) {
         Func f("f"), g("g"), h("h");
         f(x, y) = (x + y) * (x + y);
         g(x, y) = f(x, y) * 2.0f;
+=======
+	d.dump(with_extern);
+    }
+
+   std::ostringstream without_extern;
+    {
+        Func f("f"), g("g"), h("h");
+        f(x, y) = (x + y) * (x + y);
+	g(x, y) = f(x, y) * 2.0f;
+>>>>>>> Added a test for FunctionDAG
         h(x, y) = g(x, y) * 2 + 1;
 
         h.set_estimate(x, 0, 1000).set_estimate(y, 0, 1000);
@@ -79,7 +132,11 @@ int main(int argc, char **argv) {
         d.dump(without_extern);
     }
 
+<<<<<<< HEAD
     // Disabled for now: there is still work to do to populate the jacobian
+=======
+    // Disabled for now, as we're not there yet
+>>>>>>> Added a test for FunctionDAG
     //assert(with_extern.str() == without_extern.str());
 
     return 0;
