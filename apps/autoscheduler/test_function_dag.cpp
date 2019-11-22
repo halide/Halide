45df0ce1c332f58158336d5502c3ffef3f80043a
Benoit Steiner <benoitsteiner@fb.com>
2019-11-22 00:28:59 +0000

    Use the proxy (if specified) to generate pipeline features for extern function calls

--- apps/autoscheduler/test_function_dag.cpp
+++ apps/autoscheduler/test_function_dag.cpp
@@ -1,34 +1,34 @@
-#include <sstream>
-#include "Halide.h"
 #include "FunctionDAG.h"
+#include "Halide.h"
+#include <sstream>
 
 using namespace Halide;
 
 extern "C" int generateOutputVals(
-    halide_buffer_t* input,
-    halide_buffer_t* output) {
-  if (input->is_bounds_query()) {
-    // Bounds query: infer the input dimensions from the output dimensions. In
-    // this example, the dimensions are exactly the same
-    for (int i = 0; i < 2; ++i) {
-      input->dim[i] = output->dim[i];
+    halide_buffer_t *input,
+    halide_buffer_t *output) {
+    if (input->is_bounds_query()) {
+        // Bounds query: infer the input dimensions from the output dimensions. In
+        // this example, the dimensions are exactly the same
+        for (int i = 0; i < 2; ++i) {
+            input->dim[i] = output->dim[i];
+        }
+        return 0;
+    }
+
+    // Actual computation: return 2 times x as an example. The first dimension is
+    // the innermost, so iterate over it last to avoid inefficient memory access
+    // patterns.
+    for (int j = 0; j < input->dim[1].extent; ++j) {
+        for (int i = 0; i < input->dim[0].extent; ++i) {
+            float *out = (float *)output->host + i * output->dim[0].stride +
+                         j * output->dim[1].stride;
+            float *in = (float *)input->host + i * input->dim[0].stride +
+                        j * input->dim[1].stride;
+            (*out) = 2 * (*in);
+        }
     }
     return 0;
-  }
-
-  // Actual computation: return 2 times x as an example. The first dimension is
-  // the innermost, so iterate over it last to avoid inefficient memory access
-  // patterns.
-  for (int j = 0; j < input->dim[1].extent; ++j) {
-    for (int i = 0; i < input->dim[0].extent; ++i) {
-      float* out = (float*)output->host + i * output->dim[0].stride +
-          j * output->dim[1].stride;
-      float* in = (float*)input->host + i * input->dim[0].stride +
-          j * input->dim[1].stride;
-      (*out) = 2 * (*in);
-    }
-  }
-  return 0;
 }
 
 int main(int argc, char **argv) {
@@ -47,11 +47,12 @@ int main(int argc, char **argv) {
         const int input_rank = 2;
         Halide::Type input_type = Halide::Float(32);
         g.define_extern(
-                        "generateOutputVals",
-                        {arg},
-                        input_type,
-                        input_rank,
-                        Halide::NameMangling::C /*PlusPlus*/);
+            "generateOutputVals",
+            {arg},
+            input_type,
+            input_rank,
+            Halide::NameMangling::C /*PlusPlus*/);
+        g.function().extern_definition_proxy_expr() = f(x, y) * 2.0f;
 
         h(x, y) = g(x, y) * 2 + 1;
 
@@ -60,14 +61,14 @@ int main(int argc, char **argv) {
         v.push_back(h.function());
         Halide::Internal::Autoscheduler::FunctionDAG d(v, params, target);
 
-	d.dump(with_extern);
+        d.dump(with_extern);
     }
 
-   std::ostringstream without_extern;
+    std::ostringstream without_extern;
     {
         Func f("f"), g("g"), h("h");
         f(x, y) = (x + y) * (x + y);
-	g(x, y) = f(x, y) * 2.0f;
+        g(x, y) = f(x, y) * 2.0f;
         h(x, y) = g(x, y) * 2 + 1;
 
         h.set_estimate(x, 0, 1000).set_estimate(y, 0, 1000);
@@ -78,7 +79,7 @@ int main(int argc, char **argv) {
         d.dump(without_extern);
     }
 
-    // Disabled for now, as we're not there yet
+    // Disabled for now: there is still work to do to populate the jacobian
     //assert(with_extern.str() == without_extern.str());
 
     return 0;
