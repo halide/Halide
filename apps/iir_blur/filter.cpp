282b88a27f7859964f57419624dfaa58061985ce
Andrew Adams <andrew.b.adams@gmail.com>
2019-12-03 15:25:08 -0800

    Rework interpolate. Add IIR blur.

---
+++ apps/iir_blur/filter.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+
+#include "HalideRuntime.h"
+#include "HalideBuffer.h"
+
+#include "iir_blur.h"
+#include "iir_blur_auto_schedule.h"
+
+#include "halide_benchmark.h"
+#include "halide_image_io.h"
+
+using namespace Halide::Tools;
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Usage: %s in out\n", argv[0]);
+        return 1;
+    }
+
+    Halide::Runtime::Buffer<float> input = load_and_convert_image(argv[1]);
+    Halide::Runtime::Buffer<float> output(input.width(), input.height(), input.channels());
+
+    double best_manual = benchmark([&]() {
+        iir_blur(input, 0.5f, output);
+        output.device_sync();
+    });
+    printf("Manually-tuned time: %gms\n", best_manual * 1e3);
+
+    double best_auto = benchmark([&]() {
+        iir_blur_auto_schedule(input, 0.5f, output);
+        output.device_sync();
+    });
+    printf("Auto-scheduled time: %gms\n", best_auto * 1e3);
+
+    convert_and_save_image(output, argv[2]);
+
+    return 0;
+}
