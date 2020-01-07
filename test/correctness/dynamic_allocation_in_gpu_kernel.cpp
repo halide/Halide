8d18445e66de07025d673f4fb2c80a33e44cb384
Andrew Adams <andrew.b.adams@gmail.com>
2020-01-07 14:11:26 -0800

    Update existing gpu memory tests to exercise new functionality

--- test/correctness/dynamic_allocation_in_gpu_kernel.cpp
+++ test/correctness/dynamic_allocation_in_gpu_kernel.cpp
@@ -14,10 +14,33 @@ int main(int argc, char **argv) {
     g.gpu_tile(x, y, xi, yi, 32, 32);
     f.compute_at(g, xi);
 
+    g.specialize(p == 3);
+    // For the p == 3 case, f's allocation is statically sized, so it
+    // will go in local memory. For all other cases the allocation is
+    // dynamically sized and per-thread so it will go in global memory
+    // by default. It would also be legal to schedule it into shared,
+    // but for p > 3 there isn't enough shared memory.
+
+    constexpr int W = 128, H = 128;
+
     for (int i = 0; i < 10; i++) {
         p.set(i);
-        g.realize(128, 128);
+        Buffer<int> result = g.realize(W, H);
+        result.copy_to_host();
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                int correct = x + y + (x + i) + (y + i);
+                int actual = result(x, y);
+                if (correct != actual) {
+                    printf("result(%d, %d) = %d instead of %d\n",
+                           x, y, actual, correct);
+                    return -1;
+                }
+            }
+        }
     }
+
+    printf("Success!\n");
 
     return 0;
 }
