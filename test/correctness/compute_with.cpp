d17e85acd2fdd2eed787fb6f74c80197e96cdead
Volodymyr Kysenko <vksnk@google.com>
2020-03-25 10:19:12 -0700

    More tests

--- test/correctness/compute_with.cpp
+++ test/correctness/compute_with.cpp
@@ -1292,6 +1292,150 @@ int update_stage_test() {
     return 0;
 }
 
+// two in row.
+int update_stage2_test() {
+    const int f_size = 128;
+    const int g_size = 128;
+
+    Buffer<int> f_im(f_size, f_size), g_im(g_size, g_size);
+    Buffer<int> f_im_ref(f_size, f_size), g_im_ref(g_size, g_size);
+
+    {
+        Var x("x"), y("y");
+        Func f("f"), g("g");
+
+        g(x, y) = 4;
+        g(x, y) = g(x, y) + 1;
+        g(x, y) = g(x, y) + 3;
+
+        f(x, y) = 5;
+        f(x, y) = f(x, y) + 1;
+        f(x, y) = f(x, y) + 3;
+
+        f.compute_root();
+        g.compute_root();
+
+        g.bound(x, 0, g_size).bound(y, 0, g_size);
+        f.bound(x, 0, f_size).bound(y, 0, f_size);
+
+        Pipeline p({f, g});
+        p.realize({f_im_ref, g_im_ref});
+    }
+
+    {
+        Var x("x"), y("y");
+        Func f("f"), g("g");
+
+        g(x, y) = 4;
+        g(x, y) = g(x, y) + 1;
+        g(x, y) = g(x, y) + 3;
+
+        f(x, y) = 5;
+        f(x, y) = f(x, y) + 1;
+        f(x, y) = f(x, y) + 3;
+
+        g.compute_root();
+        f.compute_root();
+
+        f.update(0).compute_with(g.update(0), y);
+        f.update(1).compute_with(g.update(0), y);
+
+        g.bound(x, 0, g_size).bound(y, 0, g_size);
+        f.bound(x, 0, f_size).bound(y, 0, f_size);
+
+        Pipeline p({f, g});
+        p.realize({f_im, g_im});
+    }
+
+    auto f_func = [f_im_ref](int x, int y) {
+        return f_im_ref(x, y);
+    };
+    if (check_image(f_im, f_func)) {
+        return -1;
+    }
+
+    auto g_func = [g_im_ref](int x, int y) {
+        return g_im_ref(x, y);
+    };
+    if (check_image(g_im, g_func)) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int update_stage3_test() {
+    const int f_size = 128;
+    const int g_size = 128;
+
+    Buffer<int> f_im(f_size, f_size), g_im(g_size, g_size);
+    Buffer<int> f_im_ref(f_size, f_size), g_im_ref(g_size, g_size);
+
+    {
+        Var x("x"), y("y");
+        Func f("f"), g("g");
+
+        g(x, y) = 4;
+        g(x, y) = g(x, y) + 1;
+        g(x, y) = g(x, y) + 3;
+
+        f(x, y) = 5;
+        f(x, y) = f(x, y) + 1;
+        f(x, y) = f(x, y) + 3;
+
+        f.compute_root();
+        g.compute_root();
+
+        g.bound(x, 0, g_size).bound(y, 0, g_size);
+        f.bound(x, 0, f_size).bound(y, 0, f_size);
+
+        Pipeline p({f, g});
+        p.realize({f_im_ref, g_im_ref});
+    }
+
+    {
+        Var x("x"), y("y");
+        Func f("f"), g("g");
+
+        g(x, y) = 4;
+        g(x, y) = g(x, y) + 1;
+        g(x, y) = g(x, y) + 3;
+
+        f(x, y) = 5;
+        f(x, y) = f(x, y) + 1;
+        f(x, y) = f(x, y) + 3;
+
+        g.compute_root();
+        f.compute_root();
+
+        f.compute_with(g, y);
+        f.update(0).compute_with(g, y);
+        f.update(1).compute_with(g, y);
+
+        g.bound(x, 0, g_size).bound(y, 0, g_size);
+        f.bound(x, 0, f_size).bound(y, 0, f_size);
+
+        Pipeline p({f, g});
+        p.realize({f_im, g_im});
+    }
+
+    auto f_func = [f_im_ref](int x, int y) {
+        return f_im_ref(x, y);
+    };
+    if (check_image(f_im, f_func)) {
+        return -1;
+    }
+
+    auto g_func = [g_im_ref](int x, int y) {
+        return g_im_ref(x, y);
+    };
+    if (check_image(g_im, g_func)) {
+        return -1;
+    }
+
+    return 0;
+}
+
 int vectorize_inlined_test() {
     const int f_size = 128;
     const int g_size = 256;
@@ -1484,6 +1628,16 @@ int main(int argc, char **argv) {
 
     printf("Running update stage test\n");
     if (update_stage_test() != 0) {
+        return -1;
+    }
+
+    printf("Running update stage2 test\n");
+    if (update_stage2_test() != 0) {
+        return -1;
+    }
+
+    printf("Running update stage3 test\n");
+    if (update_stage3_test() != 0) {
         return -1;
     }
 
