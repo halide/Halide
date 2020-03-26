5157e1bfbf90d3f7804878fd783b25c6424e12d8
Tzu-Mao Li <tzumao@mit.edu>
2020-03-26 14:07:02 -0400

    push benchmark model script

---
+++ apps/onnx/benchmark_models.py
@@ -0,0 +1,28 @@
+""" Given a list of prototxt files, compile them to Halide and benchmark them.
+"""
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='+')
+    args = parser.parse_args()
+
+    for f in args.files:
+        print('Processing', f)
+        model_name = os.path.splitext(os.path.basename(f))[0]
+        # Compile the generator
+        cmd = ('bin/host/onnx_converter.generator '
+               '-e static_library,h,registration,python_extension '
+               '-g onnx_model_generator '
+               '-o bin/host '
+               '-f {} '
+               '-p ../gradient_autoscheduler/bin/libgradient_autoscheduler.so '
+               '-s Li2018 '
+               'target=host '
+               'model_file_path={}').format(model_name, f)
+        print(cmd)
+        os.system(cmd)
+
+if __name__ == '__main__':
+    main()
