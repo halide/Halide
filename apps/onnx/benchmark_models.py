42a63caebe4dc00b97bf1eef27523e4467c17c0e
Tzu-Mao Li <tzumao@persephone.csail.mit.edu>
2020-03-27 00:06:05 -0400

    add compilation

--- apps/onnx/benchmark_models.py
+++ apps/onnx/benchmark_models.py
@@ -2,26 +2,48 @@
 """
 import argparse
 import os
+import sysconfig
+
+python_include_path = sysconfig.get_paths()['include']
+python_cflags = '-I' + python_include_path + ' '
+python_cflags += sysconfig.get_config_var('CFLAGS') + ' '
+python_cflags += '-fPIC '
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='+')
     args = parser.parse_args()
+    os.system('mkdir -p bin/host-cuda')
 
     for f in args.files:
         print('Processing', f)
         model_name = os.path.splitext(os.path.basename(f))[0]
-        # Compile the generator
+        # Compile the generators
+        print('Compiling with the Li2018 autoscheduler')
         cmd = ('bin/host/onnx_converter.generator '
-               '-e static_library,h,registration,python_extension '
+               '-e object,python_extension '
                '-g onnx_model_generator '
-               '-o bin/host '
-               '-f {} '
+               '-o bin/host-cuda '
+               '-f {}_li2018 '
                '-p ../gradient_autoscheduler/bin/libgradient_autoscheduler.so '
                '-s Li2018 '
-               'target=host '
+               'target=host-cuda '
                'auto_schedule=true '
                'model_file_path={}').format(model_name, f)
+        print(cmd)
+        os.system(cmd)
+        # Compile the python extension to an object file
+        cmd = 'g++ ' + \
+              python_cflags + \
+              '-I../../distrib/include ' + \
+              '-c -o bin/host-cuda/tmp.o bin/host-cuda/{}_li2018.py.cpp'.format(model_name)
+        print(cmd)
+        os.system(cmd)
+        # Link
+        cmd = 'g++ ' + \
+              python_cflags + \
+              '-shared -o bin/host-cuda/{}_li2018.so bin/host-cuda/tmp.o bin/host-cuda/{}_li2018.o'.format(
+                      model_name, model_name)
         print(cmd)
         os.system(cmd)
 
