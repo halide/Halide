6831b2713f24ae87aa1e9328f91471a0fdd9b4af
Mark Glines <mark@glines.org>
2020-04-10 13:10:05 -0400

    Indentation fix.

--- python_bindings/src/PyRDom.cpp
+++ python_bindings/src/PyRDom.cpp
@@ -35,7 +35,7 @@ void define_rdom(py::module &m) {
             .def("dimensions", &RDom::dimensions)
             .def("where", &RDom::where, py::arg("predicate"))
             .def("__getitem__", [](RDom &r, const int i) -> RVar {
-                if(i < 0 || i >= r.dimensions())
+                if (i < 0 || i >= r.dimensions())
                     throw pybind11::key_error();
                 return r[i];
             })
