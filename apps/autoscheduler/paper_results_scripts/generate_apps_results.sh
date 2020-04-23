b0b38de845140066deee5c1e70895e4a3a59fa7e
AmeerHajAli <ameerh@berkeley.edu>
2020-04-23 16:21:38 -0700

    look at previous commit

--- apps/autoscheduler/paper_results_scripts/generate_apps_results.sh
+++ apps/autoscheduler/paper_results_scripts/generate_apps_results.sh
@@ -48,11 +48,11 @@ elif [ "$autoscheduler" == "mcts" ]; then
     fi
 
     # mcts
-    export HL_NUM_PASSES=1
+    export HL_NUM_PASSES=5
     export MCTS_MAX_MILLIS=0
-    export MCTS_MAX_ITERATIONS=16
-    export MCTS_SIMULATION_DEPTH=5
-    export MCTS_DEPTH=16
+    export MCTS_MAX_ITERATIONS=512
+    export MCTS_SIMULATION_DEPTH=1
+    export MCTS_DEPTH=8
     results="mcts"
 elif [ "$autoscheduler" == "master" ]; then
     # master
