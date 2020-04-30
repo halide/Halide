2e064d0da84331a169c82e5a32f30ee6a94a4d07
Andrew Adams <andrew.b.adams@gmail.com>
2020-04-30 15:00:06 -0700

    Fix parsing of let exprs

--- apps/super_simplify/parser.h
+++ apps/super_simplify/parser.h
@@ -10,31 +10,28 @@
 
 // Move the input cursor past any whitespace, but not beyond the end
 // pointer.
-void consume_whitespace(char **cursor, char *end);
+void consume_whitespace(const char **cursor, const char *end);
 
 // If the input cursor starts with the expected string, update it to
 // point to the end of the string and return true. Otherwise, return
 // false and don't modify the input cursor.
-bool consume(char **cursor, char *end, const char *expected);
+bool consume(const char **cursor, const char *end, const char *expected);
 
 // Calls consume and asserts that it succeeded.
-void expect(char **cursor, char *end, const char *pattern);
-
-// Equivalent to consume but does not update the cursor on success.
-bool check(char **cursor, char *end, const char *pattern);
+void expect(const char **cursor, const char *end, const char *pattern);
 
 // Consume and return a legal Halide identifier.
-std::string consume_token(char **cursor, char *end);
+std::string consume_token(const char **cursor, const char *end);
 
 // Consume and return a constant integer.
-int64_t consume_int(char **cursor, char *end);
+int64_t consume_int(const char **cursor, const char *end);
 
 // Consume and return a constant float as a constant Halide Expr of
 // the appropriate type.
-Halide::Expr consume_float(char **cursor, char *end);
+Halide::Expr consume_float(const char **cursor, const char *end);
 
 // Parse a full Halide Expr, as produced by a Halide IRPrinter elsewhere.
-Halide::Expr parse_halide_expr(char **cursor, char *end, Halide::Type expected_type);
+Halide::Expr parse_halide_expr(const char **cursor, const char *end, Halide::Type expected_type);
 
 std::vector<Halide::Expr> parse_halide_exprs_from_file(const std::string &filename);
 #endif
