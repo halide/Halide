f4cf2a61a2501b20341de4f9140d8d60bbc0248c
Andrew Adams <andrew.b.adams@gmail.com>
2020-04-30 15:03:02 -0700

    Predicate synthesis improvements

--- apps/super_simplify/synthesize_predicate.h
+++ apps/super_simplify/synthesize_predicate.h
@@ -4,16 +4,13 @@
 #include "Halide.h"
 
 bool can_disprove_nonconvex(Halide::Expr e, int beam_size, Halide::Expr *implication = nullptr);
-bool can_disprove(Halide::Expr e, int beam_size, std::set<Halide::Expr, Halide::Internal::IRDeepCompare> *implications = nullptr);
 
-// Takes a LHS and RHS with symbolic constants, and a list of example
-// bindings for the constants for which LHS has been proved to equal
-// RHS, and returns a sufficient condition on the constants for which
-// lhs is known to equal rhs. Also returns expressions that give the
-// value of any symbolic constants that only appear in the rhs.
+// Takes a LHS and RHS with symbolic constants, and returns a
+// sufficient condition on the constants for which lhs is known to
+// equal rhs. Also returns expressions that give the value of any
+// symbolic constants that only appear in the rhs.
 Halide::Expr synthesize_predicate(const Halide::Expr &lhs,
                                   const Halide::Expr &rhs,
-                                  const std::vector<std::map<std::string, Halide::Expr>> &examples,
                                   std::map<std::string, Halide::Expr> *binding, int beam_size = 16);
 
 #endif
