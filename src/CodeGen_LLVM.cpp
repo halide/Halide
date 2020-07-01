437e37ebfa0c062bda65e6ca5086ecad6f5ecc4d
Andrew Adams <andrew.b.adams@gmail.com>
2020-07-01 12:08:58 -0700

    Restrict total reductions to llvm 10+ to workaround arm issue

--- src/CodeGen_LLVM.cpp
+++ src/CodeGen_LLVM.cpp
@@ -4347,7 +4347,7 @@ void CodeGen_LLVM::codegen_vector_reduce(const VectorReduce *op, const Expr &ini
         return;
     }
 
-#if LLVM_VERSION >= 90
+#if LLVM_VERSION >= 100
     if (output_lanes == 1) {
         const int input_lanes = val.type().lanes();
         const int input_bytes = input_lanes * val.type().bytes();
