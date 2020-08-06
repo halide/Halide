73b85601c20e85ed6f13250b5806d5268d9c2993
Alex Reinking <alex.reinking@gmail.com>
2020-08-05 20:41:21 -0700

    Improve readability.

--- tools/test-packaging-linux.sh
+++ tools/test-packaging-linux.sh
@@ -2,7 +2,14 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
 
-FLAGS="-DWITH_TESTS=NO -DWITH_TUTORIALS=NO -DWITH_PYTHON_BINDINGS=NO -DWITH_APPS=YES -DWITH_DOCS=NO -DWITH_UTILS=NO"
+FLAGS=(
+  "-DWITH_TESTS=NO"
+  "-DWITH_TUTORIALS=NO"
+  "-DWITH_PYTHON_BINDINGS=NO"
+  "-DWITH_APPS=YES"
+  "-DWITH_DOCS=NO"
+  "-DWITH_UTILS=NO"
+)
 
 Halide_static="-DBUILD_SHARED_LIBS=NO"
 Halide_shared="-DBUILD_SHARED_LIBS=YES"
@@ -17,13 +24,13 @@ for HL in static shared; do
       continue
     fi
 
-    Halide_flags_var=Halide_$HL
-    LLVM_flags_var=LLVM_$LLVM
+    Halide_flags_var="Halide_$HL"
+    LLVM_flags_var="LLVM_$LLVM"
     build_dir="$DIR/build/release-$HL-$LLVM"
 
     echo HL=$HL LLVM=$LLVM
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ${FLAGS} ${!Halide_flags_var} ${!LLVM_flags_var} -S "$DIR" -B "$build_dir"
-    cmake --build ${build_dir} && (cd ${build_dir} && ctest -R bgu)
+    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release "${FLAGS[@]}" "${!Halide_flags_var}" "${!LLVM_flags_var}" -S "$DIR" -B "$build_dir"
+    cmake --build "$build_dir" && (cd "$build_dir" && ctest -R conv_layer)
     echo
     echo
   done
