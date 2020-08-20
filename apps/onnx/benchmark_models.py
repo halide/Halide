b587614ec99a0020ef2d6b85eae433d60a4848ed
Tzu-Mao Li <tzumao@node0014.inband>
2020-08-20 16:27:39 -0400

    onnx benchmark script for sioutas

--- apps/onnx/benchmark_models.py
+++ apps/onnx/benchmark_models.py
@@ -42,40 +42,44 @@ def main():
         print('Processing ', f)
         onnx_model = onnx.load(f)
         inputs = get_model_inputs(onnx_model)
-        tf_model = onnx_tf.backend.prepare(onnx_model, 'GPU')
-        # Copy the TF graph and replace the Placeholder
-        graph_def = tf_model.graph.as_graph_def()
-        new_graph_def = graph_pb2.GraphDef()
-        for node in graph_def.node:
-            if node.op == 'Placeholder':
-                shape = [d.size for d in (node.attr['shape'].shape.dim)]
-                b = tf.constant(0.5, shape=shape, name=node.name)
-                new_graph_def.node.extend([b.op.node_def])
-            else:
-                new_graph_def.node.extend([copy.deepcopy(node)])
-        output_name = new_graph_def.node[-1].name
-        tf.import_graph_def(new_graph_def)
-
-        with tf.compat.v1.Session() as sess:
-            with tf.device('/device:gpu:0'):
-                graph = tf.compat.v1.get_default_graph()
-                feed_dict = {}
-                sess.run(tf.compat.v1.global_variables_initializer())
-                outputs = [graph.get_tensor_by_name('import/' + output_name + ':0')]
-
-                run_options = tf.compat.v1.RunOptions(trace_level=tf.compat.v1.RunOptions.NO_TRACE)
-
-                output_values = sess.run(outputs, feed_dict=feed_dict, options=run_options)
-
-                num_iter = 20
-                min_time = 1e20
-                for i in range(num_iter):
-                    beg = time.time_ns()
+        if False:
+            tf_model = onnx_tf.backend.prepare(onnx_model, 'GPU')
+            # Copy the TF graph and replace the Placeholder
+            graph_def = tf_model.graph.as_graph_def()
+            new_graph_def = graph_pb2.GraphDef()
+            for node in graph_def.node:
+                if node.op == 'Placeholder':
+                    shape = [d.size for d in (node.attr['shape'].shape.dim)]
+                    with tf.device('/device:gpu:0'):
+                        b = tf.constant(0.5, shape=shape, name=node.name)
+                    new_graph_def.node.extend([b.op.node_def])
+                else:
+                    n = copy.deepcopy(node)
+                    #n.device = '/device:gpu:0'
+                    new_graph_def.node.extend([n])
+            output_name = new_graph_def.node[-1].name
+            tf.import_graph_def(new_graph_def)
+    
+            with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=False)) as sess:
+                with tf.device('/device:gpu:0'):
+                    graph = tf.compat.v1.get_default_graph()
+                    feed_dict = {}
+                    sess.run(tf.compat.v1.global_variables_initializer())
+                    outputs = [graph.get_tensor_by_name('import/' + output_name + ':0')]
+    
+                    run_options = tf.compat.v1.RunOptions(trace_level=tf.compat.v1.RunOptions.NO_TRACE)
+    
                     output_values = sess.run(outputs, feed_dict=feed_dict, options=run_options)
-                    end = time.time_ns()
-                    t = ((end - beg) / (10 ** 9))
-                    min_time = min(t, min_time)
-                print('{}: TensorFlow: {}s'.format(f, min_time))
+    
+                    num_iter = 20
+                    min_time = 1e20
+                    for i in range(num_iter):
+                        beg = time.time_ns()
+                        output_values = sess.run(outputs, feed_dict=feed_dict, options=run_options)
+                        end = time.time_ns()
+                        t = ((end - beg) / (10 ** 9))
+                        min_time = min(t, min_time)
+                    print('{}: TensorFlow: {}s'.format(f, min_time))
 
         print('Preparing Halide inputs and outputs')
         outputs = onnx_model.graph.output
@@ -95,9 +99,7 @@ def main():
             b.fill(0.0)
             b.copy_to_device()
             halide_outputs.append(b)
-
         model_name = os.path.splitext(os.path.basename(f))[0]
-
         # Compile the generators
         print('Compiling with the Li2018 autoscheduler')
         cmd = ('bin/host/onnx_converter.generator '
@@ -129,17 +131,68 @@ def main():
         print(cmd)
         os.system(cmd)
         m = __import__(model_name + '_li2018')
-        f = m.__dict__[model_name + '_li2018']
+        func = m.__dict__[model_name + '_li2018']
+        func(*halide_inputs, *halide_outputs)
+        for j in halide_outputs:
+            j.device_sync()
         num_iter = 20
         min_time = 1e20
         for i in range(num_iter):
             beg = time.time_ns()
-            f(*halide_inputs, *halide_outputs)
+            func(*halide_inputs, *halide_outputs)
+            for j in halide_outputs:
+                j.device_sync()
             end = time.time_ns()
             t = ((end - beg) / (10 ** 9))
             min_time = min(t, min_time)
-        print('{}: Gradient autoscheduler: {}s'.format(f, min_time))
+        print('{}: Gradient autoscheduler: {}s'.format(model_name, min_time))
 
+        # Compile the generators
+        print('Compiling with the Sioutas2020 autoscheduler')
+        cmd = ('bin/host/onnx_converter.generator '
+               '-e object,python_extension '
+               '-g onnx_model_generator '
+               '-o bin/host-cuda '
+               '-f {}_sioutas2020 '
+               '-p ../sioutas2020/bin/libauto_schedule.so '
+               '-s Sioutas2020 '
+               'target=host-cuda '
+               'auto_schedule=true '
+               'model_file_path={}').format(model_name, f)
+        print(cmd)
+        os.system(cmd)
+        # Compile the python extension to an object file
+        cmd = 'g++ ' + \
+              python_cflags + \
+              '-I../../distrib/include ' + \
+              '-std=c++11 ' + \
+              '-c -o bin/host-cuda/tmp.o bin/host-cuda/{}_sioutas2020.py.cpp'.format(model_name)
+        print(cmd)
+        os.system(cmd)
+        # Link
+        cmd = 'g++ ' + \
+              python_cflags + \
+              '-std=c++11 ' + \
+              '-shared -o bin/host-cuda/{}_sioutas2020.so bin/host-cuda/tmp.o bin/host-cuda/{}_sioutas2020.o'.format(
+                      model_name, model_name)
+        print(cmd)
+        os.system(cmd)
+        m = __import__(model_name + '_sioutas2020')
+        func = m.__dict__[model_name + '_sioutas2020']
+        func(*halide_inputs, *halide_outputs)
+        for j in halide_outputs:
+            j.device_sync()
+        num_iter = 20
+        min_time = 1e20
+        for i in range(num_iter):
+            beg = time.time_ns()
+            func(*halide_inputs, *halide_outputs)
+            for j in halide_outputs:
+                j.device_sync()
+            end = time.time_ns()
+            t = ((end - beg) / (10 ** 9))
+            min_time = min(t, min_time)
+        print('{}: Sioutas autoscheduler: {}s'.format(f, min_time))
 
 if __name__ == '__main__':
     main()
