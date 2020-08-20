c1a5bdd1ffa5c819c842211a766602c3f615fe2b
Tzu-Mao Li <tzumao@service0001.inband>
2020-08-20 09:43:54 -0400

    Update onnx benchmark script

--- apps/onnx/benchmark_models.py
+++ apps/onnx/benchmark_models.py
@@ -10,6 +10,12 @@ sys.path.append('../../python_bindings/bin') # python binding
 import halide as hl
 sys.path.append('./bin/host-cuda')
 import time
+import tensorflow as tf
+from tensorflow.core.framework import graph_pb2
+import copy
+import onnx_tf.backend
+
+tf.compat.v1.disable_eager_execution()
 
 python_include_path = sysconfig.get_paths()['include']
 python_cflags = '-I' + python_include_path + ' '
@@ -33,11 +39,45 @@ def main():
     os.system('mkdir -p bin/host-cuda')
 
     for f in args.files:
-        print('Processing', f)
-
-        print('Preparing inputs and outputs')
+        print('Processing ', f)
         onnx_model = onnx.load(f)
         inputs = get_model_inputs(onnx_model)
+        tf_model = onnx_tf.backend.prepare(onnx_model, 'GPU')
+        # Copy the TF graph and replace the Placeholder
+        graph_def = tf_model.graph.as_graph_def()
+        new_graph_def = graph_pb2.GraphDef()
+        for node in graph_def.node:
+            if node.op == 'Placeholder':
+                shape = [d.size for d in (node.attr['shape'].shape.dim)]
+                b = tf.constant(0.5, shape=shape, name=node.name)
+                new_graph_def.node.extend([b.op.node_def])
+            else:
+                new_graph_def.node.extend([copy.deepcopy(node)])
+        output_name = new_graph_def.node[-1].name
+        tf.import_graph_def(new_graph_def)
+
+        with tf.compat.v1.Session() as sess:
+            with tf.device('/device:gpu:0'):
+                graph = tf.compat.v1.get_default_graph()
+                feed_dict = {}
+                sess.run(tf.compat.v1.global_variables_initializer())
+                outputs = [graph.get_tensor_by_name('import/' + output_name + ':0')]
+
+                run_options = tf.compat.v1.RunOptions(trace_level=tf.compat.v1.RunOptions.NO_TRACE)
+
+                output_values = sess.run(outputs, feed_dict=feed_dict, options=run_options)
+
+                num_iter = 20
+                min_time = 1e20
+                for i in range(num_iter):
+                    beg = time.time_ns()
+                    output_values = sess.run(outputs, feed_dict=feed_dict, options=run_options)
+                    end = time.time_ns()
+                    t = ((end - beg) / (10 ** 9))
+                    min_time = min(t, min_time)
+                print('{}: TensorFlow: {}s'.format(f, min_time))
+
+        print('Preparing Halide inputs and outputs')
         outputs = onnx_model.graph.output
         halide_inputs = []
         for i in inputs:
@@ -76,41 +116,30 @@ def main():
         cmd = 'g++ ' + \
               python_cflags + \
               '-I../../distrib/include ' + \
+              '-std=c++11 ' + \
               '-c -o bin/host-cuda/tmp.o bin/host-cuda/{}_li2018.py.cpp'.format(model_name)
         print(cmd)
         os.system(cmd)
         # Link
         cmd = 'g++ ' + \
               python_cflags + \
+              '-std=c++11 ' + \
               '-shared -o bin/host-cuda/{}_li2018.so bin/host-cuda/tmp.o bin/host-cuda/{}_li2018.o'.format(
                       model_name, model_name)
         print(cmd)
         os.system(cmd)
-        from mobilenetv2_li2018 import mobilenetv2_li2018
+        m = __import__(model_name + '_li2018')
+        f = m.__dict__[model_name + '_li2018']
         num_iter = 20
-        beg = time.time_ns()
+        min_time = 1e20
         for i in range(num_iter):
-            mobilenetv2_li2018(*halide_inputs, *halide_outputs)
-        end = time.time_ns()
+            beg = time.time_ns()
+            f(*halide_inputs, *halide_outputs)
+            end = time.time_ns()
+            t = ((end - beg) / (10 ** 9))
+            min_time = min(t, min_time)
         t = ((end - beg) / (10 ** 9)) / num_iter
-        print('time for gradient autoscheduler: {}s'.format(t))
-
-        print('Running the autotuning loop for Anderson2020')
-        cmd = ('HL_MACHINE_PARAMS=80,1,1 '
-               'SAMPLES_DIR={}_autotuned_samples '
-               'HL_PERMIT_FAILED_UNROLL=1 '
-               'HL_SHARED_MEMORY_LIMIT=48 '
-               'bash ../autoscheduler/autotune_loop.sh '
-               'bin/host/onnx_converter.generator '
-               'onnx_model_generator '
-               'host-cuda '
-               '../autoscheduler/gpu.weights '
-               '../autoscheduler/bin '
-               '0 '
-               '0 '
-               'model_file_path={}').format(model_name, f)
-        print(cmd)
-        os.system(cmd)
+        print('{}: Gradient autoscheduler: {}s'.format(f, t))
 
 
 if __name__ == '__main__':
