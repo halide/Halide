ab079960070cdfb3cecfc579144d2165c91b37ad
Alex Reinking <alex.reinking@gmail.com>
2020-08-26 11:56:00 -0700

    Rework `cmake` meta-target to not promote. Warn when used as default.

--- cmake/HalideTargetHelpers.cmake
+++ cmake/HalideTargetHelpers.cmake
@@ -12,16 +12,7 @@ endif ()
 # Utilities for manipulating Halide target triples
 ##
 
-function(_Halide_auto_target OUTVAR)
-    _Halide_cmake_target(cmake_target)
-    if (Halide_HOST_TARGET STREQUAL cmake_target)
-        set(${OUTVAR} host PARENT_SCOPE)
-    else ()
-        set(${OUTVAR} "${cmake_target}" PARENT_SCOPE)
-    endif ()
-endfunction()
-
-function(_Halide_triple OUTVAR)
+function(_Halide_get_triple OUTVAR)
     # Well-formed targets must either start with "host" or a target triple.
     if (ARGN MATCHES "host")
         set(${OUTVAR} ${Halide_HOST_TARGET})
@@ -34,10 +25,11 @@ endfunction()
 function(_Halide_cmake_target OUTVAR)
     # Get arch from CMake
     string(TOLOWER "${CMAKE_SYSTEM_PROCESSOR}" arch)
-    list(TRANSFORM arch REPLACE  "^.*(x86|arm|mips|powerpc|hexagon|wasm|riscv).*$" "\\1")
+    list(TRANSFORM arch REPLACE "^.*(x86|arm|mips|powerpc|hexagon|wasm|riscv).*$" "\\1")
     list(TRANSFORM arch REPLACE "^i.?86.+$" "x86")
     list(TRANSFORM arch REPLACE "^(amd|ia|em)64t?$" "x86")
     list(TRANSFORM arch REPLACE "^ppc(64)?$" "powerpc")
+    list(TRANSFORM arch REPLACE "^aarch(64)?$" "arm")
 
     # Get bits from CMake
     math(EXPR bits "8 * ${CMAKE_SIZEOF_VOID_P}")
@@ -50,9 +42,15 @@ function(_Halide_cmake_target OUTVAR)
 endfunction()
 
 ##
-# Set Halide_HOST_TARGET when we're compiling Halide (this variable is set by package scripts)
+# Set Halide `host` and `cmake` meta-target values
 ##
 
-if (NOT DEFINED Halide_HOST_TARGET)
+# This variable is set by package scripts and might differ from Halide_CMAKE_TARGET below.
+if (NOT Halide_HOST_TARGET)
     _Halide_cmake_target(Halide_HOST_TARGET)
+endif ()
+
+if (NOT Halide_CMAKE_TARGET)
+    _Halide_cmake_target(Halide_CMAKE_TARGET)
+    message(STATUS "Halide detected active CMake target `${Halide_CMAKE_TARGET}`")
 endif ()
