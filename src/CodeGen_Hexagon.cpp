b7a88f0f4a879518b2d7142bd17ba858a64f75c6
Andrew Adams <andrew.b.adams@gmail.com>
2020-09-01 11:22:25 -0700

    Clean up scatter/gather hvx intrinsics
    I was very surprised to see scatter/gather intrinsics, because that
    would be a substantial IR design change - currently we don't use
    intrinsics for anything that mixes vector lanes (i.e. intrinsics should
    vectorize trivially, and we should be able to slice up wide vectors into
    narrow vectors trivially when using intrinsics).
    
    These are only used in the HVX backend though, so I guess within that
    restricted scope it's reasonable, because they're handled specially. I
    prefixed them with hvx_ to make this clearer and modified some code to
    avoid string comparisons.

--- src/CodeGen_Hexagon.cpp
+++ src/CodeGen_Hexagon.cpp
@@ -2406,7 +2406,7 @@ void CodeGen_Hexagon::visit(const Call *op) {
         return;
     }
 
-    if (op->is_intrinsic(Call::gather)) {
+    if (op->is_intrinsic(Call::hvx_gather)) {
         internal_assert(op->args.size() == 5);
         internal_assert(op->type.bits() == 16 || op->type.bits() == 32);
         int index_lanes = op->type.lanes();
@@ -2433,15 +2433,15 @@ void CodeGen_Hexagon::visit(const Call *op) {
             value = builder->CreateCall(fn, args);
         }
         return;
-    } else if (op->is_intrinsic(Call::scatter) ||
-               op->is_intrinsic(Call::scatter_acc)) {
+    } else if (op->is_intrinsic(Call::hvx_scatter) ||
+               op->is_intrinsic(Call::hvx_scatter_acc)) {
         internal_assert(op->args.size() == 4);
         internal_assert(op->type.bits() == 16 || op->type.bits() == 32);
         int index_lanes = op->type.lanes();
         int intrin_lanes = native_vector_bits() / op->type.bits();
 
         string name = "halide.hexagon.vscatter";
-        name += (op->name == "scatter_acc") ? "_acc" : "";
+        name += op->is_intrinsic(Call::hvx_scatter_acc) ? "_acc" : "";
         name += (op->type.bits() == 16) ? ".h.h" : ".w.w";
         llvm::Function *fn = module->getFunction(name);
 
@@ -2460,7 +2460,7 @@ void CodeGen_Hexagon::visit(const Call *op) {
             value = builder->CreateCall(fn, args);
         }
         return;
-    } else if (op->is_intrinsic(Call::scatter_release)) {
+    } else if (op->is_intrinsic(Call::hvx_scatter_release)) {
         internal_assert(op->args.size() == 1);
         Value *ptr = codegen(op->args[0]);
         llvm::Function *fn = module->getFunction("halide.hexagon.scatter.release");
