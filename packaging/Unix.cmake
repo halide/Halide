fe4533f0d5714cff5f13f60d13c3480f7625ccca
Alex Reinking <alex.reinking@gmail.com>
2020-09-08 13:44:24 -0700

    Merge Linux and macOS package scripts. Keep zsh.

--- packaging/Unix.cmake
+++ packaging/Unix.cmake
@@ -4,6 +4,8 @@ set(CPACK_COMPONENTS_HALIDE_RUNTIME Halide_Runtime)
 set(CPACK_COMPONENTS_HALIDE_DEVELOPMENT Halide_Development)
 
 set(CPACK_INSTALL_CMAKE_PROJECTS
+#    static-Debug Halide ALL /
+#    shared-Debug Halide ALL /
     static-Release Halide ALL /
     shared-Release Halide ALL /
     )
--- tools/package-unix.sh
+++ tools/package-unix.sh
@@ -1,17 +1,19 @@
-#!/bin/zsh
+#!/bin/bash
 
-DIR="$(cd "$(dirname "${(%):-%N}")"/.. >/dev/null 2>&1 && pwd)"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
 
 [ -z "$LLVM_DIR" ] && echo "Must set specific LLVM_DIR for packaging" && exit
 [ -z "$Clang_DIR" ] && echo "Must set specific Clang_DIR for packaging" && exit
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=NO \
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=NO -DHalide_BUNDLE_LLVM=YES \
   -DLLVM_DIR="$LLVM_DIR" -DClang_DIR="$Clang_DIR" \
   -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO \
   -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO \
   -S "$DIR" -B "$DIR/build/static-Release"
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=YES \
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=YES \
   -DLLVM_DIR="$LLVM_DIR" -DClang_DIR="$Clang_DIR" \
   -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO \
   -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO \
@@ -22,5 +24,5 @@ cmake --build "$DIR/build/shared-Release"
 
 (
   cd "$DIR/build" || exit
-  cpack --config "$DIR/packaging/macOS.cmake"
+  cpack --config "$DIR/packaging/Unix.cmake"
 )
