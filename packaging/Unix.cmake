eac0e093d3555e476df657275616d6d0061ffc66
Alex Reinking <alex.reinking@gmail.com>
2020-09-08 14:22:52 -0700

    Comment disabled debug builds

--- packaging/Unix.cmake
+++ packaging/Unix.cmake
@@ -4,8 +4,10 @@ set(CPACK_COMPONENTS_HALIDE_RUNTIME Halide_Runtime)
 set(CPACK_COMPONENTS_HALIDE_DEVELOPMENT Halide_Development)
 
 set(CPACK_INSTALL_CMAKE_PROJECTS
-#    static-Debug Halide ALL /
-#    shared-Debug Halide ALL /
+    # We don't package debug binaries on Unix systems. Our developers
+    # don't use them and LLVM in debug mode is next to unusable, too.
+    # static-Debug Halide ALL /
+    # shared-Debug Halide ALL /
     static-Release Halide ALL /
     shared-Release Halide ALL /
     )
