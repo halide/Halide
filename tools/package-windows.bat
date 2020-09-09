9d6ab961c85e263bcf8025b25575f477ec40fc97
Alex Reinking <alex.reinking@gmail.com>
2020-09-08 17:25:36 -0700

    Rework package scripts to take arguments

--- tools/package-windows.bat
+++ tools/package-windows.bat
@@ -1,6 +1,17 @@
 @echo off
 
-pushd %~dp0\..
+set halide_source="%~1"
+set halide_build_root="%~2"
+
+if "%halide_source%" == "" (
+    echo Usage: %~0 "<source-dir>" "<build-dir>"
+    goto return
+)
+
+if "%halide_build_root%" == "" (
+    echo Usage: %~0 "<source-dir>" "<build-dir>"
+    goto return
+)
 
 if not exist "%VCPKG_ROOT%\.vcpkg-root" (
     echo Must define VCPKG_ROOT to be the root of the VCPKG install
@@ -9,35 +20,48 @@ if not exist "%VCPKG_ROOT%\.vcpkg-root" (
 
 REM Ninja Multi-Config in 3.18 has some sort of bug. Very disappointing.
 cmake -G "Visual Studio 16 2019" -Thost=x64 -A x64 ^
-      -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake ^
-      -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO ^
-      -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO ^
-      "-DCMAKE_INSTALL_BINDIR=bin/$<CONFIG>" "-DCMAKE_INSTALL_LIBDIR=lib/$<CONFIG>" ^
+      "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake" ^
+      -DBUILD_SHARED_LIBS=YES ^
+      -DWITH_TESTS=NO ^
+      -DWITH_APPS=NO ^
+      -DWITH_TUTORIALS=NO ^
+      -DWITH_DOCS=YES ^
+      -DWITH_UTILS=NO ^
+      -DWITH_PYTHON_BINDINGS=NO ^
+      "-DCMAKE_INSTALL_BINDIR=bin/$<CONFIG>" ^
+      "-DCMAKE_INSTALL_LIBDIR=lib/$<CONFIG>" ^
       "-DHALIDE_INSTALL_CMAKEDIR=lib" ^
-      -S . -B build/shared
+      -S "%halide_source%" ^
+      -B "%halide_build_root%/shared"
 
 if %errorlevel% neq 0 goto return
 
-cmake --build build/shared --config Debug
-cmake --build build/shared --config Release
+REM We don't distribute Debug binaries because they aren't useful
+REM cmake --build %halide_build_root%/shared --config Debug
+cmake --build "%halide_build_root%/shared" --config Release
 
-pushd build\shared
-cpack -C "Debug;Release"
+pushd "%halide_build_root%\shared"
+cpack -C "Release"
 popd
 
 REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM REM
 
-:static
-
 REM Ninja Multi-Config in 3.18 has some sort of bug. Very disappointing.
 cmake -G "Visual Studio 16 2019" -Thost=x64 -A x64 ^
-      -DBUILD_SHARED_LIBS=NO -DHalide_BUNDLE_LLVM=YES ^
-      -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake ^
-      -DWITH_TESTS=NO -DWITH_APPS=NO -DWITH_TUTORIALS=NO ^
-      -DWITH_DOCS=YES -DWITH_UTILS=NO -DWITH_PYTHON_BINDINGS=NO ^
-      "-DCMAKE_INSTALL_BINDIR=bin/$<CONFIG>" "-DCMAKE_INSTALL_LIBDIR=lib/$<CONFIG>" ^
+      "-DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake" ^
+      -DBUILD_SHARED_LIBS=NO ^
+      -DHalide_BUNDLE_LLVM=YES ^
+      -DWITH_TESTS=NO ^
+      -DWITH_APPS=NO ^
+      -DWITH_TUTORIALS=NO ^
+      -DWITH_DOCS=YES ^
+      -DWITH_UTILS=NO ^
+      -DWITH_PYTHON_BINDINGS=NO ^
+      "-DCMAKE_INSTALL_BINDIR=bin/$<CONFIG>" ^
+      "-DCMAKE_INSTALL_LIBDIR=lib/$<CONFIG>" ^
       "-DHALIDE_INSTALL_CMAKEDIR=lib" ^
-      -S . -B build/static
+      -S "%halide_source%" ^
+      -B "%halide_build_root%/static"
 
 if %errorlevel% neq 0 goto return
 
@@ -45,8 +69,7 @@ REM LLVM Debug + Halide exceeds the COFF 4GB limit!!
 REM cmake --build build/static --config Debug
 cmake --build build/static --config Release
 
-pushd build\static
-REM cpack -C "Debug;Release"
+pushd "%halide_build_root%\static"
 cpack -C "Release"
 popd
 
