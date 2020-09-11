a0972ab6f45f09d8578ebfcccc93f884043b0ac6
Alex Reinking <alex.reinking@gmail.com>
2020-09-11 15:19:36 -0700

    Add argc error messages to autoscheduler loads.

--- test/auto_schedule/extern.cpp
+++ test/auto_schedule/extern.cpp
@@ -132,6 +132,11 @@ int main(int argc, char **argv) {
         return 0;
     }
 
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <autoscheduler-lib>\n", argv[0]);
+        return 1;
+    }
+
     load_plugin(argv[1]);
 
     test_case_1();
