cea1203f26d4cb1b94dafc1af72e8dce1ca61f0a
Steven Johnson <srj@google.com>
2020-10-08 13:39:35 -0700

    Update CodeGen_C.cpp

--- src/CodeGen_C.cpp
+++ src/CodeGen_C.cpp
@@ -553,8 +553,10 @@ public:
     template<size_t InputLanes>
     static Vec concat(size_t count, const CppVector<ElementType, InputLanes> vecs[]) {
         Vec r;
-        for (size_t i = 0; i < Lanes; i++) {
-            r[i] = vecs[i / InputLanes][i % InputLanes];
+        for (size_t c = 0; c < count; c++) {
+            for (size_t i = 0; i < InputLanes; i++) {
+                r[i] = vecs[c][i];
+            }
         }
         return r;
     }
@@ -1060,8 +1062,10 @@ public:
     template<size_t InputLanes>
     static Vec concat(size_t count, const NativeVector<ElementType, InputLanes> vecs[]) {
         Vec r;
-        for (size_t i = 0; i < Lanes; i++) {
-            r[i] = vecs[i / InputLanes][i % InputLanes];
+        for (size_t c = 0; c < count; c++) {
+            for (size_t i = 0; i < InputLanes; i++) {
+                r[i] = vecs[c][i];
+            }
         }
         return r;
     }
