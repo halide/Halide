a27b0b60493b1d1cd543036d16448525b2632b2d
Steven Johnson <srj@google.com>
2020-11-18 11:46:31 -0800

    Move buffer utils into separate header file

--- apps/interpret_nn/compare_vs_tflite.cpp
+++ apps/interpret_nn/compare_vs_tflite.cpp
@@ -7,6 +7,7 @@
 #endif
 
 #include "app_util.h"
+#include "buffer_util.h"
 #include "halide_benchmark.h"
 #include "interpreter.h"
 #include "tflite_parser.h"
@@ -28,15 +29,6 @@ struct TfLiteReporter : public tflite::ErrorReporter {
     }
 };
 
-size_t NumProcessorsOnline() {
-#ifdef _WIN32
-    char *num_cores = getenv("NUMBER_OF_PROCESSORS");
-    return num_cores ? atoi(num_cores) : 8;
-#else
-    return sysconf(_SC_NPROCESSORS_ONLN);
-#endif
-}
-
 inline std::ostream &operator<<(std::ostream &stream, const halide_type_t &type) {
     if (type.code == halide_type_uint && type.bits == 1) {
         stream << "bool";
@@ -52,20 +44,8 @@ inline std::ostream &operator<<(std::ostream &stream, const halide_type_t &type)
 }
 
 std::chrono::duration<double> bench(std::function<void()> f) {
-#if 1
     auto result = Halide::Tools::benchmark(f);
     return std::chrono::duration<double>(result.wall_time);
-#else
-    auto begin = std::chrono::high_resolution_clock::now();
-    auto end = begin;
-    int loops = 0;
-    do {
-        f();
-        loops++;
-        end = std::chrono::high_resolution_clock::now();
-    } while (end - begin < std::chrono::seconds(1));
-    return (end - begin) / loops;
-#endif
 }
 
 halide_type_t TfLiteTypeToHalideType(TfLiteType t) {
@@ -99,148 +79,6 @@ halide_type_t TfLiteTypeToHalideType(TfLiteType t) {
         return halide_type_t();
     }
 }
-
-// Must be constexpr to allow use in case clauses.
-inline constexpr int halide_type_code(halide_type_code_t code, int bits) {
-    return (((int)code) << 8) | bits;
-}
-
-// dynamic_type_dispatch is a utility for functors that want to be able
-// to dynamically dispatch a halide_type_t to type-specialized code.
-// To use it, a functor must be a *templated* class, e.g.
-//
-//     template<typename T> class MyFunctor { int operator()(arg1, arg2...); };
-//
-// dynamic_type_dispatch() is called with a halide_type_t as the first argument,
-// followed by the arguments to the Functor's operator():
-//
-//     auto result = dynamic_type_dispatch<MyFunctor>(some_halide_type, arg1, arg2);
-//
-// Note that this means that the functor must be able to instantiate its
-// operator() for all the Halide scalar types; it also means that all those
-// variants *will* be instantiated (increasing code size), so this approach
-// should only be used when strictly necessary.
-template<template<typename> class Functor, typename... Args>
-auto dynamic_type_dispatch(const halide_type_t &type, Args &&...args)
-    -> decltype(std::declval<Functor<uint8_t>>()(std::forward<Args>(args)...)) {
-
-#define HANDLE_CASE(CODE, BITS, TYPE)  \
-    case halide_type_code(CODE, BITS): \
-        return Functor<TYPE>()(std::forward<Args>(args)...);
-    switch (halide_type_code((halide_type_code_t)type.code, type.bits)) {
-        // HANDLE_CASE(halide_type_float, 16, float)  // TODO
-        HANDLE_CASE(halide_type_float, 32, float)
-        HANDLE_CASE(halide_type_float, 64, double)
-        HANDLE_CASE(halide_type_int, 8, int8_t)
-        HANDLE_CASE(halide_type_int, 16, int16_t)
-        HANDLE_CASE(halide_type_int, 32, int32_t)
-        HANDLE_CASE(halide_type_int, 64, int64_t)
-        HANDLE_CASE(halide_type_uint, 1, bool)
-        HANDLE_CASE(halide_type_uint, 8, uint8_t)
-        HANDLE_CASE(halide_type_uint, 16, uint16_t)
-        HANDLE_CASE(halide_type_uint, 32, uint32_t)
-        HANDLE_CASE(halide_type_uint, 64, uint64_t)
-    default:
-        APP_FATAL << "Unsupported type";
-        using ReturnType = decltype(std::declval<Functor<uint8_t>>()(std::forward<Args>(args)...));
-        return ReturnType();
-    }
-#undef HANDLE_CASE
-}
-
-template<typename T>
-struct CompareBuffers {
-public:
-    uint64_t operator()(const Buffer<const void> &tflite_buf_dynamic, const Buffer<const void> &halide_buf_dynamic) {
-        Buffer<const T> tflite_buf = tflite_buf_dynamic;
-        Buffer<const T> halide_buf = halide_buf_dynamic;
-        uint64_t diffs = 0;
-        constexpr uint64_t max_diffs_to_show = 32;
-        tflite_buf.for_each_element([&](const int *pos) {
-            T tflite_buf_val = tflite_buf(pos);
-            T halide_buf_val = halide_buf(pos);
-            // TODO: this is terrible, we should compare with some threshold instead of equality
-            if (tflite_buf_val != halide_buf_val) {
-                diffs++;
-                if (diffs > max_diffs_to_show) {
-                    return;
-                }
-                std::cerr << "*** Mismatch at (";
-                for (int i = 0; i < tflite_buf.dimensions(); ++i) {
-                    if (i > 0) std::cerr << ", ";
-                    std::cerr << pos[i];
-                }
-                std::cerr << "): tflite " << 0 + tflite_buf_val << " halide " << 0 + halide_buf_val << "\n";
-            }
-        });
-        if (diffs > max_diffs_to_show) {
-            std::cerr << "(" << (diffs - max_diffs_to_show) << " diffs suppressed)\n";
-        }
-        return diffs;
-    }
-};
-
-template<typename T>
-void FillWithRandomImpl(Buffer<T> &b, std::mt19937 &rng) {
-    std::uniform_int_distribution<T> dis(std::numeric_limits<T>::min(),
-                                         std::numeric_limits<T>::max());
-    b.for_each_value([&rng, &dis](T &value) {
-        value = dis(rng);
-    });
-}
-
-template<>
-void FillWithRandomImpl(Buffer<float> &b, std::mt19937 &rng) {
-    // Floating point. We arbitrarily choose to use the range [0.0, 1.0].
-    std::uniform_real_distribution<float> dis(0.0, 1.0);
-    b.for_each_value([&rng, &dis](float &value) {
-        value = dis(rng);
-    });
-}
-
-template<>
-void FillWithRandomImpl(Buffer<double> &b, std::mt19937 &rng) {
-    // Floating point. We arbitrarily choose to use the range [0.0, 1.0].
-    std::uniform_real_distribution<double> dis(0.0, 1.0);
-    b.for_each_value([&rng, &dis](double &value) {
-        value = dis(rng);
-    });
-}
-
-template<>
-void FillWithRandomImpl(Buffer<bool> &b, std::mt19937 &rng) {
-    std::uniform_int_distribution<int> dis(0, 1);
-    b.for_each_value([&rng, &dis](bool &value) {
-        value = static_cast<bool>(dis(rng));
-    });
-}
-
-template<typename T>
-struct FillWithRandom {
-public:
-    void operator()(Buffer<> &b_dynamic, int seed) {
-        Buffer<T> b = b_dynamic;
-        std::mt19937 rng(seed);
-        FillWithRandomImpl<T>(b, rng);
-    }
-};
-
-template<typename T>
-struct DumpBuffer {
-public:
-    void operator()(const Buffer<const void> &buf_dynamic) {
-        Buffer<const T> buf = buf_dynamic;
-        buf.for_each_element([&](const int *pos) {
-            T val = buf(pos);
-            std::cerr << "Value at (";
-            for (int i = 0; i < buf.dimensions(); ++i) {
-                if (i > 0) std::cerr << ", ";
-                std::cerr << pos[i];
-            }
-            std::cerr << "): " << 0 + val << "\n";
-        });
-    }
-};
 
 Buffer<void> WrapTfLiteTensorWithHalideBuffer(TfLiteTensor *t) {
     APP_CHECK(t->dims);
@@ -447,7 +285,12 @@ int main(int argc, char **argv) {
         }
     }
     if (threads <= 0) {
-        threads = interpret_nn::NumProcessorsOnline();
+#ifdef _WIN32
+        char *num_cores = getenv("NUMBER_OF_PROCESSORS");
+        threads = num_cores ? atoi(num_cores) : 8;
+#else
+        threads = sysconf(_SC_NPROCESSORS_ONLN);
+#endif
     }
 
     std::cout << "Using random seed: " << seed << "\n";
