612c77ecd474627826c82934b0c5f8cdbb9dbf8e
Steven Johnson <srj@google.com>
2020-11-19 15:07:36 -0800

    wip

--- apps/bilateral_grid/viz.sh
+++ apps/bilateral_grid/viz.sh
@@ -6,5 +6,7 @@ rm -f $1/bilateral_grid.mp4
 make $1/filter_viz && \
 $1/filter_viz ../images/gray_small.png $1/out_small.png 0.2 0 | \
 ../../bin/HalideTraceViz --size 1920 1080 | \
-${HL_AVCONV} -f rawvideo -pix_fmt bgr32 -s 1920x1080 -i /dev/stdin -c:v h264 $1/bilateral_grid.mp4
+${HL_AVCONV} -f rawvideo -pix_fmt bgr32 -s 1920x1080 -i /dev/stdin -c:v h264 -vf format=yuv420p $1/bilateral_grid.mp4
+open $1/bilateral_grid.mp4
+
 #mplayer -demuxer rawvideo -rawvideo w=1920:h=1080:format=rgba:fps=30 -idle -fixed-vo -
