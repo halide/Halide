c58e7bb914ebff84fd5f12847777ddf78aa7763a
Marcos Slomp <slomp@adobe.com>
2020-12-08 14:09:22 -0800

    typo

--- src/runtime/windows_d3d12compute_arm.cpp
+++ src/runtime/windows_d3d12compute_arm.cpp
@@ -1,7 +1,7 @@
 // NOTE(marcos): this file is a trivial wrapper around the "core" d3d12 back-end
 // located in "d3d12compute.cpp"; this wrapper is used as a mean to "decorate" the
 // module such that the proper build flags and target triple can be determined based
-// on the prefix (windows_) and suffix (_x86) when building the runtime module.
+// on the prefix (windows_) and suffix (_arm) when building the runtime module.
 
 #ifndef BITS_64
 // Don't emit compiler messages (via #pragma message) here: some environments might
