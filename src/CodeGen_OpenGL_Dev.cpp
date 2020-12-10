8c2e82af18c5797af6198ae6068efe3e9f46c617
xndcn <xndchn@gmail.com>
2020-12-10 12:36:14 +0800

    Fix mismatch type of uniforms for OpenGL kernel args
    OpenGL runtime will use raw type pointer to retrieve the uniform value in
    kernel args, so we have to use C-style type definition instead of GLSL-style.
    
    See also #4702

--- src/CodeGen_OpenGL_Dev.cpp
+++ src/CodeGen_OpenGL_Dev.cpp
@@ -919,12 +919,12 @@ void CodeGen_GLSL::add_kernel(const Stmt &stmt, const string &name,
             ++num_varying_floats;
         } else if (args[i].type.is_float()) {
             header << "/// UNIFORM "
-                   << CodeGen_GLSLBase::print_type(args[i].type) << " "
+                   << CodeGen_C::print_type(args[i].type) << " "  // NOLINT: Allow call to CodeGen_C::print_type
                    << print_name(args[i].name) << " uniformf" << args[i].packed_index / 4 << "[" << args[i].packed_index % 4 << "]\n";
             ++num_uniform_floats;
         } else if (args[i].type.is_int()) {
             header << "/// UNIFORM "
-                   << CodeGen_GLSLBase::print_type(args[i].type) << " "
+                   << CodeGen_C::print_type(args[i].type) << " "  // NOLINT: Allow call to CodeGen_C::print_type
                    << print_name(args[i].name) << " uniformi" << args[i].packed_index / 4 << "[" << args[i].packed_index % 4 << "]\n";
             ++num_uniform_ints;
         }
