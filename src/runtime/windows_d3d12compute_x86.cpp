3101326b2f4865de85613b741d0a6c1da865009a
Marcos Slomp <slomp@adobe.com>
2020-12-10 12:30:07 -0800

    addressing code review comments

--- src/runtime/windows_d3d12compute_x86.cpp
+++ src/runtime/windows_d3d12compute_x86.cpp
@@ -1,5 +1,5 @@
 // NOTE(marcos): this file is a trivial wrapper around the "core" d3d12 back-end
-// located in "d3d12compute.cpp"; this wrapper is used as a mean to "decorate" the
+// located in "d3d12compute.cpp"; this wrapper is used as a means to "decorate" the
 // module such that the proper build flags and target triple can be determined based
 // on the prefix (windows_) and suffix (_x86) when building the runtime module.
 
