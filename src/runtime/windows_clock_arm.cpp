2974347e3de1c4c5bcedfb71fee3de9eb69c20ca
Marcos Slomp <slomp@adobe.com>
2020-12-09 18:24:00 -0800

    removing windows_clock specializations

--- src/runtime/windows_clock_arm.cpp
+++
@@ -1,6 +0,0 @@
-// NOTE(marcos): this file is a trivial wrapper around "windows_clock.cpp"
-// this wrapper is used as a mean to "decorate" the module such that the proper
-// build flags and target triple can be determined based on the prefix (windows_)
-// and suffix (_arm) when building the runtime module.
-
-#include "windows_clock.cpp"
