2018bc9be64856a6518a31ffc051270074cd5f02
Volodymyr Kysenko <vksnk@google.com>
2020-12-21 14:33:10 -0800

    make format

--- src/runtime/xtensa_allocator.cpp
+++ src/runtime/xtensa_allocator.cpp
@@ -12,21 +12,23 @@ extern void free(void *);
 int halide_malloc_alignment();
 
 void *halide_malloc(void *user_context, size_t x) {
-  // Allocate enough space for aligning the pointer we return.
-  const size_t alignment = halide_malloc_alignment();
-  void *orig = malloc(x + alignment);
-  if (orig == 0) {
-    // Will result in a failed assertion and a call to halide_error
-    return 0;
-  }
-  // We want to store the original pointer prior to the pointer we return.
-  void *ptr = (void *)(((size_t)orig + alignment + sizeof(void *) - 1) &
-                       ~(alignment - 1));
-  ((void **)ptr)[-1] = orig;
-  return ptr;
+    // Allocate enough space for aligning the pointer we return.
+    const size_t alignment = halide_malloc_alignment();
+    void *orig = malloc(x + alignment);
+    if (orig == 0) {
+        // Will result in a failed assertion and a call to halide_error
+        return 0;
+    }
+    // We want to store the original pointer prior to the pointer we return.
+    void *ptr = (void *)(((size_t)orig + alignment + sizeof(void *) - 1) &
+                         ~(alignment - 1));
+    ((void **)ptr)[-1] = orig;
+    return ptr;
 }
 
-void halide_free(void *user_context, void *ptr) { free(((void **)ptr)[-1]); }
+void halide_free(void *user_context, void *ptr) {
+    free(((void **)ptr)[-1]);
+}
 
 #ifdef __cplusplus
 }  // extern "C"
