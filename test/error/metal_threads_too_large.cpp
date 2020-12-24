b63ad8d6725dfe9b94c605aaf9e2a76496074236
xndcn <xndchn@gmail.com>
2020-12-24 22:27:14 +0800

    Disable error/metal_threads_too_large test for non-OSX target

--- test/error/metal_threads_too_large.cpp
+++ test/error/metal_threads_too_large.cpp
@@ -4,6 +4,11 @@
 using namespace Halide;
 
 int main(int argc, char **argv) {
+    if (get_jit_target_from_environment().os != Target::OSX) {
+        printf("[SKIP] error/metal_threads_too_large ignored for non-OSX targets\n");
+        _halide_user_assert(0);
+    }
+
     ImageParam im(UInt(16), 2, "input");
     Func f("f");
     Var x("x"), y("y");
@@ -13,6 +18,7 @@ int main(int argc, char **argv) {
 
     // 65536 is larger enough than `maxTotalThreadsPerThreadgroup`
     Buffer<uint16_t> input = lambda(x, y, cast<uint16_t>(x + y)).realize(65536, 1);
+    input.set_host_dirty();
     im.set(input);
 
     Buffer<uint16_t> output(input.width(), input.height());
@@ -23,7 +29,7 @@ int main(int argc, char **argv) {
     for (int32_t i = 0; i < output.width(); i++) {
         for (int32_t j = 0; j < output.height(); j++) {
             if (output(i, j) != uint16_t(i + j + 42)) {
-                std::cerr << "Expected " << (x + y + 42) << " at (" << i << ", " << j << ") got " << output(i, j) << "\n";
+                std::cerr << "Expected " << (i + j + 42) << " at (" << i << ", " << j << ") got " << output(i, j) << "\n";
                 assert(false);
             }
         }
