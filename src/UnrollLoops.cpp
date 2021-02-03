5012aba1978fcd153790cb28046a8cde2ac7eab9
Andrew Adams <andrew.b.adams@gmail.com>
2021-02-03 12:49:10 -0800

    Fix computational complexity of unrolling large muxes

--- src/UnrollLoops.cpp
+++ src/UnrollLoops.cpp
@@ -3,10 +3,14 @@
 #include "CSE.h"
 #include "IRMutator.h"
 #include "IROperator.h"
+#include "IRVisitor.h"
 #include "Simplify.h"
+#include "Solve.h"
 #include "Substitute.h"
 
+using std::map;
 using std::pair;
+using std::string;
 using std::vector;
 
 namespace Halide {
@@ -14,10 +18,116 @@ namespace Internal {
 
 namespace {
 
+// Build a per-iteration map of replacements for each Expr.
+class FindReplacements : public IRVisitor {
+
+    using IRVisitor::visit;
+
+    void visit(const Variable *op) override {
+        if (op->name == loop_var_name) {
+            for (int i = 0; i < extent; i++) {
+                if (min_is_zero) {
+                    replacements[i][op] = i;
+                } else {
+                    replacements[i][op] = min + i;
+                }
+            }
+        }
+    }
+
+    void visit(const Select *op) override {
+        if (!min_is_zero) {
+            IRVisitor::visit(op);
+            return;
+        }
+
+        Expr rest;
+        const Select *sel = op;
+        std::set<int> handled;
+        while (true) {
+            auto result = solve_expression(sel->condition, loop_var_name);
+            if (result.fully_solved) {
+                const EQ *eq = result.result.as<EQ>();
+                const int64_t *idx = eq ? as_const_int(eq->b) : nullptr;
+                if (idx) {
+                    int i = (int)(*idx);
+                    Expr result = sel->true_value;
+                    result = substitute(loop_var_name, i, result);
+                    // Make a replacement for the original select true
+                    // that maps directly to this leaf in this case.
+                    // Note that we replace 'op', not sel. This is
+                    // what saves all the work for deeply nested muxes
+                    // and brings it down from O(n^2) to O(n)
+                    replacements[i].emplace(op, result);
+                    rest = sel->false_value;
+                    sel = rest.as<Select>();
+                    handled.insert(i);
+                    if (!sel) {
+                        // 'rest' was not a select node
+                        break;
+                    }
+                } else {
+                    // Condition was not of the form loop_var == int
+                    break;
+                }
+            } else {
+                // Failed to solve the condition for the loop var
+                break;
+            }
+        }
+
+        if (rest.defined()) {
+            for (int i = 0; i < extent; i++) {
+                if (!handled.count(i)) {
+                    replacements[i].emplace(op, substitute(loop_var_name, i, rest));
+                }
+            }
+            rest.accept(this);
+        } else {
+            IRVisitor::visit(op);
+        }
+    }
+
+    string loop_var_name;
+    Expr min;
+    int extent = 0;
+    bool min_is_zero = false;
+
+public:
+    // Maps from a loop iteration and an Expr and a loop iteration to
+    // the correct replacement for that Expr for that iteration
+    map<int, map<Expr, Expr, ExprCompare>> replacements;
+
+    FindReplacements(string loop_var_name, Expr min, int extent)
+        : loop_var_name(loop_var_name), min(min), extent(extent), min_is_zero(is_const_zero(min)) {
+    }
+};
+
+class DoReplacements : public IRMutator {
+
+    const map<Expr, Expr, ExprCompare> &replacements;
+
+public:
+    using IRMutator::mutate;
+
+    Expr mutate(const Expr &e) override {
+        auto it = replacements.find(e);
+        if (it != replacements.end()) {
+            return it->second;
+        } else {
+            return IRMutator::mutate(e);
+        }
+    }
+
+    DoReplacements(const map<Expr, Expr, ExprCompare> &replacements)
+        : replacements(replacements) {
+    }
+};
+
 class UnrollLoops : public IRMutator {
     using IRMutator::visit;
 
-    vector<pair<std::string, Expr>> lets;
+    vector<pair<string, Expr>> lets;
 
     Stmt visit(const LetStmt *op) override {
         if (is_pure(op->value)) {
@@ -79,9 +189,12 @@ class UnrollLoops : public IRMutator {
                 user_warning << "Warning: Unrolling a for loop of extent 1: " << for_loop->name << "\n";
             }
 
+            FindReplacements replacer(for_loop->name, for_loop->min, e->value);
+            body.accept(&replacer);
+
             Stmt iters;
             for (int i = e->value - 1; i >= 0; i--) {
-                Stmt iter = substitute(for_loop->name, for_loop->min + i, body);
+                Stmt iter = DoReplacements(replacer.replacements[i]).mutate(body);
                 if (!iters.defined()) {
                     iters = iter;
                 } else {
