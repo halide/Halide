halide/Halide

commit 1be92c2cd5ae9f95e37576ffb653cbe4c3458dd0

Grab-bag of minor Generator-related cleanups (#5766)
* Grab-bag of minor Generator-related cleanups

* Appease clang-tidy; rename local 'cerr'

* Update PyStubImpl.cpp

* Update PyStub.cpp

* appease clang-tidy

src/Generator.cpp
@@ -91,10 +91,11 @@ std::map<Output, std::string> compute_output_files(const Target &target,
 }
 
 Argument to_argument(const Internal::Parameter &param) {
-    ArgumentEstimates argument_estimates = param.get_argument_estimates();
     return Argument(param.name(),
                     param.is_buffer() ? Argument::InputBuffer : Argument::InputScalar,
-                    param.type(), param.dimensions(), argument_estimates);
+                    param.type(),
+                    param.dimensions(),
+                    param.get_argument_estimates());
 }
 
 Func make_param_func(const Parameter &p, const std::string &name) {
@@ -746,7 +747,7 @@ std::string halide_type_to_c_type(const Type &t) {
     return m.at(encode(t));
 }
 
-int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
+int generate_filter_main_inner(int argc, char **argv, std::ostream &error_output) {
     const char kUsage[] =
         "gengen\n"
         "  [-g GENERATOR_NAME] [-f FUNCTION_NAME] [-o OUTPUT_DIR] [-r RUNTIME_NAME] [-d 1|0]\n"
@@ -794,7 +795,7 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
         if (argv[i][0] != '-') {
             std::vector<std::string> v = split_string(argv[i], "=");
             if (v.size() != 2 || v[0].empty() || v[1].empty()) {
-                cerr << kUsage;
+                error_output << kUsage;
                 return 1;
             }
             generator_args[v[0]] = v[1];
@@ -803,15 +804,15 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
         auto it = flags_info.find(argv[i]);
         if (it != flags_info.end()) {
             if (i + 1 >= argc) {
-                cerr << kUsage;
+                error_output << kUsage;
                 return 1;
             }
             it->second = argv[i + 1];
             ++i;
             continue;
         }
-        cerr << "Unknown flag: " << argv[i] << "\n";
-        cerr << kUsage;
+        error_output << "Unknown flag: " << argv[i] << "\n";
+        error_output << kUsage;
         return 1;
     }
 
@@ -824,8 +825,8 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
     }
 
     if (flags_info["-d"] != "1" && flags_info["-d"] != "0") {
-        cerr << "-d must be 0 or 1\n";
-        cerr << kUsage;
+        error_output << "-d must be 0 or 1\n";
+        error_output << kUsage;
         return 1;
     }
     const int build_gradient_module = flags_info["-d"] == "1";
@@ -839,8 +840,8 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
 
     std::vector<std::string> generator_names = GeneratorRegistry::enumerate();
     if (generator_names.empty() && runtime_name.empty()) {
-        cerr << "No generators have been registered and not compiling a standalone runtime\n";
-        cerr << kUsage;
+        error_output << "No generators have been registered and not compiling a standalone runtime\n";
+        error_output << kUsage;
         return 1;
     }
 
@@ -848,13 +849,13 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
     if (generator_name.empty() && runtime_name.empty()) {
         // Require either -g or -r to be specified:
         // no longer infer the name when only one Generator is registered
-        cerr << "Either -g <name> or -r must be specified; available Generators are:\n";
+        error_output << "Either -g <name> or -r must be specified; available Generators are:\n";
         if (!generator_names.empty()) {
             for (const auto &name : generator_names) {
-                cerr << "    " << name << "\n";
+                error_output << "    " << name << "\n";
             }
         } else {
-            cerr << "    <none>\n";
+            error_output << "    <none>\n";
         }
         return 1;
     }
@@ -866,8 +867,8 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
     }
     std::string output_dir = flags_info["-o"];
     if (output_dir.empty()) {
-        cerr << "-o must always be specified.\n";
-        cerr << kUsage;
+        error_output << "-o must always be specified.\n";
+        error_output << kUsage;
         return 1;
     }
 
@@ -888,8 +889,8 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
     const bool stub_only = (emit_flags.size() == 1 && emit_flags[0] == "cpp_stub");
     if (!stub_only) {
         if (generator_args.find("target") == generator_args.end()) {
-            cerr << "Target missing\n";
-            cerr << kUsage;
+            error_output << "Target missing\n";
+            error_output << kUsage;
             return 1;
         }
     }
@@ -929,17 +930,17 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
         for (const std::string &opt : emit_flags) {
             auto it = output_name_to_enum.find(opt);
             if (it == output_name_to_enum.end()) {
-                cerr << "Unrecognized emit option: " << opt << " is not one of [";
+                error_output << "Unrecognized emit option: " << opt << " is not one of [";
                 auto end = output_info.cend();
                 auto last = std::prev(end);
                 for (auto iter = output_info.cbegin(); iter != end; ++iter) {
-                    cerr << iter->second.name;
+                    error_output << iter->second.name;
                     if (iter != last) {
-                        cerr << " ";
+                        error_output << " ";
                     }
                 }
-                cerr << "], ignoring.\n";
-                cerr << kUsage;
+                error_output << "], ignoring.\n";
+                error_output << kUsage;
                 return 1;
             }
             outputs.insert(it->second);
@@ -990,10 +991,11 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
         Target gcd_target = targets[0];
         for (size_t i = 1; i < targets.size(); i++) {
             if (!gcd_target.get_runtime_compatible_target(targets[i], gcd_target)) {
-                user_error << "Failed to find compatible runtime target for "
-                           << gcd_target.to_string()
-                           << " and "
-                           << targets[i].to_string() << "\n";
+                error_output << "Failed to find compatible runtime target for "
+                             << gcd_target.to_string()
+                             << " and "
+                             << targets[i].to_string() << "\n";
+                return -1;
             }
         }
 
@@ -1023,6 +1025,8 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &cerr) {
             auto module_factory = [&generator_name, &generator_args, build_gradient_module](const std::string &name, const Target &target) -> Module {
                 auto sub_generator_args = generator_args;
                 sub_generator_args.erase("target");
+                sub_generator_args.erase("auto_schedule");
+                sub_generator_args.erase("machine_params");
                 // Must re-create each time since each instance will have a different Target.
                 auto gen = GeneratorRegistry::create(generator_name, GeneratorContext(target));
                 gen->set_generator_param_values(sub_generator_args);
@@ -1134,6 +1138,7 @@ std::vector<std::string> GeneratorRegistry::enumerate() {
     GeneratorRegistry &registry = get_registry();
     std::lock_guard<std::mutex> lock(registry.mutex);
     std::vector<std::string> result;
+    result.reserve(registry.factories.size());
     for (const auto &i : registry.factories) {
         result.push_back(i.first);
     }
@@ -1341,7 +1346,7 @@ void GeneratorBase::advance_phase(Phase new_phase) {
         internal_error << "Impossible";
         break;
     case ConfigureCalled:
-        internal_assert(phase == Created) << "pase is " << phase;
+        internal_assert(phase == Created);
         break;
     case InputsSet:
         internal_assert(phase == Created || phase == ConfigureCalled);
@@ -2035,12 +2040,26 @@ void GeneratorOutputBase::resize(size_t size) {
     init_internals();
 }
 
+StubOutputBufferBase::StubOutputBufferBase() = default;
+
+StubOutputBufferBase::StubOutputBufferBase(const Func &f, const std::shared_ptr<GeneratorBase> &generator)
+    : f(f), generator(generator) {
+}
+
 void StubOutputBufferBase::check_scheduled(const char *m) const {
     generator->check_scheduled(m);
 }
 
+Realization StubOutputBufferBase::realize(std::vector<int32_t> sizes) {
+    return f.realize(std::move(sizes), get_target());
+}
+
 Target StubOutputBufferBase::get_target() const {
     return generator->get_target();
+}
+
+RegisterGenerator::RegisterGenerator(const char *registered_name, GeneratorFactory generator_factory) {
+    Internal::GeneratorRegistry::register_factory(registered_name, std::move(generator_factory));
 }
 
 void generator_test() {

python_bindings/stub/PyStub.cpp
@@ -7,7 +7,10 @@
 // to simplify build requirements in downstream environments.
 
 #include <Python.h>
+#include <functional>
 #include <memory>
+
+#include "Halide.h"
 
 #ifndef HALIDE_PYSTUB_GENERATOR_NAME
 #error "HALIDE_PYSTUB_GENERATOR_NAME must be defined"
@@ -17,16 +20,7 @@
 #define HALIDE_PYSTUB_MODULE_NAME HALIDE_PYSTUB_GENERATOR_NAME
 #endif
 
-namespace Halide {
-class GeneratorContext;
-namespace Internal {
-class GeneratorBase;
-}  // namespace Internal
-}  // namespace Halide
-
-using FactoryFunc = std::unique_ptr<Halide::Internal::GeneratorBase> (*)(const Halide::GeneratorContext &context);
-
-extern "C" PyObject *_halide_pystub_impl(const char *module_name, FactoryFunc factory);
+extern "C" PyObject *_halide_pystub_impl(const char *module_name, const Halide::Internal::GeneratorFactory &factory);
 
 #define HALIDE_STRINGIFY(x) #x
 #define HALIDE_TOSTRING(x) HALIDE_STRINGIFY(x)
@@ -34,17 +28,17 @@ extern "C" PyObject *_halide_pystub_impl(const char *module_name, FactoryFunc fa
 #define _HALIDE_CONCAT(first, second) first##second
 #define HALIDE_CONCAT(first, second) _HALIDE_CONCAT(first, second)
 
-#if !defined(HALIDE_EXPORT)
+// Don't use HALIDE_EXPORT: Halide.h will already have defined it,
+// but it might be defined the wrong way (import rather than export).
 #if defined(WIN32) || defined(_WIN32)
-#define HALIDE_EXPORT __declspec(dllexport)
+#define HALIDE_PLUGIN_EXPORT __declspec(dllexport)
 #else
-#define HALIDE_EXPORT __attribute__((visibility("default")))
-#endif
+#define HALIDE_PLUGIN_EXPORT __attribute__((visibility("default")))
 #endif
 
 static_assert(PY_MAJOR_VERSION >= 3, "Python bindings for Halide require Python 3+");
 
-#define _HALIDE_PLUGIN_IMPL(name) extern "C" HALIDE_EXPORT PyObject *PyInit_##name() /* NOLINT(bugprone-macro-parentheses) */
+#define _HALIDE_PLUGIN_IMPL(name) extern "C" HALIDE_PLUGIN_EXPORT PyObject *PyInit_##name() /* NOLINT(bugprone-macro-parentheses) */
 #define HALIDE_PLUGIN_IMPL(name) _HALIDE_PLUGIN_IMPL(name)
 
 // clang-format off

python_bindings/stub/PyStubImpl.cpp
@@ -21,11 +21,10 @@ static_assert(PY_VERSION_HEX >= 0x03000000,
 
 namespace py = pybind11;
 
-using FactoryFunc = std::unique_ptr<Halide::Internal::GeneratorBase> (*)(const Halide::GeneratorContext &context);
-
 namespace Halide {
 namespace PythonBindings {
 
+using GeneratorFactory = Internal::GeneratorFactory;
 using GeneratorParamsMap = Internal::GeneratorParamsMap;
 using Stub = Internal::GeneratorStub;
 using StubInput = Internal::StubInput;
@@ -100,7 +99,7 @@ void append_input(const py::object &value, std::vector<StubInput> &v) {
     }
 }
 
-py::object generate_impl(FactoryFunc factory, const GeneratorContext &context, const py::args &args, const py::kwargs &kwargs) {
+py::object generate_impl(const GeneratorFactory &factory, const GeneratorContext &context, const py::args &args, const py::kwargs &kwargs) {
     Stub stub(context, [factory](const GeneratorContext &context) -> std::unique_ptr<Halide::Internal::GeneratorBase> {
         return factory(context);
     });
@@ -177,7 +176,7 @@ py::object generate_impl(FactoryFunc factory, const GeneratorContext &context, c
     return std::move(py_outputs);
 }
 
-void pystub_init(pybind11::module &m, FactoryFunc factory) {
+void pystub_init(pybind11::module &m, const GeneratorFactory &factory) {
     m.def(
         "generate", [factory](const Halide::Target &target, const py::args &args, const py::kwargs &kwargs) -> py::object {
             return generate_impl(factory, Halide::GeneratorContext(target), args, kwargs);
@@ -189,7 +188,7 @@ void pystub_init(pybind11::module &m, FactoryFunc factory) {
 }  // namespace PythonBindings
 }  // namespace Halide
 
-extern "C" PyObject *_halide_pystub_impl(const char *module_name, FactoryFunc factory) {
+extern "C" PyObject *_halide_pystub_impl(const char *module_name, const Halide::Internal::GeneratorFactory &factory) {
     int major, minor;
     if (sscanf(Py_GetVersion(), "%i.%i", &major, &minor) != 2) {
         PyErr_SetString(PyExc_ImportError, "Can't parse Python version.");

src/Generator.h
@@ -258,6 +258,7 @@
  */
 
 #include <algorithm>
+#include <functional>
 #include <iterator>
 #include <limits>
 #include <memory>
@@ -1289,6 +1290,7 @@ class StubInputBuffer {
 
     template<typename T2>
     HALIDE_NO_USER_CODE_INLINE static Parameter parameter_from_buffer(const Buffer<T2> &b) {
+        internal_assert(b.defined());
         user_assert((Buffer<T>::can_convert_from(b)));
         Parameter p(b.type(), true, b.dimensions());
         p.set_buffer(b);
@@ -1316,16 +1318,11 @@ protected:
     void check_scheduled(const char *m) const;
     Target get_target() const;
 
-    explicit StubOutputBufferBase(const Func &f, std::shared_ptr<GeneratorBase> generator)
-        : f(f), generator(std::move(generator)) {
-    }
-    StubOutputBufferBase() = default;
+    StubOutputBufferBase();
+    explicit StubOutputBufferBase(const Func &f, const std::shared_ptr<GeneratorBase> &generator);
 
 public:
-    Realization realize(std::vector<int32_t> sizes) {
-        check_scheduled("realize");
-        return f.realize(std::move(sizes), get_target());
-    }
+    Realization realize(std::vector<int32_t> sizes);
 
     template<typename... Args>
     Realization realize(Args &&...args) {
@@ -2968,7 +2965,6 @@ struct NoRealizations<T, Args...> {
 };
 
 class GeneratorStub;
-class SimpleGeneratorFactory;
 
 // Note that these functions must never return null:
 // if they cannot return a valid Generator, they must assert-fail.
@@ -3251,7 +3247,6 @@ private:
     friend class GeneratorOutputBase;
     friend class GeneratorParamInfo;
     friend class GeneratorStub;
-    friend class SimpleGeneratorFactory;
     friend class StubOutputBufferBase;
 
     const size_t size;
@@ -3654,7 +3649,6 @@ protected:
 
 private:
     friend void ::Halide::Internal::generator_test();
-    friend class Internal::SimpleGeneratorFactory;
     friend void ::Halide::Internal::generator_test();
     friend class ::Halide::GeneratorContext;
 
@@ -3669,9 +3663,7 @@ namespace Internal {
 
 class RegisterGenerator {
 public:
-    RegisterGenerator(const char *registered_name, GeneratorFactory generator_factory) {
-        Internal::GeneratorRegistry::register_factory(registered_name, std::move(generator_factory));
-    }
+    RegisterGenerator(const char *registered_name, GeneratorFactory generator_factory);
 };
 
 class GeneratorStub : public NamesInterface {

commit 910344a52da77ff3da5e2c478d88e5af2bc90318

Fix autoscheduler breakage from #5766 (Fix #5769)

src/Generator.cpp
@@ -1025,8 +1025,6 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &error_output
             auto module_factory = [&generator_name, &generator_args, build_gradient_module](const std::string &name, const Target &target) -> Module {
                 auto sub_generator_args = generator_args;
                 sub_generator_args.erase("target");
-                sub_generator_args.erase("auto_schedule");
-                sub_generator_args.erase("machine_params");
                 // Must re-create each time since each instance will have a different Target.
                 auto gen = GeneratorRegistry::create(generator_name, GeneratorContext(target));
                 gen->set_generator_param_values(sub_generator_args);