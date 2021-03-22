fb8a9c1504b8471582c5bc42ff244e3c14b39ec1
Steven Johnson <srj@google.com>
2021-03-22 16:50:50 -0700

    verbosity -> hannk_verbosity to match TFLite conventions

--- apps/hannk/delegate/hannk_delegate_provider.cpp
+++ apps/hannk/delegate/hannk_delegate_provider.cpp
@@ -9,26 +9,26 @@ class HannkDelegateProvider : public DelegateProvider {
 public:
     HannkDelegateProvider() {
         default_params_.AddParam("use_hannk", ToolParam::Create<bool>(false));
-        default_params_.AddParam("verbosity", ToolParam::Create<int>(0));
+        default_params_.AddParam("hannk_verbosity", ToolParam::Create<int>(0));
     }
 
     std::vector<Flag> CreateFlags(ToolParams *params) const final {
         std::vector<Flag> flags = {
             CreateFlag<bool>("use_hannk", params, "use HANNK"),
-            CreateFlag<int>("verbosity", params, "Verbosity of HANNK debug logging"),
+            CreateFlag<int>("hannk_verbosity", params, "Verbosity of HANNK debug logging"),
         };
         return flags;
     }
 
     void LogParams(const ToolParams &params, bool verbose) const final {
         LOG_TOOL_PARAM(params, bool, "use_hannk", "Use HANNK", verbose);
-        LOG_TOOL_PARAM(params, int, "verbosity", "HANNK verbosity", verbose);
+        LOG_TOOL_PARAM(params, int, "hannk_verbosity", "HANNK verbosity", verbose);
     }
 
     TfLiteDelegatePtr CreateTfLiteDelegate(const ToolParams &params) const final {
         if (params.Get<bool>("use_hannk")) {
             HannkDelegateOptions options = {};
-            options.verbosity = params.Get<int32_t>("verbosity");
+            options.verbosity = params.Get<int32_t>("hannk_verbosity");
             if (options.verbosity >= 1) {
                 LOG(INFO) << "Registrar HannkDelegate: verbosity set to "
                           << options.verbosity << ".";
