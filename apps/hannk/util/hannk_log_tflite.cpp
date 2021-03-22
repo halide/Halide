69bdfb692a5608c65f2afb1ba858618ac63004fd
Steven Johnson <srj@google.com>
2021-03-22 16:31:49 -0700

    Create hannk_log_tflite.cpp

---
+++ apps/hannk/util/hannk_log_tflite.cpp
@@ -0,0 +1,25 @@
+#include "tensorflow/lite/tools/logging.h"
+#include "util/hannk_log.h"
+
+namespace hannk {
+namespace internal {
+
+void hannk_log(LogSeverity severity, const char *msg) {
+  switch (severity) {
+    case INFO:
+      TFLITE_LOG(INFO) << msg;
+      break;
+    case WARNING:
+      TFLITE_LOG(WARN) << msg;
+      break;
+    case ERROR:
+      TFLITE_LOG(ERROR) << msg;
+      break;
+    case FATAL:
+      TFLITE_LOG(FATAL) << msg;
+      break;
+  }
+}
+
+}  // namespace internal
+}  // namespace hannk
