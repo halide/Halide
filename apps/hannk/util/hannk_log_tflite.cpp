50136f26ca247c199eab5efcae3ff9bae078b524
Steven Johnson <srj@google.com>
2021-03-23 08:49:28 -0700

    Update hannk_log_tflite.cpp

--- apps/hannk/util/hannk_log_tflite.cpp
+++ apps/hannk/util/hannk_log_tflite.cpp
@@ -5,20 +5,20 @@ namespace hannk {
 namespace internal {
 
 void hannk_log(LogSeverity severity, const char *msg) {
-  switch (severity) {
+    switch (severity) {
     case INFO:
-      TFLITE_LOG(INFO) << msg;
-      break;
+        TFLITE_LOG(INFO) << msg;
+        break;
     case WARNING:
-      TFLITE_LOG(WARN) << msg;
-      break;
+        TFLITE_LOG(WARN) << msg;
+        break;
     case ERROR:
-      TFLITE_LOG(ERROR) << msg;
-      break;
+        TFLITE_LOG(ERROR) << msg;
+        break;
     case FATAL:
-      TFLITE_LOG(FATAL) << msg;
-      break;
-  }
+        TFLITE_LOG(FATAL) << msg;
+        break;
+    }
 }
 
 }  // namespace internal
