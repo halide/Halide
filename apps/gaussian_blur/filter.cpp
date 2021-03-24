6f8cc01276bde94b6e22fe6c5fe15e8f0d73a018
Andrew Adams <andrew.b.adams@gmail.com>
2021-03-24 14:35:39 -0700

    Use input padding rather than a boundary condition for box blur

--- apps/gaussian_blur/filter.cpp
+++ apps/gaussian_blur/filter.cpp
@@ -5,10 +5,10 @@
 #include "HalideBuffer.h"
 #include "HalideRuntime.h"
 
-#include "gaussian_blur.h"
-#include "gaussian_blur_direct.h"
 #include "box_blur.h"
 #include "box_blur_log.h"
+#include "gaussian_blur.h"
+#include "gaussian_blur_direct.h"
 
 #include "halide_benchmark.h"
 #include "halide_image_io.h"
@@ -23,6 +23,13 @@ int main(int argc, char **argv) {
 
     Halide::Runtime::Buffer<uint8_t> input = load_and_convert_image(argv[1]);
     Halide::Runtime::Buffer<uint8_t> output(input.width(), input.height());
+
+    int max_radius = 1024;
+    Halide::Runtime::Buffer<uint8_t> padded(input.width() + max_radius * 2,
+                                            input.height() + max_radius * 2);
+    padded.fill(0);
+    padded.set_min(-max_radius, -max_radius);
+    padded.cropped(0, 0, input.width()).cropped(1, 0, input.height()).copy_from(input);
 
     /*
     for (int r = 1; r < 20; r *= 2) {
@@ -42,32 +49,28 @@ int main(int argc, char **argv) {
     }
     */
 
-    for (int r = 1; r < 2048; r*=2) {
-
-        Halide::Runtime::Buffer<uint32_t> scratch(nullptr, 0, 0);
-        box_blur(input, r, scratch, output);
+    for (int r = 1; r <= 1024; r *= 2) {
+        // Assume a padded input
+        Halide::Runtime::Buffer<uint8_t> scratch(nullptr, 0, 0);
+        box_blur(input, r, output.width(), output.height(), scratch, output);
         scratch.allocate();
         printf("%d kilobytes of scratch\n", (int)(scratch.size_in_bytes() / 1024));
 
         double best_manual = benchmark(10, 10, [&]() {
-                                                 box_blur(input, r, scratch, output);
+            box_blur(padded, r, output.width(), output.height(), scratch, output);
             output.device_sync();
         });
         printf("Box blur (recursive) (%d): %gms\n", r, best_manual * 1e3);
     }
 
-    /*
-    for (int r = 1; r < 2048; r*=2) {
+    for (int r = 1; r < 256; r *= 2) {
 
-        double best_manual = benchmark(3, 3, [&]() {
-                                           box_blur_log(input, r, output);
+        double best_manual = benchmark(10, 10, [&]() {
+            box_blur_log(input, r, output);
             output.device_sync();
         });
-        printf("Box blur (recursive) (%d): %gms\n", r, best_manual * 1e3);
+        printf("Box blur (sparse/dense) (%d): %gms\n", r, best_manual * 1e3);
     }
-    */
-
-    convert_and_save_image(output, argv[2]);
 
     printf("Success!\n");
     return 0;
