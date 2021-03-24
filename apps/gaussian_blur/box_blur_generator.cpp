5447604ef0ffced36e6f67b9164f6a72cf0b8a78
Andrew Adams <andrew.b.adams@gmail.com>
2021-03-24 14:34:00 -0700

    Dithering

--- apps/gaussian_blur/box_blur_generator.cpp
+++ apps/gaussian_blur/box_blur_generator.cpp
@@ -6,145 +6,43 @@
 using namespace Halide;
 using namespace Halide::BoundaryConditions;
 
-
-#if 0
 class BoxBlur : public Generator<BoxBlur> {
 public:
     Input<Buffer<uint8_t>> input{"input", 2};
     Input<int> radius{"radius"};
-    Output<Buffer<uint8_t>> output{"output", 2};
-
-    Var x{"x"}, y{"y"};
-
-    Func blur_cols_transpose(Func in, Expr height) {
-        std::vector<Func> blurs;
-
-        Expr diameter = 2*radius + 1;
-        Expr inv_scale = 1.f / diameter;
-        RDom ry(-radius, height + diameter);
-
-        for (int working_bits : {16, 32}) {
-            Func blur{"blur_" + std::to_string(working_bits)};
-
-            Type t = UInt(working_bits);
-
-            // Pure definition: do nothing.
-            blur(x, y) = undef(t);
-
-            // The initial value of the accumulator should match the
-            // boundary condition we are using on the input, or the
-            // subtractions of the off-edge values done below will be
-            // incorrect.
-            blur(x, -radius - 1) = cast(t, diameter) * cast(t, in(x, 0));
-
-            // Derivative of a box
-            Expr v = cast<int16_t>(in(x, ry)) - in(x, ry - diameter);
-
-            // It's a 9-bit signed integer. Sign-extend then treat it as a
-            // uint16/32 with wrap-around. We know that the result can't
-            // possibly be negative in the end, so this gives us an extra
-            // bit of headroom while accumulating.
-            v = cast(t, cast(Int(working_bits), v));
-
-            blur(x, ry) = blur(x, ry - 1) + v;
-
-            blurs.push_back(blur);
-        }
-
-        // Select the appropriate blur for the radius. The other will
-        // be skipped.
-        Func blur;
-        blur(x, y) = select(diameter < 256,
-                            cast(Int(16), blurs[0](x, y)) * inv_scale,
-                            cast(Int(32), blurs[1](x, y)) * inv_scale);
-
-        // Transpose the blur and normalize.
-        Func transpose("transpose");
-        transpose(x, y) = cast<uint8_t>(round(clamp(blur(y, x), 0.f, 255.f)));
-
-        const int vec = get_target().natural_vector_size<uint8_t>();
-
-        // Schedule.  Split the transpose into tiles of
-        // rows. Parallelize strips.
-        Var xo, yo, xi, yi, strip;
-        transpose.compute_root()
-            .tile(x, y, xo, yo, x, y, vec, vec)
-            .vectorize(y)
-            .unroll(x)
-            .reorder(x, y, xo, yo)
-            .parallel(yo);
-
-        for (Func blur : blurs) {
-            // Run the filter on each row of tiles (which corresponds to a strip of
-            // columns in the input).
-            blur.compute_at(transpose, yo);
-
-            blur.update(0).vectorize(x, vec);
-
-            // Vectorize computations within the strips.
-            blur.update(1)
-                .reorder(x, ry)
-                .vectorize(x, vec);
-        }
-
-        // Load the input strip required in a pre-pass so that we
-        // don't incur stalls due to memory latency when doing the
-        // scan.
-        /*
-        in.in()
-            .compute_at(transpose, yo)
-            .vectorize(in.args()[0]);
-        */
-        return transpose;
-    }
-
-    void generate() {
-        // TODO: should actually be the output size
-        Expr width = input.width();
-        Expr height = input.height();
-
-        Func clamped = BoundaryConditions::repeat_edge(input);
-
-        // First, blur the columns of the input.
-        Func blury_T = blur_cols_transpose(clamped, height);
-
-        // Blur the columns again (the rows of the original).
-        Func blur = blur_cols_transpose(blury_T, width);
-
-        output = blur;
-    }
-};
-#endif
-
-class BoxBlur : public Generator<BoxBlur> {
-public:
-    Input<Buffer<uint8_t>> input{"input", 2};
-    Input<int> radius{"radius"};
-    Output<Buffer<uint32_t>> intermediate{"intermediate", 2};
+    Input<int> out_width{"out_width"}, out_height{"out_height"};
+    Output<Buffer<uint8_t>> intermediate{"intermediate", 2};
     Output<Buffer<uint8_t>> output{"output", 2};
 
     Var x{"x"}, y{"y"};
 
     Func blur_cols_transpose(Func in, Expr height, bool first_pass) {
-        Expr diameter = 2*radius + 1;
-        Expr inv_scale = 1.f / (diameter * diameter);
-        RDom ry(-radius, height + diameter);
+        Expr diameter = 2 * radius + 1;
+        Expr inv_scale = 1.f / diameter;
+        RDom r_init(-radius, diameter);
+        RDom ry(1, height - 1);
 
         Func blur{"blur"};
 
         Type t = UInt(32);
 
-        // Pure definition: do nothing.
+        Func wrap("wrap");
+        wrap(x, y) = in(x, y);
+
+        // Transpose the input
+        Func transpose("transpose");
+        transpose(x, y) = wrap(y, x);
+
+        // Blur in y
         blur(x, y) = undef(t);
 
-        // The initial value of the accumulator should match the
-        // boundary condition we are using on the input, or the
-        // subtractions of the off-edge values done below will be
-        // incorrect.
-        blur(x, -radius - 1) = cast(t, diameter) * cast(t, in(x, 0));
+        blur(x, 0) = cast(t, 0);
+        blur(x, 0) += cast(t, transpose(x, r_init));
 
         // Derivative of a box
-        Expr v = cast(Int(first_pass ? 16 : 32), in(x, ry)) - in(x, ry - diameter);
+        Expr v =
+            (cast(Int(first_pass ? 16 : 32), transpose(x, ry + radius)) -
+             transpose(x, ry - radius - 1));
 
         // It's a 9-bit signed integer. Sign-extend then treat it as a
         // uint16/32 with wrap-around. We know that the result can't
@@ -154,68 +52,63 @@ public:
 
         blur(x, ry) = blur(x, ry - 1) + v;
 
-        // Transpose the blur and normalize.
-        Func transpose("transpose");
-        if (first_pass) {
-            transpose(x, y) = blur(y, x);
-        } else {
-            transpose(x, y) = cast<uint8_t>(round(clamp(blur(y, x) * inv_scale, 0.f, 255.f)));
-        }
+        const int vec = get_target().natural_vector_size<uint16_t>();
 
-        const int vec = get_target().natural_vector_size<uint32_t>();
+        Func out;
+        out(x, y) = cast<uint8_t>(clamp(floor(cast<int32_t>(blur(x, y)) * inv_scale + random_float()), 0, 255));
 
         // Schedule.  Split the transpose into tiles of
         // rows. Parallelize strips.
-        Var xo, yo, xi, yi, strip;
-        transpose.compute_root()
-            .tile(x, y, xo, yo, x, y, vec, vec)
-            .vectorize(x)
-            .unroll(y)
-            .reorder(x, y, xo, yo)
-            .parallel(yo);
+        Var xo, yo, xi, yi;
+        out
+            .compute_root()
+            .split(x, xo, xi, vec)
+            .reorder(xi, y, xo)
+            .vectorize(xi)
+            .parallel(xo);
 
         // Run the filter on each row of tiles (which corresponds to a strip of
         // columns in the input).
-        blur.compute_at(transpose, yo)
+        blur.compute_at(out, xo)
             .store_in(MemoryType::Stack);
 
-        blur.update(0).vectorize(x, vec);
+        blur.update(0).vectorize(x);
+        blur.update(1).vectorize(x);
 
         // Vectorize computations within the strips.
-        blur.update(1)
+        blur.update(2)
             .reorder(x, ry)
-            .vectorize(x, vec);
+            .vectorize(x);
 
-        blur.in().compute_at(transpose, xo)
-            .reorder_storage(y, x)
+        transpose
+            .compute_at(out, xo)
+            .split(y, yo, yi, vec)
+            .unroll(x)
+            .vectorize(yi);
+
+        wrap
+            .compute_at(transpose, yo)
+            .store_in(MemoryType::Register)
             .vectorize(x)
             .unroll(y);
 
-        return transpose;
+        return out;
     }
 
     void generate() {
-        // TODO: should actually be the output size
-        Expr width = input.width();
-        Expr height = input.height();
-
-        Func clamped = BoundaryConditions::repeat_edge(input);
-
         // First, blur the columns of the input.
-        Func blury_T = blur_cols_transpose(clamped, height, true);
+        Func blury_T = blur_cols_transpose(input, out_width, true);
 
         intermediate = blury_T;
 
         // Blur the columns again (the rows of the original).
-        Func blur = blur_cols_transpose(blury_T, width, false);
+        Func blur = blur_cols_transpose(blury_T, out_height, false);
 
         output = blur;
     }
 };
 
-
 HALIDE_REGISTER_GENERATOR(BoxBlur, box_blur)
-
 
 class BoxBlurLog : public Generator<BoxBlurLog> {
 public:
@@ -224,7 +117,7 @@ public:
     Output<Buffer<uint8_t>> output{"output", 2};
 
     void generate() {
-        Expr diameter = cast<uint32_t>(2*radius + 1);
+        Expr diameter = cast<uint32_t>(2 * radius + 1);
         Var x, y;
         Func clamped = BoundaryConditions::repeat_edge(input);
 
@@ -248,7 +141,7 @@ public:
         }
 
         Func blur_y;
-        blur_y(x, y) = cast<uint8_t>(clamp((result + diameter/2) / diameter, 0, 255));
+        blur_y(x, y) = cast<uint8_t>(clamp((result + diameter / 2) / diameter, 0, 255));
 
         horiz_blurs.push_back(blur_y);
 
@@ -266,7 +159,7 @@ public:
             offset += select(use_this, (1 << i), 0);
         }
 
-        output(x, y) = cast<uint8_t>(clamp((result + diameter/2) / diameter, 0, 255));
+        output(x, y) = cast<uint8_t>(clamp((result + diameter / 2) / diameter, 0, 255));
 
         Var yi, yo;
         output
@@ -289,13 +182,6 @@ public:
                 .store_in(MemoryType::Stack)
                 .vectorize(x, natural_vector_size<uint16_t>());
         }
-
-        output.specialize(radius == 2);
-        //output.specialize_fail("poop");
-        /*
-        output.specialize(radius == 2);
-        output.specialize(radius == 4);
-        */
     }
 };
 
