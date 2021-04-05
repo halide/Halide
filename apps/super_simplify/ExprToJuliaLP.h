334fef262eadea1acec85f5662a31f998ecde8a0
Alexander Root <aroot222@mit.edu>
2021-04-05 17:08:57 -0400

    update super_synthesis_v3

--- apps/super_simplify/ExprToJuliaLP.h
+++
@@ -1,9 +0,0 @@
-#ifndef HL_JULIA_LP_H
-#define HL_JULIA_LP_H
-
-#include "Halide.h"
-#include <string>
-
-std::string expr_to_julia_lp(const Halide::Expr &expr, bool upper);
-
-#endif // HL_JULIA_LP_H
