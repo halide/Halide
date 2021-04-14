09a1123ad44f4bf9eebb84184331204895850d41
Z Stern <zalman@google.com>
2021-04-14 16:08:03 -0700

    Minor change to error text.

--- src/Pipeline.cpp
+++ src/Pipeline.cpp
@@ -933,7 +933,7 @@ void Pipeline::prepare_jit_call_arguments(RealizationArg &outputs, const Target 
                                           bool is_bounds_inference, JITCallArgs &args_result) {
     user_assert(defined()) << "Can't realize an undefined Pipeline\n";
 
-    user_assert(outputs.size() == this->outputs().size()) << "Realization requests " << outputs.size() << " output(s) for pipeline producing " << this->outputs().size() << " result(s).\n";
+    user_assert(outputs.size() == this->outputs().size()) << "Realization requires " << outputs.size() << " output(s) but pipeline produces " << this->outputs().size() << " result(s).\n";
 
     JITModule &compiled_module = contents->jit_module;
     internal_assert(compiled_module.argv_function() ||
