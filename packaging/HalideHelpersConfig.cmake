f0db1c6f6f0f40126c07594738dba1a8cec68ea4
Alex Reinking <alex.reinking@gmail.com>
2021-04-13 23:51:16 -0700

    Split Halide CMake helpers into separate package.

---
+++ packaging/HalideHelpersConfig.cmake
@@ -0,0 +1,5 @@
+cmake_minimum_required(VERSION 3.16)
+
+include(${CMAKE_CURRENT_LIST_DIR}/Halide-Interfaces.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/HalideTargetHelpers.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/HalideGeneratorHelpers.cmake)
