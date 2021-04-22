b6d1be8b803084149e25a1f989056c5934c7ecf3
Ankit Aggarwal <aankit@quicinc.com>
2021-04-22 13:59:04 -0500

    [Hexagon] Try vdelta/vrdelta before vlut for some shuffles.
    The patch tries to generate vdelta/vrdelta instructions for non-ramp
    shuffles. Eg:
    
    shuffle(lut_expr,
            < 0,  1,  2,  3,  4,  5,  6,  7,
              9, 10, 11, 12, 13, 14, 15, 16,
             18, 19, 20, 21, 22, 23, 24, 25,
             27, 28, 29, 30, 31, 32, 33, 34,
             36, 37, 38, 39, 40, 41, 42, 43,
             45, 46, 47, 48, 49, 50, 51, 52,
             54, 55, 56, 57, 58, 59, 60, 61,
             63, 64, 65, 66, 67, 68, 69, 70>)
    
    can be generated using vrdelta.
    
    The patch also fixes a bug where we bitcast vdelta/vrdelta with 16/32
    bits elements to wrong type. User would see the below error:
    
    llvm-project/llvm/lib/IR/Instructions.cpp:2905: static llvm::CastInst
    *llvm::CastInst::Create(Instruction::CastOps, llvm::Value *, llvm::Type *,
                            const llvm::Twine &, llvm::Instruction *):
                            Assertion `castIsValid(op, S, Ty) &&
                            "Invalid cast!"' failed.

--- src/CodeGen_Hexagon.cpp
+++ src/CodeGen_Hexagon.cpp
@@ -1240,7 +1240,7 @@ Value *CodeGen_Hexagon::shuffle_vectors(Value *a, Value *b,
             // Let LLVM handle concat or slices.
             return CodeGen_Posix::shuffle_vectors(a, b, indices);
         }
-        return vlut(concat_vectors({a, b}), indices);
+        return vdelta(concat_vectors({a, b}), indices);
     }
 
     if (stride == 1) {
@@ -1529,6 +1529,7 @@ bool generate_vdelta(const std::vector<int> &indices, bool reverse,
     return true;
 }
 
+// Try generating vdelta/vrdelta before falling back to vlut.
 Value *CodeGen_Hexagon::vdelta(Value *lut, const vector<int> &indices) {
     llvm::Type *lut_ty = lut->getType();
     int lut_elements = get_vector_num_elements(lut_ty);
@@ -1552,8 +1553,8 @@ Value *CodeGen_Hexagon::vdelta(Value *lut, const vector<int> &indices) {
             }
         }
         Value *result = vdelta(i8_lut, i8_indices);
-        result = builder->CreateBitCast(result, lut_ty);
-        return result;
+        llvm::Type *result_ty = get_vector_type(get_vector_element_type(lut_ty), indices.size());
+        return builder->CreateBitCast(result, result_ty);
     }
 
     // We can only use vdelta to produce a single native vector at a
@@ -1635,8 +1636,6 @@ Value *CodeGen_Hexagon::vdelta(Value *lut, const vector<int> &indices) {
 
     // TODO: If the above fails, we might be able to use a vdelta and
     // vrdelta instruction together to implement the shuffle.
-    internal_error << "Unsupported vdelta operation.\n";
-
     // TODO: If the vdelta results are sparsely used, it might be
     // better to use vlut.
     return vlut(lut, indices);
