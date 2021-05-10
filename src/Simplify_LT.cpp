f7e41b51f6dd452a54a7e0e4af259d4294c0ee8f
Dillon Sharlet <dsharlet@google.com>
2021-05-10 14:23:40 -0600

    Improve and add rules to simplify TailStrategy::Predicate/TailStrategy::GuardWithIf

--- src/Simplify_LT.cpp
+++ src/Simplify_LT.cpp
@@ -389,18 +389,14 @@ Expr Simplify::visit(const LT *op, ExprInfo *bounds) {
               rewrite(x < (x/c1)*c1, false, c1 > 0) ||
 
               // Cancel a division
-              rewrite((x + c1)/c0 < (x + c2)/c0, false, c0 > 0 && c1 >= c2) ||
-              rewrite((x + c1)/c0 < (x + c2)/c0, true, c0 > 0 && c1 <= c2 - c0) ||
+              rewrite((x + c1)/c0 < (x + c2)/c0, x%c0 < c2 - c1, c0 > 0) ||
               // c1 == 0
-              rewrite(x/c0 < (x + c2)/c0, false, c0 > 0 && 0 >= c2) ||
-              rewrite(x/c0 < (x + c2)/c0, true, c0 > 0 && 0 <= c2 - c0) ||
+              rewrite(x/c0 < (x + c2)/c0, x%c0 < c2, c0 > 0) ||
               // c2 == 0
-              rewrite((x + c1)/c0 < x/c0, false, c0 > 0 && c1 >= 0) ||
-              rewrite((x + c1)/c0 < x/c0, true, c0 > 0 && c1 <= 0 - c0) ||
+              rewrite((x + c1)/c0 < x/c0, x%c0 < -c1, c0 > 0) ||
 
               // The addition on the right could be outside
-              rewrite((x + c1)/c0 < x/c0 + c2, false, c0 > 0 && c1 >= c2 * c0) ||
-              rewrite((x + c1)/c0 < x/c0 + c2, true, c0 > 0 && c1 <= c2 * c0 - c0) ||
+              rewrite((x + c1)/c0 < x/c0 + c2, x%c0 < c2 * c0 - c1, c0 > 0) ||
 
               // With a confounding max or min
               rewrite((x + c1)/c0 < (min(x/c0, y) + c2), false, c0 > 0 && c1 >= c2 * c0) ||
@@ -441,6 +437,24 @@ Expr Simplify::visit(const LT *op, ExprInfo *bounds) {
               rewrite(min((x + c2)/c0, y) < x/c0, true, c0 > 0 && c2 + c0 <= 0) ||
               rewrite(max(y, (x + c2)/c0) < x/c0, false, c0 > 0 && c2 >= 0) ||
               rewrite(min(y, (x + c2)/c0) < x/c0, true, c0 > 0 && c2 + c0 <= 0) ||
+
+              rewrite((min(x*c0 + c1, y) + c2)/c0 < x, (y + c2)/c0 < x, c1 + c2 >= 0) ||
+              rewrite((min(x*c0 + c1, y) + c2)/c0 < x, true, c1 + c2 < 0) ||
+              rewrite((max(x*c0 + c1, y) + c2)/c0 < x, false, c1 + c2 >= 0) ||
+              rewrite((max(x*c0 + c1, y) + c2)/c0 < x, (y + c2)/c0 < x, c1 + c2 < 0) ||
+
+              rewrite((min(x*c0, y) + c2)/c0 < x, (y + c2)/c0 < x, c2 >= 0) ||
+              rewrite((min(x*c0, y) + c2)/c0 < x, true, c2 < 0) ||
+              rewrite((max(x*c0, y) + c2)/c0 < x, false, c2 >= 0) ||
+              rewrite((max(x*c0, y) + c2)/c0 < x, (y + c2)/c0 < x, c2 < 0) ||
+
+              rewrite(min(x*c0 + c1, y)/c0 < x, y/c0 < x, c1 >= 0) ||
+              rewrite(min(x*c0 + c1, y)/c0 < x, true, c1 < 0) ||
+              rewrite(max(x*c0 + c1, y)/c0 < x, false, c1 >= 0) ||
+              rewrite(max(x*c0 + c1, y)/c0 < x, y/c0 < x, c1 < 0) ||
+
+              rewrite(min(x*c0, y)/c0 < x, y/c0 < x) ||
+              rewrite(max(x*c0, y)/c0 < x, false) ||
 
               // Comparison of two mins/maxes that don't cancel when subtracted
               rewrite(min(x, c0) < min(x, c1), false, c0 >= c1) ||
