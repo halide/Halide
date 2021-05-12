89227d74c1ca634682673fe235d42b7b6862606a
Thales Sabino <thales@codeplay.com>
2021-05-12 15:10:09 +0100

    Add comment explaining the placement of the ExtractTensorCoreOperations visitor

--- src/OffloadGPULoops.cpp
+++ src/OffloadGPULoops.cpp
@@ -280,7 +280,9 @@ public:
         Stmt result = s;
 
         if (target.features_any_of({Target::CUDACapability70, Target::CUDACapability75, Target::CUDACapability80})) {
-            debug(2) << "Extracting CUDA Tensor Core Operations...\n";
+            // This needs to be done before the call to mutate(s) so the variables used within the inner loop
+            // can be picked by the HostClosure visitor
+            debug(1) << "Extracting CUDA Tensor Core Operations...\n";
             result = ExtractTensorCoreOperations{}.mutate(s);
             debug(2) << "After extracting CUDA Tensor Core Operations" << result << "\n";
         }
