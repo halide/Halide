b45caa8d7500b508d364801f1eccfd292065d0d6
Alex Reinking <alex_reinking@berkeley.edu>
2021-05-17 16:48:48 -0700

    Add Ubuntu packaging scripts and GHA testing (#5754)
    * Fix CMake & packaging glitches for Ubuntu package.
    
    * Add Ubuntu packaging scripts and presets.
    
    * Add GHA workflow to test packaging and usage on Ubuntu
    
    * Address review comments.

---
+++ test/integration/jit/main.cpp
@@ -0,0 +1,24 @@
+#include <Halide.h>
+#include <cstdio>
+#include <cstdlib>
+using namespace Halide;
+
+int main() {
+    Var x{"x"}, y{"y"};
+    Func test{"test"};
+
+    test(x, y) = x + y;
+    Buffer<int> output = test.realize({4, 4});
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            if (output(i, j) != (i + j)) {
+                fprintf(stderr, "output(%d, %d) = %d, expected %d", i, j, output(i, j), i + j);
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
+    printf("Success!\n");
+    return EXIT_SUCCESS;
+}
