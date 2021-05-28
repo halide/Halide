47c80e519e73229d5f9f1a757c9b21eeecf9a537
Ankit Aggarwal <aankit@quicinc.com>
2021-05-28 18:20:57 -0500

    Add missing file stubs.c

---
+++ apps/hannk/interpreter/stubs.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+extern "C" {
+
+void halide_print(void *user_context, const char *str) {
+    printf("%s", str);
+}
+
+void halide_error(void *user_context, const char *msg) {
+    halide_print(user_context, msg);
+}
+
+} // extern "C"
