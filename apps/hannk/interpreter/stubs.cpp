f2cacdaf3a906c19f2b9232facf370d955156983
Steven Johnson <srj@google.com>
2021-06-04 09:55:09 -0700

    clang-format

--- apps/hannk/interpreter/stubs.cpp
+++ apps/hannk/interpreter/stubs.cpp
@@ -10,8 +10,10 @@ void halide_error(void *user_context, const char *msg) {
     halide_print(user_context, msg);
 }
 
-void halide_profiler_report(void *user_context) {}
+void halide_profiler_report(void *user_context) {
+}
 
-void halide_profiler_reset() {}
+void halide_profiler_reset() {
+}
 
 }  // extern "C"
