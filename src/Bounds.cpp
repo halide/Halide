99aebfd701ae7221b017a64788909ac6ce555121
Alexander Root <aroot222@mit.edu>
2021-06-23 12:14:01 -0400

    substitute lets + CSE for find_constant_bounds

--- src/Bounds.cpp
+++ src/Bounds.cpp
@@ -19,6 +19,7 @@
 #include "PurifyIndexMath.h"
 #include "Simplify.h"
 #include "Solve.h"
+#include "Substitute.h"
 #include "Util.h"
 #include "Var.h"
 
@@ -66,7 +67,7 @@ Expr find_constant_bound(const Expr &e, Direction d, const Scope<Interval> &scop
 }
 
 Interval find_constant_bounds(const Expr &e, const Scope<Interval> &scope) {
-    Expr expr = simplify(remove_likelies(e));
+    Expr expr = common_subexpression_elimination(simplify(substitute_in_all_lets(remove_likelies(e))));
     Interval interval = bounds_of_expr_in_scope(expr, scope, FuncValueBounds(), true);
     interval.min = simplify(interval.min);
     interval.max = simplify(interval.max);
