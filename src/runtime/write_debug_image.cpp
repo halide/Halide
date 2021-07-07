7354fc2b4863c7774ec9eb3174f476efb607d5f5
Z Stern <zalman@google.com>
2021-07-07 11:18:57 -0700

    Adding padding byte size to outermost header byte count when writing MATLAB5 file format matrix. This ensures SciPy will successfully read files written by this routine.

--- src/runtime/write_debug_image.cpp
+++ src/runtime/write_debug_image.cpp
@@ -271,9 +271,11 @@ WEAK extern "C" int32_t halide_debug_to_file(void *user_context, const char *fil
         f.write(header, 128);
 
         size_t payload_bytes = buf->size_in_bytes();
+        final_padding_bytes = 7 - ((payload_bytes - 1) & 7);
 
-        // level 5 .mat files have a size limit
-        if ((uint64_t)payload_bytes >> 32) {
+        // level 5 .mat files have a size limit. (Padding itself should never cause the overfloaw.
+        // Code written this way for safety.)
+        if (((uint64_t)payload_bytes + final_padding_bytes) >> 32) {
             halide_error(user_context, "Can't debug_to_file to a .mat file greater than 4GB\n");
             return -6;
         }
@@ -288,7 +290,7 @@ WEAK extern "C" int32_t halide_debug_to_file(void *user_context, const char *fil
 
         uint32_t tags[] = {
             // This is a matrix
-            14, 40 + padded_dimensions * 4 + padded_name_size + (uint32_t)payload_bytes,
+            14, 40 + padded_dimensions * 4 + padded_name_size + (uint32_t)payload_bytes + final_padding_bytes,
             // The element type
             6, 8, pixel_type_to_matlab_class_code[type_code], 1,
             // The shape
@@ -312,8 +314,6 @@ WEAK extern "C" int32_t halide_debug_to_file(void *user_context, const char *fil
         if (!f.write(array_name, padded_name_size)) {
             return -10;
         }
-
-        final_padding_bytes = 7 - ((payload_bytes - 1) & 7);
 
         // Payload header
         uint32_t payload_header[2] = {
