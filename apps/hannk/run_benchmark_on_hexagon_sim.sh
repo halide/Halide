569db708bc83b1146250c3dd46f9dc630f1d9c94
Ankit Aggarwal <aankit@quicinc.com>
2021-07-08 01:37:05 -0500

    Corrections to run haank on hexagon-sim

--- apps/hannk/run_benchmark_on_hexagon_sim.sh
+++ apps/hannk/run_benchmark_on_hexagon_sim.sh
@@ -8,7 +8,7 @@ BIN=${BIN:-bin}
 
 echo "$HEXAGON_SDK_ROOT/rtos/qurt/computev65/debugger/lnx64/qurt_model.so" > $BIN/$HL_TARGET/osam.cfg
 echo "$DEFAULT_HEXAGON_TOOLS_ROOT/Tools/lib/iss/qtimer.so --csr_base=0xFC900000 --irq_p=1 --freq=19200000 --cnttid=1" > $BIN/$HL_TARGET/q6ss.cfg
-echo "$DEFAULT_HEXAGON_TOOLS_ROOT/Tools/lib/iss/l2vic.so 32 0xFC910000" >> $BIN)/$HL_TARGET/q6ss.cfg
+echo "$DEFAULT_HEXAGON_TOOLS_ROOT/Tools/lib/iss/l2vic.so 32 0xFC910000" >> $BIN/$HL_TARGET/q6ss.cfg
 
 $DEFAULT_HEXAGON_TOOLS_ROOT/Tools/bin/hexagon-sim \
     -mv65 \
@@ -20,4 +20,4 @@ $DEFAULT_HEXAGON_TOOLS_ROOT/Tools/bin/hexagon-sim \
     $HEXAGON_SDK_ROOT/rtos/qurt/computev65/sdksim_bin/runelf.pbn -- \
     $HEXAGON_SDK_ROOT/libs/run_main_on_hexagon/ship/hexagon_toolv84_v65/run_main_on_hexagon_sim \
     stack_size=0x400000 -- \
-    $PWD/$BIN/$HL_TARGET/$BENCHMARK_OUT --verbose $1;
+    $PWD/$BIN/$HL_TARGET/benchmark.so --verbose $1;
