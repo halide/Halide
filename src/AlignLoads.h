aaa2f0f101958de3618d8a9ac53d596fe6471dcc
Z Stern <zalman@google.com>
2021-08-12 10:33:52 -0700

    Add information to comment.

--- src/AlignLoads.h
+++ src/AlignLoads.h
@@ -13,7 +13,13 @@ namespace Internal {
 /** Attempt to rewrite unaligned loads from buffers which are known to
  * be aligned to instead load aligned vectors that cover the original
  * load, and then slice the original load out of the aligned
- * vectors. */
+ * vectors.
+ *
+ * Types that are less than min_bytes_to_align in size do not have
+ * alignment applied. This is intended to make a distinction between
+ * data that will be accessed as a scalar and that which will be
+ * accessed as a vector.
+ */
 Stmt align_loads(const Stmt &s, int alignment, int min_bytes_to_align);
 
 }  // namespace Internal
