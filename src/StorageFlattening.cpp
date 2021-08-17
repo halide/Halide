e0626eebedb7646da35ed9e2672776b8888d0264
Steven Johnson <srj@google.com>
2021-08-17 09:19:02 -0700

    Update StorageFlattening.cpp

--- src/StorageFlattening.cpp
+++ src/StorageFlattening.cpp
@@ -424,6 +424,7 @@ class PromoteToMemoryType : public IRMutator {
         Type t = upgrade(op->type);
         if (t != op->type) {
             auto [extents, changed] = mutate_exprs(op->extents);
+            (void)changed;  // unused
             return Allocate::make(op->name, t, op->memory_type, extents,
                                   mutate(op->condition), mutate(op->body),
                                   mutate(op->new_expr), op->free_function);
