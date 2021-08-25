b6afee277487ccaa4954b3d138d2b24200a0a46c
Steven Johnson <srj@google.com>
2021-08-25 14:34:48 -0700

    Hoist unrolled prefetches to top of the block
    When a loop with prefetch is unrolled, the prefetch instructions getting scattered through the loop can cause LLVM codegen issues in some cases (see https://bugs.llvm.org/show_bug.cgi?id=51172). As a partial mitigation for that issue, this PR adds a pass to hoist all prefetch instructions to the top of their loop. This is still a bit experimental; it definitely addresses the codegen issues we see, but makes the use of prefetch potentially less effective (since the hoisted prefetch may be too far from the eventual use to be effective).

--- src/Prefetch.cpp
+++ src/Prefetch.cpp
@@ -391,6 +391,43 @@ public:
     }
 };
 
+void traverse_block(const Stmt &s, std::function<void(const Stmt &)> f) {
+    const Block *b = s.as<Block>();
+    if (!b) {
+        f(s);
+    } else {
+        traverse_block(b->first, f);
+        traverse_block(b->rest, f);
+    }
+}
+
+class HoistPrefetches : public IRMutator {
+    using IRMutator::visit;
+
+    Stmt visit(const Block *op) override {
+        Stmt s = IRMutator::visit(op);
+
+        Stmt prefetches, body;
+        traverse_block(s, [&prefetches, &body](const Stmt &s) {
+            const Evaluate *eval = s.as<Evaluate>();
+            if (eval && Call::as_intrinsic(eval->value, {Call::prefetch})) {
+                prefetches = prefetches.defined() ? Block::make(prefetches, s) : s;
+            } else {
+                body = body.defined() ? Block::make(body, s) : s;
+            }
+        });
+        if (prefetches.defined()) {
+            if (body.defined()) {
+                return Block::make(prefetches, body);
+            } else {
+                return prefetches;
+            }
+        } else {
+            return body;
+        }
+    }
+};
+
 }  // anonymous namespace
 
 Stmt inject_placeholder_prefetch(const Stmt &s, const map<string, Function> &env,
@@ -432,6 +469,10 @@ Stmt reduce_prefetch_dimension(Stmt stmt, const Target &t) {
         stmt = SplitPrefetch(max_byte_size).mutate(stmt);
     }
     return stmt;
+}
+
+Stmt hoist_prefetches(const Stmt &s) {
+    return HoistPrefetches().mutate(s);
 }
 
 }  // namespace Internal
--- test/correctness/prefetch.cpp
+++ test/correctness/prefetch.cpp
@@ -403,6 +403,72 @@ int test11(const Target &t) {
     return 0;
 }
 
+int test12(const Target &t) {
+    Func f("f"), g("g"), h("h");
+    Var x("x"), y("y"), c("c"), b("b");
+    Var ci("ci"), co("co"), xo("xo");
+
+    f(c, x, y, b) = c + x + y + b;
+
+    RDom r(0, 4, 0, 1, 0, 16, "rdom");
+    g(c, x, y, b) = 0;
+    g(c, x, y, b) += f(r.z, x + r.x, y * 16 + r.y, b);
+
+    h(c, x, y, b) = cast<uint8_t>(g(c, x, y, b));
+
+    f.compute_root();
+
+    g.compute_at(h, co)
+        .store_in(MemoryType::Stack)
+        .reorder(x, c)
+        .vectorize(c, 16, TailStrategy::RoundUp);
+
+    // This schedule is deliberately constructed to unroll a loop with prefetches
+    // (so that hoist_prefetches() is tested).
+    RVar rco, rci;
+    g.update()
+        .split(r.z, rco, rci, 16)
+        .reorder(rci, c, x, rco, r.x, r.y)
+        .vectorize(c, 4, TailStrategy::RoundUp)
+        .unroll(c, 4, TailStrategy::RoundUp)
+        .atomic()
+        .vectorize(rci, 4)
+        .unroll(rci)
+        .unroll(x)
+        .prefetch(f, c, rco, /*offset*/ 1, PrefetchBoundStrategy::NonFaulting);
+
+    h.split(c, co, c, 16, TailStrategy::RoundUp)
+        .split(x, xo, x, 4, TailStrategy::RoundUp)
+        .reorder(x, c, co, xo, y, b)
+        .vectorize(c);
+
+    Module m = h.compile_to_module({}, "", t);
+    CollectPrefetches collect;
+    m.functions()[0].body.accept(&collect);
+
+    vector<vector<Expr>> expected;
+    for (int i = 0; i < 4; i++) {
+        Expr base = Variable::make(Handle(), f.name());
+        // The offset arg is a variable that is ticklish to get right, so just use a wildcard for matching
+        Expr offset = wild<int>();
+        if (t.has_feature(Target::HVX)) {
+            Expr extent0 = 16;
+            Expr stride0 = get_stride(t, 4);
+            Expr extent1 = 1;
+            Expr stride1 = wild<int>();
+            expected.push_back({base, offset, extent0, stride0, extent1, stride1});
+        } else {
+            Expr extent0 = 1;
+            Expr stride0 = get_stride(t, 4);
+            expected.push_back({base, offset, extent0, stride0});
+        }
+    }
+    if (!check(expected, collect.prefetches)) {
+        return -1;
+    }
+    return 0;
+}
+
 }  // anonymous namespace
 
 int main(int argc, char **argv) {
@@ -410,7 +476,7 @@ int main(int argc, char **argv) {
     std::cout << "Testing target: " << t << "\n";
 
     using Fn = int (*)(const Target &t);
-    std::vector<Fn> tests = {test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11};
+    std::vector<Fn> tests = {test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11, test12};
 
     for (size_t i = 0; i < tests.size(); i++) {
         printf("Running prefetch test %d\n", (int)i + 1);
