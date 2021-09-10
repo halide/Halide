1266884ccb8b9bcf242e54e48a1b102ff5de4eab
Andrew Adams <andrew.b.adams@gmail.com>
2021-09-09 19:38:47 -0700

    Join generator watchdog timer if exception thrown
    Make sure to join the generator watchdog timer when an exception is
    thrown. Otherwise the error message gets swallowed entirely and you just
    get "terminate called without an active exception"
    
    Also rejiggered the watchdog to do a timed wait on a condition variable
    instead of polling every 100ms.

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <cmath>
+#include <condition_variable>
 #include <fstream>
 #include <thread>
 #include <unordered_map>
@@ -990,21 +991,39 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &error_output
                                                               json_compiler_logger_factory :
                                                               no_compiler_logger_factory;
 
-    std::atomic<bool> generator_finished = false;
+    struct TimeoutMonitor {
+        std::atomic<bool> generator_finished = false;
+        std::thread thread;
+        std::condition_variable cond_var;
+        std::mutex mutex;
+
+        // Kill the timeout monitor as a destructor to ensure the thread
+        // gets joined in the event of an exception
+        ~TimeoutMonitor() {
+            generator_finished = true;
+            cond_var.notify_all();
+            thread.join();
+        }
+    } monitor;
+
     const int timeout_in_seconds = std::stoi(flags_info["-t"]);
     const auto timeout_time = std::chrono::steady_clock::now() + std::chrono::seconds(timeout_in_seconds);
-    std::thread timeout_monitor([timeout_time, timeout_in_seconds, &generator_finished]() {
+    monitor.thread = std::thread([timeout_time, timeout_in_seconds, &monitor]() {
+        std::unique_lock<std::mutex> lock(monitor.mutex);
+
         if (timeout_in_seconds <= 0) {
             // No watchdog timer, just let it run as long as it likes.
             return;
         }
-        while (!generator_finished) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            if (std::chrono::steady_clock::now() >= timeout_time) {
+        while (!monitor.generator_finished) {
+            auto now = std::chrono::steady_clock::now();
+            if (now > timeout_time) {
                 fprintf(stderr, "Timed out waiting for Generator to complete (%d seconds)!\n", timeout_in_seconds);
                 fflush(stdout);
                 fflush(stderr);
                 exit(1);
+            } else {
+                monitor.cond_var.wait_for(lock, timeout_time - now);
             }
         }
     });
@@ -1057,9 +1076,6 @@ int generate_filter_main_inner(int argc, char **argv, std::ostream &error_output
             compile_multitarget(function_name, output_files, targets, target_strings, module_factory, compiler_logger_factory);
         }
     }
-
-    generator_finished = true;
-    timeout_monitor.join();
 
     return 0;
 }
