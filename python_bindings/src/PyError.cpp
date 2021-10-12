0d5d63b59640a7c6075ade36176543e5c9231b9a
Andrew Adams <andrew.b.adams@gmail.com>
2021-10-12 15:27:08 -0700

    Fix more types in python bindings

--- python_bindings/src/PyError.cpp
+++ python_bindings/src/PyError.cpp
@@ -5,11 +5,11 @@ namespace PythonBindings {
 
 namespace {
 
-void halide_python_error(void *, const char *msg) {
+void halide_python_error(JITUserContext *, const char *msg) {
     throw Error(msg);
 }
 
-void halide_python_print(void *, const char *msg) {
+void halide_python_print(JITUserContext *, const char *msg) {
     py::print(msg, py::arg("end") = "");
 }
 
--- python_bindings/stub/PyStubImpl.cpp
+++ python_bindings/stub/PyStubImpl.cpp
@@ -35,11 +35,11 @@ namespace {
 // This seems redundant to the code in PyError.cpp, but is necessary
 // in case the Stub builder links in a separate copy of libHalide, rather
 // sharing the same halide.so that is built by default.
-void halide_python_error(void *, const char *msg) {
+void halide_python_error(JITUserContext *, const char *msg) {
     throw Error(msg);
 }
 
-void halide_python_print(void *, const char *msg) {
+void halide_python_print(JITUserContext *, const char *msg) {
     py::print(msg, py::arg("end") = "");
 }
 
