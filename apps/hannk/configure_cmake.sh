e443e1782d5e922282ea7f64283aef9b05464a95
Steven Johnson <srj@google.com>
2021-10-27 17:56:59 -0700

    [hannk] Add support for building/running for wasm

--- apps/hannk/configure_cmake.sh
+++ apps/hannk/configure_cmake.sh
@@ -3,56 +3,78 @@
 set -e
 
 HANNK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BUILD_DIR="${HANNK_DIR}/build"
 
-mkdir -p "${BUILD_DIR}"
-cd "${BUILD_DIR}"
+if [ -z ${BUILD_DIR} ]; then
+BUILD_DIR="${HANNK_DIR}/build"
+fi
 
 if [ -z ${HALIDE_INSTALL_PATH} ]; then
 HALIDE_INSTALL_PATH=${HOME}/halide-14-install/
 fi
-echo Using HalideInstall=${HALIDE_INSTALL_PATH}
 
 if [ -z ${HL_TARGET} ]; then
 HL_TARGET=host
 fi
-echo Using HL_TARGET=${HL_TARGET}
 
 if [ -z "${CMAKE_GENERATOR}" ]; then
 CMAKE_GENERATOR=Ninja
 fi
-echo Using build tool=${CMAKE_GENERATOR}
 
 if [ -z "${CMAKE_BUILD_TYPE}" ]; then
 CMAKE_BUILD_TYPE=Release
 fi
-echo Using CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 
 if [ -z "${HANNK_BUILD_TFLITE_DELEGATE}" ]; then
 HANNK_BUILD_TFLITE_DELEGATE=ON
 else
 HANNK_BUILD_TFLITE_DELEGATE=OFF
 fi
-echo Using HANNK_BUILD_TFLITE_DELEGATE=${HANNK_BUILD_TFLITE_DELEGATE}
 
+PREFIX=
 EXTRAS=
-# TODO: this doesn't work (yet); crosscompiling in CMake is painful.
+
 if [[ "${HL_TARGET}" =~ ^arm-64-android.* ]]; then
-echo Configuring for Android arm64-v8a build...
-echo Using ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT}
-EXTRAS="-DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a"
+
+  # TODO: this doesn't work (yet); crosscompiling in CMake is painful.
+  echo Configuring for Android arm64-v8a build...
+  echo Using ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT}
+  echo Using CMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_ROOT}/build/cmake/android.toolchain.cmake
+
+  EXTRAS="-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} -DANDROID_ABI=arm64-v8a"
+
+elif [[ "${HL_TARGET}" =~ ^wasm-32-wasmrt.* ]]; then
+
+    PREFIX=emcmake
+    HANNK_BUILD_TFLITE_DELEGATE=OFF
+
 else
-echo Assuming host build...
+
+  echo Assuming host build...
+
 fi
 
-cmake \
+if [ -n "${NODE_JS_EXECUTABLE}" ]; then
+EXTRAS="${EXTRAS} -DNODE_JS_EXECUTABLE=${NODE_JS_EXECUTABLE}"
+echo Using NODE_JS_EXECUTABLE=${NODE_JS_EXECUTABLE}
+fi
+
+echo Using HalideInstall=${HALIDE_INSTALL_PATH}
+echo Using BUILD_DIR=${BUILD_DIR}
+echo Using build tool=${CMAKE_GENERATOR}
+echo Using CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+echo Using HL_TARGET=${HL_TARGET}
+echo Using HANNK_BUILD_TFLITE_DELEGATE=${HANNK_BUILD_TFLITE_DELEGATE}
+
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+
+${PREFIX} cmake \
   ${EXTRAS} \
   -G "${CMAKE_GENERATOR}" \
   -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
-  -DHalide_DIR="${HALIDE_INSTALL_PATH}" \
-  -DCMAKE_PREFIX_PATH="${HALIDE_INSTALL_PATH}" \
+  -DHalide_DIR="${HALIDE_INSTALL_PATH}/lib/cmake/Halide" \
+  -DHalideHelpers_DIR="${HALIDE_INSTALL_PATH}/lib/cmake/HalideHelpers" \
   -DHalide_TARGET=${HL_TARGET} \
   -DHANNK_BUILD_TFLITE_DELEGATE=${HANNK_BUILD_TFLITE_DELEGATE} \
   -S "${HANNK_DIR}" \
   -B "${BUILD_DIR}"
-
