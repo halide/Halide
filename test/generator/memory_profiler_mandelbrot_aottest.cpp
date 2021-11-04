490ddfc6e7271dabffaaa59efd11ee135c2bac8f
Steven Johnson <srj@google.com>
2021-11-04 10:14:31 -0700

    Update memory_profiler_mandelbrot_aottest.cpp

--- test/generator/memory_profiler_mandelbrot_aottest.cpp
+++ test/generator/memory_profiler_mandelbrot_aottest.cpp
@@ -82,6 +82,8 @@ int main(int argc, char **argv) {
     printf("argmin expected value\n  stack peak: %d\n", argmin_stack_peak);
     printf("\n");
 
+    // Note that launcher_task() always returns zero, thus halide_do_par_for()
+    // should always return zero, but since this is a test, let's verify that.
     int result = halide_do_par_for(nullptr, launcher_task, 0, num_launcher_tasks, nullptr);
     assert(result == 0);
     (void)result;
