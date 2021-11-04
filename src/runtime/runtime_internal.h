5bc057f9b8ca05937c8736799a56d114447fe9c8
Steven Johnson <srj@google.com>
2021-11-04 11:08:17 -0700

    appease clang-format

--- src/runtime/runtime_internal.h
+++ src/runtime/runtime_internal.h
@@ -248,12 +248,12 @@ using namespace Halide::Runtime::Internal;
 /** halide_debug_assert() is like halide_assert(), but only expands into a check when
  * DEBUG_RUNTIME is defined. It is what you want to use in almost all cases. */
 #ifdef DEBUG_RUNTIME
-#define halide_debug_assert(user_context, cond)                                                                                                 \
-    do {                                                                                                                                        \
-        if (!(cond)) {                                                                                                                          \
+#define halide_debug_assert(user_context, cond)                                                                                           \
+    do {                                                                                                                                  \
+        if (!(cond)) {                                                                                                                    \
             halide_print(user_context, __FILE__ ":" _halide_expand_and_stringify(__LINE__) " halide_debug_assert() failed: " #cond "\n"); \
-            abort();                                                                                                                            \
-        }                                                                                                                                       \
+            abort();                                                                                                                      \
+        }                                                                                                                                 \
     } while (0)
 #else
 #define halide_debug_assert(user_context, cond)
