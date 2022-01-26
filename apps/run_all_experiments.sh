21cd434586647fe06962b42dc57bfff59b93ea53
Alexander Root <aroot222@mit.edu>
2022-01-26 12:29:59 -0500

    add experimental scripts

---
+++ apps/run_all_experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# make -C ../../ clean
+# make -C ../../ distrib -j32
+# APP_LIST="hist harris lens_blur max_filter unsharp camera_pipe iir_blur depthwise_separable_conv conv_layer bgu stencil_chain"
+APP_LIST="unsharp "
+
+for app in $APP_LIST; do
+    echo 
+    echo '***********' $app
+    echo
+    pushd ${app}
+    make clean
+    rm -rf results results_baseline
+    bash ../run_experiment.sh 256 512
+    popd
+done
