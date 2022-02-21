d0e0d7807043727c627758947a40f88c25233b1a
Alex Reinking <alex.reinking@gmail.com>
2022-02-21 10:21:04 -0800

    Make IRComparer consider nans to be less than non-nans. Fixes #6624

---
+++ test/correctness/cse_nan.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+
+#include "Halide.h"
+using namespace Halide;
+
+int main() {
+    ImageParam xyz{Float(32), 3, "xyz"};
+    Target t = get_host_target();
+    t.set_feature(Target::StrictFloat);
+
+    Var col{"col"}, row{"row"};
+    Func nan_or_one{"nan_or_one"};
+    nan_or_one(col, row) = Halide::select(is_nan(xyz(col, row, 0)), NAN, 1.0f);
+
+    Buffer<float> true_buf{1, 1, 1};
+    true_buf(0, 0, 0) = NAN;
+
+    Buffer<float> false_buf{1, 1, 1};
+    false_buf(0, 0, 0) = 2.0f;
+
+    Buffer<float> true_result{1, 1};
+    Buffer<float> false_result{1, 1};
+
+    xyz.set(true_buf);
+    nan_or_one.realize({true_result});
+
+    xyz.set(false_buf);
+    nan_or_one.realize({false_result});
+
+    if (std::isnan(true_result(0, 0)) && false_result(0, 0) == 1.0f) {
+        puts("Success!");
+    } else {
+        fprintf(stderr, "ERROR: T = %f ; TR = %f ; F = %f ; FR = %f\n",
+                true_buf(0, 0, 0), true_result(0, 0), false_buf(0, 0, 0), false_result(0, 0));
+    }
+}
