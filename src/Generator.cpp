52b96401c93f91eee3d484c853dec2c442b53ccc
Steven Johnson <srj@google.com>
2022-03-04 11:26:11 -0800

    Update Generator.cpp

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -647,7 +647,7 @@ void StubEmitter::emit() {
     for (const auto &out : out_info) {
         stream << get_indent() << out.getter << ",\n";
     }
-    stream << get_indent() << "generator->context().target\n";
+    stream << get_indent() << "generator->context().get_target()\n";
     indent_level--;
     stream << get_indent() << "};\n";
     indent_level--;
