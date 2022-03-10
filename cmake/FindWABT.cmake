b2c6f5c96e1abcb11870bbfff9b99bb5dc582955
Alex Reinking <alex.reinking@gmail.com>
2022-03-10 15:24:18 -0800

    Use WABT 1.0.27 (missed during rebase)

--- cmake/FindWABT.cmake
+++ cmake/FindWABT.cmake
@@ -1,22 +1,20 @@
 # TODO: if wabt ever corrects their CMake build, replace the FetchContent business
 #  with a proper find module or use their config mode.
 
-include(FindPackageHandleStandardArgs)
-include(FetchContent)
-
 function(_WABT_chainload)
     if (${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION)
         set(WABT_VERSION ${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION})
     else ()
-        set(WABT_VERSION 1.0.25)
+        set(WABT_VERSION 1.0.27)
     endif ()
 
+    include(FetchContent)
     FetchContent_Declare(
-            wabt
-            GIT_REPOSITORY https://github.com/WebAssembly/wabt.git
-            GIT_TAG ${WABT_VERSION}
-            GIT_SHALLOW TRUE
-            GIT_SUBMODULES ""
+        wabt
+        GIT_REPOSITORY https://github.com/WebAssembly/wabt.git
+        GIT_TAG ${WABT_VERSION}
+        GIT_SHALLOW TRUE
+        GIT_SUBMODULES ""
     )
 
     set(WITH_EXCEPTIONS "${WABT_WITH_EXCEPTIONS}")
@@ -39,4 +37,15 @@ endfunction()
 
 _WABT_chainload()
 
+include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(WABT CONFIG_MODE)
+
+# Delete the function to prevent it from being improperly called outside this
+# module. After the first redefinition, the original function is still present
+# as __WABT_chainload. The second redefinition overwrites __WABT_chainload so
+# that calling either one is a no-op.
+function(_WABT_chainload)
+endfunction()
+
+function(_WABT_chainload)
+endfunction()
