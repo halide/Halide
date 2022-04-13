a12e2f7476cd2bedf618d5892da85f87f828fcdd
rootbutcher2 <james.oreilly@mnsu.edu>
2022-04-12 19:38:29 -0500

    Makes a .gen but needs alot of work

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,28 @@
 pub mod Halide_Build{
     use std::ffi::OsString;
     use std::path::PathBuf;
-    use std::process::Command;
+    use std::io;
+    use std::io::prelude::*;
+    use std::process::{Command, Output};
 
     pub struct Halide{
-        halide_path: PathBuf,
-        gen_path: PathBuf,
-        rs_out_path: PathBuf,
-        generators: Vec<Halide_Gen>,
+        pub halide_path: PathBuf,
+        pub gen_path: PathBuf,
+        pub rs_out_path: PathBuf,
+        pub generators: Vec<Halide_Gen>,
     }
 
-    struct Halide_Gen{
-        halide_path: PathBuf,
-        gen_name: String,
-        gen_path: PathBuf,
-        rs_out_path: PathBuf,
+    pub struct Halide_Gen{
+        pub(crate) halide_path: PathBuf,
+        pub(crate) gen_name: String,
+        pub(crate) gen_path: PathBuf,
+        pub(crate) rs_out_path: PathBuf,
     }
+    impl Halide{
 
+    }
     impl Halide_Gen {
-        fn compile_gen(&mut self) -> std::io::Result<Output> {
+        pub fn compile_gen(&mut self) -> Output {
             let mut out = Command::new("g++");
 
             out.args(["-O3", "-O3","-std=c++17"]);
@@ -29,31 +33,31 @@ pub mod Halide_Build{
             self.halide_path.push("distrib");
             self.halide_path.push("include");
 
-            out.args(["-I",self.halide_path.as_os_str()]);
+            out.args(["-I",self.halide_path.to_str().unwrap()]);
 
             self.halide_path.pop();
             self.halide_path.push("tools");
-            out.args(["-I",self.halide_path.as_os_str()]);
+            out.args(["-I",self.halide_path.to_str().unwrap()]);
 
-            self.gen_path.push(gen_name);
+            self.gen_path.push(self.gen_name.as_str());
             self.halide_path.push("GenGen.cpp");
-            out.args(["-g",self.gen_path.as_os_str(),self.halide_path.as_os_str()]);
+            out.args(["-g",self.gen_path.to_str().unwrap(),self.halide_path.to_str().unwrap()]);
 
-            self.gen_path.set_extension(".gen");
-            out.args(["-o",self.gen_path.as_os_str()]);
+            self.gen_path.set_extension("gen");
+            out.args(["-o",self.gen_path.to_str().unwrap()]);
 
             self.halide_path.pop();
             self.halide_path.pop();
             self.halide_path.push("lib");
-            out.args(["-L",self.halide_path.as_os_str()]);
+            out.args(["-L",self.halide_path.to_str().unwrap()]);
 
             let mut temp:String = "-Wl,-rpath,".to_string();
-            temp.push_str(self.halide_path.to_str().unwrao());
+            temp.push_str(self.halide_path.to_str().unwrap());
             out.arg(temp);
 
             out.args(["-lHalide","-ldl","-lpthread","-lz"]);
 
-            out.output()
+            out.output().expect("Building the gererator failed")
         }
 
     }
@@ -61,9 +65,28 @@ pub mod Halide_Build{
 
 #[cfg(test)]
 mod tests {
+    use std::path::{Path, PathBuf};
+    use crate::Halide_Build::*;
+    use std::io;
+    use std::io::prelude::*;
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut Hs= Halide{
+            halide_path: PathBuf::from("/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide"),
+            gen_path: PathBuf::from("/home/rootbutcher2/CLionProjects/Halide_Build/Halide_gens"),
+            rs_out_path: PathBuf::from("/home/rootbutcher2/CLionProjects/Halide_Build/rs_out"),
+            generators: vec![]
+        };
+        Hs.generators.push(Halide_Gen{
+            halide_path: Hs.halide_path,
+            gen_name: "iir_blur.cpp".to_string(),
+            gen_path: Hs.gen_path,
+            rs_out_path: Hs.rs_out_path
+        });
+        let output = Hs.generators[0].compile_gen();
+        println!("status: {}", output.status);
+        io::stdout().write_all(&output.stdout).unwrap();
+        io::stderr().write_all(&output.stderr).unwrap();
+        assert!(output.status.success());
     }
 }
