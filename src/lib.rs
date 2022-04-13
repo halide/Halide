a67fb4c413c468daeccf22887280705de649d191
rootbutcher2 <james.oreilly@mnsu.edu>
2022-04-12 20:13:06 -0500

    makes iir_blur.gen iir_blur.h and iir_blur.a

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod Halide_Build{
     use std::path::PathBuf;
     use std::io;
     use std::io::prelude::*;
+    use std::ops::Add;
     use std::process::{Command, Output};
 
     pub struct Halide{
@@ -16,10 +17,10 @@ pub mod Halide_Build{
     }
 
     pub struct Halide_Gen{
-        pub(crate) halide_path: PathBuf,
-        pub(crate) gen_name: String,
-        pub(crate) gen_path: PathBuf,
-        pub(crate) rs_out_path: PathBuf,
+        pub halide_path: PathBuf,
+        pub gen_name: String,
+        pub gen_path: PathBuf,
+        pub rs_out_path: PathBuf,
     }
     impl Halide{
 
@@ -40,6 +41,7 @@ pub mod Halide_Build{
             out.args(["-I",self.halide_path.to_str().unwrap()]);
 
             self.gen_path.push(self.gen_name.as_str());
+            self.gen_path.set_extension("cpp");
             self.halide_path.push("GenGen.cpp");
             out.args(["-g",self.gen_path.to_str().unwrap(),self.halide_path.to_str().unwrap()]);
 
@@ -59,7 +61,16 @@ pub mod Halide_Build{
 
             out.output().expect("Building the gererator failed")
         }
+    pub fn run_gen(&self) -> Output {
 
+        let mut out = Command::new(self.gen_path.to_str().unwrap());
+        out.args(["-g",self.gen_name.as_str()]);
+        out.args(["-f", self.gen_name.as_str()]);
+
+        out.args(["-o","/home/rootbutcher2/CLionProjects/Halide_Build/Halide_gens"]);
+        out.arg("target=host-no_runtime");
+        out.output().expect("failed to run gen")
+    }
     }
 }
 
@@ -79,14 +90,20 @@ mod tests {
         };
         Hs.generators.push(Halide_Gen{
             halide_path: Hs.halide_path,
-            gen_name: "iir_blur.cpp".to_string(),
+            gen_name: "iir_blur".to_string(),
             gen_path: Hs.gen_path,
             rs_out_path: Hs.rs_out_path
         });
-        let output = Hs.generators[0].compile_gen();
-        println!("status: {}", output.status);
-        io::stdout().write_all(&output.stdout).unwrap();
-        io::stderr().write_all(&output.stderr).unwrap();
-        assert!(output.status.success());
+        let out1 = Hs.generators[0].compile_gen();
+        println!("status: {}", out1.status);
+        io::stdout().write_all(&out1.stdout).unwrap();
+        io::stderr().write_all(&out1.stderr).unwrap();
+        //assert!(output.status.success());
+
+        let out2 = Hs.generators[0].run_gen();
+        println!("status: {}", out2.status);
+        io::stdout().write_all(&out2.stdout).unwrap();
+        io::stderr().write_all(&out2.stderr).unwrap();
+        assert!(out2.status.success());
     }
 }
