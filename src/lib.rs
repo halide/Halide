dba03459012c7a8836dd20a1ca66c104686c5a10
rootbutcher2 <james.oreilly@mnsu.edu>
2022-04-19 15:25:56 -0500

    working design

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ use std::path::PathBuf;
 use std::process::Command;
 
 pub mod halide_build {
-    use std::default;
+    use std::{default, fs};
     use std::env;
     use std::fs::{remove_file,rename};
     use std::io::Result;
+    use std::ops::Add;
     use std::path::{Path,PathBuf};
     use std::process::{Command, Output};
 
@@ -19,10 +20,11 @@ pub mod halide_build {
 
         rs_output: PathBuf,
 
+        //Generators: Vec<Generator<'static>>,
         debug: bool,
     }
     //#[derive(Clone)]
-    pub struct Generator {
+    pub struct Generator<'a> {
         gen_name: String,
         gen_exe: PathBuf,
         halide_path: PathBuf,
@@ -30,7 +32,7 @@ pub mod halide_build {
 
         rs_output: PathBuf,
 
-        gcc_flags: Option<Vec<String>>,
+        gcc_flags: Vec<&'a str>,
         debug: bool,
     }
 
@@ -38,30 +40,41 @@ pub mod halide_build {
         pub fn new<T: Into<PathBuf>>(
             halide_path: T,
             gen_path: T,
-            rs_output:T
+            //rs_output:T
         ) -> Halide {
             Halide {
                 halide_path: halide_path.into().join("distrib"),
                 gen_path:gen_path.into(),
-                rs_output:rs_output.into(),
+                rs_output:PathBuf::from(env::var("OUT_DIR").unwrap()),
+                //Generators: Vec!{},
                 debug: false
             }
         }
 
-        pub fn newGen(self,gen_name:String)->Generator{
-            Generator {
-                gen_name: gen_name.to_string(),
-                gen_exe: PathBuf::new().join("target").join(gen_name.to_string()).with_extension("generator"),
-                halide_path: self.halide_path,
-                gen_path: self.gen_path.join(gen_name).with_extension("cpp"),
-                rs_output: self.rs_output,
-                gcc_flags: None,
-                debug: false
-            }
+        pub fn out_dir<T:Into<PathBuf>>(mut self,out:T)->Self{
+            self.rs_output = out.into();
+            self
         }
+        //Todo template all strings
+        pub fn newGen(self, gen_name:String) ->Generator<'static>{
+            println!("cargo:rerun-if-changed={}",self.gen_path.join(gen_name.as_str()).with_extension("cpp").to_str().unwrap());
+            //self.Generators.push(
+            Generator {
+                    gen_name: gen_name.to_string(),
+                    gen_exe: PathBuf::new().join("target").join(gen_name.to_string()).with_extension("generator"),
+                    halide_path: self.halide_path.clone(),
+                    gen_path: self.gen_path.join(gen_name).with_extension("cpp"),
+                    rs_output: self.rs_output.clone(),
+                    gcc_flags: vec!["-lHalide","-ldl","-lpthread","-lz"],
+                    debug: self.debug
+            }
+            //);self
+        }
+
+
     }
 
-    impl Generator {
+    impl Generator <'static>{
         pub fn make(&self) -> Output {
             let mut compile = Command::new("g++");
             compile.args(["-std=c++17",]);
@@ -74,24 +87,49 @@ pub mod halide_build {
 
             let mut temp = self.halide_path.join("tools").join("GenGen").with_extension("cpp");
             compile.args(["-g",self.gen_path.to_str().unwrap(),temp.to_str().unwrap()]);
-            compile.args(["-lHalide","-ldl","-lpthread","-lz"]);
+            compile.args(self.gcc_flags.clone());
             //compile.args(["-Wl,-rpath","-Wl,/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/lib/"]);
             compile.output().expect("Make generator failed")
 
         }
-        pub fn run_gen(mut self) -> Output {
+        pub fn run_gen(&self) -> Output {
             //assert!(!self.gen_exe.is_none());
+            //todo change to build target
+            println!("cargo:rustc-link-search=native={}", self.rs_output.to_str().unwrap());
 
             let mut gen = Command::new(self.gen_exe.to_str().unwrap());
             gen.args(["-g",self.gen_name.as_str()]);
             gen.args(["-f", self.gen_name.as_str()]);
+
+            //Todo change to build dir
             gen.args(["-o",self.rs_output.to_str().unwrap()]);
             gen.env("LD_LIBRARY_PATH", self.halide_path.join("lib"));
 
+            //Todo add debug
             gen.args(["target=host-no_runtime"]);
 
             gen.output().expect("failed to run")
 
+        }
+
+        pub fn rename_move(&self ) -> Result<()> {
+            println!("file name: {:?}",self.rs_output.join(self.gen_name.as_str()).with_extension("a").to_str().unwrap());
+            println!("file out: {:?}",self.rs_output.join(String::from("lib").add(self.gen_name.as_str())).with_extension("a").to_str().unwrap());
+            fs::rename(
+                self.rs_output.join(self.gen_name.as_str()).with_extension("a").to_str().unwrap(),
+                self.rs_output.join(String::from("lib").add(self.gen_name.as_str())).with_extension("a").to_str().unwrap()
+            )
+        }
+
+        pub fn bind(&self) -> Result<()> {
+            let bindings = bindgen::Builder::default()
+                .header(self.rs_output.join(self.gen_name.as_str()).with_extension("h").to_str().unwrap())
+                .allowlist_function(self.gen_name.as_str())
+                .blocklist_item("halide_buffer_t")
+                .generate().expect("unable to gen bindings").write_to_file(self.rs_output.join(self.gen_name.as_str()).with_extension("rs"));
+
+            println!("cargo:rustc-link-lib=static={}", self.gen_name);
+            bindings
         }
     }
 }
@@ -104,12 +142,12 @@ pub mod halide_build {
 
         #[test]
         fn it_works() {
-            let H = Halide::new(
+            let mut H = Halide::new(
                 "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide",
-                "/home/rootbutcher2/CLionProjects/halide_build/Halide_gens",
-                "/home/rootbutcher2/CLionProjects/halide_build/rs_out"
+                "/home/rootbutcher2/CLionProjects/halide_build/Halide_gens"
             );
-            let G= H.newGen("iir_blur".to_string());
+            let G = H.newGen("iir_blur".to_string());
+
             let out = G.make();
             println!("Gen Creation Status: {}", out.status);
             io::stdout().write_all(&out.stdout).unwrap();
@@ -121,5 +159,13 @@ pub mod halide_build {
             io::stdout().write_all(&out2.stdout).unwrap();
             io::stderr().write_all(&out2.stderr).unwrap();
             assert!(out2.status.success());
+
+            let out3 = G.rename_move();
+            println!("move results: {:?}", out3);
+            assert!(out3.is_ok());
+
+            let out4 = G.bind();
+            println!("bind results: {:?}", out4);
+            assert!(out4.is_ok());
         }
     }
