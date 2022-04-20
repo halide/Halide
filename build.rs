759a1537a4b2a471e5f8b414eece0417fba53f60
rootbutcher2 <james.oreilly@mnsu.edu>
2022-04-20 14:01:00 -0500

    builds runtime with dynamicly

--- build.rs
+++ build.rs
@@ -7,13 +7,13 @@ fn main() {
     let Hal = GenBuilder::new(
         "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide",
         "src/gens"
-    );
+    ).debug(true);
        // .out_dir("src/rs");
 
 
     let gen = Hal.new_gen("iir_blur".to_string());
 
-    let out = gen.make();
+    let out = gen.compile();
     println!("compile Status: {}", out.status.success());
     io::stdout().write_all(&out.stdout);
     io::stderr().write_all(&out.stderr);
@@ -23,6 +23,7 @@ fn main() {
 
     assert!(gen.bind().is_ok());
 
-    assert!(gen.rename_move().is_ok());
+    assert!(gen.rename().is_ok());
+    assert!(gen.make_runtime().is_ok());
 
 }
