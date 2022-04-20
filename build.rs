f0064a5ab15b5f306a32b991c021746e374113e3
rootbutcher2 <james.oreilly@mnsu.edu>
2022-04-19 19:07:10 -0500

    update to reflect crate and mod names

--- build.rs
+++ build.rs
@@ -1,16 +1,16 @@
 
-use halide_build::halide_build::{Generator,Gen_Builder};
+use halide::build::{Generator,GenBuilder};
 
 fn main() {
 
-    let Hal = Gen_Builder::new(
+    let Hal = GenBuilder::new(
         "/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide",
         "src/gens"
     );
        // .out_dir("src/rs");
 
 
-    let gen = Hal.newGen("iir_blur".to_string());
+    let gen = Hal.new_gen("iir_blur".to_string());
 
     assert!(gen.make().status.success());
 
