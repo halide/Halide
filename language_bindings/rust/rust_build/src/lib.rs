f798d550163410c72ef17695c80dbe2c77e58a86
Mayor-Jellies <james.oreilly@mnsu.edu>
2022-04-27 22:32:05 -0500

    rust fmt + runtime without generator

--- language_bindings/rust/rust_build/src/lib.rs
+++ language_bindings/rust/rust_build/src/lib.rs
@@ -6,18 +6,17 @@
 //! IIR blur takes a image input, goes through the full halide pipeline, and outputs a blurred image all using rust.
 //!
 
-///Required module for use in build.rs scripts
-///
-/// This Module will compile and bind a generator and create a Halide runtime.
-///
-//TODO update rust docs
-
-use std::{env, io};
 use std::fs;
 use std::io::{ErrorKind, Result};
 use std::ops::Add;
 use std::path::PathBuf;
 use std::process::{Command, Output};
+///Required module for use in build.rs scripts
+///
+/// This Module will compile and bind a generator and create a Halide runtime.
+///
+//TODO update rust docs
+use std::{env, io};
 
 /// This builder must have a path to a complied Halide folder. It is used to build multiple generators quickly
 ///
@@ -36,7 +35,6 @@ pub struct GenBuilder {
 /// This represents a halide generator that is built using [GenBuilder]
 ///
 ///
-
 pub struct Generator<'a> {
     gen_name: String,
     gen_exe: PathBuf,
@@ -78,7 +76,7 @@ impl GenBuilder {
     /// Override the output directory.
     ///
     /// by default this is the ENV "out_dir" but can be anywere
-    /// useful to place .rs and headers in a folder in the src of your project but doing that 
+    /// useful to place .rs and headers in a folder in the src of your project but doing that
     /// means cargo does not clean these artifacts
     pub fn out_dir<T: Into<PathBuf>>(mut self, out: T) -> Self {
         self.rs_output = out.into();
@@ -92,7 +90,6 @@ impl GenBuilder {
 
      */
     //Todo template all strings
-
     /// Create a generator based on its name
     ///
     /// We have strict naming conventions:   
@@ -103,21 +100,27 @@ impl GenBuilder {
     /// let gen = genBuilder.new_gen(iir_blur);
     /// ```
     pub fn new_gen(self, gen_name: String) -> Generator<'static> {
-        println!(
-            "cargo:rerun-if-changed={}",
-            self.gen_path
-                .join(gen_name.as_str())
-                .with_extension("cpp")
-                .to_str()
-                .unwrap()
-        );
-        //self.generators.push(
+        let mut temp_gen_exe = PathBuf::from(self.rs_output.to_str().unwrap());
+        if gen_name.is_empty() {
+            temp_gen_exe.push("runtime");
+        } else {
+            println!(
+                "cargo:rerun-if-changed={}",
+                self.gen_path
+                    .join(gen_name.as_str())
+                    .with_extension("cpp")
+                    .to_str()
+                    .unwrap()
+            );
+            temp_gen_exe.push(gen_name.as_str());
+        }
+        temp_gen_exe.with_extension("generator");
+
+        println!("temp: {:?}", temp_gen_exe.as_os_str());
+
         Generator {
             gen_name: gen_name.to_string(),
-            gen_exe: PathBuf::new()
-                .join("target")
-                .join(gen_name.to_string())
-                .with_extension("generator"),
+            gen_exe: temp_gen_exe,
             halide_path: self.halide_path.clone(),
             gen_path: self.gen_path.join(gen_name).with_extension("cpp"),
             rs_output: self.rs_output.clone(),
@@ -125,31 +128,33 @@ impl GenBuilder {
             debug: self.debug,                                       //Todo add functionality
             target: self.target,                                     //todo add setter
         }
-        //);self
     }
     /// Sets the console debug for Halide generators by default this is false
-    pub fn debug(mut self, b:bool) ->Self{
-        self.debug=b;
+    pub fn debug(mut self, b: bool) -> Self {
+        self.debug = b;
         self
     }
-    /*
+    ///
+    ///
+    ///
     pub fn make_runtime(self) {
-        if self.generators.is_empty() {
-            //todo make gen-gen
-        }
-        else {
-           let _gen = self.generators[0].make_runtime();
-        }
-    }
+        let gen = self.new_gen("".to_string());
 
-     */
+        let compilation_result = gen.compile();
+        println!("Gen Creation Status: {}", compilation_result.status);
+        assert!(compilation_result.status.success());
+
+        let execution_result = gen.make_runtime();
+        println!("runtime results: {:?}", execution_result);
+        assert!(execution_result.is_ok());
+
+        gen.rename_runtime();
+    }
 }
-///This is a generator built using [GenBuilder].new_gen()
-///
+
 impl Generator<'static> {
     ///Make the generator executable using halide gengen and g++
     pub fn compile(&self) -> Output {
-
         let mut cmd_compile = Command::new("g++");
         cmd_compile.args(["-std=c++17"]);
 
@@ -160,25 +165,26 @@ impl Generator<'static> {
         // Linker flag
         cmd_compile.args(["-L", self.halide_path.join("lib").to_str().unwrap()]);
 
-        // Output flag
-        cmd_compile.args(["-o", self.gen_exe.to_str().unwrap()]);
-
         let temp = self
             .halide_path
             .join("tools")
             .join("GenGen")
             .with_extension("cpp");
-
-        cmd_compile.args([
-            "-g",
-            self.gen_path.to_str().unwrap(),
-            temp.to_str().unwrap(),
-        ]);
+        if self.gen_name.is_empty() {
+            cmd_compile.args(["-g", temp.to_str().unwrap()]);
+        } else {
+            cmd_compile.args([
+                "-g",
+                self.gen_path.to_str().unwrap(),
+                temp.to_str().unwrap(),
+            ]);
+        }
+        // Output flag
+        cmd_compile.args(["-o", self.gen_exe.to_str().unwrap()]);
 
         cmd_compile.args(self.gcc_flags.clone());
         //compile.args(["-Wl,-rpath","-Wl,/home/rootbutcher2/CLionProjects/Halide-Rusts-tests/Halide/distrib/lib/"]);
         cmd_compile.output().expect("Make generator failed")
-
     }
     /// Runs the previously made executable
     ///
@@ -216,26 +222,36 @@ impl Generator<'static> {
     ///
     /// The Halide runtime is required for using halide and contains the buffer_t and other useful functions
     pub fn make_runtime(&self) -> Result<()> {
-        println!("Cmd to runt: {:?}", self.gen_exe.to_str().unwrap());
+        println!(
+            "cargo:rustc-link-search=native={}",
+            self.rs_output.to_str().unwrap()
+        );
+        println!("Cmd to run: {:?}", self.gen_exe.to_str().unwrap());
         let mut cmd = Command::new(self.gen_exe.to_str().unwrap());
-        cmd.args(["-r", "runtime", "-o", self.rs_output.to_str().unwrap(),"target=host"]);
+        cmd.args([
+            "-r",
+            "runtime",
+            "-o",
+            self.rs_output.to_str().unwrap(),
+            "target=host",
+        ]);
         cmd.env("LD_LIBRARY_PATH", self.halide_path.join("lib"));
         let output = cmd.output().expect("failed to make runtime");
         let result;
         if output.status.success() {
             result = self.rename_runtime();
-            if result.is_ok(){
+            if result.is_ok() {
                 println!("cargo:rustc-link-lib=static={}", "runtime");
             }
-        }
-        else {
-            let errors = String::from("failed to make runtime with gen: ").add(self.gen_name.as_str());
+        } else {
+            let errors =
+                String::from("failed to make runtime with gen: ").add(self.gen_name.as_str());
             result = Err(io::Error::new(ErrorKind::Other, errors.as_str()));
         }
         result
     }
-    
-    fn rename_runtime(&self)->Result<()>{
+
+    fn rename_runtime(&self) -> Result<()> {
         println!(
             "file name: {:?}",
             self.rs_output
@@ -329,14 +345,13 @@ impl Generator<'static> {
     /// This complies-> runs-> renames-> binds the generator
     ///
     /// panics if any step fails if it panics it is useful to call the functions separately and print there results or outputs
-    pub fn build_bind(&self){
-
+    pub fn build_bind(&self) {
         assert!(self.compile().status.success());
-    
+
         assert!(self.run_gen().status.success());
-    
+
         assert!(self.bind().is_ok());
-    
+
         assert!(self.rename().is_ok());
         assert!(self.make_runtime().is_ok());
     }
