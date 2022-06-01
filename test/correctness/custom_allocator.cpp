7c54a5c4a7f28173939e592615c519dafedb5de2
Steven Johnson <srj@google.com>
2022-05-31 17:47:33 -0700

    Update custom_allocator.cpp

--- test/correctness/custom_allocator.cpp
+++ test/correctness/custom_allocator.cpp
@@ -21,15 +21,11 @@ void my_free(JITUserContext *user_context, void *ptr) {
     free(((void **)ptr)[-1]);
 }
 
-void *mischievous_malloc(JITUserContext *user_context, size_t x) {
-    fprintf(stderr, "This should never get called\n");
-    abort();
-    return nullptr;
-}
-
-void run_test(bool use_callable) {
-    custom_malloc_called = false;
-    custom_free_called = false;
+int main(int argc, char **argv) {
+    if (get_jit_target_from_environment().arch == Target::WebAssembly) {
+        printf("[SKIP] WebAssembly JIT does not support custom allocators.\n");
+        return 0;
+    }
 
     Func f, g;
     Var x;
@@ -41,32 +37,10 @@ void run_test(bool use_callable) {
     g.jit_handlers().custom_malloc = my_malloc;
     g.jit_handlers().custom_free = my_free;
 
-    constexpr int size = 100000;
-    if (!use_callable) {
-        Buffer<int> im = g.realize({size});
-    } else {
-        Callable c = g.compile_to_callable({});
-
-        // Changing g's handlers shouldn't affect any existing Callables
-        g.jit_handlers().custom_malloc = mischievous_malloc;
-
-        Buffer<int> im(size);
-        int r = c(im);
-        _halide_user_assert(r == 0);
-    }
+    Buffer<int> im = g.realize({100000});
 
     assert(custom_malloc_called);
     assert(custom_free_called);
-}
-
-int main(int argc, char **argv) {
-    if (get_jit_target_from_environment().arch == Target::WebAssembly) {
-        printf("[SKIP] WebAssembly JIT does not support custom allocators.\n");
-        return 0;
-    }
-
-    run_test(false);
-    run_test(true);
 
     printf("Success!\n");
     return 0;
