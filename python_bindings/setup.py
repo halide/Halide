77c8f3813a1dc8577c6c0db781b3b6be73e96518
Lukas Trümper <lukas.truemper@outlook.de>
2022-06-01 16:49:42 +0200

    Scikit build for python bindings

---
+++ python_bindings/setup.py
@@ -0,0 +1,28 @@
+from skbuild import setup
+from setuptools import find_packages
+
+with open('version.py', 'r') as f:
+    version = f.read().strip().split(' ')[-1][1:-1]
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("readme.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name="halide",
+    version=version,
+    author="The Halide team",
+    author_email="",
+    description="",
+    long_description=long_description,
+    python_requires=">=3.6",
+    install_requires=requirements,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    cmake_install_dir="src/halide",
+    include_package_data=True,
+    extras_require={"test": ["pytest>=6.0"]},
+    zip_safe=False
+)
