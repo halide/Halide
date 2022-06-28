e2f3c81af8a03f018b5695984049ffe989e34432
Alex Reinking <reinking@google.com>
2022-06-28 17:22:15 +0000

    Correct dependencies

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -4,9 +4,6 @@ from setuptools import find_packages
 with open('version.py', 'r') as f:
     version = f.read().strip().split(' ')[-1][1:-1]
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 with open("readme.md", "r") as fp:
     long_description = fp.read()
 
@@ -18,7 +15,6 @@ setup(
     description="",
     long_description=long_description,
     python_requires=">=3.6",
-    install_requires=requirements,
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     cmake_install_dir="src/halide",
