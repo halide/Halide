3e142cfe0bd1a514a029e61742d8a97e3c54bb1f
Steven Johnson <srj@google.com>
2022-06-28 13:55:16 -0700

    Tweak python apps for better Blaze/Bazel compatibility (#6823)
    * Tweak python apps/tutorials for better Blaze/Bazel compatibility
    
    - Don't write to current directory (rely on an env var to say where to write)
    - Don't read from arbitrary absolute paths (again, rely on an env var)
    - Drive-by removal of unnecessary #include in Codegen_LLVM.cpp inside a lambda (!)
    
    * Recommended fixes
    
    * Revert all changes to tutorial
    
    * Revise apps
    
    * Remove apps_helpers.py

--- src/CodeGen_LLVM.cpp
+++ src/CodeGen_LLVM.cpp
@@ -288,7 +288,6 @@ void CodeGen_LLVM::initialize_llvm() {
 #define LLVM_ASM_PRINTER(target) \
     Initialize##target##AsmPrinter();
 #include <llvm/Config/AsmPrinters.def>
-#include <utility>
 #undef LLVM_ASM_PRINTER
     });
 }
