6f05edfe99ee19333d5e34cde4a40c24b8b3a8ce
Steven Johnson <srj@google.com>
2022-06-28 11:09:23 -0700

    Remove apps_helpers.py

--- python_bindings/apps/erode.py
+++ python_bindings/apps/erode.py
@@ -7,7 +7,18 @@ import halide as hl
 import numpy as np
 import imageio
 import os.path
-from apps_helpers import apps_output_dir, apps_images_dir
+
+# Return the directory to look in for test images:
+# - If TEST_IMAGES_DIR is defined, use that
+# - Otherwise, create a relative path to the C++ apps/images dir
+def apps_images_dir():
+    return os.environ.get("TEST_IMAGES_DIR", os.path.join(os.path.dirname(__file__), "../../apps/images"))
+
+# Return the directory to use when writing output files:
+# - If TEST_TMPDIR is defined, use that
+# - Otherwise, return an empty string (i.e., relative to whatever the current directory is)
+def apps_output_dir():
+    return os.environ.get("TEST_TMPDIR", "")
 
 def get_erode(input):
     """
