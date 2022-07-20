b10859914dd97cc48d9882306e8daeb80b09935e
Alex Reinking <reinking@google.com>
2022-07-20 15:28:54 +0000

    Avoid copying Python sources

--- python_bindings/src/halide/__init__.py
+++ python_bindings/src/halide/__init__.py
@@ -1,2 +1,7 @@
-from .halide_ import *
-from .halide_ import _, _1, _2, _3, _4, _5, _6, _7, _8, _9
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.resolve(strict=True)))
+
+from halide_ import *
+from halide_ import _, _1, _2, _3, _4, _5, _6, _7, _8, _9
