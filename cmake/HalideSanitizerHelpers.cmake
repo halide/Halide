ebbefc0279540b5cfda948f82c102e98c19479e5
Steven Johnson <srj@google.com>
2022-07-22 14:54:10 -0700

    sdf

--- cmake/HalideSanitizerHelpers.cmake
+++ cmake/HalideSanitizerHelpers.cmake
@@ -4,7 +4,7 @@
 
 option(Halide_ENABLE_ASAN "Build everything with Address Sanitizer enabled." OFF)
 if (Halide_ENABLE_ASAN)
-    message(STATUS "Enabling Address Sanitizer.")
+    message(STATUS "Enabling Address Sanitizer for project ${PROJECT_NAME}.")
     set(SANITIZER_COMPILE_OPTIONS -fsanitize=address)
     set(SANITIZER_LINK_OPTIONS -fsanitize=address $<$<CXX_COMPILER_ID:GNU>:-static-libasan>)
     # If running under ASAN, we need to suppress some errors:
