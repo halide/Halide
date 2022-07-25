0a0fc8fa2246e1f0c89ffaed7124aa3a4016847d
Steven Johnson <srj@google.com>
2022-07-25 09:55:04 -0700

    Disable all Sanitizers when running clang-tidy

--- cmake/HalideSanitizerHelpers.cmake
+++ cmake/HalideSanitizerHelpers.cmake
@@ -2,23 +2,27 @@
 # Utilities for setting up Sanitizer settings
 ##
 
-message(STATUS "TODO: DEFAULTING TO ON FOR NOW FOR DRAFT PR, PLEASE IGNORE")
-option(Halide_ENABLE_ASAN "Build everything with Address Sanitizer enabled." ON)
-if (Halide_ENABLE_ASAN)
-    message(STATUS "Enabling Address Sanitizer for project ${PROJECT_NAME}.")
-    set(SANITIZER_COMPILE_OPTIONS -fsanitize=address)
-    set(SANITIZER_LINK_OPTIONS -fsanitize=address $<$<CXX_COMPILER_ID:GNU>:-static-libasan>)
-    # If running under ASAN, we need to suppress some errors:
-    # - detect_leaks, because circular Expr chains in Halide can indeed leak,
-    #   but we don't care here
-    # - detect_container_overflow, because this is a known false-positive
-    #   if compiling with a non-ASAN build of LLVM (which is usually the case)
-    set(SANITIZER_ENV_VARS "ASAN_OPTIONS=detect_leaks=0:detect_container_overflow=0")
-    # -fPIC required for some ASAN build configurations (generally .so), so just enable it unversally here
-    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+if (Halide_CLANG_TIDY_BUILD)
+    message(STATUS "Ignoring all sanitizers because Clang-Tidy is in use.")
 else()
-    set(SANITIZER_COMPILE_OPTIONS )
-    set(SANITIZER_LINK_OPTIONS )
-    set(SANITIZER_ENV_VARS )
+    message(STATUS "TODO: DEFAULTING TO ON FOR NOW FOR DRAFT PR, PLEASE IGNORE")
+    option(Halide_ENABLE_ASAN "Build everything with Address Sanitizer enabled." ON)
+    if (Halide_ENABLE_ASAN)
+        message(STATUS "Enabling Address Sanitizer for project ${PROJECT_NAME}.")
+        set(SANITIZER_COMPILE_OPTIONS -fsanitize=address)
+        set(SANITIZER_LINK_OPTIONS -fsanitize=address $<$<CXX_COMPILER_ID:GNU>:-static-libasan>)
+        # If running under ASAN, we need to suppress some errors:
+        # - detect_leaks, because circular Expr chains in Halide can indeed leak,
+        #   but we don't care here
+        # - detect_container_overflow, because this is a known false-positive
+        #   if compiling with a non-ASAN build of LLVM (which is usually the case)
+        set(SANITIZER_ENV_VARS "ASAN_OPTIONS=detect_leaks=0:detect_container_overflow=0")
+        # -fPIC required for some ASAN build configurations (generally .so), so just enable it unversally here
+        set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+    else()
+        set(SANITIZER_COMPILE_OPTIONS )
+        set(SANITIZER_LINK_OPTIONS )
+        set(SANITIZER_ENV_VARS )
+    endif()
 endif()
 
