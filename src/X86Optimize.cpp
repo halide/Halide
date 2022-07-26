614b7ea76dd42166790459a18c3075b181007c25
Alexander Root <aroot222@mit.edu>
2022-07-25 23:47:23 -0400

    missing paren

--- src/X86Optimize.cpp
+++ src/X86Optimize.cpp
@@ -359,7 +359,7 @@ protected:
             // int(8 | 16 | 32) -> uint is supported via SSE41
             // float32 is always supported (via SSE2).
             (((target.has_feature(Target::SSE41) && bits <= 32) ||
-              (op->type.is_float() && bits == 32) &&
+              (op->type.is_float() && bits == 32)) &&
              rewrite(
                  abs(x),
                  v_intrin("abs", x))) ||
