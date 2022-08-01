e4fa11fbb2ced38b2700a55ce3d2853cba2982d5
Steven Johnson <srj@google.com>
2022-08-01 15:32:11 -0700

    Fix broken Makefile rules for autoschedulers on OSX
    A few issues here:
    - Make was building the plugins as .dylib on OSX, but they should have been .so to match Linux (and just on general principles)
    - On OSX, explicitly linking libHalide.dylib into a plugin means that it will load its own copy of libHalide, which is bad, because it means the plugin doesn't share the same set of globals. We need to omit that explicit dependency and allow it to just find the exported symbols at load time.
    - Add a test to verify the fix; run it everywhere even though it should only have been failing for Make-build OSX builds.
    
    Finally, let me add that we really need to set a sunset date for supporting Make in Halide. The Makefiles aren't really maintained properly anymore, and when something subtle goes wrong, it takes an unreasonable amount of time to debug for something that is no longer our canonical build tool.

---
+++ test/correctness/load_plugin.cpp
@@ -0,0 +1,42 @@
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s path-to-adams2019 path-to-li2018 path-to-mullapudi2016\n", argv[0]);
+        exit(-1);
+    }
+
+    // Use a fixed target for the analysis to get consistent results from this test.
+    Target target("x86-64-linux-sse41-avx-avx2");
+
+    const char *autoscheduler_names[3] = {
+        "Adams2019",
+        "Li2018",
+        "Mullapudi2016",
+    };
+
+    // The entire point of this test is to ensure that plugins are built in a way that
+    // will never attempt to load their own copy of libHalide.
+    //
+    // Failure to do so can end up with the plugin's libHalide having a separate set of global vars,
+    // meaning that the global list of available autoschedulers is never set in the 'host' libHalide,
+    // so calls to apply_autoscheduler() would fail in that case.
+    for (int i = 0; i < 3; i++) {
+        Func f("f");
+        Var x("x"), y("y");
+        f(x, y) = x + y;
+        f.set_estimates({{0, 256}, {0, 256}});
+        Pipeline p(f);
+
+        printf("Loading: %s\n", argv[i + 1]);
+        load_plugin(argv[i + 1]);
+
+        p.apply_autoscheduler(target, {autoscheduler_names[i], {}});
+    }
+
+    printf("Success!\n");
+    return 0;
+}
