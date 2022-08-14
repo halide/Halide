52b91a4e7970dab73ce645207c5249e84c8b67a0
Andrew Adams <andrew.b.adams@gmail.com>
2022-08-14 10:24:30 -0700

    Add minimal useful implementation of extracting and concatenating bits (#6928)
    * Minimal approach to making Deinterleave correct for Reinterpret
    
    * Add minimal useful implementation of extracting and concatenating bits
    
    * clang-tidy
    
    * More clang-tidy fixes
    
    * Add missing error message
    
    * Add low-bit-depth noise test
    
    * Add test to cmake build
    
    * Fix power-of-two check
    
    * Remove dead object
    
    * Add little-endian comment to reinterpret IR node
    
    * Simplify concat_bits of single arg
    
    * Add missing second arg
    
    * Fix concat_bits call
    
    Co-authored-by: Andrew Adams <anadams@adobe.com>

--- src/IR.h
+++ src/IR.h
@@ -34,7 +34,8 @@ struct Cast : public ExprNode<Cast> {
     static const IRNodeType _node_type = IRNodeType::Cast;
 };
 
-/** Reinterpret a node as another type, without affecting any of the bits. */
+/** Reinterpret value as another type, without affecting any of the bits
+ * (on little-endian systems). */
 struct Reinterpret : public ExprNode<Reinterpret> {
     Expr value;
 
@@ -510,15 +511,26 @@ struct Call : public ExprNode<Call> {
         bitwise_or,
         bitwise_xor,
         bool_to_mask,
-        bundle,  // Bundle multiple exprs together temporarily for analysis (e.g. CSE)
+
+        // Bundle multiple exprs together temporarily for analysis (e.g. CSE)
+        bundle,
         call_cached_indirect_function,
         cast_mask,
+
+        // Concatenate bits of the args, with least significant bits as the
+        // first arg (i.e. little-endian)
+        concat_bits,
         count_leading_zeros,
         count_trailing_zeros,
         debug_to_file,
         declare_box_touched,
         div_round_to_zero,
         dynamic_shuffle,
+
+        // Extract some contiguous slice of bits from the argument starting at
+        // the nth bit, counting from the least significant bit, with the number
+        // of bits determined by the return type.
+        extract_bits,
         extract_mask_element,
         get_user_context,
         gpu_thread_barrier,
@@ -563,7 +575,9 @@ struct Call : public ExprNode<Call> {
         shift_right,
         signed_integer_overflow,
         size_of_halide_buffer_t,
-        sorted_avg,  // Compute (arg[0] + arg[1]) / 2, assuming arg[0] < arg[1].
+
+        // Compute (arg[0] + arg[1]) / 2, assuming arg[0] < arg[1].
+        sorted_avg,
         strict_float,
         stringify,
         undef,
