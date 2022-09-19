5744e6c38feab6123dc8e06c65d8a69c14368cf4
Steven Johnson <srj@google.com>
2022-09-19 15:18:49 -0700

    Update Error.cpp

--- src/Error.cpp
+++ src/Error.cpp
@@ -3,7 +3,6 @@
 #include "Util.h"  // for get_env_variable
 
 #include <csignal>
-#include <iostream>
 
 #ifdef _MSC_VER
 #include <io.h>
@@ -173,8 +172,6 @@ ErrorReport::~ErrorReport() noexcept(false) {
     }
 
 #ifdef HALIDE_WITH_EXCEPTIONS
-std::cerr<<"ERR:"<<msg.str()<<"\n"<<std::flush;
-abort();
     if (std::uncaught_exceptions() > 0) {
         // This should never happen - evaluating one of the arguments
         // to the error message would have to throw an
