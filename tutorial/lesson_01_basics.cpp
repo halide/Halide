202dba4a7bd4d23afa1ad3705e23543d1c11d04e
Darya Verzhbinsky <dverzhbinsky@adobe.com>
2022-09-22 09:42:33 -0700

    reverting some changes I made to get ready for PR

--- tutorial/lesson_01_basics.cpp
+++ tutorial/lesson_01_basics.cpp
@@ -22,49 +22,6 @@
 // We'll also include stdio for printf.
 #include <stdio.h>
 
-using namespace Halide;
-
-// example for testing StmtCost.cpp
-Func example_fixed(Halide::Buffer<uint8_t> input) {
-
-    // Next we define our Func object that represents our one pipeline
-    // stage.
-    Halide::Func brighter("brighter");
-
-    // Our Func will have three arguments, representing the position
-    // in the image and the color channel. Halide treats color
-    // channels as an extra dimension of the image.
-    Halide::Var x("x"), y("y"), c("c");
-
-    // Normally we'd probably write the whole function definition on
-    // one line. Here we'll break it apart so we can explain what
-    // we're doing at every step.
-
-    // For each pixel of the input image.
-    Halide::Expr value = input(x, y, c);
-
-    // Cast it to a floating point value.
-    value = Halide::cast<float>(value);
-
-    // Multiply it by 1.5 to brighten it. Halide represents real
-    // numbers as floats, not doubles, so we stick an 'f' on the end
-    // of our constant.
-    value = value * 1.5f;
-
-    // Clamp it to be less than 255, so we don't get overflow when we
-    // cast it back to an 8-bit unsigned int.
-    value = Halide::min(value, 255.0f);
-
-    // Cast it back to an 8-bit unsigned integer.
-    value = Halide::cast<uint8_t>(value);
-
-    // Define the function.
-    brighter(x, y, c) = value;
-
-    return  brighter;
-
-}
-
 int main(int argc, char **argv) {
 
     // This program defines a single-stage imaging pipeline that
