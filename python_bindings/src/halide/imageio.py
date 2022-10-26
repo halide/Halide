523c18b6e80eb1063fbe668fbdd6e7c0e813aeff
Steven Johnson <srj@google.com>
2022-10-26 09:56:39 -0700

    public halide.imageio utilities should copy() buffers

--- python_bindings/src/halide/imageio.py
+++ python_bindings/src/halide/imageio.py
@@ -13,35 +13,55 @@ def is_interleaved(im):
     return im.ndim == 3 and im.strides[2] == 1 and im.shape[2] in [1, 2, 3, 4]
 
 
-def to_planar(im):
-    """If the given ndarray is 3-dimensional and appears to be interleaved
-       layout, return a copy that is in planar form. Otherwise, return the
-       ndarray unchanged."""
-    if is_interleaved(im):
-        im = numpy.moveaxis(im, 2, 0).copy()
-
-    return im
-
-
-def to_interleaved(im):
+def _as_interleaved(im):
     """If the given ndarray is 3-dimensional and appears to be planar
-       layout, return a copy that is in interleaved form. Otherwise, return the
-       ndarray unchanged."""
+       layout, return a copy that is in interleaved form, leaving the input unchanged.
+       Otherwise, return the image ndarray unchanged.
+       Note that this call must be used with care, as the returnee may or may not be a copy."""
     if im.ndim == 3 and not is_interleaved(im):
-        im = numpy.moveaxis(im, 0, 2).copy()
+        im = numpy.moveaxis(im, 0, 2)
+
+
+def _as_planar(im):
+    """If the given ndarray is 3-dimensional and appears to be interleaved
+       layout, return a copy that is in planar form, leaving the input unchanged.
+       Otherwise, return the image ndarray unchanged.
+       Note that this call must be used with care, as the returnee may or may not be a copy."""
+    if is_interleaved(im):
+        im = numpy.moveaxis(im, 2, 0)
 
     return im
+
+
+def copy_to_interleaved(im):
+    """If the given ndarray is 3-dimensional and appears to be planar
+       layout, return a copy that is in interleaved form.
+       Otherwise, return the image an unchanged copy of the ndarray.
+       Note that this call will always return a copy, leaving the input unchanged."""
+    return _as_interleaved(im).copy()
+
+
+def copy_to_planar(im):
+    """If the given ndarray is 3-dimensional and appears to be interleaved
+       layout, return a copy that is in planar form.
+       Otherwise, return the image an unchanged copy of the ndarray.
+       Note that this call will always return a copy, leaving the input unchanged."""
+    return _as_planar(im).copy()
 
 
 def imread(uri, format=None, **kwargs):
     """halide.imageio.imread is a thin wrapper around imagio.imread,
        except that for 3-dimensional images that appear to be interleaved,
        the result is converted to a planar layout before returning."""
-    return to_planar(imageio.imread(uri, format, **kwargs))
+    return copy_to_planar(imageio.imread(uri, format, **kwargs))
 
 
 def imwrite(uri, im, format=None, **kwargs):
     """halide.imageio.imwrite is a thin wrapper around imagio.imwrite,
        except that for 3-dimensional images that appear to be planar,
        the image has a temporary interleaved copy made, which is used for saving."""
-    imageio.imwrite(uri, to_interleaved(im), format, **kwargs)
+
+    # We can use _as_interleaved() here to save a possible copy; since the caller
+    # will never see the possibly-a-copy value, there should be no risk of possibly-different
+    # behavior between cases that need converting and cases that don't.
+    imageio.imwrite(uri, _as_interleaved(im), format, **kwargs)
