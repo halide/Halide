f82d12fb32191dfeb52b0b795fbb80234161b532
Steven Johnson <srj@google.com>
2022-10-27 16:49:58 -0700

    apps

---
+++ python_bindings/test/apps/local_laplacian_app.py
@@ -0,0 +1,61 @@
+"""
+Shell for running Fast image interpolation using a pyramid.
+"""
+
+from local_laplacian import local_laplacian
+from local_laplacian_Mullapudi2016 import local_laplacian_Mullapudi2016
+import imageio
+import numpy as np
+import sys
+from timeit import Timer
+
+
+def main():
+    if len(sys.argv) < 7:
+        print(
+            "Usage: %s input.png input.png levels alpha beta output.png timing_iterations"
+            % sys.argv[0]
+        )
+        print("e.g. %s input.png 8 1 1 output.png 10" % sys.argv[0])
+        sys.exit(1)
+
+    input_path = sys.argv[1]
+    levels = int(sys.argv[2])
+    alpha = float(sys.argv[3])
+    beta = float(sys.argv[4])
+    output_path = sys.argv[5]
+    timing_iterations = int(sys.argv[6])
+
+    print("Reading from %s ..." % input_path)
+    input_buf_u8 = imageio.imread(input_path)
+    assert input_buf_u8.dtype == np.uint8
+    # Convert to uint16 in range [0..1]
+    # TODO: clean this up after #7125 lands
+    input_buf = np.copy(input_buf_u8, order="F").astype(np.uint16) * 257
+    # TODO: clean this up after #7125 lands
+    h = input_buf.shape[0]
+    w = input_buf.shape[1]
+    output_buf = np.empty([h, w, 3], dtype=input_buf.dtype, order="F")
+
+    tests = {
+        "Manual": local_laplacian,
+        "Mullapudi2016": local_laplacian_Mullapudi2016,
+    }
+
+    for name, fn in tests.items():
+        print("Running %s... " % name, end="")
+        t = Timer(lambda: fn(input_buf, levels, alpha / (levels - 1), beta, output_buf))
+        avg_time_sec = t.timeit(number=timing_iterations) / timing_iterations
+        print("time: %fms" % (avg_time_sec * 1e3))
+
+    output_buf_u8 = (output_buf // 257).astype(np.uint8)
+
+    print("Saving to %s ..." % output_path)
+    imageio.imsave(output_path, output_buf_u8)
+
+    print("Success!")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
