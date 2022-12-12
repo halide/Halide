0d977e7db5e4ffb35e6171d19c2d26350aa4c258
Andrew Adams <andrew.b.adams@gmail.com>
2022-12-12 09:20:43 -0800

    Better comments

--- src/StageStridedLoads.h
+++ src/StageStridedLoads.h
@@ -2,7 +2,9 @@
 #define HALIDE_INTERNAL_STAGE_STRIDED_LOADS_H
 
 /** \file
- * TODO
+ *
+ * Defines the compiler pass that converts strided loads into dense loads
+ * followed by shuffles.
  */
 
 #include "Expr.h"
@@ -10,12 +12,30 @@
 namespace Halide {
 namespace Internal {
 
-/** For each strided load in a statement, look for evidence elsewhere that it's
- * safe to read a few elements later or earlier so that it can done as a dense
- * load followed by a shuffle. Additionally, the dense load is selected to try to
- * get groups of strided loads with small offsets from each other to share the
- * same dense load. E.g. if we have f[ramp(x, 2, 16)] + f[ramp(x+1, 2, 16)]) we
- * want to rewrite them to be a single dense load and a shuffle. */
+/** Convert all unpredicated strided loads in a Stmt into dense loads followed
+ * by shuffles.
+ *
+ * For a stride of two, the trick is to do a dense load of twice the size, and
+ * then extract either the even or odd lanes. This was previously done in
+ * codegen, where it was challenging, because it's not easy to know there if
+ * it's safe to do the double-sized load, as it either loads one element beyond
+ * or before the original load. We used the alignment of the ramp base to try to
+ * tell if it was safe to shift backwards, and we added padding to internal
+ * allocations so that for those at least it was safe to shift
+ * forwards. Unfortunately the alignment of the ramp base is usually unknown if
+ * you don't know anything about the strides of the input, and adding padding to
+ * allocations was a serious wart in our memory allocators.
+ *
+ * This pass instead actively looks for evidence elsewhere in the Stmt (at some
+ * location which definitely executes whenever the load being transformed
+ * executes) that it's safe to read further forwards or backwards in memory. The
+ * evidence is in the form of a load at the same base address with a different
+ * constant offset. It also clusters groups of these loads so that they do the
+ * same dense load and extract the appropriate slice of lanes. If it fails to
+ * find any evidence, for loads from external buffers it just does a shorter
+ * load, and for loads from internal allocations it adds padding to the
+ * allocation explicitly, by setting the padding field on Allocate nodes.
+ */
 Stmt stage_strided_loads(const Stmt &s);
 
 }  // namespace Internal
