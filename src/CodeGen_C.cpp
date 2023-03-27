37ac255f58b53b5eccaaa6d0e48d2a013cc33e42
Andrew Adams <andrew.b.adams@gmail.com>
2023-03-27 12:34:20 -0700

    Promote fixed-point intrinsics out of the Internal namespace (#7446)
    * Promote fixed-point intrinsics out of the Internal namespace
    
    and add deprecated wrappers for them in the Internal namespace so that
    we don't break any existing code
    
    * Pacify clang-tidy
    
    * Remove HALIDE_NO_USER_CODE_INLINE

--- src/CodeGen_C.cpp
+++ src/CodeGen_C.cpp
@@ -2075,7 +2075,7 @@ Expr CodeGen_C::scalarize_vector_reduce(const VectorReduce *op) {
         binop = Or::make;
         break;
     case VectorReduce::SaturatingAdd:
-        binop = saturating_add;
+        binop = Halide::saturating_add;
         break;
     }
 
