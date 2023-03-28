8ebc04ae4460ef6c85c2f7843ce7ff5f7dc7643f
Steven Johnson <srj@google.com>
2023-03-27 18:36:58 -0700

    WIP

--- src/XtensaOptimize.cpp
+++ src/XtensaOptimize.cpp
@@ -1951,7 +1951,7 @@ private:
             binop = Or::make;
             break;
         case VectorReduce::SaturatingAdd:
-            binop = saturating_add;
+            binop = [](Expr a, Expr b) { return saturating_add(a, b); };
             break;
         }
 
