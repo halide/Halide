f7eba4a424182399cfbafe4fd5345cfe2e597c6d
Steven Johnson <srj@google.com>
2023-04-13 11:39:32 -0700

    Backport #7494 to release/15.x (#7499)
    fix python binding method export (#7494)
    
    Co-authored-by: 郑启航 <597323109@qq.com>

--- python_bindings/src/halide/__init__.py
+++ python_bindings/src/halide/__init__.py
@@ -1,5 +1,5 @@
 from .halide_ import *
-from .halide_ import _, _1, _2, _3, _4, _5, _6, _7, _8, _9
+from .halide_ import _, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9
 from ._generator_helpers import (
     _create_python_generator,
     _generatorcontext_enter,
