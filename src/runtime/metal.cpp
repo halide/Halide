3f1886804063a85ecba83fa0762ed51f065717ec
Marcos Slomp <slomp@adobe.com>
2023-04-14 11:01:31 -0700

    args[i] could well be 0!

--- src/runtime/metal.cpp
+++ src/runtime/metal.cpp
@@ -790,7 +790,7 @@ WEAK int halide_metal_run(void *user_context,
     set_compute_pipeline_state(encoder, pipeline_state);
 
     int num_kernel_args = 0;
-    for (int i = 0; args[i] != nullptr; i++) {
+    for (int i = 0; arg_types[i].as_u32() != 0; i++) {
         ++num_kernel_args;
     }
 
