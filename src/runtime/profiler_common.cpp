fc3cde81b8e85f838fe96dc1e6d0a46f305c9afc
Martijn Courteaux <courteauxmartijn@gmail.com>
2023-07-25 19:21:35 +0200

    Clang-format ain't liking pretty code.

--- src/runtime/profiler_common.cpp
+++ src/runtime/profiler_common.cpp
@@ -394,9 +394,9 @@ WEAK void halide_profiler_report_unlocked(void *user_context, halide_profiler_st
 
                 float ft = fs->time / (p->runs * 1000000.0f);
                 if (ft < 10000) sstr << " ";
-                if (ft <  1000) sstr << " ";
-                if (ft <   100) sstr << " ";
-                if (ft <    10) sstr << " ";
+                if (ft < 1000) sstr << " ";
+                if (ft < 100) sstr << " ";
+                if (ft < 10) sstr << " ";
                 sstr << ft;
                 // We don't need 6 sig. figs.
                 sstr.erase(3);
