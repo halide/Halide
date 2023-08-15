e19c712d080856850cc56e9a6103c017ea56159f
Derek Gerstmann <dgerstmann@adobe.com>
2023-08-15 14:23:04 -0700

    Update doc strings to use Doxygen formatted args

--- src/Serialization.h
+++ src/Serialization.h
@@ -5,16 +5,16 @@
 
 namespace Halide {
 
-/**
- * Serialize a Halide pipeline into the given data buffer.
- * params is an optional map which can be used to bind external parameters to objects in the pipeline by name
- */
+/// @brief Serialize a Halide pipeline into the given data buffer.
+/// @param pipeline The Halide pipeline to serialize.
+/// @param data The data buffer to store the serialized Halide pipeline into. Any existing contents will be destroyed.
+/// @param params Optional map of named parameters (used to bind external parameters to objects in the pipeline by name).
 void serialize_pipeline(const Pipeline &pipeline, std::vector<uint8_t> &data, std::map<std::string, Internal::Parameter> &params);
 
-/**
- * Serialize a Halide pipeline into the given filename.
- * params is an optional map which can be used to bind external parameters to objects in the pipeline by name
- */
+/// @brief Serialize a Halide pipeline into the given filename.
+/// @param pipeline The Halide pipeline to serialize.
+/// @param filename The location of the file to write into to store the serialized pipeline.  Any existing contents will be destroyed.
+/// @param params Optional map of named parameters (used to bind external parameters to objects in the pipeline by name).
 void serialize_pipeline(const Pipeline &pipeline, const std::string &filename, std::map<std::string, Internal::Parameter> &params);
 
 }  // namespace Halide
