e129aa6ffd72836e24693aca1a64db6f4fcf63e2
Derek Gerstmann <dgerstmann@adobe.com>
2023-08-24 11:43:59 -0700

    Resolve conflict for writeArchive interface changes.

--- src/LLVM_Output.cpp
+++ src/LLVM_Output.cpp
@@ -597,10 +597,11 @@ void create_static_library(const std::vector<std::string> &src_files_in, const T
         return;
     }
 
-#if LLVM_VERSION < 180
+
+#if LLVM_VERSION >= 180
+    const llvm::SymtabWritingMode write_symtab = llvm::SymtabWritingMode::NormalSymtab;
+#else
     const bool write_symtab = true;
-#else 
-    llvm::SymtabWritingMode write_symtab = llvm::SymtabWritingMode::NormalSymtab;
 #endif
     const auto kind = Internal::get_triple_for_target(target).isOSDarwin() ? llvm::object::Archive::K_BSD : llvm::object::Archive::K_GNU;
     const bool thin = false;
