caf07d6a3ae0e4f1d5649e6a0b421edb5b9b3e5c
Derek Gerstmann <dgerstmann@adobe.com>
2023-09-07 11:27:34 -0600

    Addressing comments from review. Fix punctuation and comment nits. Add timing estimates as comments. Add std::function example. Enable advanced scheduling directives.

--- tutorial/lesson_22_jit_performance.cpp
+++ tutorial/lesson_22_jit_performance.cpp
@@ -4,11 +4,11 @@
 // various Halide methods of doing "Just-In-Time" compilation.
 
 // On linux, you can compile and run it like so:
-// g++ lesson_22*.cpp -g -I <path/to/Halide.h> -L <path/to/libHalide.so> -lHalide -lpthread -ldl -o lesson_22 -std=c++17
+// g++ lesson_22*.cpp -g -I <path/to/Halide.h> -I <path/to/tools/halide_benchmark.h> -L <path/to/libHalide.so> -lHalide -lpthread -ldl -o lesson_22 -std=c++17
 // LD_LIBRARY_PATH=<path/to/libHalide.so> ./lesson_20
 
 // On os x:
-// g++ lesson_22*.cpp -g -I <path/to/Halide.h> -L <path/to/libHalide.so> -lHalide -o lesson_22 -std=c++17
+// g++ lesson_22*.cpp -g -I <path/to/Halide.h> -I <path/to/tools/halide_benchmark.h> -L <path/to/libHalide.so> -lHalide -o lesson_22 -std=c++17
 // DYLD_LIBRARY_PATH=<path/to/libHalide.dylib> ./lesson_22
 
 // If you have the entire Halide source tree, you can also build it by
@@ -23,24 +23,24 @@
 using namespace Halide;
 using namespace Halide::Tools; // for benchmark()
 
-// Lets define a helper function to construct a simple pipeline that we'll use for our performance tests
+// Let's define a helper function to construct a simple pipeline that we'll use for our performance tests.
 Pipeline make_pipeline() {
 
-    // We'll start with a simple transpose operation 
+    // We'll start with a simple transpose operation ...
     Func input("input"), output("output");
     Var x("x"), y("y");
 
-    // Fill the input with a linear combination of the coordinate values
+    // Fill the input with a linear combination of the coordinate values ...
     input(x, y) = cast<uint16_t>(x + y);
     input.compute_root();
 
     // Transpose the rows and cols 
     output(x, y) = input(y, x);
 
-    // Schedule it ... there's a number of possibilities here to do an efficient block-wise transpose
+    // Schedule it ... there's a number of possibilities here to do an efficient block-wise transpose.
     Var xi("xi"), yi("yi");
     
-    // Let's focus on 8x8 subtiles, and then vectorize across X, and unroll across Y
+    // Let's focus on 8x8 subtiles, and then vectorize across X, and unroll across Y.
     output.tile(x, y, xi, yi, 8, 8).vectorize(xi).unroll(yi);
 
     // For more advanced scheduling: 
@@ -49,36 +49,41 @@ Pipeline make_pipeline() {
     // which allows us to interpose new Funcs in between input and output.
     // 
     // Here we can inject a block_transpose function to allow us to do 8 vectorized loads from the input.
-    // Func block_transpose("block_transpose"), block("block");
-    // block_transpose = input.in(output).compute_at(output, x).vectorize(x).unroll(y);
+    Func block_transpose("block_transpose"), block("block");
+    block_transpose = input.in(output).compute_at(output, x).vectorize(x).unroll(y);
     //
-    // And now lets reorder and vectorize in X across the block 
-    // block = block_transpose.in(output).reorder_storage(y, x).compute_at(output, x).vectorize(x).unroll(y);
+    // And now Let's reorder and vectorize in X across the block 
+    block = block_transpose.in(output).reorder_storage(y, x).compute_at(output, x).vectorize(x).unroll(y);
 
     return Pipeline(output);
 }
 
 int main(int argc, char **argv) {
 
+    // Since we'll be using the same sample and iteration counts for our benchmarking,
+    // let's define them here in the outermost scope.
+    constexpr int samples = 100;
+    constexpr int iterations = 1;
     
-    // Lets measure the performance of calling realize on a simple pipeline
+    // Now, let's measure the performance of calling realize on a simple pipeline ...
     {
         Pipeline pipeline = make_pipeline();
 
-        // Create an 1024x1024 output buffer to hold the results 
+        // Create an 1024x1024 output buffer to hold the results.
         Buffer<uint16_t> result(1024, 1024);
 
-        // Lets realize the pipeline (which implicitly JIT compiles, dynamically links and executes the code)
+        // Let's realize the pipeline (which implicitly JIT compiles, dynamically links and executes the code).
         size_t count = 0;
-        double t = benchmark([&]() {
+        double t = benchmark(samples, iterations, [&]() {
             pipeline.realize(result);
             ++count;
         });
 
+        // On a MacBook Pro M1, we should get around ~190000 times/sec
         std::cout << "Execute Pipeline (realize): " << int(count / t) << " times/sec\n";
     }
 
-    // This time let's explicitly invoke the JIT compile, before we realize and execute the pipeline
+    // This time let's explicitly invoke the JIT compile, before we realize and execute the pipeline ...
     {
         Pipeline pipeline = make_pipeline();
         Buffer<uint16_t> result(1024, 1024);
@@ -86,14 +91,16 @@ int main(int argc, char **argv) {
         // Let's explicitly JIT compile before we realize, which will cache the generated code
         // associated with the Pipeline object. Now when we call realize, there's no code generation 
         // overhead when we execute the pipeline.
-        pipeline.compile_jit();
+        const Target target = get_jit_target_from_environment();
+        pipeline.compile_jit(target);
 
         size_t count = 0;
-        double t = benchmark([&]() {
+        double t = benchmark(samples, iterations, [&]() {
             pipeline.realize(result);
             ++count;
         });
 
+        // On a MacBook Pro M1, this should be slightly faster ... around ~190300 times/sec (or ~2% faster)
         std::cout << "Execute Pipeline (compile before realize): " << int(count / t) << " times/sec\n";
     }
 
@@ -107,17 +114,37 @@ int main(int argc, char **argv) {
         Callable callable = pipeline.compile_to_callable(arguments, target);
 
         // The Callable object acts as a convienient way of invoking the compiled code like
-        // a function call, using an argv like syntax for the argument list. It also caches 
+        // a function call, using an argv-like syntax for the argument list. It also caches 
         // the JIT compiled code, so there's no code generation overhead when invoking the
         // callable object and executing the pipeline.
 
         size_t count = 0;
-        double t = benchmark([&]() {
+        double t = benchmark(samples, iterations, [&]() {
             callable(result);
             ++count;
         });
 
+        // This should be about the same as the previous run
         std::cout << "Execute Pipeline (compile to callable): " << int(count / t) << " times/sec\n";
+
+        // Perhaps even more convient, we can create a std::function object from the callable,
+        // which supports cleaner type checking for the parameters, and slightly less overhead
+        // for invoking the function. The list used for the template parameters needs to match
+        // the list for the parameters of the pipeline.  Here, we have a single result buffer,
+        // so we specify Buffer<uint16_t> in our call to .make_std_function<>. If we had other 
+        // scalar parameters, input buffers or output buffers, we'd pass them in the template 
+        // parameter list too.
+
+        auto function = callable.make_std_function<Buffer<uint16_t>>();
+
+        count = 0;
+        t = benchmark(samples, iterations, [&]() {
+            function(result);
+            ++count;
+        });
+
+        // On a MacBook Pro M1, this should be slightly faster than the callable (~1-2% faster)
+        std::cout << "Execute Pipeline (compile to std::function): " << int(count / t) << " times/sec\n";
     }
 
     // Let's see how much time is spent on just compiling ...
@@ -130,20 +157,23 @@ int main(int argc, char **argv) {
         // very little overhead after its been cached.
 
         size_t count = 0;
-        double t = benchmark([&]() {
+        double t = benchmark(samples, iterations, [&]() {
             pipeline.compile_jit();
             ++count;
         });
 
+        // Since only the first call does any work and the rest are essentially free, we should expect 1-2B times/sec
         std::cout << "Compile JIT (using cache): " << int(count / t) << " times/sec\n";
 
         // You can invalidate the cache manually, which will destroy all the compiled state.
         count = 0;
-        t = benchmark([&]() {
+        t = benchmark(samples, iterations, [&]() {
             pipeline.invalidate_cache();
             pipeline.compile_jit();
             ++count;
         });
+
+        // This is an intentionally expensive loop, and we should see only about 2000 times/sec
         std::cout << "Compile JIT (from scratch): " << int(count / t) << " times/sec\n";
 
     }
@@ -160,14 +190,14 @@ int main(int argc, char **argv) {
         // static libs, bitcode, etc.
 
         size_t count = 0;
-        double t = benchmark([&]() {
+        double t = benchmark(samples, iterations, [&]() {
             Module m = pipeline.compile_to_module(args, "transpose");
             ++count;
         });
 
+        // On a MacBook Pro M1, this should be around ~10000 times/sec
         std::cout << "Compile to Module: " << int(count / t) << " times/sec\n";
     }
-
 
     printf("DONE!\n");
     return 0;
