1deb7525e855eb3beee9e227b7f3b8a5257a019b
Volodymyr Kysenko <vksnk@google.com>
2023-10-03 12:25:42 -0700

    Basic hoist_storage test

---
+++ test/correctness/hoist_storage.cpp
@@ -0,0 +1,33 @@
+#include "Halide.h"
+
+using namespace Halide;
+
+int main(int argc, char **argv) {
+    // Basic compute-root async producer
+    {
+        Func f("f"), g("g");
+        Var x("x"), y("y"), xo("xo"), yo("yo"), xi("xi"), yi("yi");
+
+        f(x, y) = x + y;
+        g(x, y) = f(x - 1, y - 1) + f(x + 1, y + 1);
+        g.compute_root()
+            .tile(x, y, xo, yo, xi, yi, 16, 16, TailStrategy::GuardWithIf);
+
+        f.compute_at(g, xo)
+            .hoist_storage(g, Var::outermost());
+
+        Buffer<int> out = g.realize({128, 128});
+
+        out.for_each_element([&](int x, int y) {
+            int correct = 2 * (x + y);
+            if (out(x, y) != correct) {
+                printf("out(%d, %d) = %d instead of %d\n",
+                       x, y, out(x, y), correct);
+                exit(1);
+            }
+        });
+    }
+
+    printf("Success!\n");
+    return 0;
+}
