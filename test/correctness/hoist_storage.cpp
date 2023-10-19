38bc6cee6c1d310e79099bf288ceedd8cd4b5df0
Volodymyr Kysenko <vksnk@google.com>
2023-10-19 11:12:54 -0700

    More tests

--- test/correctness/hoist_storage.cpp
+++ test/correctness/hoist_storage.cpp
@@ -202,6 +202,169 @@ int main(int argc, char **argv) {
             }
         });
     }
+
+    // Two functions are hoisted at the same level.
+    {
+        Func f("f"), h("h"), g("g");
+        Var x("x"), y("y"), xo("xo"), yo("yo"), xi("xi"), yi("yi");
+
+        f(x, y) = x + y;
+        h(x, y) = 2 * x + 3 * y;
+        g(x, y) = f(x - 1, y - 1) + f(x + 1, y + 1) + h(x, y);
+
+        g.compute_root()
+            .tile(x, y, xo, yo, xi, yi, 16, 16, TailStrategy::GuardWithIf);
+
+        f.compute_at(g, xo)
+            .hoist_storage(g, Var::outermost())
+            // Store in heap to make sure that custom malloc is called.
+            .store_in(MemoryType::Heap);
+        h.compute_at(g, xo)
+            .hoist_storage(g, Var::outermost())
+            // Store in heap to make sure that custom malloc is called.
+            .store_in(MemoryType::Heap);
+
+        g.jit_handlers().custom_malloc = custom_malloc;
+        g.jit_handlers().custom_free = custom_free;
+
+        malloc_count = 0;
+        malloc_total_size = 0;
+
+        Buffer<int> out = g.realize({128, 128});
+
+        const int expected_malloc_count = 2;
+        if (malloc_count != expected_malloc_count) {
+            std::cerr << "Wrong number of mallocs. "
+                      << "Expected " << expected_malloc_count << " got " << malloc_count << "\n";
+            exit(1);
+        }
+
+        const int expected_malloc_total_size = 16 * 16 * sizeof(int32_t) + 18 * 18 * sizeof(int32_t);
+        if (malloc_total_size != expected_malloc_total_size) {
+            std::cerr << "Wrong allocation size "
+                      << "Expected " << expected_malloc_total_size << " got " << malloc_total_size << "\n";
+            exit(1);
+        }
+
+        out.for_each_element([&](int x, int y) {
+            int correct = 4 * x + 5 * y;
+            if (out(x, y) != correct) {
+                printf("out(%d, %d) = %d instead of %d\n",
+                       x, y, out(x, y), correct);
+                exit(1);
+            }
+        });
+    }
+
+    // Two functions are hoisted, but at different loop levels.
+    {
+        Func f("f"), h("h"), g("g");
+        Var x("x"), y("y"), xo("xo"), yo("yo"), xi("xi"), yi("yi");
+
+        f(x, y) = x + y;
+        h(x, y) = 2 * x + 3 * y;
+        g(x, y) = f(x - 1, y - 1) + f(x + 1, y + 1) + h(x, y);
+
+        g.compute_root()
+            .tile(x, y, xo, yo, xi, yi, 16, 16, TailStrategy::GuardWithIf);
+
+        f.compute_at(g, xo)
+            .hoist_storage(g, Var::outermost())
+            // Store in heap to make sure that custom malloc is called.
+            .store_in(MemoryType::Heap);
+        h.compute_at(g, xo)
+            .hoist_storage(g, yo)
+            // Store in heap to make sure that custom malloc is called.
+            .store_in(MemoryType::Heap);
+
+        g.jit_handlers().custom_malloc = custom_malloc;
+        g.jit_handlers().custom_free = custom_free;
+
+        malloc_count = 0;
+        malloc_total_size = 0;
+
+        Buffer<int> out = g.realize({128, 128});
+
+        const int expected_malloc_count = 1 + 8;
+        if (malloc_count != expected_malloc_count) {
+            std::cerr << "Wrong number of mallocs. "
+                      << "Expected " << expected_malloc_count << " got " << malloc_count << "\n";
+            exit(1);
+        }
+
+        const int expected_malloc_total_size = 8 * 16 * 16 * sizeof(int32_t) + 18 * 18 * sizeof(int32_t);
+        if (malloc_total_size != expected_malloc_total_size) {
+            std::cerr << "Wrong allocation size "
+                      << "Expected " << expected_malloc_total_size << " got " << malloc_total_size << "\n";
+            exit(1);
+        }
+
+        out.for_each_element([&](int x, int y) {
+            int correct = 4 * x + 5 * y;
+            if (out(x, y) != correct) {
+                printf("out(%d, %d) = %d instead of %d\n",
+                       x, y, out(x, y), correct);
+                exit(1);
+            }
+        });
+    }
+
+    // Two functions are hoisted, but at different loop levels.
+    {
+        Func f("f"), h("h"), g("g");
+        Var x("x"), y("y"), xo("xo"), yo("yo"), xi("xi"), yi("yi");
+
+        f(x, y) = x + y;
+        h(x, y) = 2 * x + 3 * y;
+        g(x, y) = f(x - 1, y - 1) + f(x + 1, y + 1) + h(x, y);
+
+        g.compute_root()
+            .tile(x, y, xo, yo, xi, yi, 16, 16, TailStrategy::GuardWithIf);
+
+        f.compute_at(g, xo)
+            // Store in heap to make sure that custom malloc is called.
+            .store_in(MemoryType::Heap);
+        h.compute_at(g, xo)
+            .hoist_storage(g, yo)
+            // Store in heap to make sure that custom malloc is called.
+            .store_in(MemoryType::Heap);
+
+        g.jit_handlers().custom_malloc = custom_malloc;
+        g.jit_handlers().custom_free = custom_free;
+
+        malloc_count = 0;
+        malloc_total_size = 0;
+
+        Buffer<int> out = g.realize({128, 128});
+
+        const int expected_malloc_count = 64 + 8;
+        if (malloc_count != expected_malloc_count) {
+            std::cerr << "Wrong number of mallocs. "
+                      << "Expected " << expected_malloc_count << " got " << malloc_count << "\n";
+            exit(1);
+        }
+
+        const int expected_malloc_total_size = 8 * 16 * 16 * sizeof(int32_t) + 64 * 18 * 18 * sizeof(int32_t);
+        if (malloc_total_size != expected_malloc_total_size) {
+            std::cerr << "Wrong allocation size "
+                      << "Expected " << expected_malloc_total_size << " got " << malloc_total_size << "\n";
+            exit(1);
+        }
+
+        out.for_each_element([&](int x, int y) {
+            int correct = 4 * x + 5 * y;
+            if (out(x, y) != correct) {
+                printf("out(%d, %d) = %d instead of %d\n",
+                       x, y, out(x, y), correct);
+                exit(1);
+            }
+        });
+    }
+
+    // two functions, different levels of hoist_storage
+    // two functions, only one is hoisted
+    // fold_storage test
+    // parallel test
     // {
     //     Buffer<int> input(64, 64);
     //     Func f("f"), g("g");
