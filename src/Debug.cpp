a5a689be8c6ad351674f3ced3bbf542335f91d75
Volodymyr Kysenko <vksnk@google.com>
2023-10-26 14:07:10 -0700

    Change debug level to get more info about segfault

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -6,8 +6,9 @@ namespace Internal {
 
 int debug::debug_level() {
     static int cached_debug_level = ([]() -> int {
-        std::string lvl = get_env_variable("HL_DEBUG_CODEGEN");
-        return !lvl.empty() ? atoi(lvl.c_str()) : 0;
+        return 1;
+        // std::string lvl = get_env_variable("HL_DEBUG_CODEGEN");
+        // return !lvl.empty() ? atoi(lvl.c_str()) : 1;
     })();
     return cached_debug_level;
 }
