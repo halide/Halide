841b718860089db021d2c5d21ceb17c39fb0dc9e
Volodymyr Kysenko <vksnk@google.com>
2023-10-27 08:55:18 -0700

    Revert "Change debug level to get more info about segfault"
    This reverts commit a5a689be8c6ad351674f3ced3bbf542335f91d75.

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -6,9 +6,8 @@ namespace Internal {
 
 int debug::debug_level() {
     static int cached_debug_level = ([]() -> int {
-        return 1;
-        // std::string lvl = get_env_variable("HL_DEBUG_CODEGEN");
-        // return !lvl.empty() ? atoi(lvl.c_str()) : 1;
+        std::string lvl = get_env_variable("HL_DEBUG_CODEGEN");
+        return !lvl.empty() ? atoi(lvl.c_str()) : 0;
     })();
     return cached_debug_level;
 }
