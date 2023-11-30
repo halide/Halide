31368194a88405741eaf361e7a793cbb1b8f8fd9
Xuanda Yang <th3charlie@gmail.com>
2023-12-01 01:59:30 +0800

    [serialization] Add Halide version and serialization version in serialization format (#7905)
    * halide version
    
    * serialization version
    
    * format
    
    * Fix Makefile
    
    * trigger buildbots
    
    ---------
    
    Co-authored-by: Andrew Adams <andrew.b.adams@gmail.com>
    Co-authored-by: Steven Johnson <srj@google.com>

--- src/Deserialization.cpp
+++ src/Deserialization.cpp
@@ -1403,6 +1403,25 @@ Pipeline Deserializer::deserialize(const std::vector<uint8_t> &data) {
         user_warning << "deserialized pipeline is empty\n";
         return Pipeline();
     }
+
+    std::string deserialized_halide_version = deserialize_string(pipeline_obj->halide_version());
+    std::string halide_version = std::to_string(HALIDE_VERSION_MAJOR) + "." +
+                                 std::to_string(HALIDE_VERSION_MINOR) + "." +
+                                 std::to_string(HALIDE_VERSION_PATCH);
+    if (deserialized_halide_version != halide_version) {
+        user_warning << "deserialized pipeline is built with Halide version " << deserialized_halide_version
+                     << ", but current Halide version is " << halide_version << "\n";
+    }
+
+    std::string deserialized_serialization_version = deserialize_string(pipeline_obj->serialization_version());
+    std::string serialization_version = std::to_string(HALIDE_SERIALIZATION_VERSION_MAJOR) + "." +
+                                        std::to_string(HALIDE_SERIALIZATION_VERSION_MINOR) + "." +
+                                        std::to_string(HALIDE_SERIALIZATION_VERSION_PATCH);
+    if (deserialized_serialization_version != serialization_version) {
+        user_error << "deserialized pipeline is built with Halide serialization version " << deserialized_serialization_version
+                   << ", but current Halide serialization version is " << serialization_version << "\n";
+    }
+
     const std::vector<std::string> func_names_in_order =
         deserialize_vector<flatbuffers::String, std::string>(pipeline_obj->func_names_in_order(),
                                                              &Deserializer::deserialize_string);
--- src/Serialization.cpp
+++ src/Serialization.cpp
@@ -1501,6 +1501,14 @@ void Serializer::serialize(const Pipeline &pipeline, std::vector<uint8_t> &resul
         buffers_serialized.push_back(serialize_buffer(builder, buffer.second));
     }
 
+    std::string halide_version = std::to_string(HALIDE_VERSION_MAJOR) + "." +
+                                 std::to_string(HALIDE_VERSION_MINOR) + "." +
+                                 std::to_string(HALIDE_VERSION_PATCH);
+
+    std::string serialization_version = std::to_string(HALIDE_SERIALIZATION_VERSION_MAJOR) + "." +
+                                        std::to_string(HALIDE_SERIALIZATION_VERSION_MINOR) + "." +
+                                        std::to_string(HALIDE_SERIALIZATION_VERSION_PATCH);
+
     auto pipeline_obj = Serialize::CreatePipeline(builder,
                                                   builder.CreateVector(funcs_serialized),
                                                   builder.CreateVector(output_names_serialized),
@@ -1509,7 +1517,9 @@ void Serializer::serialize(const Pipeline &pipeline, std::vector<uint8_t> &resul
                                                   builder.CreateVector(func_names_in_order_serialized),
                                                   builder.CreateVector(parameters_serialized),
                                                   builder.CreateVector(external_parameters_serialized),
-                                                  builder.CreateVector(buffers_serialized));
+                                                  builder.CreateVector(buffers_serialized),
+                                                  serialize_string(builder, halide_version),
+                                                  serialize_string(builder, serialization_version));
     builder.Finish(pipeline_obj);
 
     uint8_t *buf = builder.GetBufferPointer();
