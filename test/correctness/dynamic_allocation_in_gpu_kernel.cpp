a9ccd94403de9b5e0f8da15420552d3a1033b5da
Steven Johnson <srj@google.com>
2024-02-07 15:27:44 -0800

    Update dynamic_allocation_in_gpu_kernel.cpp

--- test/correctness/dynamic_allocation_in_gpu_kernel.cpp
+++ test/correctness/dynamic_allocation_in_gpu_kernel.cpp
@@ -3,6 +3,12 @@
 using namespace Halide;
 
 int main(int argc, char **argv) {
+    Target t(get_jit_target_from_environment());
+    if (!t.has_gpu_feature()) {
+        printf("[SKIP] No GPU target enabled.\n");
+        return 0;
+    }
+
     Func f1, f2, f3, f4, f5, f6, g;
     Var x, y;
     Param<int> p;
