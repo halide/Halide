59d6462a3340d36f6896669c9f539bd4b840188f
Andrew Adams <andrew.b.adams@gmail.com>
2024-03-17 14:02:30 -0700

    A 'broadcast' shuffle is more complex than it seems
    I was poking at the Shuffle node, and checking its usage, and it seems
    that despite the comment, Shuffles that return true for is_broadcast are
    not the same as a Broadcast node. Instead of repeating the input vector
    some number of times, it repeats a shuffle of the input vector. This
    means IRPrinter was incorrect. None of the other usages were bad.
    
    This PR makes this clearer in the comment, and fixes IRPrinter.

--- src/IR.h
+++ src/IR.h
@@ -877,11 +877,10 @@ struct Shuffle : public ExprNode<Shuffle> {
      * arguments. */
     bool is_interleave() const;
 
-    /** Check if this shuffle can be represented as a broadcast.
-     * For example:
-     * A uint8 shuffle of with 4*n lanes and indices:
-     *     0, 1, 2, 3, 0, 1, 2, 3, ....., 0, 1, 2, 3
-     * can be represented as a uint32 broadcast with n lanes (factor = 4). */
+    /** Check if this shuffle can be represented as a repeating pattern that
+     * repeats the same shuffle of the single input vector some number of times.
+     * For example: 0, 3, 1, 1,  0, 3, 1, 1, .....,  0, 3, 1, 1
+     */
     bool is_broadcast() const;
     int broadcast_factor() const;
 
