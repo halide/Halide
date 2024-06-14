8476583f4ba553b86d15949c4b56f6f3786baeff
Alex Reinking <areinking@adobe.com>
2024-06-14 17:56:29 -0400

    Add detection for armv7s (Apple A6)

--- src/runtime/arm_cpu_features.cpp
+++ src/runtime/arm_cpu_features.cpp
@@ -31,6 +31,14 @@ static void set_platform_features(CpuFeatures &features) {
 
 #elif OSX
 
+typedef int integer_t;
+
+typedef integer_t cpu_type_t;
+typedef integer_t cpu_subtype_t;
+
+#define CPU_TYPE_ARM ((cpu_type_t)12)
+#define CPU_SUBTYPE_ARM_V7S ((cpu_subtype_t)11) /* Swift */
+
 extern int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
 
 static bool sysctl_is_set(const char *name) {
@@ -39,15 +47,28 @@ static bool sysctl_is_set(const char *name) {
     return sysctlbyname(name, &enabled, &enabled_len, nullptr, 0) == 0 && enabled;
 }
 
+static bool is_armv7s() {
+    cpu_type_t type;
+    size_t type_len = sizeof(type);
+    if (sysctlbyname("hw.cputype", &type, &type_len, nullptr, 0)) {
+        return false;
+    }
+
+    cpu_subtype_t subtype;
+    size_t subtype_len = sizeof(subtype);
+    if (sysctlbyname("hw.cpusubtype", &subtype, &subtype_len, nullptr, 0)) {
+        return false;
+    }
+
+    return type == CPU_TYPE_ARM && subtype == CPU_SUBTYPE_ARM_V7S;
+}
+
 static void set_platform_features(CpuFeatures &features) {
-    // TODO: add runtime detection for ARMv7s. AFAICT Apple doesn't
-    // provide an Officially Approved Way to detect this at runtime.
-    // Could probably use some variant of sysctl() to detect, but would
-    // need some experimentation and testing to get right.
-    // features.set_known(halide_target_feature_armv7s);
-    // if () {
-    //    features.set_available(halide_target_feature_armv7s);
-    // }
+    // runtime detection for ARMv7s
+    features.set_known(halide_target_feature_armv7s);
+    if (is_armv7s()) {
+        features.set_available(halide_target_feature_armv7s);
+    }
 
     // runtime detection for ARMDotProd extension
     if (sysctl_is_set("hw.optional.arm.FEAT_DotProd")) {
