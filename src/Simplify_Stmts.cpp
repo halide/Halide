9dd1bc45282eb9222a161facbfd61cd0defd1cb3
Martijn Courteaux <courteauxmartijn@gmail.com>
2024-06-20 22:03:38 +0200

    Complementary scoped_truth for the loop lower bound.

--- src/Simplify_Stmts.cpp
+++ src/Simplify_Stmts.cpp
@@ -247,6 +247,8 @@ Stmt Simplify::visit(const For *op) {
         Expr new_max = mutate(new_min + new_extent, nullptr);
         ScopedFact fact_loop_var_less_than_extent = scoped_truth(loop_var < new_max);
 
+        ScopedFact fact_loop_var_ge_than_min = scoped_truth(new_min <= loop_var);
+
         new_body = mutate(op->body);
     }
 
