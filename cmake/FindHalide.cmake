1a7b91426b26f94eec76d4b19a9a3f04e74b7c47
Alex Reinking <areinking@adobe.com>
2024-08-02 11:45:58 -0700

    Quick CMake fixes enabled by 3.28 (#8365)
    * Use FindCUDAToolkit in apps/cuda_mat_mul
    
    * Replace dummy FindHalide.cmake with pkg redirects
    
    Having the dummy file on disk is confusing and is easy
    to accidentally install when modifying CMake install
    rules. Better to use the CMake 3.24+ feature of the
    package redirects dir to truly disable find_package
    for Halide inside the build.
    
    * Avoid creating a dummy file for Halide_Python
    
    * Fix formatting in CMakeLists.txt
    
    * Add SpirvIR.h to the list of Halide sources
    
    * Use BUILD_LOCAL_INTERFACE in PyStubs
    
    * Consistently use HALIDE_H variable
    
    * Comment overriding POSITION_INDEPENDENT_CODE
    
    * Check Halide_STATIC_DEFINE at configure time.
    
    This avoids sending a generator expression downstream. These
    are functionally identical, but it's just one less thing to
    evaluate.
    
    * Use BUILD_LOCAL_INTERFACE for SPIRV-Headers

--- cmake/FindHalide.cmake
+++
@@ -1,6 +0,0 @@
-# This file should NOT be installed.
-# It is used by python_bindings (and future externalizable projects) to satisfy
-# calls to `find_package(Halide)` when used in-tree.
-
-message(VERBOSE "Spoofing find_package(Halide) since in-tree builds already have Halide available.")
-set(Halide_FOUND 1)
