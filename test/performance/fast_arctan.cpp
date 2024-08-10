56cab266e796128340b082a8b0cd514f0d70a451
Martijn Courteaux <courteauxmartijn@gmail.com>
2024-08-10 14:08:27 +0200

    Default to not using fast atan versions if on CUDA.

--- test/performance/fast_arctan.cpp
+++ test/performance/fast_arctan.cpp
@@ -15,20 +15,36 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    Func atan_f, atan2_f, atan_ref, atan2_ref;
+    Func atan_f{"fast_atan"}, atan2_f{"fast_atan2"}, atan_ref{"atan_ref"}, atan2_ref{"atan2_ref"};
     Var x, y;
     float range = -10.0f;
     Expr t0 = x / 1000.f;
     Expr t1 = y / 1000.f;
-    atan_f(x) = fast_atan(-range * t0 + (1 - t0) * range);
+    atan_f(x) = fast_atan(-range * t0 + (1 - t0) * range, ApproximationPrecision::Poly5);
     atan2_f(x, y) = fast_atan2(-range * t0 + (1 - t0) * range,
-                               -range * t1 + (1 - t1) * range);
+                               -range * t1 + (1 - t1) * range, ApproximationPrecision::Poly5);
     atan_ref(x) = atan(-range * t0 + (1 - t0) * range);
     atan2_ref(x, y) = atan2(-range * t0 + (1 - t0) * range, -range * t1 + (1 - t1) * range);
-    atan_f.vectorize(x, 8);
-    atan2_f.vectorize(x, 8);
-    atan_ref.vectorize(x, 8);
-    atan2_ref.vectorize(x, 8);
+
+    if (target.has_gpu_feature()) {
+        Var xo, xi;
+        Var yo, yi;
+        atan_f.never_partition_all();
+        atan2_f.never_partition_all();
+        atan_ref.never_partition_all();
+        atan2_ref.never_partition_all();
+
+        atan_f.gpu_tile(x, xo, xi, 512, TailStrategy::ShiftInwards);
+        atan_ref.gpu_tile(x, xo, xi, 512, TailStrategy::ShiftInwards);
+
+        atan2_f.gpu_tile(x, y, xo, yo, xi, yi, 32, 16, TailStrategy::ShiftInwards);
+        atan2_ref.gpu_tile(x, y, xo, yo, xi, yi, 32, 16, TailStrategy::ShiftInwards);
+    } else {
+        atan_f.vectorize(x, 8);
+        atan2_f.vectorize(x, 8);
+        atan_ref.vectorize(x, 8);
+        atan2_ref.vectorize(x, 8);
+    }
 
     double t_fast_atan = 1e6 * benchmark([&]() { atan_f.realize({1000}); });
     double t_fast_atan2 = 1e3 * benchmark([&]() { atan2_f.realize({1000, 1000}); });
@@ -40,14 +56,24 @@ int main(int argc, char **argv) {
            "atan2: %f ns per pixel\n"
            "fast_atan2: %f ns per pixel\n",
            t_atan, t_fast_atan, t_atan2, t_fast_atan2);
-
-    if (t_atan < t_fast_atan) {
-        printf("fast_atan is not faster than atan\n");
-        return 1;
-    }
-    if (t_atan2 < t_fast_atan2) {
-        printf("fast_atan2 is not faster than atan\n");
-        return 1;
+    if (target.has_gpu_feature()) {
+        if (t_atan * 1.1 < t_fast_atan) {
+            printf("fast_atan more than 10%% slower than atan on GPU.\n");
+            return 1;
+        }
+        if (t_atan2 * 1.1 < t_fast_atan2) {
+            printf("fast_atan2 more than 10%% slower than atan2 on GPU.\n");
+            return 1;
+        }
+    } else {
+        if (t_atan < t_fast_atan) {
+            printf("fast_atan is not faster than atan\n");
+            return 1;
+        }
+        if (t_atan2 < t_fast_atan2) {
+            printf("fast_atan2 is not faster than atan2\n");
+            return 1;
+        }
     }
 
     printf("Success!\n");
