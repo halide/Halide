d8e3225395ae0b0b8618269ccd7875830039df82
Martijn Courteaux <courteauxmartijn@gmail.com>
2024-08-12 10:04:37 +0200

    Improve fast atan performance tests for GPU.

--- test/performance/fast_arctan.cpp
+++ test/performance/fast_arctan.cpp
@@ -17,14 +17,24 @@ int main(int argc, char **argv) {
 
     Func atan_f{"fast_atan"}, atan2_f{"fast_atan2"}, atan_ref{"atan_ref"}, atan2_ref{"atan2_ref"};
     Var x, y;
+    const int test_w = 512;
+    const int test_h = 256;
+
+    Expr t0 = x / float(test_w);
+    Expr t1 = y / float(test_h);
+    // To make sure we time mostely the computation of the arctan, and not memory bandwidth,
+    // we will compute many arctans per output and sum them. In my testing, GPUs suffer more
+    // from bandwith with this test, so we give it more arctangenses to compute per output.
+    const int test_d = target.has_gpu_feature() ? 1024 : 64;
+    RDom rdom{0, test_d};
+    Expr off = rdom / float(test_d) - 0.5f;
+
     float range = -10.0f;
-    Expr t0 = x / 1000.f;
-    Expr t1 = y / 1000.f;
-    atan_f(x, y) = fast_atan(-range * t0 + (1 - t0) * range);
-    atan2_f(x, y) = fast_atan2(-range * t0 + (1 - t0) * range,
-                               -range * t1 + (1 - t1) * range);
-    atan_ref(x, y) = atan(-range * t0 + (1 - t0) * range);
-    atan2_ref(x, y) = atan2(-range * t0 + (1 - t0) * range, -range * t1 + (1 - t1) * range);
+    atan_f(x, y) = sum(fast_atan(-range * t0 + (1 - t0) * range + off));
+    atan2_f(x, y) = sum(fast_atan2(-range * t0 + (1 - t0) * range + off,
+                                   -range * t1 + (1 - t1) * range));
+    atan_ref(x, y) = sum(atan(-range * t0 + (1 - t0) * range + off));
+    atan2_ref(x, y) = sum(atan2(-range * t0 + (1 - t0) * range + off, -range * t1 + (1 - t1) * range));
 
     if (target.has_gpu_feature()) {
         Var xo, xi;
@@ -34,8 +44,8 @@ int main(int argc, char **argv) {
         atan_ref.never_partition_all();
         atan2_ref.never_partition_all();
 
-        atan_f.gpu_tile(x, xo, xi, 256, TailStrategy::ShiftInwards);
-        atan_ref.gpu_tile(x, xo, xi, 256, TailStrategy::ShiftInwards);
+        atan_f.gpu_tile(x, y, xo, yo, xi, yi, 32, 16, TailStrategy::ShiftInwards);
+        atan_ref.gpu_tile(x, y, xo, yo, xi, yi, 32, 16, TailStrategy::ShiftInwards);
 
         atan2_f.gpu_tile(x, y, xo, yo, xi, yi, 32, 16, TailStrategy::ShiftInwards);
         atan2_ref.gpu_tile(x, y, xo, yo, xi, yi, 32, 16, TailStrategy::ShiftInwards);
@@ -46,10 +56,13 @@ int main(int argc, char **argv) {
         atan2_ref.vectorize(x, 8);
     }
 
-    double t_fast_atan = 1e3 * benchmark([&]() { atan_f.realize({1000, 1000}); });
-    double t_fast_atan2 = 1e3 * benchmark([&]() { atan2_f.realize({1000, 1000}); });
-    double t_atan = 1e3 * benchmark([&]() { atan_ref.realize({1000, 1000}); });
-    double t_atan2 = 1e3 * benchmark([&]() { atan2_ref.realize({1000, 1000}); });
+    double scale = 1e9 / (double(test_w) * (test_h * test_d));
+    // clang-format off
+    double t_fast_atan  = scale * benchmark([&]() {    atan_f.realize({test_w, test_h}); });
+    double t_fast_atan2 = scale * benchmark([&]() {   atan2_f.realize({test_w, test_h}); });
+    double t_atan       = scale * benchmark([&]() {  atan_ref.realize({test_w, test_h}); });
+    double t_atan2      = scale * benchmark([&]() { atan2_ref.realize({test_w, test_h}); });
+    // clang-format on
 
     printf("atan: %f ns per pixel\n"
            "fast_atan: %f ns per pixel\n"
@@ -57,11 +70,11 @@ int main(int argc, char **argv) {
            "fast_atan2: %f ns per pixel\n",
            t_atan, t_fast_atan, t_atan2, t_fast_atan2);
     if (target.has_gpu_feature()) {
-        if (t_atan * 1.1 < t_fast_atan) {
+        if (t_atan * 1.10 < t_fast_atan) {
             printf("fast_atan more than 10%% slower than atan on GPU.\n");
             return 1;
         }
-        if (t_atan2 * 1.1 < t_fast_atan2) {
+        if (t_atan2 * 1.10 < t_fast_atan2) {
             printf("fast_atan2 more than 10%% slower than atan2 on GPU.\n");
             return 1;
         }
