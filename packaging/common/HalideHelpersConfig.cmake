061453015c5f7e7e0a11f3e50f1b07abe24d521e
Alex Reinking <areinking@adobe.com>
2024-09-03 20:43:07 -0700

    Pip packaging at last! (#8405)

--- packaging/common/HalideHelpersConfig.cmake
+++ packaging/common/HalideHelpersConfig.cmake
@@ -1,4 +1,5 @@
 cmake_minimum_required(VERSION 3.28)
+@PACKAGE_INIT@
 
 set(Halide_HOST_TARGET @Halide_HOST_TARGET@)
 
@@ -6,3 +7,5 @@ include(${CMAKE_CURRENT_LIST_DIR}/Halide-Interfaces.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/HalideTargetHelpers.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/HalideGeneratorHelpers.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/TargetExportScript.cmake)
+
+check_required_components(${CMAKE_FIND_PACKAGE_NAME})
--- python_bindings/src/halide/__init__.py
+++ python_bindings/src/halide/__init__.py
@@ -1,3 +1,15 @@
+def patch_dll_dirs():
+    import os
+    if hasattr(os, 'add_dll_directory'):
+        from pathlib import Path
+        bin_dir = Path(__file__).parent / 'bin'
+        if bin_dir.exists():
+            os.add_dll_directory(str(bin_dir))
+
+
+patch_dll_dirs()
+del patch_dll_dirs
+
 from .halide_ import *
 from .halide_ import _, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9
 from ._generator_helpers import (
