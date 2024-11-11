bc2ed82d8584021231595fcfb56d485e13ed0fbf
Martijn Courteaux <courteauxmartijn@gmail.com>
2024-11-11 18:13:29 +0100

    Rework precision specification. Generalize towards using this for other functions.

--- test/correctness/fast_arctan.cpp
+++ test/correctness/fast_arctan.cpp
@@ -20,35 +20,34 @@ int bits_diff(float fa, float fb) {
 int main(int argc, char **argv) {
     Target target = get_jit_target_from_environment();
 
-    struct Prec {
+    struct Test {
         ApproximationPrecision precision;
-        float epsilon;
         const char *objective;
     } precisions_to_test[] = {
         // MAE
-        {ApproximationPrecision::MAE_1e_2, 1e-2f, "MAE"},
-        {ApproximationPrecision::MAE_1e_3, 1e-3f, "MAE"},
-        {ApproximationPrecision::MAE_1e_4, 1e-4f, "MAE"},
-        {ApproximationPrecision::MAE_1e_5, 1e-5f, "MAE"},
-        {ApproximationPrecision::MAE_1e_6, 1e-6f, "MAE"},
+        {{ApproximationPrecision::MAE, 0, 1e-2}, "MAE"},
+        {{ApproximationPrecision::MAE, 0, 1e-3}, "MAE"},
+        {{ApproximationPrecision::MAE, 0, 1e-4}, "MAE"},
+        {{ApproximationPrecision::MAE, 0, 1e-5}, "MAE"},
+        {{ApproximationPrecision::MAE, 0, 1e-6}, "MAE"},
 
         // MULPE
-        {ApproximationPrecision::MULPE_1e_2, 1e-2f, "MULPE"},
-        {ApproximationPrecision::MULPE_1e_3, 1e-3f, "MULPE"},
-        {ApproximationPrecision::MULPE_1e_4, 1e-4f, "MULPE"},
-        {ApproximationPrecision::MULPE_1e_5, 1e-5f, "MULPE"},
-        {ApproximationPrecision::MULPE_1e_6, 1e-6f, "MULPE"},
+        {{ApproximationPrecision::MULPE, 0, 1e-2f}, "MULPE"},
+        {{ApproximationPrecision::MULPE, 0, 1e-3f}, "MULPE"},
+        {{ApproximationPrecision::MULPE, 0, 1e-4f}, "MULPE"},
+        {{ApproximationPrecision::MULPE, 0, 1e-5f}, "MULPE"},
+        {{ApproximationPrecision::MULPE, 0, 1e-6f}, "MULPE"},
     };
 
-    for (Prec precision : precisions_to_test) {
-        printf("\nTesting for precision %.1e (%s optimized)...\n", precision.epsilon, precision.objective);
+    for (Test test : precisions_to_test) {
+        printf("\nTesting for precision %.1e (%s optimized)...\n", test.precision.constraint_max_absolute_error, test.objective);
         Func atan_f, atan2_f;
         Var x, y;
         const int steps = 1000;
         Expr vx = (x - steps / 2) / float(steps / 8);
         Expr vy = (y - steps / 2) / float(steps / 8);
 
-        atan_f(x) = fast_atan(vx, precision.precision);
+        atan_f(x) = fast_atan(vx, test.precision);
         if (target.has_gpu_feature()) {
             Var xo, xi;
             Var yo, yi;
@@ -70,14 +69,14 @@ int main(int argc, char **argv) {
             int mantissa_error = bits_diff(atan_x, atan_x_ref);
             max_error = std::max(max_error, abs_error);
             max_mantissa_error = std::max(max_mantissa_error, mantissa_error);
-            if (abs_error > precision.epsilon) {
+            if (abs_error > test.precision.constraint_max_absolute_error) {
                 fprintf(stderr, "fast_atan(%.6f) = %.20f not equal to %.20f (error=%.5e)\n", x, atan_x, atan_x_ref, atan_x_ref - atan_x);
                 exit(1);
             }
         }
         printf("Passed: max abs error: %.5e  max mantissa bits wrong: %d\n", max_error, max_mantissa_error);
 
-        atan2_f(x, y) = fast_atan2(vx, vy, precision.precision);
+        atan2_f(x, y) = fast_atan2(vx, vy, test.precision);
         if (target.has_gpu_feature()) {
             Var xo, xi;
             Var yo, yi;
@@ -100,7 +99,7 @@ int main(int argc, char **argv) {
                 int mantissa_error = bits_diff(atan2_x_y, atan2_x_y_ref);
                 max_error = std::max(max_error, abs_error);
                 max_mantissa_error = std::max(max_mantissa_error, mantissa_error);
-                if (abs_error > precision.epsilon) {
+                if (abs_error > test.precision.constraint_max_absolute_error) {
                     fprintf(stderr, "fast_atan2(%.6f, %.6f) = %.20f not equal to %.20f (error=%.5e)\n", x, y, atan2_x_y, atan2_x_y_ref, atan2_x_y_ref - atan2_x_y);
                     exit(1);
                 }
