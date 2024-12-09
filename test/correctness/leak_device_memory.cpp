f956f40372bc55859bd4cb35a85cdb6d55e456e9
Derek Gerstmann <dgerstmann@adobe.com>
2024-12-09 12:01:16 -0800

    Re-enable GPU object lifetime management tests and leak device tests for Vulkan.

--- test/correctness/leak_device_memory.cpp
+++ test/correctness/leak_device_memory.cpp
@@ -21,11 +21,6 @@ int main(int argc, char **argv) {
 
     Target target = get_jit_target_from_environment();
 
-    if (target.has_feature(Target::Vulkan)) {
-        printf("[SKIP] Skipping test for Vulkan. Driver issues are preventing proper cleanup for shutdown.\n");
-        return 0;
-    }
-
     // We need debug output to record object creation.
     target.set_feature(Target::Debug);
 
