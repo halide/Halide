f81171d9fb32a5d1fbbf475b8128a42ca4fc7f2c
Martijn Courteaux <courteauxmartijn@gmail.com>
2025-03-12 15:30:21 +0100

    Working on better optimizations. Improving PR and code.

--- src/ApproximationTables.h
+++ src/ApproximationTables.h
@@ -9,13 +9,26 @@ namespace Halide {
 namespace Internal {
 
 struct Approximation {
-    ApproximationPrecision::OptimizationObjective objective;
     struct Metrics {
         double mse;
         double mae;
         double mulpe;
-    } metrics_f32, metrics_f64;
-    std::vector<double> coefficients;
+    } metrics_f16, metrics_f32, metrics_f64;
+
+    std::vector<double> p;       // Polynomial in the numerator
+    std::vector<double> q = {};  // Polynomial in the denominator (empty if not a Padé approximant)
+
+    const Metrics &metrics_for(Type type) const {
+        if (type == Float(16)) {
+            return metrics_f16;
+        } else if (type == Float(32)) {
+            return metrics_f32;
+        } else if (type == Float(64)) {
+            return metrics_f64;
+        }
+        internal_error << "No correct type found.";
+        return metrics_f32;
+    }
 };
 
 const Approximation *best_atan_approximation(Halide::ApproximationPrecision precision, Type type);
@@ -24,7 +37,6 @@ const Approximation *best_cos_approximation(Halide::ApproximationPrecision preci
 const Approximation *best_tan_approximation(Halide::ApproximationPrecision precision, Type type);
 const Approximation *best_log_approximation(Halide::ApproximationPrecision precision, Type type);
 const Approximation *best_exp_approximation(Halide::ApproximationPrecision precision, Type type);
-const Approximation *best_expm1_approximation(Halide::ApproximationPrecision precision, Type type);
 
 }  // namespace Internal
 }  // namespace Halide
