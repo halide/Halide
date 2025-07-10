0ed20b80e850a4a1afc71c01f480b75934c3556b
Alex Reinking <areinking@adobe.com>
2025-07-10 11:20:59 -0400

    Reduce brittle repetition with macros

--- python_bindings/src/halide/halide_/PyFuncRef.cpp
+++ python_bindings/src/halide/halide_/PyFuncRef.cpp
@@ -25,94 +25,45 @@ namespace PythonBindings {
  * our __setitem__ implementation that this is the case.
  */
 
+namespace {
+
+#define X_FuncRef(op, rhs) (StageFromInPlaceUpdate{self.operator op(rhs), self})
+#define X_StageFromInPlaceUpdate(op, rhs) (self.func_ref.operator op(rhs), self)
+
+#define DEF_IOP(T, pyop, op)                           \
+    .def(pyop, [](T &self, const Expr &other) {        \
+        return X##_##T(op, other);                     \
+    }).def(pyop, [](T &self, const py::tuple &other) { \
+          return X##_##T(op, to_halide_tuple(other));  \
+      }).def(pyop, [](T &self, const FuncRef &other) { \
+        return X##_##T(op, other);                     \
+    })
+
+#define DEF_IOPS(cls, T)                    \
+    do {                                    \
+        cls DEF_IOP(T, "__iadd__", +=);     \
+        cls DEF_IOP(T, "__isub__", -=);     \
+        cls DEF_IOP(T, "__imul__", *=);     \
+        cls DEF_IOP(T, "__itruediv__", /=); \
+    } while (0)
+
+}  // namespace
+
 void define_func_ref(py::module &m) {
     auto stage_from_in_place_update_class =
-        py::class_<StageFromInPlaceUpdate>(m, "_StageFromInPlaceUpdate")
-            .def("__iadd__", [](StageFromInPlaceUpdate &self, const Expr &other) {
-                return self.func_ref += other, self;
-            })
-            .def("__iadd__", [](StageFromInPlaceUpdate &self, const py::tuple &other) {
-                return self.func_ref += to_halide_tuple(other), self;
-            })
-            .def("__iadd__", [](StageFromInPlaceUpdate &self, const FuncRef &other) {
-                return self.func_ref += other, self;
-            })
-            .def("__isub__", [](StageFromInPlaceUpdate &self, const Expr &other) {
-                return self.func_ref -= other, self;
-            })
-            .def("__isub__", [](StageFromInPlaceUpdate &self, const py::tuple &other) {
-                return self.func_ref -= to_halide_tuple(other), self;
-            })
-            .def("__isub__", [](StageFromInPlaceUpdate &self, const FuncRef &other) {
-                return self.func_ref -= other, self;
-            })
-            .def("__imul__", [](StageFromInPlaceUpdate &self, const Expr &other) {
-                return self.func_ref *= other, self;
-            })
-            .def("__imul__", [](StageFromInPlaceUpdate &self, const py::tuple &other) {
-                return self.func_ref *= to_halide_tuple(other), self;
-            })
-            .def("__imul__", [](StageFromInPlaceUpdate &self, const FuncRef &other) {
-                return self.func_ref *= other, self;
-            })
-            .def("__itruediv__", [](StageFromInPlaceUpdate &self, const Expr &other) {
-                return self.func_ref /= other, self;
-            })
-            .def("__itruediv__", [](StageFromInPlaceUpdate &self, const py::tuple &other) {
-                return self.func_ref /= to_halide_tuple(other), self;
-            })
-            .def("__itruediv__", [](StageFromInPlaceUpdate &self, const FuncRef &other) {
-                return self.func_ref /= other, self;
-            });
+        py::class_<StageFromInPlaceUpdate>(m, "_StageFromInPlaceUpdate");
+    DEF_IOPS(stage_from_in_place_update_class, StageFromInPlaceUpdate);
 
     auto func_tuple_element_ref_class =
         py::class_<FuncTupleElementRef>(m, "FuncTupleElementRef")
             .def("index", &FuncTupleElementRef::index);
-
     add_binary_operators(func_tuple_element_ref_class);
 
-    auto func_ref_class =
-        py::class_<FuncRef>(m, "FuncRef")
-            .def("__getitem__", &FuncRef::operator[])
-            .def("size", &FuncRef::size)
-            .def("__len__", &FuncRef::size)
-            .def("__iadd__", [](FuncRef &self, const Expr &other) {
-                return StageFromInPlaceUpdate{self += other, self};
-            })
-            .def("__iadd__", [](FuncRef &self, const py::tuple &other) {
-                return StageFromInPlaceUpdate{self += to_halide_tuple(other), self};
-            })
-            .def("__iadd__", [](FuncRef &self, const FuncRef &other) {
-                return StageFromInPlaceUpdate{self += other, self};
-            })
-            .def("__isub__", [](FuncRef &self, const Expr &other) {
-                return StageFromInPlaceUpdate{self -= other, self};
-            })
-            .def("__isub__", [](FuncRef &self, const py::tuple &other) {
-                return StageFromInPlaceUpdate{self -= to_halide_tuple(other), self};
-            })
-            .def("__isub__", [](FuncRef &self, const FuncRef &other) {
-                return StageFromInPlaceUpdate{self -= other, self};
-            })
-            .def("__imul__", [](FuncRef &self, const Expr &other) {
-                return StageFromInPlaceUpdate{self *= other, self};
-            })
-            .def("__imul__", [](FuncRef &self, const py::tuple &other) {
-                return StageFromInPlaceUpdate{self *= to_halide_tuple(other), self};
-            })
-            .def("__imul__", [](FuncRef &self, const FuncRef &other) {
-                return StageFromInPlaceUpdate{self *= other, self};
-            })
-            .def("__itruediv__", [](FuncRef &self, const Expr &other) {
-                return StageFromInPlaceUpdate{self /= other, self};
-            })
-            .def("__itruediv__", [](FuncRef &self, const py::tuple &other) {
-                return StageFromInPlaceUpdate{self /= to_halide_tuple(other), self};
-            })
-            .def("__itruediv__", [](FuncRef &self, const FuncRef &other) {
-                return StageFromInPlaceUpdate{self /= other, self};
-            });
-
+    auto func_ref_class = py::class_<FuncRef>(m, "FuncRef")
+                              .def("__getitem__", &FuncRef::operator[])
+                              .def("size", &FuncRef::size)
+                              .def("__len__", &FuncRef::size);
+    DEF_IOPS(func_ref_class, FuncRef);
     add_binary_operators(func_ref_class);
 }
 
