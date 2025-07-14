71d3bd595980e362fe0fb9e07a8bf166b3a53292
Andrew Adams <andrew.b.adams@gmail.com>
2025-07-14 12:59:40 -0700

    Fix complexity of bounds of nested pure intrinsics
    Fixes #8686
    
    Alternative to #8687

---
+++ test/correctness/bounds_of_pure_intrinsics.cpp
@@ -0,0 +1,31 @@
+#include "Halide.h"
+
+using namespace Halide;
+using namespace Halide::Internal;
+
+int main(int argc, char **argv) {
+
+    // There were scalability problems with taking bounds of nested pure
+    // intrinsics. This test hangs if those problems still exist, using the
+    // strict float intrinsics. https://github.com/halide/Halide/issues/8686
+
+    Param<float> p1, p2, p2_min, p2_max;
+    Scope<Interval> scope;
+    scope.push(p2.name(), Interval{p2_min, p2_max});
+
+    for (int limit = 1; limit < 500; limit++) {
+        Expr e1 = p1, e2 = p2;
+        int ops = 0;
+        for (int i = 0; i < limit; i++) {
+            e1 = e1 * p1 + (i + 1);
+            e2 = e2 * p2 + (i + 1);
+            ops += 4;
+        }
+        Expr e = e1 + e2;
+        bounds_of_expr_in_scope(e, scope);
+        e = strictify_float(e);
+        bounds_of_expr_in_scope(e, scope);
+    }
+
+    return 0;
+}
