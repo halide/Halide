17167044b8f0ea1377f54576c079d5a1b872171e
Alex Reinking <areinking@adobe.com>
2025-08-07 12:36:10 -0400

    Fix the bad_func_object.cpp test.

--- test/error/bad_func_object.cpp
+++ test/error/bad_func_object.cpp
@@ -2,10 +2,21 @@
 using namespace Halide;
 
 int main() {
-    const Internal::Function func{};
-    const Func f{func};  // internal_assert
+#ifdef HALIDE_WITH_EXCEPTIONS
+    try {
+#endif
+        const Internal::Function func{};
+        const Func f{func};  // internal_assert
 
-    std::cout << f.name() << "\n";  // segfaults without above assert
+        std::cout << f.name() << "\n";  // segfaults without above assert
+#ifdef HALIDE_WITH_EXCEPTIONS
+    } catch (const InternalError &e) {
+        // The harness rejects internal errors as they should typically not be
+        // _expected_. However, we are directly testing a constructor invariant
+        // check here, so an internal error is both expected and appropriate.
+        throw std::runtime_error(e.what());
+    }
+#endif
 
     printf("Success!\n");
     return 0;
