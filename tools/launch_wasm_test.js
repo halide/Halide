9b4f0cae4d9b6ca01b65cc280da616939f8d8654
Alex Reinking <areinking@adobe.com>
2025-08-11 15:57:43 -0400

    Use HL_TARGET instead of an argument to launch_wasm_test.js
    It is already available when launched from CTest

--- tools/launch_wasm_test.js
+++ tools/launch_wasm_test.js
@@ -1,9 +1,8 @@
 // argv[0] = path to node
 // argv[1] = path to this script
 // argv[2] = path to the Halide-generated js file we want to test
-// argv[3] = the Halide::Target string for the code being tested (e.g. wasm-32-wasmrt, wasm-32-wasmrt-webgpu)
 const target_js_path = process.argv[2];
-const halide_target_string = process.argv[3];
+const halide_target_string = process.env["HL_TARGET"]; // e.g. "wasm-32-wasmrt", "wasm-32-wasmrt-webgpu"
 
 if (halide_target_string.includes("webgpu")) {
     const provider = require("dawn");
