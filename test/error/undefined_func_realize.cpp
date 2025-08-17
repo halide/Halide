7ff5d871d9ce3c1080a06a8350d667934902b23a
Alex Reinking <areinking@adobe.com>
2025-08-17 19:27:50 -0400

    Write undefined_func_compile.cpp with less boilerplate, but larger diff

--- test/error/undefined_func_realize.cpp
+++ test/error/undefined_func_realize.cpp
@@ -1,19 +1,13 @@
 #include "Halide.h"
-#include "halide_test_dirs.h"
 #include "halide_test_error.h"
 
 using namespace Halide;
 
-namespace {
-void TestUndefinedFuncRealize() {
-    Func f("f");
-
-    Buffer<int32_t> result = f.realize({100, 5, 3});
-
-    // We shouldn't reach here, because there should have been a compile error.
-}
-}  // namespace
-
 TEST(ErrorTests, UndefinedFuncRealize) {
-    EXPECT_COMPILE_ERROR(TestUndefinedFuncRealize, MatchesPattern(R"(Can't realize undefined Func\.)"));
+    EXPECT_COMPILE_ERROR(
+        [] {
+            Func f("f");
+            Buffer<int32_t> result = f.realize({100, 5, 3});
+        },
+        HasSubstr("Can't realize undefined Func."));
 }
