ea437b64e23628d74cbe41af46de53145d234929
Alex Reinking <areinking@adobe.com>
2025-08-21 06:55:16 -0400

    Clean up custom_error_reporter.cpp test

--- test/correctness/custom_error_reporter.cpp
+++ test/correctness/custom_error_reporter.cpp
@@ -1,6 +1,6 @@
 #include "Halide.h"
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace Halide;
 
@@ -18,27 +18,22 @@ int should_never_be_evaluated() {
     return 0;
 }
 
-class MyCustomErrorReporter : public Halide::CompileTimeErrorReporter {
-public:
-    int errors_occurred;
-    int warnings_occurred;
-
-    MyCustomErrorReporter()
-        : errors_occurred(0), warnings_occurred(0) {
-    }
+struct MyCustomErrorReporter final : CompileTimeErrorReporter {
+    int errors_occurred{0};
+    int warnings_occurred{0};
 
     void warning(const char *msg) override {
-        auto msg_safe = Halide::Internal::replace_all(msg, ":", "(semicolon)");
+        const auto msg_safe = Internal::replace_all(msg, ":", "(colon)");
         printf("Custom warn: %s\n", msg_safe.c_str());
         warnings_occurred++;
     }
 
     [[noreturn]] void error(const char *msg) override {
         // Emitting "error.*:" to stdout or stderr will cause CMake to report the
-        // test as a failure on Windows, regardless of error code returned.
+        // test as a failure on Windows, regardless of the error code returned.
         // The error text we get from ErrorReport probably contains some variant
         // of this, so let's make sure it doesn't match that pattern.
-        auto msg_safe = Halide::Internal::replace_all(msg, ":", "(semicolon)");
+        const auto msg_safe = Internal::replace_all(msg, ":", "(colon)");
         printf("Custom err: %s\n", msg_safe.c_str());
         errors_occurred++;
 
@@ -56,7 +51,7 @@ public:
 int main(int argc, char **argv) {
 
     // Use argc here so that the compiler cannot optimize it away:
-    // we know argc > 0 always, but compiler (probably) doesn't.
+    // we know argc > 0 always, but the compiler (probably) doesn't.
     _halide_user_assert(argc > 0) << should_never_be_evaluated();
 
     MyCustomErrorReporter reporter;
