acf39e3bf79eecb0af79445c287285b716f1f7f8
Alex Reinking <areinking@adobe.com>
2025-08-26 14:06:17 -0400

    Add test for Python memoization.

---
+++ python_bindings/test/correctness/memoize.py
@@ -0,0 +1,24 @@
+from halide import EvictionKey, Func, Var
+
+
+def test_memoize():
+    x = Var("x")
+
+    f = Func("f")
+    f[x] = 0.0
+    f[x] = f[x] + 1
+    f.memoize()
+
+    output = Func("output")
+    output[x] = f[x]
+
+    result = output.realize([3])
+    assert list(result) == [1., 1., 1.]
+
+
+def main():
+    test_memoize()
+
+
+if __name__ == "__main__":
+    main()
