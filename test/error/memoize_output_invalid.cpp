025ddc08a8bc32781a34168ef65c48fe5e9246f1
Alex Reinking <areinking@adobe.com>
2025-08-27 12:30:38 -0400

    fixup! Merge branch 'main' into experiment/google-test
    - correctness/bad_partition_always_throws.cpp is redundant
      with error/bad_partition_always.cpp
    - added and converted memoize_output_invalid.cpp

--- test/error/memoize_output_invalid.cpp
+++ test/error/memoize_output_invalid.cpp
@@ -1,7 +1,10 @@
-#include <Halide.h>
+#include "Halide.h"
+#include "halide_test_error.h"
+
 using namespace Halide;
 
-int main(int argc, char **argv) {
+namespace {
+void TestMemoizeOutputInvalid() {
     Var x{"x"};
     Func f{"f"};
     f(x) = 0.0f;
@@ -9,6 +12,13 @@ int main(int argc, char **argv) {
     f.memoize();
 
     f.realize({3});
+}
+}  // namespace
 
-    printf("Success!\n");
+TEST(ErrorTests, MemoizeOutputInvalid) {
+    EXPECT_COMPILE_ERROR(
+        TestMemoizeOutputInvalid,
+        MatchesPattern(R"(Can't compile Pipeline with memoized output Func: )"
+                       R"(f(\$\d+)?. Memoization is valid only on intermediate )"
+                       R"(Funcs because it takes control of buffer allocation.)"));
 }
