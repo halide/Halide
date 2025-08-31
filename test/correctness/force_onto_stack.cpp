3d88d279cb40a70c8e37089ee5b47d1155cf5c8a
Alex Reinking <areinking@adobe.com>
2025-08-31 17:48:11 -0400

    Convert force_onto_stack.cpp

--- test/correctness/force_onto_stack.cpp
+++ test/correctness/force_onto_stack.cpp
@@ -1,65 +1,76 @@
 #include "Halide.h"
+#include <gtest/gtest.h>
+
+#include <cstdlib>
+
 using namespace Halide;
 
-void *my_malloc(JITUserContext *user_context, size_t x) {
-    printf("There was not supposed to be a heap allocation\n");
-    exit(1);
-    return nullptr;
+namespace {
+struct TestJITContext : JITUserContext {
+    bool saw_malloc{false};
+    std::string error_msg;
+};
+
+void *my_malloc(JITUserContext *ctx, size_t x) {
+    static_cast<TestJITContext *>(ctx)->saw_malloc = true;
+    return malloc(x);
 }
 
-void my_free(JITUserContext *user_context, void *ptr) {
+void my_free(JITUserContext *, void *ptr) {
+    free(ptr);
 }
 
-bool errored = false;
-void my_error(JITUserContext *user_context, const char *msg) {
-    errored = true;
-    char expected[] = "Bounds given for f in x (from 0 to 7) do not cover required region (from 0 to 9)";
-    if (strncmp(expected, msg, sizeof(expected) - 1)) {
-        printf("Unexpected error: '%s'\n", msg);
-        exit(1);
-    }
+void my_error(JITUserContext *ctx, const char *msg) {
+    static_cast<TestJITContext *>(ctx)->error_msg = msg;
 }
 
-int main(int argc, char **argv) {
-    if (get_jit_target_from_environment().arch == Target::WebAssembly) {
-        printf("[SKIP] WebAssembly JIT does not support custom allocators.\n");
-        return 0;
-    }
-
-    {
-        Func f("f"), g;
-        Var x("x"), xo, xi;
-
-        Param<int> p;
-
-        f(x) = x;
-        g(x) = f(x);
-        g.split(x, xo, xi, p);
-
-        // We need p elements of f per split of g. This could create a
-        // dynamic allocation. Instead we'll assert that 8 is enough, so
-        // that f can go on the stack and be entirely vectorized.
-        f.compute_at(g, xo).bound_extent(x, 8).vectorize(x);
-
-        // Check there's no malloc when the bound is good
-        g.jit_handlers().custom_malloc = my_malloc;
-        g.jit_handlers().custom_free = my_free;
-        p.set(5);
-        g.realize({20});
-        g.jit_handlers().custom_malloc = nullptr;
-        g.jit_handlers().custom_free = nullptr;
-
-        // Check there was an assertion failure of the appropriate type when the bound is violated
-        g.jit_handlers().custom_error = my_error;
-        p.set(10);
-        g.realize({20});
-
-        if (!errored) {
-            printf("There was supposed to be an error\n");
-            return 1;
+class ForceOntoStackTest : public ::testing::Test {
+protected:
+    TestJITContext ctx;
+    void SetUp() override {
+        if (get_jit_target_from_environment().arch == Target::WebAssembly) {
+            GTEST_SKIP() << "WebAssembly JIT does not support custom allocators.";
         }
+        ctx.handlers.custom_malloc = my_malloc;
+        ctx.handlers.custom_free = my_free;
+        ctx.handlers.custom_error = my_error;
     }
+};
+}  // namespace
 
+TEST_F(ForceOntoStackTest, Basic) {
+    Func f("f"), g;
+    Var x("x"), xo, xi;
+
+    Param<int> p;
+
+    f(x) = x;
+    g(x) = f(x);
+    g.split(x, xo, xi, p);
+
+    // We need p elements of f per split of g. This could create a
+    // dynamic allocation. Instead we'll assert that 8 is enough, so
+    // that f can go on the stack and be entirely vectorized.
+    f.compute_at(g, xo).bound_extent(x, 8).vectorize(x);
+
+    // Check there's no malloc when the bound is good
+    p.set(5);
+    g.realize(&ctx, {20});
+
+    EXPECT_FALSE(ctx.saw_malloc) << "There was supposed to be no malloc";
+
+    // Check there was an assertion failure of the appropriate type when the bound is violated
+    ctx.handlers.custom_malloc = nullptr;
+    ctx.handlers.custom_free = nullptr;
+    p.set(10);
+    g.realize(&ctx, {20});
+
+    EXPECT_STREQ(
+        ctx.error_msg.c_str(),
+        "Bounds given for f in x (from 0 to 7) do not cover required region (from 0 to 9)");
+}
+
+TEST_F(ForceOntoStackTest, TailStrategies) {
     for (TailStrategy tail_strategy : {TailStrategy::GuardWithIf, TailStrategy::Predicate, TailStrategy::PredicateLoads}) {
         // Another way in which a larger static allocation is
         // preferable to a smaller dynamic one is when you compute
@@ -79,11 +90,7 @@ int main(int argc, char **argv) {
         // nasty thing), so we'll add a bound.
         f.bound_extent(x, 8);
 
-        g.jit_handlers().custom_malloc = my_malloc;
-        g.jit_handlers().custom_free = my_free;
-        g.realize({20});
+        g.realize(&ctx, {20});
+        EXPECT_FALSE(ctx.saw_malloc) << "There was supposed to be no malloc";
     }
-
-    printf("Success!\n");
-    return 0;
 }
