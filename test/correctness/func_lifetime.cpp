721b57d9e19b7c7fa113afd11a51b2f6130f9d80
Alex Reinking <areinking@adobe.com>
2025-09-01 14:52:32 -0400

    Convert func_lifetime.cpp and func_lifetime_2.cpp

--- test/correctness/func_lifetime.cpp
+++ test/correctness/func_lifetime.cpp
@@ -1,78 +1,56 @@
 #include "Halide.h"
-#include <iostream>
-#include <stdio.h>
+#include <gtest/gtest.h>
 
 using namespace Halide;
 
-bool validate(const Buffer<int> &im, int add) {
-    // Check the result was what we expected
+namespace {
+const Target target{get_jit_target_from_environment()};
+Var x{"x"}, y{"y"}, xi{"xi"}, yi{"yi"};
+
+void expect_valid(const Buffer<int> &im, const int add) {
     for (int i = 0; i < im.width(); i++) {
         for (int j = 0; j < im.height(); j++) {
             int correct = i * j + add;
-            if (im(i, j) != correct) {
-                printf("im[%d, %d] = %d instead of %d\n", i, j, im(i, j), correct);
-                return false;
-            }
+            EXPECT_EQ(im(i, j), correct) << "i = " << i << ", j = " << j;
         }
     }
-    return true;
 }
 
-int main(int argc, char **argv) {
-
-    Var x("x"), y("y"), xi("xi"), yi("yi");
-    Func f("f");
-
-    printf("Defining function f...\n");
-
-    f(x, y) = x * y + 1;
-
-    Target target = get_jit_target_from_environment();
+void schedule(Func &f) {
     if (target.has_gpu_feature()) {
         f.gpu_tile(x, y, xi, yi, 8, 8);
     } else if (target.has_feature(Target::HVX)) {
         f.hexagon().vectorize(x, 32);
     }
+}
+}  // namespace
+
+TEST(FuncLifetimeTest, Basic) {
+    Func f("f");
+
+    f(x, y) = x * y + 1;
+    schedule(f);
 
     {
-        printf("Realizing function f...\n");
-
-        Buffer<int> imf = f.realize({32, 32}, target);
-        if (!validate(imf, 1)) {
-            return 1;
-        }
+        SCOPED_TRACE("Initial realization of Func f");
+        const Buffer<int> imf = f.realize({32, 32});
+        expect_valid(imf, 1);
     }
 
-    // Create (and destroy) a new function g.
     {
+        SCOPED_TRACE("Create (and destroy) a new function g.");
         Func g("g");
 
-        printf("Defining function g...\n");
-
         g(x, y) = x * y + 2;
+        schedule(g);
 
-        if (target.has_gpu_feature()) {
-            g.gpu_tile(x, y, xi, yi, 8, 8);
-        } else if (target.has_feature(Target::HVX)) {
-            g.hexagon().vectorize(x, 32);
-        }
-
-        printf("Realizing function g...\n");
-
-        Buffer<int> img = g.realize({32, 32}, target);
-        if (!validate(img, 2)) {
-            return 1;
-        }
+        const Buffer<int> img = g.realize({32, 32});
+        expect_valid(img, 2);
     }
 
-    // Try using f again to ensure it is still valid (after g's destruction).
-    printf("Realizing function f again...\n");
-
-    Buffer<int> imf2 = f.realize({32, 32}, target);
-    if (!validate(imf2, 1)) {
-        return 1;
+    {
+        SCOPED_TRACE("Try using f again to ensure it is still valid (after g's destruction)");
+        const Buffer<int> imf2 = f.realize({32, 32});
+        expect_valid(imf2, 1);
     }
-
-    printf("Success!\n");
-    return 0;
 }
--- test/correctness/func_lifetime_2.cpp
+++ test/correctness/func_lifetime_2.cpp
@@ -1,78 +1,54 @@
 #include "Halide.h"
-#include <iostream>
-#include <stdio.h>
+#include <gtest/gtest.h>
 
 using namespace Halide;
 
-bool validate(const Buffer<int> &im, int add) {
-    // Check the result was what we expected
+namespace {
+const Target target{get_jit_target_from_environment()};
+Var x{"x"}, y{"y"}, xi{"xi"}, yi{"yi"};
+
+void validate(const Buffer<int> &im, int add) {
     for (int i = 0; i < im.width(); i++) {
         for (int j = 0; j < im.height(); j++) {
             int correct = i * j + add;
-            if (im(i, j) != correct) {
-                printf("im[%d, %d] = %d instead of %d\n", i, j, im(i, j), correct);
-                return false;
-            }
+            EXPECT_EQ(im(i, j), correct) << "i = " << i << ", j = " << j;
         }
     }
-    return true;
 }
 
-int main(int argc, char **argv) {
+void schedule(Func &f) {
+    if (target.has_gpu_feature()) {
+        f.gpu_tile(x, y, xi, yi, 8, 8);
+    } else if (target.has_feature(Target::HVX)) {
+        f.hexagon().vectorize(x, 32);
+    }
+}
+}  // namespace
 
-    Var x("x"), y("y"), xi("xi"), yi("yi");
-
+TEST(FuncLifetimeTest2, Basic) {
     Func g("g");
 
-    Target target = get_jit_target_from_environment();
-
     {
-        printf("Defining function f...\n");
         Func f("f");
 
         f(x, y) = x * y + 1;
-
-        if (target.has_gpu_feature()) {
-            f.gpu_tile(x, y, xi, yi, 8, 8);
-        } else if (target.has_feature(Target::HVX)) {
-            f.hexagon().vectorize(x, 32);
-        }
+        schedule(f);
 
         {
-            printf("Realizing function f...\n");
-
-            Buffer<int> imf = f.realize({32, 32}, target);
-            if (!validate(imf, 1)) {
-                return 1;
-            }
+            SCOPED_TRACE("Realizing function f");
+            Buffer<int> imf = f.realize({32, 32});
+            validate(imf, 1);
         }
-
-        printf("Defining function g...\n");
 
         g(x, y) = x * y + 2;
+        schedule(g);
 
-        if (target.has_gpu_feature()) {
-            g.gpu_tile(x, y, xi, yi, 8, 8);
-        } else if (target.has_feature(Target::HVX)) {
-            g.hexagon().vectorize(x, 32);
-        }
-
-        printf("Realizing function g...\n");
-
-        Buffer<int> img1 = g.realize({32, 32}, target);
-        if (!validate(img1, 2)) {
-            return 1;
-        }
+        SCOPED_TRACE("Realizing function g (f still live)");
+        Buffer<int> img1 = g.realize({32, 32});
+        validate(img1, 2);
     }
 
-    // Try using g again to ensure it is still valid (after f's destruction).
-    printf("Realizing function g again...\n");
-
-    Buffer<int> img2 = g.realize({32, 32}, target);
-    if (!validate(img2, 2.0f)) {
-        return 1;
-    }
-
-    printf("Success!\n");
-    return 0;
+    SCOPED_TRACE("Realizing function g (after f destroyed)");
+    Buffer<int> img2 = g.realize({32, 32});
+    validate(img2, 2);
 }
