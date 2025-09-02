09a9918eb1b884743587a28fdebe13761c3511c7
Alex Reinking <areinking@adobe.com>
2025-09-01 22:25:42 -0400

    Convert gpu_allocation_cache.cpp (major revision)
    - Remove benchmark as it tripled the runtime and wasn't useful.
    - Refactor redundant scheduling and testing code.
    
    TODO: is the ThreadPool actually testing anything useful?
    TODO: is it valid to turn `reuse_device_allocations` on and off
          in this concurrent setting?

--- test/correctness/gpu_allocation_cache.cpp
+++ test/correctness/gpu_allocation_cache.cpp
@@ -1,9 +1,10 @@
 #include "Halide.h"
-#include "halide_benchmark.h"
 #include "halide_thread_pool.h"
+#include <gtest/gtest.h>
 
 using namespace Halide;
 
+namespace {
 int fib(int N, int a, int b) {
     while (N > 2) {
         a += b;
@@ -13,164 +14,106 @@ int fib(int N, int a, int b) {
     return b;
 }
 
-int main(int argc, char **argv) {
-    Target target = get_jit_target_from_environment();
-    if (!target.has_gpu_feature()) {
-        printf("[SKIP] No GPU target enabled.\n");
-        return 0;
-    }
-    if (target.has_feature(Target::D3D12Compute)) {
-        // https://github.com/halide/Halide/issues/5000
-        printf("[SKIP] Allocation cache not yet implemented for D3D12Compute.\n");
-        return 0;
-    }
-    if (target.has_feature(Target::Vulkan) && ((target.os == Target::IOS) || target.os == Target::OSX)) {
-        printf("[SKIP] Skipping test for Vulkan on iOS/OSX (MoltenVK only allows 30 buffers to be allocated)!\n");
-        return 0;
-    }
-    if (target.has_feature(Target::Vulkan) && (target.os == Target::Windows)) {
-        printf("[SKIP] Skipping test for Vulkan on Windows ... fails unless run on its own!\n");
-        return 0;
-    }
-    if (target.has_feature(Target::WebGPU)) {
-        printf("[SKIP] Allocation cache not yet implemented for WebGPU.\n");
-        return 0;
-    }
-    const int N = 30;
-    Var x, y, xi, yi;
+class GPUAllocationCacheTest : public testing::Test {
+protected:
+    Target target{get_jit_target_from_environment()};
+    static constexpr int N = 30;
+    static constexpr int kIters = 300;
+    Var x{"x"}, y{"y"}, xi{"xi"}, yi{"yi"};
 
-    // Fixed size, overlapping lifetimes, looped 300 times. Should have 3 allocations live and OOM if there's a leak.
+    void SetUp() override {
+        if (!target.has_gpu_feature()) {
+            GTEST_SKIP() << "No GPU target enabled.";
+        }
+        if (target.has_feature(Target::D3D12Compute)) {
+            // TODO: https://github.com/halide/Halide/issues/5000
+            GTEST_SKIP() << "Allocation cache not yet implemented for D3D12Compute.";
+        }
+        if (target.has_feature(Target::Vulkan) && (target.os == Target::IOS || target.os == Target::OSX)) {
+            // TODO: open an issue to track if this restriction is ever lifted
+            GTEST_SKIP() << "Skipping test for Vulkan on iOS/OSX (MoltenVK only allows 30 buffers to be allocated)!";
+        }
+        if (target.has_feature(Target::Vulkan) && target.os == Target::Windows) {
+            GTEST_SKIP() << "Skipping test for Vulkan on Windows ... fails unless run on its own!";
+        }
+        if (target.has_feature(Target::WebGPU)) {
+            GTEST_SKIP() << "Allocation cache not yet implemented for WebGPU.";
+        }
+    }
+};
+}  // namespace
+
+TEST_F(GPUAllocationCacheTest, Basic) {
+    // Fixed size, overlapping lifetimes, looped kIters times. Should have 3 allocations live and OOM if there's a leak.
     Func f1[N];
     f1[0](x, y) = 1.0f;
-    f1[0].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     f1[1](x, y) = 2.0f;
-    f1[1].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     for (int i = 2; i < N; i++) {
         f1[i](x, y) = f1[i - 1](x, y) + f1[i - 2](x, y);
-        f1[i].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     }
 
-    // Decreasing size, overlapping lifetimes, looped 300 times. Should OOM on leak.
+    // Decreasing size, overlapping lifetimes, looped kIters times. Should OOM on leak.
     Func f2[N];
     f2[0](x, y) = 3.0f;
-    f2[0].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     f2[1](x, y) = 4.0f;
-    f2[1].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     for (int i = 2; i < N; i++) {
         f2[i](x, y) = f2[i - 1](x + 1, y) + f2[i - 2](x, y);
-        f2[i].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     }
 
+    // Increasing size, overlapping lifetimes, looped kIters times. Should OOM on leak.
     Func f3[N];
     f3[0](x, y) = 5.0f;
-    f3[0].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     f3[1](x, y) = 6.0f;
-    f3[1].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     for (int i = 2; i < N; i++) {
         f3[i](x, y) = f3[i - 1](x, clamp(y, 0, i)) + f3[i - 2](x, clamp(y, 0, i));
-        f3[i].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
     }
 
-    float correct1 = fib(N, 1, 2), correct2 = fib(N, 3, 4), correct3 = fib(N, 5, 6);
+    for (Func *fs : {f1, f2, f3}) {
+        for (int i = 0; i < N; i++) {
+            fs[i].compute_root().gpu_tile(x, y, xi, yi, 8, 8);
+        }
+    }
 
-    auto test1 = [&](bool use_cache, bool validate = true) {
-        Halide::Internal::JITSharedRuntime::reuse_device_allocations(use_cache);
+    float correct1 = fib(N, 1, 2);
+    float correct2 = fib(N, 3, 4);
+    float correct3 = fib(N, 5, 6);
 
-        for (int i = 0; i < 300; i++) {
-            Buffer<float> result = f1[N - 1].realize({128, 128});
-            if (validate) {
-                result.copy_to_host();
-                result.for_each_value([=](float f) {
-                    if (f != correct1) {
-                        printf("result is %f instead of %f\n", f, correct1);
-                        abort();
-                    }
-                });
-            } else {
-                result.device_sync();
-            }
+    std::optional<float> test1_incorrect{};
+    std::optional<float> test2_incorrect{};
+    std::optional<float> test3_incorrect{};
+
+    bool use_cache = true;
+
+    auto test_func = [&](Func f, float correct, std::optional<float> &incorrect) {
+        Internal::JITSharedRuntime::reuse_device_allocations(use_cache);
+
+        for (int i = 0; i < kIters; i++) {
+            Buffer<float> result = f.realize({128, 128});
+            result.copy_to_host();
+            result.for_each_value([&](float value) {
+                // Can't run GTest macros on other threads.
+                if (value != correct) {
+                    incorrect = value;
+                }
+            });
         }
         // We don't want the cache to persist across these tests
-        Halide::Internal::JITSharedRuntime::reuse_device_allocations(false);
+        Internal::JITSharedRuntime::reuse_device_allocations(false);
     };
 
-    auto test2 = [&](bool use_cache, bool validate = true) {
-        Halide::Internal::JITSharedRuntime::reuse_device_allocations(use_cache);
-
-        for (int i = 0; i < 300; i++) {
-            Buffer<float> result = f2[N - 1].realize({128, 128});
-            if (validate) {
-                result.copy_to_host();
-                result.for_each_value([=](float f) {
-                    if (f != correct2) {
-                        printf("result is %f instead of %f\n", f, correct2);
-                        abort();
-                    }
-                });
-            } else {
-                result.device_sync();
-            }
-        }
-        // We don't want the cache to persist across these tests
-        Halide::Internal::JITSharedRuntime::reuse_device_allocations(false);
-    };
-
-    auto test3 = [&](bool use_cache, bool validate = true) {
-        Halide::Internal::JITSharedRuntime::reuse_device_allocations(use_cache);
-        // Increasing size, overlapping lifetimes, looped 300 times. Should OOM on leak.
-        for (int i = 0; i < 300; i++) {
-            Buffer<float> result = f3[N - 1].realize({128, 128});
-            if (validate) {
-                result.copy_to_host();
-                result.for_each_value([=](float f) {
-                    if (f != correct3) {
-                        printf("result is %f instead of %f\n", f, correct3);
-                        abort();
-                    }
-                });
-            } else {
-                result.device_sync();
-            }
-        }
-        // We don't want the cache to persist across these tests
-        Halide::Internal::JITSharedRuntime::reuse_device_allocations(false);
-    };
-
-    // First run them serially (compilation of a Func isn't thread-safe).
-    // test1(true);
-    // test2(true);
-    // test3(true);
-    // return 0;
-
-    // Now run all at the same time to check for concurrency issues.
-
-    Halide::Tools::ThreadPool<void> pool(1);
+    Tools::ThreadPool<void> pool(1);
     std::vector<std::future<void>> futures;
-    futures.emplace_back(pool.async(test1, true));
-    futures.emplace_back(pool.async(test1, true));
-    futures.emplace_back(pool.async(test2, true));
-    futures.emplace_back(pool.async(test2, true));
-    futures.emplace_back(pool.async(test3, true));
-    futures.emplace_back(pool.async(test3, true));
+    futures.emplace_back(pool.async(test_func, f1[N - 1], correct1, test1_incorrect));
+    futures.emplace_back(pool.async(test_func, f1[N - 1], correct1, test1_incorrect));
+    futures.emplace_back(pool.async(test_func, f2[N - 1], correct2, test2_incorrect));
+    futures.emplace_back(pool.async(test_func, f2[N - 1], correct2, test2_incorrect));
+    futures.emplace_back(pool.async(test_func, f3[N - 1], correct3, test3_incorrect));
+    futures.emplace_back(pool.async(test_func, f3[N - 1], correct3, test3_incorrect));
     for (auto &f : futures) {
         f.get();
     }
 
-    // Now benchmark with and without, (just informational, as this isn't a performance test)
-    double t1 = Tools::benchmark([&]() {
-        test1(true, false);
-        test2(true, false);
-        test3(true, false);
-    });
-    double t2 = Tools::benchmark([&]() {
-        test1(false, false);
-        test2(false, false);
-        test3(false, false);
-    });
-    printf("Runtime with cache: %f\n"
-           "Without cache: %f\n",
-           t1, t2);
-
-    printf("Success!\n");
-    return 0;
+    EXPECT_EQ(test1_incorrect, std::nullopt);
+    EXPECT_EQ(test2_incorrect, std::nullopt);
+    EXPECT_EQ(test3_incorrect, std::nullopt);
 }
