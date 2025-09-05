5dbd927e4510f333c9ea99ed03d76056f435d5a0
Alex Reinking <areinking@adobe.com>
2025-09-05 00:03:45 -0400

    Convert many_small_extern_stages.cpp

--- test/correctness/many_small_extern_stages.cpp
+++ test/correctness/many_small_extern_stages.cpp
@@ -1,21 +1,10 @@
 #include "Halide.h"
-#include <stdio.h>
+#include <gtest/gtest.h>
 
-void dump_buffer_shape(halide_buffer_t *b) {
-    for (int i = 0; i < b->dimensions; i++) {
-        printf(" %d %d %d\n", b->dim[i].min, b->dim[i].extent, b->dim[i].stride);
-    }
-}
+using namespace Halide;
 
-extern "C" HALIDE_EXPORT_SYMBOL int copy(halide_buffer_t *in, halide_buffer_t *out) {
-
-    /*
-    printf("out:\n");
-    dump_buffer_shape(out);
-    printf("in:\n");
-    dump_buffer_shape(in);
-    */
-
+namespace {
+extern "C" HALIDE_EXPORT_SYMBOL int many_small_extern_stages_copy(halide_buffer_t *in, halide_buffer_t *out) {
     if (in->is_bounds_query()) {
         // Give it the same shape as the output
         in->dim[0] = out->dim[0];
@@ -26,30 +15,29 @@ extern "C" HALIDE_EXPORT_SYMBOL int copy(halide_buffer_t *in, halide_buffer_t *o
         // because we compute the input to the extern stage at the
         // same granularity as the extern stage.
 
-        assert(in->dim[0] == out->dim[0]);
-        assert(in->dim[1] == out->dim[1]);
+        EXPECT_EQ(in->dim[0], out->dim[0]);
+        EXPECT_EQ(in->dim[1], out->dim[1]);
 
         size_t sz = out->type.bytes() * out->dim[0].extent * out->dim[1].extent;
 
-        // Make sure we can safely do a dense memcpy. Should be true because the extent..
-        assert(out->dim[0].stride == 1 && out->dim[1].stride == out->dim[0].extent);
+        // Make sure we can safely do a dense memcpy. Should be true because of the extent.
+        ASSERT_EQ(out->dim[0].stride, 1) << "Bad stride", halide_error_code_bad_dimensions;
+        ASSERT_EQ(out->dim[1].stride, out->dim[0].extent) << "Bad stride", halide_error_code_bad_dimensions;
 
         memcpy(out->host, in->host, sz);
     }
-
     return 0;
 }
+}  // namespace
 
-using namespace Halide;
-
-int main(int argc, char **argv) {
+TEST(ManySmallExternStagesTest, Basic) {
     Func f, g, h;
     Var x, y;
 
     f(x, y) = x * x + y;
 
     // Name of the function and the args, then types of the outputs, then dimensionality
-    g.define_extern("copy", {f}, Int(32), 2);
+    g.define_extern("many_small_extern_stages_copy", {f}, Int(32), 2);
 
     RDom r(0, 100);
     h(x, y) += r * (g(x, y) - f(x, y));
@@ -59,16 +47,9 @@ int main(int argc, char **argv) {
 
     Buffer<int> result = h.realize({10, 10});
 
-    for (int y = 0; y < result.height(); y++) {
-        for (int x = 0; x < result.width(); x++) {
-            uint8_t correct = 0;
-            if (result(x, y) != 0) {
-                printf("result(%d, %d) = %d instead of %d\n", x, y, result(x, y), correct);
-                return 1;
-            }
+    for (int yy = 0; yy < result.height(); yy++) {
+        for (int xx = 0; xx < result.width(); xx++) {
+            EXPECT_EQ(result(xx, yy), 0) << "result(" << xx << ", " << yy << ")";
         }
     }
-
-    printf("Success!\n");
-    return 0;
 }
