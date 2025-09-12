8ae380fc565f9034ed1216f8505d8a558314c189
Alex Reinking <areinking@adobe.com>
2025-09-12 11:35:53 -0400

    Appease clang-tidy

--- src/Tracing.cpp
+++ src/Tracing.cpp
@@ -7,6 +7,7 @@
 #include "runtime/HalideRuntime.h"
 
 #include <unordered_set>
+#include <utility>
 
 namespace Halide {
 namespace Internal {
@@ -443,7 +444,7 @@ Stmt inject_tracing(Stmt s, const string &pipeline_name, bool trace_pipeline,
                 builder.func = func_name;
                 builder.trace_tag_expr =
                     Call::make(type_of<const char *>(), Call::stringify, strings, Call::PureIntrinsic);
-                return Block::make(Evaluate::make(builder.build()), s);
+                return Block::make(Evaluate::make(builder.build()), std::move(s));
             };
 
         for (const auto &o : reverse_view(order)) {
