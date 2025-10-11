4949046f829bf336f442de732420ef8ee9a01e78
Martijn Courteaux <courteauxmartijn@gmail.com>
2025-10-11 10:48:31 +0200

    Mimick code of CodeGen_Internal in CodeGen_PTX for making target machine

--- src/CodeGen_PTX_Dev.cpp
+++ src/CodeGen_PTX_Dev.cpp
@@ -606,11 +606,14 @@ vector<char> CodeGen_PTX_Dev::compile_to_src() {
 
     llvm::Triple triple(module->getTargetTriple());
 
-    // Allocate target machine
-
+    // Allocate target machine (similar to code in CodeGen_Internal.cpp make_target_machine)
     std::string err_str;
-    const llvm::Target *llvm_target = TargetRegistry::lookupTarget(triple, err_str);
+    auto triple = llvm::Triple(module.getTargetTriple());
+    const llvm::Target *llvm_target = TargetRegistry::lookupTarget(
+            module.getTargetTriple(),
+            err_str);
     internal_assert(llvm_target) << err_str << "\n";
+    internal_assert(llvm_target) << "Could not create LLVM target for " << triple.str() << "\n";
 
     TargetOptions options;
     options.AllowFPOpFusion = FPOpFusion::Fast;
