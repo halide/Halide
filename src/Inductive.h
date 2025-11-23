5bb3e06cfe4acc4a533313e57aae5be225bd305e
Steven Raphael <stevenraphael44@gmail.com>
2025-11-22 20:12:53 -0500

    clang-format

--- src/Inductive.h
+++ src/Inductive.h
@@ -10,15 +10,15 @@
 namespace Halide {
 namespace Internal {
 
-/** Given an initial box for an inductively defined function, 
+/** Given an initial box for an inductively defined function,
     returns an expanded box that includes the function's non-inductive base case. */
 const Box expand_to_include_base_case(const std::vector<std::string> &vars, const Expr &RHS, const std::string &func, const Box &box_required);
 
-const Box expand_to_include_base_case(const Function &fn, const Box &box_required, const int &pos=0);
+const Box expand_to_include_base_case(const Function &fn, const Box &box_required, const int &pos = 0);
 
 const Box expand_to_include_base_case(const Function &fn, const Box &box_required);
 
-} // namespace Internal
-} // namespace Halide
+}  // namespace Internal
+}  // namespace Halide
 
 #endif
