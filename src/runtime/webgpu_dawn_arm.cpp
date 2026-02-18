d388c42a95815a99fee429802a429118609b7bab
Shoaib Kamil <kamil@adobe.com>
2026-02-18 12:59:53 -0500

    Both native and wasm32 webgpu targets build and test

--- src/runtime/webgpu_dawn_arm.cpp
+++ src/runtime/webgpu_dawn_arm.cpp
@@ -8,5 +8,11 @@
 // go back to building this in a sane manner, but for now,
 // we use this sad-but-effective approach.
 
+#ifndef BITS_64
+// Don't emit compiler messages (via #pragma message) here: some environments might
+// treat compiler messages as warnings, which would then be escalated to errors since
+// we generally build with warnings-as-errors enabled.
+#else
 #define HALIDE_RUNTIME_WEBGPU_NATIVE_API 1
 #include "webgpu.cpp"
+#endif
