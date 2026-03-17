79ae701b723665752acf153ed2b9e09344517a1a
Martijn Courteaux <courteauxmartijn@gmail.com>
2026-03-17 12:29:32 +0100

    Shared fuzz expression generator. (#9048)
    * Shared fuzz expression generator.
    
    Took out the stuff from fuzz_simplifier, and fuzz_extract_lanes from the other PR.
    Now everything broke, because the new Expr generator produces MORE expressions, which uncovers many bugs.
    
    * Make the expression generator configurable.
    Disable expression types that trip the simplifier.

--- src/Deinterleave.cpp
+++ src/Deinterleave.cpp
@@ -256,7 +256,7 @@ private:
         if (base_lanes > 1) {
             if (new_lanes == 1) {
                 int index = starting_lane / base_lanes;
-                Expr expr = op->base + cast(op->base.type(), index) * op->stride;
+                Expr expr = simplify(op->base + cast(op->base.type(), index) * op->stride);
                 ScopedValue<int> old_starting_lane(starting_lane, starting_lane % base_lanes);
                 ScopedValue<int> old_lane_stride(lane_stride, base_lanes);
                 expr = mutate(expr);
@@ -274,10 +274,10 @@ private:
                 return mutate(flatten_nested_ramps(op));
             }
         }
-        Expr expr = op->base + cast(op->base.type(), starting_lane) * op->stride;
+        Expr expr = simplify(op->base + cast(op->base.type(), starting_lane) * op->stride);
         internal_assert(expr.type() == op->base.type());
         if (new_lanes > 1) {
-            expr = Ramp::make(expr, op->stride * lane_stride, new_lanes);
+            expr = Ramp::make(expr, simplify(op->stride * lane_stride), new_lanes);
         }
         return expr;
     }
@@ -410,7 +410,7 @@ Expr deinterleave(Expr e, int starting_lane, int lane_stride, int new_lanes, con
     Deinterleaver d(starting_lane, lane_stride, new_lanes, lets);
     e = d.mutate(e);
     e = common_subexpression_elimination(e);
-    return simplify(e);
+    return e;
 }
 
 Expr extract_odd_lanes(const Expr &e, const Scope<> &lets) {
