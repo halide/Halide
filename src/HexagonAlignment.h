9c8205f586e5fd193d028d400bd8d16b48d28197
Alex Reinking <areinking@adobe.com>
2026-04-07 16:43:44 -0400

    Simplify HexagonAlignmentAnalyzer to work around lossy cast warnings

--- src/HexagonAlignment.h
+++ src/HexagonAlignment.h
@@ -10,59 +10,40 @@
 namespace Halide {
 namespace Internal {
 
-// TODO: This class is barely stateful, and could probably be replaced with free functions.
-class HexagonAlignmentAnalyzer {
-    const int required_alignment;
-
-public:
-    HexagonAlignmentAnalyzer(int required_alignment)
-        : required_alignment(required_alignment) {
-        internal_assert(required_alignment != 0);
+template<typename T>
+bool is_hexagon_aligned(const T *op, int required_alignment, int *aligned_offset) {
+    int native_lanes;
+    if constexpr (std::is_same_v<T, Load>) {
+        native_lanes = required_alignment / op->type.bytes();
+    } else {
+        native_lanes = required_alignment / op->value.type().bytes();
     }
 
-    /** Analyze the index of a load/store instruction for alignment
-     *  Returns true if it can determining that the address of the store or load is aligned, false otherwise.
-     */
-    template<typename T>
-    bool is_aligned_impl(const T *op, int native_lanes, int64_t *aligned_offset) {
-        debug(3) << "HexagonAlignmentAnalyzer: Check if " << op->index << " is aligned to a "
-                 << required_alignment << " byte boundary\n"
-                 << "native_lanes: " << native_lanes << "\n";
-        Expr index = op->index;
-        const Ramp *ramp = index.as<Ramp>();
-        if (ramp) {
-            index = ramp->base;
-        } else if (index.type().is_vector()) {
-            debug(3) << "Is Unaligned\n";
-            return false;
-        }
+    debug(3) << "HexagonAlignmentAnalyzer: Check if " << op->index << " is aligned to a "
+             << required_alignment << " byte boundary\n"
+             << "native_lanes: " << native_lanes << "\n";
 
-        internal_assert(native_lanes != 0) << "Type is larger than required alignment of " << required_alignment << " bytes\n";
-
-        // If this is a parameter, the base_alignment should be
-        // host_alignment. Otherwise, this is an internal buffer,
-        // which we assume has been aligned to the required alignment.
-        if (op->param.defined() && ((op->param.host_alignment() % required_alignment) != 0)) {
-            return false;
-        }
-
-        bool known_alignment = (op->alignment.modulus % native_lanes) == 0;
-        if (known_alignment) {
-            *aligned_offset = op->alignment.remainder % native_lanes;
-        }
-        return known_alignment && (*aligned_offset == 0);
+    if (Expr index = op->index; !index.as<Ramp>() && index.type().is_vector()) {
+        debug(3) << "Is Unaligned\n";
+        return false;
     }
 
-    bool is_aligned(const Load *op, int64_t *aligned_offset) {
-        int native_lanes = required_alignment / op->type.bytes();
-        return is_aligned_impl<Load>(op, native_lanes, aligned_offset);
+    internal_assert(native_lanes != 0) << "Type is larger than required alignment of " << required_alignment << " bytes\n";
+
+    // If this is a parameter, the base_alignment should be
+    // host_alignment. Otherwise, this is an internal buffer,
+    // which we assume has been aligned to the required alignment.
+    if (op->param.defined() && ((op->param.host_alignment() % required_alignment) != 0)) {
+        return false;
     }
 
-    bool is_aligned(const Store *op, int64_t *aligned_offset) {
-        int native_lanes = required_alignment / op->value.type().bytes();
-        return is_aligned_impl<Store>(op, native_lanes, aligned_offset);
+    bool known_alignment = (op->alignment.modulus % native_lanes) == 0;
+    int64_t remainder = op->alignment.remainder % native_lanes;
+    if (known_alignment && aligned_offset != nullptr) {
+        *aligned_offset = static_cast<int>(remainder);
     }
-};
+    return known_alignment && remainder == 0;
+}
 
 }  // namespace Internal
 }  // namespace Halide
