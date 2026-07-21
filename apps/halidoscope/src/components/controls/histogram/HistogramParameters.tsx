9e80fb6e37eddcbcc69a6d8090cdf713cc57a5a1
Parker Ziegler <parkerellisziegler@gmail.com>
2026-07-21 10:12:43 -0700

    Add support for Thread Coverage rendering.

--- apps/halidoscope/src/components/controls/histogram/HistogramParameters.tsx
+++ apps/halidoscope/src/components/controls/histogram/HistogramParameters.tsx
@@ -7,7 +7,7 @@ import { funcAtom } from "@/state/func";
 import { histogramAtom, type HistogramScale } from "@/state/histogram";
 import { type NormalizationMode, renderAtom } from "@/state/render";
 
-function HistogramSelect() {
+function HistogramParameters() {
   const { funcs } = useTraceContext();
   const [activeFunc, setActiveFunc] = useAtom(funcAtom);
   const [histogram, setHistogram] = useAtom(histogramAtom);
@@ -28,26 +28,13 @@ function HistogramSelect() {
         >
           <Select.Trigger
             id="func-select"
-            className="bg-ps-border-primary text-ps-text-primary border-ps-border-tertiary inline-flex h-8 w-full items-center justify-center rounded border px-2 uppercase focus:outline-none"
+            className="bg-ps-border-primary text-ps-text-primary border-ps-border-tertiary inline-flex h-8 w-full items-center justify-center rounded border px-2 focus:outline-none"
           >
-            <span className="truncate">
+            <span className="truncate uppercase">
               <Select.Value />
             </span>
             <Select.Icon className="ml-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-3 w-3"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                />
-              </svg>
+              <ArrowDownIcon />
             </Select.Icon>
           </Select.Trigger>
           <Select.Content
@@ -60,7 +47,7 @@ function HistogramSelect() {
                 <Select.Item
                   key={func}
                   value={func}
-                  className="hover:bg-ps-border-tertiary cursor-pointer p-2 uppercase transition-colors"
+                  className="hover:bg-ps-border-tertiary cursor-pointer p-2 transition-colors"
                 >
                   <Select.ItemText className="uppercase">
                     {func}
@@ -172,4 +159,4 @@ function HistogramSelect() {
   );
 }
 
-export default HistogramSelect;
+export default HistogramParameters;
