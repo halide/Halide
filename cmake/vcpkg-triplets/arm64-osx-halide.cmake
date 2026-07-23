a3ec4284fce73fa6f019cdfc6b3bf07f7068012a
Alex Reinking <areinking@adobe.com>
2026-07-22 21:25:26 -0400

    Test upstream vcpkg port

--- cmake/vcpkg-triplets/arm64-osx-halide.cmake
+++ cmake/vcpkg-triplets/arm64-osx-halide.cmake
@@ -8,3 +8,9 @@ set(VCPKG_OSX_ARCHITECTURES arm64)
 if (PORT STREQUAL "dawn")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif ()
+
+# Skip Debug builds in CI: they're unused (we only ever load Release Dawn) and
+# ~2x the build time / ~30x the artifact size for no benefit.
+if (DEFINED ENV{CI})
+    set(VCPKG_BUILD_TYPE release)
+endif ()
--- cmake/vcpkg-triplets/x64-osx-halide.cmake
+++ cmake/vcpkg-triplets/x64-osx-halide.cmake
@@ -7,3 +7,9 @@ set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 if (PORT STREQUAL "dawn")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif ()
+
+# Skip Debug builds in CI: they're unused (we only ever load Release Dawn) and
+# ~2x the build time / ~30x the artifact size for no benefit.
+if (DEFINED ENV{CI})
+    set(VCPKG_BUILD_TYPE release)
+endif ()
