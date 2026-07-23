bc0b154ab590cb50be6f0e4ee5e3d962e1bfd257
Parker Ziegler <parkerellisziegler@gmail.com>
2026-07-23 15:30:02 -0700

    Back out changes to Python tracing infrastructure that are not on main and not used by Halidoscope.

--- python_bindings/halide/src/halide_/PyHalide.cpp
+++ python_bindings/halide/src/halide_/PyHalide.cpp
@@ -23,7 +23,6 @@
 #include "PyRDom.h"
 #include "PySerialization.h"
 #include "PyTarget.h"
-#include "PyTrace.h"
 #include "PyTuple.h"
 #include "PyType.h"
 #include "PyVar.h"
@@ -78,7 +77,6 @@ PYBIND11_MODULE(HALIDE_PYBIND_MODULE_NAME, m) {
     define_derivative(m);
     define_generator(m);
     define_serialization(m);
-    define_trace(m);
 
     // There is no PyUtil yet, so just put this here
     m.def("load_plugin", &Halide::load_plugin, py::arg("lib_name"));
