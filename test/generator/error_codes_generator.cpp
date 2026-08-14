888c4707cf53bcfc13698f52dd737b413b9d3114
Martijn Courteaux <courteauxmartijn@gmail.com>
2026-08-14 14:53:13 +0200

    Fixup the error-codes test.

--- test/generator/error_codes_generator.cpp
+++ test/generator/error_codes_generator.cpp
@@ -6,13 +6,13 @@ class ErrorCodes : public Halide::Generator<ErrorCodes> {
 public:
     Input<Buffer<int32_t, 2>> input{"input"};
     Input<int> f_explicit_bound{"f_explicit_bound", 1, 0, 64};
-    Output<Buffer<int32_t, 2>> output{"output"};
+    Output<Buffer<int32_t, 3>> output{"output"};
 
     void generate() {
         assert(!get_target().has_feature(Target::LargeBuffers));
-        Var x, y;
+        Var x, y, z;
 
-        output(x, y) = input(x, y);
+        output(x, y, z) = input(x, y);
         output.bound(x, 0, f_explicit_bound);
 
         add_requirement(input.dim(1).extent() == 123);
